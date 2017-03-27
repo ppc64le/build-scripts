b244edbb1f0f54856c7f3db200686d488d6a43c4
sowani <sowani@gmail.com>
2017-03-27 08:12:55 -0400

    Build scripts for ppc64le/Linux.

---
+++ bloom/bloom_ubuntu_16.04.sh
@@ -0,0 +1,32 @@
+# ----------------------------------------------------------------------------
+#
+# Package	: bloom
+# Version	: Not available.
+# Source repo	: https://github.com/ArashPartow/bloom
+# Tested on	: ubuntu_16.04
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Atul Sowani <sowania@us.ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+#!/bin/bash
+
+# Install dependencies.
+sudo apt-get update -y
+sudo apt-get install -y git make build-essential
+
+# Clone and build bloom.
+git clone https://github.com/ArashPartow/bloom bloom
+cd bloom
+make
+
+# Execute test examples.
+./bloom_filter_example01
+./bloom_filter_example02
+./bloom_filter_example03
