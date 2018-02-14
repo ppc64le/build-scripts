79ad63e1b41a6c5ebbf7365fd07930108b309fef
sowani <sowani@gmail.com>
2018-02-14 03:04:48 -0500

    Build scripts for ppc64le/Linux.

---
+++ jotted/jotted_rhel_7.4.sh
@@ -0,0 +1,35 @@
+# ----------------------------------------------------------------------------
+#
+# Package       : jotted
+# Version       : master
+# Source repo   : https://github.com/ghinda/jotted.git
+# Tested on     : rhel_7.4
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Snehlata Mohite <smohite@us.ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+
+WDIR=`pwd`
+sudo yum update -y
+sudo yum install -y git wget openssl-devel.ppc64le nodejs
+#install npm 
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh| sh
+source ~/.nvm/nvm.sh
+nvm install stable
+nvm use stable
+export PATH=./node_modules/.bin:$PATH
+export SAUCE_USERNAME=[secure]
+export SAUCE_ACCESS_KEY=[secure]
+
+cd $WDIR
+git clone https://github.com/ghinda/jotted.git
+cd $WDIR/jotted
+npm install
+npm test 
