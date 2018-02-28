944b51acbcc52fd85ffafdbe03019b791b45587d
sowani <sowani@gmail.com>
2018-02-28 06:45:58 -0500

    Build scripts for ppc64le/Linux.

---
+++ powermock/powermock_ubuntu_16.04.sh
@@ -0,0 +1,37 @@
+# ----------------------------------------------------------------------------
+#
+# Package       : Powermokito
+# Version       : 1.7.3
+# Source repo   : https://github.com/powermock/powermock.git
+# Tested on     : ubuntu_16.04
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Meghali Dhoble <dhoblem@us.ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# NOTE:	Please make sure to set the ENV value for 'GH_USER' to your
+#       github login. e.g. export GH_USER=user123
+# ----------------------------------------------------------------------------
+#!/bin/bash
+
+# Install Dependencies
+sudo apt-get update -y
+sudo apt-get install -y git openjdk-8-jdk
+
+# Download source
+git clone https://github.com/powermock/powermock.git
+cd powermock
+git checkout powermock-1.7.3
+
+#Setting environment variables
+export GH_USER=$GH_USER
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-ppc64el
+export PATH=$PATH:$JAVA_HOME/bin
+
+# Build
+./gradlew assemble -s -PcheckJava6Compatibility && \
+./gradlew check
