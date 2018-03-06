b1cddd8d801d4a9860d4075374d86b3e45380d57
sowani <sowani@gmail.com>
2018-03-05 23:26:28 -0500

    Build scripts for ppc64le/Linux.

--- mirdeep2/mirdeep2_ubuntu_16.04.sh
+++ mirdeep2/mirdeep2_ubuntu_16.04.sh
@@ -20,14 +20,19 @@ sudo apt-get update -y
 sudo apt-get install -y wget curl unzip python perl build-essential \
     libwww-perl libfont-ttf-perl libpdf-api2-perl
 export PERL_MM_USE_DEFAULT=1
-wget https://www.mdc-berlin.de/45995549/en/research/research_teams/systems_biology_of_gene_regulatory_elements/projects/miRDeep/mirdeep2_0_0_8.zip
-unzip mirdeep2_0_0_8.zip
-cd mirdeep2_0_0_8
+
+# Source zip is not available any more, need to check out from git.
+#wget https://www.mdc-berlin.de/45995549/en/research/research_teams/systems_biology_of_gene_regulatory_elements/projects/miRDeep/mirdeep2_0_0_8.zip
+#unzip mirdeep2_0_0_8.zip
+#cd mirdeep2_0_0_8
+
+git clone https://github.com/rajewsky-lab/mirdeep2
+cd mirdeep2
 
 # Changes required to build mirdeep2 on ppc64le.
 sed -i -e "s/if(not -d 'bin'){/\$dtool ='wget';\nif(not -d 'bin'){/" install.pl
-sed -i -e 's/\$err=system("\$dtool http:\/\/netcologne.dl.sourceforge.net\/project\/bowtie-bio\/bowtie\/\$bowtie_version\/\$bowtie \$dopt");/\$err=system("\$dtool http:\/\/netcologne.dl.sourceforge.net\/project\/bowtie-bio\/bowtie\/\$bowtie_version\/\$bowtie \$dopt \$bowtie");/' install.pl
-sed -i -e 's/\$err=system("\$dtool http:\/\/netcologne.dl.sourceforge.net\/project\/bowtie-bio\/bowtie\/old\/\$bowtie_version\/\$bowtie \$dopt");/\$err=system("$dtool http:\/\/netcologne.dl.sourceforge.net\/project\/bowtie-bio\/bowtie\/old\/\$bowtie_version\/\$bowtie \$dopt \$bowtie");/' install.pl
+#sed -i -e 's/\$err=system("\$dtool http:\/\/netcologne.dl.sourceforge.net\/project\/bowtie-bio\/bowtie\/\$bowtie_version\/\$bowtie \$dopt");/\$err=system("\$dtool http:\/\/netcologne.dl.sourceforge.net\/project\/bowtie-bio\/bowtie\/\$bowtie_version\/\$bowtie \$dopt \$bowtie");/' install.pl
+#sed -i -e 's/\$err=system("\$dtool http:\/\/netcologne.dl.sourceforge.net\/project\/bowtie-bio\/bowtie\/old\/\$bowtie_version\/\$bowtie \$dopt");/\$err=system("$dtool http:\/\/netcologne.dl.sourceforge.net\/project\/bowtie-bio\/bowtie\/old\/\$bowtie_version\/\$bowtie \$dopt \$bowtie");/' install.pl
 sed -i -e 's/\$err=system("\$dtool http:\/\/www.tbi.univie.ac.at\/RNA\/packages\/source\/ViennaRNA-1.8.4.tar.gz \$dopt");/\$err=system("\$dtool http:\/\/www.tbi.univie.ac.at\/RNA\/packages\/source\/ViennaRNA-1.8.4.tar.gz \$dopt \$dfile");/' install.pl
 sed -i -e 's/`.\/configure --prefix=\$dir\/essentials\/ViennaRNA-1.8.4\/install_dir`;/`.\/configure --prefix=\$dir\/essentials\/ViennaRNA-1.8.4\/install_dir --build=ppc64le`;/' install.pl
 sed -i -e "s/\`\$dtool http:\/\/eddylab.org\/software\/squid\/squid.tar.gz \$dopt\`;/\`\$dtool http:\/\/eddylab.org\/software\/squid\/squid.tar.gz \$dopt \$dfile\`;/" install.pl
@@ -35,4 +40,4 @@ sed -i -e "s/\`\$dtool http:\/\/bioinformatics.psb.ugent.be\/supplementary_data\
 
 ./install.pl
 source $HOME/.bash_profile
-./install.pl
+perl ./install.pl
