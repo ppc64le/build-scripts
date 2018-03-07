ff674f0ec0020f01057329f68601b64ed79152ff
Priya Seth <sethp@us.ibm.com>
2018-03-07 10:02:08 +0000

    Update lightGBM_ubuntu_16.04_gpu.sh

--- lightGBM/lightGBM_ubuntu_16.04_gpu.sh
+++ lightGBM/lightGBM_ubuntu_16.04_gpu.sh
@@ -31,10 +31,7 @@ cd LightGBM
 mkdir build ; cd build
 
 #Disable altivec support
-export CFLAGS="-mno-altivec -mno-vsx"
-export CXXFLAGS="-mno-altivec -mno-vsx"
-
-cmake -DUSE_GPU=1 ..
+cmake -DUSE_GPU=1 -DCMAKE_C_FLAGS="-mno-altivec -mno-vsx" -DCMAKE_CXX_FLAGS="-mno-altivec -mno-vsx" ..
 make -j$(nproc)
 cd ..
 
