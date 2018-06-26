fe55266c16ab42737cf0f70e1bb49b020bde9830
sowani <sowani@gmail.com>
2018-06-26 06:03:49 -0400

    Build scripts for ppc64le/Linux.

---
+++ redis/redis_rhel_7.4.sh
@@ -0,0 +1,27 @@
+# ----------------------------------------------------------------------------
+#
+# Package	: redis
+# Version	: 3.2.9
+# Source repo	: https://github.com/antirez/redis.git
+# Tested on	: rhel_7.4
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Atul Sowani <sowania@us.ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+
+# Install dependencies.
+sudo yum update -y
+sudo yum install -y git tcl
+sudo yum group install -y 'Development Tools'
+
+# Clone and build source.
+git clone https://github.com/antirez/redis.git
+cd redis
+sudo make install
---
+++ redis/redis_ubuntu_16.04.sh
@@ -0,0 +1,29 @@
+# ----------------------------------------------------------------------------
+#
+# Package	: redis
+# Version	: 3.2.9
+# Source repo	: https://github.com/antirez/redis.git
+# Tested on	: ubuntu_16.04
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Atul Sowani <sowania@us.ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+
+# Install dependencies.
+sudo apt-get update -y
+sudo apt-get install -y git build-essential tcl
+
+# Clone and build source.
+git clone https://github.com/antirez/redis.git
+cd redis
+make distclean
+make V=1
+make test
+sudo make install
