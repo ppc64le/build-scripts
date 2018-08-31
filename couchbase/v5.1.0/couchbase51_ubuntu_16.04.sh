2a5a87ff85fc1bfcffd794d0255cccb869bb791c
sowani <sowani@gmail.com>
2018-08-31 05:13:22 -0400

    Build scripts for ppc64le/Linux.

--- couchbase/v5.1.0/couchbase51_ubuntu_16.04.sh
+++ couchbase/v5.1.0/couchbase51_ubuntu_16.04.sh
@@ -1,13 +1,28 @@
+# ----------------------------------------------------------------------------
+#
+# Package	: couchbase
+# Version	: 5.1
+# Source repo	: https://github.com/couchbase
+# Tested on	: ubuntu_16.04
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Atul Sowani <sowania@us.ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install -y wget tar git g++ make curl libssl-dev libevent-dev \
+apt-get update -y
+apt-get install -y wget tar git g++ make curl libssl-dev libevent-dev \
      libcurl4-openssl-dev libsnappy-dev ncurses-dev openssl libiodbc2-dev \
-     cmake python sudo golang-go subversion cmake openjdk-8-jdk \
-     openjdk-8-jre
+     autoconf cmake libtool python python-dev golang-go subversion cmake \
+     gnupg openjdk-8-jdk openjdk-8-jre lsb-release
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-ppc64el
-
 WDIR=`pwd`
 
 # Build and install Erlang 17.4
@@ -20,37 +35,43 @@ ERL_TOP=$WDIR/otp_src_17.4
 cd otp_src_17.4
 ./configure --build=ppc64le-unknown-linux-gnu --prefix=/usr
 make
-sudo make install
+make install
+cd ..
+rm -rf otp_src_17.4
 unset ERL_TOP
-
-# Build and install V8
-echo " ------- building v8 ------- "
-cd $WDIR
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH=$WDIR/depot_tools:$PATH
-#gclient
-cd $WDIR
-fetch v8
-cd v8
-gclient sync
-python gypfiles/gyp_v8
-sudo make -j4 ppc64.release -i werror=no
-sudo make -j4 ppc64.release library=shared -i werror=no
-sudo cp -vR include/* /usr/include
-sudo cp -v out/ppc64.release/lib.target/lib*.so /usr/local/lib
-sudo cp /usr/local/lib/libv8.so /usr/lib
-sudo cp -v out/ppc64.release/obj.target/tools/gyp/lib*.a /usr/local/lib
-export LD_LIBRARY_PATH=/usr/local/lib
 
 # Build and install ICU 58-1 (default does not work)
 echo " ------- building icu ------- "
 cd $WDIR
 svn export http://source.icu-project.org/repos/icu/tags/release-58-1/ icu
 cd icu/icu4c/source
-./configure --enable-static --prefix=/usr
+./configure --enable-static --disable-renaming --prefix=/usr
 make
-sudo make install
-sudo cp /usr/lib/libicu* /usr/local/lib
+make install
+cp /usr/lib/libicu* /usr/local/lib
+cd $WDIR
+rm -rf icu
+
+# Build and install V8
+echo " ------- building v8 ------- "
+cd $WDIR
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$WDIR/depot_tools:$PATH
+export VPYTHON_BYPASS="manually managed python not supported by chrome operations"
+
+cd $WDIR
+fetch v8
+cd v8
+git checkout 5.8.75
+gclient sync
+python gypfiles/gyp_v8
+make -j4 ppc64.release -i werror=no GYPFLAGS+="-Dcomponent=shared_library -Dv8_enable_i18n_support=0"
+make -j4 ppc64.release library=shared -i werror=no GYPFLAGS+="-Dcomponent=shared_library -Dv8_enable_i18n_support=0"
+cp -vR include/* /usr/include
+cp -v out/ppc64.release/lib.target/lib*.so /usr/local/lib
+cp /usr/local/lib/libv8.so /usr/lib
+cp -v out/ppc64.release/obj.target/tools/gyp/lib*.a /usr/local/lib
+export LD_LIBRARY_PATH=/usr/local/lib
 
 # Install flatbuffers
 echo " ------- building flatbuffers ------- "
@@ -60,27 +81,32 @@ cd flatbuffers
 cmake -G "Unix Makefiles"
 make
 export PATH=$PATH:`pwd`
-sudo make install
+make install
+cd ..
+rm -rf flatbuffers
 
 # Copy go compilers in .cbdepscache/exploded
+echo " ------- installing go 1.7.6 ------- "
 mkdir -p ~/.cbdepscache/exploded/ppc64le
 wget https://dl.google.com/go/go1.7.6.linux-ppc64le.tar.gz
 tar -xzf go1.7.6.linux-ppc64le.tar.gz
-mv go go-1.7.6
-mv go-1.7.6 ~/.cbdepscache/exploded/ppc64le
+mkdir ~/.cbdepscache/exploded/ppc64le/go-1.7.6
+mv go ~/.cbdepscache/exploded/ppc64le/go-1.7.6
 ln -s ~/.cbdepscache/exploded/ppc64le/go-1.7.6 ~/.cbdepscache/exploded/ppc64le/go-1.7.3
 rm go1.7.6.linux-ppc64le.tar.gz
 
+echo " ------- installing go 1.8.1 ------- "
 wget https://dl.google.com/go/go1.8.1.linux-ppc64le.tar.gz
 tar -xzf go1.8.1.linux-ppc64le.tar.gz
-mv go go-1.8.1
-mv go-1.8.1 ~/.cbdepscache/exploded/ppc64le
+mkdir ~/.cbdepscache/exploded/ppc64le/go-1.8.1
+mv go ~/.cbdepscache/exploded/ppc64le/go-1.8.1
 rm go1.8.1.linux-ppc64le.tar.gz
 
+echo " ------- installing go 1.8.5 ------- "
 wget https://dl.google.com/go/go1.8.5.linux-ppc64le.tar.gz
 tar -xzf go1.8.5.linux-ppc64le.tar.gz
-mv go go-1.8.5
-mv go-1.8.5 ~/.cbdepscache/exploded/ppc64le
+mkdir ~/.cbdepscache/exploded/ppc64le/go-1.8.5
+mv go ~/.cbdepscache/exploded/ppc64le/go-1.8.5
 ln -s ~/.cbdepscache/exploded/ppc64le/go-1.8.5 ~/.cbdepscache/exploded/ppc64le/go-1.8.3
 rm go1.8.5.linux-ppc64le.tar.gz
 
@@ -109,18 +135,19 @@ cd ../../..
 
 # apply patches, copy crc files.
 echo "------- applying patch -------"
-tar -xzf $WDIR/crc-files.tar.gz
-mv $WDIR/crc-files/crc32_constants.h platform/include/platform/crc32_constants.h
-mv $WDIR/crc-files/crc32_wrapper.c platform/src/crc32_wrapper.c
-mv $WDIR/crc-files/crc32.S platform/src/crc32.S
-mv $WDIR/crc-files/ppc-opcode.h platform/include/platform/ppc-opcode.h
-rmdir $WDIR/crc-files
-patch -p2 < $WDIR/patch
+tar -xzf /tmp/crc-files.tar.gz -C /tmp
+mv /tmp/crc-files/crc32_constants.h platform/include/platform/crc32_constants.h
+mv /tmp/crc-files/crc32_wrapper.c platform/src/crc32_wrapper.c
+mv /tmp/crc-files/crc32.S platform/src/crc32.S
+mv /tmp/crc-files/ppc-opcode.h platform/include/platform/ppc-opcode.h
+rmdir /tmp/crc-files
+patch -p2 < /tmp/patch
 
 # build couchbase
 echo "------- building tlm deps -------"
 cd tlm/deps/packages
 
+echo "------- building boost -------"
 mkdir build-boost
 cd build-boost
 cmake .. -DPACKAGE=boost
@@ -130,7 +157,9 @@ m_filename=`ls deps/boost/*/boost*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
+rm -rf build-boost
 
+echo "------- building curl -------"
 mkdir build-curl
 cd build-curl
 cmake .. -DPACKAGE=curl
@@ -140,7 +169,9 @@ m_filename=`ls deps/curl/*/curl*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
+rm -rf build-curl
 
+echo "------- building erlang -------"
 mkdir build-erlang
 cd build-erlang
 cmake .. -DPACKAGE=erlang
@@ -150,7 +181,9 @@ m_filename=`ls deps/erlang/*/erlang*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
+rm -rf build-erlang
 
+echo "------- building flatbuffers -------"
 mkdir build-flatbuffers
 cd build-flatbuffers
 cmake .. -DPACKAGE=flatbuffers
@@ -160,7 +193,9 @@ m_filename=`ls deps/flatbuffers/*/flatbuffers*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
+rm -rf build-flatbuffers
 
+echo "------- building icu4c -------"
 mkdir build-icu4c
 cd build-icu4c
 cmake .. -DPACKAGE=icu4c
@@ -170,7 +205,9 @@ m_filename=`ls deps/icu4c/*/icu4c*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
+rm -rf build-icu4c
 
+echo "------- building jemalloc -------"
 mkdir build-jemalloc
 cd build-jemalloc
 cmake .. -DPACKAGE=jemalloc
@@ -180,7 +217,9 @@ m_filename=`ls deps/jemalloc/*/jemalloc*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
+rm -rf build-jemalloc
 
+echo "------- building json -------"
 mkdir build-json
 cd build-json
 cmake .. -DPACKAGE=json
@@ -190,7 +229,9 @@ m_filename=`ls deps/json/*/json*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
+rm -rf build-json
 
+echo "------- building libcouchbase -------"
 mkdir build-libcouchbase
 cd build-libcouchbase
 cmake .. -DPACKAGE=libcouchbase
@@ -200,7 +241,9 @@ m_filename=`ls deps/libcouchbase/*/libcouchbase*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
+rm -rf build-libcouchbase
 
+echo "------- building libevent -------"
 mkdir build-libevent
 cd build-libevent
 cmake .. -DPACKAGE=libevent
@@ -210,7 +253,9 @@ m_filename=`ls deps/libevent/*/libevent*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
+rm -rf build-libevent
 
+echo "------- building snappy -------"
 mkdir build-snappy
 cd build-snappy
 cmake .. -DPACKAGE=snappy
@@ -220,7 +265,9 @@ m_filename=`ls deps/snappy/*/snappy*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
+rm -rf build-snappy
 
+echo "------- building python-snappy -------"
 mkdir build-python-snappy
 cd build-python-snappy
 cmake .. -DPACKAGE=python-snappy
@@ -230,7 +277,9 @@ m_filename=`ls deps/python-snappy/*/python-snappy*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
+rm -rf build-python-snappy
 
+echo "------- building rocksdb -------"
 mkdir build-rocksdb
 cd build-rocksdb
 cmake .. -DPACKAGE=rocksdb
@@ -240,17 +289,18 @@ m_filename=`ls deps/rocksdb/*/rocksdb*.md5`
 cp $t_filename ~/.cbdepscache
 cp $m_filename ~/.cbdepscache/`basename $t_filename`.md5
 cd ..
-
+rm -rf build-rocksdb
 cd ../../..
 
 echo "------- starting cb build -------"
 cd ..
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=Debug ../couchbase
+cmake -D CMAKE_BUILD_TYPE=Debug -DU_DISABLE_RENAMING=1 ../couchbase
 make all
 
 # build test binaries, which do not get built automatically.
+echo "------- building test binaries -------"
 make benchmark_test
 make basic_test
 make filter_test
@@ -396,6 +446,6 @@ make vbucket_testketama
 echo "------- Couchbase build completed -------"
 echo "------- starting tests -------"
 
-time ctest > ctest.log
+time ctest
 cd ..
 echo "------- tests completed -------"
