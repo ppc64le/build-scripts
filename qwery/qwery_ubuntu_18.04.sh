8200a1dfb307f291d038340ed8e006c338c0fddd
Atul Sowani <sowania@us.ibm.com>
2018-09-19 10:26:27 +0530

    ppc64le port.

---
+++ qwery/qwery_ubuntu_18.04.sh
@@ -0,0 +1,24 @@
+# ----------------------------------------------------------------------------
+#
+# Package       : qwery
+# Version       : 4.0.0
+# Source repo   : https://github.com/ded/qwery
+# Tested on     : ubuntu_18.04
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Sandip Giri <sgiri@us.ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+
+# Install dependencies.
+sudo apt-get update -y
+sudo apt-get install -y nodejs npm 
+
+# Build
+npm install qwery
