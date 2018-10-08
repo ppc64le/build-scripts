ecae925ff890b9ad12d8f78fc85a34a58e758278
Priya Seth <sethp@us.ibm.com>
2018-10-08 06:00:48 -0400

    build script for invariant-v2.2.4 for ppc64le/linux

---
+++ invariant/invariant_ubuntu_18.04.sh
@@ -0,0 +1,24 @@
+# ----------------------------------------------------------------------------
+#
+# Package       : invariant
+# Version       : v2.2.4
+# Source repo   : https://github.com/zertosh/invariant
+# Tested on     : ubuntu_18.04
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Sandip Giri <sgiri@us.ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+
+# Install dependencies.
+sudo apt-get update -y
+sudo apt-get install -y nodejs npm
+
+# Build Package
+npm install invariant
