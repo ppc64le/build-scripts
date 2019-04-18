ba939b66033653490ce2836e6c47b4d68369cc5d
ghatwala <ghatwala@us.ibm.com>
2019-04-18 15:10:34 +0530

    Adding missing gpg keys
    To resolve below error
    
    # gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
    gpg: directory `/root/.gnupg' created
    gpg: new configuration file `/root/.gnupg/gpg.conf' created
    gpg: WARNING: options in `/root/.gnupg/gpg.conf' are not yet active during this run
    gpg: keyring `/root/.gnupg/secring.gpg' created
    gpg: keyring `/root/.gnupg/pubring.gpg' created
    gpg: requesting key D39DC0E3 from hkp server keys.gnupg.net
    gpg: /root/.gnupg/trustdb.gpg: trustdb created
    gpg: key D39DC0E3: public key "Michal Papis (RVM signing) <mpapis@gmail.com>" imported
    gpg: no ultimately trusted keys found
    gpg: Total number processed: 1
    gpg:               imported: 1  (RSA: 1)
    
    # curl -L https://get.rvm.io | bash -s stable
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   194  100   194    0     0    529      0 --:--:-- --:--:-- --:--:--   528
    100 24173  100 24173    0     0  58181      0 --:--:-- --:--:-- --:--:-- 58181
    Downloading https://github.com/rvm/rvm/archive/1.29.7.tar.gz
    Downloading https://github.com/rvm/rvm/releases/download/1.29.7/1.29.7.tar.gz.asc
    gpg: Signature made Thu Jan  3 22:01:48 2019 UTC using RSA key ID 39499BDB
    gpg: Can't check signature: No public key
    GPG signature verification failed for '/usr/local/rvm/archives/rvm-1.29.7.tgz' - 'https://github.com/rvm/rvm/releases/download/1.29.7/1.29.7.tar.gz.asc'! Try to install GPG v2 and then fetch the public key:
    
        gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
    
    or if it fails:
    
        command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
        command curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
    
    In case of further problems with validation please refer to https://rvm.io/rvm/security
    
    
    Below worked after adding key "7D2BAF1CF37B13E2069D6956105BD0E739499BDB" 
    # gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
    gpg: requesting key D39DC0E3 from hkp server pool.sks-keyservers.net
    gpg: requesting key 39499BDB from hkp server pool.sks-keyservers.net
    gpg: key D39DC0E3: "Michal Papis (RVM signing) <mpapis@gmail.com>" not changed
    gpg: key 39499BDB: public key "Piotr Kuczynski <piotr.kuczynski@gmail.com>" imported
    gpg: no ultimately trusted keys found
    gpg: Total number processed: 2
    gpg:               imported: 1  (RSA: 1)
    gpg:              unchanged: 1
    
    # curl -L https://get.rvm.io | bash -s stable
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   194  100   194    0     0   1391      0 --:--:-- --:--:-- --:--:--  1385
    100 24173  100 24173    0     0   123k      0 --:--:-- --:--:-- --:--:--  123k
    Downloading https://github.com/rvm/rvm/archive/1.29.7.tar.gz
    Downloading https://github.com/rvm/rvm/releases/download/1.29.7/1.29.7.tar.gz.asc
    gpg: Signature made Thu Jan  3 22:01:48 2019 UTC using RSA key ID 39499BDB
    gpg: Good signature from "Piotr Kuczynski <piotr.kuczynski@gmail.com>"
    gpg: WARNING: This key is not certified with a trusted signature!
    gpg:          There is no indication that the signature belongs to the owner.
    Primary key fingerprint: 7D2B AF1C F37B 13E2 069D  6956 105B D0E7 3949 9BDB
    GPG verified '/usr/local/rvm/archives/rvm-1.29.7.tgz'

--- fluentd/fluentd_ubuntu_16.04.sh
+++ fluentd/fluentd_ubuntu_16.04.sh
@@ -21,7 +21,7 @@ sudo apt-get update -y
 sudo apt-get install -y git build-essential ruby-dev ruby gnupg2 curl
 
 # Install ruby and rvm.
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -L https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 rvm install 2.4.0
