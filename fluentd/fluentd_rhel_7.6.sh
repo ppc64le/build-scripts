b2fec3564874bc036d0489588bf16ab3d729e19e
ghatwala <ghatwala@us.ibm.com>
2019-04-29 14:11:14 +0530

    Adding fluent build script for RHEL
    Adding "fluentd_rhel_7.6.sh" build script for RHEL 7.6 distribution for version 1.4.2

---
+++ fluentd/fluentd_rhel_7.6.sh
@@ -0,0 +1,33 @@
+# ----------------------------------------------------------------------------
+#
+# Package	: fluentd
+# Version	: v1.4.2 
+# Source repo	: https://github.com/fluent/fluentd
+# Tested on	: RHEL 7.6
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Amit Ghatwal <ghatwala@us.ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+# Install dependencies.
+sudo yum update -y
+sudo yum install -y git build-essential ruby-dev ruby gnupg2 curl
+
+# Install ruby and rvm.
+gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -L https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.4.6
+rvm use 2.4.6 --default
+ruby -v
+gem install bundler
+
+# Clone and build source.
+git clone https://github.com/fluent/fluentd && cd fluentd
+bundle install --path=./vendor/bundle && bundle exec rake
