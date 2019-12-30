9f8bd0cfb1b61f8091e1575304dfdfb806eed04e
harinreddy <hnredy@us.ibm.com>
2019-12-29 20:07:51 -0500

    Build script for integrating LightGBM-CUDA with H2o

--- lightGBM/h2o_lightgbm_integrate.sh
+++ lightGBM/h2o_lightgbm_integrate.sh
@@ -7,10 +7,11 @@
 # Description:  : This script integrates native cuda enabled lighgbm package into 
 #                 the H2O installation specified in this script 
 # Source repo for lightgbm   	: https://github.com/bordaw/LightGBM-CUDA
+# Maintainer    : Rajesh Bordawekar <bordaw@us.ibm.com>
 # Source repo for this script	: https://github.com/ppc64le/build-scripts/tree/master/lightGBM/h2o_lightgbm_integrate.sh
+# Maintainer    : Hari Reddy <hnreddy@us.ibm.com>
 # Tested on     : RHEL_7.6
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Hari Reddy <hnreddy@us.ibm.com>
 #
 # Disclaimer: This script has been tested in non-root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -32,6 +33,15 @@
 #          Make sure the lig_lightgbm.so file is copied to the proper location
 #          Example:   dai-1.8.0-linux-ppc64le/cuda-10.0/lib/python3.6/site-packages/lightgbm_cpu/lib_lightgbm.so
 
+	
+#	prior to launching a H2O job, please update the following file located in the H2O installation directory
+#			config.toml
+#	with the following setting for "params_lightgbm" parameter
+
+#	params_lightgbm = "{'num_gpu': '2', 'metric': 'l2', 'metric_freq': 1, 'max_bin': 255, 'min_sum_hessian_in_leaf':5.0, 'min_data_in_leaf':50, 'min_sum_hessian_in_leaf':100, 'num_leaves': 255, 'learning_rate': 0.1, 'feature_fraction': 0.8, 'bagging_fraction': 0.8, 'bagging_freq': 5, 'verbose': 1, 'num_iterations':100, 'device_type':'cuda', 'device': 'cuda', 'num_leaves': 63, 'max_depth': 6, 'gpu_use_dp': 'true', 'is_enable_sparse': 'false'}"
+
+#	This restriction will be removed in future updates
+ 	
 
 CURRENT_DIR=$(pwd)
 LIGHTGBM_DIR=$CURRENT_DIR/python-package
