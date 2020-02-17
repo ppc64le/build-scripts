4582e6bd2e743e30d8c3ba781717c8784785e422
Ryan D'Mello <Ryan.Dmello1@ibm.com>
2020-02-17 14:07:40 +0530

    Cleaning directories if exists

--- awx/ansible-awx_rhel_7.7.sh
+++ awx/ansible-awx_rhel_7.7.sh
@@ -21,22 +21,34 @@ set -e
 CURRENT_DIR=`pwd`
 # Install all dependencies.
 sudo yum -y update
-sudo yum -y install git curl gcc python3-devel libffi-devel openssl-devel make ansible python3-pip
- 
+sudo yum -y install git curl gcc python3-devel libffi-devel openssl-devel make python3-pip
+
+# Install ansible
+pip3 install ansible
+
 #The above pulls Intel specific rabbitmq image from DockerHub instead of being built locally.
 #Build rabbitmq on Power and retag it.
 cd $CURRENT_DIR
+docker rmi ansible/awx_rabbitmq:3.7.4 ansible/awx_rabbitmq:latest -f
+# check if source code already checked out
+if [ -d "$CURRENT_DIR/awx-rabbitmq" ]; then
+    # if source code already exists remove the directory
+    rm -rf "$CURRENT_DIR/awx-rabbitmq/"
+fi
 git clone https://github.com/ansible/awx-rabbitmq
 cd awx-rabbitmq/
-docker rmi ansible/awx_rabbitmq:3.7.4 ansible/awx_rabbitmq:latest -f
 make
 docker tag ansible/awx_rabbitmq:3.7.21 ansible/awx_rabbitmq:3.7.4
 
 # Clone and build missing dependencies from source.
 cd $CURRENT_DIR
+# check if source code already checked out
+if [ -d "$CURRENT_DIR/awx" ]; then
+    rm -rf "$CURRENT_DIR/awx"
+fi
 git clone https://github.com/seth-priya/awx.git
 cd awx
 git apply ../awx_ppc64le.diff
-pip install docker-compose
+pip3 install docker-compose
 cd installer
 ansible-playbook -i inventory install.yml
