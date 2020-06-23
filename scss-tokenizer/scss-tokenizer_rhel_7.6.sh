93d0cb3bdc1a5c6a49ded1d440fa069f178805c9
Amol Patil <amol.patil2@ibm.com>
2020-06-23 00:54:50 -0400

    Build Scripts for NPM packages - scss-tokenizer, supports-color, url

---
+++ scss-tokenizer/scss-tokenizer_rhel_7.6.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+#
+# Package       : scss-tokenizer
+# Version       : v0.4.2
+# Source        : https://github.com/sasstools/scss-tokenizer.git
+# Tested on     : RHEL 7.6
+# Node Version  : v12.16.1
+# Maintainer    : Amol Patil <amol.patil2@ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+# ----------------------------------------------------------------------------
+
+set -e
+
+# Install all dependencies.
+sudo yum clean all
+sudo yum -y update
+
+PACKAGE_VERSION=v0.4.2
+
+#Install nvm
+if [ ! -d ~/.nvm ]; then
+        #Install the required dependencies
+        sudo yum install -y openssl-devel.ppc64le curl git 
+        curl https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+fi
+
+source ~/.nvm/nvm.sh
+
+#Install node version v12.16.1
+if [ `nvm list | grep -c "v12.16.1"` -eq 0 ]
+then
+        nvm install v12.16.1
+fi
+
+nvm alias default v12.16.1
+
+git clone https://github.com/sasstools/scss-tokenizer.git && cd scss-tokenizer
+git checkout $PACKAGE_VERSION
+
+sed -i -e 's/lib/src/g' index.js
+
+npm install
+npm test
+
