8aba1ceeaa9158b0a6491196b9e42d9c81e39cf2
Amol Patil <amol.patil2@ibm.com>
2020-06-25 09:08:09 -0400

    Build script for NPM package - stdout-stream v1.4.1

---
+++ stdout-stream/stdout-stream_rhel_7.6.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+#
+# Package       : stdout-stream  
+# Version       : v1.4.1
+# Source        : https://github.com/mafintosh/stdout-stream.git
+# Tested on     : RHEL 7.6
+# Node Version  : v12.16.1
+# Maintainer    : Amol Patil <amol.patil2@ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+# ----------------------------------------------------------------------------
+
+set -e
+
+# Install all dependencies.
+sudo yum clean all
+sudo yum -y update
+
+PACKAGE_VERSION=v1.4.1
+
+#Install nvm
+if [ ! -d ~/.nvm ]; then
+        #Install the required dependencies
+        sudo yum install -y openssl-devel.ppc64le curl git 
+        curl https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+fi
+
+source ~/.nvm/nvm.sh
+
+#Install node version v12.16.1
+if [ `nvm list | grep -c "v12.16.1"` -eq 0 ]
+then
+        nvm install v12.16.1
+fi
+
+nvm alias default v12.16.1
+
+git clone https://github.com/mafintosh/stdout-stream.git && cd stdout-stream
+git checkout $PACKAGE_VERSION
+
+
+npm install npm@6.14.5
+sed -i -e 's/Buffer.concat(buf)/Buffer.from(buf[0])/g'  test/index.js
+
+npm install
+npm test
+
