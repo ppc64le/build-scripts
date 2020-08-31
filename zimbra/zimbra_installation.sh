2fd5bb7b3191d1969f5c1c6e93c55c10b80cb809
Kandarpa Malipeddi <kandarpa.malipeddi@ibm.com>
2020-08-31 14:02:30 +0530

    Added buildscripts and patches to build the ZCS for PPC64LE.

---
+++ zimbra/zimbra_installation.sh
@@ -0,0 +1,2 @@
+yum update -y;
+yum install -y file libaio sudo sysstat unzip perl-Socket6 nmap-ncat perl-core libX11 libXext libXi libXtst  libXrender alsa-lib freetype java-1.8.0-openjdk net-tools
