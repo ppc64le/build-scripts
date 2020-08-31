16fb1609259d749dbc251c8a80fc3d0e4376f9aa
Kandarpa Malipeddi <kandarpa.malipeddi@ibm.com>
2020-08-31 14:26:46 +0530

    Removed unwanted file.

--- zimbra/zimbra_installation.sh
+++
@@ -1,2 +0,0 @@
-yum update -y;
-yum install -y file libaio sudo sysstat unzip perl-Socket6 nmap-ncat perl-core libX11 libXext libXi libXtst  libXrender alsa-lib freetype java-1.8.0-openjdk net-tools
