5f5bdaf568fb50247789ee7811d373d504d6cd74
Amit Sadaphule <amits2@us.ibm.com>
2020-12-10 02:50:37 -0500

    Incorporate review comments and fix dependencies
    - Refer https://github.com/ppc64le/build-scripts/pull/302
    - fixed mesa-libgbm-devel rpm version to match mesa-libgbm from yum
    - fixed timeout issue in fetching specific chromium tag

--- c/chromium/Chromium_84.0.4118.0_UBI_8.2.sh
+++ c/chromium/Chromium_84.0.4118.0_UBI_8.2.sh
@@ -28,7 +28,7 @@ dnf -y --disableplugin=subscription-manager install \
 yum install -y git vim make cmake3 python2 cups-devel pkgconfig nss-devel openssl-devel glib2-devel pango-devel dbus-devel atk-devel at-spi2-atk-devel gtk3-devel krb5-devel pulseaudio-libs-devel libXScrnSaver-devel epel-release subversion curl alsa-lib-devel pciutils-devel mesa-libGLw bison patch bzip2 libuuid-devel mesa-libgbm libsecret-devel python38 python2-psutil python38-psutil java-1.*.0-openjdk-devel libXtst-devel libatomic gcc-c++ expat-devel zlib-devel perl-ExtUtils-MakeMaker wget diffutils libdrm-devel
 
 dnf install -y http://mirror.centos.org/centos/8.2.2004/PowerTools/ppc64le/os/Packages/gperf-3.1-5.el8.ppc64le.rpm
-dnf install -y http://mirror.centos.org/centos/8.2.2004/PowerTools/ppc64le/os/Packages/mesa-libgbm-devel-19.3.4-2.el8.ppc64le.rpm
+dnf install -y http://mirror.centos.org/centos/8.3.2011/PowerTools/ppc64le/os/Packages/mesa-libgbm-devel-20.1.4-1.el8.ppc64le.rpm
 dnf install -y http://mirror.centos.org/centos/8.2.2004/PowerTools/ppc64le/os/Packages/re2c-0.14.3-2.el8.ppc64le.rpm
 dnf install -y http://mirror.centos.org/centos/8.2.2004/PowerTools/ppc64le/os/Packages/ninja-build-1.8.2-1.el8.ppc64le.rpm
 
@@ -68,18 +68,6 @@ export PATH=${CHROMIUM_DIR}/gn/out:$PATH:${CHROMIUM_DIR}/depot_tools
 export VPYTHON_BYPASS="manually managed python not supported by chrome operations"
 export GYP_DEFINES="disable_nacl=1"
 
-# check out LLVM and Clang
-export CLANG_REVISION=375507
-cd $CHROMIUM_DIR
-svn checkout --force https://llvm.org/svn/llvm-project/llvm/trunk@$CLANG_REVISION llvm
-svn checkout --force https://llvm.org/svn/llvm-project/cfe/trunk@$CLANG_REVISION llvm/tools/clang
-svn checkout --force https://llvm.org/svn/llvm-project/compiler-rt/trunk@$CLANG_REVISION llvm/compiler-rt 
-
-# Build the compiler
-mkdir llvm_build && cd llvm_build
-cmake3 -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="PowerPC" -G "Unix Makefiles" ../llvm
-make -j8
-
 # Downloading the source code
 cd $CHROMIUM_DIR
 mkdir chromium && cd chromium
@@ -87,7 +75,7 @@ gclient config --name "src" --unmanaged https://chromium.googlesource.com/chromi
 gclient sync --nohooks --no-history
 
 cd src
-git fetch https://chromium.googlesource.com/chromium/src.git +refs/tags/84.0.4118.0:chromium_84.0.4118.0
+git fetch https://chromium.googlesource.com/chromium/src.git +refs/tags/84.0.4118.0:chromium_84.0.4118.0 --depth 1
 git checkout chromium_84.0.4118.0
 
 curl -k "https://wiki.raptorcs.com/w/images/1/10/Binutils-download.py-PPC.patch" | patch -p1 third_party/binutils/download.py
@@ -96,6 +84,34 @@ cp -f $CHROMIUM_DIR/gn/out/gn buildtools/linux64/gn
 # Disable gn_version_check since that overwrites power gn with amd64 gn
 sed -i '/'\''src\/buildtools\/linux64'\''/,+9d' DEPS
 gclient sync --with_branch_heads --with_tags
+
+# check out LLVM and Clang
+REVISION=$(grep -Po "(?<=CLANG_REVISION = ')\w+(?=')" tools/clang/scripts/update.py | head -n 1)
+cd $CHROMIUM_DIR
+if [ -d "llvm-project" ]; then
+    cd llvm-project
+    git add -A
+    git status
+    git reset --hard HEAD
+    git fetch
+    git status
+    cd ../
+else
+    git clone https://github.com/llvm/llvm-project.git
+fi
+
+git -C llvm-project checkout "${REVISION}"
+
+# Build the compiler
+mkdir -p llvm_build
+cd llvm_build
+
+LLVM_BUILD_DIR=$(pwd)
+
+cmake3 -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="PowerPC" -DLLVM_ENABLE_PROJECTS="clang;lld" -G "Ninja" ../llvm-project/llvm
+ninja -j$(nproc)
+
+export PATH=$LLVM_BUILD_DIR/bin/:$PATH
 
 # Patching Chromium
 cd $CHROMIUM_DIR
@@ -112,10 +128,6 @@ python3 cpf.py $CHROMIUM_DIR/chromium/src
 cd $CHROMIUM_DIR/chromium/src/third_party/crashpad/crashpad/
 cat ../../../../../chromium_power/crashpad/0001-Implement-support-for-PPC64-on-Linux.patch | patch -p1
 
-# Install CLANG
-cd $CHROMIUM_DIR/llvm_build/
-make install 
-
 cd $CHROMIUM_DIR/chromium/src/third_party/ffmpeg
 ./chromium/scripts/build_ffmpeg.py linux ppc64
 ./chromium/scripts/generate_gn.py
@@ -126,31 +138,7 @@ mkdir source/config/linux/ppc64
 ./generate_gni.sh
 
 cd $CHROMIUM_DIR/chromium/src
-
 curl -k "https://wiki.raptorcs.com/w/images/b/bb/0001-sandbox-linux-seccomp-bpf-helpers-Fix-TCGETS-declara.patch" | patch -p1
-
-# Configure build parameters with gn
-mkdir out/Default
-cd out/Default
-
-echo '# Build arguments go here.
-# See "gn args <out_dir> --list" for available build arguments.
-# Release mode
-is_component_build = false
-is_debug = false
-# Disable broken features
-enable_nacl = false
-treat_warnings_as_errors = false
-enable_dav1d_decoder = false
-# For clang, add the following lines:
-is_clang = true
-clang_base_path = "/root/chromium_build/llvm_build"
-clang_use_chrome_plugins = false
-use_lld = false
-use_glib=true
-use_gnome_keyring=false' > args.gn
-
-cd ../../
 
 # Additional patches for ppc for gn gen failures (20/04/2020)
 sed -i '414i  } else if (current_cpu == "ppc64") { \n    sources = libvpx_srcs_ppc64'  third_party/libvpx/BUILD.gn
@@ -167,7 +155,7 @@ perl -i -0pe 's/\(defined\(ARCH_CPU_MIPS_FAMILY\) \&\& defined\(ARCH_CPU_64_BITS
 perl -i -0pe 's/\(defined\(ARCH_CPU_MIPS_FAMILY\) \&\& defined\(ARCH_CPU_64_BITS\)\)\n\/\/ These give a lot of ambient authority and bypass the setuid sandbox.\n  static bool IsSystemVSharedMemory\(int sysno\);/\(defined\(ARCH_CPU_MIPS_FAMILY\) \&\& defined\(ARCH_CPU_64_BITS\)\) || \\\n defined\(__powerpc64__\)\n\/\/ These give a lot of ambient authority and bypass the setuid sandbox.\n  static bool IsSystemVSharedMemory\(int sysno\);/' sandbox/linux/seccomp-bpf-helpers/syscall_sets.h
 
 # Generate files
-gn gen out/Default
+gn gen out/Default --args="is_component_build = false is_debug = false enable_nacl = false treat_warnings_as_errors = false enable_dav1d_decoder = false is_clang = true clang_base_path = \"$LLVM_BUILD_DIR\" clang_use_chrome_plugins = false use_lld = false use_glib=true use_gnome_keyring=false"
 
 # Build chromium and chromedriver
 ninja -C out/Default chrome
