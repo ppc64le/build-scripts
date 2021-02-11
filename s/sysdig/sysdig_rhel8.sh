ab24dda82f085c72cd4a98b1783d9ee04369f3c5
Amit Shirodkar <amit.shirodkar@ibm.com>
2021-02-11 11:39:12 -0500

    fix missed cd

--- s/sysdig/sysdig_rhel8.sh
+++ s/sysdig/sysdig_rhel8.sh
@@ -8,7 +8,7 @@
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Amit Shirodkar <amit.shirodkar@ibm.com>
 #
-# Disclaimer: This script has been tested in root mode on the platform
+# Disclaimer: This script has been tested in non-root mode using sudo on the platform
 # ==========  as specified, and the version of the package as indicated.
 #             It may not work as expected with newer versions of the
 #             package and/or distribution. In such casea, please
@@ -17,12 +17,13 @@
 # ----------------------------------------------------------------------------
 
 
-yum install git kernel-headers kernel-devel gcc libgcc gcc-c++ cmake make wget patch python36 c-ares c-ares-devel 
+sudo yum install git kernel-headers kernel-devel gcc libgcc gcc-c++ cmake make wget patch python36 c-ares c-ares-devel -y 
 git clone https://github.com/draios/sysdig.git
 cd sysdig
 git apply ../sysdig_dynamic_stdlib.patch
 
 mkdir build
+cd build 
 cmake ..
 make 
 make package 
