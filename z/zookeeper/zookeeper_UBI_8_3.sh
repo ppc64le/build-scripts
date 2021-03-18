cb0ad55bf55ab8a8a707065a6352a3c52f6841d1
Nishikant Thorat <thorat.nishikant@gmail.com>
2021-03-18 09:28:24 -0400

    Zookeeper for UBI8.3

---
+++ z/zookeeper/zookeeper_UBI_8_3.sh
@@ -0,0 +1,26 @@
+# ----------------------------------------------------------------------------
+#
+# Package       : Apache Zookeeper
+# Version       : 3.6.2
+# Source repo   : https://github.com/apache/zookeeper.git
+# Tested on     : UBI 8.2
+# Script License: Apache License
+# Maintainer    : nishikant.thorat@ibm.com
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+sudo yum update -y
+
+export PKG_CONFIG=/usr/bin/pkg-config
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+
+yum install -y git wget maven hostname automake libtool autoconf-2.69 gcc-c++ make
+git clone git://anongit.freedesktop.org/git/libreoffice/cppunit/
+cd cppunit && ./autogen.sh && ./configure --build=ppc64le && make && make install && cd ..
+
+yum install -y pkgconf-1.4.2
+#
+# "-fn" option in mvn command will continue compilatio,n even if one module/ component compilation fails. Kindly check in logs, if all# components are compiled successfully or not
+#
+git clone https://github.com/apache/zookeeper.git -b release-3.6.2
+cd zookeeper &&  mvn clean apache-rat:check verify -DskipTests spotbugs:check checkstyle:check -Pfull-build -Drat.skip=true -Dlicense.skip  -Dlicense.skipDownloadLicenses -Drat.numUnapprovedLicenses=100 -fn
