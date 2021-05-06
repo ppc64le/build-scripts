49c12a59803e216c1f2310281d4efe8a6378fa6b
Nagesh A T <74532266+nageshlop@users.noreply.github.com>
2021-05-06 10:58:25 +0530

    Create stringtemplate4_ubuntu_16.04.sh
    Created shell script to run the build and verified the working on local VM.

---
+++ s/stringtemplate4_ubuntu_16.04.sh
@@ -0,0 +1,55 @@
+# ----------------------------------------------------------------------------
+#
+# Package       : stringtemplate4
+# Version       : 4.3.1
+# Source repo   : https://github.com/antlr/stringtemplate4
+# Tested on     : Ubuntu 20.04.1 LTS (Focal Fossa)
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Nagesh Tarale <Nagesh.Tarale@ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+#!/bin/bash
+
+export URL=https://github.com/antlr/stringtemplate4
+export BRANCH="$1"
+PKG_NAME=${URL##*/}
+
+if [ -d $PKG_NAME ] ; then
+  rm -rf $PKG_NAME
+fi
+
+#Clean and Install Updates
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get install maven
+
+# Verify the arguments passed from command line.
+
+if [ $# -lt 1 ]
+then
+  echo "Usage: $0 branch or tag_name"
+  exit 0
+fi
+echo "Branch is $1 and no of arguments $# "
+if [ $# -gt 1 ]
+then
+        echo "Specify no of arguments correctly, USAGE: $0 BRANCH"
+        exit 0
+else
+        echo "Cloning the target"
+fi
+
+# Build and test the package
+# BRANCHES can be master, or any other tags supported. 
+
+git clone --depth 1 --branch $BRANCH $URL --single-branch
+cd $PKG_NAME
+sudo ant -f build.xml
+sudo ant clean
