29374f5e133654ab6fa0e34391f4579b7fc42b79
Amit Shirodkar <amit.shirodkar@ibm.com>
2021-05-13 17:31:54 +0530

    add buildscript for breakpad

---
+++ b/breakpad/breakpad_build.sh
@@ -0,0 +1,59 @@
+# ----------------------------------------------------------------------------
+#
+# Package       : breakpad
+# Version       : https://chromium-review.googlesource.com/c/breakpad/breakpad/+/1426283/21
+# Source repo   : https://chromium.googlesource.com/breakpad/breakpad
+# Tested on     : RHEL8
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Amit Shirodkar <amit.shirodkar@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+
+#!/bin/bash
+
+#breakpad build script 
+
+git clone https://chromium.googlesource.com/breakpad/breakpad
+cd breakpad
+
+#fetch , patch and copy linux-syscall-support
+git clone https://chromium.googlesource.com/linux-syscall-support
+cd linux-syscall-support/
+git fetch https://chromium.googlesource.com/linux-syscall-support refs/changes/73/1430973/4 && git checkout FETCH_HEAD
+cd ../breakpad
+mkdir -p src/third_party/lss
+cp ../linux-syscall-support/linux_syscall_support.h src/third_party/lss/
+
+cd ..
+
+# download google test and copy the contents to breakpad/src/testing - needed for make check 
+https://github.com/google/googletest/archive/refs/tags/release-1.8.1.tar.gz
+tar -xvf release-1.8.1.tar.gz 
+cp -r googletest-release-1.8.1/google* breakpad/src/testing
+
+
+#fetch patch 21 https://chromium-review.googlesource.com/c/breakpad/breakpad/+/1426283/21
+git fetch https://chromium.googlesource.com/breakpad/breakpad refs/changes/83/1426283/21 && git checkout FETCH_HEAD
+
+./configure
+make 
+make check 
+
+
+
+
+
+
+
+
+
+
+
+
