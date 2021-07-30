7cf176977a5681e36cedc4ddb6f2beb5a80d9ef1
Sachin Kakatkar <sachin.kakatkar@ibm.com>
2021-07-30 16:48:30 +0530

    Added calcite-avatica package build script to test on RHEL 8 docker container

---
+++ c/calcite-avatica/calcite-avatica_ubi_8.sh
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------------------------
+#
+# Package       : calcite-avatica
+# Version       : 945d06c2a
+# Source repo   : https://github.com/apache/calcite-avatica.git
+# Tested on     : RHEL 8.3
+# Script License: Apache License, Version 2 or later
+# Maintainer    : sachin.kakatkar@ibm.com
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+PACKAGE_NAME=calcite-avatica
+PACKAGE_VERSION=945d06c2a
+PACKAGE_URL=https://github.com/apache/calcite-avatica
+docker run -it --name calcite-avatica registry.access.redhat.com/ubi8/ubi bash
+dnf install git -y
+dnf install maven
+mkdir test
+cd test
+git clone https://github.com/apache/calcite-avatica.git
+cd calcite-avatica
+git checkout 945d06c2a
+sed -i 's/protobuf.version=3.6.1/protobuf.version=3.7.0/g' gradle.properties
+./gradlew build
+
