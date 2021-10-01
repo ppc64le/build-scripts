5d2f6b0e6dde9a8071ec2f22d1837a89cf3a9bad
Sumit Dubey2 <sumit.dubey2@ibm.com>
2021-10-01 17:02:28 +0530

    Build script and license file for jsr505 3.0.2 Ubi 8.3

--- j/jsr305/jsr305_3.0.2_ubi_8.3.sh
+++ j/jsr305/jsr305_3.0.2_ubi_8.3.sh
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------
 #
 # Package       : jar305
-# Version       : 3.0.1
+# Version       : 3.0.2
 # Tested on     : UBI 8.3 (Docker)
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Sumit Dubey <Sumit.Dubey2@ibm.com>
@@ -23,19 +23,19 @@ yum install -y wget unzip java-1.8.0-openjdk java-1.8.0-openjdk-devel
 cd /opt
 mkdir jsr305
 cd jsr305
-wget https://repo1.maven.org/maven2/com/google/code/findbugs/jsr305/3.0.1/jsr305-3.0.1-sources.jar
-jar xf jsr305-3.0.1-sources.jar
+wget https://repo1.maven.org/maven2/com/google/code/findbugs/jsr305/3.0.2/jsr305-3.0.2-sources.jar
+jar xf jsr305-3.0.2-sources.jar
 find -name "*.java" > sources.txt
 
 #build sources
 javac @sources.txt
-rm -f sources.txt jsr305-3.0.1-sources.jar
+rm -f sources.txt jsr305-3.0.2-sources.jar
 
 #create jar
 find . -name "*.java" -type f -delete
-jar cvf jsr305-3.0.1.jar javax/
+jar cvf jsr305-3.0.2.jar javax/
 rm -rf META-INF/ javax/
 
 #conclude
-echo "/opt/jsr305/jsr305-3.0.1.jar"
+echo "/opt/jsr305/jsr305-3.0.2.jar"
 echo "Complete!"
