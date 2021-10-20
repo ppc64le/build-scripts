76be8fe54dae47f286274e30aafcda122d81125b
Sumit Dubey2 <sumit.dubey2@ibm.com>
2021-10-20 16:56:41 +0530

    Build script - react-draggable master ubi 8.3 - script heade added

--- r/react-draggable/react-draggable-master_ubi8.3.sh
+++ r/react-draggable/react-draggable-master_ubi8.3.sh
@@ -1,3 +1,18 @@
+# ----------------------------------------------------------------------------------------------------
+#
+# Package       : react-draggable
+# Version       : master
+# Tested on     : UBI 8.3 (Docker)
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Sumit Dubey <Sumit.Dubey2@ibm.com>
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------------------------------
+
 #!/bin/bash
 
 set -ex
