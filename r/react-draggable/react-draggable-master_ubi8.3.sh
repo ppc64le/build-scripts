38cdf33cd22e1a73bd2429f1cdfe4548d810f8b8
sumitd2 <91451282+sumitd2@users.noreply.github.com>
2021-10-20 17:15:58 +0530

    Jquery.i18n master ubi 8.3 (#1117)
    * Build script for carbon-charts 0.16.25, 0.29.1, 0.40.0, 0.40.14, 0.41.10, 0.41.15, 0.41.31, 0.41.67 ubi8.3
    
    * Some extra commands trimmed
    
    * Build Script for Jquery 3.4.1 Ubi 8.3
    
    * jquery_3.4.0_3.4.1_3.5.1_ubi_8.3
    
    * Extra script file removed
    
    * Build script - Jquery 3.6.0 ubi 8.3
    
    * Build script - use-resize-observer master ubi 8.3
    
    * Build script - react-draggable master ubi 8.3
    
    * Build script - jquery.i18n master ubi 8.3

---
+++ r/react-draggable/react-draggable-master_ubi8.3.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -ex
+
+#Variables
+REPO=https://github.com/react-grid-layout/react-draggable.git
+PACKAGE_VERSION=master
+
+echo "Usage: $0 [-v <PACKAGE_VERSION>]"
+echo "PACKAGE_VERSION is an optional paramater whose default value is master, not all versions are supported."
+
+PACKAGE_VERSION="${1:-$PACKAGE_VERSION}"
+
+#install dependencies
+yum install git sed unzip bzip2 make -y
+
+dnf module install -y nodejs:14
+
+#clone the repo
+cd /opt && git clone $REPO
+cd react-draggable/
+git submodule update --init
+if [[ "$PACKAGE_VERSION" = "master" ]]
+then
+	git checkout master
+else
+	git checkout v$PACKAGE_VERSION
+fi
+
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
+rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
+dnf install -y yarn
+
+
+#Get phantomjs
+cd /opt
+yum install -y wget bzip2 fontconfig-devel
+wget https://github.com/ibmsoe/phantomjs/releases/download/2.1.1/phantomjs-2.1.1-linux-ppc64.tar.bz2
+tar -xvf phantomjs-2.1.1-linux-ppc64.tar.bz2
+ln -s /phantomjs-2.1.1-linux-ppc64/bin/phantomjs /usr/local/bin/phantomjs
+export PATH=$PATH:/opt/phantomjs-2.1.1-linux-ppc64/bin/
+
+#build
+cd react-draggable/
+yarn install
+yarn build
+
+#conclude
+echo "Build Complete. Uncomment the following lines to run tests, they may take a while to complete."
+
+#dnf remove -y module nodejs
+#dnf module reset nodejs -y
+#dnf -y install \
+#http://mirror.centos.org/centos/8/BaseOS/ppc64le/os/Packages/centos-linux-repos-8-3.el8.noarch.rpm \
+#http://mirror.centos.org/centos/8/BaseOS/ppc64le/os/Packages/centos-gpg-keys-8-3.el8.noarch.rpm
+#yum install -y firefox libXScrnSaver libdrm mesa-libgbm alsa-lib python3 libarchive
+#cd /opt
+#git clone https://github.com/ppc64le/build-scripts.git
+#cd build-scripts/c/chromium
+#sed -i "s#./chromedriver --version#echo \$(pwd) > /opt/chrome.binary#g" Chromium_84.0.4118.0_UBI.sh
+#./Chromium_84.0.4118.0_UBI.sh
+#CHROME_DIR=$(cat /opt/chrome.binary)
+#export CHROME_BIN=$CHROME_DIR/chrome
+#chmod 777 $CHROME_BIN
+#CHROME_PUPPETEER=$(find /opt/jquery.i18n -name chrome-linux)
+#rm -rf $CHROME_PUPPETEER
+#cp -r /opt/chromium_84_0_4118_0 $CHROME_PUPPETEER
+#dnf module install -y nodejs:14
+#cd /opt/react-draggable/
+#sed -i "s#this.browserDisconnectTimeout = 2000#this.browserDisconnectTimeout = 210000#g" /opt/react-draggable/node_modules/karma/lib/config.js
+#sed -i "s#this.captureTimeout = 60000#this.captureTimeout = 210000#g" /opt/react-draggable/node_modules/karma/lib/config.js
+#sed -i "s#this.browserNoActivityTimeout = 30000#this.browserNoActivityTimeout = 210000#g" /opt/react-draggable/node_modules/karma/lib/config.js
+#sed -i "s#this.browserDisconnectTolerance = 0#this.browserDisconnectTolerance = 3#g" /opt/react-draggable/node_modules/karma/lib/config.js
+#sed -i "s#'--headless'#'--headless', '--no-sandbox'#g" /opt/react-draggable/node_modules/karma-chrome-launcher/index.js
+#sed -i "s#url, '-profile'#url, '-headless', '-profile'#g" /opt/react-draggable/node_modules/karma-firefox-launcher/index.js
+#yarn test
+#echo "Tests Complete!"
