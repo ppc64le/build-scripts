c03b2b2a508a724ce7552911fb9d324766adadd6
Ryan D'Mello <Ryan.Dmello1@ibm.com>
2021-10-21 16:21:34 +0530

    Sample testing of PR changes

--- a/acorn-jsx/acorn-jsx_rhel_8.3.sh
+++ a/acorn-jsx/acorn-jsx_rhel_8.3.sh
@@ -1,9 +1,9 @@
 # -----------------------------------------------------------------------------
 #
 # Package	: acorn-jsx
-# Version	: 5.3.1
+# Version	: 5.2.0
 # Source repo	: https://github.com/acornjs/acorn-jsx
-# Tested on	: RHEL 8.3
+# Tested on	: UBI 8.4
 # Script License: Apache License, Version 2 or later
 # Maintainer	: BulkPackageSearch Automation <sethp@us.ibm.com>
 #
@@ -16,18 +16,17 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=acorn-jsx
-PACKAGE_VERSION=5.3.1
+PACKAGE_VERSION=5.2.0
 PACKAGE_URL=https://github.com/acornjs/acorn-jsx
 
 yum -y update && yum install -y yum-utils nodejs nodejs-devel nodejs-packaging npm python38 python38-devel ncurses git gcc gcc-c++ libffi libffi-devel ncurses git jq make cmake
-yum-config-manager --add-repo http://rhn.pbm.ihost.com/rhn/latest/8.3Server/ppc64le/appstream/
-yum-config-manager --add-repo http://rhn.pbm.ihost.com/rhn/latest/8.3Server/ppc64le/baseos/
-yum-config-manager --add-repo http://rhn.pbm.ihost.com/rhn/latest/7Server/ppc64le/optional/
 
-yum install -y firefox liberation-fonts xdg-utils && npm install n -g && n latest && npm install -g npm@latest && export PATH="$PATH" && npm install --global yarn grunt-bump xo testem acorn
+npm install n -g && n latest && npm install -g npm@latest && export PATH="$PATH" && npm install --global yarn grunt-bump xo testem acorn
 
-OS_NAME=`python3 -c "os_file_data=open('/etc/os-release').readlines();os_info = [i.replace('PRETTY_NAME=','').strip() for i in os_file_data if i.startswith('PRETTY_NAME')];print(os_info[0])"`
-HOME_DIR=`pwd`
+mkdir -p output
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
 		echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/clone_fails
@@ -35,27 +34,27 @@ if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     	exit 0
 fi
 
-cd $HOME_DIR/$PACKAGE_NAME
+cd /home/tester/$PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 PACKAGE_VERSION=$(jq -r ".version" package.json)
 # run the test command from test.sh
 
 if ! npm install && npm audit fix && npm audit fix --force; then
      	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+	echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/install_fails
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails" > /home/tester/output/version_tracker
 	exit 0
 fi
 
-cd $HOME_DIR/$PACKAGE_NAME
+cd /home/tester/$PACKAGE_NAME
 if ! npm test; then
 	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+	echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/test_fails 
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails" > /home/tester/output/version_tracker
 	exit 0
 else
 	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+	echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/test_success 
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success" > /home/tester/output/version_tracker
 	exit 0
 fi
