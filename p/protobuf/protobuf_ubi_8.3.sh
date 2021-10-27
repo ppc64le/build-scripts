5956145470934a2f5ebd46cd4eb6bc8d75e9093c
santosh653 <70637961+santosh653@users.noreply.github.com>
2021-10-27 11:26:08 +0530

    Create protobuf_ubi_8.3.sh
    add

---
+++ p/protobuf/protobuf_ubi_8.3.sh
@@ -0,0 +1,63 @@
+# ----------------------------------------------------------------------------
+#
+# Package               : protobuf
+# Version               : v1.3.2, v1.3.1
+# Source repo           : https://github.com/gogo/protobuf
+# Tested on             : UBI 8.3
+# Script License        : Apache License, Version 2 or later
+# Passing Arguments     : Passing Arguments: 1.Version of package,
+# Script License        : Apache License, Version 2 or later
+# Maintainer            : Santosh Kulkarni<santoshkulkarni70@gmail.com>/Priya Seth<sethp@us.ibm.com>
+#
+# Disclaimer            : This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  export VERSION=master
+else
+  export VERSION=$1
+fi
+
+if [ -d "protobuf" ] ; then
+  rm -rf protobuf
+fi
+
+# Dependency installation
+sudo dnf install -y git golang 
+# Download the repos
+git clone https://github.com/gogo/protobuf
+
+
+# Build and Test protobuf
+cd protobuf
+git checkout $VERSION
+ret=$?
+if [ $ret -eq 0 ] ; then
+ echo "$VERSION found to checkout "
+else
+ echo "$VERSION not found "
+ exit
+fi
+
+
+#Build and test
+go get -v -t ./...
+
+ret=$?
+if [ $ret -ne 0 ] ; then
+  echo "Build failed "
+else
+  go test -v ./...
+  if [ $ret -ne 0 ] ; then
+    echo "Tests failed "
+  else
+    echo "Build & unit tests Successful "
+  fi
+fi
