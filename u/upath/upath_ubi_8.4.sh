41c1b1f6a55da9bed6b48186a3e75973f6f992c4
vikasgupta8 <vikas.gupta8@ibm.com>
2021-11-18 15:02:16 +0000

    Task-143665 : updated script

--- u/upath/upath_ubi_8.4.sh
+++ u/upath/upath_ubi_8.4.sh
@@ -40,8 +40,7 @@ cd $HOME_DIR/$PACKAGE_NAME/
 git checkout $PACKAGE_VERSION
 PACKAGE_VERSION=$(jq -r ".version" package.json)
 
-#npm install -g grunt && npm install -g grunt-cli && npm install
-#cd /home/tester/$PACKAGE_NAME/$PACKAGE_NAME
+cd $HOME_DIR/$PACKAGE_NAME/
 
 # run the test command from test.sh
 if ! npm install && npm audit fix && npm audit fix --force; then
@@ -51,7 +50,6 @@ if ! npm install && npm audit fix && npm audit fix --force; then
         exit 0
 fi
 
-#cd /home/tester/$PACKAGE_NAME
 if ! npm test; then
         echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/test_fails
