4a4b11c0c78686579054a8ce97f44bd9692977aa
vikasgupta8 <vikas.gupta8@ibm.com>
2021-11-18 14:31:32 +0000

    Task-143665 : Updated script.

--- u/upath/upath_ubi_8.4.sh
+++ u/upath/upath_ubi_8.4.sh
@@ -23,25 +23,27 @@ yum -y update && yum install -y yum-utils nodejs nodejs-devel nodejs-packaging n
 
 npm install n -g && n latest && npm install -g npm@latest && export PATH="$PATH" && npm install --global yarn grunt-bump xo testem acorn
 
-mkdir -p /home/tester/$PACKAGE_NAME
+mkdir -p /home/tester/output
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+HOME_DIR=`pwd`
 
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
         echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-                echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/clone_fails
+        echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/clone_fails
         echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails" > /home/tester/output/version_tracker
         exit 0
 fi
 
-cd /home/tester/$PACKAGE_NAME
+cd $HOME_DIR/$PACKAGE_NAME/
+
 git checkout $PACKAGE_VERSION
 PACKAGE_VERSION=$(jq -r ".version" package.json)
 
-npm install -g grunt && npm install -g grunt-cli && npm install
+#npm install -g grunt && npm install -g grunt-cli && npm install
+#cd /home/tester/$PACKAGE_NAME/$PACKAGE_NAME
 
 # run the test command from test.sh
-
 if ! npm install && npm audit fix && npm audit fix --force; then
         echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/install_fails
@@ -49,7 +51,7 @@ if ! npm install && npm audit fix && npm audit fix --force; then
         exit 0
 fi
 
-cd /home/tester/$PACKAGE_NAME
+#cd /home/tester/$PACKAGE_NAME
 if ! npm test; then
         echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/test_fails
