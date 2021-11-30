528dc73159a450da7a9ebd6bff2d8b1fa29949d8
root <root@p006vm80.pbm.ihost.com>
2021-11-30 12:18:37 +0000

    Adding build scripts bluebird-retry, jszip, libcipm, loopback-cli, sinon-chai, void-elemnts

---
+++ l/loopback-cli/loopback-cli_ubi8.sh
@@ -0,0 +1,41 @@
+# ----------------------------------------------------------------------------
+#
+# Package               : loopback-cli
+# Version               : 5.1.0
+# Source repo           : https://github.com/strongloop/loopback-cli
+# Tested on             : UBI 8
+# Script License        : Apache License, Version 2 or later
+# Maintainer            : Swati Singhal <swati.singhal@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+#!/bin/bash
+PACKAGE_VERSION=5.1.0
+
+echo "Usage: $0 [-v <PACKAGE_VERSION>]"
+echo "       PACKAGE_VERSION is an optional paramater whose default value is 5.1.0"
+
+PACKAGE_VERSION="${1:-$PACKAGE_VERSION}"
+
+# install tools and dependent packages
+yum -y install git wget gcc-c++ make python2 curl
+
+NODE_VERSION=v12.22.4
+#installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install $NODE_VERSION
+
+# clone, build and test specified version
+
+git clone https://github.com/strongloop/loopback-cli
+cd loopback-cli
+git checkout v$PACKAGE_VERSION
+npm install
+
