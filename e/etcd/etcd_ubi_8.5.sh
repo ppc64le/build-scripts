ed6ea925760201dd8617d4fb5caaa4f6432c89c5
Atharv Phadnis <atharv.phadnis@ibm.com>
2021-12-02 16:06:15 +0530

    Updated build script for etcd

--- e/etcd/etcd_ubi_8.5.sh
+++ e/etcd/etcd_ubi_8.5.sh
@@ -16,41 +16,48 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=etcd
-PACKAGE_VERSION=v3.5.1
+PACKAGE_VERSION=${1:-v3.5.1}
 PACKAGE_URL=https://github.com/etcd-io/etcd
 
 yum install -y git golang make
 
-HOME_DIR=`pwd`
-
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
+export GOPATH=$HOME/go
+mkdir -p $GOPATH/src/github.com/etcd-io/
+
+cd $GOPATH/src/github.com/etcd-io/
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
-    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-		echo "$PACKAGE_URL $PACKAGE_NAME"
-		echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME= | Fail |  Clone_Fails"
-    	exit 0
+	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | Fail |  Clone_Fails"
+	exit 0
 fi
 
-cd $HOME_DIR/$PACKAGE_NAME
+cd $GOPATH/src/github.com/etcd-io/$PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 if ! make build; then
-	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+	echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
 	exit 1
 fi
 
-cd $HOME_DIR/$PACKAGE_NAME
+cd $GOPATH/src/github.com/etcd-io/$PACKAGE_NAME
 if ! make test; then
 	echo "------------------$PACKAGE_NAME:test_fails---------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
 	exit 1
 else
-	echo "------------------$PACKAGE_NAME:install_check_and_test_success-------------------------"
+	echo "------------------$PACKAGE_NAME:build_and_test_success-------------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Install_Check_and_Test_Success"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass |  Build_and_Test_Success"
+	
+	# Copy binaries to GOPATH/bin
+	cd $GOPATH/src/github.com/etcd-io/$PACKAGE_NAME
+	cp -r bin/* $GOPATH/bin/
+	echo "Copied binaries to $GOPATH/bin"
 	exit 0
 fi
