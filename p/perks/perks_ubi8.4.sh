6a152864074db6ea4fd8235f3c47987d81fa6126
sapnashukla28 <Sapna.Shukla@ibm.com>
2021-12-06 14:34:34 +0000

    Build scripts for beorn7/perks

---
+++ p/perks/perks_ubi8.4.sh
@@ -0,0 +1,67 @@
+# -----------------------------------------------------------------------------
+#
+# Package	: beorn7/perks
+# Version	: 1.0.1
+# Source repo	: https://github.com/beorn7/perks
+# Tested on	: ubi 8.4
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Sapna Shukla<Sapna.Shukla@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=perks
+PACKAGE_VERSION=v1.0.1
+PACKAGE_URL=https://github.com/beorn7/perks
+
+yum install -y wget git tar 
+
+GO_VERSION=1.17
+
+# Install Go and setup working directory
+wget https://golang.org/dl/go$GO_VERSION.linux-ppc64le.tar.gz
+tar -C /bin -xf go$GO_VERSION.linux-ppc64le.tar.gz
+mkdir -p /home/tester/go/src 
+rm -f go$GO_VERSION.linux-ppc64le.tar.gz
+
+export PATH=$PATH:/bin/go/bin
+export GOPATH=/home/tester/go
+
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+export PATH=$GOPATH/bin:$PATH
+export GO111MODULE=on
+
+#running the go commands
+cd /home/tester/go/src
+git clone --recurse $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+go mod init 
+go mod tidy
+
+if ! go install -v ./...; then
+	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  intsall_Fails"
+	exit 0
+fi
+
+
+if ! go test -v ./...; then
+	echo "------------------$PACKAGE_NAME:test_fails-------------------------------------"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  test_Fails"
+	exit 0
+else
+	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Install_and_Test_Success"
+	echo "------------------$PACKAGE_NAME:Installed at path: /home/tester/go/src/$PACKAGE_NAME------------------------"
+	exit 0
+fi
+
