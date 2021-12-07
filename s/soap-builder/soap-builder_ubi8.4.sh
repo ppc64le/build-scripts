dcd012540fa47aca776acd348b1a36e124894121
sapnashukla28 <Sapna.Shukla@ibm.com>
2021-12-07 14:50:52 +0000

    Changed exit 0 to 1 for failures

--- s/soap-builder/soap-builder_ubi8.4.sh
+++ s/soap-builder/soap-builder_ubi8.4.sh
@@ -34,15 +34,16 @@ git checkout $PACKAGE_VERSION
 # Build and test.
 if ! mvn -T 1C clean install -Dlicense.skip=true -Dadditionalparam=-Xdoclint:none -DskipTests; then
 	echo "------------------$BASE_PACKAGE_NAME:INSTALL_FAILED-------------------------------------"
-	exit 0
+	exit 1
 fi
 cd $PACKAGE_NAME
 if ! mvn clean install -Dlicense.skip=true; then
 	echo "------------------$PACKAGE_NAME:INSTALL_AND_TEST_FAILED-------------------------------------------"
-	exit 0
+	exit 1
 else
         echo -e "\n------------------$PACKAGE_NAME:INSTALL_AND_TEST_PASSED-------------------------"
         PATH=$(find -name *.jar)
         echo -e "\n------------------.JAR CREATED IN DIRECTORY:$BASE_PACKAGE_NAME/$PACKAGE_NAME, PATH-------------------------"
         echo "$PATH"
+	exit 0
 fi
