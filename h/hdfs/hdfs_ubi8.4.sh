f1b33620c02a5feb8a261cc15a5ffc306d9eeb6d
vikasgupta8 <vikas.gupta8@ibm.com>
2021-12-17 09:27:39 +0000

    updated script

--- h/hdfs/hdfs_ubi8.4.sh
+++ h/hdfs/hdfs_ubi8.4.sh
@@ -155,6 +155,9 @@ go get google.golang.org/protobuf/reflect/protoreflect
 go mod tidy
 export PATH=$GOPATH/bin:$PATH
 
+sed -i '/^export JAVA_HOME/d' fixtures.sh
+sed -i '/^export JAVA_HOME/d' cmd/hdfs/test/helper.bash
+
 if ! make hdfs; then
         echo "------------------$PACKAGE_NAME: build failed retrying-------------------------"
 	cp -rf internal/protocol/hadoop_common/github.com/colinmarc/hdfs/v2/internal/protocol/hadoop_common/Security.pb.go internal/protocol/hadoop_common/
@@ -171,29 +174,21 @@ $HADOOP_FS -chmod 777 "/_test"
 $HADOOP_FS -put ./testdata/foo.txt "/_test/foo.txt"
 $HADOOP_FS -Ddfs.block.size=1048576 -put ./testdata/mobydick.txt "/_test/mobydick.txt"
 
-if ! make test; then
-        echo "------------------$PACKAGE_NAME: make test failed-------------------------"
-	echo "------------------$PACKAGE_NAME: running go test-------------------------"
-	if ! go test -v ./...; then
-		echo "------------------$PACKAGE_NAME: go test failed-------------------------"
-		exit 0
-	else
-        	echo "------------------$PACKAGE_NAME: go test success-------------------------"
-	fi
-	
-	if ! bats ./cmd/hdfs/test/*.bats; then
-		echo "------------------$PACKAGE_NAME: bat test failed-------------------------"
-		exit 0
-	else
-        	echo "------------------$PACKAGE_NAME: go and bat test success-------------------------"
-        	echo "------------------$PACKAGE_NAME: install, build and test success-------------------------"
-	fi
-        exit 0
+echo "------------------$PACKAGE_NAME: running go test-------------------------"
+if ! go test -v ./...; then
+	echo "------------------$PACKAGE_NAME: go test failed-------------------------"
+	exit 0
 else
-        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-        echo "$PACKAGE_VERSION $PACKAGE_NAME" > /home/tester/output/test_success
-        echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success" > /home/tester/output/version_tracker
-        exit 0
+       	echo "------------------$PACKAGE_NAME: go test success-------------------------"
+fi
+
+echo "------------------$PACKAGE_NAME: running bat test-------------------------"
+if ! bats ./cmd/hdfs/test/*.bats; then
+	echo "------------------$PACKAGE_NAME: bat test failed-------------------------"
+	exit 0
+else
+       	echo "------------------$PACKAGE_NAME: go and bat test success-------------------------"
+       	echo "------------------$PACKAGE_NAME: install, build and test success-------------------------"
 fi
 
 cd $HOME_DIR
