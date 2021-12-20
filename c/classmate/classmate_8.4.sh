3b37bd8a9ffef2dc1b8a1973e4bf7d243a2791cd
vikasgupta8 <vikas.gupta8@ibm.com>
2021-12-20 12:35:31 +0000

    script for package classmate

---
+++ c/classmate/classmate_8.4.sh
@@ -0,0 +1,49 @@
+#----------------------------------------------------------------------------
+#
+# Package		: classmate
+# Version		: 1.3.4, 1.5.1
+# Source repo		: https://github.com/FasterXML/java-classmate
+# Tested on		: RHEL 8.4
+# Script License	: Apache License Version 2.0
+# Maintainer		: Vikas Gupta <vikas.gupta8@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+#!/bin/bash
+
+mkdir -p /logs
+
+# variables
+PKG_NAME="classmate"
+DIR_NAME="java-classmate"
+PKG_VERSION=${1:-v1.3.4}
+#PKG_VERSION_LATEST=1.5.1
+LOGS_DIRECTORY=/logs
+LOCAL_DIRECTORY=/home/tester
+REPOSITORY="https://github.com/FasterXML/java-classmate.git"
+
+
+# ------- Clone and build source -------
+
+export PATH=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.312.b07-2.el8_5.ppc64le/bin/:$PATH
+java -version
+yum install maven
+
+mkdir -p $LOCAL_DIRECTORY
+cd $LOCAL_DIRECTORY
+
+git clone $REPOSITORY
+cd $DIR_NAME
+git checkout $PKG_NAME-$PKG_VERSION
+mvn clean install | tee $LOGS_DIRECTORY/$PKG_NAME-$PKG_VERSION.txt
+
+echo "`date +'%d-%m-%Y %T'` - Installed classmate ---------------------------------------"
+echo "- --------------------------------------------------------------------------------------"
+
+
