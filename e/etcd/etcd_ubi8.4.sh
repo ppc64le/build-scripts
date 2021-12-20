62c0be6baf09f2933021de5d629c01a357fc3751
vikasgupta8 <vikas.gupta8@ibm.com>
2021-12-20 16:21:12 +0000

    build script for package etcd.

---
+++ e/etcd/etcd_ubi8.4.sh
@@ -0,0 +1,63 @@
+# -----------------------------------------------------------------------------
+#
+# Package       : github.com/etcd-io/etcd and go.etcd.io/etcd
+# Version       : 0, v0.1.0 and v0.5.0
+# Source repo   : https://github.com/etcd-io/etcd
+# Tested on     : RHEL 8.4
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Vikas Gupta <vikas.gupta8@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#
+PACKAGE_NAME=etcd
+PACKAGE_PATH=github.com/etcd-io/etcd/v3
+PACKAGE_VERSION=${1:-v3.5.1}
+PACKAGE_URL=https://github.com/etcd-io/etcd
+
+yum install -y git wget make gcc diffutils
+
+wget https://golang.org/dl/go1.15.4.linux-ppc64le.tar.gz && tar -C /bin -xf go1.15.4.linux-ppc64le.tar.gz && mkdir -p /home/tester/go/src /home/tester/go/bin /home/tester/go/pkg
+
+mkdir -p /home/tester/output
+
+export PATH=$PATH:/bin/go/bin
+export GOPATH=/home/tester/go
+
+export PATH=$GOPATH/bin:$PATH
+export GO111MODULE=on
+
+
+echo "Building $PACKAGE_PATH with master branch"
+
+if ! git clone --recurse $PACKAGE_URL; then
+	echo "------------------$PACKAGE_NAME:install_ failed-------------------------"
+	exit 0
+fi
+
+cd $PACKAGE_NAME
+
+git checkout $PACKAGE_VERSION
+
+if ! ./test.sh; then
+	echo "------------------$PACKAGE_NAME: test.sh script failed -------------------------"
+	echo "------------------$PACKAGE_NAME: trying with make test -------------------------"
+
+	if ! make build; then
+		echo "------------------$PACKAGE_NAME:build_ failed-------------------------"
+		exit 0
+	fi
+
+	echo "make test from script"
+	if ! make test; then
+		echo "------------------$PACKAGE_NAME:test_ failed-------------------------"
+		exit 0
+	fi
+fi 
+
+echo "------------------$PACKAGE_NAME: success... -------------------------"
