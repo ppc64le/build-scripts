9f2095a2bd4f961347e1d4253cc1d240340576b7
Swati Singhal <swati.singhal@ibm.com>
2021-12-23 08:16:34 +0000

    Adding build script for apollo-cache-inmemory and scala-library

---
+++ s/scala-library/scala-library_ubi8.sh
@@ -0,0 +1,47 @@
+ ----------------------------------------------------------------------------
+#
+# Package       : Scala-library
+# Version       : 2.13.6
+# Source repo   : https://github.com/scala/scala
+# Tested on     : UBI 8
+# Script License: Apache License Version 2.0
+# Maintainer    : Swati Singhal <swati.singhal@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+PACKAGE_VERSION=2.13.6
+
+echo "Usage: $0 [-v <PACKAGE_VERSION>]"
+echo "       PACKAGE_VERSION is an optional paramater whose default value is 2.13.6"
+
+PACKAGE_VERSION="${1:-$PACKAGE_VERSION}"
+
+yum install -y git curl
+
+#install java
+yum install -y java-1.8.0-openjdk-devel
+JDK_PATHS=$(compgen -G '/usr/lib/jvm/java-1.8.0-openjdk-*')
+export JAVA_HOME=${JDK_PATHS%$'\n'*}
+
+#update path
+export PATH=$PATH:$JAVA_HOME/bin
+
+#install sbt
+rm -f /etc/yum.repos.d/bintray-rpm.repo
+curl -L https://www.scala-sbt.org/sbt-rpm.repo > sbt-rpm.repo
+mv sbt-rpm.repo /etc/yum.repos.d/
+yum install -y sbt
+
+git clone https://github.com/scala/scala
+cd scala
+git checkout v$PACKAGE_VERSION
+sbt compile
+sbt Test/compile
+
+
