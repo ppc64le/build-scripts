76b6643eba47f04f9cc53e8a948e3acd046e96dc
vikasgupta8 <vikas.gupta8@ibm.com>
2021-12-27 08:16:12 +0000

    Build script for package restic.

--- r/restic/restic_ubi8.4.sh
+++ r/restic/restic_ubi8.4.sh
@@ -22,7 +22,7 @@ PACKAGE_PATH=github.com/restic/restic
 PACKAGE_VERSION=${1:-v0.12.1}
 PACKAGE_URL=https://github.com/restic/restic/
 
-yum install git wget tar make python3
+yum install -y git wget tar make python3 gcc-c++ fuse
 
 ln -s /usr/bin/python3 /usr/bin/python
 
@@ -36,7 +36,7 @@ cp -r ./ninja /usr/bin/
 ninja --version
 cd ..
 
-wget https://golang.org/dl/go1.17.4.linux-ppc64le.tar.gz && tar -C /bin -xf go1.17.4.linux-ppc64le.tar.gz && mkdir -p /home/tester/go/src /home/tester/go/bin /home/tester/go/pkg
+wget https://golang.org/dl/go1.16.4.linux-ppc64le.tar.gz && tar -C /bin -xf go1.16.4.linux-ppc64le.tar.gz && mkdir -p /home/tester/go/src /home/tester/go/bin /home/tester/go/pkg
 
 mkdir -p /home/tester/output
 export HOME_DIR=/home/tester
@@ -46,4 +46,41 @@ export GOPATH=/home/tester/go
 
 export PATH=$GOPATH/bin:$PATH
 export GO111MODULE=on
+
+echo "Building $PACKAGE_PATH with $PACKAGE_VERSION"
+
+if ! go get -d -u -t $PACKAGE_PATH@$PACKAGE_VERSION; then
+	echo "------------------$PACKAGE_NAME:install_ failed-------------------------"
+	exit 0
+fi
+
+cd $(ls -d $GOPATH/pkg/mod/$PACKAGE_PATH@$PACKAGE_VERSION)
+
+echo `pwd`
+
+go mod init $PACKAGE_PATH
+go mod tidy
+
+# building
+echo "Building and Testing $PACKAGE_PATH with $PACKAGE_VERSION"
+
+if ! go build -v ./...; then
+        echo "------------------$PACKAGE_NAME: build and  Test failed-------------------------"
+        exit 0
+else
+        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME" > /home/tester/output/test_success
+        echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success" > /home/tester/output/version_tracker
+        exit 0
+fi
+
+if ! go test -v ./...; then
+        echo "------------------$PACKAGE_NAME: build and  Test failed-------------------------"
+        exit 0
+else
+        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME" > /home/tester/output/test_success
+        echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success" > /home/tester/output/version_tracker
+        exit 0
+fi
 
