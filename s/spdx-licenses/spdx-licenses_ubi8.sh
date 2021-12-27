dae6c24d2388b5d95538355bece7769300b4d757
vedang-wartikar <81617623+vedang-wartikar@users.noreply.github.com>
2021-12-27 19:59:06 +0530

    Add build script for composer/spdx-licenses

---
+++ s/spdx-licenses/spdx-licenses_ubi8.sh
@@ -0,0 +1,38 @@
+# -----------------------------------------------------------------------------
+#
+# Package	: composer/spdx-licenses
+# Version	: 1.5.5
+# Source repo	: https://github.com/composer/spdx-licenses
+# Tested on	: RHEL 8.3
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Vedang Wartikar <vedang.wartikar@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=spdx-licenses
+PACKAGE_VERSION=1.5.5
+PACKAGE_URL=https://github.com/composer/spdx-licenses
+
+yum update -y
+
+yum module enable php:7.4 -y
+
+yum install php php-json php-devel zip unzip php-zip wget git -y
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+composer install --no-interaction --no-progress --prefer-dist --ansi
+
+sed -i '409d' /etc/php.ini && sed -i '409imemory_limit = 500M' /etc/php.ini
+
+./vendor/bin/phpunit --coverage-clover=coverage.xml
