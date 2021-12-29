4e615cc0b975f774c7d5db937b3d252756af7f2a
varsha-aaynure <Varsha.Aaynure@ibm.com>
2021-12-29 21:57:37 +0530

    Updated build-scripts.

--- a/apicurio-registry-common/apicurio-registry-common_ubi8_3.sh
+++ a/apicurio-registry-common/apicurio-registry-common_ubi8_3.sh
@@ -2,11 +2,12 @@
 #
 # Package       : apicurio-registry-common
 # Version       : 1.3.0.Final
+# Language      : Java 
 # Source repo   : https://github.com/apicurio/apicurio-registry
 # Tested on     : UBI 8.3
 # Script License: Apache-2.0 License
 # Maintainer    : Varsha Aaynure <Varsha.Aaynure@ibm.com>
-#
+# 
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
 #             It may not work as expected with newer versions of the
@@ -14,19 +15,11 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-
 #!/bin/bash
 
 #Variables
 PACKAGE_URL=https://github.com/Apicurio/apicurio-registry.git
-PACKAGE_VERSION=1.3.0.Final
-
-echo "Usage: $0 [<PACKAGE_VERSION>]"
-echo "PACKAGE_VERSION is an optional parameter whose default value is 1.3.0.Final, not all versions are supported."
-
-PACKAGE_VERSION="${1:-$PACKAGE_VERSION}"
-
-yum update -y 
+PACKAGE_VERSION="${1:-1.3.0.Final}"
 
 #Install required files
 yum install -y git maven 
