b7e0659080a430d33a3fa8f5c2f73d653151a18b
root <root@p006vm79.pbm.ihost.com>
2022-01-03 23:52:20 -0600

    build script for coreos/pkg

---
+++ p/pkg/pkg_ubi8.4.sh
@@ -0,0 +1,56 @@
+# ----------------------------------------------------------------------------
+#
+# Package        : pkg
+# Version        : v4 
+# Source repo    : https://github.com/coreos/pkg
+# Tested on      : UBI 8.4
+# Language       : GO
+# Script License : Apache License, Version 2 or later
+# Maintainer     : Sapana Khemkar <spana.khemkar@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+set -e
+
+PACKAGE_URL=https://github.com/coreos/pkg
+PACKAGE_NAME=pkg
+PACKAGE_VERSION=v4
+
+
+GO_VERSION=go1.17.5
+
+yum install -y git wget tar make gcc-c++  
+
+#install go
+rm -rf /bin/go
+wget https://golang.org/dl/$GO_VERSION.linux-ppc64le.tar.gz 
+tar -C /bin -xzf $GO_VERSION.linux-ppc64le.tar.gz  
+rm -f $GO_VERSION.linux-ppc64le.tar.gz 
+
+#set go path
+export PATH=$PATH:/bin/go/bin
+export GOPATH=/home/tester/go
+mkdir -p /home/tester/go
+cd $GOPATH
+
+#clone package
+mkdir -p $GOPATH/src/github.com/coreos
+cd $GOPATH/src/github.com/coreos
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+
+go mod init
+go mod tidy
+
+go install ./...
+go test -v ./...
+ 
+exit 0
