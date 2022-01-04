9d994bf64ca2bb6a81dceb7ef04e0d695a38b913
Sachin Kakatkar <sachin.kakatkar@ibm.com>
2022-01-04 10:51:22 +0530

    Added node 12 and git hub repo url

--- c/chosen/chosen_rhel_8_4.sh
+++ c/chosen/chosen_rhel_8_4.sh
@@ -2,8 +2,9 @@
 #
 # Package       : chosen
 # Version       : 1.0.0
-# Source repo   : https://github.com/harvesthq/chosen/archive/1.0.0.zip
+# Source repo   : https://github.com/harvesthq/chosen
 # Tested on     : UBI 8
+# Language      : HTML
 # Script License: Apache License, Version 2 or later
 # Maintainer    : sachin.kakatkar@ibm.com
 #
@@ -15,17 +16,26 @@
 #
 # ----------------------------------------------------------------------------
 #To Run: ./chosen_rhel_8_4.sh
-dnf install git wget npm make gcc-c++ bzip2 unzip ruby -y
-wget https://github.com/harvesthq/chosen/archive/1.0.0.zip
-unzip 1.0.0.zip
-mv chosen-1.0.0 chosen
+dnf install git make gcc-c++ ruby -y
+NODE_VERSION=v12.22.4
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install $NODE_VERSION
+PACKAGE_NAME=chosen
+PACKAGE_VERSION=$1
+PACKAGE_URL=https://github.com/harvesthq/chosen.git
+if [ -z "$1" ]
+  then
+    PACKAGE_VERSION=1.0.0
+fi
+git clone $PACKAGE_URL
 cd chosen
+git checkout $PACKAGE_VERSION
 npm install && gem install bundler && bundle install
 npm audit fix
 npm audit fix --force
 npm install -g grunt-cli
 npm install -g grunt
 npm install --dev coffeescript
-npm install && gem install bundler && bundle install
 grunt build
 grunt build --force
