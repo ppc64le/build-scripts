2cb231440fc03b69176faed8da0c8138b19372eb
Atharv Phadnis <atharv.phadnis@ibm.com>
2022-01-05 20:35:07 +0530

    Added build script for gopsutil

---
+++ g/gopsutil/gopsutil_ubi_8.5.sh
@@ -0,0 +1,49 @@
+# -----------------------------------------------------------------------------
+#
+# Package	: github.com/shirou/gopsutil/v3
+# Version	: v3.21.1
+# Source repo	: https://github.com/shirou/gopsutil
+# Tested on	: RHEL 8.3
+# Language      : GO
+# Travis-Check  : False
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Atharv Phadnis <Atharv.Phadnis@ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=github.com/shirou/gopsutil/v3
+PACKAGE_VERSION=${1:-v3.21.1}
+
+set -e
+
+# Used sudo assuming non-root user. 
+sudo yum install -y git golang
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+GO111MODULE=on
+
+if ! go get -d -t $PACKAGE_NAME@$PACKAGE_VERSION; then
+	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+	exit 1
+fi
+
+cd ~/go/pkg/mod/$PACKAGE_NAME*
+if ! go test ./...; then
+	echo "------------------$PACKAGE_NAME:test_fails---------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
+	exit 1
+else
+	echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass |  Install_and_Test_Success"
+	exit 0
+fi
