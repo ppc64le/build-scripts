fec4285ff21e5f27a96c763c8ab8473b4554376b
Atharv Phadnis <atharv.phadnis@ibm.com>
2022-01-05 20:24:08 +0530

    Added build script for golang_protobuf_extensions v1.0.1

---
+++ g/golang_protobuf_extensions/golang_protobuf_extensions_1.0.1_ubi_8.5.sh
@@ -0,0 +1,63 @@
+# -----------------------------------------------------------------------------
+#
+# Package	: github.com/matttproud/golang_protobuf_extensions
+# Version	: v1.0.1
+# Source repo	: https://github.com/matttproud/golang_protobuf_extensions
+# Tested on	: UBI 8.5
+# Language      : GO
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Atharv Phadnis <Atharv.Phadnis@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=github.com/matttproud/golang_protobuf_extensions
+PACKAGE_VERSION=${1:-v1.0.1}
+
+set -e
+
+yum install -y git golang make
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+if ! go get -d -t $PACKAGE_NAME@$PACKAGE_VERSION; then
+	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+	exit 1
+fi
+
+cd ~/go/pkg/mod/$PACKAGE_NAME*
+if ! go mod init $PACKAGE_NAME; then
+	echo "------------------$PACKAGE_NAME:initialize_fails-------------------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Initialize_Fails"
+	exit 1
+fi
+
+cd ~/go/pkg/mod/$PACKAGE_NAME*
+if ! go mod tidy; then
+	echo "------------------$PACKAGE_NAME:dependency_fails-------------------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Dependency_Fails"
+	exit 1
+fi
+
+cd ~/go/pkg/mod/$PACKAGE_NAME*
+if ! make -f Makefile.TRAVIS; then
+	echo "------------------$PACKAGE_NAME:test_fails---------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
+	exit 1
+else
+	echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass |  Install_and_Test_Success"
+	exit 0
+fi
