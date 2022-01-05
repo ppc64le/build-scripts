63b76c101a7b8479f55b696cd1e815c9bf224015
sapnashukla28 <Sapna.Shukla@ibm.com>
2022-01-05 07:07:25 +0000

    changed the naming convention of file and added travis-check

--- o/operand-deployment-lifecycle-manager/operand-deployment-lifecycle-manager_ubi8.5.sh
+++ o/operand-deployment-lifecycle-manager/operand-deployment-lifecycle-manager_ubi8.5.sh
@@ -3,8 +3,9 @@
 # Package       : operand-deployment-lifecycle-manager
 # Version       : v1.4.1
 # Source repo   : https://github.com/IBM/operand-deployment-lifecycle-manager
-# Tested on     : RHEL ubi 8.4
+# Tested on     : UBI  8.4
 # Language      : GO
+# Travis-Check  : True
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Sapna Shukla <Sapna.Shukla@ibm.com>
 #
@@ -31,9 +32,9 @@ PACKAGE_URL=https://github.com/IBM/operand-deployment-lifecycle-manager
 yum install -y make git wget gcc
 
 # Download and install go
-wget https://golang.org/dl/go1.14.13.linux-ppc64le.tar.gz
-tar -xzf go1.14.13.linux-ppc64le.tar.gz
-rm -rf go1.14.13.linux-ppc64le.tar.gz
+wget https://golang.org/dl/go1.17.5.linux-ppc64le.tar.gz
+tar -xzf go1.17.5.linux-ppc64le.tar.gz
+rm -rf go1.17.5.linux-ppc64le.tar.gz
 export GOPATH=`pwd`/gopath
 export PATH=`pwd`/go/bin:$GOPATH/bin:$PATH
 
