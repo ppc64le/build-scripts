e3c2eae3bb5e7520e23465aa52658ee8d2c6fb0d
root <root@p006vm88.pbm.ihost.com>
2022-01-12 12:51:11 +0000

    Build script for package go-hclog.

--- g/go-hclog/go-hclog_v0.9.2_ubi8.4.sh
+++ g/go-hclog/go-hclog_v0.9.2_ubi8.4.sh
@@ -7,7 +7,7 @@
 # Language      : GO
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: BulkPackageSearch Automation {maintainer}
+# Maintainer	: Vikas Gupta <vikas.gupta8@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
