229a8185d47f09177fc3455c69e729cf6812a44e
Sapana Khemkar <Sapana.Khemkar@ibm.com>
2022-01-13 04:10:53 -0600

    cleaned up and removed packages which are not required

--- e/erubi/erubi_ubi8.4.sh
+++ e/erubi/erubi_ubi8.4.sh
@@ -20,40 +20,18 @@
 set -e 
 
 PACKAGE_NAME=erubi
-PACKAGE_VERSION=1.10.0
+PACKAGE_VERSION=${1:-1.10.0}
 PACKAGE_URL=https://github.com/jeremyevans/erubi
 
-yum install -y nodejs nodejs-devel nodejs-packaging git wget curl ruby ruby-devel rubygem-rake procps libcurl-devel libffi-devel sqlite sqlite-devel
-
-yum install -y --allowerasing gcc gcc-c++ yum-utils make automake autoconf libtool gdb* binutils rpm-build gettext wget
-
-yum-config-manager --add-repo http://mirror.centos.org/centos/8/AppStream/ppc64le/os/ && yum-config-manager --add-repo http://mirror.centos.org/centos/8/PowerTools/ppc64le/os/ && yum-config-manager --add-repo http://mirror.centos.org/centos/8/BaseOS/ppc64le/os/ && yum-config-manager --add-repo http://mirror.centos.org/centos/8/virt/ppc64le/ovirt-44/
-
-wget https://www.centos.org/keys/RPM-GPG-KEY-CentOS-Official && mv RPM-GPG-KEY-CentOS-Official /etc/pki/rpm-gpg/. && rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-Official
-
-wget https://www.centos.org/keys/RPM-GPG-KEY-CentOS-SIG-Virtualization && mv RPM-GPG-KEY-CentOS-SIG-Virtualization /etc/pki/rpm-gpg/. && rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Virtualization
-
-#yum install -y libsodium-devel libicu-devel libicu langtable
+yum install -y gcc git make ruby ruby-devel redhat-rpm-config
 
 gem install bundle
 gem install rake
-gem install kramdown-parser-gfm
-curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
-curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
-curl -L https://get.rvm.io | bash -s stable
-export PATH=$PATH:/usr/local/rvm/bin
 
-/bin/bash -c "source /etc/profile.d/rvm.sh; rvm install ruby-2.7;"
-
-mkdir -p /home/tester/output
+mkdir -p /home/tester
 cd /home/tester
-export LC_ALL=C.UTF-8
-export LANG=en_US.UTF-8
-export LANGUAGE=en_US.UTF-8
 
-source /etc/profile.d/rvm.sh;
-
-git clone $PACKAGE_URL $PACKAGE_NAME
+git clone $PACKAGE_URL 
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
