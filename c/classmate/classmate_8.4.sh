a193dd8dadac3f38c598f430d88c9584d872fe33
root <root@p006vm88.pbm.ihost.com>
2022-01-19 08:13:00 +0000

    Updated script

--- c/classmate/classmate_8.4.sh
+++ c/classmate/classmate_8.4.sh
@@ -1,9 +1,12 @@
+#!/bin/bash -e
 #----------------------------------------------------------------------------
 #
-# Package		: classmate
-# Version		: 1.3.4, 1.5.1
+# Package		: java-classmate
+# Version		: classmate-1.3.4, classmate-1.5.1
 # Source repo		: https://github.com/FasterXML/java-classmate
 # Tested on		: RHEL 8.4
+# Language      	: Java
+# Travis-Check  	: True
 # Script License	: Apache License Version 2.0
 # Maintainer		: Vikas Gupta <vikas.gupta8@ibm.com>
 #
@@ -15,32 +18,25 @@
 #
 # ----------------------------------------------------------------------------
 
-#!/bin/bash
+set -e
 
-mkdir -p /logs
+PACKAGE_NAME="java-classmate"
+PACKAGE_VERSION=${1:-classmate-1.3.4}
+PACKAGE_URL="https://github.com/FasterXML/java-classmate.git"
 
-# variables
-PKG_NAME="classmate"
-DIR_NAME="java-classmate"
-PKG_VERSION=${1:-v1.3.4}
-#PKG_VERSION_LATEST=1.5.1
-LOGS_DIRECTORY=/logs
-LOCAL_DIRECTORY=/home/tester
-REPOSITORY="https://github.com/FasterXML/java-classmate.git"
-
-
-# ------- Clone and build source -------
+yum install maven git
 
 export PATH=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.312.b07-2.el8_5.ppc64le/bin/:$PATH
 java -version
-yum install maven
 
-mkdir -p $LOCAL_DIRECTORY
-cd $LOCAL_DIRECTORY
+mkdir -p /home/tester/output
+cd /home/tester
 
-git clone $REPOSITORY
-cd $DIR_NAME
-git checkout $PKG_NAME-$PKG_VERSION
+# ------- Clone and build source -------
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PKG_VERSION
 mvn clean install | tee $LOGS_DIRECTORY/$PKG_NAME-$PKG_VERSION.txt
 
 echo "`date +'%d-%m-%Y %T'` - Installed classmate ---------------------------------------"
