9a55aaa763b14652e87d98ec74cb117e04e6a0f9
Nishikant Thorat <Nishikant.Thorat@ibm.com>
2022-01-19 07:34:13 +0000

    Changes from review comments

--- b/boringssl/boringssl_ubuntu_18.04.sh
+++ b/boringssl/boringssl_ubuntu_18.04.sh
@@ -2,7 +2,7 @@
 # ----------------------------------------------------------------------------
 #
 # Package       : boringssl
-# Version       : n/a
+# Version       : Commit(36a41bf0bf2dd3176f8780e09c03585351f29963)
 # Source repo   : https://boringssl.googlesource.com/boringssl/
 # Tested on     : ubuntu_18.04
 # Script License: Apache License, Version 2 or later
@@ -20,8 +20,8 @@
 #
 # Install dependencies
 #
+apt-get update -y
 apt-get install -y sudo
-sudo apt-get update -y
 sudo apt-get install -y cmake build-essential g++ wget git pkg-config libunwind-dev
 #
 # Install go
@@ -33,12 +33,13 @@ rm go1.15.11.linux-ppc64le.tar.gz
 #
 # Clone and build source code
 #
+PACKAGE_VERSION=${1:-36a41bf0bf2dd3176f8780e09c03585351f29963}
 git clone https://boringssl.googlesource.com/boringssl/
 cd boringssl
+git checkout $PACKAGE_VERSION
 mkdir build
 cd build
 cmake ..
 make
 make all_tests
 make run_tests
-
