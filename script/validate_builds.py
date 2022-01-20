7e5325c80f32a2ec9cbc1a63a577258ac10db53b
Ryan D'Mello <Ryan.Dmello1@ibm.com>
2022-01-20 18:44:55 +0530

    Travis-Check flag check added, along with review suggestions

--- script/validate_builds.py
+++ script/validate_builds.py
@@ -20,6 +20,8 @@ def trigger_basic_validation_checks(file_name):
         "# Language": "package_type",
         "# Travis-Check": ""
     }
+    should_travis_run_for_script = True
+    package_name = package_version = package_url = None
     matched_keys = []
     # Check if apache license file exists
     file_parts = file_name.split('/')
@@ -48,12 +50,43 @@ def trigger_basic_validation_checks(file_name):
                     elif key in line:
                         matched_keys.append(key)
                         package_data[key_checks[key]] = line.split(':')[-1].strip()
-            except IndexError as ie:
-                raise IndexError(str(ie))
+                        if line.startswith('# Travis-Check'):
+                            key_value = line.split(':')
+                            should_travis_run_for_script = eval(key_value[-1].strip())
+                        elif line.startswith('# Package'):
+                            key_value = line.split(':')
+                            package_name = key_value[1].strip()
+                        elif line.startswith('# Version'):
+                            key_value = line.split(':')
+                            package_version = key_value[1].strip()
+                        elif line.startswith('# Source repo'):
+                            key_value = line.split(':')
+                            package_url = ":".join(key_value[1:]).strip()
+            except (IndexError, TypeError) as ie:
+                print("Failure: {}".format(str(ie)))
+                raise Exception(str(ie))
+            # Assuming, actual script will be available, once doc string is completely parsed.
+            if package_name and 'PACKAGE_NAME=' in line:
+                key_val = line.split('=')
+                alt_package_name_value = "${1:-%s}"%package_name
+                if str(package_name) != str(key_val[-1].strip()) and str(alt_package_name_value) != str(key_val[-1].strip()):
+                    raise ValueError("PACKAGE_NAME in script, does not match the Package in doc string")
+            elif package_version and 'PACKAGE_VERSION=' in line:
+                key_val = line.split('=')
+                alt_version_value = "${1:-%s}"%package_version
+                if str(package_version) != str(key_val[-1].strip()) and str(alt_version_value) != str(key_val[-1].strip()):
+                    raise ValueError("PACKAGE_VERSION in script, does not match the Version in doc string")
+            elif package_url and 'PACKAGE_URL=' in line:
+                key_val = line.split('=')
+                alt_package_url_value = "${1:-%s}"%package_url
+                if str(package_url) != str(key_val[-1].strip()) and str(alt_package_url_value) != str(key_val[-1].strip()):
+                    raise ValueError("PACKAGE_URL in script, does not match the Source repo in doc string")
+        # Also check if package name, version and URL from doc string matches with the one used in script
         # check if all required keys are available
         if set(matched_keys) == set(list(key_checks.keys())):
             print("Basic Checks passed")
-            return True
+            # Check if Travis validation for build scripts needs to be triggered
+            return should_travis_run_for_script
         else:
             print("Basic Validation Checks Failed!!!")
             print("Requried keys: {}".format(",".join(key_checks.keys())))
@@ -61,6 +94,7 @@ def trigger_basic_validation_checks(file_name):
             raise ValueError("Basic Validation Checks Failed!!!")
     else:
         raise ValueError("Build script not found.")
+    return should_travis_run_for_script
 
 def trigger_script_validation_checks(file_name, image_name = "registry.access.redhat.com/ubi8/ubi:8.5"):
     # Spawn a container and pass the build script
@@ -100,9 +134,12 @@ def trigger_build_validation_travis(pr_number):
         file_name = i.get('filename', "")
         if file_name.endswith('.sh'):
             # perform basic validation check
-            trigger_basic_validation_checks(file_name)
-            # Build/test script files
-            trigger_script_validation_checks(file_name)
+            should_travis_run_for_script = trigger_basic_validation_checks(file_name)
+            if should_travis_run_for_script:
+                # Build/test script files
+                trigger_script_validation_checks(file_name)
+            else:
+                print("Skipping Build script validation as Travis-Check flag is set to False")
 
 if __name__=="__main__":
     trigger_build_validation_travis(sys.argv[1])
