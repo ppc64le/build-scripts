840c8c615dfb2c0995801c6d71384cee7bf55941
bhagat-singh001 <98086129+bhagat-singh001@users.noreply.github.com>
2022-01-24 11:08:27 +0530

    Added build script for downshift.

---
+++ d/downshift/downshift_v5.2.1_v1.31.16_rhel_8.5.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -e
+
+# -----------------------------------------------------------------------------
+#
+# Package           : downshift
+# Version           : v5.2.1, v1.31.16
+# Source repo       : https://github.com/downshift-js/downshift
+# Tested on         : RHEL 8.5,UBI 8.5
+# Language          : Node
+# Travis-Check      : True
+# Script License    : Apache License, Version 2 or later
+# Maintainer        : Bhagat Singh <Bhagat.singh1@ibm.com>
+#
+# Disclaimer        : This script has been tested in root mode on given
+# ==========          platform using the mentioned version of the package.
+#                     It may not work as expected with newer versions of the
+#                     package and/or distribution. In such case, please
+#                     contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+#Exit immediately if a command exits with a non-zero status.    
+set -e
+
+PACKAGE_NAME=downshift
+#PACKAGE_VERSION is configurable can be passed as an argument.
+PACKAGE_VERSION=${1:-v5.2.1}
+PACKAGE_URL=https://github.com/downshift-js/downshift
+
+yum install -y yum-utils nodejs nodejs-devel nodejs-packaging npm git jq
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+#Check if package exists
+if [ -d "$PACKAGE_NAME" ] ; then
+  rm -rf $PACKAGE_NAME
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"  
+ 
+fi
+ 
+
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+        exit 0
+fi
+
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+PACKAGE_VERSION=$(jq -r ".version" package.json)
+# run the test command from test.sh
+# Ignore all peerDependencies when installing  --legacy-peer-deps
+
+if ! npm install --legacy-peer-deps; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! npm test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 1
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
