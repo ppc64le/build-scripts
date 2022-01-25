bb1e896658565a10d12e32d2745f683ca5c79355
Sapana Khemkar <Sapana.Khemkar@ibm.com>
2022-01-24 22:45:38 -0600

    build script for path_expander

---
+++ p/path_expander/path_expander_ubi8.4.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package	: path_expander
+# Version	: v1.0.2
+# Source repo	: https://github.com/seattlerb/path_expander
+# Tested on	: UBI 8.4
+# Language      : Ruby
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Sapana Khemkar {Sapana.khemkar@ibm.com}
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=path_expander
+PACKAGE_VERSION=${1:-v1.0.2}
+PACKAGE_URL=https://github.com/seattlerb/path_expander.git
+
+yum install -y git wget curl ruby ruby-devel rubygem-rake procps libcurl-devel libffi-devel
+
+yum install -y --allowerasing gcc gcc-c++ yum-utils make automake autoconf libtool gdb* binutils rpm-build gettext 
+
+yum-config-manager --add-repo http://mirror.centos.org/centos/8/AppStream/ppc64le/os/ && yum-config-manager --add-repo http://mirror.centos.org/centos/8/PowerTools/ppc64le/os/ && yum-config-manager --add-repo http://mirror.centos.org/centos/8/BaseOS/ppc64le/os/ && yum-config-manager --add-repo http://mirror.centos.org/centos/8/virt/ppc64le/ovirt-44/
+
+wget https://www.centos.org/keys/RPM-GPG-KEY-CentOS-Official && mv RPM-GPG-KEY-CentOS-Official /etc/pki/rpm-gpg/. && rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-Official
+
+wget https://www.centos.org/keys/RPM-GPG-KEY-CentOS-SIG-Virtualization && mv RPM-GPG-KEY-CentOS-SIG-Virtualization /etc/pki/rpm-gpg/. && rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Virtualization
+
+gem install bundle
+gem install rake
+gem install kramdown-parser-gfm
+curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
+curl -L https://get.rvm.io | bash -s stable
+export PATH=$PATH:/usr/local/rvm/bin
+
+/bin/bash -c "source /etc/profile.d/rvm.sh; rvm install ruby-2.6"
+
+mkdir -p /home/tester/output
+cd /home/tester
+
+export LC_ALL=C.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+source /etc/profile.d/rvm.sh;
+
+git clone $PACKAGE_URL 
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+gem install hoe
+rake 
+exit 0
