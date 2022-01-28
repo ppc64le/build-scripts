918118a3e1d26759fc3ff252d18281416ceeb227
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2022-01-28 18:10:00 +0530

    Update keyv_ubi_8.5.sh

--- k/keyv/keyv_ubi_8.5.sh
+++ k/keyv/keyv_ubi_8.5.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : keyv
-# Version       : v4.0.4
+# Version       : v4.0.4,v4.0.3
 # Source repo   : https://github.com/lukechilds/keyv
 # Tested on	: UBI 8.5
 # Language      : Node
@@ -20,52 +20,57 @@
 PACKAGE_NAME="keyv"
 PACKAGE_VERSION=${1:-"v4.0.4"}
 PACKAGE_URL="https://github.com/lukechilds/keyv"
-export NODE_VERSION=${NODE_VERSION:-v12.22.4}
-OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
 
-yum install -y git
+yum -y install git wget gcc-c++ make curl python36 openssl openssl-devel jq
+alias python=python3
+ln -s /usr/bin/python3 /usr/bin/python
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+NODE_VERSION=v12.22.1
+#installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.bashrc
+nvm install $NODE_VERSION
 
-nvm install "$NODE_VERSION"
-npm install -g npm@latest
+mkdir -p /home/tester
+cd /home/tester
 
-HOME_DIR=$PWD
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
 
-echo "cloning..."
-if ! git clone -q $PACKAGE_URL $PACKAGE_NAME; then
-	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-	exit 1
+#Check if package exists
+if [ -d "$PACKAGE_NAME" ] ; then
+  rm -rf $PACKAGE_NAME
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
+
 fi
 
-cd "$HOME_DIR"/$PACKAGE_NAME || exit 1
-git checkout "$PACKAGE_VERSION" || exit 1
-if ! npm install && npm audit fix; then
-	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
-	exit 1
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+        exit 0
 fi
-
-cd "$HOME_DIR"/$PACKAGE_NAME || exit 1
-if ! npm run | grep -q "test"; then
-	echo "------------------$PACKAGE_NAME:install_success_but_test_not_present---------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub |  |  Install_success_but_test_not_present"
-	exit 0
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+PACKAGE_VERSION=$(jq -r ".version" package.json)
+# run the test command from test.sh
+if ! npm install; then
+        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
 fi
-
 if ! npm test; then
-	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
-	exit 1
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 1
 else
-	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
-	exit 0
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
 fi
+
+
