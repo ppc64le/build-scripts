0003234371741e24e82886f8168b189d021a387f
kumarvikas2020 <64921043+kumarvikas2020@users.noreply.github.com>
2022-01-28 15:07:05 +0530

    Updated script with latest supported node version

--- s/string_decoder/string_decoder_ubi_8.5.bash
+++ s/string_decoder/string_decoder_ubi_8.5.bash
@@ -3,7 +3,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : string_decoder
-# Version       : v1.1.1
+# Version       : v1.1.1, v1.2.0, v1.3.0
 # Source repo	: https://github.com/nodejs/string_decoder.git
 # Tested on     : UBI 8.5
 # Language      : Node
@@ -19,15 +19,20 @@
 #
 # ----------------------------------------------------------------------------
 
-#Exit immediately if a command exits with a non-zero status.
-set -e
-
 PACKAGE_NAME=string_decoder
 #PACKAGE_VERSION is configurable can be passed as an argument.
 PACKAGE_VERSION=${1:-v1.1.1}
 PACKAGE_URL=https://github.com/nodejs/string_decoder.git
 
-yum install -y yum-utils nodejs nodejs-devel nodejs-packaging npm git jq
+yum install -y git jq
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install node v11.15.0 and corresponding npm version. This is the maximum node version supported for this package.
+nvm install --latest-npm v11.15.0
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
@@ -37,7 +42,7 @@ if [ -d "$PACKAGE_NAME" ] ; then
   echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"  
  
 fi
- 
+
 
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
