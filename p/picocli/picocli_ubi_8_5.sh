56e2034655e78347a6966f2e799d640aa485d816
Saurabh Gore <saurabh.gore@ibm.com>
2022-01-31 10:08:18 +0530

    Updated build script for picocli

---
+++ p/picocli/picocli_ubi_8_5.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+#
+# Package       : picocli
+# Version       : 4.0.4 / 4.6.1 
+# Source repo   : https://github.com/remkop/picocli
+# Tested on		: UBI 8.5
+# Language      : Java
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Saurabh Gore <Saurabh.Gore@ibm.com> 
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=picocli
+PACKAGE_VERSION="${1:-v4.0.4}"
+PACKAGE_URL=https://github.com/remkop/picocli.git
+
+# Install required dependencies
+yum install -y git  java-1.8.0-openjdk-devel
+
+#Clonning repo
+git config --global core.longpaths true
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME/
+
+git checkout $PACKAGE_VERSION
+
+#Build and test package
+./gradlew build
