65a818a9242a01f55104d47082c872a79e004d31
bhagat-singh001 <98086129+bhagat-singh001@users.noreply.github.com>
2022-01-31 13:59:15 +0530

    Update set-value_rhel_8.5.sh

--- s/set-value/set-value_rhel_8.5.sh
+++ s/set-value/set-value_rhel_8.5.sh
@@ -26,9 +26,12 @@ PACKAGE_URL=https://github.com/jonschlinkert/set-value
 
 yum install -y yum-utils git jq
 
-#Install node latest version.
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-source ~/.bashrc
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install latest version of node and npm
 nvm install --latest-npm node
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
