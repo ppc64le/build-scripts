ba6d32c3c433f4dbd06ee258279c6e22df649b9a
bhagat-singh001 <98086129+bhagat-singh001@users.noreply.github.com>
2022-01-31 09:48:12 +0530

    Added node latest version for test.

--- h/httpsnippet/httpsnippet_rhel_8.5.sh
+++ h/httpsnippet/httpsnippet_rhel_8.5.sh
@@ -2,14 +2,14 @@
 
 # -----------------------------------------------------------------------------
 #
-# Package	:     httpsnippet
-# Version	:     v1.16.7
-# Source repo	: https://github.com/Mashape/httpsnippet
-# Tested on	:     RHEL 8.5,UBI 8.5
+# Package   :     httpsnippet
+# Version   :     v1.16.7
+# Source repo   : https://github.com/Mashape/httpsnippet
+# Tested on :     RHEL 8.5,UBI 8.5
 # Language      : Node
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: Bhagat Singh <Bhagat.singh1@ibm.com>
+# Maintainer    : Bhagat Singh <Bhagat.singh1@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -19,15 +19,17 @@
 #
 # ----------------------------------------------------------------------------
 
-#Exit immediately if a command exits with a non-zero status.
-set -e
-
 PACKAGE_NAME=httpsnippet
 #PACKAGE_VERSION is configurable can be passed as an argument.
 PACKAGE_VERSION=${1:-v1.16.7}
 PACKAGE_URL=https://github.com/Mashape/httpsnippet
 
-yum install -y yum-utils nodejs nodejs-devel nodejs-packaging npm git jq
+yum install -y yum-utils git jq
+
+#Install node latest version.
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install --latest-npm node
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
@@ -38,10 +40,10 @@ if [ -d "$PACKAGE_NAME" ] ; then
 fi
  
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
-    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-		echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    	exit 0
+        exit 0
 fi
 
 cd  $PACKAGE_NAME
@@ -50,22 +52,22 @@ PACKAGE_VERSION=$(jq -r ".version" package.json)
 # run the test command from test.sh
 
 if ! npm install && npm audit fix && npm audit fix --force; then
-     	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
-	exit 1
+        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
 fi
 
 if ! npm test; then
-	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
-	exit 1
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 1
 else
-	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
-	exit 0
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
 fi
 
 # Test is in parity with intel x86. Install_success_but_test_fails
