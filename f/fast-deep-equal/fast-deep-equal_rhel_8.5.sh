c557c2328fde556695af9921f2037632c39dfdc4
bhagat-singh001 <98086129+bhagat-singh001@users.noreply.github.com>
2022-01-31 09:44:32 +0530

    Added node latest version for test.

--- f/fast-deep-equal/fast-deep-equal_rhel_8.5.sh
+++ f/fast-deep-equal/fast-deep-equal_rhel_8.5.sh
@@ -19,15 +19,17 @@
 #   
 # ----------------------------------------------------------------------------
 
-#Exit immediately if a command exits with a non-zero status.
-set -e
-
 PACKAGE_NAME=fast-deep-equal
 #PACKAGE_VERSION is configurable can be passed as an argument.
 PACKAGE_VERSION=${1:-v3.1.3}
 PACKAGE_URL=https://github.com/epoberezkin/fast-deep-equal
 
-yum install -y yum-utils nodejs nodejs-devel nodejs-packaging npm git jq
+yum install -y yum-utils git jq
+
+#Install node latest version.
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install --latest-npm node
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
