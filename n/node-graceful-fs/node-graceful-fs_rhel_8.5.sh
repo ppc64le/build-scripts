a3e63ddfecc13aa0ba1e7d588c9a50e7b6d8f2f0
bhagat-singh001 <98086129+bhagat-singh001@users.noreply.github.com>
2022-01-31 09:52:53 +0530

    Added node latest version for test.

--- n/node-graceful-fs/node-graceful-fs_rhel_8.5.sh
+++ n/node-graceful-fs/node-graceful-fs_rhel_8.5.sh
@@ -19,15 +19,17 @@
 #
 # ----------------------------------------------------------------------------
 
-#Exit immediately if a command exits with a non-zero status.
-set -e
-
 PACKAGE_NAME=node-graceful-fs
 #PACKAGE_VERSION is configurable can be passed as an argument.
 PACKAGE_VERSION=${1:-v4.2.6}
 PACKAGE_URL=https://github.com/isaacs/node-graceful-fs
 
-yum install -y yum-utils nodejs nodejs-devel nodejs-packaging npm git jq
+yum install -y yum-utils git jq
+
+#Install node latest version.
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install --latest-npm node
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
