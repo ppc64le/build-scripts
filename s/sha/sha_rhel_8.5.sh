440cee4848cebc9de54d259133b0023b558ef2a6
bhagat-singh001 <98086129+bhagat-singh001@users.noreply.github.com>
2022-02-01 10:39:40 +0530

    Update sha_rhel_8.5.sh

--- s/sha/sha_rhel_8.5.sh
+++ s/sha/sha_rhel_8.5.sh
@@ -33,8 +33,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Install latest version of node and npm
-nvm install node npm
-
+nvm install node
+node -v
+nvm install-latest-npm
+npm -v
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
