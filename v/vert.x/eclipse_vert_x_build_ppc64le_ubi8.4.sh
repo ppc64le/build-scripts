4495f43e831730dca638514561348d26e06d2c6b
root <root@p006vm88.pbm.ihost.com>
2022-02-02 15:58:58 +0000

    updated script

--- v/vert.x/eclipse_vert_x_build_ppc64le_ubi8.4.sh
+++ v/vert.x/eclipse_vert_x_build_ppc64le_ubi8.4.sh
@@ -1,12 +1,14 @@
+#!/bin/bash -e
 # ----------------------------------------------------------------------------
 #
 # Package       : vert.x
 # Version       : 3.9.7 and 4.1.2
 # Source repo   : https://github.com/eclipse-vertx/vert.x
-# Tested on     : rhel UBI 8.4
+# Tested on     : UBI 8.4
+# Language      : Java
+# Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer's  : Santosh Magdum <santosh.magdum@us.ibm.com>
-#                 Priya Seth <priya.seth@us.ibm.com>
+# Maintainer	: Vikas Gupta <vikas.gupta8@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -15,7 +17,6 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-#!/bin/bash
 
 PACKAGE_NAME=vert.x
 PACKAGE_PATH=github.com/eclipse-vertx/vert.x
@@ -28,27 +29,8 @@ echo "`date +'%d-%m-%Y %T'` - Staring eclipse vert.x build. Dependencies will be
 
 # ------- Install dependencies -------
 
-yum -y install maven make python3
-
-yum -y install subscription-manager.ppc64le
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-subscription-manager repos --enable rhel-7-for-power-le-extras-rpms
-yum -y install epel-release
-
-yum -y install gcc-c++.ppc64le
-yum -y install wget
-yum -y install git
+yum -y install git wget gcc-c++ cmake maven make python3 autoconf automake libtool glibc-devel openssl-devel lksctp-tools apr-devel apr-util-devel
 yum -y install java-1.8.0-openjdk-devel
-dnf install openssl-devel.ppc64le -y
-dnf group install "Development Tools" -y
-
-dnf install ninja-build.ppc64le -y
-dnf install golang -y
-dnf install autoconf -y
-dnf install automake -y
-dnf install libtool -y
-dnf install make tar glibc-devel libaio-devel openssl-devel lksctp-tools -y
-dnf install apr-devel apr-util-devel -y
 
 ln -s /usr/bin/python3 /usr/bin/python
 
@@ -56,21 +38,8 @@ echo "`date +'%d-%m-%Y %T'` - Installed Standard Packages ----------------------
 echo "---------------------------------------------------------------------------------------"
 
 # ------- Clone and build source -------
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.312.b07-2.el8_5.ppc64le/
-
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.322.b06-2.el8_5.ppc64le/
 cd $BUILD_HOME
-
-# --------- Installing cmake version 3.21.2 -----------------
-echo "--------- Installing cmake version 3.21.2 -----------------"
-
-wget https://github.com/Kitware/CMake/releases/download/v3.21.2/cmake-3.21.2.tar.gz
-tar -xvf cmake-3.21.2.tar.gz
-cd cmake-3.21.2
-./bootstrap
-make
-make install
-cd ..
-
 
 # --------- Installing ninja version v1.4.0 -----------------
 echo "--------- Installing ninja version v1.4.0 -----------------"
@@ -91,7 +60,7 @@ git clone --recurse https://github.com/netty/netty-tcnative.git
 cd netty-tcnative
 git checkout netty-tcnative-parent-2.0.36.Final
 
-./mvnw clean install -DskipTests
+./mvnw clean install
 cd ..
 
 #-------- Building netty version 4.1.60.Final ----------------
