7b6df6311f96fd8fad06b83c2f1047d5c6137fa7
Sapana-Khemkar <94051076+Sapana-Khemkar@users.noreply.github.com>
2022-02-02 16:23:14 +0530

    updated build script of bcprov-jdk15on to support version 1.70 (#1696)
    * use correct git url for sentry sdk build script
    
    * merge with latest master code
    
    * update script to support version r1rv70

--- b/bc-java/bcprov-jdk15on_ubi_8.3.sh
+++ b/bc-java/bcprov-jdk15on_ubi_8.3.sh
@@ -2,7 +2,7 @@
 # ----------------------------------------------------------------------------
 #
 # Package         : bcprov-jdk15on
-# Version         : r1rv61, r1rv68
+# Version         : r1rv61, r1rv68, r1rv70
 # Source repo     : https://github.com/bcgit/bc-java
 # Tested on       : UBI: 8.3
 # Language        : Java
@@ -19,8 +19,6 @@
 #
 # ----------------------------------------------------------------------------
 
-set -e
-
 PACKAGE_NAME=bc-java
 PACKAGE_URL=https://github.com/bcgit/bc-java
 PACKAGE_VERSION="${1:-r1rv61}"
@@ -29,16 +27,24 @@ yum install -y git wget unzip
 yum install -y java-1.8.0-openjdk-devel
 
 # install gradle
-wget https://downloads.gradle-dn.com/distributions/gradle-3.3-all.zip
-unzip -d /opt/gradle gradle-3.3-all.zip
-ls /opt/gradle/gradle-3.3/
-export PATH=$PATH:/opt/gradle/gradle-3.3/bin
+if [ "$PACKAGE_VERSION" = "r1rv70" ]
+then 
+  GRADLE_VERSION=5.1.1
+else
+  GRADLE_VERSION=3.3
+fi
+wget https://downloads.gradle-dn.com/distributions/gradle-$GRADLE_VERSION-all.zip
+unzip -d /opt/gradle gradle-$GRADLE_VERSION-all.zip
+ls /opt/gradle/gradle-$GRADLE_VERSION/
+export PATH=$PATH:/opt/gradle/gradle-$GRADLE_VERSION/bin
 
 #Cloning Repo
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout ${PACKAGE_VERSION}
 cd prov
+
+export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF8"
 
 #Build and test package
 gradle build
