b738d8469e749ffe0f59462fa6e280e677e8acee
Vaibhav <vaibhav.bhadade@ibm.com>
2022-02-02 13:38:52 +0000

    remove EOL charecters

--- a/azure-sdk-for-python/azure-mgmt-servicebus_0.5.3_ubi_8.sh
+++ a/azure-sdk-for-python/azure-mgmt-servicebus_0.5.3_ubi_8.sh
@@ -8,7 +8,7 @@
 # Language      : Python
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: Vaibhav Bhadade <vaibhav.bhadade@ibm.com> 
+# Maintainer	: Vaibhav Bhadade <vaibhav.bhadade@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
