3609152060cacafe8e72f5996bb5d630fa8cf432
JotirlingSwami <jotirling.swami1@ibm.com>
2022-02-03 17:05:03 +0530

    Added build script for javamail

---
+++ j/javax-mail/javax.mail_v1.6.2_ubi_8.3.sh
@@ -0,0 +1,79 @@
+#!/bin/bash -e
+#----------------------------------------------------------------------------
+#
+# Package       : javax-mail
+# Version       : JAVAMAIL-1_6_2
+# Source repo   : https://github.com/javaee/javamail
+# Tested on     : ubuntu_18.04 (Docker)
+# Language      : Java
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Jotirling Swami <Jotirling.Swami1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------------------------------
+
+set -ex
+
+#Variables
+PACKAGE_NAME=javamail
+PACKAGE_VERSION=JAVAMAIL-1_6_2
+PACKAGE_URL=https://github.com/javaee/javamail.git
+
+#Extract version from command line
+echo "Usage: $0 [-v <PACKAGE_VERSION>]"
+echo "PACKAGE_VERSION is an optional paramater whose default value is $PACKAGE_VERSION and building for ${1:-$PACKAGE_VERSION}"
+
+PACKAGE_VERSION="${1:-$PACKAGE_VERSION}"
+
+#Dependencies
+yum install -y git wget java-1.8.0-openjdk-devel
+
+#install maven
+cd /opt/
+wget https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -P /tmp
+tar -xzvf /tmp/apache-maven-3.6.3-bin.tar.gz -C /opt
+ln -s /opt/apache-maven-3.6.3 /opt/maven
+export M2_HOME=/opt/maven
+export PATH=${M2_HOME}/bin:${PATH}
+mvn -version
+
+#Clone repo
+HOME_DIR=`pwd`
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    exit 1
+fi
+
+#Build and test
+cd $HOME_DIR/$PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! mvn clean install -DskipTests ; then
+	echo "------------------$PACKAGE_NAME:dependency_fails-------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Dependency_Fails"
+	exit 1
+fi
+
+#test failure is in parity with x86 
+if ! mvn test; then
+	echo "------------------$PACKAGE_NAME:test_fails---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
+	exit 1
+else
+	echo "------------------$PACKAGE_NAME:build_and_test_success-------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Build_and_Test_Success"
+	exit 0
+fi
