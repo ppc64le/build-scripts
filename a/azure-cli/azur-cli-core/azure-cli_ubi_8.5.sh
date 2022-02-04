44e202ffd4ef869cf2773c1bc524995f7db235b3
Vaibhav <vaibhav.bhadade@ibm.com>
2022-02-04 08:25:36 +0000

    travis failed for folder not found

--- a/azure-cli/azur-cli-core/azure-cli_ubi_8.5.sh
+++ a/azure-cli/azur-cli-core/azure-cli_ubi_8.5.sh
@@ -31,7 +31,7 @@ cd /home/tester
 ln -s /usr/bin/python3 /usr/bin/python
 
 echo "Cloning pacakge $PACKAGE_URL"
-if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+if ! git clone $PACKAGE_URL ; then
         echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
                 echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/clone_fails
                 echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail |  Clone_Fails" > /home/tester/output/version_tracker
