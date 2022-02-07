190b4414623be8bced2fd30c950721c58dd84e92
Manik-ibm <manik.fulpagar@ibm.com>
2022-02-07 04:33:47 -0600

    removed unwanted lines

--- m/MongoDB-driver-core/mongodb-driver-core_4.1.2_4.2.2_ubuntu_18.04.sh
+++ m/MongoDB-driver-core/mongodb-driver-core_4.1.2_4.2.2_ubuntu_18.04.sh
@@ -46,7 +46,7 @@ PACKAGE_VERSION="${1:-$PACKAGE_VERSION}"
 ARCH=$(arch)
 
 # install tools and dependent packages
-apt update && apt install -y git wget curl unzip nano vim make dos2unix openjdk-11-jdk
+apt update && apt install -y git wget curl unzip nano vim make dos2unix openjdk-11-jdk python3
 
 # install gradle 
 GRADLE_VERSION=6.2.2
@@ -58,23 +58,44 @@ rm -rf gradle-$GRADLE_VERSION-bin.zip
 export GRADLE_HOME=/usr/local/gradle
 export PATH=$PATH:$GRADLE_HOME/gradle-$GRADLE_VERSION/bin
 
-export WORK_DIR=`pwd`
-export HOME=$WORK_DIR
+export HOME=/home/tester
+
+mkdir -p /home/tester/output
+cd /home/tester
+
+ln -s /usr/bin/python3 /bin/python
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
-# clone repo
-if ! git clone $PACKAGE_URL ; then
-	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails" > $WORK_DIR/output/version_tracker
-	exit 0
+function get_checkout_url(){
+        url=$1
+        CHECKOUT_URL=`python3 -c "url='$url';github_url=url.split('tree')[0];print(github_url);"`
+        echo $CHECKOUT_URL
+}
+
+function get_working_path(){
+        url=$1
+        CHECKOUT_URL=`python3 -c "url='$url';github_url,uri=url.split('tree');uris=uri.split('/');print('/'.join(uris[2:]));"`
+        echo $CHECKOUT_URL
+}
+
+CLONE_URL=$(get_checkout_url $PACKAGE_URL)
+
+if [ "$PACKAGE_URL" = "$CLONE_URL" ]; then
+        WORKING_PATH="./"
+else
+        WORKING_PATH=$(get_working_path $PACKAGE_URL)
 fi
 
-export HOME_DIR=$WORK_DIR/$PACKAGE_NAME
+if ! git clone $CLONE_URL $PACKAGE_NAME; then
+	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/clone_fails
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails" > /home/tester/output/version_tracker
+	exit 1
+fi
 
-cd $HOME_DIR
+cd /home/tester/$PACKAGE_NAME
 git checkout $PACKAGE_VERSION
-
 #gradle build | tee $LOGS_DIRECTORY/$PACKAGE_NAME-$PACKAGE_VERSION.txt
 
 function try_gradle_with_jdk11(){
@@ -83,12 +104,12 @@ function try_gradle_with_jdk11(){
     echo "JAVA_HOME is $JAVA_HOME"
     export PATH=$PATH:$JAVA_HOME/bin
 	
-    cd $WORK_DIR/$PACKAGE_NAME/driver-core
+    cd /home/tester/$PACKAGE_NAME/driver-core
 	if ! gradle build; then
         exit 0
 	fi
 
-	cd $WORK_DIR/$PACKAGE_NAME/driver-core
+	cd /home/tester/$PACKAGE_NAME/driver-core
 	if ! gradle test; then
 		echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
 		echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/test_fails 
@@ -98,7 +119,6 @@ function try_gradle_with_jdk11(){
 		echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
 		echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/test_success 
 		echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success" > /home/tester/output/version_tracker
-		get_list_of_jars_generated $HOME_DIR /home/tester/output/post_build_jars.txt
 		exit 0
 	fi
 }
