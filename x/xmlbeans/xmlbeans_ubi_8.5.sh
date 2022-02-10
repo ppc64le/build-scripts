3afd3f32bfb2689c252ba195465c94bdcbeca8e5
bhagat-singh001 <98086129+bhagat-singh001@users.noreply.github.com>
2022-02-10 11:09:58 +0530

    Update xmlbeans_ubi_8.5.sh

--- x/xmlbeans/xmlbeans_ubi_8.5.sh
+++ x/xmlbeans/xmlbeans_ubi_8.5.sh
@@ -26,10 +26,11 @@ PACKAGE_VERSION=${1:-REL_5_0_2}
 
 #Dependencies
 yum install -y java-11-openjdk-devel git wget unzip
+cd /opt/
 wget https://dlcdn.apache.org//ant/binaries/apache-ant-1.10.12-bin.zip
 unzip apache-ant-1.10.12-bin.zip
-export ANT_HOME=apache-ant-1.10.12
-export PATH=apache-ant-1.10.12/bin:$PATH
+export ANT_HOME=/opt/apache-ant-1.10.12
+export PATH=/opt/apache-ant-1.10.12/bin:$PATH
 
 # Cloning the repository from remote to local
 git clone $PACKAGE_URL
