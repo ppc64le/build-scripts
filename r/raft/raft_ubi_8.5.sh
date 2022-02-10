d1a366e4cf26ff51e918382f542ae1d69e075a58
adilhusain-s <95226191+adilhusain-s@users.noreply.github.com>
2022-02-10 20:01:15 +0530

    adding raft script for ubi 8.5 (#1756)

---
+++ r/raft/raft_ubi_8.5.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package	: raft
+# Version	: v1.1.2
+# Source repo	: https://github.com/hashicorp/raft
+# Tested on	: ubi 8.5
+# Language      : GO
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Adilhusain Shaikh <Adilhusain.Shaikh@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME="raft"
+PACKAGE_VERSION=${1:-"v1.1.2"}
+PACKAGE_URL="https://github.com/hashicorp/raft"
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+export GO_VERSION=${GO_VERSION:-"1.16"}
+export GOROOT=${GOROOT:-"/usr/local/go"}
+export GOPATH=${GOPATH:-$HOME/go}
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:/usr/local/bin
+PACKAGE_SOURCE_ROOT=$(awk -F '/' '{print  "/src/" $3 "/" $4;}' <<<"$PACKAGE_URL" | xargs printf "%s" "$GOPATH")
+export PACKAGE_SOURCE_ROOT
+
+echo "installing dependencies from system repo"
+dnf install -y wget git gcc-c++ make >/dev/null
+
+# installing golang
+wget https://golang.org/dl/go"$GO_VERSION".linux-ppc64le.tar.gz
+tar -C /usr/local/ -xzf go"$GO_VERSION".linux-ppc64le.tar.gz
+rm -f go"$GO_VERSION".linux-ppc64le.tar.gz
+
+if ! git clone "$PACKAGE_URL" "$PACKAGE_SOURCE_ROOT"/"$PACKAGE_NAME"; then
+    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    exit 1
+fi
+
+cd "$PACKAGE_SOURCE_ROOT"/"$PACKAGE_NAME"
+#git checkout "$PACKAGE_VERSION" || exit 1
+export GO111MODULE=on
+go mod tidy
+if ! make deps; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! make test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 1
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
