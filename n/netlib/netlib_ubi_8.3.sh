9d0f3b7291e563cd8f6115012e4ec45ec3b38b93
rajusah15 <Raju.Sah@ibm.com>
2022-02-10 09:03:11 -0500

    added buildscript for netlib

---
+++ n/netlib/netlib_ubi_8.3.sh
@@ -0,0 +1,62 @@
+
+#!/bin/bash -e
+
+# -----------------------------------------------------------------------------
+#
+# Package       : netlib
+# Version       : master
+# Source repo   : https://github.com/microsoft/mimalloc.git
+# Tested on     : UBI 8.3
+# Language      : GO
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Raju.Sah@ibm.com
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+# 
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=github.com/remyoudompheng/bigfft@v0.0.0-*
+PACKAGE_VERSION=${1:-master}
+PACKAGE_URL=gonum.org/v1/netlib/...
+
+yum install -y git golang make
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+if ! go get -d $PACKAGE_URL; then
+        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+        exit 1
+fi
+
+git clone https://github.com/xianyi/OpenBLAS
+cd OpenBLAS
+make
+make install PREFIX=/usr/local/lib/
+
+cd ~/go/pkg/mod/$PACKAGE_NAME
+if ! go mod tidy ; then
+        echo "------------------$PACKAGE_NAME:initialize_fails-------------------------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Initialize_Fails"
+        exit 1
+fi
+
+
+if ! go test; then
+        echo "------------------$PACKAGE_NAME:test_fails---------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
+        exit 1
+else
+        echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass |  Install_and_Test_Success"
+        exit 0
+fi
