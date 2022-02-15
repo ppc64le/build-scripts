18485efd5c5e2efd02c593c6d84651a8a71c8c80
Vedang <vedang.wartikar@ibm.com>
2022-02-15 14:05:13 +0530

    Add build script for symfony component - VarDumper: v4.4.7

---
+++ s/symfony_vardumper/symfony_vardumper_ubi8.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package	: symfony_vardumper
+# Version	: v4.4.7
+# Source repo	: https://github.com/symfony/symfony
+# Tested on	: UBI 8.5
+# Language	: PHP
+# Travis-Check	: True
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Vedang Wartikar <Vedang.Wartikar@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=symfony_vardumper
+PACKAGE_VERSION=${1:-v4.4.7}
+PACKAGE_URL=https://github.com/symfony/symfony
+
+yum module enable php:7.3 -y
+
+yum install git php php-devel php-zip zip php-pdo php-xml php-mbstring php-json -y
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
+
+git clone $PACKAGE_URL $PACKAGE_NAME
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+sed -i '28d' composer.json && sed -i '28i \ \ \ \ \ \ \ \ \"symfony/contracts": "self.version",' composer.json
+
+composer update
+
+cd src/Symfony/Component/
+
+cd VarDumper/
+
+composer install
+
+../../../.././phpunit --exclude-group tty,benchmark,intl-data
