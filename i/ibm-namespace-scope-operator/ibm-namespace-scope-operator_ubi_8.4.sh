f9b459f22d2992c2ee5d868744279f5101dadef6
ApurvaAgrawal3 <Apurva.Agrawal3@ibm.com>
2022-02-15 10:33:42 +0000

    Build script for ibm-namespace-scope-operator

--- i/ibm-namespace-scope-operator/ibm-namespace-scope-operator_ubi_8.4.sh
+++ i/ibm-namespace-scope-operator/ibm-namespace-scope-operator_ubi_8.4.sh
@@ -26,6 +26,27 @@ PACKAGE_URL=https://github.com/IBM/ibm-namespace-scope-operator
 # Install dependencies
 yum install -y make git wget gcc
 
+# Install unit test dependencies and run unit tests
+mkdir $CWD/kubebuilder-tools
+cd $CWD/kubebuilder-tools
+
+wget https://github.com/kubernetes-sigs/kubebuilder/releases/download/v2.3.1/kubebuilder_2.3.1_linux_ppc64le.tar.gz
+tar -xzf kubebuilder_2.3.1_linux_ppc64le.tar.gz
+cp kubebuilder_2.3.1_linux_ppc64le/bin/kubebuilder .
+rm -rf kubebuilder_2.3.1_linux_ppc64le.tar.gz kubebuilder_2.3.1_linux_ppc64le
+
+wget https://dl.k8s.io/v1.19.0/kubernetes-server-linux-ppc64le.tar.gz
+tar -xzf kubernetes-server-linux-ppc64le.tar.gz
+cp kubernetes/server/bin/kube-apiserver .
+rm -rf kubernetes-server-linux-ppc64le.tar.gz kubernetes
+
+wget https://github.com/etcd-io/etcd/releases/download/v3.4.14/etcd-v3.4.14-linux-ppc64le.tar.gz
+tar -xzf etcd-v3.4.14-linux-ppc64le.tar.gz
+cp etcd-v3.4.14-linux-ppc64le/etcd .
+rm -rf etcd-v3.4.14-linux-ppc64le.tar.gz etcd-v3.4.14-linux-ppc64le
+
+export KUBEBUILDER_ASSETS=$CWD/kubebuilder-tools
+
 # Download and install go
 wget https://golang.org/dl/go1.17.5.linux-ppc64le.tar.gz
 tar -xzf go1.17.5.linux-ppc64le.tar.gz
@@ -46,25 +67,6 @@ if ! go build -v ./...; then
         echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
         exit 1
 fi
-
-# Install unit test dependencies and run unit tests
-
-wget https://github.com/kubernetes-sigs/kubebuilder/releases/download/v2.3.1/kubebuilder_2.3.1_linux_ppc64le.tar.gz
-tar -xzf kubebuilder_2.3.1_linux_ppc64le.tar.gz
-cp kubebuilder_2.3.1_linux_ppc64le/bin/kubebuilder .
-rm -rf kubebuilder_2.3.1_linux_ppc64le.tar.gz kubebuilder_2.3.1_linux_ppc64le
-
-wget https://dl.k8s.io/v1.19.0/kubernetes-server-linux-ppc64le.tar.gz
-tar -xzf kubernetes-server-linux-ppc64le.tar.gz
-cp kubernetes/server/bin/kube-apiserver .
-rm -rf kubernetes-server-linux-ppc64le.tar.gz kubernetes
-
-wget https://github.com/etcd-io/etcd/releases/download/v3.4.14/etcd-v3.4.14-linux-ppc64le.tar.gz
-tar -xzf etcd-v3.4.14-linux-ppc64le.tar.gz
-cp etcd-v3.4.14-linux-ppc64le/etcd .
-rm -rf etcd-v3.4.14-linux-ppc64le.tar.gz etcd-v3.4.14-linux-ppc64le
-
-export KUBEBUILDER_ASSETS=$CWD/kubebuilder-tools
 
 echo "Testing $PACKAGE_PATH$PACKAGE_NAME with $PACKAGE_VERSION"
 go mod init
