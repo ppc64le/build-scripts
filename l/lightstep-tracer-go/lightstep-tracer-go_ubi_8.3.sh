db1004db801b7bbd94bc2fc1bc4cc48db4b861a0
rajusah15 <Raju.Sah@ibm.com>
2022-02-16 05:53:57 -0500

    changed script

--- l/lightstep-tracer-go/lightstep-tracer-go_ubi_8.3.sh
+++ l/lightstep-tracer-go/lightstep-tracer-go_ubi_8.3.sh
@@ -22,36 +22,13 @@
 
 PACKAGE_NAME=github.com/lightstep/lightstep-tracer-go
 PACKAGE_VERSION=${1:-v0.25.0}
-PACKAGE_URL=github.com/lightstep/lightstep-tracer-go
+PACKAGE_URL=https://github.com/lightstep/lightstep-tracer-go
 
 yum install -y git golang make
 
-OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
-
-if ! go get -d -t $PACKAGE_URL@$PACKAGE_VERSION; then
-        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-        echo "$PACKAGE_VERSION $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
-        exit 1
-fi
+go get -d -t $PACKAGE_NAME@$PACKAGE_VERSION
 
 cd ~/go/pkg/mod/$PACKAGE_NAME@$PACKAGE_VERSION
-if ! go mod tidy ; then
-        echo "------------------$PACKAGE_NAME:initialize_fails-------------------------------------"
-        echo "$PACKAGE_VERSION $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Initialize_Fails"
-        exit 1
-fi
-
-
-if ! go test; then
-        echo "------------------$PACKAGE_NAME:test_fails---------------------"
-        echo "$PACKAGE_VERSION $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
-        exit 1
-else
-        echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
-        echo "$PACKAGE_VERSION $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass |  Install_and_Test_Success"
-        exit 0
-fi
+go mod tidy
+go install
+go test
