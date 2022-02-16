1fc01318dedef472dd487cdb076af4dd8adafa14
rajusah15 <Raju.Sah@ibm.com>
2022-02-16 02:18:32 -0500

    modified script as travis job failing

--- n/netlib/netlib_ubi_8.3.sh
+++ n/netlib/netlib_ubi_8.3.sh
@@ -1,46 +1,40 @@
-
 #!/bin/bash -e
 
 # -----------------------------------------------------------------------------
 #
 # Package       : netlib
 # Version       : master
-# Source repo   : https://github.com/microsoft/mimalloc.git
+# Source repo   : https://github.com/gonum/netlib.git
 # Tested on     : UBI 8.3
 # Language      : GO
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Raju.Sah@ibm.com
+# Maintainer    : Raju Sah <Raju.Sah@ibm.com>
 #
-# Disclaimer: This script has been tested in root mode on given
+# Disclaimer: This script has been tested in root mode on given 129d659deeba
 # ==========  platform using the mentioned version of the package.
 #             It may not work as expected with newer versions of the
 #             package and/or distribution. In such case, please
 #             contact "Maintainer" of this script.
-# 
+#
 # ----------------------------------------------------------------------------
 
-PACKAGE_NAME=github.com/remyoudompheng/bigfft@v0.0.0-*
-PACKAGE_VERSION=${1:-master}
+PACKAGE_NAME=github.com/remyoudompheng/bigfft
+PACKAGE_VERSION=${1:-v0.0.0-20190728182440-6a916e37a237}
 PACKAGE_URL=gonum.org/v1/netlib/...
-
 yum install -y git golang make
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
-if ! go get -d $PACKAGE_URL; then
+if ! go get -d -t $PACKAGE_URL; then
         echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
         echo "$PACKAGE_VERSION $PACKAGE_NAME"
         echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
         exit 1
 fi
 
-git clone https://github.com/xianyi/OpenBLAS
-cd OpenBLAS
-make
-make install PREFIX=/usr/local/lib/
 
-cd ~/go/pkg/mod/$PACKAGE_NAME
+cd ~/go/pkg/mod/$PACKAGE_NAME@$PACKAGE_VERSION
 if ! go mod tidy ; then
         echo "------------------$PACKAGE_NAME:initialize_fails-------------------------------------"
         echo "$PACKAGE_VERSION $PACKAGE_NAME"
