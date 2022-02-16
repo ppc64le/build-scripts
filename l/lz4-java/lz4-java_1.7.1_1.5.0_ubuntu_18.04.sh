88a5322fa5bde92ba340a135384527570f0415fb
Manik-ibm <87144616+Manik-ibm@users.noreply.github.com>
2022-02-16 17:17:48 +0530

    Update lz4-java_1.7.1_1.5.0_ubuntu_18.04.sh

--- l/lz4-java/lz4-java_1.7.1_1.5.0_ubuntu_18.04.sh
+++ l/lz4-java/lz4-java_1.7.1_1.5.0_ubuntu_18.04.sh
@@ -41,22 +41,45 @@ wget https://dlcdn.apache.org//ant/binaries/apache-ant-1.10.12-bin.zip
 unzip apache-ant-1.10.12-bin.zip
 export ANT_HOME=/opt/apache-ant-1.10.12
 export PATH=/opt/apache-ant-1.10.12/bin:$PATH
+cd ..
 
-export WORK_DIR=`pwd`
-export HOME=$WORK_DIR
+export HOME=/home/tester
+
+mkdir -p /home/tester/output
+cd /home/tester
+
+ln -s /usr/bin/python3 /bin/python
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
-# clone repo
-if ! git clone $PACKAGE_URL; then
-	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails" > $WORK_DIR/output/version_tracker
-	exit 0
+function get_checkout_url(){
+        url=$1
+        CHECKOUT_URL=`python3 -c "url='$url';github_url=url.split('tree')[0];print(github_url);"`
+        echo $CHECKOUT_URL
+}
+
+function get_working_path(){
+        url=$1
+        CHECKOUT_URL=`python3 -c "url='$url';github_url,uri=url.split('tree');uris=uri.split('/');print('/'.join(uris[2:]));"`
+        echo $CHECKOUT_URL
+}
+
+CLONE_URL=$(get_checkout_url $PACKAGE_URL)
+
+if [ "$PACKAGE_URL" = "$CLONE_URL" ]; then
+        WORKING_PATH="./"
+else
+        WORKING_PATH=$(get_working_path $PACKAGE_URL)
 fi
 
-export HOME_DIR=$WORK_DIR/$PACKAGE_NAME
+if ! git clone $CLONE_URL $PACKAGE_NAME; then
+	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/clone_fails
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails" > /home/tester/output/version_tracker
+	exit 1
+fi
 
-cd $HOME_DIR
+cd /home/tester/$PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 git submodule init
@@ -65,13 +88,13 @@ git submodule update
 # to build
 function try_ant_with_jdk8(){
 	echo "Building package with jdk 1.8"
-    cd $WORK_DIR/$PACKAGE_NAME
-	
+    cd /home/tester/$PACKAGE_NAME
+
     if ! ant ivy-bootstrap; then
 	exit 0
-        fi
+    fi
 
-	cd $WORK_DIR/$PACKAGE_NAME
+	cd /home/tester/$PACKAGE_NAME
 
 	if ! ant test; then
 		echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
@@ -80,9 +103,8 @@ function try_ant_with_jdk8(){
 		exit 1
 	else
 		echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-		echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/test_success 
+		echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/test_success
 		echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success" > /home/tester/output/version_tracker
-		get_list_of_jars_generated $HOME_DIR /home/tester/output/post_build_jars.txt
 		exit 0
 	fi
 }
