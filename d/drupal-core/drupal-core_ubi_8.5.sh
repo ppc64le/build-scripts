df6a93049cb4313145d8c37b2eef6e8b35ae8c5b
bhagat-singh001 <98086129+bhagat-singh001@users.noreply.github.com>
2022-02-22 13:01:27 +0530

    Added build script for drupal-core.

---
+++ d/drupal-core/drupal-core_ubi_8.5.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -e
+
+# ----------------------------------------------------------------------------
+# Package          : drupal-core
+# Version          : 8.8.3
+# Source repo      : https://github.com/drupal/core
+# Tested on        : UBI 8.5
+# Language         : PHP
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Bhagat Singh <Bhagat.singh1@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#   
+# ----------------------------------------------------------------------------
+
+# Variables
+PACKAGE_NAME=core
+PACKAGE_URL=https://github.com/drupal/core
+#PACKAGE_VERSION is configurable can be passed as an argument.
+PACKAGE_VERSION=${1:-8.8.3}
+
+yum install -y git php php-json php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
+
+OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
+
+#Check if package exists
+if [ -d "$PACKAGE_NAME" ] ; then
+  rm -rf $PACKAGE_NAME
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"  
+ 
+fi
+
+HOME_DIR=`pwd`
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+		echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    	exit 0
+fi
+cd $HOME_DIR/$PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+#Install phpunit inside current dir
+composer require --dev phpunit/phpunit --with-all-dependencies ^8
+
+if ! composer install; then
+     	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+	exit 0
+fi
+
+if ! php vendor/bin/phpunit; then
+	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+	exit 0
+else
+	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+	exit 0
+fi
+
+#Test is in parity with intel.
+#No such file or directory in /root/core/tests/bootstrap.php on line 137
+#PHP Fatal error:  require(): Failed opening required '/root/core/tests/../../autoload.php' (include_path='/root/core/vendor/pear/archive_tar:/root/core/vendor/pear/console_getopt:/root/core/vendor/pear/pear-core-minimal/src:/root/core/vendor/pear/pear_exception:.:/usr/share/pear:/usr/share/php') in /root/core/tests/bootstrap.php on line 137
