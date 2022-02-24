c2ea106b55ea477b81f0079e1f3f3c4a38ab9f8d
rajusah15 <Raju.Sah@ibm.com>
2022-02-24 00:51:14 -0500

    build-script for fsnotify:v1.5.1

---
+++ f/fsnotify/fsnotify_ubi_8.3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : fsnotify
+# Version       : v1.5.1
+# Source repo   : https://github.com/fsnotify/fsnotify.git
+# Tested on     : UBI 8.3
+# Language      : GO
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Raju.Sah@ibm.com
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#   
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=github.com/fsnotify/fsnotify
+PACKAGE_VERSION=${1:-v1.5.1}
+PACKAGE_URL=https://github.com/fsnotify/fsnotify.git
+
+yum install -y git golang
+
+go get $PACKAGE_NAME@$PACKAGE_VERSION
+
+cd ~/go/pkg/mod/$PACKAGE_NAME@$PACKAGE_VERSION
+go mod tidy
+go install
+go test
