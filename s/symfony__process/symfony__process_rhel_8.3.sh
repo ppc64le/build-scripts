eef5ee5d0b5c565c1be465c6c3f403d3f3be3f22
ApurvaAgrawal3 <Apurva.Agrawal3@ibm.com>
2022-03-01 12:59:12 +0000

    Changes in build script of symfony__process

--- s/symfony__process/symfony__process_rhel_8.3.sh
+++ s/symfony__process/symfony__process_rhel_8.3.sh
@@ -1,11 +1,14 @@
+#!/bin/bash -e
 # -----------------------------------------------------------------------------
 #
 # Package	: symfony/process
-# Version	: 3.4.36
+# Version	: 3.4.36, v4.4.0
 # Source repo	: https://github.com/symfony/process
-# Tested on	: RHEL 8.3
+# Tested on	: RHEL 8.3, UBI 8.4
+# Language      : PHP
+# Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: BulkPackageSearch Automation <sethp@us.ibm.com>
+# Maintainer	: BulkPackageSearch Automation <sethp@us.ibm.com>, Apurva Agrawal <Apurva.Agrawal3@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -16,14 +19,10 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=symfony/process
-PACKAGE_VERSION=3.4.36
+PACKAGE_VERSION=${1:-v4.4.0}
 PACKAGE_URL=https://github.com/symfony/process
 
 yum -y update && yum install -y nodejs nodejs-devel nodejs-packaging npm python38 python38-devel ncurses git jq curl php php-curl php-dom php-mbstring php-json nodejs make gcc-c++ patch diffutils php-gd php-pecl-zip
-
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
-composer require --dev phpunit/phpunit --with-all-dependencies ^7
-mkdir output
 
 OS_NAME=`python3 -c "os_file_data=open('/etc/os-release').readlines();os_info = [i.replace('PRETTY_NAME=','').strip() for i in os_file_data if i.startswith('PRETTY_NAME')];print(os_info[0])"`
 HOME_DIR=`pwd`
@@ -31,24 +30,28 @@ if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
 		echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    	exit 0
+    	exit 1
 fi
 
 cd $HOME_DIR/$PACKAGE_NAME
 git checkout $PACKAGE_VERSION
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
+composer require --dev phpunit/phpunit --with-all-dependencies ^7
+mkdir output
+
 if ! composer install; then
      	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
-	exit 0
+	exit 1
 fi
 
-cd $HOME_DIR/$PACKAGE_NAME
-if ! /home/tester/vendor/bin/phpunit; then
+if ! vendor/bin/phpunit; then
 	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
-	exit 0
+	exit 1
 else
 	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
