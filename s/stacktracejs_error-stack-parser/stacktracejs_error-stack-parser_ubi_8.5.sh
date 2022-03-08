d5e2f0e8e5d9a0d3719952836a60bc3f733e57db
vathsalashetty25 <vathsalashetty69@gmail.com>
2022-03-08 16:23:18 +0530

    Added buildscript for error-stack-parser

---
+++ s/stacktracejs_error-stack-parser/stacktracejs_error-stack-parser_ubi_8.5.sh
@@ -0,0 +1,70 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : error-stack-parser
+# Version       : v2.0.5,v2.0.7
+# Source repo   : https://github.com/stacktracejs/error-stack-parser
+# Tested on	: UBI 8.5
+# Language      : Node
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Vathsala . <vaths367@in.ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+PACKAGE_URL="https://github.com/stacktracejs/error-stack-parser.git"
+PACKAGE_NAME="error-stack-parser"
+PACKAGE_VERSION=${1:-"v2.0.7"}
+
+yum install bzip2 git wget -y fontconfig freetype freetype-devel fontconfig-devel libstdc++
+
+#install phantomjs
+wget https://github.com/ibmsoe/phantomjs/releases/download/2.1.1/phantomjs-2.1.1-linux-ppc64.tar.bz2
+tar -xvf phantomjs-2.1.1-linux-ppc64.tar.bz2
+ln -s /phantomjs-2.1.1-linux-ppc64/bin/phantomjs /bin/phantomjs
+export PATH=$PATH:/phantomjs-2.1.1-linux-ppc64/bin/phantomjs
+
+phantomjs --version
+
+# build nodejs
+NODE_VERSION=v12.22.1
+#installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install $NODE_VERSION
+
+npm install --global yarn
+
+
+#clone the repo
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+#build 
+if ! yarn install; then
+        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+#test 
+if ! yarn test; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+        exit 1
+else
+        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+        exit 0
+fi
+
