3b305a00a1803dd98e4c4af7aeb2e75066e9968e
saraswati-patra <99007011+saraswati-patra@users.noreply.github.com>
2022-03-15 12:48:37 +0530

    Update logstash_ubi_8.5.sh.sh

--- l/logstash/logstash_ubi_8.5.sh.sh
+++ l/logstash/logstash_ubi_8.5.sh.sh
@@ -17,19 +17,14 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-
-
 PACKAGE_NAME=logstash-filter-geoip
 #PACKAGE_VERSION is configurable can be passed as an argument.
 PACKAGE_VERSION=${1:-v6.0.0}
 PACKAGE_URL=https://github.com/logstash-plugins/logstash-filter-geoip.git
-
 if [ -d "$PACKAGE_NAME" ] ; then
   rm -rf $PACKAGE_NAME
   echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"  
- 
-fi
-
+ fi
 yum -y update && yum install -y nodejs nodejs-devel nodejs-packaging npm python38 python38-devel ncurses git jq curl make gcc-c++ procps gnupg2
 yum install ruby -y && yum install -y libcurl-devel libffi-devel ruby-devel ruby-devel.ppc64le redhat-rpm-config sqlite sqlite-devel java-1.8.0-openjdk-devel
 gem install bundle && gem install bundler:1.17.3 && gem install rake
@@ -39,25 +34,19 @@ curl -L https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm install jruby
 export PATH=$PATH:/usr/local/rvm/rubies/jruby-9.2.9.0/bin/
-
 source /etc/profile.d/rvm.sh;
 OS_NAME=`python3 -c "os_file_data=open('/etc/os-release').readlines();os_info = [i.replace('PRETTY_NAME=','').strip() for i in os_file_data if i.startswith('PRETTY_NAME')];print(os_info[0])"`
-
 #install previous version of bundler
 gem install bundler:1.17.3
 HOME_DIR=`pwd`
-
-
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
 		echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
     	exit 0
 fi
-
 cd $HOME_DIR/$PACKAGE_NAME
 git checkout $PACKAGE_VERSION
-
 if ! bundle _1.17.3_  install; then
 	if ! bundle install; then
      		echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
@@ -66,7 +55,6 @@ if ! bundle _1.17.3_  install; then
 		exit 0
 	fi
 fi
-
 cd $HOME_DIR/$PACKAGE_NAME
 if ! bundle _1.17.3_ exec rake; then
 	if ! bundle exec rake; then
