60f1fabbcc55a1d08df32c9f37052ab34ce78af6
saraswati-patra <99007011+saraswati-patra@users.noreply.github.com>
2022-03-15 12:36:52 +0530

    Update logstash_ubi_8.5.sh.sh

--- l/logstash/logstash_ubi_8.5.sh.sh
+++ l/logstash/logstash_ubi_8.5.sh.sh
@@ -81,5 +81,4 @@ else
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
 	exit 0
 fi
-
 #logstash-filter-geoip:install_&_test_both_success
