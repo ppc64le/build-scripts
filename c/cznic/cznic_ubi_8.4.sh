3af88b1effffc7d452ced9aaa2a4837083e28317
Kandarpa Malipeddi <69784217+kandarpamalipeddi@users.noreply.github.com>
2022-03-30 13:05:58 +0530

    Update cznic_ubi_8.4.sh

--- c/cznic/cznic_ubi_8.4.sh
+++ c/cznic/cznic_ubi_8.4.sh
@@ -2,13 +2,13 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : cznic
-# Version       : 1.0.0
+# Version       : v0.0.0-20180608152220-f44710a21d00, 1.0.0
 # Source repo   : https://github.com/cznic/internal.git
 # Tested on     : UBI 8.4
 # Language      : go
 # Travis-Check  : true
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Sachin K {sachin.kakatkar@ibm.com}
+# Maintainer    : Sachin K {sachin.kakatkar@ibm.com}, Reynold Vaz <Reynold.Vaz@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -17,63 +17,40 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-#Run the script:./cznic_ubi_8.4.sh 1.0.0(version_to_test)
-PACKAGE_NAME=cznic
-PACKAGE_VERSION=${1:-1.0.0}
-GO_VERSION=1.16.1
-PACKAGE_URL=https://github.com/cznic/internal.git
+#Run the script:./cznic_ubi_8.4.sh 1.0.0
+PACKAGE_NAME=internal
+PACKAGE_VERSION=${1:-f44710a21d00}
+PACKAGE_URL=https://github.com/cznic/internal
 
-dnf install git wget sudo -y
+yum install go git -y
 
-mkdir -p /home/tester/output
-cd /home/tester
-
-wget https://golang.org/dl/go$GO_VERSION.linux-ppc64le.tar.gz
-rm -rf /home/tester/go && tar -C /home/tester -xzf go$GO_VERSION.linux-ppc64le.tar.gz
-rm -f go$GO_VERSION.linux-ppc64le.tar.gz
+export PATH=$PATH:/bin/go/bin
 export GOPATH=/home/tester/go
-export PATH=$PATH:$GOPATH/bin
+export PATH=$GOPATH/bin:$PATH
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
 
-
-rm -rf $PACKAGE_NAME
-
-OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
-
-
-if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
-        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/clone_fails
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails" > /home/tester/output/version_tracker
-        exit 1
-fi
-
-cd /home/tester/$PACKAGE_NAME
+mkdir -p $GOPATH/src && cd $GOPATH/src
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-INSTALL_SUCCESS="false"
-
-go mod init slice
+go mod init
 go mod tidy
-cd slice
 
-if ! go build; then
-        INSTALL_SUCCESS="false"
-        else
-        INSTALL_SUCCESS="true"
-fi
+sed -i 's|github.com/cznic/internal/slice|github.com/cznic/common/slice|g' buffer/buffer.go
+sed -i 's|github.com/cznic/internal/buffer|github.com/cznic/common/buffer|g' file/all_test.go
+sed -i 's|github.com/cznic/internal/buffer|github.com/cznic/common/buffer|g' file/file.go
 
-# Ensure go.mod file exists
-if ! go test -v; then
-        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/test_fails
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails" > /home/tester/output/version_tracker
-        exit 1
+go get github.com/cznic/common/slice
+go get github.com/cznic/common/buffer
+
+if ! go test -v ./...; then
+	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+	echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails" 
+	exit 1
 else
-        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-        echo "$PACKAGE_VERSION $PACKAGE_NAME" > /home/tester/output/test_success
-        echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success" > /home/tester/output/version_tracker
-        exit 0
+	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+	echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+	exit 0
 fi
-
-#Build failed for file and buffer. use of internal package not allowed.
 
