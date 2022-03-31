74d3f2eb354781aef7ba7126028db46cf878e9b0
Pranav Pandit <pranav.pandit1@ibm.com>
2022-03-31 18:18:03 +0530

    review comments updated julia v1.7.2 build script

--- j/julia/julia_v1.7.2_ubi8.5.sh
+++ j/julia/julia_v1.7.2_ubi8.5.sh
@@ -2,9 +2,9 @@
 # ----------------------------------------------------------------------------
 #
 # Package       : julia
-# Version       : v1.7.2 (master branch, commit:bf534986350a991e4a1b29126de0342ffd76205e)
-# Source repo   : https://github.com/JuliaLang/julia/releases/tag/v1.7.2
-# Language	    : Julia
+# Version       : v1.7.2
+# Source repo   : https://github.com/JuliaLang/julia
+# Language      : Julia
 # Tested on     : UBI 8.5
 # Script License: Apache License Version 2.0
 # Travis-Check  : True
@@ -20,15 +20,15 @@
 
 #!/bin/bash
 
-#currently julia works on power with the following commit(v1.7.2)
-JULIA_VERSION=bf534986350a991e4a1b29126de0342ffd76205e
+SRCDIR=`pwd`
+PACKAGE_NAME=julia
+PACKAGE_VERSION=${1:-v1.7.2}
+PACKAGE_URL=https://github.com/JuliaLang/julia
+BLDDIR=juliabuild
 
 #Install required dependencies
 yum update -y
 yum install wget expat-devel openssl-devel libcurl-devel tk make gcc gcc-c++ patch bzip2 m4 python38 git -y
-
-SRCDIR=`pwd`
-DIRNAME="julia"
 
 #Build gettext
 wget  https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.gz
@@ -38,24 +38,12 @@ cd gettext-0.21
 make
 make install
 
-#Build cmake from source
-cd $SRCDIR
-wget https://cmake.org/files/v3.11/cmake-3.11.4.tar.gz
-tar -zxvf cmake-3.11.4.tar.gz
-
-cd cmake-3.11.4
-./bootstrap --system-curl
-make
-make install
-export PATH=/usr/local/bin:$PATH
-
 #download and build julia
 cd $SRCDIR
-mkdir -p juliabuild
-if [ ! -d "$DIRNAME" ]; then
-        git clone https://github.com/JuliaLang/julia julia
+mkdir -p $BLDDIR
+if [ ! -d "$PACKAGE_NAME" ]; then
+        git clone $PACKAGE_URL
 fi
-cd julia
-#currently we will checkout only version 1.7.2
-git checkout $JULIA_VERSION
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
 make O=../juliabuild
