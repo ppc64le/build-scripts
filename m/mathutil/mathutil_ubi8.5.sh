f2d5e90ebff434604c8789f9c6920db041d23678
amitmukati-2604 <amit.mukati3@ibm.com>
2022-04-11 10:30:11 +0530

    added the test cases status

--- m/mathutil/mathutil_ubi8.5.sh
+++ m/mathutil/mathutil_ubi8.5.sh
@@ -69,3 +69,74 @@ else
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Build_and_Test_Success"
     exit 0
 fi
+
+# test cases status
+<<comment
+--- PASS: TestQuadPolyFactors (2.29s)
+=== RUN   TestFCPRNG
+    all_test.go:5550: [43690 43691 43691]
+--- PASS: TestFCPRNG (0.05s)
+=== RUN   TestNewFloat
+--- PASS: TestNewFloat (0.00s)
+=== RUN   TestFloatDiv2
+--- PASS: TestFloatDiv2 (0.00s)
+=== RUN   TestFloatSqr
+--- PASS: TestFloatSqr (0.00s)
+=== RUN   TestFloatEq1
+--- PASS: TestFloatEq1 (0.00s)
+=== RUN   TestFloatGe2
+--- PASS: TestFloatGe2 (0.00s)
+=== RUN   TestFloatMaxFracBits
+--- PASS: TestFloatMaxFracBits (0.00s)
+=== RUN   TestBinaryLog
+--- PASS: TestBinaryLog (0.00s)
+=== RUN   TestMaxInt128
+--- PASS: TestMaxInt128 (0.00s)
+=== RUN   TestMinInt128
+--- PASS: TestMinInt128 (0.00s)
+=== RUN   TestInt128Add
+--- PASS: TestInt128Add (0.00s)
+=== RUN   TestInt128Add2
+--- PASS: TestInt128Add2 (1.20s)
+=== RUN   TestInt128BigInt
+--- PASS: TestInt128BigInt (0.00s)
+=== RUN   TestInt128BigInt2
+--- PASS: TestInt128BigInt2 (1.25s)
+=== RUN   TestInt128Cmp
+--- PASS: TestInt128Cmp (0.00s)
+=== RUN   TestInt128Cmp2
+--- PASS: TestInt128Cmp2 (1.70s)
+=== RUN   TestInt128Neg
+--- PASS: TestInt128Neg (0.00s)
+=== RUN   TestInt128Neg2
+--- PASS: TestInt128Neg2 (1.29s)
+=== RUN   TestInt128SetInt64
+--- PASS: TestInt128SetInt64 (1.13s)
+=== RUN   TestInt128SetUint64
+--- PASS: TestInt128SetUint64 (1.04s)
+=== RUN   ExampleBinaryLog
+--- PASS: ExampleBinaryLog (0.00s)
+PASS
+ok      mathutil        (cached)
+=== RUN   TestNew
+--- PASS: TestNew (0.26s)
+=== RUN   TestHasFactorUint32
+--- PASS: TestHasFactorUint32 (0.00s)
+=== RUN   TestHasFactorUint64
+--- PASS: TestHasFactorUint64 (0.00s)
+=== RUN   TestHasFactorBigInt
+--- PASS: TestHasFactorBigInt (0.00s)
+=== RUN   TestFromFactorBigInt
+--- PASS: TestFromFactorBigInt (0.02s)
+=== RUN   TestMod
+--- PASS: TestMod (0.39s)
+=== RUN   TestModPow
+--- PASS: TestModPow (0.53s)
+=== RUN   TestModPow2
+--- PASS: TestModPow2 (0.00s)
+PASS
+ok      mathutil/mersenne       (cached)
+------------------mathutil:build_&_test_both_success-------------------------
+https://gitlab.com/cznic/mathutil mathutil
+mathutil  |  https://gitlab.com/cznic/mathutil | v1.0.0 | "Red Hat Enterprise Linux 8.5 (Ootpa)" | GitHub  | Pass |  Both_Build_and_Test_Success
+comment
