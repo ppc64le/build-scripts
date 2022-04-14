25f94fcc681ce5e1c1f3e7c7b5831638f40472bb
bhagat-singh001 <98086129+bhagat-singh001@users.noreply.github.com>
2022-04-14 10:19:24 +0530

    Update protobuf_ubi_8.5.sh

--- p/protobuf/protobuf_ubi_8.5.sh
+++ p/protobuf/protobuf_ubi_8.5.sh
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package	: github.com/gogo/protobuf
-# Version	: v1.3.2, c0656edd0d9eab7c66d1eb0c568f9039345796f7
+# Version	: v1.3.2,c0656edd0d9eab7c66d1eb0c568f9039345796f7
 # Source repo	: https://github.com/gogo/protobuf
 # Tested on	: UBI 8.5
 # Language      : Go
