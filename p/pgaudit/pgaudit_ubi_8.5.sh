5eefb1b3b0b4c8f0cf3475e3d65663c58f65dd4d
bhagat-singh001 <98086129+bhagat-singh001@users.noreply.github.com>
2022-04-19 14:31:55 +0530

    Update pgaudit_ubi_8.5.sh

--- p/pgaudit/pgaudit_ubi_8.5.sh
+++ p/pgaudit/pgaudit_ubi_8.5.sh
@@ -6,7 +6,7 @@
 # Source repo      : https://github.com/pgaudit/pgaudit
 # Tested on        : UBI 8.5
 # Language         : C
-# Travis-Check     : True
+# Travis-Check     : False
 # Script License   : Apache License, Version 2 or later
 # Maintainer       : Bhagat Singh <Bhagat.singh1@ibm.com>
 #
