8365154991b5dbe7ed34919baea0d195de0bc95e
bhagat-singh001 <98086129+bhagat-singh001@users.noreply.github.com>
2022-04-19 14:31:40 +0530

    Update pgaudit_ubi_8.5.sh

--- p/pgaudit/pgaudit_ubi_8.5.sh
+++ p/pgaudit/pgaudit_ubi_8.5.sh
@@ -2,7 +2,7 @@
 
 # ----------------------------------------------------------------------------
 # Package          : pgaudit
-# Version          : 1.4.0, 1.4.1
+# Version          : REL_11_STABLE,REL_12_STABLE
 # Source repo      : https://github.com/pgaudit/pgaudit
 # Tested on        : UBI 8.5
 # Language         : C
@@ -22,13 +22,14 @@
 PACKAGE_NAME=pgaudit
 PACKAGE_URL=https://github.com/pgaudit/pgaudit
 #PACKAGE_VERSION is configurable can be passed as an argument.
-PACKAGE_VERSION=${1:-1.4.0}
+PACKAGE_VERSION=${1:-REL_12_STABLE}
 
 #Dependencies 
  yum install -y git openssl-devel redhat-rpm-config wget automake cmake libtool autoconf-2.69 gcc-c++ make
  dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
  dnf install -y postgresql postgresql12-server postgresql12-devel
-
+ dnf install https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-ppc64le/postgresql12-contrib-12.10-1PGDG.rhel8.ppc64le.rpm
+ 
  OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
 #Check if package exists
@@ -49,7 +50,7 @@ if ! make install USE_PGXS=1 PG_CONFIG=/usr/pgsql-12/bin/pg_config; then
     exit 1
 fi
 
-if ! make test USE_PGXS=1 PG_CONFIG=/usr/pgsql-12/bin/pg_config; then
+if ! PGUSER=postgres make installcheck USE_PGXS=1 PG_CONFIG=/usr/pgsql-12/bin/pg_config; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
