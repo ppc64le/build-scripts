38544bce3535430de428b9d8b92bec96034eb62f
csrividya <Srividya.Chittiboina@ibm.com>
2022-05-02 16:14:58 +0530

    Updated kotlin-stdlib_v1.3.61 script for ubi8.3

--- k/kotlin-stdlib/v1.3.61/kotlin-stdlib_v1.3.61_ubi_8.3.sh
+++ k/kotlin-stdlib/v1.3.61/kotlin-stdlib_v1.3.61_ubi_8.3.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -e
 # ----------------------------------------------------------------------------
 #
 # Package       : Kotlin (stdlib & stdlib-common)
@@ -14,43 +15,47 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-#!/bin/bash
 
-set -e
+export VERSION=v1.3.61
 
-VERSION=${1:-v1.3.61}
+if [ "X$1" != "X" ]
+then
+  export VERSION=$1
+fi
 
-yum install -y wget git patch
+yum install -y wget git patch sudo 
 
 useradd -G root -d /home/testuser testuser -p test123
 
-su testuser
-cd 
+export JAVA_HOME=/home/testuser/jdk8u302-b08;
+export JDK_9=/home/testuser/jdk-9.0.4+11;
+export JDK_16=$JAVA_HOME;
+export JDK_17=$JAVA_HOME;
+export JDK_18=$JAVA_HOME;
 
-wget https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/OpenJDK8U-jdk_ppc64le_linux_hotspot_8u302b08.tar.gz
-tar xf OpenJDK8U-jdk_ppc64le_linux_hotspot_8u302b08.tar.gz
+sudo -E -u testuser bash -c 'bash << EOF
+cd $HOME;
 
-wget https://github.com/AdoptOpenJDK/openjdk9-binaries/releases/download/jdk-9.0.4%2B11/OpenJDK9U-jdk_ppc64le_linux_hotspot_9.0.4_11.tar.gz
-tar xf OpenJDK9U-jdk_ppc64le_linux_hotspot_9.0.4_11.tar.gz
+wget https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/OpenJDK8U-jdk_ppc64le_linux_hotspot_8u302b08.tar.gz;
+tar xf OpenJDK8U-jdk_ppc64le_linux_hotspot_8u302b08.tar.gz;
 
-rm -rf OpenJDK8U-jdk_ppc64le_linux_hotspot_8u302b08.tar.gz OpenJDK9U-jdk_ppc64le_linux_hotspot_9.0.4_11.tar.gz
+wget https://github.com/AdoptOpenJDK/openjdk9-binaries/releases/download/jdk-9.0.4%2B11/OpenJDK9U-jdk_ppc64le_linux_hotspot_9.0.4_11.tar.gz;
+tar xf OpenJDK9U-jdk_ppc64le_linux_hotspot_9.0.4_11.tar.gz;
+rm -rf OpenJDK8U-jdk_ppc64le_linux_hotspot_8u302b08.tar.gz OpenJDK9U-jdk_ppc64le_linux_hotspot_9.0.4_11.tar.gz;
 
-export JAVA_HOME=$HOME/jdk8u302-b08
-export JDK_9=$HOME/jdk-9.0.4+11
-export JDK_16=$JAVA_HOME
-export JDK_17=$JAVA_HOME
-export JDK_18=$JAVA_HOME
-export PATH=$JAVA_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:$PATH;
 
-wget https://raw.githubusercontent.com/ppc64le/build-scripts/master/k/kotlin-stdlib/v1.3.61/kotlin.patch
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/master/k/kotlin-stdlib/v1.3.61/kotlin.patch;
 
-git clone https://github.com/JetBrains/kotlin.git
-cd kotlin
-git checkout $VERSION
-patch -p1 < $HOME/kotlin.patch
+git clone https://github.com/JetBrains/kotlin.git;
+cd kotlin;
+git checkout $VERSION;
+patch -p1 < $HOME/kotlin.patch;
 
-./gradlew  :kotlin-stdlib-common:test 
-./gradlew :kotlin-stdlib:test
+./gradlew  :kotlin-stdlib-common:test;
+./gradlew :kotlin-stdlib:test;
 
-echo "JARS at  "
-find -name *kotlin-stdlib*SNAPSHOT.jar
+echo "JARS at  ";
+find -name *kotlin-stdlib*SNAPSHOT.jar;
+
+EOF'
