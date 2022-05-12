f01117e4c61e6530f2d56d89d9a9a831e3277d9d
Kandarpa Malipeddi <kandarpa.malipeddi@ibm.com>
2022-05-12 17:54:33 +0530

    Fixed issue with the patch

--- e/elasticsearch/elasticsearch_v8.1.0_ubi_8.5.sh
+++ e/elasticsearch/elasticsearch_v8.1.0_ubi_8.5.sh
@@ -22,7 +22,7 @@ WORKDIR=`pwd`
 ELASTICSEARCH_VERSION=${1:-v8.1.0}
 
 # install dependencies
-yum update -y && yum install -y maven wget git zip unzip sudo libtool-ltdl docker
+yum update -y && yum install -y wget git zip unzip sudo libtool-ltdl docker
 cd $WORKDIR
 
 
@@ -30,9 +30,10 @@ cd $WORKDIR
 wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jdk_ppc64le_linux_hotspot_17.0.2_8.tar.gz && \
 tar -C /usr/local -xzf OpenJDK17U-jdk_ppc64le_linux_hotspot_17.0.2_8.tar.gz
 export JAVA_HOME=/usr/local/jdk-17.0.2+8/
-export JAVA13_HOME=/usr/local/jdk-17.0.2+8/
+export JAVA17_HOME=/usr/local/jdk-17.0.2+8/
 export PATH=$PATH:/usr/local/jdk-17.0.2+8/bin
 sudo ln -sf /usr/local/jdk-17.0.2+8/bin/java /usr/bin/
+rm -f OpenJDK17U-jdk_ppc64le_linux_hotspot_17.0.2_8.tar.gz
 
 
 # build elasticsearch from source
@@ -41,7 +42,7 @@ git clone https://github.com/elastic/elasticsearch.git
 cd elasticsearch && git checkout $ELASTICSEARCH_VERSION
 
 # Apply patches
-wget https://raw.githubusercontent.com/ppc64le/build-scripts/master/e/elasticsearch/elasticsearch_v8.1.0.patch
+wget https://raw.githubusercontent.com/kandarpamalipeddi/build-scripts/master/e/elasticsearch/elasticsearch_v8.1.0.patch
 git apply elasticsearch_v8.1.0.patch
 mkdir -p distribution/archives/linux-ppc64le-tar
 echo "// This file is intentionally blank. All configuration of the distribution is done in the parent project." > distribution/archives/linux-ppc64le-tar/build.gradle
@@ -49,23 +50,3 @@ mkdir -p distribution/archives/oss-linux-ppc64le-tar
 echo "// This file is intentionally blank. All configuration of the distribution is done in the parent project." > distribution/archives/oss-linux-ppc64le-tar/build.gradle
 
 ./gradlew :distribution:archives:oss-linux-ppc64le-tar:assemble --parallel
-
-# build docker image
-./gradlew distribution:docker:assemble --parallel
-
-# copy tar file and delete source code
-tar -C /usr/share/ -xf distribution/archives/oss-linux-ppc64le-tar/build/distributions/elasticsearch-oss-$ELASTICSEARCH_VERSION*-SNAPSHOT-*.tar.gz
-mkdir /usr/share/elasticsearch/
-mv /usr/share/elasticsearch-$ELASTICSEARCH_VERSION*-SNAPSHOT/* /usr/share/elasticsearch/
-
-
-# add elasticsearch user 
-groupadd elasticsearch && useradd elasticsearch -g elasticsearch
-chown elasticsearch:elasticsearch -R /usr/share/elasticsearch
-
-# command to start elasticsearch
-#sudo -u elasticsearch /usr/share/elasticsearch/bin/elasticsearch &
-
-# NOTE: if you are facing issues with elasticsearch startup follow these steps after isntallation
-# wget https://repo1.maven.org/maven2/net/java/dev/jna/jna/4.5.1/jna-4.5.1.jar
-# mv jna-4.5.1.jar /usr/share/elasticsearch/lib/
