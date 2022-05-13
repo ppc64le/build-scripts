3fb3f25032a565a00c03d78300203b12de13b6d2
jotirlingSwami <89772969+jotirlingSwami@users.noreply.github.com>
2022-05-13 16:33:32 +0530

    Added ubuntu build script for Bootstrap-datetimepicker (#1762)

---
+++ b/bootstrap-datepicker/Bootstrap-datetimepicker_v1.9.0_ubuntu_18.04.sh
@@ -0,0 +1,92 @@
+#!/bin/bash -e
+#----------------------------------------------------------------------------
+#
+# Package       : Bootstrap-datetimepicker
+# Version       : v1.9.0
+# Source repo   : https://github.com/uxsolutions/bootstrap-datepicker
+# Tested on     : ubuntu_18.04 (Docker)
+# Language      : Java
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Jotirling Swami <Jotirling.Swami1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------------------------------
+
+set -ex
+
+#Variables
+PACKAGE_NAME=bootstrap-datepicker
+PACKAGE_VERSION=v1.9.0 
+PACKAGE_URL=https://github.com/uxsolutions/bootstrap-datepicker
+
+#Extract version from command line
+echo "Usage: $0 [-v <PACKAGE_VERSION>]"
+echo "PACKAGE_VERSION is an optional paramater whose default value is $PACKAGE_VERSION and building for ${1:-$PACKAGE_VERSION}"
+
+PACKAGE_VERSION="${1:-$PACKAGE_VERSION}"
+
+#Dependencies
+apt update -y && apt install -y git wget bzip2 fontconfig python make build-essential gcc
+
+# install node
+apt install -y curl
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 14.17.5
+node -v
+
+# install phantomjs
+wget https://github.com/ibmsoe/phantomjs/releases/download/2.1.1/phantomjs-2.1.1-linux-ppc64.tar.bz2
+tar -xvf phantomjs-2.1.1-linux-ppc64.tar.bz2
+cp phantomjs-2.1.1-linux-ppc64/bin/phantomjs /usr/local/bin/phantomjs
+phantomjs --version
+
+#Clone repo
+HOME_DIR=`pwd`
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    exit 1
+fi
+
+#Build and test
+cd $HOME_DIR/$PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if !  npm install --global grunt-cli; then
+	echo "------------------$PACKAGE_NAME:dependency_fails-------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Dependency_Fails"
+	exit 1
+fi
+
+cd $HOME_DIR/$PACKAGE_NAME
+if ! npm install ; then
+	echo "------------------$PACKAGE_NAME:build_fails---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+	exit 1
+fi
+
+if ! npm test; then
+	echo "------------------$PACKAGE_NAME:test_fails---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
+	exit 1
+else
+	echo "------------------$PACKAGE_NAME:build_and_test_success-------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Build_and_Test_Success"
+	exit 0
+fi
