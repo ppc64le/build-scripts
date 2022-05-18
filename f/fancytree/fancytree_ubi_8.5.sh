a1429af618c90b7970d1df9f07a19b4fc5ed4d74
Shreya Kajbaje <shreya.kajbaje@ibm.com>
2022-05-18 12:56:19 +0530

    Add build script for fancytree: v2.37.0

--- f/fancytree/fancytree_ubi_8.5.sh
+++ f/fancytree/fancytree_ubi_8.5.sh
@@ -8,7 +8,7 @@
 # Language      : Node
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Shreya Kajbaje <Vedang.Wartikar@ibm.com>
+# Maintainer    : Shreya Kajbaje <shreya.kajbaje@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -22,7 +22,7 @@ PACKAGE_NAME=fancytree
 PACKAGE_VERSION=${1:-v2.37.0}
 PACKAGE_URL=https://github.com/mar10/fancytree.git
 
-yum -y update && yum install -y yum-utils nodejs npm git make wget tar zip nss libXScrnSaver libX11-xcb libXcomposite libXcursor libXfixes libXrender libXdamage atk at-spi2-atk at-spi2-core cups-libs avahi-libs libXrandr libdrm mesa-libgbm libwayland-server alsa-lib pango gtk3
+sudo yum -y update && sudo yum install -y yum-utils nodejs npm git make wget tar zip nss libXScrnSaver libX11-xcb libXcomposite libXcursor libXfixes libXrender libXdamage atk at-spi2-atk at-spi2-core cups-libs avahi-libs libXrandr libdrm mesa-libgbm libwayland-server alsa-lib pango gtk3
 
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
@@ -38,6 +38,9 @@ if ! npm install  ; then
 else
 	echo "------------------$PACKAGE_NAME:build_success-------------------------"
 fi
+
+# The code for testing this package is commented since the chrome binaries required for testing may not be accessible.
+# Please refer to README.md to install the chrome binaries required for the testing.
 
 #if ! npm test ; then
 #	echo "------------------$PACKAGE_NAME:test_failure---------------------"
