3df6a614094825aa5e63eeffdb271495d5e4c874
ApurvaAgrawal3 <Apurva.Agrawal3@ibm.com>
2022-05-19 08:22:22 +0000

    Modified the build script of archiver

--- a/archiver/archiver_ubi_8.4.sh
+++ a/archiver/archiver_ubi_8.4.sh
@@ -1,13 +1,14 @@
+#!/bin/bash -e
 # ----------------------------------------------------------------------------
 #
 # Package       : archiver 
-# Version       : v3.5.0
+# Version       : v3.2.0, v3.5.0
 # Source repo   : https://github.com/mholt/archiver 
 # Tested on     : UBI 8.4
 # Language      : GO
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: Saurabh Ghumnar / Siddhesh Ghadi <Siddhesh.Ghadi@ibm.com>
+# Maintainer	: Saurabh Ghumnar / Siddhesh Ghadi <Siddhesh.Ghadi@ibm.com>, Apurva Agrawal <apurva.agrawal3@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -17,54 +18,42 @@
 #
 # ----------------------------------------------------------------------------
 
-#!/bin/bash
-set -e
+PACKAGE_PATH=github.com/mholt/
+PACKAGE_NAME=archiver
+PACKAGE_VERSION=${1:-v3.5.0}
+PACKAGE_URL=https://github.com/mholt/archiver 
 
-if [ -z "$1" ]; then
-  export VERSION=v3.5.0
+# Install dependencies
+yum install -y make git wget
+
+# Download and install go
+wget https://golang.org/dl/go1.17.5.linux-ppc64le.tar.gz
+tar -xzf go1.17.5.linux-ppc64le.tar.gz
+rm -rf go1.17.5.linux-ppc64le.tar.gz
+export GOPATH=`pwd`/gopath
+export PATH=`pwd`/go/bin:$GOPATH/bin:$PATH
+
+# Clone the repo and checkout submodules
+mkdir -p $GOPATH/src/$PACKAGE_PATH
+cd $GOPATH/src/$PACKAGE_PATH
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+echo "Building $PACKAGE_PATH$PACKAGE_NAME with $PACKAGE_VERSION"
+go mod init 
+go mod tidy
+if ! go build -v ./...; then
+	echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
+	exit 1
+fi
+
+echo "Testing $PACKAGE_PATH$PACKAGE_NAME with $PACKAGE_VERSION"
+
+if ! go test -v ./...; then
+	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+	exit 1
 else
-  export VERSION=$1
+	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    exit 0
 fi
-
-if [ -d "archiver" ] ; then
-  rm -rf archiver
-fi
-
-# Dependency installation
-dnf install -y go git
-
-mkdir -p /home/tester/go
-export GOPATH=/home/tester/go
-
-mkdir -p $GOPATH/src/github.com/mholt
-
-cd $GOPATH/src/github.com/mholt/
-# Download the repos
-git clone https://github.com/mholt/archiver
-
-cd archiver 
-git checkout $VERSION
-
-ret=$?
-
-if [ $ret -eq 0 ] ; then
- echo "$VERSION found to checkout "
-else
- echo "$VERSION not found "
- exit
-fi
-
-#Build
-go build cmd/arc/*.go
-ret=$?
-
-if [ $ret -eq 0 ] ; then
- echo "Build is successful"
-else
- echo "Build command failed"
- exit
-fi
-
-#Test
-echo "TESTING IN PROGRESS....."
-go test -v
