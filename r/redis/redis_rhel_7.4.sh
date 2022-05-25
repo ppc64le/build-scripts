83b7d48da9f20540857c0c26e6459dd31f55b6c6
Kandarpa Malipeddi <69784217+kandarpamalipeddi@users.noreply.github.com>
2022-05-25 16:07:10 +0530

    Made build-script version configurable.

--- r/redis/redis_rhel_7.4.sh
+++ r/redis/redis_rhel_7.4.sh
@@ -19,13 +19,15 @@
 # ----------------------------------------------------------------------------
 
 # Install dependencies.
+
+PACKAGE_VERSION=${1:-3.2.9}
 yum install -y sudo
 sudo yum update -y
 sudo yum install -y git tcl
 sudo yum group install -y 'Development Tools'
 
 # Clone and build source.
-git clone https://github.com/antirez/redis.git
+git clone https://github.com/antirez/redis.git --branch $PACKAGE_VERSION
 cd redis
 make V=1 MALLOC=libc
 sudo make install
