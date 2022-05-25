4ad61d848f3b7c55c330c5626c1cc0689a20a369
Kandarpa Malipeddi <69784217+kandarpamalipeddi@users.noreply.github.com>
2022-05-25 22:32:02 +0530

    Update redis_ubuntu_16.04.sh

--- r/redis/redis_ubuntu_16.04.sh
+++ r/redis/redis_ubuntu_16.04.sh
@@ -6,7 +6,7 @@
 # Source repo	: https://github.com/antirez/redis.git
 # Tested on	: ubuntu-16.04
 # Language      : C
-# Travis-Check  : True
+# Travis-Check  : False
 # Script License: Apache License, Version 2 or later
 # Maintainer	: Atharv Phadnis <Atharv.Phadnis@ibm.com>
 #
