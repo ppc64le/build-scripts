7fd6833be749daae12b709e42068ec30f8bdc5db
Shreya Kajbaje <shreya.kajbaje@ibm.com>
2022-05-26 13:21:58 +0530

    Add build script for Add build script for golang_protobuf_extensions: fc2b8d3a73c4867e51861bbdd5ae3c1f0869dd6a

--- g/golang-protobuf-extensions/golang_protobuf_extensions_ubi_8.5.sh
+++ g/golang-protobuf-extensions/golang_protobuf_extensions_ubi_8.5.sh
@@ -17,60 +17,52 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-CWD=`pwd`
 
-PACKAGE_PATH=github.com/matttproud/golang_protobuf_extensions
 PACKAGE_NAME=golang_protobuf_extensions
 PACKAGE_VERSION=${1:-fc2b8d3a73c4867e51861bbdd5ae3c1f0869dd6a}
 PACKAGE_URL=https://github.com/matttproud/golang_protobuf_extensions
 
-# Install dependencies
-yum install -y make git wget
+#Install the required dependencies
+yum -y update && yum install git gcc make wget tar zip -y
+GO_VERSION=1.17
 
+# Install Go and setup working directory
+wget https://golang.org/dl/go$GO_VERSION.linux-ppc64le.tar.gz
+tar -C /bin -xf go$GO_VERSION.linux-ppc64le.tar.gz
+export PATH=$PATH:/bin/go/bin
+rm -f go$GO_VERSION.linux-ppc64le.tar.gz
+export GOPATH=/home/runner/go
+export PATH=$GOPATH/bin:$PATH
 
-# Download and install go
-wget https://golang.org/dl/go1.17.5.linux-ppc64le.tar.gz
-tar -xzf go1.17.5.linux-ppc64le.tar.gz
-rm -rf go1.17.5.linux-ppc64le.tar.gz
-export GOPATH=`pwd`/gopath
-export PATH=`pwd`/go/bin:$GOPATH/bin:$PATH
+#Setup working directory
+mkdir -p $GOPATH/src/github.com/shirou && cd $GOPATH/src/github.com/shirou
 
-# Clone the repo and checkout submodules
-mkdir -p $GOPATH/src/$PACKAGE_PATH
-cd $GOPATH/src/$PACKAGE_PATH
-
-if [ -d "golang_protobuf_extensions" ] ; then
-  rm -rf golang_protobuf_extensions
-fi
-
+#Clone the repository
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
-
-export PATH=$GOPATH/bin:$PATH
-export GO111MODULE=on
-
-go mod init
+go mod init github.com/matttproud/golang_protobuf_extensions
 go mod tidy
+go get google.golang.org/protobuf/proto
+go get github.com/matttproud/golang_protobuf_extensions/pbtest
 
-go get github.com/golang/protobuf/proto
-
-echo "Building $PACKAGE_PATH$PACKAGE_NAME with $PACKAGE_VERSION"
-
-if ! go build -v ./...; then
-echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
-exit 1
+if ! go build ./...; then
+    echo "------------------$PACKAGE_NAME:Build_fails---------------------"
+    echo "$PACKAGE_VERSION $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
+    exit 1
 else
-echo "------------------$PACKAGE_NAME:install_success-------------------------"
-exit 0
+    echo "------------------$PACKAGE_NAME:Build_success-------------------------"
+    echo "$PACKAGE_VERSION $PACKAGE_NAME"
 fi
-
-if ! go test -v ./...; then
-echo "------------------$PACKAGE_NAME:test_fails-------------------------------------"
-exit 1
+if ! go test ./... ; then
+    echo "------------------$PACKAGE_NAME:test_fails---------------------"
+    echo "$PACKAGE_VERSION $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
+    exit 1
 else
-echo "------------------$PACKAGE_NAME:test_success-------------------------"
-exit 0
+    echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
+    echo "$PACKAGE_VERSION $PACKAGE_NAME"
+    exit 0
 fi
