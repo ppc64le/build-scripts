eb554e6bfb9f7b18fd04df747dbdd22dd578709f
Sachin Kakatkar <sachin.kakatkar@ibm.com>
2022-05-26 15:09:31 +0530

    Run only on vm. name changed to rhel. Tested version v1.9.0

--- p/pgconn/pgconn_rhel_8.2.sh
+++ p/pgconn/pgconn_rhel_8.2.sh
@@ -2,9 +2,9 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : pgconn
-# Version       : v1.6.1
+# Version       : v1.6.1, v1.9.0
 # Source repo   : https://github.com/jackc/pgconn.git
-# Tested on     : ubi 8.5
+# Tested on     : rhel 8.2
 # Language      : go
 # Travis-Check  : false
 # Script License: Apache License, Version 2 or later
@@ -17,11 +17,11 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-#Run the script:./pgconn_ubi_8.5.sh v1.6.1(version_to_test)
+#Run the script:./pgconn_rhel_8.2.sh v1.6.1(version_to_test)
+#Tested on virtual machine and will not run on the container.
 PACKAGE_NAME=pgconn
 PACKAGE_VERSION=v1.6.1
 PACKAGE_URL=https://github.com/jackc/pgconn.git
-
 
 dnf install git -y
 
@@ -30,7 +30,6 @@ docker run --name postgres-server -e POSTGRES_PASSWORD=password -d postgres
 IP_PG_SERVER=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" postgres-server)
 
 echo $ip_of_pg_server
-
 
 PACKAGE_VERSION=${1:-v1.6.1}
 
@@ -52,7 +51,6 @@ OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 export PATH=$GOPATH/bin:$PATH
 export GO111MODULE=on
 
-
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
         echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/clone_fails
@@ -63,15 +61,12 @@ fi
 cd /home/tester/$PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-INSTALL_SUCCESS="false"
-
-
-if ! go build; then
-        INSTALL_SUCCESS="false"
-        else
-        INSTALL_SUCCESS="true"
+if ! (go build); then
+       echo "------------------$PACKAGE_NAME:build failed---------------------"
+       echo "$PACKAGE_VERSION $PACKAGE_NAME"
+       echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  build_Fails"
+       exit 1
 fi
-
 
 # Ensure go.mod file exists
 if ! PGX_TEST_CONN_STRING="host=$IP_PG_SERVER dbname=postgres user=postgres password=password" go test -v -race ./...; then
