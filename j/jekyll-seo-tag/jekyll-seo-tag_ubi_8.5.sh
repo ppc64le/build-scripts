2cda57ea5807d19869cc59ce355a87886a8d75a4
vishakadesai <vishakandesai@gmail.com>
2022-05-30 16:26:14 +0530

    changed ruby version in build script

--- j/jekyll-seo-tag/jekyll-seo-tag_ubi_8.5.sh
+++ j/jekyll-seo-tag/jekyll-seo-tag_ubi_8.5.sh
@@ -48,7 +48,7 @@ curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
 curl -L https://get.rvm.io | bash -s stable --auto-dotfiles
 source /etc/profile.d/rvm.sh 
-rvm install ruby-2.7
+rvm install ruby-2.1
 
 mkdir -p /home/tester/output
 cd /home/tester
