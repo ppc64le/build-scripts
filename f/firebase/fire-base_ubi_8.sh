363ee1775179f824a637142407a5176e8c89224e
ankitpar <ankit.paraskar@ibm.com>
2022-05-30 01:28:34 -0400

    added_script

---
+++ f/firebase/fire-base_ubi_8.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -e
+
+# ----------------------------------------------------------------------------
+# Package          : php-jwt
+# Version          : v6.2.0
+# Source repo      : https://github.com/firebase/php-jwt
+# Tested on        : UBI 8.5
+# Language         : PHP
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Ankit Paraskar <Ankit.Paraskar@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+# Variables
+
+PACKAGE_NAME=Php-jwt
+PACKAGE_URL=https://github.com/firebase/php-jwt
+#PACKAGE_VERSION is configurable can be passed as an argument.
+PACKAGE_VERSION=v6.2.0
+
+yum install -y git curl php php-curl php-dom php-mbstring php-json php-gd php-pecl-zip
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+yum install unzip
+
+HOME_DIR=$(pwd)
+echo $HOME_DIR
+
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    exit 1
+fi
+
+composer require firebase/php-jwt
+
+
+
+cd "$HOME_DIR"/$PACKAGE_NAME || exit
+git checkout "$PACKAGE_VERSION"
+# Install symfony/error-handler on compatible PHP versions to avoid a deprecation warning of the old DebugClassLoader and ErrorHandler classes
+composer require --no-update --dev symfony/error-handler "^4.4 || ^5.0"
+composer require --dev phpunit/phpunit --with-all-dependencies ^4
+
+composer require paragonie/sodium_compat
+if ! composer install; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+cd "$HOME_DIR"/$PACKAGE_NAME || exit
+if ! ./vendor/bin/phpunit; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 1
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
+
+
