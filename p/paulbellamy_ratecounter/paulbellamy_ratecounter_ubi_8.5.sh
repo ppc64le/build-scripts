a7a2c7c7738b19db739e2e9e82a9219a0f7eda57
Sachin Kakatkar <sachin.kakatkar@ibm.com>
2022-06-02 13:43:59 +0530

    paulbellamy_ratecounter: Added build script

---
+++ p/paulbellamy_ratecounter/paulbellamy_ratecounter_ubi_8.5.sh
@@ -0,0 +1,74 @@
+I#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : paulbellamy/ratecounter
+# Version       : v0.2.0
+# Source repo   : https://github.com/paulbellamy/ratecounter.git
+# Tested on     : ubi 8.5
+# Language      : go
+# Travis-Check  : true
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Sachin K {sachin.kakatkar@ibm.com}
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#Run the script:./paulbellamy_ratecounter_ubi_8.5.sh v0.2.0(version_to_test)
+PACKAGE_NAME=ratecounter
+PACKAGE_VERSION=${1:-v0.2.0}
+GO_VERSION=1.17.4
+PACKAGE_URL=https://github.com/paulbellamy/ratecounter.git
+
+dnf install git wget sudo diffutils.ppc64le make gcc gcc-c++ -y
+
+mkdir -p /home/tester/output
+cd /home/tester
+
+wget https://golang.org/dl/go$GO_VERSION.linux-ppc64le.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local/ -xzf go$GO_VERSION.linux-ppc64le.tar.gz
+rm -rf go$GO_VERSION.linux-ppc64le.tar.gz
+export GOROOT=${GOROOT:-"/usr/local/go"}
+export GOPATH=${GOPATH:-/home/tester/go}
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:/usr/local/bin
+export  GO111MODULE=on
+mkdir -p $GOPATH/src/github.com/paulbellamy
+cd $GOPATH/src/github.com/paulbellamy
+
+rm -rf $PACKAGE_NAME
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/clone_fails
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails" > /home/tester/output/version_tracker
+        exit 1
+fi
+
+cd $PACKAGE_NAME
+
+git checkout $PACKAGE_VERSION
+
+if ! (go mod init && go mod tidy); then
+       echo "------------------$PACKAGE_NAME:build failed---------------------"
+       echo "$PACKAGE_VERSION $PACKAGE_NAME"
+       echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  build_Fails"
+       exit 1
+fi
+
+if ! go test ./... -v; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/test_fails
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails" > /home/tester/output/version_tracker
+        exit 1
+else
+        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME" > /home/tester/output/test_success
+        echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success" > /home/tester/output/version_tracker
+        exit 0
+fi
+
