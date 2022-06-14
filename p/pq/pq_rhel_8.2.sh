694315bc1267ee7d4ef87c122268b5891135ba98
Sachin Kakatkar <sachin.kakatkar@ibm.com>
2022-06-14 17:44:52 +0530

    Updated formatting changes and build logic

--- p/pq/pq_rhel_8.2.sh
+++ p/pq/pq_rhel_8.2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 # -----------------------------------------------------------------------------
 #
-# Package       : lib/pq
+# Package       : pq
 # Version       : v1.5.1
 # Source repo   : https://github.com/lib/pq
 # Tested on     : rhel 8.2
 # Language      : go
 # Travis-Check  : false
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Sachin K {sachin.kakatkar@ibm.com}
+# Maintainer    : Sachin K <sachin.kakatkar@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -17,7 +17,7 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-#Run the script:./pq_rhel_8.2.sh v1.5.1(version_to_test)
+# Run the script:./pq_rhel_8.2.sh v1.5.1(version_to_test)
 PACKAGE_NAME=pq
 PACKAGE_VERSION=${1:-v1.5.1}
 POSTGRES_VERSION=11
@@ -42,10 +42,7 @@ rm -f go$GO_VERSION.linux-ppc64le.tar.gz
 export GOPATH=/home/tester/go
 export PATH=$PATH:$GOPATH/bin
 
-
-
-
-#Configure postgres database
+# Configure postgres database
 sudo chmod 777 /var/lib/pgsql/$POSTGRES_VERSION/data/pg_hba.conf
 echo "local     all         postgres                          trust"    >  /var/lib/pgsql/$POSTGRES_VERSION/data/pg_hba.conf
 echo "local     all         all                               trust"    >> /var/lib/pgsql/$POSTGRES_VERSION/data/pg_hba.conf
@@ -55,7 +52,7 @@ echo "hostssl   all         pgx_ssl     127.0.0.1/32          md5"      >> /var/
 echo "host      replication pgx_replication 127.0.0.1/32      md5"      >> /var/lib/pgsql/$POSTGRES_VERSION/data/pg_hba.conf
 echo "host      pgx_test pgx_replication 127.0.0.1/32      md5"      >> /var/lib/pgsql/$POSTGRES_VERSION/data/pg_hba.conf
 
-#Restart
+# Restart postgres server
 sudo systemctl restart postgresql-$POSTGRES_VERSION
 
 psql -U postgres -c 'create database pgx_test'
@@ -68,30 +65,37 @@ psql -U postgres -c "create user pgx_replication with replication password 'secr
 psql -U postgres -c "create user \" tricky, ' } \"\" \\ test user \" superuser password 'secret'" 
 
 rm -rf $PACKAGE_NAME
-
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
-
 
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
         echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/clone_fails
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails" > /home/tester/output/version_tracker
+    	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails" > /home/tester/output/version_tracker
         exit 1
 fi
 
 cd /home/tester/$PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-INSTALL_SUCCESS="false"
-
-
-if ! go build; then
-        INSTALL_SUCCESS="false"
-        else
-        INSTALL_SUCCESS="true"
+# Ensure go.mod file exist and build
+if [ ! -f go.mod ]
+then
+if ! (go mod init && go mod tidy); then
+       echo "------------------$PACKAGE_NAME:build failed---------------------"
+       echo "$PACKAGE_VERSION $PACKAGE_NAME"
+       echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  build_Fails"
+       exit 1
+fi
+else
+if ! (go build); then
+       echo "------------------$PACKAGE_NAME:build failed---------------------"
+       echo "$PACKAGE_VERSION $PACKAGE_NAME"
+       echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  build_Fails"
+       exit 1
+fi
 fi
 
-# Ensure go.mod file exists
+# Run the tests
 if ! PGHOST=127.0.0.1 PGPORT=5432 PGUSER=pgx_pw PGSSLMODE=disable PGDATABASE=pgx_test PGPASSWORD=secret go test -v; then
         echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/test_fails
@@ -103,5 +107,3 @@ else
         echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success" > /home/tester/output/version_tracker
         exit 0
 fi
-
-
