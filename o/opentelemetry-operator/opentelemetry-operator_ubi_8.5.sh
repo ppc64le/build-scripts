3daced832dc9e56757ff9c56f9365f89479d22b0
adilhusain_s <Adilhusain.Shaikh@ibm.com>
2022-06-16 18:09:50 +0530

    adding opentelemetry-operator script for ubi 8.5

---
+++ o/opentelemetry-operator/opentelemetry-operator_ubi_8.5.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -ex
+# -----------------------------------------------------------------------------
+#
+# Package	: opentelemetry-operator
+# Version	: v0.52.0
+# Source repo	: https://github.com/open-telemetry/opentelemetry-operator
+# Tested on	: ubi 8.5
+# Language      : GO
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Adilhusain Shaikh <Adilhusain.Shaikh@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME="opentelemetry-operator"
+PACKAGE_VERSION=${1:-"v0.52.0"}
+PACKAGE_URL="https://github.com/open-telemetry/opentelemetry-operator"
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+export GO_VERSION=${GO_VERSION:-"1.18"}
+export GOROOT=${GOROOT:-"/usr/local/go"}
+export GOPATH=${GOPATH:-$HOME/go}
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:/usr/local/bin
+
+echo "installing dependencies from system repo"
+dnf install -qy wget git gcc-c++ make 
+
+# installing golang
+wget https://golang.org/dl/go"$GO_VERSION".linux-ppc64le.tar.gz
+tar -C /usr/local/ -xzf go"$GO_VERSION".linux-ppc64le.tar.gz
+rm -f go"$GO_VERSION".linux-ppc64le.tar.gz
+
+if ! git clone "$PACKAGE_URL" "$PACKAGE_NAME"; then
+    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    exit 1
+fi
+
+cd "$PACKAGE_NAME"
+git checkout "$PACKAGE_VERSION"
+if ! make; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! make test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 1
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
