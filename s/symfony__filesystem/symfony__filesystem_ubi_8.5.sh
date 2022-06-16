9e7162f7b1b802dbc0da0e595dfda4d934172787
vishakadesai <vishakandesai@gmail.com>
2022-06-16 16:59:10 +0530

    edited build script

--- s/symfony__filesystem/symfony__filesystem_ubi_8.5.sh
+++ s/symfony__filesystem/symfony__filesystem_ubi_8.5.sh
@@ -25,7 +25,7 @@ PACKAGE_URL=https://github.com/symfony/filesystem
 # yum install -y nodejs nodejs-devel nodejs-packaging npm python38 python38-devel ncurses git jq curl php php-curl php-dom php-mbstring php-json nodejs make gcc-c++ patch diffutils php-gd php-pecl-zip
 yum -y update && yum install -y git php php-json php-dom php-mbstring zip unzip
 
-OS_NAME=`python3 -c "os_file_data=open('/etc/os-release').readlines();os_info = [i.replace('PRETTY_NAME=','').strip() for i in os_file_data if i.startswith('PRETTY_NAME')];print(os_info[0])"`
+# OS_NAME=`python3 -c "os_file_data=open('/etc/os-release').readlines();os_info = [i.replace('PRETTY_NAME=','').strip() for i in os_file_data if i.startswith('PRETTY_NAME')];print(os_info[0])"`
 HOME_DIR=`pwd`
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
