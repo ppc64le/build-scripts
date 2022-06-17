b1027a411033e35c7886153b53d74cb7ebc19068
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2022-06-17 18:18:02 +0530

    Update starlark-go_ubi_8.5.sh

--- s/starlark-go/starlark-go_ubi_8.5.sh
+++ s/starlark-go/starlark-go_ubi_8.5.sh
@@ -35,7 +35,13 @@ export GOPATH=/home/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 export  GO111MODULE=on
 
-git clone $PACKAGE_URL
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    exit 1
+fi
+
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
