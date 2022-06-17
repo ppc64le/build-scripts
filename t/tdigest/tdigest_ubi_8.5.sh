c6589a835789805758a04c47566ce96faa0570cd
ambujkumar3 <104371292+ambujkumar3@users.noreply.github.com>
2022-06-17 10:31:19 +0530

    Update tdigest_ubi_8.5.sh

--- t/tdigest/tdigest_ubi_8.5.sh
+++ t/tdigest/tdigest_ubi_8.5.sh
@@ -17,18 +17,15 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-#Run the script:./tdigest_ubi_8.5.sh v0.0.1(version_to_test)
+# Run the script:./tdigest_ubi_8.5.sh v0.0.1(version_to_test)
 PACKAGE_NAME=tdigest
-#PACKAGE_VERSION=${1:-v0.0.1}
+# PACKAGE_VERSION=${1:-v0.0.1}
 PACKAGE_VERSION=${1:-bf2b5ad3c0a9}
 GO_VERSION=1.16.1
 PACKAGE_URL=https://github.com/influxdata/tdigest.git
-
 dnf install git wget sudo make gcc gcc-c++ -y
-
 mkdir -p /home/tester/output
 cd /home/tester
-
 wget https://golang.org/dl/go$GO_VERSION.linux-ppc64le.tar.gz
 rm -rf /home/tester/go && tar -C /home/tester -xzf go$GO_VERSION.linux-ppc64le.tar.gz
 rm -f go$GO_VERSION.linux-ppc64le.tar.gz
@@ -38,19 +35,14 @@ export  GO111MODULE=on
 mkdir -p $GOPATH/src/github.com/influxdata
 cd $GOPATH/src/github.com/influxdata
 rm -rf $PACKAGE_NAME
-
-
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
-
 
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
         echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME" > /home/tester/output/clone_fails
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails" > /home/tester/output/version_tracker
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails" > /home/tester/output/version_tracker
         exit 1
 fi
-
-echo $PWD
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 echo $PWD
@@ -59,22 +51,18 @@ sed -i 's/99.90103781043621/99.9010378104362/g' tdigest_test.go
 sed -i 's/98.98503400959562/98.98503400959561/g' tdigest_test.go
 sed -i 's/89.98281777095822/89.9828177709582/g' tdigest_test.go
 sed -i 's/10.000673533707138/10.00067353370714/g' tdigest_test.go
-
 file="$GOPATH/src/github.com/influxdata/tdigest/go.mod"
 if [ ! -f "$file" ]
 then
-go mod init
-go mod tidy
+        go mod init
+        go mod tidy
 fi
-
 INSTALL_SUCCESS="false"
-
 if ! go build; then
         INSTALL_SUCCESS="false"
         else
         INSTALL_SUCCESS="true"
 fi
-
 if ! go test -v; then
         echo "------------------$PACKAGE_NAME :install_success_but_test_fails---------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME " > /home/tester/output/test_fails
