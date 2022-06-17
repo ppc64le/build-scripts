eb421f2c6e1cbeeb807d6be863445254947c10bf
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2022-06-17 18:12:38 +0530

    Update pear_archive-tar_ubi_8.5.sh

--- p/pear_archive-tar/pear_archive-tar_ubi_8.5.sh
+++ p/pear_archive-tar/pear_archive-tar_ubi_8.5.sh
@@ -25,8 +25,9 @@ OS_NAME=`cat /etc/os-release | grep "PRETTY" | awk -F '=' '{print $2}'`
 HOME_DIR=`pwd`
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
         echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-                echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+        exit 1
 fi
 cd $HOME_DIR/$PACKAGE_NAME
 git checkout $PACKAGE_VERSION
