99a3586c2715c440d062b08b8906490e01c8aedc
ankitpar <100115421+ankitpar@users.noreply.github.com>
2022-06-17 10:10:27 +0530

    Update angular-toaster_ubi_8.sh

--- a/angulartoaster/angular-toaster_ubi_8.sh
+++ a/angulartoaster/angular-toaster_ubi_8.sh
@@ -20,7 +20,7 @@ PACKAGE_NAME=angular-toaster
 PACKAGE_VERSION=1.2.0
 PACKAGE_URL=https://github.com/jirikavi/AngularJS-Toaster
 
-yum -y update && yum install -y yum-utils nodejs nodejs-devel nodejs-packaging npm python38 python38-devel ncurses git gcc gcc-c++ libffi libffi-devel ncurses git jq make cmake
+yum -y update && yum install -y yum-utils nodejs nodejs-devel nodejs-packaging npm python38 python38-devel ncurses git gcc gcc-c++ libffi libffi-devel ncurses git jq make cmake npm
 
 npm install n -g && n latest && npm install -g npm@latest && export PATH="$PATH" && npm install --global yarn grunt-bump xo testem acorn
 
