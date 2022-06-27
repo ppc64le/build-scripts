fc1f94ae530270605584ed788ab47701d388ff34
vishakadesai <vishakandesai@gmail.com>
2022-06-27 11:41:09 +0530

    fixed typo in build script

--- d/drupal-warmer/drupal-warmer_ubi_8.5.sh
+++ d/drupal-warmer/drupal-warmer_ubi_8.5.sh
@@ -8,7 +8,7 @@
 # Language         : PHP
 # Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
-# Maintainer       : Bhagat Singh <Bhagat.singh1@ibm.com>, Vishaka Desai <Vishaka.FDesai@ibm.com>
+# Maintainer       : Bhagat Singh <Bhagat.singh1@ibm.com>, Vishaka Desai <Vishaka.Desai@ibm.com>
 #
 # Disclaimer       : This script has been tested in root mode on given
 # ==========         platform using the mentioned version of the package.
