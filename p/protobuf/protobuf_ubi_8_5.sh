a7f9c5db1beb405713a86df3ed3c8d5088ab815e
ankitpar <100115421+ankitpar@users.noreply.github.com>
2022-06-29 13:42:56 +0530

    Update protobuf_ubi_8_5.sh
    Skipping travis check due to timeout issue

--- p/protobuf/protobuf_ubi_8_5.sh
+++ p/protobuf/protobuf_ubi_8_5.sh
@@ -6,7 +6,7 @@
 # Source repo      : https://github.com/protocolbuffers/protobuf.git
 # Tested on        : UBI 8.5
 # Language         : C++
-# Travis-Check     : True
+# Travis-Check     : False
 # Script License   : Apache License, Version 2 or later
 # Maintainer       : Ankit Paraskar <Ankit.Paraskar@ibm.com>
 #
