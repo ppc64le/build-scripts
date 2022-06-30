749aac9713fee5161c2a1c55d4f297b857f0654d
vathsalashetty25 <vathsalashetty69@gmail.com>
2022-06-30 19:35:25 +0530

    Adding buildscript for package site-process

---
+++ c/consolidation_site-process/consolidation_site-process_ubi_8.5.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : site-process
+# Version       : 4.1.0
+# Source repo   : https://github.com/consolidation/site-process.git
+# Tested on     : UBI 8.5
+# Language      : PHP
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : vathsala . <vaths367@in.ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+PACKAGE_NAME=site-process
+PACKAGE_VERSION=${1:-4.1.0}
+PACKAGE_URL=https://github.com/consolidation/site-process.git
+yum install -y git php php-json php-dom php-mbstring php-pdo php-intl zip unzip
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
+OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
+HOME_DIR=`pwd`
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+        exit 1
+fi
+cd $HOME_DIR/$PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+composer require --dev phpunit/phpunit --with-all-dependencies ^7
+if ! composer install; then
+        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+        exit 1
+fi
+cd $HOME_DIR/$PACKAGE_NAME
+if ! vendor/bin/phpunit; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+        exit 1
+else
+        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+        exit 0
+fi
