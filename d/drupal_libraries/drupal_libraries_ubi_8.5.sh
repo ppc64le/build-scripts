fd4baf913acfbc77aea5bf8d06053fc38f3b8cd4
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2022-07-05 11:41:01 +0530

    Update drupal_libraries_ubi_8.5.sh

--- d/drupal_libraries/drupal_libraries_ubi_8.5.sh
+++ d/drupal_libraries/drupal_libraries_ubi_8.5.sh
@@ -81,3 +81,5 @@ else
         exit 0
 fi
 
+# drupal-libraries has 3 types of test Unit/Functional/Kernel.Unit test dont need drupal framework and DB etc. So can be executed by given script.
+# Please follow README file for more information to run Functional test cases.
