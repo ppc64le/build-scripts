9a7cec1a7ae0c3f8c26f3b27fe14024f447250b4
saraswati-patra <Saraswati.patra@ibm.com>
2022-07-08 18:16:49 +0530

    added build script for cache
    added build script for cache

---
+++ d/cache/cache_8.5.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+# -----------------------------------------------------------------------------
+#
+# Package       : cache
+# Version       : 1.11.0,1.12.1
+# Source repo   : https://github.com/doctrine/cache
+# Tested on     : RHEL 8.5
+#Language         : PHP
+# Travis-Check     : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Saraswati Patra <saraswati.patra@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+PACKAGE_NAME=cache
+PACKAGE_VERSION=${1:-1.11.0}
+PACKAGE_URL=https://github.com/doctrine/cache
+#yum install && yum install -y git php php-json php-dom php-mbstring php-devel php-pear make zip unzip
+#yum -y update && yum install-y git php php-json php-dom php-mbstring php-devel php-pear make zip unzip
+yum install -y git php php-json php-dom php-mbstring php-devel php-pear make zip unzip
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
+composer require --dev phpunit/phpunit --with-all-dependencies ^7
+phpenv global 7.1 2>/dev/null
+pecl install mongodb
+echo extension=mongodb.so >> /etc/php.ini
+OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
+HOME_DIR=`pwd`
+if [ -d "$PACKAGE_NAME" ] ; then
+  rm -rf $PACKAGE_NAME
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
+fi
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+                echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+        exit 1
+fi
+cd $HOME_DIR/$PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+if ! composer install; then
+        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+        exit 1
+fi
+cd $HOME_DIR/$PACKAGE_NAME
+if ! vendor/bin/phpunit; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+        exit 1
+else
+        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+        exit 0
+fi
+#Both build and test success.
+
