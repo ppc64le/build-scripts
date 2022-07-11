93925da8d19801b1b6d993baddbb90823e47ef9e
rajusah15 <85694603+rajusah15@users.noreply.github.com>
2022-07-11 12:12:34 +0530

    Apply suggestions from code review
    Co-authored-by: Prashant Khoje <prashant.khoje@ibm.com>

--- d/drupal-search_api_autocomplete/drupal-search_api_autocomplete_ubi_8.5.sh
+++ d/drupal-search_api_autocomplete/drupal-search_api_autocomplete_ubi_8.5.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 # ----------------------------------------------------------------------------
 # Package          : search_api_autocomplete
@@ -25,35 +25,31 @@ PACKAGE_URL=https://git.drupalcode.org/project/search_api_autocomplete.git
 CORE_PACKAGE_URL=https://github.com/drupal/drupal
 PACKAGE_VERSION=${1:-8.x-1.5}
 
-
 yum install -y git php php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
 
 OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
 
 #Check if package exists
 if [ -d "$CORE_PACKAGE_NAME" ] ; then
-  rm -rf $CORE_PACKAGE_NAME
-  echo "$CORE_PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
-
+    rm -rf $CORE_PACKAGE_NAME
+    echo "$CORE_PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package"
 fi
 
 if ! git clone $CORE_PACKAGE_URL $CORE_PACKAGE_NAME; then
-        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-        echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
-        echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-        exit 0
+    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
+    echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    exit 1
 fi
 
 cd $CORE_PACKAGE_NAME
-
 composer require  drupal/jquery_ui
-#composer require  drush/drush
 
 if ! composer install --no-interaction; then
-        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
-        exit 1
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
 fi
 
 cd modules/
@@ -64,10 +60,10 @@ git checkout 8.x-1.7
 cd ..
 
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
-        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-        exit 0
+    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    exit 1
 fi
 
 cd $PACKAGE_NAME
@@ -79,15 +75,14 @@ cd ../..
 ./vendor/bin/drush en contact_block
 
 cd core/
-
 if ! ../vendor/phpunit/phpunit/phpunit ../modules/search_api_autocomplete/src/Tests/; then
-        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
-        exit 1
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 1
 else
-        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
-        exit 0
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
 fi
