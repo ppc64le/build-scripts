4247eeddc0e06585b7c72ab03e4d7befd8bdc9f2
vaishnavipatil3 <Vaishnavi.Patil3@ibm.com>
2022-07-12 02:28:24 -0400

    Build-script for package drupal-eu_cookie_compliance

---
+++ d/drupal-eu_cookie_compliance/drupal-eu_cookie_compliance_ubi_8.5.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+
+# ----------------------------------------------------------------------------
+# Package          : drupal/eu_cookie_compliance
+# Version          : 1.14.0
+# Source repo      : https://git.drupalcode.org/project/eu_cookie_compliance
+# Tested on        : UBI 8.5
+# Language         : PHP
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vaishnavi Patil <Vaishnavi.Patil3@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+# Variables
+PACKAGE_NAME=eu_cookie_compliance
+PACKAGE_URL=https://git.drupalcode.org/project/eu-cookie-compliance/
+#PACKAGE_VERSION is configurable can be passed as an argument.
+PACKAGE_VERSION=${1:-8.x-1.14}
+
+yum module enable php:7.4 -y
+yum install -y git php php-json php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
+
+OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
+
+#Check if package exists
+if [ -d "$PACKAGE_NAME" ] ; then
+  rm -rf $PACKAGE_NAME
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
+
+fi
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+                echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+        exit 1
+fi
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+composer require --dev phpunit/phpunit --with-all-dependencies ^7 --no-interaction
+
+if ! composer install --no-interaction; then
+        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+        exit 1
+fi
+
+# No test case available.
