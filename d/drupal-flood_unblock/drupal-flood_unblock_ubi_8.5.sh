e213f276a8e7daa6384fdfed368f95127fa36d63
Ambuj Kumar <Ambuj.Kumar3@ibm.com>
2022-07-12 10:04:17 +0000

    drupal-flood_unblock Version 3.2.0 build and validation script

---
+++ d/drupal-flood_unblock/drupal-flood_unblock_ubi_8.5.sh
@@ -0,0 +1,89 @@
+#!/bin/bash -e
+
+# ----------------------------------------------------------------------------
+# Package          : flood_unblock
+# Version          : 3.2.0
+# Source repo      : https://git.drupalcode.org/project/flood_unblock.git
+# Tested on        : UBI 8.5
+# Language         : PHP
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Ambuj Kumar <Ambuj.Kumar3@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+# Variables
+PACKAGE_NAME=flood_unblock
+CORE_PACKAGE_NAME=drupal
+PACKAGE_URL=https://git.drupalcode.org/project/flood_unblock.git
+CORE_PACKAGE_URL=https://github.com/drupal/drupal
+
+PACKAGE_VERSION=${1:-3.2.0}
+
+yum module enable php:7.4 -y
+yum install -y git php php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
+
+OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
+
+# Check if package exists
+if [ -d "$CORE_PACKAGE_NAME" ] ; then
+	rm -rf $CORE_PACKAGE_NAME
+	echo "$CORE_PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
+fi
+
+if ! git clone $CORE_PACKAGE_URL $CORE_PACKAGE_NAME; then
+	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+	echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
+	echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+	exit 0
+fi
+
+cd $CORE_PACKAGE_NAME
+git checkout 8.9.0
+rm -rf composer.lock
+composer config allow-plugins true --no-interaction
+composer require --dev phpunit/phpunit --with-all-dependencies ^7 --no-interaction
+
+
+if ! composer install --no-interaction; then
+	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+	exit 1
+fi
+cd modules/
+
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+	exit 0
+fi
+
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+cd ../..
+
+cd core/
+
+if ! ../vendor/phpunit/phpunit/phpunit ../modules/flood_unblock; then
+	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+	exit 1
+else
+	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+	echo "The test cases are not available."
+	exit 0
+fi
+
+#echo "The test cases are not available."
