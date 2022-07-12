7ae37e3236ae6c15b86c941807bceb3574319a6d
ambujkumar3 <104371292+ambujkumar3@users.noreply.github.com>
2022-07-12 16:29:02 +0530

    Update drupal-address_ubi_8.5.sh

--- d/drupal-address/drupal-address_ubi_8.5.sh
+++ d/drupal-address/drupal-address_ubi_8.5.sh
@@ -6,7 +6,7 @@
 # Source repo      : https://git.drupalcode.org/project/address.git
 # Tested on        : UBI 8.5
 # Language         : PHP
-# Travis-Check     : False
+# Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
 # Maintainer       : Ambuj Kumar <Ambuj.Kumar3@ibm.com>
 #
@@ -25,7 +25,9 @@ PACKAGE_URL=https://git.drupalcode.org/project/address.git
 CORE_PACKAGE_URL=https://github.com/drupal/drupal
 PACKAGE_VERSION=${1:-8.x-1.9}
 
+yum module enable php:7.4 -y
 yum install -y git php php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
 
 OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
 
@@ -43,7 +45,10 @@ if ! git clone $CORE_PACKAGE_URL $CORE_PACKAGE_NAME; then
 fi
 
 cd $CORE_PACKAGE_NAME
-
+git checkout 8.9.0
+rm -rf composer.lock
+composer config allow-plugins true --no-interaction
+composer require --dev phpunit/phpunit --with-all-dependencies ^7 --no-interaction
 composer require 'drupal/token:^1.10'
 composer require 'commerceguys/addressing:*'
 
@@ -67,10 +72,8 @@ cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 cd ../..
- ./vendor/bin/drush en address
-
 cd core/
-if ! ../vendor/phpunit/phpunit/phpunit ../modules/address/tests/src; then
+if ! ../vendor/phpunit/phpunit/phpunit ../modules/address/tests/src/Unit; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
@@ -81,3 +84,6 @@ else
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
     exit 0
 fi
+
+# drupal-rules has 4 types of test Unit/Functional/Kernel/FunctionalJavascript. Unit test don't need drupal framework and DB etc. So can be executed by given script.
+# Please follow README file for more information to run /Functional/Kernel/FunctionalJavascript test cases.
