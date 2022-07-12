eb610f794c0a6dd875a6a430728a330c135a036a
ambujkumar3 <104371292+ambujkumar3@users.noreply.github.com>
2022-07-12 17:02:27 +0530

    Update drupal-flag_ubi_8.5.sh

--- d/drupal-flag/drupal-flag_ubi_8.5.sh
+++ d/drupal-flag/drupal-flag_ubi_8.5.sh
@@ -6,7 +6,7 @@
 # Source repo      : https://git.drupalcode.org/project/flag.git
 # Tested on        : UBI 8.5
 # Language         : PHP
-# Travis-Check     : False
+# Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
 # Maintainer       : Ambuj Kumar <Ambuj.Kumar3@ibm.com>
 #
@@ -26,8 +26,9 @@ CORE_PACKAGE_URL=https://github.com/drupal/drupal
 
 PACKAGE_VERSION=${1:-8.x-4.0-beta2}
 
-
+yum module enable php:7.4 -y
 yum install -y git php php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
 
 OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
 
@@ -45,6 +46,10 @@ if ! git clone $CORE_PACKAGE_URL $CORE_PACKAGE_NAME; then
 fi
 
 cd $CORE_PACKAGE_NAME
+git checkout 8.9.0
+rm -rf composer.lock
+composer config allow-plugins true --no-interaction
+composer require --dev phpunit/phpunit --with-all-dependencies ^7 --no-interaction
 
 if ! composer install --no-interaction; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
@@ -53,7 +58,7 @@ if ! composer install --no-interaction; then
     exit 1
 fi
 
- cd modules/
+cd modules/
 
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
@@ -66,11 +71,9 @@ cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 cd ../..
- ./vendor/bin/drush en flag
-
 cd core/
 
- if ! ../vendor/phpunit/phpunit/phpunit ../modules/flag/tests/src; then
+if ! ../vendor/phpunit/phpunit/phpunit ../modules/flag/tests/src/Unit; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
@@ -81,3 +84,6 @@ else
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
     exit 0
 fi
+
+# drupal-flag has 5 types of test Unit/Functional/Kernel/FunctionalJavascript/Traits. Unit test don't need drupal framework and DB etc. So can be executed by given script.
+# Please follow README file for more information to run /Functional/Kernel/FunctionalJavascript/Traits test cases.
