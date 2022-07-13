8a80c3d2a73f533ba2e59a0e77c6d54f761a32a3
HarithaPatchari054 <107609563+HarithaPatchari054@users.noreply.github.com>
2022-07-13 15:12:32 +0530

    Update drupal-ultimate_cron_ubi_8.5.sh

--- d/drupal-ultimate_cron/drupal-ultimate_cron_ubi_8.5.sh
+++ d/drupal-ultimate_cron/drupal-ultimate_cron_ubi_8.5.sh
@@ -26,7 +26,6 @@ CORE_PACKAGE_URL=https://github.com/drupal/drupal
 #PACKAGE_VERSION is configurable can be passed as an argument.
 PACKAGE_VERSION=${1:-7.x-2.0}
 
-
 yum module enable php:7.4 -y
 yum install -y git php php-json php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
@@ -48,13 +47,11 @@ if ! git clone $CORE_PACKAGE_URL $CORE_PACKAGE_NAME; then
 fi
 
 cd $CORE_PACKAGE_NAME
-
  
 #Check if package exists
 if [ -d "$PACKAGE_NAME" ] ; then
   rm -rf $PACKAGE_NAME
-  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"  
- 
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"   
 fi
 
  if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
@@ -65,11 +62,8 @@ fi
 fi
 
 cd $PACKAGE_NAME
-
 git checkout $PACKAGE_VERSION
-
 composer require --dev phpunit/phpunit --with-all-dependencies ^8
-
 
 if ! composer install --no-interaction; then
      	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
