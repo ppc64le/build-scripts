3c64e63b44581d19cf9f99239dfadcea4eb36d4e
HarithaPatchari054 <107609563+HarithaPatchari054@users.noreply.github.com>
2022-07-14 14:51:34 +0530

    Update drupal-slick_views_ubi_8.5.sh

--- d/drupal-slick_views/drupal-slick_views_ubi_8.5.sh
+++ d/drupal-slick_views/drupal-slick_views_ubi_8.5.sh
@@ -3,7 +3,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : slick_views
-# Version       : 2.4.0
+# Version       : 8.x-2.4
 # Source repo   : https://git.drupalcode.org/project/slick_views
 # Tested on     : UBI 8.5
 # Language      : PHP
@@ -18,6 +18,7 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
+#Variables
 
 PACKAGE_NAME=slick_views
 CORE_PACKAGE_NAME=drupal
@@ -29,47 +30,62 @@ PACKAGE_VERSION=${1:-8.x-2.4}
 yum module enable php:7.4 -y
 yum install -y git php php-json php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
-
 OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
-
 #Check if package exists
 if [ -d "$CORE_PACKAGE_NAME" ] ; then
   rm -rf $CORE_PACKAGE_NAME
-  echo "$CORE_PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"  
- 
-fi
+  echo "$CORE_PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
 
+fi
 if ! git clone $CORE_PACKAGE_URL $CORE_PACKAGE_NAME; then
-    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-		echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+                echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
         echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    	exit 0
+        exit 1
 fi
-
 cd $CORE_PACKAGE_NAME
- 
+git checkout 8.9.0
+rm -rf composer.lock
+
+composer config allow-plugins true --no-interaction
+if ! composer install --no-interaction; then
+        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+        exit 1
+fi
+    composer require --dev phpunit/phpunit --with-all-dependencies ^7 --no-interaction
+    cd modules/
+
 #Check if package exists
 if [ -d "$PACKAGE_NAME" ] ; then
   rm -rf $PACKAGE_NAME
-  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"   
-fi
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
 
+fi
  if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
-    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-		echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+                echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    	exit 0
+        exit 1
 fi
-
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
-composer require --dev phpunit/phpunit --with-all-dependencies ^8
+cd ../../
+cd core/
 
-if ! composer install --no-interaction; then
-     	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
-	exit 1
+if !  ../vendor/phpunit/phpunit/phpunit ../modules/slick_views/; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+        exit 1
+else
+        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+        exit 0
 fi
+
+
 #No test case available
 
