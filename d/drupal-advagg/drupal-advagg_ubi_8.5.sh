64915324d97aca3f18c3b90967002ba03bc597b9
vaishnavipatil3 <Vaishnavi.Patil3@ibm.com>
2022-07-18 03:42:39 -0400

    Updated build-script for drupal-advagg package for testing part

--- d/drupal-advagg/drupal-advagg_ubi_8.5.sh
+++ d/drupal-advagg/drupal-advagg_ubi_8.5.sh
@@ -1,64 +1,91 @@
 #!/bin/bash -e
 
-# -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# Package          : drupal/advagg
+# Version          : 7.x-2.35,8.x-4.1
+# Source repo      : https://git.drupalcode.org/project/advagg
+# Tested on        : UBI 8.5
+# Language         : PHP
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vathsala . <vaths367@in.ibm.com>, Vaishnavi Patil <Vaishnavi.Patil3@ibm.com>
 #
-# Package       : advagg
-# Version       : 7.x-2.35,8.x-4.1
-# Source repo   : https://git.drupalcode.org/project/advagg.git
-# Tested on     : UBI 8.5
-# Language      : PHP
-# Travis-Check  : True
-# Script License: Apache License, Version 2 or later
-# Maintainer    : Vathsala . <vaths367@in.ibm.com>, Vaishnavi Patil <Vaishnavi.Patil3@ibm.com>
-#
-# Disclaimer: This script has been tested in root mode on given
-# ==========  platform using the mentioned version of the package.
-#             It may not work as expected with newer versions of the
-#             package and/or distribution. In such case, please
-#             contact "Maintainer" of this script.
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-
-
+# Variables
 PACKAGE_NAME=advagg
-PACKAGE_URL=https://git.drupalcode.org/project/advagg.git
+CORE_PACKAGE_NAME=drupal
+PACKAGE_URL=https://git.drupalcode.org/project/advagg
+CORE_PACKAGE_URL=https://github.com/drupal/drupal
 #PACKAGE_VERSION is configurable can be passed as an argument.
-PACKAGE_VERSION=${1:-7.x-2.35}
-yum install -y git curl php php-curl php-dom php-mbstring php-json php-gd php-pecl-zip
+PACKAGE_VERSION=${1:-8.x-4.1}
+
+yum module enable php:7.4 -y
+yum install -y git php php-json php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
-HOME_DIR=$(pwd)
+
+OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
+
+#Check if package exists
+if [ -d "$CORE_PACKAGE_NAME" ] ; then
+  rm -rf $CORE_PACKAGE_NAME
+  echo "$CORE_PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
+
+fi
+
+if ! git clone $CORE_PACKAGE_URL $CORE_PACKAGE_NAME; then
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+                echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
+        echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+        exit 1
+fi
+cd $CORE_PACKAGE_NAME
+git checkout 8.9.0
+rm -rf composer.lock
+
+composer config allow-plugins true --no-interaction
+
+if ! composer install --no-interaction; then
+        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+        exit 1
+fi
+    composer require --dev phpunit/phpunit --with-all-dependencies ^7 --no-interaction
+    cd modules/
+
+#Check if package exists
+if [ -d "$PACKAGE_NAME" ] ; then
+  rm -rf $PACKAGE_NAME
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
+fi
+
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
-    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    exit 1
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+                echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+        exit 1
 fi
-cd "$HOME_DIR"/$PACKAGE_NAME || exit
-git checkout "$PACKAGE_VERSION"
-# Install symfony/error-handler on compatible PHP versions to avoid a deprecation warning of the old DebugClassLoader and ErrorHandler classes
-composer require --no-update --dev symfony/error-handler "^4.4 || ^5.0"
-composer require --dev phpunit/phpunit --with-all-dependencies ^4
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+cd ../../
+cd core/
 
-#Tests not available(Test N/A).
-
-if ! composer install; then
-    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
-    exit 1
-fi
-
-cd ../../core/
-
-if ! ../vendor/phpunit/phpunit/phpunit ../modules/$PACKAGE_NAME/tests/; then
-	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
-	exit 1
+if !  ../vendor/phpunit/phpunit/phpunit ../modules/$PACKAGE_NAME/tests/modules/; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+        exit 1
 else
-	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
-	exit 0
+        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+        exit 0
 fi
-
+# Functional, Kernel tests need drupal framework to test.
+# Refer to README for further details.
