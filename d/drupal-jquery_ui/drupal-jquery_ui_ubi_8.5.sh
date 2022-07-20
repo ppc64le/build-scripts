2910a06679a46d17bc52fbb363660f79355deea5
HarithaPatchari054 <107609563+HarithaPatchari054@users.noreply.github.com>
2022-07-20 11:03:05 +0530

    Update drupal-jquery_ui_ubi_8.5.sh

--- d/drupal-jquery_ui/drupal-jquery_ui_ubi_8.5.sh
+++ d/drupal-jquery_ui/drupal-jquery_ui_ubi_8.5.sh
@@ -34,31 +34,31 @@ OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
 
 #Check if package exists
 if [ -d "$CORE_PACKAGE_NAME" ] ; then
-  rm -rf $CORE_PACKAGE_NAME
-  echo "$CORE_PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"  
+	rm -rf $CORE_PACKAGE_NAME
+	echo "$CORE_PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"  
  
 fi
 
 if ! git clone $CORE_PACKAGE_URL $CORE_PACKAGE_NAME; then
-    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-		echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
-        echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    	exit 0
+	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+	echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
+    	echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+	exit 0
 fi
 
 cd $CORE_PACKAGE_NAME
  
 #Check if package exists
 if [ -d "$PACKAGE_NAME" ] ; then
-  rm -rf $PACKAGE_NAME
-  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"   
+	rm -rf $PACKAGE_NAME
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"   
 fi
 
  if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
-    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-		echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    	exit 0
+	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+	exit 0
 fi
 
 cd $PACKAGE_NAME
@@ -66,11 +66,10 @@ git checkout $PACKAGE_VERSION
 composer require --dev phpunit/phpunit --with-all-dependencies ^8
 
 if ! composer install --no-interaction; then
-     	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
 	exit 1
 fi
 
 #No test case available
-
