ad707e43f6c5bad09690d1f6694681ed3da9a9b1
saraswati-patra <99007011+saraswati-patra@users.noreply.github.com>
2022-07-21 12:10:11 +0530

    Removed non-public link

--- y/yaeti/yaeti_ubi_8.5.sh
+++ y/yaeti/yaeti_ubi_8.5.sh
@@ -60,7 +60,6 @@ if ! npm install && npm audit fix && npm audit fix --force; then
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
 	exit 1
 fi
-#https://jazz06.rchland.ibm.com:12443/jazz/web/projects/Power%20Ecosystem#action=com.ibm.team.workitem.viewWorkItem&id=149836
 #version:0.0.6
 #Source Repo: https://github.com/ibc/yaeti.git
 #License Link: https://github.com/ibc/yaeti/blob/master/LICENSE
