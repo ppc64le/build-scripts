8acf9e15e0e9ce5022c3b7cf2d96fecbc5e08a44
saraswati-patra <99007011+saraswati-patra@users.noreply.github.com>
2022-07-21 11:46:49 +0530

    code changes done with Exit code 1

--- h/htmlpurifier/htmlpurifier_ubi_8.5.sh
+++ h/htmlpurifier/htmlpurifier_ubi_8.5.sh
@@ -58,7 +58,7 @@ if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    exit 0
+    exit 1
 fi
 cd "$HOME_DIR"/$PACKAGE_NAME || exit 1
 git checkout "$PACKAGE_VERSION"
@@ -69,7 +69,7 @@ if ! php tests/index.php; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  install_success"
-    exit 0
+    exit 1
 fi
 
 
