a02981d672a1ef3615068ec1d4e87afc98d77c4c
vishakadesai <vishakandesai@gmail.com>
2022-07-22 11:06:34 +0530

    added patch within build script

--- r/redis/redis_7.0.4_ubi_8.5.sh
+++ r/redis/redis_7.0.4_ubi_8.5.sh
@@ -51,13 +51,59 @@ if ! make CFLAGS=-D__linux=1 -j $(nproc); then
 	exit 1
 fi
 
+# Adding patchfile within build script temporarily
+cat > redis_7.0.4.patch << 'EOF'
+diff --git a/redis.conf b/redis.conf
+index 0431c3742..23298439f 100644
+--- a/redis.conf
++++ b/redis.conf
+@@ -1564,8 +1564,8 @@ aof-timestamp-enabled no
+ # to disable this mechanism (uninterrupted execution). Note that in the past
+ # this config had a different name, which is now an alias, so both of these do
+ # the same:
+-# lua-time-limit 5000
+-# busy-reply-threshold 5000
++lua-time-limit 1000
++busy-reply-threshold 1000
+ 
+ ################################ REDIS CLUSTER  ###############################
+ 
+diff --git a/tests/integration/replication.tcl b/tests/integration/replication.tcl
+index d60c91918..51e8c5061 100644
+--- a/tests/integration/replication.tcl
++++ b/tests/integration/replication.tcl
+@@ -383,7 +383,7 @@ start_server {tags {"repl external:skip"}} {
+         test "Master stream is correctly processed while the replica has a script in -BUSY state" {
+             set load_handle0 [start_write_load $master_host $master_port 3]
+             set slave [srv 0 client]
+-            $slave config set lua-time-limit 500
++            $slave config set lua-time-limit 10
+             $slave slaveof $master_host $master_port
+ 
+             # Wait for the slave to be online
+diff --git a/tests/unit/scripting.tcl b/tests/unit/scripting.tcl
+index f1c05a4d4..f9f9bda4c 100644
+--- a/tests/unit/scripting.tcl
++++ b/tests/unit/scripting.tcl
+@@ -1613,7 +1613,7 @@ start_server {tags {"scripting"}} {
+             $rd close
+         }
+         r config set min-replicas-to-write 0
+-        r config set lua-time-limit 5000
++        r config set lua-time-limit 1000
+     } {OK} {external:skip needs:repl}
+ 
+     test "allow-stale shebang flag" {
+EOF
+
 # Skipping tests observed to fail often
 echo "Verify command got unblocked after resharding" > skipfile
 echo "All time-to-live(TTL) in commands are propagated as absolute timestamp in milliseconds in AOF" >> skipfile
 echo "SHUTDOWN will abort if rdb save failed on signal" >> skipfile
 
 cd $HOME_DIR/$PACKAGE_NAME
-git apply $HOME_DIR/redis_7.0.4.patch
+# wget https://raw.githubusercontent.com/ppc64le/build-scripts/master/r/redis/redis_7.0.4.patch
+patch -p1 < redis_7.0.4.patch
 
 if ! ./runtest --skipfile skipfile; then
 	echo "------------------$PACKAGE_NAME:build_success_but_test_fails---------------------"
