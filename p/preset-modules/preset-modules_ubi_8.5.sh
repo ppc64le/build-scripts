99909a3202950b7f7c94e0a9e6c0cc40129fe812
saraswati-patra <Saraswati.patra@ibm.com>
2022-07-25 18:10:02 +0530

    added build script for preset-modules

---
+++ p/preset-modules/preset-modules_ubi_8.5.sh
@@ -0,0 +1,79 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package	: preset-modules
+# Version	: 0.1.4
+# Source repo	: https://github.com/babel/preset-modules.git
+# Tested on	: UBI 8.5
+# Language      : Node
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Saraswati Patra <Saraswati.patra@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=preset-modules
+PACKAGE_VERSION=${1:-0.1.4}
+PACKAGE_URL=https://github.com/babel/preset-modules.git
+
+yum -y update && yum install -y nodejs nodejs-devel nodejs-packaging npm git jq
+
+npm install n -g && n latest && npm install -g npm@latest
+
+export npm_config_yes=true
+
+HOME_DIR=`pwd`
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+#Check if package exists
+if [ -d "$PACKAGE_NAME" ] ; then
+  rm -rf $PACKAGE_NAME
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
+fi
+
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+		echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    	exit 1
+fi
+
+cd $HOME_DIR/$PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+npm install -g yarn
+yarn install
+
+if ! npm run build; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! npm test -- --ci; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME" 
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 1
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
+#both build and test pass
+#Test Suites: 6 passed, 6 total
+#Tests:       61 passed, 61 total
+#Snapshots:   1 passed, 1 total
+#Time:        27.785s
+#Ran all test suites.
+#------------------preset-modules:install_&_test_both_success-------------------------
+#https://github.com/babel/preset-modules.git preset-modules
+#preset-modules  |  https://github.com/babel/preset-modules.git | 0.1.4 | "Red Hat Enterprise #Linux 8.6 (Ootpa)" | GitHub  | Pass |  Both_Install_and_Test_Success
+#[root@5d71e9450069 /]#
