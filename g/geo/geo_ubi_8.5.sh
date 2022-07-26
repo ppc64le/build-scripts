08ced154774b79bf9b8014ab9527d274b64dcc02
sid226 <siddesh.sangodkar@ibm.com>
2022-07-26 14:40:24 +0530

    Added v740aa86cb551

--- g/geo/geo_ubi_8.5.sh
+++ g/geo/geo_ubi_8.5.sh
@@ -1,14 +1,13 @@
-#!/bin/bash -ex
 # -----------------------------------------------------------------------------
 #
-# Package       : geo
-# Version       : 5b978397cfec, master
-# Source repo   : https://github.com/golang/geo
-# Tested on     : UBI 8.5
+# Package	: github.com/golang/geo
+# Version	: v0.0.0-20210211234256-740aa86cb551
+# Source repo	: https://github.com/golang/geo
+# Tested on	: UBI 8.6
 # Language      : GO
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Prashant Khoje <prashant.khoje@ibm.com>
+# Maintainer	: Siddesh Sangodkar <siddesh226@gmail.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -18,37 +17,54 @@
 #
 # ----------------------------------------------------------------------------
 
-PACKAGE_NAME=geo
-# Defaults to PACKAGE_VERSION=v0.0.0-20190916061304-5b978397cfec
-# This script also works with master branch.
-PACKAGE_VERSION=${1:-5b978397cfec}
-PACKAGE_URL="https://github.com/golang/geo.git"
+PACKAGE_NAME=github.com/golang/geo
+PACKAGE_VERSION=v0.0.0-20210211234256-740aa86cb551
+PACKAGE_URL=https://github.com/golang/geo
+COMMIT_ID=740aa86cb551d6388f5cf4a8f39568d52fac6ed7
+CURDIR="$(pwd)"
 
-export GOPATH=$HOME/go
-dnf install -y golang git
+yum install -y git wget golang
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
-export SRC=$GOPATH/src/github.com/$PACKAGE_NAME
-mkdir -p $SRC
-cd $SRC
-git clone $PACKAGE_URL
-cd $PACKAGE_NAME
-git checkout $PACKAGE_VERSION
+# set GOPATH
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$PATH
 
-echo "\nFollowing tests fail due to floating point precision differences on ppc64le platform:"
-echo "\tTestClosestEdgeQueryTrueDistanceLessThanChordAngleDistance"
-echo "\tTestPointMeasuresPointArea"
-echo "\tTestPredicatesRobustSignEqualities\n"
 
-echo "Testing $PACKAGE_NAME with $PACKAGE_VERSION"
-if ! go test -v ./...; then
-    echo "------------------ $PACKAGE_NAME: test fail ---------------------"
-    echo "$PACKAGE_VERSION $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_VERSION | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Test fails"
-    exit 1
+# Install geo 
+mkdir -p $GOPATH/src/github.com/golang/
+cd $GOPATH/src/github.com/golang/
+git clone $PACKAGE_URL.git
+cd geo
+git checkout $COMMIT_ID
+
+#add testcase patch
+sed -i '71d' r3/vector.go
+sed -i "71i func (v Vector) Dot(ov Vector) float64 { return float64(v.X*ov.X) + float64(v.Y*ov.Y) + float64(v.Z*ov.Z) }"  r3/vector.go
+sed -i '76d' r3/vector.go
+sed -i "76i float64(v.Y*ov.Z) - float64(v.Z*ov.Y),"  r3/vector.go
+sed -i '77d' r3/vector.go
+sed -i "77i float64(v.Z*ov.X) - float64(v.X*ov.Z),"  r3/vector.go
+sed -i '78d' r3/vector.go
+sed -i "78i float64(v.X*ov.Y) - float64(v.Y*ov.X),"  r3/vector.go
+
+cd $GOPATH/src/$PACKAGE_NAME
+if ! go mod tidy ; then
+        echo "------------------$PACKAGE_NAME:initialize_fails-------------------------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Initialize_Fails"
+        exit 1
+fi
+
+if ! go test ./... ; then
+        echo "------------------$PACKAGE_NAME:test_fails---------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
+        exit 1
 else
-    echo "------------------ $PACKAGE_NAME: test success --------------------"
-    echo "$PACKAGE_VERSION $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_VERSION | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass | Test success"
+        echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass |  Install_and_Test_Success"
+        exit 0
 fi
