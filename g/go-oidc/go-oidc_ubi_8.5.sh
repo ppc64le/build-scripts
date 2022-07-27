09e078dbe4cf5b7a78c1f400dba5d0ebfe70e1c2
Balavva <Balavva.Mirji@ibm.com>
2022-07-27 15:56:47 +0530

    Update script for go-oidc

--- g/go-oidc/go-oidc_ubi_8.5.sh
+++ g/go-oidc/go-oidc_ubi_8.5.sh
@@ -8,7 +8,7 @@
 # Language      : GO
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: Reynold Vaz <Reynold.Vaz@ibm.com>
+# Maintainer	: Reynold Vaz <Reynold.Vaz@ibm.com>/ Balavva Mirji <Balavva.Mirji@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -24,6 +24,8 @@ PACKAGE_NAME=go-oidc
 PACKAGE_VERSION=${1:-v2.1.0}
 PACKAGE_URL=https://github.com/coreos/go-oidc.git
 
+PACKAGE_COMMIT_HASH=`echo $PACKAGE_VERSION | cut -d'-' -f3`
+
 yum install go git -y
 
 export GOPATH=/home/tester/go
@@ -32,22 +34,14 @@ OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
 mkdir -p $GOPATH/src/github.com/coreos && cd $GOPATH/src/github.com/coreos
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
-git checkout $PACKAGE_VERSION
-
-go get -u golang.org/x/lint/golint
-cp /home/tester/go/bin/golint /usr/bin
-go get -v -t github.com/coreos/go-oidc/...
-go get golang.org/x/tools/cmd/cover
+git checkout $PACKAGE_COMMIT_HASH
 
 go mod init
-go mod tidy
+#To upgrade to the versions selected by go 1.16
+go mod tidy -go=1.16 && go mod tidy -go=1.17
 
-if ! ./test ; then
-	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-	echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails" 
-	exit 1
-else
-	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-	echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
-	exit 0
-fi
+go install ./...
+go test -v ./...
+
+exit 0
+
