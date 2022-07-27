2ae4782c6bc8c6027339ac9dd9c873369b868bc9
rajusah15 <85694603+rajusah15@users.noreply.github.com>
2022-07-27 17:27:16 +0530

    Update drupal-metatag_ubi_8.5.sh
    modified Travis-check to False as unit test not available only Functional and kernel avail.

--- d/drupal-metatag/drupal-metatag_ubi_8.5.sh
+++ d/drupal-metatag/drupal-metatag_ubi_8.5.sh
@@ -6,7 +6,7 @@
 # Source repo      : https://git.uwaterloo.ca/drupal-org/metatag.git
 # Tested on        : UBI 8.5
 # Language         : PHP
-# Travis-Check     : True
+# Travis-Check     : False
 # Script License   : Apache License, Version 2 or later
 # Maintainer       : Raju Sah <Raju.Sah@ibm.com>
 #
