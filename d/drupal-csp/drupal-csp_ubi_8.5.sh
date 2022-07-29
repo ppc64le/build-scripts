5216fb477f34b93bae9833fbc363add8364b4390
abhishekdighePersIbm1 <109340088+abhishekdighePersIbm1@users.noreply.github.com>
2022-07-29 13:22:02 +0530

    Update drupal-csp_ubi_8.5.sh

--- d/drupal-csp/drupal-csp_ubi_8.5.sh
+++ d/drupal-csp/drupal-csp_ubi_8.5.sh
@@ -8,7 +8,7 @@
 # Language	: PHP
 # Travis-Check	: True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: Abhishek Dighe (Abhishek.Dighe@ibm.com)
+# Maintainer	: Abhishek Dighe <Abhishek.Dighe@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -20,21 +20,25 @@
 
 PACKAGE_NAME=csp
 CORE_PACKAGE_NAME=drupal
-PACKAGE_URL=https://git.drupalcode.org/project/csp.git/
-CORE_PACKAGE_URL=https://github.com/drupal/drupal.git/
+PACKAGE_URL=https://git.drupalcode.org/project/csp.git
+CORE_PACKAGE_URL=https://github.com/drupal/drupal.git
 PACKAGE_VERSION=${1:-8.x-1.15}
 
 yum module enable php:7.3 -y
 yum install php php-json php-devel zip unzip php-zip wget git php-pdo php-dom php-mbstring -y
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
 
-rm -rf $CORE_PACKAGE_NAME
+# Check if package exists
+if [ -d "$CORE_PACKAGE_NAME" ] ; then
+	rm -rf $CORE_PACKAGE_NAME
+	echo "$CORE_PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package"
+fi
 
 if ! git clone $CORE_PACKAGE_URL $CORE_PACKAGE_NAME; then
-    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-		echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
-        echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    	exit 0
+	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+	echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
+	echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+	exit 0
 fi
 
 cd $CORE_PACKAGE_NAME
@@ -43,15 +47,15 @@ cd core/modules
 
 #Check if package exists
 if [ -d "$PACKAGE_NAME" ] ; then
-  rm -rf $PACKAGE_NAME
-  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"   
+	rm -rf $PACKAGE_NAME
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"   
 fi
 
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
-    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-		echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    	exit 0
+	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+	exit 0
 fi
 
 cd $PACKAGE_NAME
