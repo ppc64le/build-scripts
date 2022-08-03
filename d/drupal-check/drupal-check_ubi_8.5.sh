2603a0feeef1ce0029a9b8fcc896272631b96908
ambujkumar3 <104371292+ambujkumar3@users.noreply.github.com>
2022-08-03 13:18:14 +0530

    Update drupal-check_ubi_8.5.sh

--- d/drupal-check/drupal-check_ubi_8.5.sh
+++ d/drupal-check/drupal-check_ubi_8.5.sh
@@ -18,41 +18,57 @@
 # ----------------------------------------------------------------------------
 # Variables
 PACKAGE_NAME=drupal-check
-
+CORE_PACKAGE_NAME=drupal
+CORE_PACKAGE_URL=https://github.com/drupal/drupal
 PACKAGE_URL=https://github.com/mglaman/drupal-check
 #PACKAGE_VERSION is configurable can be passed as an argument.
 PACKAGE_VERSION=${1:-1.4.0}
-yum install -y git php php-json php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
 
+yum module enable php:7.4 -y
+yum install -y git php php-json php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
 OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
 #Check if package exists
-if [ -d "$PACKAGE_NAME" ] ; then
-  rm -rf $PACKAGE_NAME
-  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
+if [ -d "$CORE_PACKAGE_NAME" ] ; then
+    rm -rf $CORE_PACKAGE_NAME
+    echo "$CORE_PACKAGE_NAMEE  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
 fi
-composer require --dev phpunit/phpunit --with-all-dependencies ^8  --no-interaction
-HOME_DIR=`pwd`
+
+if ! git clone $CORE_PACKAGE_URL $CORE_PACKAGE_NAME; then
+    echo "------------------$CORE_PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
+    echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    exit 0
+fi
+cd $CORE_PACKAGE_NAME
+git checkout 8.9.0
+rm -rf composer.lock
+composer config allow-plugins true --no-interaction
+composer require --dev phpunit/phpunit --with-all-dependencies ^8 --no-interaction
+composer require mglaman/drupal-check --dev
+
+if ! composer install --no-interaction; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+cd modules/
+
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
         echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
         exit 0
 fi
-rm -rf drupal
-composer create-project drupal-composer/drupal-project:8.x-dev drupal --no-interaction --stability=dev
-composer require mglaman/drupal-check --dev
-composer require nette/utils
-if ! composer install --no-interaction; then
-        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
-    exit 1
-fi
 
-cd drupal
-if ! php ../vendor/bin/drupal-check web/modules/; then
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+cd ../..
+cd core/
+
+if ! php ../vendor/bin/drupal-check ../modules/drupal-check/tests/; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
