7da5854c5899abfabd0338f436ff0b543b7cebbf
saraswati-patra <Saraswati.patra@ibm.com>
2022-08-16 18:31:13 +0530

    build script created for concat-with -sourcemap

---
+++ c/concat-with-sourcemaps/concat-with-sourcemaps_ubi_8.5.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -e
+
+# -----------------------------------------------------------------------------
+#
+# Package          : concat-with-sourcemaps
+# Version          : v1.0.7
+# Source repo      : https://github.com/floridoo/concat-with-sourcemaps
+# Tested on        : RHEL 8.5,UBI 8.5
+# Language         : Node
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Saraswati Patra <Saraswati.Patra@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#   
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=concat-with-sourcemaps
+#PACKAGE_VERSION is configurable can be passed as an argument.
+PACKAGE_VERSION=${1:-v1.0.7}
+PACKAGE_URL=https://github.com/floridoo/concat-with-sourcemaps.git
+yum install -y yum-utils git jq
+
+NODE_VERSION=v12.22.4
+#installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install $NODE_VERSION
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+#Check if package exists
+if [ -d "$PACKAGE_NAME" ] ; then
+  rm -rf $PACKAGE_NAME
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"  
+ 
+fi
+ 
+
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+        exit 1
+fi
+
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+if ! npm install && npm audit fix && npm audit fix --force; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! npm test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME" 
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 1
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
+
+#✓ should not allocate an uninitialized buffer when pass
+# tests 77
+# pass  77
+#✓ ok
+#------------------concat-with-sourcemaps:install_&_test-------------
+#https://github.com/floridoo/concat-with-sourcemaps.git
+#concat-with-sourcemaps  |  https://github.com/floridoo/t | v1.0.7 | "Red Hat Enterprise Linux 8.5 (Ootpa)" | Gtall_and_Test_Success
+#[root@99e5b2686e53 /]#
+
