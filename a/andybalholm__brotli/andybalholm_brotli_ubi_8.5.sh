23ae84498fc83d8b5b344f648ce56934119d4a16
HarithaPatchari054 <haritha.patchari@ibm.com>
2022-08-16 10:21:20 +0000

    Build Script for andybalholm__brotli:

---
+++ a/andybalholm__brotli/andybalholm_brotli_ubi_8.5.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -ex
+# -----------------------------------------------------------------------------
+#
+# Package       : brotli
+# Version       : 71eb68cc467c35a70c4b3d0c46b590260fe3f303
+# Source repo   : https://github.com/andybalholm/brotli
+# Tested on     : UBI 8.5
+# Language      : GO
+# Travis-Check  : True
+# Script License:  Apache License, Version 2 or later
+# Maintainer    : Haritha Patchari <haritha.patchari@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=brotli
+PACKAGE_VERSION=${1:-71eb68cc467c35a70c4b3d0c46b590260fe3f303}
+PACKAGE_URL=https://github.com/andybalholm/brotli
+
+
+
+# Install dependencies
+yum -y update && yum install git gcc make wget tar zip -y
+
+GO_VERSION=1.17
+
+# Install Go and setup working directory
+wget https://golang.org/dl/go$GO_VERSION.linux-ppc64le.tar.gz
+tar -C /bin -xf go$GO_VERSION.linux-ppc64le.tar.gz
+
+rm -f go$GO_VERSION.linux-ppc64le.tar.gz
+
+export PATH=$PATH:/bin/go/bin
+export GOPATH=/home/go
+
+mkdir -p $GOPATH/src && cd $GOPATH/src
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+go mod init
+go mod tidy
+
+echo "Building $PACKAGE_PATH$PACKAGE_NAME with $PACKAGE_VERSION"
+
+if ! go build -v ./...; then
+	echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+	exit 1
+fi
+echo "Testing $PACKAGE_PATH$PACKAGE_NAME with $PACKAGE_VERSION"
+
+if ! go test -v ./...; then
+	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+	exit 1
+else
+	echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Install_and_Test_Success"
+	exit 0
+fi
