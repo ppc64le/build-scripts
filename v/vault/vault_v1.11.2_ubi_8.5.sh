4326f1b2ea6d0a20c38f0addd9a2cd6c056cbbbd
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2022-08-18 13:09:47 +0530

    Update vault_v1.11.2_ubi_8.5.sh

--- v/vault/vault_v1.11.2_ubi_8.5.sh
+++ v/vault/vault_v1.11.2_ubi_8.5.sh
@@ -1,3 +1,23 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package	: github.com/hashicorp/vault
+# Version	: v1.11.2
+# Source repo	: https://github.com/hashicorp/vault
+# Tested on	: UBI: 8.5
+# Language      : Go
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Vinod K <Vinod.K1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
 PACKAGE_NAME=vault
 PACKAGE_VERSION=${1:-v1.11.2}
 PACKAGE_URL=https://github.com/hashicorp/vault.git
