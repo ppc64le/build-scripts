40ab4c8b715f6e21799cceae3b44ed3fe0342538
stutiibm <110608613+stutiibm@users.noreply.github.com>
2022-08-24 11:04:03 +0530

    Update vaultlib_v0.6.1.sh

--- v/vault-api/vaultlib_v0.6.1.sh
+++ v/vault-api/vaultlib_v0.6.1.sh
@@ -9,7 +9,7 @@
 # Language         : GO
 # Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
-# Maintainer       : Stuti Wali <Stuti.Wali@ibm.com>
+# Maintainer       : Stuti Wali 
 #
 # Disclaimer       : This script has been tested in root mode on given
 # ==========         platform using the mentioned version of the package.
