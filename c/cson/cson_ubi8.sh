636c626de2fa5a3125d1c46b9aea9ce567471c79
saraswati-patra <Saraswati.patra@ibm.com>
2022-08-24 12:21:02 +0530

    Modified build script  with requested version  for cson

--- c/cson/cson_ubi8.sh
+++ c/cson/cson_ubi8.sh
@@ -1,11 +1,14 @@
+#!/bin/bash -e
 # ----------------------------------------------------------------------------
 #
 # Package               : cson
-# Version               : 7.0.0
+# Version               : 7.0.0,6.9.0
 # Source repo           : https://github.com/bevry/cson
 # Tested on             : UBI 8
+# Language              : Node
+# Travis-Check          : True
 # Script License        : Apache License, Version 2 or later
-# Maintainer            : Swati Singhal <swati.singhal@ibm.com>
+# Maintainer            : Swati Singhal<swati.singhal@ibm.com>,Saraswati Patra <saraswati.patra@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -15,7 +18,6 @@
 #
 # ----------------------------------------------------------------------------
 
-#!/bin/bash
 PACKAGE_VERSION=7.0.0
 
 echo "Usage: $0 [-v <PACKAGE_VERSION>]"
@@ -40,3 +42,19 @@ git checkout v$PACKAGE_VERSION
 npm install
 #1 test failing
 npm test
+
+#build is passed but test is in parity for both mentioned version
+
+#internal/modules/cjs/loader.js:818
+#  throw err;
+#  ^
+
+#Error: Cannot find module '/cson/edition-esnext/test.js'
+#    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:815:15)
+#    at Function.Module._load (internal/modules/cjs/loader.js:667:27)
+#    at Function.executeUserEntryPoint [as runMain] (internal/modules/run_main.js:60:12)
+#    at internal/main/run_main_module.js:17:47 {
+#  code: 'MODULE_NOT_FOUND',
+#  requireStack: []
+#}
+#npm ERR! Test failed.  See above for more details.
