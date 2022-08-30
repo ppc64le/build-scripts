b8575a94a26818000d6b2f0d08d7f2ef7b4f6057
Shantanu8268 <shantanu.kadam@ibm.com>
2022-08-30 10:18:44 +0000

    pborman/uuid ca53cad383cad2479bbba7f7a1a05797ec1386e4 test and validation script

---
+++ p/pborman__uuid/pborman_uuid_ubi_8.5.sh
@@ -0,0 +1,84 @@
+#!/bin/bash -e
+
+# ----------------------------------------------------------------------------
+#
+# Package        : pborman/uuid
+# Version        : ca53cad383cad2479bbba7f7a1a05797ec1386e4
+# Source repo    : https://github.com/pborman/uuid
+# Tested on      : UBI 8.5
+# Language      : go
+# Travis-Check  : True
+# Script License : Apache License, Version 2 or later
+# Maintainer     : Shantanu Kadam <shantanu.kadam@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+set -e
+PACKAGE_NAME=${2:-uuid}
+PACKAGE_URL=https://github.com/pborman/uuid
+PACKAGE_VERSION=${1:-ca53cad383cad2479bbba7f7a1a05797ec1386e4}
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+PACKAGE_COMMIT_HASH=`echo $PACKAGE_VERSION | cut -d'-' -f3`
+
+#install dependencies
+yum install -y  go git gcc-c++ 
+
+#removed old data 
+#set GO PATH
+export PATH=$PATH:/bin/go/bin
+export GOPATH=/home/tester/go/
+
+#clone package
+mkdir -p $GOPATH/src/github.com/
+cd $GOPATH/src/github.com/
+
+if ! git clone $PACKAGE_URL; then
+    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    exit 1
+fi
+
+cd $PACKAGE_NAME
+git checkout $PACKAGE_COMMIT_HASH
+
+#patch
+sed -i '319b1; 328b1; 331b1; b ;:1;s/, s//g' uuid_test.go
+
+go mod init
+go mod tidy
+
+if ! go build -v ./...; then
+    echo "------------------$PACKAGE_NAME:Build_fails---------------------"
+    echo "$PACKAGE_VERSION $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+else
+    echo "------------------$PACKAGE_NAME:Build_success-------------------------"
+    echo "$PACKAGE_VERSION $PACKAGE_NAME"
+fi 
+
+if ! go test -v ./... ; then
+echo "------------------$PACKAGE_NAME:test_fails---------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
+	exit 1
+else
+	echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Pass |  Install_and_Test_Success"
+	exit 0
+fi
+
+bash
+EOF
+exit 0
+
+
+
