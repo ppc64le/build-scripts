53c6af435ff84d0691db714a6a628da94e715efc
ambujkumar3 <104371292+ambujkumar3@users.noreply.github.com>
2022-09-06 12:12:44 +0530

    Update drupal-entity_embed_ubi_8.5.sh

--- d/drupal-entity_embed/drupal-entity_embed_ubi_8.5.sh
+++ d/drupal-entity_embed/drupal-entity_embed_ubi_8.5.sh
@@ -24,27 +24,29 @@ CORE_PACKAGE_NAME=drupal
 PACKAGE_URL=https://git.drupalcode.org/project/entity_embed.git
 CORE_PACKAGE_URL=https://github.com/drupal/drupal
 PACKAGE_VERSION=${1:-8.x-1.1}
+CORE_PACKAGE_VERSION=${1:-8.9.0}
 
 yum module enable php:7.4 -y
 yum install -y git php php-dom php-mbstring zip unzip gd gd-devel php-gd php-pdo php-mysqlnd
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
 
 OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
 
 #Check if package exists
 if [ -d "$CORE_PACKAGE_NAME" ] ; then
 	rm -rf $CORE_PACKAGE_NAME
-	echo "$CORE_PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
+	echo "$CORE_PACKAGE_NAME  | $CORE_PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
 fi
 
 if ! git clone $CORE_PACKAGE_URL $CORE_PACKAGE_NAME; then
 	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
 	echo "$CORE_PACKAGE_URL $CORE_PACKAGE_NAME"
-	echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+	echo "$CORE_PACKAGE_NAME  |  $CORE_PACKAGE_URL |  $CORE_PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
 	exit 0
 fi
 
 cd $CORE_PACKAGE_NAME
-git checkout 8.9.0
+git checkout $CORE_PACKAGE_VERSION
 rm -rf composer.lock
 composer config allow-plugins true --no-interaction
 composer require --dev phpunit/phpunit --with-all-dependencies ^7 --no-interaction
