695059e8fd4d717f69f1d3d8a79ab0030eed7824
Kandarpa Malipeddi <69784217+kandarpamalipeddi@users.noreply.github.com>
2022-09-06 18:58:24 +0530

    Update aws-sdk-go_ubi_8.5.sh

--- a/aws-sdk-go/aws-sdk-go_ubi_8.5.sh
+++ a/aws-sdk-go/aws-sdk-go_ubi_8.5.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package	: aws-sdk-go
-# Version	: v1.44.89
+# Version	: v1.44.84, v1.44.89
 # Source repo	: https://github.com/aws/aws-sdk-go
 # Tested on	: UBI: 8.5
 # Language      : Go
@@ -19,7 +19,7 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=aws-sdk-go
-PACKAGE_VERSION=v1.44.84
+PACKAGE_VERSION=${1:-v1.44.89}
 PACKAGE_URL=https://github.com/aws/aws-sdk-go
 
 yum -y update && yum install -y nodejs nodejs-devel nodejs-packaging npm python38 python38-devel ncurses git jq wget gcc-c++ make
@@ -28,7 +28,6 @@ wget https://golang.org/dl/go1.16.1.linux-ppc64le.tar.gz
 tar -C /bin -xf go1.16.1.linux-ppc64le.tar.gz && mkdir -p /home/tester/go/src /home/tester/go/bin /home/tester/go/pkg
 
 export PATH=$PATH:/bin/go/bin
-OS_NAME=`python3 -c "os_file_data=open('/etc/os-release').readlines();os_info = [i.replace('PRETTY_NAME=','').strip() for i in os_file_data if i.startswith('PRETTY_NAME')];print(os_info[0])"`
 export PATH=$GOPATH/bin:$PATH
 export GO111MODULE=on
 
@@ -39,10 +38,10 @@ cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 if ! go build ./...; then
-	echo "------------------Build_Install_fails---------------------"
+	echo "------------------Build_fails---------------------"
 	exit 1
 else
-	echo "------------------Build_Install_success-------------------------"
+	echo "------------------Build_success-------------------------"
 	echo "$PACKAGE_NAME  | $PACKAGE_VERSION |"
 fi
 
