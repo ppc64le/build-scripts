23b96a4c6c49701c72c09546c8ce73c692d007b1
HarithaPatchari054 <haritha.patchari@ibm.com>
2022-09-06 10:14:00 +0000

    Build Script for counterfeiter:v6.2.2

---
+++ c/counterfeiter/counterfieter_ubi_8.5.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -ex
+# -----------------------------------------------------------------------------
+#
+# Package       : counterfeiter
+# Version       : v6.2.2
+# Source repo   : https://github.com/maxbrunsfeld/counterfeiter
+# Tested on     : UBI 8.5
+# Language      : GO
+# Travis-Check  : True
+# Script License:  Apache License, Version 2 or later
+# Maintainer    : Haritha Patchari <haritha.patchari@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=counterfeiter
+PACKAGE_VERSION=${1:-v6.2.2}
+PACKAGE_URL=https://github.com/maxbrunsfeld/counterfeiter
+
+# Install dependencies
+yum install -y make git wget gcc
+
+# Download and install go
+wget https://golang.org/dl/go1.17.5.linux-ppc64le.tar.gz
+tar -xzf go1.17.5.linux-ppc64le.tar.gz
+rm -rf go1.17.5.linux-ppc64le.tar.gz
+export GOPATH=`pwd`/gopath
+export PATH=`pwd`/go/bin:$GOPATH/bin:$PATH
+
+# Clone the repo and checkout submodules
+mkdir -p $GOPATH/src/$PACKAGE_PATH
+cd $GOPATH/src/$PACKAGE_PATH
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+go mod init
+go mod tidy
+
+echo "Building $PACKAGE_PATH$PACKAGE_NAME with $PACKAGE_VERSION"
+
+if ! go build -v ./...; then
+	echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+	exit 1
+fi
+echo "Testing $PACKAGE_PATH$PACKAGE_NAME with $PACKAGE_VERSION"
+
+if ! go test -v ./...; then
+	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+	exit 1
+else
+	echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Install_and_Test_Success"
+	exit 0
+fi
