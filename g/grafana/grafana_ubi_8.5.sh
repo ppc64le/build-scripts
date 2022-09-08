a772eb6cf1480e47d9880713be273c0d7b9c8b0f
vinodk99 <Vinod.K1@ibm.com>
2022-09-08 07:30:14 +0000

    Added build-script for grafana

---
+++ g/grafana/grafana_ubi_8.5.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+# Package          : grafana
+# Version          : v9.1.3
+# Source repo      : https://github.com/grafana/grafana.git
+# Tested on        : UBI 8.5
+# Language         : Go
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vinod K <Vinod.K1@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#   
+# ----------------------------------------------------------------------------
+yum -y update
+yum -y install wget git
+
+# Install GO
+wget https://golang.org/dl/go1.16.6.linux-ppc64le.tar.gz
+tar -C /usr/local -xzf go1.16.6.linux-ppc64le.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+#Install Node
+yum install -y openssl-devel.ppc64le curl
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install node
+
+#Build Grafana
+mkdir grafana
+cd grafana
+export GOPATH=`pwd`
+git clone https://github.com/grafana/grafana.git
+cd grafana
+git checkout v9.1.3
+go run build.go setup
+go run build.go build
+
+#Test Grafana
+go test -v ./pkg/...
