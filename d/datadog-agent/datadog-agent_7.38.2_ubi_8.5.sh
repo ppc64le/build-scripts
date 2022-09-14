5bcc36624c2b53323cb730c79a665eaecb12d450
vishakadesai <vishakandesai@gmail.com>
2022-09-14 23:02:29 +0530

    attempt to fix invoke error

--- d/datadog-agent/datadog-agent_7.38.2_ubi_8.5.sh
+++ d/datadog-agent/datadog-agent_7.38.2_ubi_8.5.sh
@@ -50,8 +50,9 @@ wget https://raw.githubusercontent.com/vishakadesai/build-scripts/dda/d/datadog-
 git apply datadog-agent_7.38.2.patch
 
 # Build and install dependencies
-python3 -m pip install codecov -r requirements.txt && \
-inv -e install-tools && \
+python3 -m pip install codecov -r requirements.txt 
+export PATH=$PATH:/usr/local/bin
+inv -e install-tools 
 invoke agent.build --build-exclude=systemd
 
 # To build rtloader
