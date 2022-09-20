c7e7c1f474e9b50ae457167afdf13f6a67d1816b
Siddesh-Sangodkar1 <siddesh.sangodkar1@ibm.com>
2022-09-20 09:49:06 +0530

    added fixes

--- j/jest-circus/jest-circus_ubi_8.6.sh
+++ j/jest-circus/jest-circus_ubi_8.6.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 # -----------------------------------------------------------------------------
 #
 # Package	: jest-circus
@@ -22,25 +22,34 @@ PACKAGE_NAME=jest-circus
 PACKAGE_VERSION=${1:-v27.0.5}
 PACKAGE_URL=https://github.com/facebook/jest
 
-dnf install -y wget git yum-utils nodejs nodejs-devel nodejs-packaging npm  
+dnf install -y wget git yum-utils nodejs nodejs-devel nodejs-packaging npm python3 make  gcc gcc-c++   
 
-curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
-rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
-dnf install -y yarn
+NODE_VERSION=16.14.2 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install $NODE_VERSION
+export PATH=/usr/local/bin:$PATH
+ln -s /usr/bin/python3 /bin/python
 
-#Install jest-circus
-npm install $PACKAGE_NAME@$PACKAGE_VERSION
+source ~/.bashrc
 
-#Run tests
+
+npm i --global yarn 
+yarn policies set-version  1.22.19
+
+export PATH=/root/.nvm/versions/node/v16.14.2/bin/:$PATH
+#Install and Run tests
 git clone $PACKAGE_URL jest
 cd jest 
 git checkout $PACKAGE_VERSION
-yarn add --dev jest@v27.0.5
-yarn add --dev jest-circus@v27.0.5
-yarn install
-yarn build
-
-
+yarn add --dev @jest/globals@27.0.5  -
+yarn add --dev @jest/test-utils@0.0.0  
+yarn add --dev jest@27.0.5 
+yarn add --dev jest-changed-files@27.0.2 
+yarn add --dev jest-mock@27.0.3 
+yarn add --dev jest-snapshot@27.0.5 
+yarn install 
+yarn build:js
 #Note that there are test failures which are in parity with Intel. Details are provided in the README file.
 
 if ! yarn jest ./packages/jest-circus/ 2>&1 | tee test.log ; then
