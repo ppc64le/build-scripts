645a909c9277525efcecb35891b9122a7d41c013
stutiibm <Stuti.Wali@ibm.com>
2022-09-22 09:06:17 +0000

    openldap build and validate script

---
+++ o/openldap/openldap_ubi_8_5.sh
@@ -0,0 +1,45 @@
+# ----------------------------------------------------------------------------
+#
+# Package       : openldap
+# Version       : OPENLDAP_REL_ENG_2_6_3
+# Source repo   : https://github.com/openldap/openldap
+# Tested on     : UBI: 8.5
+# Travis-Check  : True
+# Script License: Apache License 2.0
+# Maintainer's  : Stuti.Wali@ibm.com
+#
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+
+# Variables
+REPO=https://github.com/openldap/openldap
+
+# Default tag for openldap
+if [ -z "$1" ]; then
+  export VERSION="OPENLDAP_REL_ENG_2_6_3"
+else
+  export VERSION="$1"
+fi
+
+# install tools and dependent packages
+yum update -y
+yum install -y git make autoconf automake libtool gcc-c++
+
+# Cloning Repo
+git clone $REPO
+cd openldap
+git checkout ${VERSION}
+cd /openldap/libraries/liblmdb
+
+# Build and test package
+mkdir -p /usr/local/man/man1
+make
+make install
+make test
