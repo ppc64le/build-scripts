16c7c457966d9d468c397eb6552192bfbcbc5b7b
vinodk99 <Vinod.K1@ibm.com>
2022-09-29 10:43:39 +0000

    update the script

--- w/wildfly/wildfly_ubi_8.5.sh
+++ w/wildfly/wildfly_ubi_8.5.sh
@@ -1,3 +1,24 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+#
+# Package        : Wildfly
+# Version        : 27.0.0.Alpha5
+# Source repo    : https://github.com/wildfly/wildfly.git
+# Language       : Java
+# Travis-Check   : True
+# Tested on      : UBI 8.5
+# Script License : GNU Lesser General Public License Version 2.1
+# Maintainer     : Vindo K <Vinod.K1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+
 PACKAGE_NAME=wildfly
 PACKAGE_VERSION=${1:-27.0.0.Alpha5}
 PACKAGE_URL=https://github.com/wildfly/wildfly.git
