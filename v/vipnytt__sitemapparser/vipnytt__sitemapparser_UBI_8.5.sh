5abe4ba981d217215584941f6fb2f3c8ec63512b
Shalmontitre1 <Shalmon.Titre@ibm.com>
2022-10-13 10:41:00 +0000

    Added build script for vipnytt_sitemapparser

---
+++ v/vipnytt__sitemapparser/vipnytt__sitemapparser_UBI_8.5.sh
@@ -0,0 +1,89 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+# Package          : VIPnytt/SitemapParser
+# Version          : 1.1.4
+# Source repo      : https://github.com/VIPnytt/SitemapParser
+# Tested on        : UBI 8.5
+# Language         : PHP
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Shalmon Titre <Shalmon.Titre1@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#   
+# ----------------------------------------------------------------------------
+PACKAGE_NAME=SitemapParser
+PACKAGE_VERSION=1.1.4
+PACKAGE_URL=https://github.com/VIPnytt/SitemapParser.git
+
+yum -y update 
+yum module enable php:7.3 -y
+yum install -y nodejs nodejs-devel nodejs-packaging npm python38 python38-devel ncurses git jq curl php php-curl php-dom php-mbstring php-json nodejs make gcc-c++ patch diffutils php-gd php-pecl-zip
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
+yum install php-xdebug -y
+
+OS_NAME=`python3 -c "os_file_data=open('/etc/os-release').readlines();os_info = [i.replace('PRETTY_NAME=','').strip() for i in os_file_data if i.startswith('PRETTY_NAME')];print(os_info[0])"`
+HOME_DIR=`pwd`
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+		echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    	exit 0
+fi
+
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+sed -i '8 i \ \ \  <filter>' phpunit.xml
+sed -i '9 i \ \ \ \ \ \ \ \ \<whitelist>' phpunit.xml
+sed -i '10 i \ \ \ \ \ \ \ \ \ \ \ \ \<directory>src</directory>' phpunit.xml
+sed -i '11 i \ \ \ \ \ \ \ \ \</whitelist>' phpunit.xml
+sed -i '12 i \ \ \  </filter>' phpunit.xml
+
+sed -i '18s/.*/\        $this->assertTrue(is_array($parser->getSitemaps()));/' tests/DownloadTest.php
+sed -i '19s/.*/\        $this->assertTrue(is_array($parser->getURLs()));/' tests/DownloadTest.php
+sed -i '22s/.*/\        $this->assertTrue(is_string($url));/' tests/DownloadTest.php
+sed -i '23s/.*/\        $this->assertTrue(is_array($tags));/' tests/DownloadTest.php
+sed -i '28s/.*/\        $this->assertTrue(is_string($url));/' tests/DownloadTest.php
+sed -i '29s/.*/\        $this->assertTrue(is_array($tags));/' tests/DownloadTest.php
+
+sed -i '19s/.*/\        $this->assertTrue(is_array($parser->getSitemaps()));/' tests/RecursiveTest.php
+sed -i '20s/.*/\        $this->assertTrue(is_array($parser->getURLs()));/' tests/RecursiveTest.php
+sed -i '23s/.*/\        $this->assertTrue(is_string($url));/' tests/RecursiveTest.php
+sed -i '24s/.*/\        $this->assertTrue(is_array($tags));/' tests/RecursiveTest.php
+sed -i '29s/.*/\        $this->assertTrue(is_string($url));/' tests/RecursiveTest.php
+sed -i '30s/.*/\        $this->assertTrue(is_array($tags));/' tests/RecursiveTest.php
+
+sed -i '19s/.*/\        $this->assertTrue(is_array($parser->getSitemaps()));/' tests/StringTest.php
+sed -i '20s/.*/\        $this->assertTrue(is_array($parser->getURLs()));/' tests/StringTest.php
+sed -i '24s/.*/\        $this->assertTrue(is_string($url));/' tests/StringTest.php
+sed -i '25s/.*/\        $this->assertTrue(is_array($tags));/' tests/StringTest.php
+sed -i '30s/.*/\        $this->assertTrue(is_string($url));/' tests/StringTest.php
+sed -i '31s/.*/\        $this->assertTrue(is_array($tags));/' tests/StringTest.php
+
+
+if ! composer install; then
+     	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+	exit 0
+fi
+
+cd $HOME_DIR/$PACKAGE_NAME
+if ! vendor/bin/phpunit; then
+	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+	exit 0
+else
+	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+	exit 0
+fi
+
