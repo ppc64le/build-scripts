c5b7bd7f348d7f2f34f183aba0fc24c0bb8f9904
BhimraoPatil <Bhimrao.Patil@ibm.com>
2022-10-27 09:15:06 +0000

    symfony_lock Version  v3.4.37, v3.4.39, v3.4.47 test and validation script

--- l/lock/lock_ubi_8.5.sh
+++ l/lock/lock_ubi_8.5.sh
@@ -1,13 +1,14 @@
+#!/bin/bash -e
 # -----------------------------------------------------------------------------
 #
-# Package	: lock
-# Version	: v3.4.42
-# Source repo	: https://github.com/symfony/lock.git
-# Tested on	: ubi 8.5
+# Package       : lock
+# Version       : v3.4.37, v3.4.39, v3.4.42, v3.4.47
+# Source repo   : https://github.com/symfony/lock.git
+# Tested on     : ubi 8.5
 # Language      : PHP
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: Adilhusain Shaikh <Adilhusain.Shaikh@ibm.com>
+# Maintainer    : Adilhusain Shaikh <Adilhusain.Shaikh@ibm.com>, Bhimrao Patil <Bhimrao.Patil@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -19,13 +20,20 @@
 
 PACKAGE_NAME="lock"
 PACKAGE_URL="https://github.com/symfony/lock.git"
-PACKAGE_VERSION=${1:-"v3.4.42"}
+PACKAGE_VERSION=${1:-"v3.4.47"}
 OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
 
 yum install -y git curl php php-curl php-dom php-mbstring php-json php-gd php-pecl-zip
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
 
 HOME_DIR=$(pwd)
+
+#Check if package exists
+if [ -d "$PACKAGE_NAME" ] ; then
+  rm -rf $PACKAGE_NAME
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
+fi
+
 if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
@@ -55,3 +63,6 @@ else
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
     exit 0
 fi
+
+
+
