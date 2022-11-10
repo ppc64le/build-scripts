70683199b420b70b87fb04b26a4959fbc63538a6
vinodk99 <Vinod.K1@ibm.com>
2022-11-10 06:57:58 +0000

    Added build-script,Dockerfile and build_info for filebeat

---
+++ f/filebeat/filebeat_UBI_8.5.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+#
+# Package       : filebeat
+# Version       : 8.5.0
+# Source repo   : https://github.com/elastic/beats.git
+# Tested on     : UBI 8.5
+# Language      : GO
+# Travis-Check  : True
+# Script License: Apache License Version 2.0
+# Maintainer    : Vinod K <Vinod.K1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=filebeat
+PACKAGE_VERSION=v8.5.0
+PACKAGE_URL=https://github.com/elastic/beats.git
+
+WORKDIR=`pwd`
+
+#Install the required dependencies
+yum update -y
+yum install -y wget git make gcc-c++ python3-virtualenv
+
+cd $WORKDIR
+wget https://golang.org/dl/go1.17.linux-ppc64le.tar.gz
+tar -zxvf go1.17.linux-ppc64le.tar.gz
+
+export GOPATH=$WORKDIR/go
+export PATH=$PATH:$GOPATH/bin
+export CGO_ENABLED="0"
+
+#Clone and build the source
+mkdir -p ${GOPATH}/src/github.com/elastic
+cd ${GOPATH}/src/github.com/elastic
+git clone $PACKAGE_URL
+cd beats
+git checkout $BEATS_VERSION
+cd $PACKAGE_NAME
+make
+make unit
+
