a324cb5499ca560bf0b06c306be389bc9d0096a6
Shreya Kajbaje <shreya.kajbaje@ibm.com>
2022-11-23 15:08:40 +0530

    Add build script for global-state: v5.0.2

---
+++ s/sebastian__global-state/global-state_ubi_8.5.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+#
+# Package       : global-state
+# Version       : 5.0.2
+# Source repo   : https://github.com/sebastianbergmann/global-state
+# Tested on     : UBI: 8.5
+# Language      : Go
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Shreya Kajbaje <Shreya.Kajbaje@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+PACKAGE_NAME=global-state
+PACKAGE_VERSION=${1:-5.0.2}
+PACKAGE_URL=https://github.com/sebastianbergmann/global-state
+
+yum module enable php:7.4 -y
+yum install php php-json php-devel zip git -y
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && php composer-setup.php --install-dir=/bin --filename=composer
+php -v
+
+if ! git clone $PACKAGE_URL; then
+    	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+		echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    	exit 0
+fi
+
+cd $PACKAGE_NAME/
+git checkout $PACKAGE_VERSION
+
+if ! composer install; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+	exit 0
+fi
+
+cd $HOME_DIR/$PACKAGE_NAME
+if ! ./vendor/bin/phpunit; then
+	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+	exit 0
+else
+	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+	exit 0
+fi
