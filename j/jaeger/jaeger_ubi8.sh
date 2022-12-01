d7e8b4228ab73a225e1625e364e230a122a95c8f
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2022-12-01 14:17:45 +0530

    Update jaeger_ubi8.sh

--- j/jaeger/jaeger_ubi8.sh
+++ j/jaeger/jaeger_ubi8.sh
@@ -10,7 +10,7 @@
 # Script License: Apache License, Version 2 or later
 # Maintainer	: Vinod K <Vinod.K1@ibm.com>
 #
-# Disclaimer: This script has been tested in root mode on given
+# Disclaimer: This script has been tested in non-root mode on given
 # ==========  platform using the mentioned version of the package.
 #             It may not work as expected with newer versions of the
 #             package and/or distribution. In such case, please
