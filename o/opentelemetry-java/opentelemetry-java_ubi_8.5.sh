f798b99b755a04a90d4647a2453ec50be1701783
stutiibm <Stuti.Wali@ibm.com>
2023-01-03 05:08:11 +0000

    modified build script

--- o/opentelemetry-java/opentelemetry-java_ubi_8.5.sh
+++ o/opentelemetry-java/opentelemetry-java_ubi_8.5.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -ex
 # -----------------------------------------------------------------------------
 #
-# Package       : opentelemetry-java
-# Version       : v1.16.0, v1.21.0
-# Source repo   : https://github.com/open-telemetry/opentelemetry-java
-# Tested on     : ubi 8.5
+# Package	: opentelemetry-java
+# Version	: v1.16.0, v1.21.0
+# Source repo	: https://github.com/open-telemetry/opentelemetry-java
+# Tested on	: ubi 8.5
 # Language      : java
-# Travis-Check  : True
+# Travis-Check  : False
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Adilhusain Shaikh <Adilhusain.Shaikh@ibm.com>, Stuti Wali <Stuti.Wali@ibm.com>
+# Maintainer	: Adilhusain Shaikh <Adilhusain.Shaikh@ibm.com>, Stuti Wali <Stuti.Wali@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -29,10 +29,10 @@ dnf install -qy git make gcc-c++ java-17-openjdk-devel libtool file diffutils bc
 
 echo "cloning..."
 if ! git clone -q $PACKAGE_URL $PACKAGE_NAME; then
-        echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-        exit 1
+	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+	exit 1
 fi
 
 cd $PACKAGE_NAME
@@ -40,8 +40,8 @@ git checkout "$PACKAGE_VERSION"
 export GRPC_JAVA_VERSION=$(grep -m1 grpc-bom dependencyManagement/build.gradle.kts | cut -f3 -d ":" | sed "s/\",//")
 export PROTOBUF_VERSION=$(grep -m1 protobuf-bom dependencyManagement/build.gradle.kts | cut -f3 -d ":" | sed "s/\",//")
 
-sed -i "s/mavenLocal/mavenCentral/" settings.gradle.kts                                                                                                      
-sed -i "0,/mavenCentral/s/mavenCentral/mavenLocal/" settings.gradle.kts                                                                                      
+sed -i "s/mavenLocal/mavenCentral/" settings.gradle.kts                                                                                                                  
+sed -i "0,/mavenCentral/s/mavenCentral/mavenLocal/" settings.gradle.kts                                                                                                                  
 
 # building  and installing  protobuf from source
 cd "$HOME_DIR"
@@ -63,21 +63,20 @@ sed -i "s/checkArch \"\$FILE\"/#checkArch \"\$FILE\"/" ./compiler/check-artifact
 
 cd "$HOME_DIR"/$PACKAGE_NAME
 if ! ./gradlew build; then
-        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
-        exit 1
+	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+	exit 1
 fi
 
 if ! ./gradlew test; then
-        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
-        exit 1
+	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+	exit 1
 else
-        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
-        exit 0
+	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+	exit 0
 fi
-
