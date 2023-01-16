3988d959bf7614cd22451817dc49bbfdc8c0c353
Shantanu8268 <Shantanu.Kadam@ibm.com>
2023-01-16 07:04:59 +0000

    Added build script for kiali@v1.62.0

---
+++ k/kiali/kiali_v1_62_0_ubi_8_5.sh
@@ -0,0 +1,63 @@
+#!/bin/bash -e
+
+# ----------------------------------------------------------------------------
+#
+# Package	: kiali
+# Version	: v1.62.0
+# Source repo	: https://github.com/kiali/kiali
+# Tested on	: UBI 8.5
+# Script License: Apache License, Version 2 or later
+# Maintainer	: Shantanu Kadam <Shantanu.Kadam@ibm.com>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+
+# Install dependencies
+yum update -y
+yum install -y gcc-c++
+yum install -y make
+yum install -y python36
+yum install -y wget git
+yum install -y tar zip
+yum install -y npm
+npm install -g yarn
+
+BUILD_VERSION=v1.62.0
+
+# go setup
+wget https://go.dev/dl/go1.18.7.linux-ppc64le.tar.gz
+tar -C /usr/local -xzf go1.18.7.linux-ppc64le.tar.gz
+export GOROOT=/usr/local/go/
+export GOPATH=$HOME
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+# node setup
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 14
+nvm use 14
+
+# Clone git repository
+git clone https://github.com/kiali/kiali.git
+cd kiali/
+git checkout $BUILD_VERSION
+
+make lint-install
+make lint
+
+# Build and test
+
+##build: Runs `make go-check` internally and build Kiali binary
+## test: Run tests, excluding third party tests under vendor and frontend
+make clean build test
+
+make clean-ui build-ui build-ui-test
+
