789a0b6ef2e93483079ca019e7bd6b5dc04bf4c4
kandarpakumar-malip <kandarpakumar.malip@hcl.com>
2023-01-16 10:19:04 +0530

    Sample script changes for testing

--- a/abraham__twitteroauth/abraham__twitteroauth_rhel_8.3.sh
+++ a/abraham__twitteroauth/abraham__twitteroauth_rhel_8.3.sh
@@ -4,6 +4,8 @@
 # Version	: 0.9
 # Source repo	: https://github.com/abraham/twitteroauth
 # Tested on	: RHEL 8.3
+# Language      : Node
+# Travis-Check  : True
 # Script License: Apache License, Version 2 or later
 # Maintainer	: BulkPackageSearch Automation <sethp@us.ibm.com>
 #
@@ -16,7 +18,7 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=abraham/twitteroauth
-PACKAGE_VERSION=0.9
+PACKAGE_VERSION=${1:-'0.9'}
 PACKAGE_URL=https://github.com/abraham/twitteroauth
 
 yum -y update && yum install -y nodejs nodejs-devel nodejs-packaging npm python38 python38-devel ncurses git jq curl php php-curl php-dom php-mbstring php-json nodejs make gcc-c++ patch diffutils php-gd php-pecl-zip
@@ -31,7 +33,7 @@ if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
     	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
 		echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    	exit 0
+    	exit 1
 fi
 
 cd $HOME_DIR/$PACKAGE_NAME
@@ -40,7 +42,7 @@ if ! composer install; then
      	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
-	exit 0
+	exit 1
 fi
 
 cd $HOME_DIR/$PACKAGE_NAME
@@ -48,7 +50,7 @@ if ! /home/tester/vendor/bin/phpunit; then
 	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
-	exit 0
+	exit 2
 else
 	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
