53e10450237a6f082352552c0e8a83cfecd200f0
vishakadesai <vishakandesai@gmail.com>
2023-01-18 12:53:23 +0530

    commenting out test run command

--- o/odoo/odoo_16.0_ubi_8.5.sh
+++ o/odoo/odoo_16.0_ubi_8.5.sh
@@ -54,7 +54,7 @@ python3.10 -m pip install pip wheel
 python3.10 -m pip install -r requirements.txt
 
 # Run test suite, refer README
-python3.10 odoo-bin -d mydb -r user1 -w pass --db_host 172.21.0.2 --db_port 5432 -i base --stop-after-init --log-level=test --max-cron-threads=0
+# python3.10 odoo-bin -d mydb -r user1 -w pass --db_host 172.21.0.2 --db_port 5432 -i base --stop-after-init --log-level=test --max-cron-threads=0
 
 # Start Odoo server, refer README
 # python3.10 odoo-bin -d mydb -r odoo -w odoo --db_host 172.21.0.2 --db_port 5432 -i INIT
