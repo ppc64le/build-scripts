08fc26df9ba2933b3fbaabe83952d8eb4f662415
HarithaPatchari054 <107609563+HarithaPatchari054@users.noreply.github.com>
2023-01-18 15:21:55 +0530

    Update apache-tomcat_ubi_8.5.sh

--- a/apache-tomcat/apache-tomcat_ubi_8.5.sh
+++ a/apache-tomcat/apache-tomcat_ubi_8.5.sh
@@ -22,36 +22,22 @@
 PACKAGE_NAME=https://github.com/apache/tomcat.git
 PACKAGE_VERSION=${1:-v11.0.0-M1}
 PACKAGE_URL=https://github.com/apache/tomcat.git
-
 cd ${HOME}
 yum update -y
 TOMCAT_VERSION="11.0.0"
-
 yum install -y git wget
 yum install -y java-11-openjdk-devel
-
-## Installing Apache-Ant
-
 wget http://mirror.downloadvn.com/apache/ant/binaries/apache-ant-1.10.12-bin.tar.gz
 tar -xf apache-ant-1.10.12-bin.tar.gz
 export ANT_HOME=${HOME}/apache-ant-1.10.12/
 export PATH=${PATH}:${ANT_HOME}/bin
-
-## Configuring Tomcat build
-
 git clone https://github.com/apache/tomcat.git
 cd tomcat
 git checkout 11.0.0-M1
 yes | cp build.properties.default build.properties
 echo >> build.properties
 echo "skip.installer=true" >> build.properties
-
-## Building tomcat server
-
 ant release
-
-## Testing Tomcat
-
 export CATALINA_HOME=${HOME}/tomcat/output/dist
 export PATH=${HOME}/tomcat/output/dist/bin:${PATH}
 catalina.sh start &
@@ -59,6 +45,10 @@ catalina.sh run &
 
 sleep 30
 
+#netstat -tulnp | grep 8005
+#netstat -tulnp | grep 8080
+
 curl localhost:8080
+#curl localhost:8005
 
 catalina.sh stop
