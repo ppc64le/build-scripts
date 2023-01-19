15e873bda18c52c4c8beb82b3c5cd51087aaf244
Pratikt2312 <Pratik.Tonage@ibm.com>
2023-01-19 09:35:47 +0000

    Updated the build-script and added build info for grails-core

--- g/grails/grails_ubi_8.5.sh
+++ g/grails/grails_ubi_8.5.sh
@@ -1,12 +1,12 @@
 # -----------------------------------------------------------------------------
-#
 # Package	: grails
-# Version	: v5.0.2
+# Version	: v5.0.2,v5.2.5
 # Source repo	: https://github.com/grails/grails-core.git
 # Tested on	: ubi 8.5
-# Language	: JAVA
+# Language	: Java
+# Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: Sapna Shukla <Sapna.Shukla@ibm.com>
+# Maintainer	: Sapna Shukla <Sapna.Shukla@ibm.com>,Pratik Tonage <Pratik.Tonage@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -18,12 +18,13 @@
 
 HOME_DIR=`pwd`
 PACKAGE_NAME=grails-core
-PACKAGE_VERSION=${1:-v5.0.2}
+PACKAGE_VERSION=${1:-v5.2.5}
 PACKAGE_URL=https://github.com/grails/grails-core.git
 mkdir -p /home/tester
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
+# Install required dependencies
 yum install git unzip wget maven -y
 
 #clone the repository
@@ -32,13 +33,15 @@ git clone $PACKAGE_URL
 export WORK_DIR=$HOME_DIR/$PACKAGE_NAME
 cd $WORK_DIR
 git checkout $PACKAGE_VERSION
-	
+
+#Build	
 if ! ./gradlew install; then
 	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
 	echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
 	exit 1
 fi
 
+#Test
 if ! ./gradlew test; then
 	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
