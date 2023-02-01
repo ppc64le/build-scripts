936d630ef90cbe99e73dd0b57498eb8e5d415e3d
Shreya Kajbaje <shreya.kajbaje@ibm.com>
2023-02-01 13:44:26 +0530

    Add build script for wildfly-operator: v0.5.6

---
+++ w/wildfly-operator/wildfly_operator_ubi_8.5.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : wildfly-operator
+# Version       : 0.5.6
+# Source repo   : https://github.com/wildfly/wildfly-operator/
+# Tested on     : UBI 8.6
+# Language      : GO
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Shreya Kajbaje <Shreya.Kajbaje@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=wildfly-operator
+PACKAGE_VERSION=${1:-0.5.6}
+PACKAGE_URL=https://github.com/wildfly/wildfly-operator.git
+
+yum install -y git gcc wget make
+
+GO_VERSION=${GO_VERSION:-1.19.4}
+
+wget https://golang.org/dl/go$GO_VERSION.linux-ppc64le.tar.gz
+tar -C /bin -xf go$GO_VERSION.linux-ppc64le.tar.gz
+
+rm -f go$GO_VERSION.linux-ppc64le.tar.gz
+
+export PATH=$PATH:/bin/go/bin
+export GOPATH=/home/go
+
+mkdir -p $GOPATH/src && cd $GOPATH/src
+
+git clone https://github.com/wildfly/wildfly-operator.git $GOPATH/src/github.com/wildfly/wildfly-operator
+
+cd $GOPATH/src/github.com/wildfly/wildfly-operator
+
+git checkout 0.5.6
+
+make build
