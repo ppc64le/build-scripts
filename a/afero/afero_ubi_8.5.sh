91cf6b49c516a6638be6176684841be068978736
vishakadesai <vishakandesai@gmail.com>
2023-02-07 15:06:00 +0530

    edited package name

--- a/afero/afero_ubi_8.5.sh
+++ a/afero/afero_ubi_8.5.sh
@@ -19,14 +19,14 @@
 # ------------------------------------------------------------------------------
 
 PACKAGE_URL=https://github.com/spf13/afero
-PACKAGE_NAME=github.com/spf13/afero
+PACKAGE_NAME=afero
 PACKAGE_VERSION=${1:-v1.9.3}
 
 export GOPATH=$ROOT/go
 mkdir $GOPATH
 yum install -y golang git
 
-git clone $PACKAGE_URL $PACKAGE_NAME
+git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 
 #Build and test the package
