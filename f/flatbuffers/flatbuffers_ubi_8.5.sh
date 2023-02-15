42584e3eb394da2ca2c55744ec2f5de047907eae
Pratikt2312 <Pratik.Tonage@ibm.com>
2023-02-15 08:22:02 +0000

    Currency:Updated build-script and added build_info for flatbuffers

--- f/flatbuffers/flatbuffers_ubi_8.5.sh
+++ f/flatbuffers/flatbuffers_ubi_8.5.sh
@@ -2,13 +2,13 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : flatbuffers
-# Version       : v1.12.0
+# Version       : v1.12.0,v23.1.21
 # Source repo   : https://github.com/google/flatbuffers.git
 # Tested on     : UBI 8.5
 # Language      : C++
 # Travis-Check  : true
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Ankit.Paraskar@ibm.com
+# Maintainer    : Ankit.Paraskar@ibm.com, Pratik Tonage <Pratik.Tonage@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -19,13 +19,15 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=flatbuffers
-PACKAGE_VERSION=v1.12.0
+PACKAGE_VERSION=v23.1.21
 PACKAGE_URL=https://github.com/google/flatbuffers.git
 
+# Install required dependencies
 yum install -y git wget gcc-c++ cmake
 
 rm -rf $PACKAGE_NAME
 
+# Cloning the repository
 git clone $PACKAGE_URL
 
 cd $PACKAGE_NAME
@@ -34,6 +36,7 @@ git checkout $PACKAGE_VERSION
 
 cmake ./
 
+# Build and test
 if ! (make && make test) ; then
                         echo "------------------$PACKAGE_NAME:build or test fail---------------------"
                         echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | master  | $OS_NAME | GitHub | Fail |  build or test fail"
@@ -44,3 +47,5 @@ if ! (make && make test) ; then
                         exit 0
                 fi
 
+# We can also test with following command
+# ./flattests
