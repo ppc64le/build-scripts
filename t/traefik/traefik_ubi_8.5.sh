18f4cf2d5bb047d64a888b0380ae624dea9b71f6
vinodk99 <Vinod.K1@ibm.com>
2023-02-16 05:44:03 +0000

    Currency:Added build-script,Dockerfile and build_info for traefik

---
+++ t/traefik/traefik_ubi_8.5.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+#
+# Package	: Traefik
+# Version	: v2.9.6
+# Source repo	: https://github.com/traefik/traefik
+# Tested on	: UBI 8.5
+# Language   	: Go
+# Travis-Check  : True
+# Script License: Apache License 2.0 or later
+# Maintainer	: Vinod K <Vinod.K1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+yum install -y gcc-c++ make wget git tar
+
+wget https://go.dev/dl/go1.19.3.linux-ppc64le.tar.gz
+rm -rf /usr/local/go
+tar -C /usr/local -xzf  go1.19.3.linux-ppc64le.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+git clone https://github.com/traefik/traefik
+cd traefik
+git checkout v2.9.6
+
+if ! go build -v ./... ; then
+       echo "------------------$PACKAGE_NAME:Build_fails---------------------"
+       echo "$PACKAGE_VERSION $PACKAGE_NAME"
+       echo "$PACKAGE_NAME  | $PACKAGE_VERSION |  GitHub | Fail |  Build_Fails"
+       exit 1
+fi
+
+sed -i '142d' pkg/middlewares/ratelimiter/rate_limiter_test.go
+sed -i '142 i\      incomingLoad: 200,' pkg/middlewares/ratelimiter/rate_limiter_test.go
+sed -i '217d' pkg/middlewares/ratelimiter/rate_limiter_test.go
+sed -i '217 i\      Period:  ptypes.Duration(10 * time.Second),' pkg/middlewares/ratelimiter/rate_limiter_test.go
+
+if ! go test ./... ; then
+      echo "------------------$PACKAGE_NAME::Build_and_Test_fails-------------------------"
+      echo "$PACKAGE_URL $PACKAGE_NAME"
+      echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Fail|  Build_and_Test_fails"
+      exit 2
+else
+      echo "------------------$PACKAGE_NAME::Build_and_Test_success-------------------------"
+      echo "$PACKAGE_URL $PACKAGE_NAME"
+      echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Build_and_Test_Success"
+      exit 0
+fi
