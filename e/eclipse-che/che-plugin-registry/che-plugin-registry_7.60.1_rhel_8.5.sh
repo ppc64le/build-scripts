31e0323e61d1a33a6f30145b9cf9743509a3a2e4
shubham-bhagwat0 <118804486+shubham-bhagwat0@users.noreply.github.com>
2023-02-16 17:37:03 +0530

    Update che-plugin-registry_7.60.1_rhel_8.5.sh

--- e/eclipse-che/che-plugin-registry/che-plugin-registry_7.60.1_rhel_8.5.sh
+++ e/eclipse-che/che-plugin-registry/che-plugin-registry_7.60.1_rhel_8.5.sh
@@ -40,43 +40,11 @@ echo "Installing additional dependencies..."
 yum install -y python39
 export PYTHONPATH=/usr/local/bin/python3
 
-echo "cloning the repository..."
-#Check if package exists
-if [ -d "$PACKAGE_NAME" ] ; then
-    rm -rf $PACKAGE_NAME
-    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
-fi
-
-if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
-    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail |  Install_Fails"
-    exit 0
-fi
-
+git clone $PACKAGE_URL $PACKAGE_NAME; 
 cd $PACKAGE_NAME/
 git checkout $PACKAGE_VERSION
 
 sed -i 's/x86_64/ppc64le/g' build/dockerfiles/Dockerfile
 sed -i 's/x64/ppc64le/g' build/dockerfiles/import-vsix.sh
 
-echo "building..."
-if ! ./build.sh -t 7.60.1-rhel; then
-    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-    echo "$PACKAGE_VERSION $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-    echo "------------------Second execution Re-run for flaky test case ---------------------"    
-
-    if ! ./build.sh -t 7.60.1-rhel; then
-        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-        exit 1
-    else    
-        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-        exit 0
-    fi                
-fi
-
-
+./build.sh -t 7.60.1-rhel
