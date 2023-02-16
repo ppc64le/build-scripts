b1f8d8dd0595a4df5433d6482ec861843502fac9
shubham-bhagwat0 <118804486+shubham-bhagwat0@users.noreply.github.com>
2023-02-16 14:32:21 +0530

    Update che-plugin-registry_7.60.1_rhel_8.5.sh

--- e/eclipse-che/che-plugin-registry/che-plugin-registry_7.60.1_rhel_8.5.sh
+++ e/eclipse-che/che-plugin-registry/che-plugin-registry_7.60.1_rhel_8.5.sh
@@ -61,23 +61,4 @@ sed -i 's/x86_64/ppc64le/g' build/dockerfiles/Dockerfile
 sed -i 's/x64/ppc64le/g' build/dockerfiles/import-vsix.sh
 
 echo "building..."
-if ! ./build.sh -t 7.60.1-rhel; then
-    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-    echo "$PACKAGE_VERSION $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-    echo "------------------Second execution Re-run for flaky test case ---------------------"    
-
-    if ! ./build.sh -t 7.60.1-rhel; then
-        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-        exit 1
-    else    
-        echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-        exit 0
-    fi                
-fi
-
-
-
+./build.sh -t 7.60.1-rhel
