9c3a85d04294cde5c529227e7ef9bf52a3c6f707
shubham-bhagwat0 <118804486+shubham-bhagwat0@users.noreply.github.com>
2023-02-17 10:41:57 +0530

    Update che-plugin-registry_7.60.1_rhel_8.5.sh

--- e/eclipse-che/che-plugin-registry/che-plugin-registry_7.60.1_rhel_8.5.sh
+++ e/eclipse-che/che-plugin-registry/che-plugin-registry_7.60.1_rhel_8.5.sh
@@ -30,6 +30,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 yum install npm -y
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "configuring npm and nvm..."
 npm install -g -y yarn
