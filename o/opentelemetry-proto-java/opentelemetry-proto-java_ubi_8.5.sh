b2a2a9e5fa7ef731f0e1a535c0a49e73b8305b5b
stutiibm <Stuti.Wali@ibm.com>
2023-03-20 08:31:10 +0000

    Currency:updated build-info.json

--- o/opentelemetry-proto-java/opentelemetry-proto-java_ubi_8.5.sh
+++ o/opentelemetry-proto-java/opentelemetry-proto-java_ubi_8.5.sh
@@ -18,52 +18,62 @@
 #
 # ----------------------------------------------------------------------------
 
+set -e
 PACKAGE_NAME="opentelemetry-proto-java"
-PACKAGE_VERSION=${1:-"0.19.0"}
 PACKAGE_URL="https://github.com/open-telemetry/opentelemetry-proto-java"
 OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
 HOME_DIR=$PWD
 
-echo "insstalling dependencies from system repo..."
-dnf install -qy git make gcc-c++ java-17-openjdk-devel libtool file diffutils bc
-
-echo "cloning..."
-if ! git clone -q $PACKAGE_URL $PACKAGE_NAME; then
-	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-	echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-	exit 1
+# Default tag opentelemetry-proto-java
+if [ -z "$1" ]; then
+  export PACKAGE_VERSION="0.19.0"
+else
+  export PACKAGE_VERSION="$1"
 fi
 
+#installing required dependencies
+echo "installing dependencies from system repo..."
+dnf install -y git make gcc gcc-c++ java-17-openjdk-devel.ppc64le libtool file diffutils bc wget
+export JAVA_HOME=/usr/lib/jvm/$(ls /usr/lib/jvm/ | grep -P '^(?=.*java-17)(?=.*ppc64le)')
+export PATH=$JAVA_HOME/bin:$PATH
+export GRPC_JAVA_VERSION="1.53.0"
+export PROTOBUF_VERSION="3.19.4"
+
+git clone $PACKAGE_URL $PACKAGE_NAME
 cd $PACKAGE_NAME
-git checkout v"$PACKAGE_VERSION"
-export GRPC_JAVA_VERSION=$(grep -m1 grpcVersion build.gradle.kts | cut -d "=" -f 2 | sed "s/\"//g" | sed "s/\s//g")
-export PROTOBUF_VERSION=$(grep -m1 protobufVersion build.gradle.kts | cut -d "=" -f 2 | sed "s/\"//g" | sed "s/\s//g")
-IFS=. read -r major minor patch < <(echo "$GRPC_JAVA_VERSION")
-if [ $(echo "$major.$minor<=1.44" | bc -l) -eq 1 ]; then
-	sed -i "s/\(grpcVersion = \).*/\1\"1\.47\.0\"/" build.gradle.kts
-	export GRPC_JAVA_VERSION="1.47.0"
-fi
+git checkout "v$PACKAGE_VERSION"
 
+sed -i "s/\(grpcVersion = \).*/\1\"1\.53\.0\"/" build.gradle.kts
 sed -i "s/mavenLocal/mavenCentral/" build.gradle.kts
 sed -i "0,/mavenCentral/s/mavenCentral/mavenLocal/" build.gradle.kts
 
+
 # building  and installing  protobuf from source
 cd "$HOME_DIR"
-git clone -q https://github.com/protocolbuffers/protobuf
+git clone  https://github.com/protocolbuffers/protobuf
 cd protobuf
 git checkout v"$PROTOBUF_VERSION"
 git submodule update --init
 ./autogen.sh
 ./configure
-make -j"$(nproc)" && make install
+make -j"$(nproc)" 
+make install
 
 #building and installing grpc-java
 cd "$HOME_DIR"
-git clone -q https://github.com/grpc/grpc-java
+git clone https://github.com/grpc/grpc-java
 cd grpc-java
 git checkout v"$GRPC_JAVA_VERSION"
-sed -i "s/checkArch \"\$FILE\"/#checkArch \"\$FILE\"/" ./compiler/check-artifact.sh
+
+sed -i "s#powerpc64le-linux-gnu-##g" ./compiler/build.gradle ./compiler/check-artifact.sh ./buildscripts/kokoro/linux_artifacts.sh ./buildscripts/make_dependencies.sh
+sed -i 's#white_list="linux-vdso64\\.so\\.1\\|libpthread\\.so\\.0\\|libm\\.so\\.6\\|libc\\.so\\.6\\|ld64\\.so\\.2"#white_list="linux-vdso64\\.so\\.1\\|libpthread\\.so\\.0\\|libm\\.so\\.6\\|libc\\.so\\.6\\|ld64\\.so\\.2\\|libstdc++\\.so\\.6\\|libgcc_s\\.so\\.1"#g' ./compiler/check-artifact.sh
+
+sed -i "s#https://developers.google.com/protocol-buffers/docs/reference/java/#https://protobuf.dev/reference/java/api-docs/#g" ./protobuf/build.gradle
+
+echo "org.gradle.daemon=true" >> gradle.properties
+echo "org.gradle.configureondemand=true" >> gradle.properties
+echo "org.gradle.jvmargs=-Xmx4g -XX:MetaspaceSize=2048m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8" >> gradle.properties
+
 ./gradlew publishToMavenLocal -PskipAndroid=true
 
 cd "$HOME_DIR"/$PACKAGE_NAME
@@ -78,7 +88,7 @@ if ! ./gradlew -Prelease.version="$PACKAGE_VERSION" test; then
 	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
-	exit 1
+	exit 2
 else
 	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
