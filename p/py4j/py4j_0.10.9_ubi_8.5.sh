6a5a2dfe5b2e522c3a38a68b38ac0b98e35a3db4
shubham-garud <118799785+shubham-garud@users.noreply.github.com>
2023-03-22 15:51:50 +0530

    Currency : Adding build_script for py4j:0.10.9 (#2982)

---
+++ p/py4j/py4j_0.10.9_ubi_8.5.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -e
+#----------------------------------------------------------------------------
+#
+# Package          : py4j
+# Version          : 0.10.9
+# Source repo      : https://github.com/py4j/py4j
+# Tested on        : UBI 8.5
+# Language         : Java,python
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Shubham Garud <Shubham.Garud@ibm.com>
+#
+# Disclaimer       : This script has been tested in non-root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+PACKAGE_NAME=py4j
+PACKAGE_VERSION=${1:-0.10.9}
+PACKAGE_URL=https://github.com/py4j/py4j
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+yum install -y git wget gcc java-1.8.0-openjdk-devel python38 python38-pip python38-devel python27
+
+pip3 install pytest tox
+
+wget https://archive.apache.org/dist/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
+tar -xvzf apache-maven-3.8.6-bin.tar.gz
+cp -R apache-maven-3.8.6 /usr/local
+ln -s /usr/local/apache-maven-3.8.6/bin/mvn /usr/bin/mvn
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+cd py4j-java
+
+if ! mvn install; then
+echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+        exit 1
+fi
+if ! mvn test; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+        exit 2
+fi
+
+cd ..
+cd py4j-python/
+
+if ! pip3 install -r requirements-test.txt; then
+     echo "------------------$PACKAGE_NAME:test-requirements install_fails-------------------------------------"
+     echo "$PACKAGE_URL $PACKAGE_NAME"
+     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+     exit 1
+fi
+
+
+if ! pytest -k "not java_tls_test. and not java_gateway_test."; then
+     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+     echo "$PACKAGE_URL $PACKAGE_NAME"
+     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+     exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
+
