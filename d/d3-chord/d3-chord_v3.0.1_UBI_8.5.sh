254e783bd2a99543f09a4c64a780d35d00bd6a19
Shubham Garud <Shubham.Garud@ibm.com>
2023-03-24 14:50:03 +0000

    Updated Node version var

--- d/d3-chord/d3-chord_v3.0.1_UBI_8.5.sh
+++ d/d3-chord/d3-chord_v3.0.1_UBI_8.5.sh
@@ -22,14 +22,25 @@ PACKAGE_NAME=d3-chord
 PACKAGE_VERSION=${1:-v3.0.1}
 PACKAGE_URL=https://github.com/d3/d3-chord
 
-yum install -y yum-utils git wget openssl-devel python2 nodejs nodejs-devel nodejs-packaging npm python38 python38-devel ncurses gcc gcc-c++ libffi libffi-devel jq
-npm install n -g && n latest && npm install -g npm@latest && export PATH="$PATH" && npm install --global yarn grunt-bump xo testem acorn
+export NODE_VERSION=${NODE_VERSION:-16}
+
+# Install dependencies
+yum install -y python38 python38-devel git gcc gcc-c++ libffi make
+
+#Installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source "$HOME"/.bashrc
+echo "installing nodejs $NODE_VERSION"
+nvm install "$NODE_VERSION" >/dev/null
+nvm use $NODE_VERSION
+
+
 
 git clone $PACKAGE_URL
 cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-if ! npm install && npm audit fix && npm audit fix --force; then
+if !(  npm install && npm audit fix --force ); then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
