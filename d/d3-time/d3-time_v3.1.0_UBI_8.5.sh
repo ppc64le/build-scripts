5c99509ecc4553311a8de816d9a342035861f977
sai_nammi <sai.nammi@ibm.com>
2023-03-27 07:48:48 +0000

    updated build script

--- d/d3-time/d3-time_v3.1.0_UBI_8.5.sh
+++ d/d3-time/d3-time_v3.1.0_UBI_8.5.sh
@@ -4,11 +4,11 @@
 # Package       : d3-time
 # Version       : v3.1.0
 # Source repo   : https://github.com/d3/d3-time
-# Language      : Node
+# Tested on     : UBI: 8.5
+# Language      : Javascript
 # Travis-Check  : True
-# Tested on     : UBI 8.5
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Sai Nammi <Sai.Nammi@ibm.com>
+# Maintainer    : Sai Nammi <sai.nammi@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -20,42 +20,43 @@
 
 set -e
 
-# Variables
+#setting variables
 PACKAGE_NAME=d3-time
 PACKAGE_VERSION=${1:-v3.1.0}
 PACKAGE_URL=https://github.com/d3/d3-time
-export NODE_VERSION=${NODE_VERSION:-19}
+HOME_DIR=${PWD}
 
-#Install dependencies
-yum install -y git gcc gcc-c++ make
+#Installing dependencies
+yum install -y yum-utils git wget tar gzip
 
-#Installing nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source "$HOME"/.bashrc
-echo "installing nodejs $NODE_VERSION"
-nvm install "$NODE_VERSION" >/dev/null
-nvm use $NODE_VERSION
+#Installing Nodejs v14.21.2
+cd $HOME_DIR
+wget https://nodejs.org/dist/v14.21.2/node-v14.21.2-linux-ppc64le.tar.gz
+tar -xzf node-v14.21.2-linux-ppc64le.tar.gz
+export PATH=$HOME_DIR/node-v14.21.2-linux-ppc64le/bin:$PATH
+node -v
+npm -v
 
-# clone the repository
+#Cloning d3-time repo
+cd $HOME_DIR
 git clone $PACKAGE_URL
-cd $PACKAGE_NAME
+cd $PACKAGE_NAME/
 git checkout $PACKAGE_VERSION
 
-
-# Build  package
+#Build d3-time
 if ! npm install && npm audit fix && npm audit fix --force; then
-        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
         exit 1
 fi
 
-# Run test cases
+#Run test cases
 if ! npm test; then
         echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
-        exit 1
+        exit 2
 else
         echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
