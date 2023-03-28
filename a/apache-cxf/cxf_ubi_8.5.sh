6f1dc5de7fdbbd074d70cfa0d8960692f1b52caa
vinodk99 <Vinod.K1@ibm.com>
2023-03-28 11:29:14 +0000

    Added patch file for cxf

--- a/apache-cxf/cxf_ubi_8.5.sh
+++ a/apache-cxf/cxf_ubi_8.5.sh
@@ -50,6 +50,8 @@ fi
 
 export MAVEN_OPTS="-Xmx2048m -Xms1024m  -Djava.awt.headless=true"
 
+wget https://raw.githubusercontent.com/vinodk99/build-scripts/apache-cxf/a/apache-cxf/cxf.patch;
+git apply cxf.patch 
 if ! mvn -U -B clean install ; then
     echo "------------------$PACKAGE_NAME::Install_and_Test_fails-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
