e811f839f5d74f2e746d191ce9a162efbbfd3b2c
Pratikt2312 <115208162+Pratikt2312@users.noreply.github.com>
2023-03-28 13:30:09 +0530

    Update directory-ldap-api_ubi_8.3.sh

--- a/apache-directory-ldap-api/directory-ldap-api_ubi_8.3.sh
+++ a/apache-directory-ldap-api/directory-ldap-api_ubi_8.3.sh
@@ -31,9 +31,9 @@ else
 fi
 
 #install Java8
-yum install java-1.8.0-openjdk-devel git wget -y
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.302.b08-0.el8_4.ppc64le
-export PATH=${JAVA_HOME}/bin:${PATH}
+yum install java-1.8.0-openjdk-devel.ppc64le git wget -y
+export JAVA_HOME=/usr/lib/jvm/$(ls /usr/lib/jvm/ | grep -P '^(?=.*java-)(?=.*ppc64le)')
+export PATH=$PATH:$JAVA_HOME/bin
 
 #install maven
 wget https://www-eu.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
