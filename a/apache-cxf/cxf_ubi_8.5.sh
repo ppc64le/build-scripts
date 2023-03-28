88be207fe4108e70fc0509e43a7ba010a8223b43
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2023-03-28 14:58:07 +0530

    Update cxf_ubi_8.5.sh

--- a/apache-cxf/cxf_ubi_8.5.sh
+++ a/apache-cxf/cxf_ubi_8.5.sh
@@ -41,12 +41,14 @@ cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 
-if ! mvn clean install -P -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -DskipTests ; then
+if ! mvn clean install -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -DskipTests ; then
     echo "------------------$PACKAGE_NAME:Install_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
     exit 1
 fi
+
+export MAVEN_OPTS="-Xmx2048m -Xms1024m  -Djava.awt.headless=true"
 
 if ! mvn -U -B clean install ; then
     echo "------------------$PACKAGE_NAME::Install_and_Test_fails-------------------------"
