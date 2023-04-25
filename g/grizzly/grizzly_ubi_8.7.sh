5aa04843d497d3832b6fd280fd51b840b98d5e67
vinodk99 <Vinod.K1@ibm.com>
2023-04-25 09:33:30 +0000

    Currency:Added build-script,Dockerfile and build_info.json for grizzly

---
+++ g/grizzly/grizzly_ubi_8.7.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : grizzly
+# Version          : v4.0.0
+# Source repo      : https://github.com/eclipse-ee4j/grizzly
+# Tested on        : UBI 8.7
+# Language         : Java
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vinod K <Vinod.K1@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=grizzly
+PACKAGE_VERSION=${1:-v4.0.0}
+PACKAGE_URL=https://github.com/eclipse-ee4j/grizzly
+HOME_DIR=${PWD}
+
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+yum install -y git make wget gcc-c++ java-17-openjdk java-17-openjdk-devel java-17-openjdk-headless
+
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+export PATH=$PATH:$JAVA_HOME/bin
+
+wget https://archive.apache.org/dist/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.tar.gz
+tar -zxf apache-maven-3.8.7-bin.tar.gz
+cp -R apache-maven-3.8.7 /usr/local
+ln -s /usr/local/apache-maven-3.8.7/bin/mvn /usr/bin/mvn
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! mvn clean install -DskipTests=true ; then
+      echo "------------------$PACKAGE_NAME:Build_fails---------------------"
+      echo "$PACKAGE_VERSION $PACKAGE_NAME"
+      echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+      exit 1
+fi
+
+if ! mvn test ; then
+      echo "------------------$PACKAGE_NAME::Build_and_Test_fails-------------------------"
+      echo "$PACKAGE_URL $PACKAGE_NAME"
+      echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Build_and_Test_Success"
+      exit 2
+else
+      echo "------------------$PACKAGE_NAME::Build_and_Test_success-------------------------"
+      echo "$PACKAGE_URL $PACKAGE_NAME"
+      echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Build_and_Test_Success"
+      exit 0
+fi
+
