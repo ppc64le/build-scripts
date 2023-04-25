8e3217aa7102b7714612dca04a45584e4f71879f
Shubham Garud <Shubham.Garud@ibm.com>
2023-04-25 08:48:11 +0000

    Updating Travis Check field

--- a/apache-solr/solr_ubi_8.5.sh
+++ a/apache-solr/solr_ubi_8.5.sh
@@ -6,7 +6,7 @@
 # Source repo      : https://github.com/apache/solr
 # Tested on        : UBI 8.5
 # Language         : Java
-# Travis-Check     : True
+# Travis-Check     : False 
 # Script License   : Apache License, Version 2 or later
 # Maintainer       : Shubham Garud <Shubham.Garud@ibm.com>
 #
