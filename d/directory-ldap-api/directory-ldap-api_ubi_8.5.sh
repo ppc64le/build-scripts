c05c5ca1d7786d928bb85050462d8a3e1ff12f37
Valipi-Venkatesh <131649221+Valipi-Venkatesh@users.noreply.github.com>
2023-04-26 11:14:56 +0530

    Update directory-ldap-api_ubi_8.5.sh

--- d/directory-ldap-api/directory-ldap-api_ubi_8.5.sh
+++ d/directory-ldap-api/directory-ldap-api_ubi_8.5.sh
@@ -2,7 +2,7 @@
 # ----------------------------------------------------------------------------
 #
 # Package          : directory-ldap-api
-# Version          : 2.1.3
+# Version          : master
 # Source repo      : https://github.com/apache/directory-ldap-api.git
 # Tested on        : UBI 8.5
 # Language         : Java
@@ -18,10 +18,11 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=directory-ldap-api
-PACKAGE_VERSION=2.1.3
+PACKAGE_VERSION=master
 PACKAGE_URL=https://github.com/apache/directory-ldap-api
 
 OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+yum -y update
 
 #Dependencies
 yum install -y java-17-openjdk java-17-openjdk-devel java-17-openjdk-headless wget git 
@@ -40,7 +41,6 @@ export PATH=$PATH:$M2_HOME/bin
 # Cloning the repository from remote to local
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
-git checkout $PACKAGE_VERSION
 if ! mvn -U clean install -Djava.awt.headless=true -fae -B ; then
 	echo "------------------$PACKAGE_NAME:Build_fails---------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
