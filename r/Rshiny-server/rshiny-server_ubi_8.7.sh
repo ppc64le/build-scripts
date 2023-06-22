ed2969b3ff2cac778bc869763e66d9921d450ccb
ujwal-akare <120369418+ujwal-akare@users.noreply.github.com>
2023-06-22 19:01:43 +0530

    Update rshiny-server_ubi_8.7.sh
    removed sudo

--- r/Rshiny-server/rshiny-server_ubi_8.7.sh
+++ r/Rshiny-server/rshiny-server_ubi_8.7.sh
@@ -31,18 +31,18 @@ PACKAGE_BRANCH=master
 PACKAGE_URL=https://github.com/rstudio/shiny-server.git
 
 #Download Updates and Dependencies
-sudo dnf update -y
+dnf update -y
 
 #install required prerequisites
-sudo dnf install -y gcc gcc-c++ git wget xz cmake make python3.8 openssl-devel
+dnf install -y gcc gcc-c++ git wget xz cmake make python3.8 openssl-devel
 
 #Install R from Source
 
 # Required repo to pickup additional EPEL package
-sudo dnf install --nodocs -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-sudo dnf update -y
+dnf install --nodocs -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf update -y
 #Install required dependencies
-sudo dnf builddep R -y
+dnf builddep R -y
 export R_VERSION=4.2.3
 # Download and extract R
 curl -O https://cran.rstudio.com/src/base/R-4/R-${R_VERSION}.tar.gz
@@ -50,8 +50,8 @@ tar -xzvf R-${R_VERSION}.tar.gz
 cd R-${R_VERSION}
 #Build and install R
 ./configure --prefix=/opt/R/${R_VERSION} --enable-R-shlib --enable-memory-profiling
-sudo make
-sudo make install
+make
+make install
 export PATH=$PATH:/opt/R/4.2.3/bin
 R --version
 
@@ -75,7 +75,7 @@ cd $PACKAGE_NAME
 #checkout to latest version
 git checkout $PACKAGE_TAG 
 
-sudo mkdir tmp
+mkdir tmp
 
 cd tmp
 
@@ -85,23 +85,23 @@ sed -i '8s/.*/NODE_SHA256=25aa3bb52ee6ca29b93dec388c2b5d66265315ffae18be9a8fc239
 #searche for the string "linux-x64.tar.xz" in the install-node.sh file and replace it with "linux-ppc64le.tar.xz"
 sed -i 's/linux-x64.tar.xz/linux-ppc64le.tar.xz/'  ../external/node/install-node.sh
 
-sudo ../external/node/install-node.sh
+../external/node/install-node.sh
 DIR=`pwd`
 PATH=$DIR/../bin:$PATH
 export PYTHON=`which python3.8`
 export PATH=$PYTHON:$PATH
 
 #configure the build system using cmake.
-sudo cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DPYTHON="$PYTHON" ../
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DPYTHON="$PYTHON" ../
 
-sudo make
-sudo mkdir ../build
+make
+mkdir ../build
 
 #change the current working directory to the parent directory and then execute the npm install.
 (cd .. && ./bin/npm install)
 
 #install the built files
-if !  sudo make install; then
+if !    make install; then
         echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_TAG | $OS_NAME | GitHub | Fail |  Install_Fails"
@@ -109,8 +109,8 @@ if !  sudo make install; then
 fi
 
 #Configuration for R-shiny server
-sudo mkdir -p /etc/shiny-server
-sudo cp ../config/default.config /etc/shiny-server/shiny-server.conf
+mkdir -p /etc/shiny-server
+cp ../config/default.config /etc/shiny-server/shiny-server.conf
 
 if ! ( npm install &&  npm test); then
         echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
