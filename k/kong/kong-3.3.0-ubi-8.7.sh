b837bc679fe5d4d4ef548307ea8352dabe578268
sumitd2 <91451282+sumitd2@users.noreply.github.com>
2023-06-26 13:12:55 +0530

    Update kong-3.3.0-ubi-8.7.sh

--- k/kong/kong-3.3.0-ubi-8.7.sh
+++ k/kong/kong-3.3.0-ubi-8.7.sh
@@ -5,7 +5,7 @@
 # Version       : 3.3.0
 # Source repo   : https://github.com/kong/kong/
 # Tested on     : UBI 8.7
-# Language      : Rust, Lua 
+# Language      : Rust 
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Sumit Dubey <Sumit.Dubey2@ibm.com>
