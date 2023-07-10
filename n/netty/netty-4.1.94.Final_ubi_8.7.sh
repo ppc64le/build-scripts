808baa1293807f005b4495b14c5dabf227ec6652
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2023-07-10 13:56:11 +0530

    Update and rename netty-4.1.92.Final_ubi_8.7.sh to netty-4.1.94.Final_ubi_8.7.sh

--- n/netty/netty-4.1.94.Final_ubi_8.7.sh
+++ n/netty/netty-4.1.94.Final_ubi_8.7.sh
@@ -2,7 +2,7 @@
 # ----------------------------------------------------------------------------
 #
 # Package       : netty
-# Version       : netty-4.1.92.Final
+# Version       : netty-4.1.94.Final
 # Language      : Java
 # Source repo   : https://github.com/netty/netty
 # Tested on     : UBI 8.7
@@ -21,7 +21,7 @@
 WORKDIR=`pwd`
 PACKAGE_NAME=netty
 PACKAGE_URL=https://github.com/netty/netty
-PACKAGE_VERSION=${1:-netty-4.1.92.Final}
+PACKAGE_VERSION=${1:-netty-4.1.94.Final}
 
 yum install -y make maven git sudo wget gcc-c++ apr-devel perl go openssl-devel automake autoconf libtool libstdc++-static
 
@@ -43,7 +43,7 @@ cd ..
 #netty requires netty-tcnative binaries to build and test ,community made this changes to build netty-tcnative binaries,tried to build and test the netty-tcnative with the following link:https://github.com/linux-on-ibm-z/docs/wiki/Building-netty-tcnative
 git clone https://github.com/netty/netty-tcnative.git 
 cd netty-tcnative/
-git checkout netty-tcnative-parent-2.0.60.Final
+git checkout netty-tcnative-parent-2.0.61.Final
 sed -i '85,85 s/chromium-stable/patch-s390x-Jan2021/g' pom.xml
 sed -i '89,89 s/1ccef4908ce04adc6d246262846f3cd8a111fa44/d83fd4af80af244ac623b99d8152c2e53287b9ad/g' pom.xml
 sed -i '54,54 s/boringssl.googlesource.com/github.com\/linux-on-ibm-z/g' boringssl-static/pom.xml
