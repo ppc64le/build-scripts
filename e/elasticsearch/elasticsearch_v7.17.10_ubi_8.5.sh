714c96959588a718f351c3a3d0e4cc8611a0c774
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2023-07-28 11:04:31 +0530

    Update elasticsearch_v7.17.10_ubi_8.5.sh

--- e/elasticsearch/elasticsearch_v7.17.10_ubi_8.5.sh
+++ e/elasticsearch/elasticsearch_v7.17.10_ubi_8.5.sh
@@ -37,7 +37,7 @@ export JAVA17_HOME=$JAVA_HOME
 git clone https://github.com/elastic/elasticsearch.git
 cd elasticsearch && git checkout $ELASTICSEARCH_VERSION
 # Apply patches
-wget https://github.com/vinodk99/build-scripts/blob/elastic_patch/e/elasticsearch/elasticsearch_v7.17.10.patch
+wget https://raw.githubusercontent.com/vinodk99/build-scripts/elastic_patch/e/elasticsearch/elasticsearch_v7.17.10.patch
 git apply elasticsearch_v7.17.10.patch
 mkdir -p distribution/archives/linux-ppc64le-tar
 echo "// This file is intentionally blank. All configuration of the distribution is done in the parent project." > distribution/archives/linux-ppc64le-tar/build.gradle
