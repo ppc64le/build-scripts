7e5a49f392951e0c4b4f748e0d61a9e23c8983a4
saiNammi <118737990+saiNammi@users.noreply.github.com>
2023-08-03 16:18:16 +0530

    Update build_docker.sh

--- script/build_docker.sh
+++ script/build_docker.sh
@@ -1,16 +1,13 @@
  #!/bin/bash -xe
-echo "Sourcing variables to get values of build_info.json" 
-source variable.sh
 
 version="$VERSION"
 packageDirPath="$PKG_DIR_PATH"
 configFile="build_info.json"
 imageName=$IMAGE_NAME
-buildDocker=true
+buildDocker=$BUILD_DOCKER
 
 cd $packageDirPath
-echo "Checking for docker_build value in build_info.json"
-buildDocker=$(jq .docker_build $configFile)
+
 if [ $buildDocker != false ];then
     if [[ $(jq --arg ver $version '.[$ver]' $configFile) != null ]]; then
         dockerBuildDir=$(jq -r --arg ver $version '.[$ver].dir' $configFile)
