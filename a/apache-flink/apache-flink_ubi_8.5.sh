a144501dce40e58d0bdf4ebe89f48de887c74eb3
Pratikt2312 <Pratik.Tonage@ibm.com>
2023-08-13 15:55:54 +0000

    Currency:Fix Apache-flink jenkins build

--- a/apache-flink/apache-flink_ubi_8.5.sh
+++ a/apache-flink/apache-flink_ubi_8.5.sh
@@ -2,7 +2,7 @@
 # ----------------------------------------------------------------------------
 # 
 # Package       : Apache Flink
-# Version       : master
+# Version       : master,release-1.17.1
 # Source repo   : https://github.com/apache/flink
 # Tested on     : UBI: 8.5
 # Language      : Java
@@ -19,20 +19,23 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=apache-flink
-PACKAGE_VERSION=${1:-master}
+PACKAGE_VERSION=${1:-release-1.17.1}
 PACKAGE_URL=https://github.com/apache/flink.git
 
-MAVEN_VERSION=3.5.4
+MAVEN_VERSION=3.9.4
 
 # Install dependencies and tools.
 yum update -y
-yum install -y git wget java-1.8.0-openjdk-devel xz
+yum install -y git wget java-1.8.0-openjdk-devel xz python2 python2-devel
+
+ln -sf /usr/bin/python2 /usr/bin/python
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+export PATH=$JAVA_HOME/bin:$PATH
 
 # Install postgresql build dependencies
 yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-ppc64le/pgdg-redhat-repo-latest.noarch.rpm
-yum install -y postgresql10-libs
-yum install -y postgresql10 postgresql10-server postgresql10-contrib
+yum install -y postgresql12-libs
+yum install -y postgresql12 postgresql12-server postgresql12-contrib
 
 # Install maven package
 wget https://downloads.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
@@ -48,7 +51,7 @@ cd flink
 git checkout $PACKAGE_VERSION
 
 # Compile and build package using threads
-mvn clean package -DskipTests -Pskip-webui-build
+mvn clean package -DskipTests -Pskip-webui-build -Drat.skip=true
 
-# Several modules cause test failure, excluding those modules during testing
-mvn test -pl '!flink-core, !flink-runtime, !flink-state-backends/flink-statebackend-rocksdb, !flink-state-backends/flink-statebackend-changelog, !flink-connectors/flink-connector-kafka, !flink-connectors/flink-connector-jdbc, !flink-table/flink-table-runtime, !flink-python, !flink-connectors/flink-connector-hbase-2.2, !flink-filesystems/flink-s3-fs-base, !flink-runtime-web, !flink-filesystems/flink-azure-fs-hadoop, !flink-yarn, !flink-fs-tests, !flink-filesystems/flink-hadoop-fs, !flink-formats/flink-hadoop-bulk'
+# Several modules cause test failure, excluding those modules during testing because it is in parity with Intel
+mvn test -Drat.skip=true -pl '!flink-connectors/flink-connector-kafka, !flink-connectors/flink-connector-hbase-2.2, !flink-filesystems/flink-s3-fs-base, !flink-filesystems/flink-s3-fs-hadoop, !flink-filesystems/flink-s3-fs-presto, !flink-filesystems/flink-azure-fs-hadoop,!flink-state-backends/flink-statebackend-changelog'
