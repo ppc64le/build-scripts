d4c16162ea0ad18f60637b03557a7128c2e2f951
Nikhil Kalbande <nkalband@us.ibm.com>
2023-08-21 18:59:16 +0530

    trying with code changes

--- script/read_buildinfo.sh
+++ script/read_buildinfo.sh
@@ -42,42 +42,35 @@ if [ -f $configFile ]; then
   fi
   echo "Checking for string/pattern match for version in build_info.json"
 
-  # if [[ $(jq --arg ver $version '.[$ver]' $configFile) == null ]]; then
-    # jsonObj=$(cat $configFile)
-    # # Inline Python code using python3 -c
-    # result_version=$(python3<< END_OF_PYTHON_SCRIPT
-
-    # import re
-
-    # def find_matching_version(jsonObj, version):
-        # for entry in jsonObj:
-            # key = entry
-            # subKeys = [subKey.strip() for subKey in key.split(',')]
-            # if version in subKeys:
-                # version = key
-                # print (f"BREAK1 {version}")
-                # return version
-            # else:
-                # for subKey in subKeys:
-                    # regex_str = '^' + subKey.replace(".", "\\.").replace("*", ".*") + '$'
-                    # regex = re.compile(regex_str)
-                    # if regex.match(version):
-                        # version = key
-                        # print (f"BREAK2 {version}")
-                        # return version
-
-    # input_version = str("$VERSION")
-    # input_jsonObj = "$jsonObj"
-    # result_version = find_matching_version(input_jsonObj, input_version)
-    # print(f"BREAK3 {result_version}")
-
-    # END_OF_PYTHON_SCRIPT
-    # # End of Python script
-    # )
+  if [[ $(jq --arg ver $version '.[$ver]' $configFile) == null ]]; then
+    jsonObj=$(cat $configFile)
+    # Inline Python code using python3 -c
+    result_version=$(python3<< END_OF_PYTHON_SCRIPT
+    import re
+    input_version = str("$VERSION")
+    input_jsonObj = "$jsonObj"
+    for entry in jsonObj:
+      key = entry
+      subKeys = [subKey.strip() for subKey in key.split(',')]
+      if version in subKeys:
+        version = key
+        print (f"BREAK1 {version}")
+        return version
+      else:
+        for subKey in subKeys:
+          regex_str = '^' + subKey.replace(".", "\\.").replace("*", ".*") + '$'
+          regex = re.compile(regex_str)
+          if regex.match(version):
+            version = key
+            print (f"BREAK2 {version}")
+            return version
+    END_OF_PYTHON_SCRIPT
+    # End of Python script
+    )
     
-    # VERSION=$result_version
+    VERSION=$result_version
 
-  # fi
+  fi
 
   if [[ $(jq --arg ver $VERSION '.[$ver]' $configFile) != null ]] && 
     [[ $(jq -r --arg ver $VERSION '.[$ver].base_docker_image' $configFile) != null ]]; then
