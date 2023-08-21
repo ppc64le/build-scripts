0114c1d0834b556a4ee10c8f72ee29e2cb3a8759
Nikhil Kalbande <nkalband@us.ibm.com>
2023-08-21 19:04:03 +0530

    trying with code changes

--- script/read_buildinfo.sh
+++ script/read_buildinfo.sh
@@ -45,25 +45,32 @@ if [ -f $configFile ]; then
   if [[ $(jq --arg ver $version '.[$ver]' $configFile) == null ]]; then
     jsonObj=$(cat $configFile)
     # Inline Python code using python3 -c
-    result_version=$(python3<< END_OF_PYTHON_SCRIPT
+    result_version=$(python<< END_OF_PYTHON_SCRIPT
+
     import re
+
+    def find_matching_version(jsonObj, version):
+        for entry in jsonObj:
+            key = entry
+            subKeys = [subKey.strip() for subKey in key.split(',')]
+            if version in subKeys:
+                version = key
+                print (f"BREAK1 {version}")
+                return version
+            else:
+                for subKey in subKeys:
+                    regex_str = '^' + subKey.replace(".", "\\.").replace("*", ".*") + '$'
+                    regex = re.compile(regex_str)
+                    if regex.match(version):
+                        version = key
+                        print (f"BREAK2 {version}")
+                        return version
+
     input_version = str("$VERSION")
     input_jsonObj = "$jsonObj"
-    for entry in jsonObj:
-      key = entry
-      subKeys = [subKey.strip() for subKey in key.split(',')]
-      if version in subKeys:
-        version = key
-        print (f"BREAK1 {version}")
-        return version
-      else:
-        for subKey in subKeys:
-          regex_str = '^' + subKey.replace(".", "\\.").replace("*", ".*") + '$'
-          regex = re.compile(regex_str)
-          if regex.match(version):
-            version = key
-            print (f"BREAK2 {version}")
-            return version
+    result_version = find_matching_version(input_jsonObj, input_version)
+    print(f"BREAK3 {result_version}")
+
     END_OF_PYTHON_SCRIPT
     # End of Python script
     )
