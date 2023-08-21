b42be39f4872fe694130b9ec1ba7db5523ac5e10
Nikhil Kalbande <nkalband@us.ibm.com>
2023-08-21 20:46:12 +0530

    adding build log to file to print later

--- script/build_package.sh
+++ script/build_package.sh
@@ -7,13 +7,13 @@ pip3 install docker
 
 echo "Running build script execution in background for "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" " 
       
-python3 script/validate_builds_currency.py "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" &
+python3 script/validate_builds_currency.py "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" > build_log &
 
 SCRIPT_PID=$!
 while ps -p $SCRIPT_PID > /dev/null
 do 
   echo "$SCRIPT_PID is running"
-  sleep 60
+  sleep 100
 done
 wait $SCRIPT_PID
 my_pid_status=$?
@@ -21,5 +21,8 @@ if [ $my_pid_status != 0 ]
 then
     echo "Script execution failed for "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" "
     exit 1
+else
+    echo "Script execution completed successfully for "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" "
+    cat build_log&
 fi
 
