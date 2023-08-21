48c2ac8fe6a45a2c899b8732ba49c549b2e27656
saiNammi <118737990+saiNammi@users.noreply.github.com>
2023-08-21 16:14:39 +0530

    Travis currency poc (#3314)

---
+++ script/build_package.sh
@@ -0,0 +1,25 @@
+ #!/bin/bash -xe
+
+sudo apt update -y && sudo apt-get install file -y
+docker pull registry.access.redhat.com/ubi8/ubi:8.7
+pip3 install requests
+pip3 install docker
+
+echo "Running build script execution in background for "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" " 
+      
+python3 script/validate_builds_currency.py "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" &
+
+SCRIPT_PID=$!
+while ps -p $SCRIPT_PID > /dev/null
+do 
+  echo "$SCRIPT_PID is running"
+  sleep 60
+done
+wait $SCRIPT_PID
+my_pid_status=$?
+if [ $my_pid_status != 0 ]
+then
+    echo "Script execution failed for "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" "
+    exit 1
+fi
+
