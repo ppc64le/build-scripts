62d866fa51d26d132b2471b9dde2a63517039ecc
Nikhil Kalbande <nkalband@us.ibm.com>
2023-08-22 15:52:59 +0530

    upgraded python code

--- script/parse_buildinfo.py
+++ script/parse_buildinfo.py
@@ -1,28 +1,22 @@
-import re
+import json
 import os
-
-def find_matching_version(jsonObj, version):
-    for entry in jsonObj:
-        key = entry
-        subKeys = [subKey.strip() for subKey in key.split(',')]
-        if version in subKeys:
-            version = key
-            print (f"BREAK1 {version}")
-            return version
-        else:
-            for subKey in subKeys:
-                regex_str = '^' + subKey.replace(".", "\\.").replace("*", ".*") + '$'
-                regex = re.compile(regex_str)
-                if regex.match(version):
-                    version = key
-                    print (f"BREAK2 {version}")
-                    return version
-
-print("Inside parse_buildinfo prog")
-input_version = str(os.environ['VERSION'])
-input_jsonObj = os.environ['jsonObj']
-result_version = find_matching_version(input_jsonObj, input_version)
-print("result_version")
-print(result_version)
-os.environ['result_version'] = result_version
-#print(f"BREAK3 {result_version}")
+import re
+version = str(os.environ['VERSION'])
+match_version=""
+f = open('build_info.json')
+data = json.load(f)
+for key,value in data.items():
+  subKeys = [subKey.strip() for subKey in key.split(',')]
+  if version in subKeys:
+    match_version = key
+    break
+  else:
+    for subKey in subKeys:
+      regex_str = '^' + subKey.replace(".", "\\.").replace("*", ".*") + '$'
+      regex = re.compile(regex_str)
+      if regex.match(version):
+        match_version = key
+        print("String mathing")
+  if len(match_version) != 0:
+    break
+print(match_version)
