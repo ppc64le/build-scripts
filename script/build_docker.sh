5c8001a77eae0fc41a19bce38ec05b64d25b55af
nikhil-kalbande <82815888+nikhil-kalbande@users.noreply.github.com>
2023-08-24 14:27:18 +0530

    currency build on travis POC code changes #3320)
    - Reformatted code
    - Added code ( python + shell) to match passed version with keys in build_info.json
    - Fixed issues about build logging
    - Parallel jobs  for package and docker build
    - Renamed some variables in standard format
    - updated read_buildinfo.sh to process and generate additional variables

--- script/build_docker.sh
+++ script/build_docker.sh
@@ -1,51 +1,56 @@
  #!/bin/bash -xe
 
 version="$VERSION"
-packageDirPath="$PKG_DIR_PATH"
-configFile="build_info.json"
-imageName=$IMAGE_NAME
-buildDocker=$BUILD_DOCKER
+package_dirpath="$PKG_DIR_PATH"
+config_file="build_info.json"
+image_name=$IMAGE_NAME
+build_docker=$BUILD_DOCKER
 
+CUR_DIR=$(pwd)
+cd $package_dirpath
 
-cd $packageDirPath
+# Using python script to find matched version string/key in build_info.json for version passed 
+match_version=$(python $CUR_DIR/script/match_version_buildinfo.py)
 
-if [ $buildDocker != false ];then
-    if [[ $(jq --arg ver $version '.[$ver]' $configFile) != null ]]; then
-        dockerBuildDir=$(jq -r --arg ver $version '.[$ver].dir' $configFile)
-        args=$(jq -r --arg ver $version '.[$ver].args' $configFile)
-        patches=$(jq -r --arg ver $version '.[$ver].patches' $configFile)
+if [ $build_docker != false ];then
+    if [[ $(jq --arg ver "$match_version" '.[$ver]' $config_file) != null ]]; then
+        docker_builddir=$(jq -r --arg ver "$match_version" '.[$ver].dir' $config_file)
+        args=$(jq -r --arg ver "$match_version" '.[$ver].args' $config_file)
+        patches=$(jq -r --arg ver "$match_version" '.[$ver].patches' $config_file)
         # By default send PACKAGE_VERSION argument.
-        buildArgs ="--build-arg PACKAGE_VERSION=$(VERSION)"
+        build_args ="--build-arg PACKAGE_VERSION=$(VERSION)"
         if [ $args != null ]; then
             for row in $(echo "$args" | jq -r 'to_entries[] | @base64'); do
             key=$(echo "$row" | base64 -d | jq -r '.key')
             value=$(echo "$row" | base64 -d | jq -r '.value')
-            buildArgs=$(echo $buildArgs --build-arg $key=$value )
+            build_args=$(echo $build_args --build-arg $key=$value )
             done
         fi
         if [ $patches != null ]; then
             for row in $(echo "$patches" | jq -r 'to_entries[] | @base64'); do
             key=$(echo "$row" | base64 -d | jq -r '.key')
             value=$(echo "$row" | base64 -d | jq -r '.value')
-            buildArgs=$(echo $buildArgs --build-arg $key=$value )
+            build_args=$(echo $build_args --build-arg $key=$value )
             done
         fi
-        if [[ $(jq --arg ver $version '.[$ver]' $configFile) != null ]] && 
-            [[ $(jq -r --arg ver $version '.[$ver].base_docker_image' $configFile) != null ]]; then
-            baseName=$(jq -r --arg ver $version '.[$ver].base_docker_image' $configFile)
+        if [[ $(jq --arg ver "$match_version" '.[$ver]' $config_file) != null ]] && 
+            [[ $(jq -r --arg ver "$match_version" '.[$ver].base_docker_image' $config_file) != null ]]; then
+            basename=$(jq -r --arg ver "$match_version" '.[$ver].base_docker_image' $config_file)
         fi
-        cmd="$buildArgs -t $imageName $dockerBuildDir"
-        final_upload_image_link=$(DOCKER_UPLOAD_LINK)/$imageName
-        docker_file_path="${packageDirPath}/Dockerfiles"
+        cmd="$build_args -t $image_name $docker_builddir"
+        final_upload_image_link=$(DOCKER_UPLOAD_LINK)/$image_name
+        docker_file_path="${package_dirpath}/Dockerfiles"
     fi
 
     cd Dockerfiles
     echo "Deleting existing docker image"
-    docker rmi -f ${imageName}
-    #docker rmi -f ${baseName}
+    docker rmi -f ${image_name}
+    #docker rmi -f ${basename}
     echo "Building docker image"
-    sudo docker build $buildArgs -t $imageName $dockerBuildDir
-    docker save -o "$HOME/build/$TRAVIS_REPO_SLUG/image.tar" $IMAGE_NAME
+    echo "sudo docker build $build_args -t $image_name $docker_builddir"
+    echo "*************************************************************************************"
+    sudo docker build $build_args -t $image_name $docker_builddir
+    docker save -o "$HOME/build/$TRAVIS_REPO_SLUG/image.tar" $image_name
 else
     echo "Docker image is not supported"
 fi
@@ -53,7 +58,7 @@ fi
 # # Publish code keeping commented for now
 # if [ $? == 0 ]
 #   then
-#     sudo docker tag ${imageName} ${docker_upload_link}/${imageName}
+#     sudo docker tag ${image_name} ${docker_upload_link}/${image_name}
 #     sudo docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword} ${docker_upload_link}
-#     sudo docker push ${docker_upload_link}/${imageName}
+#     sudo docker push ${docker_upload_link}/${image_name}
 # fi
--- script/build_package.sh
+++ script/build_package.sh
@@ -6,20 +6,28 @@ pip3 install requests
 pip3 install docker
 
 echo "Running build script execution in background for "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" " 
+echo "*************************************************************************************"
       
-python3 script/validate_builds_currency.py "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" &
+python3 script/validate_builds_currency.py "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" > build_log &
 
 SCRIPT_PID=$!
 while ps -p $SCRIPT_PID > /dev/null
 do 
   echo "$SCRIPT_PID is running"
-  sleep 60
+  sleep 100
 done
 wait $SCRIPT_PID
 my_pid_status=$?
 if [ $my_pid_status != 0 ]
 then
     echo "Script execution failed for "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" "
+    echo "*************************************************************************************"
+    cat build_log
     exit 1
+else
+    echo "Script execution completed successfully for "$PKG_DIR_PATH/$BUILD_SCRIPT" "$VERSION" "
+    echo "*************************************************************************************"
+    cat build_log
 fi
+exit 0
 
