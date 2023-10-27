bb08410315671a7b2ce451974058b293851a0dff
saiNammi <118737990+saiNammi@users.noreply.github.com>
2023-10-27 10:59:56 +0530

    Travis pre process and remaining scanner stages(syft and grype) (#1)
    * added pre-process stage
    
    * Added pre_process.sh file and also updated the code in currency-build.yml
    
    * Update currency-build.yml
    
    * Update pre_process.sh
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update currency-build.yml
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Update currency-build.yml
    
    * Update pre_process.sh
    
    * Update pre_process.sh
    
    * Create syft_code_scan.sh
    
    * Create grype_code_scan
    
    * Update syft_code_scan.sh
    
    * Update currency-build.yml
    
    * Update pre_process.sh
    
    * Rename grype_code_scan to grype_code_scan.sh
    
    * Update grype_code_scan.sh

---
+++ script/grype_code_scan.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+validate_build_script=$VALIDATE_BUILD_SCRIPT
+actual_package_name=$(awk -F'/' 'tolower($0) ~ /^# source repo.*github.com/{sub(/\.git/, "", $NF); print $NF}' $PKG_DIR_PATH$BUILD_SCRIPT)
+
+cd package-cache
+
+if [ $validate_build_script == true ];then
+     wget https://github.com/anchore/grype/releases/download/v0.67.0/grype_0.67.0_linux_ppc64le.tar.gz
+     tar -xf grype_0.67.0_linux_ppc64le.tar.gz
+     chmod +x grype
+     sudo mv grype /usr/bin
+                                
+     sudo grype -q -o cyclonedx-json dir:${actual_package_name} > grype_source_sbom_results.json
+     #curl -s -k -u ${env.dockerHubUser}:${env.dockerHubPassword} --upload-file grype_source_sbom_results.json ${url_prefix}/source/Grype_sbom_results.json
+     cat grype_source_sbom_results.json
+                                
+     sudo grype -q -o json dir:${actual_package_name} > grype_source_vulnerabilities_results.json
+     #curl -s -k -u ${env.dockerHubUser}:${env.dockerHubPassword} --upload-file grype_source_vulnerabilities_results.json ${url_prefix}/source/Grype_vulnerabilities_results.json
+     cat grype_source_vulnerabilities_results.json
+fi
