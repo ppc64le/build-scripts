0eb2f1be8784d8a9199e99a5f1a1645e45840161
Shubham Garud <Shubham.Garud@ibm.com>
2023-11-03 06:56:02 +0000

    Updated file path

--- script/node_bs.py
+++ script/node_bs.py
@@ -186,11 +186,11 @@ def create_new_script():
         elif template_lines[i].startswith("PACKAGE_NAME"):
             template_lines[i]=f"PACKAGE_NAME={package_name}\n"
         
-    with open (f"{current_directory}/templates/{script_language}",'w') as newfile:
+    with open (f"{dir_name}/{package_name}_ubi_8.7.sh",'w') as newfile:
         newfile.writelines(template_lines)
 
     shutil.copyfile(f"{current_directory}/templates/{script_language}",f"{dir_name}/{package_name}_ubi_8.7.sh")
-    new_cmd=f"python3 script/trigger_container.py -f templates/{script_language}"
+    new_cmd=f"python3 script/trigger_container.py -f {dir_name}/{package_name}_ubi_8.7.sh}"
      
     container_result=subprocess.Popen(new_cmd,shell=True)
     stdout, stderr=container_result.communicate()
