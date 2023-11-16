316dd5de23170f8cc34e290187257a0efd5171b3
AbhiIBM <137172943+AbhiIBM@users.noreply.github.com>
2023-11-16 19:14:55 +0530

    Currency: To add build-script and build_info.json for greenlet.
    Currency: To add build-script and build_info.json for greenlet.

---
+++ g/greenlet/greenlet_ubi_8.7.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : greenlet
+# Version          : v3.0.1
+# Source repo      : https://github.com/python-greenlet/greenlet.git
+# Tested on        : UBI 8.7
+# Language         : C++, Python
+# Travis-Check     : True
+# Script License   : GNU General Public License v3.0
+# Maintainer       : Abhishek Dwivedi <Abhishek.Dwivedi6@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=greenlet
+PACKAGE_VERSION=${1:-3.0.1}
+PACKAGE_URL=https://github.com/python-greenlet/greenlet.git
+
+PYTHON_VERSION=3.9
+
+wrkdir=`pwd`
+
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+yum install -y python39-devel.ppc64le git gcc gcc-c++
+
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+python3 -m pip install -r dev-requirements.txt
+
+if ! python3 setup.py install ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+python3 -m pip install -U -e ".[test]"
+
+if ! python3 -m unittest discover -v greenlet.tests ; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
