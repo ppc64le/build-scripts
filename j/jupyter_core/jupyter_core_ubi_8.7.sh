d0bbffdad8282ee76d14df6115c5543b4cd10d1d
AbhiIBM <137172943+AbhiIBM@users.noreply.github.com>
2023-11-22 15:34:01 +0530

    Currency: To add build-script and build_info.json for jupyter_core (#3412)
    Currency: To add build-script and build_info.json for jupyter_core

---
+++ j/jupyter_core/jupyter_core_ubi_8.7.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : jupyter_core
+# Version          : v5.5.0
+# Source repo      : https://github.com/jupyter/jupyter_core.git
+# Tested on        : UBI 8.7
+# Language         : Python, javascript
+# Travis-Check     : True
+# Script License   : GNU General Public License v3.0
+# Maintainer       : Abhishek Dwivedi <Abhishek.Dwivedi6@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=jupyter_core
+PACKAGE_VERSION=${1:-v5.5.0}
+PACKAGE_URL=https://github.com/jupyter/jupyter_core.git
+
+PYTHON_VERSION=3.9
+NODE_VERSION=v18.18.2
+
+wrkdir=`pwd`
+
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+yum update -y
+yum install wget git gcc gcc-c++ -y
+
+# miniconda installation
+wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.9.2-Linux-ppc64le.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+# conda --version
+# python3 --version
+python3 -m pip install -U pip
+conda install --yes -c conda-forge hatch
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install $NODE_VERSION
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! pip install -e . ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+pip install -e ".[test]"
+export JUPYTER_PATH=/usr/share/jupyter
+
+if ! hatch run test:nowarn ; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
