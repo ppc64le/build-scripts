3bdfa8893a7d2c32dc5ff089c1533439cccc56ad
Atharv Phadnis <94899024+atharv-phadnis@users.noreply.github.com>
2023-11-22 14:44:19 +0530

    Currency: Updated the build-script and build_info.json for Z-schema (#3419)
    * Fixed build-script for Z-schema
    
    * Added missing maintainer

--- z/z-schema/z-schema_ubi_8.5.sh
+++ z/z-schema/z-schema_ubi_8.5.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : z-schema
-# Version       : v3.25.1, v4.2.2, v4.2.3, v5.0.0, v5.0.3
+# Version       : v3.25.1, v4.2.2, v4.2.3, v5.0.0, v5.0.3, v6.0.1
 # Source repo   : https://github.com/zaggino/z-schema
 # Tested on     : ubi 8.5
 # Language      : Node
@@ -17,12 +17,14 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-#Run the script:./z-schema_ubi_8.5.sh v5.0.3
 PACKAGE_NAME=z-schema
-PACKAGE_VERSION=${1:-v5.0.3}
+PACKAGE_VERSION=${1:-v6.0.1}
 PACKAGE_URL=https://github.com/zaggino/z-schema.git
-dnf module enable nodejs:12 -y
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+dnf module enable nodejs:16 -y
 dnf install git wget fontconfig bzip2 npm -y
+
 wget https://github.com/ibmsoe/phantomjs/releases/download/2.1.1/phantomjs-2.1.1-linux-ppc64.tar.bz2
 tar -xvf phantomjs-2.1.1-linux-ppc64.tar.bz2
 ln -sf $(pwd)/phantomjs-2.1.1-linux-ppc64/bin/phantomjs /usr/local/bin/phantomjs
@@ -32,6 +34,22 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 git submodule update --init --recursive
-npm ci
-npm test
 
+if ! npm ci; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Install_Fails"
+    exit 1
+fi
+
+if ! npm test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Install_success_but_test_Fails"
+    exit 1
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
