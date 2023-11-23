4d02e155527e904ac86b28d698de974e76e89718
mohitp333 <mohit.pawar@.com>
2023-11-23 05:27:40 -0500

    Renamed build-script name to ubi_8.7

--- c/cloudpickle/cloudpickle_ubi_8.7.sh
+++ c/cloudpickle/cloudpickle_ubi_8.7.sh
@@ -18,17 +18,40 @@
 #
 # ----------------------------------------------------------------------------
 
+PACKAGE_NAME=cloudpickle
+PACKAGE_VERSION=v3.0.0
+PACKAGE_URL=https://github.com/cloudpipe/cloudpickle.git
+
 yum install -y gcc gcc-c++ yum-utils make automake autoconf libtool gdb* binutils rpm-build gettext wget
 yum install -y python39 python39-devel python39-setuptools
 
 python3 -m ensurepip --upgrade
 
-git clone https://github.com/cloudpipe/cloudpickle.git
-cd cloudpickle
-git checkout v3.0.0
+#Clone repo
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
 
 pip3 install tox 
 python3 -m pip install -r dev-requirements.txt
-#python3 setup.py install
+
+if ! python3 -m build ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
 python3 -m pip install tox --ignore-installed
-tox -e py3
+
+if ! tox -e py3 ; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
