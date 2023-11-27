43fab3f1abbd09e849263c7ba12d6ff4a622eb23
saiNammi <118737990+saiNammi@users.noreply.github.com>
2023-11-27 12:59:14 +0530

    Update syft_code_scan.sh

--- script/syft_code_scan.sh
+++ script/syft_code_scan.sh
@@ -14,5 +14,11 @@ if [ $validate_build_script == true ];then
       sudo syft -q -o cyclonedx-json dir:${actual_package_name} > syft_source_sbom_results.json
       cat syft_source_sbom_results.json
       #curl -s -k -u ${env.dockerHubUser}:${env.dockerHubPassword} --upload-file syft_source_sbom_results.json ${url_prefix}/source/Syft_sbom_results.json
+      token_request=$(curl -X POST https://iam.cloud.ibm.com/identity/token \
+  -H "content-type: application/x-www-form-urlencoded" \
+  -H "accept: application/json" \
+  -d "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey=$ibm_cos_api")
+      token=$(echo "$token_request" | jq -r '.access_token')
+      curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: text/plain" -d @syft_source_sbom_results.json "https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket/$PACKAGE_NAME/$VERSION/syft_source_sbom_results.json"
 fi
 
