a171b9281c07d02050b65fe62379eda8514b6e90
saiNammi <118737990+saiNammi@users.noreply.github.com>
2023-11-27 13:06:58 +0530

    Update build_package.sh

--- script/build_package.sh
+++ script/build_package.sh
@@ -7,8 +7,16 @@ pip3 install --upgrade docker
 
 echo "Running build script execution in background for "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" " 
 echo "*************************************************************************************"
-      
-python3 script/validate_builds_currency.py "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" > build_log &
+
+docker_image="registry.access.redhat.com/ubi8/ubi:8.7"
+if [ "$NON_ROOT_BUILD" == "true" ];
+then
+    echo "building docker image for non root user build"
+    docker build -t docker_non_root_image -f script/dockerfile_non_root .
+    docker_image="docker_non_root_image"
+fi
+
+python3 script/validate_builds_currency.py "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "$docker_image" > build_log &
 
 SCRIPT_PID=$!
 while ps -p $SCRIPT_PID > /dev/null
@@ -22,12 +30,12 @@ if [ $my_pid_status != 0 ]
 then
     echo "Script execution failed for "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "
     echo "*************************************************************************************"
-    cat build_log
+    tail -100 build_log
     exit 1
 else
     echo "Script execution completed successfully for "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "
     echo "*************************************************************************************"
-    cat build_log
+    tail -100 build_log
 fi
 exit 0
 
