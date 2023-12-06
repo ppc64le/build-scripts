975f77046862341d37aef53707abf62dfd17bfbb
AbhiIBM <137172943+AbhiIBM@users.noreply.github.com>
2023-12-06 18:28:48 +0530

    Currency: To add build-script and buil_info.json for cytoolz. (#3442)
    * Currency: To add build-script and buil_info.json for cytoolz.
    
    * Updated
    
    * Updated
    
    * Updated
    
    * Added Dockerfile for cytoolz.
    
    * Updated build_info.json
    
    * Updated.

---
+++ c/cytoolz/cytoolz_ubi_8.7.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : cytoolz
+# Version          : 0.12.2
+# Source repo      : https://github.com/pytoolz/cytoolz.git
+# Tested on        : UBI 8.7
+# Language         : Python, Cython
+# Travis-Check     : True
+# Script License   : GNU General Public License v3.0
+# Maintainer       : Abhishek Dwivedi <Abhishek.Dwivedi6@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=cytoolz
+PACKAGE_VERSION=${1:-0.12.2}
+PACKAGE_URL=https://github.com/pytoolz/cytoolz.git
+
+wrkdir=`pwd`
+
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+yum install -y python39-devel.ppc64le git gcc
+
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install cython pytest
+python3 -m pip install git+https://github.com/pytoolz/toolz.git
+python3 setup.py build_ext --inplace --with-cython
+
+if ! python3 -m pip install -e . ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! python3 -m pytest ; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
