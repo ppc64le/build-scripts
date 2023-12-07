6b06e95325c9fc9b2d9ed872d05766aef1912abd
saiNammi <118737990+saiNammi@users.noreply.github.com>
2023-12-07 15:53:06 +0530

    Currency travis migration scanner (#2)
    * Update upload_build_log.sh
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Create upload_source_code_scanner.sh
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Update trivy_code_scan.sh
    
    * Update trivy_code_scan.sh
    
    * Update currency-build.yml
    
    * Update grype_code_scan.sh
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Update upload_source_code_scanner.sh
    
    * Update currency-build.yml
    
    * Update grype_code_scan.sh
    
    * Update syft_code_scan.sh
    
    * Update trivy_code_scan.sh
    
    * Update currency-build.yml
    
    * Update upload_source_code_scanner.sh
    
    * Update currency-build.yml
    
    * Update and rename trivy_scan.sh to trivy_image_scan.sh
    
    * Update currency-build.yml
    
    * Update and rename upload_source_code_scanner.sh to upload_scanner_results.sh
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Update and rename grype_scan.sh to grype_image_scan.sh
    
    * Update grype_image_scan.sh
    
    * Update syft_scan.sh
    
    * Rename syft_scan.sh to syft_image_scan.sh
    
    * Update currency-build.yml
    
    * Update currency-build.yml
    
    * Update grype_image_scan.sh
    
    * Update trivy_code_scan.sh
    
    * Update grype_code_scan.sh
    
    * Update syft_code_scan.sh
    
    * Update trivy_code_scan.sh
    
    * Update trivy_image_scan.sh
    
    * Update syft_image_scan.sh
    
    * Update currency-build.yml

--- script/grype_code_scan.sh
+++ script/grype_code_scan.sh
@@ -9,20 +9,7 @@ if [ $validate_build_script == true ];then
      wget https://github.com/anchore/grype/releases/download/v0.67.0/grype_0.67.0_linux_ppc64le.tar.gz
      tar -xf grype_0.67.0_linux_ppc64le.tar.gz
      chmod +x grype
-     sudo mv grype /usr/bin
-                                
-     sudo grype -q -o cyclonedx-json dir:${actual_package_name} > grype_source_sbom_results.json
-     #curl -s -k -u ${env.dockerHubUser}:${env.dockerHubPassword} --upload-file grype_source_sbom_results.json ${url_prefix}/source/Grype_sbom_results.json
-     cat grype_source_sbom_results.json
-                                
+     sudo mv grype /usr/bin                      
+     sudo grype -q -o cyclonedx-json dir:${actual_package_name} > grype_source_sbom_results.json                         
      sudo grype -q -o json dir:${actual_package_name} > grype_source_vulnerabilities_results.json
-     #curl -s -k -u ${env.dockerHubUser}:${env.dockerHubPassword} --upload-file grype_source_vulnerabilities_results.json ${url_prefix}/source/Grype_vulnerabilities_results.json
-     cat grype_source_vulnerabilities_results.json
-     token_request=$(curl -X POST https://iam.cloud.ibm.com/identity/token \
-  -H "content-type: application/x-www-form-urlencoded" \
-  -H "accept: application/json" \
-  -d "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey=$ibm_cos_api")
-     token=$(echo "$token_request" | jq -r '.access_token')
-     curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: text/plain" -d @grype_source_sbom_results.json "https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket/$PACKAGE_NAME/$VERSION/grype_source_sbom_results.json"
-     curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: text/plain" -d @grype_source_vulnerabilities_results.json "https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket/$PACKAGE_NAME/$VERSION/grype_source_vulnerabilities_results.json"
 fi
