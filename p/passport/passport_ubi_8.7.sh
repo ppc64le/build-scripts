54f04cf6c8430fe4cd48ab55f5ac485c2c619b7b
AbhiIBM <137172943+AbhiIBM@users.noreply.github.com>
2023-12-12 19:16:31 +0530

    Added Dockerfile

--- p/passport/passport_ubi_8.7.sh
+++ p/passport/passport_ubi_8.7.sh
@@ -8,7 +8,7 @@
 # Language      : Node
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: ICH <ich@us.ibm.com>
+# Maintainer	: Abhishek Dwivedi <Abhishek.Dwivedi6@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
