53f301efd38234ef34cb901ecceeb96c12982611
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2023-12-26 10:20:30 +0530

    Update ipywidgets_ubi_8.7.sh

--- i/ipywidgets/ipywidgets_ubi_8.7.sh
+++ i/ipywidgets/ipywidgets_ubi_8.7.sh
@@ -22,7 +22,7 @@ PACKAGE_VERSION=${1:-8.1.1}
 PACKAGE_URL=https://github.com/jupyter-widgets/ipywidgets
 
 export NODE_VERSION=${NODE_VERSION:-16}
-yum install -y python38 python38-devel python38-pip git gcc gcc-c++ libffi make 
+yum install -y wget python38 python38-devel python38-pip git gcc gcc-c++ libffi make 
 
 #Installing nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
