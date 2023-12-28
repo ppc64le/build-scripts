c8b8e200efbb2fdd917d4566cff93e9cee3e1cf8
saiNammi <118737990+saiNammi@users.noreply.github.com>
2023-12-28 19:36:07 +0530

    Update upload_scanner_results.sh

--- script/upload-scripts/upload_scanner_results.sh
+++ script/upload-scripts/upload_scanner_results.sh
@@ -8,12 +8,21 @@ token_request=$(curl -X POST https://iam.cloud.ibm.com/identity/token \
 #token=$(echo "$token_request" | jq -r '.access_token')
 #curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: application/gzip" -T $1 "https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket/$PACKAGE_NAME/$VERSION/$1"
 
-
+# Check if the token request was successful (HTTP status code 2xx)
 if [[ $(echo "$token_request" | jq -r '.status') =~ ^[2-9][0-9][0-9]$ ]]; then
     token=$(echo "$token_request" | jq -r '.access_token')
     
-    # curl command for uploading the file.
-    curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: application/gzip" -T $1 "https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket/$PACKAGE_NAME/$VERSION/$1"
+    # curl command for uploading the file
+    response=$(curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: application/gzip" -T $1 "https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket/$PACKAGE_NAME/$VERSION/$1")
+
+    # Check if the PUT request was successful (HTTP status code 2xx)
+    if [[ $(echo "$response" | jq -r '.status') =~ ^[2-9][0-9][0-9]$ ]]; then
+        echo "File successfully uploaded."
+    else
+        # Handle PUT request failure
+        echo "Error: PUT request failed. Response: $response"
+        exit 1
+    fi
 else
     # Handle token request failure
     echo "Error: Token request failed. Response: $token_request"
