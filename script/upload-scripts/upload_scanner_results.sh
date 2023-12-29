1e080640dcfbb939ad2736f4ad97395b57abd8ec
saiNammi <118737990+saiNammi@users.noreply.github.com>
2023-12-29 18:35:06 +0530

    Currency travis migration (#3502)
    * Update upload_scanner_results.sh
    
    * Update upload_scanner_results.sh
    
    * Update upload_scanner_results.sh
    
    * Update upload_scanner_results.sh
    
    * Update upload_scanner_results.sh
    
    * Update upload_scanner_results.sh
    
    * Update upload_scanner_results.sh
    
    * Update upload_scanner_results.sh
    
    * Rename upload_scanner_results.sh to upload_logs.sh
    
    * Update upload_logs.sh
    
    * Update currency-build.yml
    
    * Delete script/upload-scripts/upload_build_log.sh
    
    * Update currency-build.yml
    
    * Update upload_docker_image.sh
    
    * Update currency-build.yml
    
    * Update upload_logs.sh
    
    * Update upload_docker_image.sh
    
    * Update upload_docker_image.sh
    
    * Update upload_docker_image.sh
    
    * Update upload_logs.sh
    
    * Update upload_docker_image.sh
    
    * Update currency-build.yml
    
    * Rename upload_logs.sh to upload_file.sh
    
    * Update currency-build.yml

--- script/upload-scripts/upload_scanner_results.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-token_request=$(curl -X POST https://iam.cloud.ibm.com/identity/token \
-  -H "content-type: application/x-www-form-urlencoded" \
-  -H "accept: application/json" \
-  -d "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey=$ibm_cos_api")
-
-token=$(echo "$token_request" | jq -r '.access_token')
-curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: application/gzip" -T $1 "https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket/$PACKAGE_NAME/$VERSION/$1"
