0394d533a704a7357b94c51e3fc8b608c1c73646
saiNammi <118737990+saiNammi@users.noreply.github.com>
2023-12-29 15:13:48 +0530

    Update upload_scanner_results.sh

--- script/upload-scripts/upload_scanner_results.sh
+++ script/upload-scripts/upload_scanner_results.sh
@@ -15,14 +15,14 @@ if [[ $(echo "$token_request" | jq -r '.errorCode') == "null" ]]; then
     # curl command for uploading the file
     response=$(curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: application/gzip" -H "accept: application/json" -T $1 "https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket/$PACKAGE_NAME/$VERSION/$1")
 
-    # Check if the PUT request was successful based on the presence of 'errorCode'
-    if [[ $(echo "$response" | jq -r '.errorCode') == "null" ]]; then
+    # Check if the PUT request was successful based on the absence of an <Error> block
+    if ! echo "$response" | grep -q "<Error>"; then
         echo "File successfully uploaded."
     else
         # Handle PUT request failure
         echo "Error: PUT request failed. Response: $response"
         exit 1
-    fi
+    fi    
 else
     # Handle token request failure
     echo "Error: Token request failed. Response: $token_request"
