eed4015aaece546bcd587fd4b8a7c13853ff47ab
Atharv Phadnis <94899024+atharv-phadnis@users.noreply.github.com>
2024-01-02 11:24:26 +0530

    Currency: Updated build-script, Dockerfile & build_info for Apache-flink (#3465)
    * Added support for Jetty 12.0.*
    
    * Added result message
    
    * Updated apache-flink build-script, Dockerfile & build-info
    
    * Revert jetty

--- a/apache-flink/apache-flink_ubi_8.5.sh
+++ a/apache-flink/apache-flink_ubi_8.5.sh
@@ -2,13 +2,13 @@
 # ----------------------------------------------------------------------------
 # 
 # Package       : Apache Flink
-# Version       : master
+# Version       : release-1.17.1
 # Source repo   : https://github.com/apache/flink
 # Tested on     : UBI: 8.5
 # Language      : Java
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Muskaan Sheik <Muskaan.Sheik@ibm.com>, Vishaka Desai <Vishaka.Desai@ibm.com>
+# Maintainer    : Atharv Phadnis <Atharv.Phadnis@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -19,23 +19,23 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=apache-flink
-PACKAGE_VERSION=${1:-master}
+PACKAGE_VERSION=${1:-release-1.17.1}
 PACKAGE_URL=https://github.com/apache/flink.git
 
-MAVEN_VERSION=3.5.4
+MAVEN_VERSION=3.2.5
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
 # Install dependencies and tools.
 yum update -y
-yum install -y git wget java-1.8.0-openjdk-devel xz
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+yum install -y git wget java-11-openjdk-devel xz
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
 
 # Install postgresql build dependencies
 yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-ppc64le/pgdg-redhat-repo-latest.noarch.rpm
-yum install -y postgresql10-libs
-yum install -y postgresql10 postgresql10-server postgresql10-contrib
+yum install -y postgresql11 postgresql11-libs postgresql11-server postgresql11-contrib
 
 # Install maven package
-wget https://downloads.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
+wget https://archive.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
 tar xzf apache-maven-${MAVEN_VERSION}-bin.tar.gz
 
 # Set ENV variables
@@ -43,12 +43,26 @@ export M2_HOME=`pwd`/apache-maven-${MAVEN_VERSION}
 export PATH=`pwd`/apache-maven-${MAVEN_VERSION}/bin:${PATH}
 
 # Clone and build source
-git clone $PACKAGE_URL
-cd flink
+git clone $PACKAGE_URL $PACKAGE_NAME
+cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-# Compile and build package using threads
-mvn clean package -DskipTests -Pskip-webui-build
+if ! mvn clean package -DskipTests -Pskip-webui-build; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Install_Fails"
+    exit 1
+fi
 
 # Several modules cause test failure, excluding those modules during testing
-mvn test -pl '!flink-core, !flink-runtime, !flink-state-backends/flink-statebackend-rocksdb, !flink-state-backends/flink-statebackend-changelog, !flink-connectors/flink-connector-kafka, !flink-connectors/flink-connector-jdbc, !flink-table/flink-table-runtime, !flink-python, !flink-connectors/flink-connector-hbase-2.2, !flink-filesystems/flink-s3-fs-base, !flink-runtime-web, !flink-filesystems/flink-azure-fs-hadoop, !flink-yarn, !flink-fs-tests, !flink-filesystems/flink-hadoop-fs, !flink-formats/flink-hadoop-bulk'
+if ! mvn test -pl '!flink-core, !flink-runtime, !flink-state-backends/flink-statebackend-rocksdb, !flink-state-backends/flink-statebackend-changelog, !flink-connectors/flink-connector-kafka, !flink-table/flink-table-runtime, !flink-python, !flink-connectors/flink-connector-hbase-2.2, !flink-filesystems/flink-s3-fs-base, !flink-runtime-web, !flink-filesystems/flink-azure-fs-hadoop, !flink-yarn, !flink-fs-tests, !flink-filesystems/flink-hadoop-fs, !flink-formats/flink-hadoop-bulk, !flink-filesystems/flink-s3-fs-hadoop, !flink-filesystems/flink-s3-fs-presto'; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Install_success_but_test_Fails"
+    exit 2
+else
+	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass | Both_Install_and_Test_Success"
+	exit 0
+fi
