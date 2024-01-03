99b8ce18692ed447dd3c7f9ca982affdae74d2c2
mohitp333 <mohit.pawar@.com>
2024-01-03 09:53:21 -0500

    modified install and test steps

--- s/spring-retry/spring_retry_ubi_8.7.sh
+++ s/spring-retry/spring_retry_ubi_8.7.sh
@@ -45,6 +45,22 @@ mvn --version
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
-mvn -f pom.xml clean install -DskipTests
-#mvn install
-mvn test
+
+if ! mvn clean install ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi 
+
+if ! mvn test ; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
