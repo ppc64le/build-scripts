e259ad32bed14dae8298c71984fe08c48698d2b5
AbhiIBM <137172943+AbhiIBM@users.noreply.github.com>
2024-01-24 03:13:37 +0530

    updated

--- script/validate_builds.py
+++ script/validate_builds.py
@@ -109,13 +109,12 @@ def validate_build_info_file(file_name):
         # Check for valid Github url
         print(str(data['github_url']))
         if str(data['github_url']).endswith('/'):
-            raise Exception(f"Build info validation failed for {file_name} due to invalid github url")
+            raise Exception(f"Build info validation failed for {file_name} due to invalid github url !")
         
         # Check for empty lines
-
         for line in data :
             if line.strip():
-                raise Exception(f"Build info validation failed for {file_name} due to empty line present")
+                raise Exception(f"Build info validation failed for {file_name} due to empty line present !")
 
         # Check for mandatory fields.
         for field in mandatory_fields:
