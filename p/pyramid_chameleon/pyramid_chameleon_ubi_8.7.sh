4ca19f6a5dc453429d149bbae92df42cfe2ca65c
mohitp333 <mohit.pawar@.com>
2024-02-08 10:23:32 -0500

    fixed tox command not found

--- p/pyramid_chameleon/pyramid_chameleon_ubi_8.7.sh
+++ p/pyramid_chameleon/pyramid_chameleon_ubi_8.7.sh
@@ -43,7 +43,7 @@ if ! python3 setup.py install ; then
     exit 1
 fi
 
-if ! tox -e py3.11 ; then
+if ! tox -e py39 ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
