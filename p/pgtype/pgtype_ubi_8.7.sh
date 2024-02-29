0b4165a3630e8b8659cb679444f7f078e5891b0d
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-02-29 09:52:44 +0530

    Update pgtype_ubi_8.7.sh

--- p/pgtype/pgtype_ubi_8.7.sh
+++ p/pgtype/pgtype_ubi_8.7.sh
@@ -47,11 +47,11 @@ psql "host=127.0.0.1 dbname=pgx_test user=postgres password=secret sslmode=disab
 psql "host=127.0.0.1 dbname=pgx_test user=postgres password=secret sslmode=disable" -c "CREATE EXTENSION ltree;"
 psql "host=127.0.0.1 dbname=pgx_test user=postgres password=secret sslmode=disable" -c "CREATE TYPE int8multirange AS RANGE (subtype = bigint);"
 psql "host=127.0.0.1 dbname=pgx_test user=postgres password=secret sslmode=disable" -c "CREATE TYPE int4multirange AS RANGE (subtype = bigint);"
-#psql "host=127.0.0.1 dbname=pgx_test user=postgres password=secret sslmode=disable" -c "SET client_encoding=UTF8;"
-psql "host=127.0.0.1 dbname=pgx_test user=postgres password=secret sslmode=disable options='-c client_encoding=UTF8'"
-
-#sed -i '727d' /var/lib/pgsql/16/data/postgresql.conf
-#sed -i '727 i\ client_encoding=UTF8' /var/lib/pgsql/16/data/postgresql.conf
+# Set client encoding to UTF8 in postgresql.conf using ALTER SYSTEM
+echo "ALTER SYSTEM SET client_encoding TO 'UTF8';" | sudo -u postgres /usr/pgsql-16/bin/postgres --single -D /var/lib/pgsql/16/data
+# Restart PostgreSQL to apply the changes
+sudo -u postgres /usr/pgsql-16/bin/pg_ctl -D /var/lib/pgsql/16/data restart
+# Check if client encoding is set to UTF8
 psql -h 127.0.0.1 -p 5432 -U postgres -d pgx_test -c "SHOW client_encoding;"
 
 if ! go build ./... ; then
