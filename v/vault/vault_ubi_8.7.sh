2f39b6941852ca4520a383464e38f99ef4e57211
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-03-14 11:33:02 +0530

    Update vault_ubi_8.7.sh

--- v/vault/vault_ubi_8.7.sh
+++ v/vault/vault_ubi_8.7.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -e
 # -----------------------------------------------------------------------------
 #
-# Package	        : vault
-# Version	        : v1.11.2, v1.11.3, v1.12.3,v1.13.1,v1.14.0
-# Source repo	    : https://github.com/hashicorp/vault
-# Tested on   	   : UBI: 8.7
-# Language        : Go
-# Travis-Check    : True
-# Script License  : Apache License, Version 2 or later
-# Maintainer	     : Vinod K <Vinod.K1@ibm.com>
+# Package       : vault
+# Version       : v1.11.2, v1.11.3, v1.12.3,v1.13.1,v1.14.0
+# Source repo   : https://github.com/hashicorp/vault
+# Tested on     : UBI 8.7
+# Language      : Go
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Vinod K <Vinod.K1@ibm.com>
 #
-# Disclaimer      : This script has been tested in root mode on given
-# ==========        platform using the mentioned version of the package.
-#                   It may not work as expected with newer versions of the
-#                   package and/or distribution. In such case, please
-#                   contact "Maintainer" of this script.
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
 
@@ -42,20 +42,20 @@ cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 if ! make ; then
-    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "------------------$PACKAGE_NAME:Build_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_Fails"
     exit 1
 fi
 
 if ! make testrace TEST=./vault ; then
-    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "------------------$PACKAGE_NAME:Build_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_success_but_test_Fails"
     exit 2
 else
-    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "------------------$PACKAGE_NAME:Build_&_test_both_success-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Build_and_Test_Success"
     exit 0
 fi
