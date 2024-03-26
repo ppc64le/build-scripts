9aa30c5e2fcc3d796c743ccbb173dea30d6d2891
Shubham Garud <Shubham.Garud@ibm.com>
2024-03-26 02:28:20 -0400

    Updated UBI version,updated tags

--- u/unist-util-visit/unist-util-visit_ubi_9.3.sh
+++ u/unist-util-visit/unist-util-visit_ubi_9.3.sh
@@ -4,7 +4,7 @@
 # Package          : unist-util-visit
 # Version          : 5.0.0
 # Source repo      : https://github.com/syntax-tree/unist-util-visit
-# Tested on	: UBI 8.7
+# Tested on	: UBI 9.3
 # Language      : Node
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
