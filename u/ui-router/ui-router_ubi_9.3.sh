43e4ffdf95ad76123901395f5f95d04c535bfd74
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-03-28 14:01:45 +0530

    Currency:Adding build-script,build_info.json for ui-router. (#3658)
    * Create ui-router_ubi_9.3.sh
    
    * Create build_info.json
    
    * Create LICENSE

---
+++ u/ui-router/ui-router_ubi_9.3.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e 
+# -----------------------------------------------------------------------------
+#
+# Package          : ui-router
+# Version          : 1.1.0
+# Source repo      : https://github.com/angular-ui/ui-router
+# Tested on	       : UBI 9.3
+# Language         : Typescript
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : vinodk99 <Vinod.K1@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+PACKAGE_NAME=ui-router
+PACKAGE_VERSION=${1:-1.1.0}
+PACKAGE_URL=https://github.com/angular-ui/ui-router
+
+yum install -y git wget gcc gcc-c++ curl 
+export NODE_VERSION=${NODE_VERSION:-20}
+
+#Installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source "$HOME"/.bashrc
+echo "installing nodejs $NODE_VERSION"
+nvm install "$NODE_VERSION" >/dev/null
+nvm use $NODE_VERSION
+
+git clone $PACKAGE_URL $PACKAGE_NAME
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+source "$HOME"/.bashrc
+npm install -g yarn
+
+if ! yarn install --immutable ; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! yarn test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
