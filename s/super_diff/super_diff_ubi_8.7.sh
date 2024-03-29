44cabd35b295377b8f71ee6603fef7481b4a87e2
SaiKumarSaddi <157705672+SaiKumarSaddi@users.noreply.github.com>
2024-03-29 17:35:21 +0530

    Update super_diff_ubi_8.7.sh

--- s/super_diff/super_diff_ubi_8.7.sh
+++ s/super_diff/super_diff_ubi_8.7.sh
@@ -18,53 +18,62 @@
 #
 # ----------------------------------------------------------------------------
 
-PACKAGE_NAME=super_diff
-PACKAGE_VERSION=${1:-v0.11.0}
-PACKAGE_URL=https://github.com/mcmire/super_diff.git
 
-yum install -y git procps yum-utils wget xz
-yum install sudo -y
-yum install -y gcc openssl-devel libyaml-devel libffi-devel zlib-devel ncurses-devel libxml2-devel libxslt-devel zlib-devel libxml2
+PKG_NAME="super_diff"
+PKG_VERSION="${1:-v0.11.0}"
+REPOSITORY="https://github.com/mcmire/super_diff.git"
 
-curl https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer | bash
+echo "Usage: $0 [r<PKG_VERSION>]"
+echo "       PKG_VERSION is an optional paramater whose default value is v0.11.0"
+
+yum -y update
+yum install -y git procps ncurses-devel wget m4 redhat-rpm-config xz 
+
+
+wget https://rpmfind.net/linux/centos/8-stream/AppStream/ppc64le/os/Packages/bison-3.0.4-10.el8.ppc64le.rpm
+wget https://rpmfind.net/linux/centos/8-stream/BaseOS/ppc64le/os/Packages/readline-devel-7.0-10.el8.ppc64le.rpm
+rpm -i *.rpm
+
+
+git clone $REPOSITORY $PKG_NAME-$PKG_VERSION
+cd $PKG_NAME-$PKG_VERSION/
+git checkout $PKG_VERSION
+
+# setup build environment
+sed -i s/sudo//g bin/setup
+bin/setup
+
+# install ruby version manager (rvm) and get required ruby version
+# link: https://rvm.io/
+gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+
 source /etc/profile.d/rvm.sh
-export RUBY_VERSION=${RUBY_VERSION:-3.2.1}
+rvm -v
 
-dnf install -qy http://mirror.nodesdirect.com/centos/8-stream/BaseOS/ppc64le/os/Packages/centos-gpg-keys-8-6.el8.noarch.rpm
-dnf install -qy http://mirror.nodesdirect.com/centos/8-stream/BaseOS/ppc64le/os/Packages/centos-stream-repos-8-6.el8.noarch.rpm
-rvm install "$RUBY_VERSION"
-ruby --version
+yum install -y libyaml-devel
+rvm install "ruby-$(cat .ruby-version)"
 
 gem install bundle
-gem install rake
-gem install bundler
-gem install nokogiri
-gem install rspec
-gem install zeus
-gem install kramdown-parser-gfm
-
-
-git clone ${PACKAGE_URL} 
-cd ${PACKAGE_NAME}
-git checkout ${PACKAGE_VERSION}
 
 export BUNDLE_GEMFILE=gemfiles/rails_7_0_rspec_lt_3_10.gemfile
 
-if ! bundle install ;  then
-    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
-    exit 1
-fi
+# install dependent gems
+bundle install
 
-if ! bundle exec rake --trace ; then
-    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-    exit 2
+
+
+# build package
+gem build *.gemspec
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "Build failed "
 else
-    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-    exit 0
+    bundle exec rake
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        echo "Test failed"
+    else
+        echo "Build & Test Successful "
+    fi
 fi
