6f59419cb3fbd9ef2111f4a8cf37320f6caefc3b
stutiibm <Stuti.Wali@ibm.com>
2024-04-01 19:14:48 +0530

    Update loopback-connector_ubi_8.7.sh

--- l/loopback-connector/loopback-connector_ubi_8.7.sh
+++ l/loopback-connector/loopback-connector_ubi_8.7.sh
@@ -22,7 +22,7 @@ PACKAGE_VERSION=${1:-v6.1.0}
 PACKAGE_URL=https://github.com/loopbackio/loopback-connector
 
 export NODE_VERSION=${NODE_VERSION:-16}
-yum install -y python38 python38-devel git gcc gcc-c++ libffi make
+yum install -y  git gcc gcc-c++ libffi make
 
 #Installing nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
