7572cac490bac3a016fa186ea41ac0bade02fbad
nikhil-kalbande <82815888+nikhil-kalbande@users.noreply.github.com>
2024-04-03 15:35:48 +0530

    Update pre_process.sh to test javascript package failures

--- script/pre_process.sh
+++ script/pre_process.sh
@@ -17,15 +17,15 @@ for language in "${langs[@]}"; do
         if [ -f "package-lock.json" ] || [ -f "yarn.lock" ]; then
 	    sudo chown travis:travis -R .
      	    if [ -f ${nvm_path} ]; then
-	  	sudo source ${nvm_path}
+	  	source ${nvm_path}
     	    else
 	    	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-            	sudo source ~/.bashrc
+            	source ~/.bashrc
             	sudo nvm install 16
 	    fi
 	    sudo npm install -g yarn
-            sudo yarn import || true
-            sudo rm -rf node_modules/ package-lock.json
+            yarn import || true
+            rm -rf node_modules/ package-lock.json
 	    ls -ltr
         fi
     fi
