b16e4f960b7ac43bb91a7467410b204a312ac20d
saiNammi <118737990+saiNammi@users.noreply.github.com>
2024-04-04 16:49:02 +0530

    Update grype_code_scan.sh

--- script/scanner-scripts/grype_code_scan.sh
+++ script/scanner-scripts/grype_code_scan.sh
@@ -10,6 +10,7 @@ if [ $validate_build_script == true ];then
      tar -xf grype_0.67.0_linux_ppc64le.tar.gz
      chmod +x grype
      sudo mv grype /usr/bin                      
+     echo "Executing Grype scanner"
      sudo grype -q -o cyclonedx-json dir:${cloned_package} > grype_source_sbom_results.json    
      #cat grype_source_sbom_results.json 
      sudo grype -q -o json dir:${cloned_package} > grype_source_vulnerabilities_results.json
