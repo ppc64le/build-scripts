183e0b71d70407cff459e54ad602228999845ed7
saiNammi <118737990+saiNammi@users.noreply.github.com>
2024-04-04 13:32:36 +0530

    Update pre_process.sh

--- script/pre_process.sh
+++ script/pre_process.sh
@@ -21,8 +21,8 @@ for language in "${langs[@]}"; do
     	    else
 	    	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
             	source ~/.bashrc
+	        nvm install 16
 	    fi
-     	    nvm install 16
        	    nvm use 16
 	    node --version
 	    npm install -g yarn
