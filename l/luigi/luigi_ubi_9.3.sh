74579fc13ee907982ab75d3b57b708c37099d316
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-04-05 16:05:35 +0530

    Update luigi_ubi_9.3.sh

--- l/luigi/luigi_ubi_9.3.sh
+++ l/luigi/luigi_ubi_9.3.sh
@@ -4,7 +4,7 @@
 # Package          : luigi
 # Version          : 3.5.0
 # Source repo      : https://github.com/spotify/luigi.git
-# Tested on        : UBI 9.3
+# Tested on        : UBI:9.3
 # Language         : Python,Javascript
 # Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
