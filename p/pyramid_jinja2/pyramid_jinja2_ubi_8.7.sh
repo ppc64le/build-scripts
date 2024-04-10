c36cd581872ecd71b1fae37dcfa6a55250846c8a
mohitp333 <mohit.pawar@ibm.com>
2024-04-10 16:15:26 -0400

    rollback changes

--- p/pyramid_jinja2/pyramid_jinja2_ubi_8.7.sh
+++ p/pyramid_jinja2/pyramid_jinja2_ubi_8.7.sh
@@ -25,21 +25,11 @@ PACKAGE_VERSION=${1:-"2.10"}
 PACKAGE_URL=https://github.com/Pylons/pyramid_jinja2.git
 
 yum install -y git gcc gcc-c++ yum-utils make automake autoconf libtool gdb* binutils rpm-build gettext wget
-<<<<<<< HEAD
-#yum install -y python39 python39-devel python39-setuptools
-yum install -y python3 python3-devel python3-setuptools
-=======
 yum install -y python39 python39-devel python39-setuptools
->>>>>>> master
 
 python3 -m ensurepip --upgrade
 python3 -m pip install build tox webtest
 
-<<<<<<< HEAD
-pip install --upgrade jinja2==3.0.3
-
-=======
->>>>>>> master
 # Clone and build source code.
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
