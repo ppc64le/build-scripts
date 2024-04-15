33d2f45c53d89801ad142f673d1c19ec8a62966d
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-04-15 14:09:42 +0530

    Update rethinkdb_ubi_9.3.sh

--- r/rethinkdb/rethinkdb_ubi_9.3.sh
+++ r/rethinkdb/rethinkdb_ubi_9.3.sh
@@ -21,8 +21,17 @@ PACKAGE_VERSION=${1:-v2.4.4}
 PACKAGE_URL=https://github.com/rethinkdb/rethinkdb.git
 PACKAGE_NAME=rethinkdb
 
-yum install -y patch bzip2 git make gcc-c++ openssl-devel tar libcurl-devel wget m4 ncurses-devel libicu-devel python3 python3-devel protobuf-c
+yum install -y patch bzip2 git make gcc-c++ openssl-devel tar libcurl-devel wget m4 ncurses-devel libicu-devel python3 python3-devel 
 yum install -y https://dl.fedoraproject.org/pub/epel/9/Everything/ppc64le/Packages/e/epel-release-9-7.el9.noarch.rpm
+
+#Install protobuf-c
+wget https://github.com/protocolbuffers/protobuf/releases/download/v3.19.4/protobuf-cpp-3.19.4.tar.gz
+tar -xzf protobuf-cpp-3.19.4.tar.gz
+cd protobuf-3.19.4
+./configure --prefix=/usr/local
+make 
+make install
+protoc --version
 
 #Install nodejs
 export NODE_VERSION=${NODE_VERSION:-16}
@@ -34,12 +43,13 @@ nvm use $NODE_VERSION
 
 ln -s /usr/bin/python3 /usr/bin/python
 
+
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 ./configure --allow-fetch
 
-if ! make -j8; then
+if ! make && make install ; then
     echo "------------------$PACKAGE_NAME:Build_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
