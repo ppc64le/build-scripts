c994dc96001d5baddc278e86100ef56b4815f2fc
stutiibm <Stuti.Wali@ibm.com>
2024-04-22 19:19:22 +0530

    Currency : updated script

--- d/d3-dispatch/d3-dispatch_ubi_9_3.sh
+++ d/d3-dispatch/d3-dispatch_ubi_9_3.sh
@@ -44,7 +44,7 @@ cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 # Build the package
-if !(npm install; npm audit fix ; npm audit fix --force); then
+if !(npm install; npm audit fix; npm audit fix --force); then
     echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_Fails"
