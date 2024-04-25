c5b6c4e57fc983f996400fb1c3994bd869108a18
AshwiniAdwalpalkar <ashwiniadwalpalkar@gmail.com>
2024-04-25 03:49:58 +0000

    Currency:Modified build script file

--- m/moment/moment_ubi_9_3.sh
+++ m/moment/moment_ubi_9_3.sh
@@ -44,7 +44,7 @@ cd moment
 git checkout $PACKAGE_VERSION
 
 #Build Package
-if ! npm install && npm audit fix && npm audit fix --force ;
+if ! npm install && npm audit fix && npm audit fix --force;
 then
         echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
