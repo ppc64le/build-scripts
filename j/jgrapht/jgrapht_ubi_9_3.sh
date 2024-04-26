9449dc9917898440366299b853236794da48c9fa
stutiibm <Stuti.Wali@ibm.com>
2024-04-26 09:23:20 +0530

    Update jgrapht_ubi_9_3.sh

--- j/jgrapht/jgrapht_ubi_9_3.sh
+++ j/jgrapht/jgrapht_ubi_9_3.sh
@@ -1,3 +1,24 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+#
+# Package       : jgrapht
+# Version       : jgrapht-1.5.2
+# Source repo   : https://github.com/jgrapht/jgrapht
+# Tested on     : UBI: 9.3
+# Language      : java
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Stuti Wali <Stuti.Wali@ibm.com>
+#
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
 set -e
 export PACKAGE_NAME=jgrapht
 export PACKAGE_URL=https://github.com/jgrapht/jgrapht
