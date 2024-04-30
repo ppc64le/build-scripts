1e02637ed0562cbdb3c2b7637f7e6004acda9c1d
Md. Shafi Hussain <hussainshafi46@gmail.com>
2024-04-30 16:44:45 +0530

    Modified Script & License

--- r/react-grid-layout/react-grid-layout_ubi_9.3.sh
+++ r/react-grid-layout/react-grid-layout_ubi_9.3.sh
@@ -17,42 +17,51 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-#Exit immediately if a command exits with a non-zero status.
-set -e
 
 PACKAGE_NAME=react-grid-layout
 PACKAGE_VERSION=${1:-1.4.1}
 PACKAGE_URL=https://github.com/STRML/react-grid-layout
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+export NODE_VERSION=${NODE_VERSION:-18}
+export NODE_OPTIONS="--dns-result-order=ipv4first"
 
 yum update -yq && yum install -yq git make
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
-nvm install 18
-export NODE_OPTIONS="--dns-result-order=ipv4first"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source "$HOME"/.bashrc
+echo "installing nodejs $NODE_VERSION"
+nvm install "$NODE_VERSION" >/dev/null
+nvm use $NODE_VERSION
 npm i --global yarn
 
-git clone $PACKAGE_URL $PACKAGE_NAME
+if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
+    exit 0
+fi
+
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-# install deps
-if ! yarn; then
-    echo "yarn dependency installation failed"
+# install deps & build
+if ! yarn && yarn build; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
-# Build
-if echo "Starting BUILD..." && ! yarn build; then
-    echo "!!    BUILD FAILED    !!"
-    exit 1
-elif echo "Starting TESTS..." && ! yarn test; then
-    echo "!!    TESTS FAILED    !!"
+# test
+if ! yarn test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
     exit 1
 else
-    echo "!!    BUILD & TESTS SUCCESSFUL    !!"
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
 fi
-
