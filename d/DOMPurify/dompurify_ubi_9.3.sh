9b79486f00c7b3eaea5bf1d6f9bad46fbd7d241b
Md. Shafi Hussain <hussainshafi46@gmail.com>
2024-04-30 15:18:43 +0530

    Modified base directory name, License, build script, build_info.json

--- d/DOMPurify/dompurify_ubi_9.3.sh
+++ d/DOMPurify/dompurify_ubi_9.3.sh
@@ -8,8 +8,8 @@
 # Tested on         : RHEL 9.3
 # Language          : Node
 # Travis-Check      : True
-# Script License    : Apache License, Version 2.0 or Mozilla Public License, version 2.0
-# Maintainer        : Md. Shafi Hussain <Md.Hussain@ibm.com>
+# Script License    : Apache License, Version 2.0
+# Maintainer        : Vinod K <Vinod.K1@ibm.com>
 #
 # Disclaimer        : This script has been tested in root mode on given
 # ==========          platform using the mentioned version of the package.
@@ -23,6 +23,7 @@ PACKAGE_NAME=dompurify
 #PACKAGE_VERSION is configurable can be passed as an argument.
 PACKAGE_VERSION=${1:-3.1.0}
 PACKAGE_URL=https://github.com/cure53/DOMPurify
+export NODE_VERSION=${NODE_VERSION:-21}
 
 yum install -y yum-utils git jq \
     https://mirror.stream.centos.org/9-stream/BaseOS/ppc64le/os/Packages/centos-gpg-keys-9.0-24.el9.noarch.rpm \
@@ -32,29 +33,22 @@ yum install -y firefox
 
 export MOZ_HEADLESS=1
 
-NODE_VERSION=21
 #installing nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-source ~/.bashrc
-nvm install $NODE_VERSION
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source "$HOME"/.bashrc
+echo "installing nodejs $NODE_VERSION"
+nvm install "$NODE_VERSION" >/dev/null
+nvm use $NODE_VERSION
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
-#Check if package exists
-if [ -d "$PACKAGE_NAME" ]; then
-    rm -rf $PACKAGE_NAME
-    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
-
-fi
-
-if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
+if ! git clone $PACKAGE_URL && cd "$_"; then
     echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
     exit 0
 fi
 
-cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 PACKAGE_VERSION=$(jq -r ".version" package.json)
 # run the test command from test.sh
