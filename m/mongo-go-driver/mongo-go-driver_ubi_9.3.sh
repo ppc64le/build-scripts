061025540ec6667678f26a898b6cbc04f68d82a8
Dipti <diptikri254@gmail.com>
2024-04-30 12:19:34 +0530

    Script updated

--- m/mongo-go-driver/mongo-go-driver_ubi_9.3.sh
+++ m/mongo-go-driver/mongo-go-driver_ubi_9.3.sh
@@ -8,7 +8,7 @@
 # Language      : Go
 # Travis-Check  : False
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Dipti Kumari <Shreya.Kajbaje@ibm.com>
+# Maintainer    : Dipti Kumari 
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -17,47 +17,43 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-#install mongodb
-yum install https://repo.mongodb.com/yum/redhat/8/mongodb-enterprise/7.0/ppc64le/RPMS/mongodb-enterprise-server-7.0.8-1.el8.ppc64le.rpm
-
-#install golang
-yum install -y wget
-yum install golang -y
-wget https://golang.org/dl/go1.17.5.linux-ppc64le.tar.gz
-tar -C /usr/local/ -xzf go1.17.5.linux-ppc64le.tar.gz
-cd /usr/local/
-vi ~/.bash_profile
-export PATH=$PATH:/usr/local/go/bin
-:wq
-source ~/.bash_profile
-#check version
-go version
-cd /usr/local/src
-
-#clone repo
 PACKAGE_PATH=github.com/mongodb/
 PACKAGE_NAME=mongo-go-driver
 PACKAGE_VERSION=v1.15.0
 PACKAGE_URL=https://github.com/mongodb/mongo-go-driver
 
+# Install dependencies
 yum install -y make git wget gcc
-yum install git -y
-mkdir $PACKAGE_PATH
+
+# Download and install go
+wget https://golang.org/dl/go1.17.5.linux-ppc64le.tar.gz
+tar -xzf go1.17.5.linux-ppc64le.tar.gz
+rm -rf go1.17.5.linux-ppc64le.tar.gz
+export GOPATH=`pwd`/gopath
+export PATH=`pwd`/go/bin:$GOPATH/bin:$PATH
+
+# Clone the repo and checkout submodules
+mkdir -p $GOPATH/src/$PACKAGE_PATH
+cd $GOPATH/src/$PACKAGE_PATH
 git clone $PACKAGE_URL
-cd mongo-go-driver
-git checkout v1.15.0
-echo "Building github.com/mongodb/mongo-go-driver v1.15.0"
-go get go.mongodb.org/mongo-driver/mongo
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+#install mongodb
+yum install https://repo.mongodb.com/yum/redhat/8/mongodb-enterprise/7.0/ppc64le/RPMS/mongodb-enterprise-server-7.0.8-1.el8.ppc64le.rpm
+
+echo "Building $PACKAGE_PATH$PACKAGE_NAME with $PACKAGE_VERSION"
 go mod init
 go mod tidy
 go mod vendor
 
-#install python mongodb
-yum install python3-devel -y
-systemctl start mongod
-systemctl status mongod
+if ! go build -v ./...; then
+        echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
+        exit 1
+fi
+
+echo "Testing $PACKAGE_PATH$PACKAGE_NAME with $PACKAGE_VERSION"
 cd x/mongo/driver/topology/
-go build -v ./...
 if ! go test -v ./...; then
         echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
         exit 1
@@ -65,5 +61,3 @@ else
         echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
     exit 0
 fi
-
-
