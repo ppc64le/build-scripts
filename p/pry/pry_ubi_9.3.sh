dbd35bd8628360c600558f46789a70cfe8eee6ce
Md. Shafi Hussain <84100241+hussainshafi46@users.noreply.github.com>
2024-05-02 13:54:32 +0530

    Update pry_ubi_9.3.sh

--- p/pry/pry_ubi_9.3.sh
+++ p/pry/pry_ubi_9.3.sh
@@ -41,13 +41,7 @@ rvm install ruby-3.3.0
 
 gem install bundle
 
-if ! git clone $PACKAGE_URL $PACKAGE_NAME; then
-    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Clone_Fails"
-    exit 0
-fi
-
+git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
