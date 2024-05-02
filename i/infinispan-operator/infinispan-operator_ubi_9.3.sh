ab8877fa3dc1d23e080279ffcee803e9f3aff751
Shubham Bhagwat <118804486+shubham-bhagwat0@users.noreply.github.com>
2024-05-02 18:51:03 +0530

    Update and rename infinispan-operator_ubi_8.7.sh to infinispan-operator_ubi_9.3.sh

--- i/infinispan-operator/infinispan-operator_ubi_9.3.sh
+++ i/infinispan-operator/infinispan-operator_ubi_9.3.sh
@@ -2,8 +2,8 @@
 #
 # Package	: infinispan-operator
 # Version	: 2.4.0.Final
-# Source repo	: https://github.com/infinispan/infinispan-operator
-# Tested on	: UBI 8.7
+# Source repo	: https://github.com/infinispan/infinispan-operator.git
+# Tested on	: Red Hat Enterprise Linux 87(8.7) && UBI 9 (9.3)
 # Language      : Go
 # Travis-Check  : False
 # Script License: Apache License, Version 2 or later
@@ -21,28 +21,30 @@
 
 PACKAGE_NAME=infinispan-operator
 PACKAGE_VERSION=2.4.0.Final
-PACKAGE_URL=https://github.com/infinispan/infinispan-operator
+PACKAGE_URL=https://github.com/infinispan/infinispan-operator.git
 PACKAGE_BRANCH=2.4.0.Final
 
-GO_VERSION=1.21.8
+
+GO_VERSION=1.21.6
 
 #Install the required dependencies
 yum install git gcc make wget tar zip -y
 
 # Install Go and setup working directory
-wget https://golang.org/dl/go$GO_VERSION.linux-ppc64le.tar.gz
-tar -C /bin -xf go$GO_VERSION.linux-ppc64le.tar.gz
-rm -f go$GO_VERSION.linux-ppc64le.tar.gz
-
-export PATH=$PATH:/bin/go/bin
-export GOPATH=/home/go
+wget https://go.dev/dl/go1.21.6.linux-ppc64le.tar.gz
+tar -C  /usr/local -xf go1.21.6.linux-ppc64le.tar.gz
+export GOROOT=/usr/local/go
+export GOPATH=$HOME
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 mkdir -p $GOPATH/src && cd $GOPATH/src
+
 # Install operator-sdk
 wget https://github.com/operator-framework/operator-sdk/releases/download/v1.24.1/operator-sdk_linux_ppc64le
 chmod +x operator-sdk_linux_ppc64le
 mv operator-sdk_linux_ppc64le /usr/local/bin/operator-sdk
 operator-sdk version
+
 
 #Clone the repository
 git clone $PACKAGE_URL
@@ -50,19 +52,23 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
+#Check if package exists
+if [ -d "$PACKAGE_NAME" ] ; then
+  rm -rf $PACKAGE_NAME
+  echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Removed existing package if any"
+fi
+
+if ! git clone $PACKAGE_URL -b $PACKAGE_BRANCH ; then
+    echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    exit 0
+fi
+
 # Increase timeout to resolve golangcli-lint timeout error
 /home/go/src/infinispan-operator/bin/golangci-lint run --enable errorlint --timeout=10m
 go mod vendor
+make lint
 
-#build
-if ! make lint ; then
-    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
-    exit 1
-fi
-
-#test
 if ! make test; then
 	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
