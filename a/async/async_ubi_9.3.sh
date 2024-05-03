527df67b397f05925bd9ee2d39edd963c47c5ad7
mohitp333 <mohit.pawar@ibm.com>
2024-05-03 02:57:19 -0400

    fixes

--- a/async/async_ubi_9.3.sh
+++ a/async/async_ubi_9.3.sh
@@ -4,7 +4,7 @@
 # Package           : async
 # Version           : v3.2.5
 # Source repo       : https://github.com/caolan/async.git
-# Tested on         : UBI: 9.3
+# Tested on         : UBI:9.3
 # Language          : JavaScript
 # Travis-Check      : True
 # Script License    : Apache License, Version 2 or later
