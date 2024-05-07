0494771839d22297ee61344150965c1c5f03ff9c
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-05-07 11:31:19 +0530

    Currency:Updated folder and filenames for xxhash (#3786)
    * Currency:Updated folder and filenames for xxhash
    
    * Update and rename xxHash_rhel_7.3.sh to xxhash_rhel_7.3.sh
    
    * Update and rename xxHash_ubuntu_16.04.sh to xxhash_ubuntu_16.04.sh
    
    * Rename xxHash_ubi_8.7.sh to xxhash_ubi_8.7.sh
    
    * Update build_info.json
    
    * Delete x/xxhash/xxhash_rhel_7.3.sh
    
    * Delete x/xxhash/xxhash_ubuntu_16.04.sh

---
+++ x/xxhash/xxhash_ubi_9.3.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : xxHash
+# Version       : v0.8.2
+# Source repo   : https://github.com/Cyan4973/xxHash
+# Tested on     : UBI:9.3
+# Language      : C
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Vinod K <Vinod.K1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+PACKAGE_NAME=xxHash
+PACKAGE_VERSION=${1:-v0.8.2}
+PACKAGE_URL=https://github.com/Cyan4973/xxHash
+
+yum install -y git gcc-c++ gcc wget make
+
+git clone $PACKAGE_URL 
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! make && make install ;  then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! make test ; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
