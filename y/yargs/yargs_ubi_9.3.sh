3c3d6de3eedbb0e6a68af56bdcfe8050d8ba79b0
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-05-08 11:23:09 +0530

    Currency:Updated build-script,build_info.json for yargs (#3807)
    * Currency:Updated build-script,build_info.jonf for yargs
    
    * Update yargs_ubi_9.3.sh
    
    * Update yargs_ubi_9.3.sh
    
    * Update yargs_ubi_9.3.sh

---
+++ y/yargs/yargs_ubi_9.3.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : yargs
+# Version          : v17.7.2
+# Source repo      : https://github.com/yargs/yargs
+# Tested on        : UBI:9.3
+# Language         : Javascript,Typescript
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vinod K <Vinod.K1@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=yargs
+PACKAGE_VERSION=${1:-v17.7.2}
+PACKAGE_URL=https://github.com/yargs/yargs
+
+export NODE_VERSION=${NODE_VERSION:-20}
+
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+yum install -y git wget gcc-c++ gcc libcurl-devel
+
+#Installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source "$HOME"/.bashrc
+echo "installing nodejs $NODE_VERSION"
+nvm install "$NODE_VERSION" >/dev/null
+nvm use $NODE_VERSION
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! npm --before 2023-04-27 install; then
+       echo "------------------$PACKAGE_NAME:Install_fails---------------------"
+       echo "$PACKAGE_VERSION $PACKAGE_NAME"
+       echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+       exit 1
+fi
+
+if !  npm test ; then
+      echo "------------------$PACKAGE_NAME::Install_and_Test_fails-------------------------"
+      echo "$PACKAGE_URL $PACKAGE_NAME"
+      echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+      exit 2
+else
+      echo "------------------$PACKAGE_NAME::Build_and_Test_success-------------------------"
+      echo "$PACKAGE_URL $PACKAGE_NAME"
+      echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+      exit 0
+fi
