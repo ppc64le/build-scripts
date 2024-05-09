6e336cc71bd4fd6f2d2d3a7fc53ab6dff59b1673
mohitp333 <mohit.pawar@ibm.com>
2024-05-09 01:00:44 -0400

    fixed node-fetch v3.3.2

--- n/node-fetch/node-fetch_ubi_9.3.sh
+++ n/node-fetch/node-fetch_ubi_9.3.sh
@@ -4,7 +4,7 @@
 # Package           : node-fetch
 # Version           : v3.3.2
 # Source repo       : https://github.com/node-fetch/node-fetch.git
-# Tested on         : UBI 9.3
+# Tested on         : UBI:9.3
 # Language          : JavaScript
 # Travis-Check      : True
 # Script License    : Apache License, Version 2 or later
@@ -30,26 +30,42 @@ source "$HOME"/.bashrc
 echo "installing nodejs $NODE_VERSION"
 nvm install "$NODE_VERSION" >/dev/null
 nvm use $NODE_VERSION
+HOME_DIR=$PWD
 
 git clone $PACKAGE_URL $PACKAGE_NAME
 cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-if ! npm install && npm audit fix --force; then
-    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
-    exit 1
+echo "cloning..."
+if ! git clone -q $PACKAGE_URL $PACKAGE_NAME; then
+	echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL |  $PACKAGE_VERSION | GitHub | Fail |  Clone_Fails"
+	exit 1
 fi
-
+cd "$HOME_DIR"/$PACKAGE_NAME || exit 1
+git checkout "$PACKAGE_VERSION" || exit 1
+if ! npm install && npm audit fix; then
+	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+	exit 1
+fi
+cd "$HOME_DIR"/$PACKAGE_NAME || exit 1
+if ! npm run | grep -q "test"; then
+	echo "------------------$PACKAGE_NAME:install_success_but_test_not_present---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub |  |  Install_success_but_test_not_present"
+	exit 0
+fi
 if ! npm test; then
-    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-    exit 2
+	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+	exit 1
 else
-    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-    exit 0
+	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+	exit 0
 fi
