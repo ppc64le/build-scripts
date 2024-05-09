941d82a0d94ef6f8427fcecc25af13f5a19867c4
AbhiIBM <137172943+AbhiIBM@users.noreply.github.com>
2024-05-09 14:20:32 +0530

    Update apache-freemarker_ubi_9.3.sh

--- a/apache-freemarker/apache-freemarker_ubi_9.3.sh
+++ a/apache-freemarker/apache-freemarker_ubi_9.3.sh
@@ -8,7 +8,7 @@
 # Language      : Java
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Muskaan Sheik <Muskaan.Sheik@ibm.com>
+# Maintainer    : Abhishek Dwivedi <Abhishek.Dwivedi6@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
