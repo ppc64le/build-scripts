542eb8c8d4a552a98359b4f11b259449938f0f22
Shubham Garud <Shubham.Garud@ibm.com>
2024-05-09 06:28:48 -0400

    Moved old script and updated keys

--- a/apd/github.com__cockroachdb__apd_rhel_8.3.sh
+++ a/apd/github.com__cockroachdb__apd_rhel_8.3.sh
@@ -4,6 +4,8 @@
 # Version	: v1.1.0
 # Source repo	: https://github.com/cockroachdb/apd
 # Tested on	: RHEL 8.3
+# Language      : GO
+# Travis-Check  : False
 # Script License: Apache License, Version 2 or later
 # Maintainer	: BulkPackageSearch Automation <sethp@us.ibm.com>
 #
