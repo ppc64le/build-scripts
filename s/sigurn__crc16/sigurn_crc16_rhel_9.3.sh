9792f7a14601b2011d8d68643a9982f790791217
Dipti <diptikri254@gmail.com>
2024-05-10 13:24:02 +0530

    "last commit"

---
+++ s/sigurn__crc16/sigurn_crc16_rhel_9.3.sh
@@ -0,0 +1,57 @@
+
+PACKAGE_NAME=crc16
+PACKAGE_URL=https://github.com/sigurn/crc16
+PACKAGE_VERSION=${1:-83fcde1e29d10de8a8bfd2279244673d91cdce38}
+
+PACKAGE_COMMIT_HASH=`echo $PACKAGE_VERSION | cut -d'-' -f3`
+
+GO_VERSION=go1.17.5
+
+yum install -y git wget 
+
+yum install golang -y
+wget https://golang.org/dl/go1.17.5.linux-ppc64le.tar.gz
+tar -xzf go1.17.5.linux-ppc64le.tar.gz
+rm -rf go1.17.5.linux-ppc64le.tar.gz
+export GOPATH=`pwd`/gopath
+export PATH=`pwd`/go/bin:$GOPATH/bin:$PATH
+
+#clone package
+mkdir -p $GOPATH/src/github.com/sigurn
+cd $GOPATH/src/github.com/sigurn
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_COMMIT_HASH
+
+if ! go mod init; then
+	echo "------------------$PACKAGE_NAME:initialize_fails-------------------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | GitHub | Fail |  Initialize_Fails"
+	exit 1
+fi
+
+if ! go mod tidy; then
+	echo "------------------$PACKAGE_NAME:dependency_fails-------------------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | GitHub | Fail |  Dependency_Fails"
+	exit 1
+fi
+
+if ! go build -v ./...; then
+	echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | GitHub | Fail |  Build_Fails"
+	exit 1
+fi
+
+if ! go test -v ./...; then
+	echo "------------------$PACKAGE_NAME:test_fails---------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | GitHub | Fail |  Test_Fails"
+	exit 1
+else
+	echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
+	echo "$PACKAGE_VERSION $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | GitHub  | Pass |  Build_and_Test_Success"
+	exit 0
+fi
