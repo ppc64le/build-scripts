75485dbafc960abf8a99b4168cc33fb043d6d289
AbhiIBM <137172943+AbhiIBM@users.noreply.github.com>
2024-05-15 14:18:58 +0530

    Update Test command in mocha_ubi_9.3.sh

--- m/mocha/mocha_ubi_9.3.sh
+++ m/mocha/mocha_ubi_9.3.sh
@@ -44,7 +44,7 @@ if ! npm install && npm audit fix --force; then
     exit 1
 fi
 
-if ! npm test; then
+if ! npm run test:smoke ; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
