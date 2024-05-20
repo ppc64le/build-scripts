9fd96dee366af6205db2bddf8ebc54ea4cdb66e3
Shubham Bhagwat <118804486+shubham-bhagwat0@users.noreply.github.com>
2024-05-20 18:10:30 +0530

    Update infinispan-operator_ubi_9.3.sh

--- i/infinispan-operator/infinispan-operator_ubi_9.3.sh
+++ i/infinispan-operator/infinispan-operator_ubi_9.3.sh
@@ -50,7 +50,8 @@ git checkout $PACKAGE_VERSION
 
 # Increase timeout to resolve golangcli-lint timeout error
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@$GOLANGCI_LINT_VERSION
-home/go/bin/golangci-lint run --enable errorlint --timeout=10m
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+$SCRIPT_DIR/bin/golangci-lint run --enable errorlint --timeout=10m
 go mod vendor
 
 if ! make lint; then
