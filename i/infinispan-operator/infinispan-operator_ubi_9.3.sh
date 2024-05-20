dff9351fb959a26ca959feb96a24c5a058dc62f7
Shubham Bhagwat <118804486+shubham-bhagwat0@users.noreply.github.com>
2024-05-20 16:56:40 +0530

    Update infinispan-operator_ubi_9.3.sh

--- i/infinispan-operator/infinispan-operator_ubi_9.3.sh
+++ i/infinispan-operator/infinispan-operator_ubi_9.3.sh
@@ -22,15 +22,16 @@
 PACKAGE_NAME=infinispan-operator
 PACKAGE_URL=https://github.com/infinispan/infinispan-operator
 PACKAGE_VERSION=2.4.1.Final
+GO_VERSION=1.21.8
+GOLANGCI_LINT_VERSION=v1.53.3
 
 #Install the required dependencies
 yum install git gcc make wget tar zip -y
 
-GO_VERSION=1.21.6
 # Install Go and setup working directory
 wget https://go.dev/dl/go$GO_VERSION.linux-ppc64le.tar.gz
 tar -C /bin -xf go$GO_VERSION.linux-ppc64le.tar.gz
-#rm -f go$GO_VERSION.linux-ppc64le.tar.gz
+rm -f go$GO_VERSION.linux-ppc64le.tar.gz
 export PATH=$PATH:/bin/go/bin
 export GOPATH=/home/go
 mkdir -p $GOPATH/src && cd $GOPATH/src
@@ -41,20 +42,27 @@ chmod +x operator-sdk_linux_ppc64le
 mv operator-sdk_linux_ppc64le /usr/local/bin/operator-sdk
 operator-sdk version
 
+
 #Clone the repository
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 # Increase timeout to resolve golangcli-lint timeout error
-/root/src/infinispan-operator/bin/golangci-lint run --enable errorlint --timeout=10m
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@$GOLANGCI_LINT_VERSION
+bin/golangci-lint run --enable errorlint --timeout=10m
 go mod vendor
 
 if ! make lint; then
 	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
 	exit 2
-else
+else	
+	echo "------------------$PACKAGE_NAME:install_&_test_both_fails-------------------------"
+	echo "$PACKAGE_URL $PACKAGE_NAME"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+	exit 0
+fi
 
 if ! make test; then
 	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
