1e7023e535d7e7c272bbe4a87116820882e4707f
Shubham Bhagwat <118804486+shubham-bhagwat0@users.noreply.github.com>
2024-05-20 14:10:31 +0530

    Update 3scale-operator_ubi_9.3.sh

--- 3/3scale-operator/3scale-operator_ubi_9.3.sh
+++ 3/3scale-operator/3scale-operator_ubi_9.3.sh
@@ -21,31 +21,25 @@
 PACKAGE_NAME=3scale-operator
 PACKAGE_VERSION=3scale-2.14.1-GA
 PACKAGE_URL=https://github.com/3scale/3scale-operator
-PACKAGE_BRANCH=3scale-2.14.1-GA
 
 #Install the required dependencies
 yum install git gcc make wget tar zip -y
 
 git clone $PACKAGE_URL
-
 GO_VERSION=1.21.8
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION\
 
 # Install Go and setup working directory
 wget https://go.dev/dl/go$GO_VERSION.linux-ppc64le.tar.gz
 tar -C /bin -xf go$GO_VERSION.linux-ppc64le.tar.gz
-
 rm -f go$GO_VERSION.linux-ppc64le.tar.gz
-
 export PATH=$PATH:/bin/go/bin
 export GOPATH=/home/go
-
 mkdir -p $GOPATH/src && cd $GOPATH/src
 
-cd $PACKAGE_NAME
-git checkout $PACKAGE_VERSION\
 
 # Install the dependencies
-
 if ! make download; then
 	echo "------------------$PACKAGE_NAME:install_fails---------------------"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
