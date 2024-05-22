b5d3ddae91b2e257614705325c32a40592a8317e
stutiibm <Stuti.Wali@ibm.com>
2024-05-22 12:02:13 +0530

    Update apache-poi_ubi_9_3.sh

--- a/apache-poi/apache-poi_ubi_9_3.sh
+++ a/apache-poi/apache-poi_ubi_9_3.sh
@@ -83,7 +83,7 @@ if ! ./gradlew clean build -PjdkVersion=11 --no-daemon --refresh-dependencies -x
     echo "$PACKAGE_URL $PACKAGE_NAME"
     exit 1
 fi
-if ! ./gradlew test -PjdkVersion=11 --no-daemon --refresh-dependencies --exclude-task poi-integration:test ;then
+if ! ./gradlew test -PjdkVersion=11 --no-daemon --refresh-dependencies ;then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     exit 2
