0beecef84ba085e529985151e7d9f02a537c992f
Shubham Garud <Shubham.Garud@ibm.com>
2024-05-23 03:41:01 -0400

    Updated steps

--- u/uritools/uritools_ubi_9.3.sh
+++ u/uritools/uritools_ubi_9.3.sh
@@ -41,61 +41,26 @@ PATH=$PATH:/usr/local/bin/
 
 # Clone the repository
 git clone $PACKAGE_URL $PACKAGE_NAME
-
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-#install dependencies from requirements.txt
-
-REQUIREMENTS_PRESENT=(`find . -print | grep -i requirements.txt`)
-
-for i in "${REQUIREMENTS_PRESENT[@]}"; do
-        echo "Installing using pip from file:"
-        echo $i
-        pip3  install -r $i
-done
-
-#check if setup.py file is present
-if [ -f "setup.py" ];then
-        if ! python3 setup.py install ; then
-        echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
-        exit 1
-        fi
-        echo "setup.py file exists"
-else
-        echo "setup.py not present"
+if !(python3 setup.py install) ; then
+    echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_Fails"
+    exit 1
 fi
 
-
-#check if tox file is present
-if [ -f "tox.ini" ];then
-        echo "tox.ini file exists"
-        if ! (tox -e py3) ; then
-        echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-        exit 2
-        else
-        echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-        exit 0
-        fi
+# Run test cases
+if !(tox); then
+    echo "------------------$PACKAGE_NAME:build_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_success_but_test_Fails"
+    exit 2
 else
-        echo "tox.ini not present"
-        if ! pytest ; then
-        echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-        exit 2
-        else
-        echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-        exit 0
-        fi
-
+    echo "------------------$PACKAGE_NAME:build_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Build_and_Test_Success"
+    exit 0
 fi
 
