39c4be79d0f0ae1f16fd3f01cad24375d228612f
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-06-07 16:16:46 +0530

    Update influxdb_ubi_9.3.sh

--- i/influxdb/influxdb_ubi_9.3.sh
+++ i/influxdb/influxdb_ubi_9.3.sh
@@ -63,14 +63,14 @@ git clone $PACKAGE_URL
 cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-if ! make; then
+if ! go build ./...; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
-if ! make test; then
+if ! go test -race ./...; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
