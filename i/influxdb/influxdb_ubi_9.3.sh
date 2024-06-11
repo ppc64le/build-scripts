d65743e59bf868e3db7d81f0fe351f380ba4e872
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-06-11 14:47:02 +0530

    Update influxdb_ubi_9.3.sh

--- i/influxdb/influxdb_ubi_9.3.sh
+++ i/influxdb/influxdb_ubi_9.3.sh
@@ -72,7 +72,7 @@ fi
 
 export str="authorization authorizer backup bolt checks cmd context dashboards dbrp gather http influxql inmem jsonweb kit kv label mock models notebooks notification pkg pkger predicate prometheus query rand remotes replications secret session snowflake sqlite static task telegraf telemetry tenant toml tools v1"
 
-if ! go test ./...; then
+if ! go test ./... -timeout=30m; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
