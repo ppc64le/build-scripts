08eb3a0306bcab023da09ba69f782802f9c2b098
AbhiIBM <137172943+AbhiIBM@users.noreply.github.com>
2024-06-11 09:47:42 +0530

    Updated

--- j/jqTree/jqTree_ubi_9.3.sh
+++ j/jqTree/jqTree_ubi_9.3.sh
@@ -41,7 +41,7 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME 
 git checkout $PACKAGE_VERSION
 
-if ! pnmp install ; then
+if ! pnpm install ; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
