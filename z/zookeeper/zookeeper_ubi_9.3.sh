3fbdd83fc2a08bb2eb089fd428fe7ec447e51822
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-06-13 09:57:15 +0000

    Currency:Added ubi9 build-script and dockerfile for zookeeper

---
+++ z/zookeeper/zookeeper_ubi_9.3.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : zookeeper
+# Version          : release-3.9.2
+# Source repo      : https://github.com/apache/zookeeper
+# Tested on        : UBI:9.3
+# Language         : Java
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vinod K <Vinod.K1@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=zookeeper
+PACKAGE_VERSION=${1:-release-3.9.2}
+PACKAGE_URL=https://github.com/apache/zookeeper
+
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+dnf install -y gcc gcc-c++ gcc-gfortran git xz cmake make openssl-devel yum-utils wget sudo llvm -y
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/ppc64le/os/
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/BaseOS/ppc64le/os/
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/CRB/ppc64le/os/
+
+wget http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-Official
+mv RPM-GPG-KEY-CentOS-Official /etc/pki/rpm-gpg/.
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-Official
+
+dnf install --nodocs -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+yum install -y git wget gcc gcc-c++ java-11-openjdk java-11-openjdk-devel java-11-openjdk-headless tzdata-java hostname autoconf libtool automake  cppunit cppunit-devel
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
+export PATH=$PATH:$JAVA_HOME/bin
+
+wget https://archive.apache.org/dist/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.tar.gz
+tar -zxf apache-maven-3.8.7-bin.tar.gz
+cp -R apache-maven-3.8.7 /usr/local
+ln -s /usr/local/apache-maven-3.8.7/bin/mvn /usr/bin/mvn
+mvn --version
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! mvn clean install -DskipTests=true ; then
+    echo "------------------$PACKAGE_NAME:Build_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+    exit 1
+fi
+
+if ! mvn apache-rat:check verify -DskipTests spotbugs:check checkstyle:check -Pfull-build  -Dlicense.skip  -Dlicense.skipDownloadLicenses -Drat.numUnapprovedLicenses=100 ; then
+    echo "------------------$PACKAGE_NAME::Build_and_Test_fails-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail|  Build_and_Test_fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME::Build_and_Test_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Build_and_Test_Success"
+    exit 0
+fi
