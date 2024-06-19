5d2797006e743c89640c042bc870b6542839dbd2
nikhil-kalbande <82815888+nikhil-kalbande@users.noreply.github.com>
2024-06-19 11:47:00 +0530

    Update infinispan-operator_ubi_9.3.sh

--- i/infinispan-operator/infinispan-operator_ubi_9.3.sh
+++ i/infinispan-operator/infinispan-operator_ubi_9.3.sh
@@ -62,7 +62,7 @@ if ! make test; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-    exit 1
+    exit 2
 else
     echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
