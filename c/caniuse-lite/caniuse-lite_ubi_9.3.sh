da26ab4c505d3e39a59e8d286089f5d1405934b9
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-06-24 11:49:51 +0530

    Update caniuse-lite_ubi_9.3.sh

--- c/caniuse-lite/caniuse-lite_ubi_9.3.sh
+++ c/caniuse-lite/caniuse-lite_ubi_9.3.sh
@@ -36,14 +36,14 @@ cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 npm install -g pnpm
 
-if ! npm install && npm audit fix --force; then
+if ! pnpm install --frozen-lockfile --ignore-scripts ; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
-if ! npm test; then
+if ! pnpm test ; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
