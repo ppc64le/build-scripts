704335dbb3bd6e105f6a7640cfad372cbeeba6ac
Vipul-Ajmera <Vipul.Ajmera@ibm.com>
2024-07-03 16:23:37 +0530

    Currency: Added build_script and updated build_info.json for Grails-core (#4052)
    * Create grails-core_ubi_9.3.sh
    
    * Update build_info.json

---
+++ g/grails-core/grails-core_ubi_9.3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : grails-core
+# Version          : v6.2.0
+# Source repo      : https://github.com/grails/grails-core
+# Tested on        : UBI:9.3
+# Language         : Java
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vipul Ajmera <Vipul.Ajmera@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ---------------------------------------------------------------------------
+
+#variables
+PACKAGE_NAME=grails-core
+PACKAGE_VERSION=${1:-v6.2.0}
+PACKAGE_URL=https://github.com/grails/grails-core.git
+
+#install dependencies
+yum install -y git unzip wget maven
+
+#clone repository
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+#Build	
+if ! ./gradlew install -x test; then
+	echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+	echo "$PACKAGE_NAME  |  $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+	exit 1
+fi
+
+#Test
+if ! ./gradlew test; then
+	echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+	exit 1
+else
+	echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+	exit 0
+fi
