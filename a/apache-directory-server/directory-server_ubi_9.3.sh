10e98ff26d31b2ef97b85b544de0a23c1faa1837
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-07-04 18:36:03 +0530

    Update directory-server_ubi_9.3.sh

--- a/apache-directory-server/directory-server_ubi_9.3.sh
+++ a/apache-directory-server/directory-server_ubi_9.3.sh
@@ -22,7 +22,7 @@ PACKAGE_VERSION=${1:-2.0.0.AM27}
 PACKAGE_NAME=directory-server
 PACKAGE_URL=https://github.com/apache/directory-server
 
-OS_NAME=`cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'`
+OS_NAME=$(cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"')
 
 yum install -y git wget tar openssl-devel freetype fontconfig
 
@@ -31,28 +31,28 @@ tar -C /usr/local -xzf OpenJDK11U-jdk_ppc64le_linux_hotspot_11.0.22_7.tar.gz
 export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF8"
 export JAVA_HOME=/usr/local/jdk-11.0.22+7/
 export PATH=$PATH:/usr/local/jdk-11.0.22+7/bin
-ln -sf /usr/local/jdk-11.0.22+7/bin/java /usr/bin/
+ln -sf /usr/local/jdk-11.0.22+7/bin/java /usr/bin/java
 rm -rf OpenJDK11U-jdk_ppc64le_linux_hotspot_11.0.22_7.tar.gz
 
-#install maven
-wget https://archive.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
-tar -zxf apache-maven-3.6.0-bin.tar.gz
-cp -R apache-maven-3.6.0 /usr/local
-ln -s /usr/local/apache-maven-3.6.0/bin/mvn /usr/bin/mvn
+# Install Maven
+wget https://archive.apache.org/dist/maven/maven-3/3.9.1/binaries/apache-maven-3.9.1-bin.tar.gz
+tar -zxf apache-maven-3.9.1-bin.tar.gz
+cp -R apache-maven-3.9.1 /usr/local
+ln -s /usr/local/apache-maven-3.9.1/bin/mvn /usr/bin/mvn
 
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-#Build and test.
-if !  mvn -U clean install -Djava.awt.headless=true -fae -B -Dskiptests=true ; then
+# Build and test.
+if ! mvn -U clean install -Djava.awt.headless=true -fae -B -Dskiptests=true; then
     echo "------------------$PACKAGE_NAME:Build_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
     exit 1
 fi
 
-if ! mvn -U test -Djava.awt.headless=true -fae -B ; then
+if ! mvn -U test -Djava.awt.headless=true -fae -B; then
     echo "------------------$PACKAGE_NAME::Build_and_Test_fails-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail|  Build_and_Test_fails"
