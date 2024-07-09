1e422323f59a918654a54a21d7f298c88081d991
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-07-09 13:14:53 +0000

    Updated script

--- p/php-src/php-src_ubi_9.3.sh
+++ p/php-src/php-src_ubi_9.3.sh
@@ -50,7 +50,7 @@ if ! ./configure --enable-debug ; then
        echo "------------------$PACKAGE_NAME::Configure_fails-------------------------"
        echo "$PACKAGE_URL $PACKAGE_NAME"
        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail|  Configure_fails"
-       exit 2
+       exit 1
 fi
 
 if ! make -j$(/usr/bin/nproc) && make install; then
