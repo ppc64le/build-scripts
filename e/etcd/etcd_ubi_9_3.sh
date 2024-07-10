0b028a5c0b85e28589327711518f93dacd3c950b
stutiibm <Stuti.Wali@ibm.com>
2024-07-10 13:21:38 +0530

    Currency : updated build_info.json

--- e/etcd/etcd_ubi_9_3.sh
+++ e/etcd/etcd_ubi_9_3.sh
@@ -49,7 +49,7 @@ if ! make test; then
 	echo "------------------$PACKAGE_NAME:test_fails---------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
 	echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
-	exit 1
+	exit 2
 else
 	echo "------------------$PACKAGE_NAME:build_and_test_success-------------------------"
 	echo "$PACKAGE_URL $PACKAGE_NAME"
