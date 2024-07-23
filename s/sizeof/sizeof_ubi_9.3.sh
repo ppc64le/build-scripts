945f400034d2bbf1cccf33c34601ea8c6467cd02
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-07-23 08:51:57 +0000

    Currency:Added ubi9.3 build-script and build_info.json for sizeof

---
+++ s/sizeof/sizeof_ubi_9.3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : sizeof
+# Version       : master
+# Source repo   : https://github.com/ehcache/sizeof
+# Tested on     : UBI:9.3
+# Language      : Java
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Vinod K <Vinod.K1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=jaxb-ri
+PACKAGE_VERSION=${1:-master}
+PACKAGE_URL=https://github.com/ehcache/sizeof
+
+yum install git wget java-1.8.0-openjdk java-1.8.0-openjdk-devel java-1.8.0-openjdk-headless -y
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+export PATH=$PATH:$JAVA_HOME/bin
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! mvn clean install -DskipTests=true ; then
+    echo "------------------$PACKAGE_NAME:Install_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+    exit 1
+fi
+
+if ! mvn test ; then
+    echo "------------------$PACKAGE_NAME::Install_and_Test_fails-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail|  Install_and_Test_fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME::Install_and_Test_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
