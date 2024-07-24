784bd3aeb19cd08c6800157dff55e6d1ad08cda1
Vipul-Ajmera <Vipul.Ajmera@ibm.com>
2024-07-24 19:20:32 +0530

    Create socketry-async_ubi_9.3.sh

---
+++ s/socketry-async/socketry-async_ubi_9.3.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : async
+# Version          : v2.14.2
+# Source repo      : https://github.com/socketry/async.git
+# Tested on        : UBI:9.3
+# Language         : Ruby
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vipul Ajmera <Vipul.Ajmera@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ---------------------------------------------------------------------------
+
+#variables
+PACKAGE_NAME=async
+PACKAGE_VERSION=${1:-v2.14.2}
+PACKAGE_URL=https://github.com/socketry/async.git
+
+#dependencies
+yum install -y git wget
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/CRB/ppc64le/os/
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/ppc64le/os/
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/BaseOS/ppc64le/os/
+wget http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-Official
+mv RPM-GPG-KEY-CentOS-Official /etc/pki/rpm-gpg/.
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-Official
+
+dnf module list ruby
+dnf module reset ruby -y
+dnf module enable ruby:3.1 -y
+dnf module -y update ruby:3.1
+yum install -y ruby
+ruby -v
+
+yum install -y gcc gcc-c++ make libffi-devel libxml2-devel libxslt-devel zlib-devel ruby-devel
+
+gem install bundle
+
+#clone repository
+git clone $PACKAGE_URL
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+
+#build
+if ! bundle install; then
+    echo "------------------$PACKAGE_NAME:Build_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_Fails"
+    exit 1
+fi
+
+#test
+if ! bundle exec bake test; then
+    echo "------------------$PACKAGE_NAME:Build_success_and_Test_fails-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Fail |  Build_Success_and_Test_fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Build_and_Test_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Build_and_Test_Success"
+    exit 0
+fi
+
