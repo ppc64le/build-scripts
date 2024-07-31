4b3d506a25dc2856440f14bdc492d7c08d357228
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-07-31 18:59:31 +0530

    Currency:Added ubi9.3 build-script,build_info.json for opentelemetry-operator (#4208)
    * Currency:Added ubi9.3 build-script,build_info.json for opentelemetry-operator
    
    * Update opentelemetry-operator_ubi_9.3.sh

---
+++ o/opentelemetry-operator/opentelemetry-operator_ubi_9.3.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : opentelemetry-operator
+# Version          : v0.104.0
+# Source repo      : https://github.com/open-telemetry/opentelemetry-operator
+# Tested on        : UBI:9.3
+# Language         : Go
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vinod K <Vinod.K1@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=opentelemetry-operator
+PACKAGE_VERSION=${1:-v0.104.0}
+PACKAGE_URL=https://github.com/open-telemetry/opentelemetry-operator
+
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+yum install -y openssl sudo make git gcc wget
+
+#Install go
+export GO_VERSION=${GO_VERSION:-1.22.5}
+export GOROOT=${GOROOT:-"/usr/local/go"}
+export GOPATH=${GOPATH:-$HOME/go}
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:/usr/local/bin
+wget https://golang.org/dl/go${GO_VERSION}.linux-ppc64le.tar.gz
+tar -C /usr/local -xvzf go${GO_VERSION}.linux-ppc64le.tar.gz
+rm -rf go${GO_VERSION}.linux-ppc64le.tar.gz
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! make ; then
+    echo "------------------$PACKAGE_NAME:Build_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+    exit 1
+fi
+
+if ! make test ; then
+    echo "------------------$PACKAGE_NAME::Build_and_Test_fails-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail|  Build_and_Test_fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME::Build_and_Test_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Build_and_Test_Success"
+    exit 0
+fi
