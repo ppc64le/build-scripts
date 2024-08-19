d27b54c1a5b28a48d3315174d3ff4fbf0b9a5b7e
stutiibm <Stuti.Wali@ibm.com>
2024-08-19 02:42:55 +0000

    Currency : Added ubi9 script for ci.ant

---
+++ c/ci.ant/ci.ant_ubi_9_3.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -e
+
+# ----------------------------------------------------------------------------
+# Package          : ci.ant
+# Version          : liberty-ant-tasks-1.9.15
+# Source repo      : https://github.com/OpenLiberty/ci.ant
+# Tested on        : UBI: 9.3
+# Language         : Java
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Stuti Wali <Stuti.Wali@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#   
+# ----------------------------------------------------------------------------
+
+# Variables
+PACKAGE_NAME="ci.ant"
+PACKAGE_URL="https://github.com/OpenLiberty/ci.ant"
+PACKAGE_VERSION=${1:-liberty-ant-tasks-1.9.15}
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+HOME_DIR=$PWD
+MAVEN_VERSION=3.9.6
+
+#installing required dependencies
+echo "installing dependencies from system repo..."
+dnf install -y git make gcc gcc-c++ java-17-openjdk-devel.ppc64le  libtool file diffutils bc wget initscripts
+export JAVA_HOME=/usr/lib/jvm/$(ls /usr/lib/jvm/ | grep -P '^(?=.*java-17)(?=.*ppc64le)')
+export PATH=$JAVA_HOME/bin:$PATH
+java --version 
+
+#Install maven
+wget https://dlcdn.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
+tar xvf apache-maven-${MAVEN_VERSION}-bin.tar.gz
+rm -rf apache-maven-${MAVEN_VERSION}-bin.tar.gz
+PATH=$CWD/apache-maven-${MAVEN_VERSION}/bin:$PATH
+mvn --version
+
+
+# Cloning the repository from remote to local
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! mvn clean install -DskipTests; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+if ! mvn test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
+
+
+
