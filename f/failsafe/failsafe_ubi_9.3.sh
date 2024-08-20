deba8c631509b74cbc8d637f2f53ce6e3f9eb0fb
Pooja Shah <shahpooja0805@gmail.com>
2024-08-20 12:18:29 +0530

    Removed clean from mvn install

--- f/failsafe/failsafe_ubi_9.3.sh
+++ f/failsafe/failsafe_ubi_9.3.sh
@@ -48,7 +48,7 @@ git clone $PACKAGE_URL
 cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-if ! mvn clean install -DskipTests=true; then
+if ! mvn install -DskipTests=true; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Install_Fails"
