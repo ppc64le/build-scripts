7f66dd2e7ffcfb0eff2a7aefccb28b8f05061e7e
prachikurade1 <prachi.kurade1@ibm.com>
2024-08-23 14:59:19 +0530

    Update config-array_ubi_9.3.sh

--- c/config-array/config-array_ubi_9.3.sh
+++ c/config-array/config-array_ubi_9.3.sh
@@ -7,7 +7,7 @@
 # Tested on	    : UBI 9.3
 # Language          : Node
 # Travis-Check      : True
-# Script License    : MIT License
+# Script License    : Apache 2.0 License
 # Maintainer	    : Prachi Kurade <prachi.kurade1@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
