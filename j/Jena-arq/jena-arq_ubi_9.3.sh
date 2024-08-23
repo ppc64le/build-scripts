acf7c176cb8c684e46f7d02267188116b474d26a
Mayur Bhosure <45959986+mayurbhosure@users.noreply.github.com>
2024-08-23 15:56:53 +0530

    Update jena-arq_ubi_9.3.sh

--- j/Jena-arq/jena-arq_ubi_9.3.sh
+++ j/Jena-arq/jena-arq_ubi_9.3.sh
@@ -5,10 +5,10 @@
 # Version       : jena-4.9.0
 # Source repo   : https://github.com/apache/jena/tree/main/jena-arq
 # Tested on     : UBI: 9.3
-# Language : Java
-# Travis-Check : True
-# Script License: Apache License 2.0
-# Maintainer's  : Mayur Bhosure <Mayur.Bhosure2@ibm.com>
+# Language      : Java
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Mayur Bhosure <Mayur.Bhosure2@ibm.com>
 #
 #
 # Disclaimer: This script has been tested in root mode on given
@@ -18,54 +18,48 @@
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-
 set -e
-# Variables
-REPO=https://github.com/apache/jena.git
-VERSION=jena-4.9.0
-DIR=jena/jena-arq
+PACKAGE_NAME=jena-arq
+PACKAGE_URL=https://github.com/apache/jena.git
+PACKAGE_VERSION=${1:-jena-4.9.0}
 
 # install tools and dependent packages
-yum update -y
-yum install -y git wget
+yum install -y git wget java-11-openjdk-devel
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
+export PATH=$JAVA_HOME/bin:$PATH
 
-# install java
-yum -y install java-1.8.0-openjdk-devel
-
-#install maven
+# install maven
 MAVEN_VERSION=${MAVEN_VERSION:-3.8.8}
 wget https://downloads.apache.org/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
 tar -C /usr/local/ -xzf apache-maven-$MAVEN_VERSION-bin.tar.gz
 mv /usr/local/apache-maven-$MAVEN_VERSION /usr/local/maven
-export M2_HOME=/usr/local/maven 
+export M2_HOME=/usr/local/maven
 
 # update the path env. variable
 export PATH=$PATH:$M2_HOME/bin
 
+# Cloning the repository
+git clone $PACKAGE_URL
+cd jena/$PACKAGE_NAME
+git checkout $PACKAGE_VERSION
 
-# Cloning the repository from remote to local
-cd /home
-git clone $REPO
-cd $DIR
-git checkout $VERSION
-
-# Build and test package
-if ! mvn -B --file pom.xml -Dmaven.javadoc.skip=true install; then
-    echo "------------------$DIR:Install_fails-------------------------------------"
-    echo "$REPO $DIR"
-    echo "$DIR  |  $REPO | $VERSION | GitHub | Fail |  Install_Fails"
+#Build
+if !  mvn -B --file pom.xml -Dmaven.javadoc.skip=true install; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
-fi 
+fi
 
 #Test
 if ! mvn test -DforkCount=2 ; then
-    echo "------------------$DIR:Install_success_but_test_fails---------------------"
-    echo "$REPO $DIR"
-    echo "$DIR  |  $REPO | $VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
     exit 2
 else
-    echo "------------------$DIR:Install_&_test_both_success-------------------------"
-    echo "$REPO $DIR"
-    echo "$DIR  |  $REPO | $VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
     exit 0
 fi
