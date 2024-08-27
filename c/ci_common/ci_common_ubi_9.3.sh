ce9921170cc2f3db0ccb91e72e5bd12360391c12
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-08-27 07:38:00 +0000

    Currency:Added ubi9.3 build-script,build_info.json for ci_common

---
+++ c/ci_common/ci_common_ubi_9.3.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : ci_common
+# Version          : ci.common-1.8.33
+# Source repo      : https://github.com/OpenLiberty/ci.common
+# Tested on        : UBI:9.3
+# Language         : Java
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vinod K <Vinod.K1@ibm.com>
+#
+# Disclaimer       : This script has been tested in non-root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ---------------------------------------------------------------------------
+
+PACKAGE_NAME=ci.common
+PACKAGE_URL=https://github.com/OpenLiberty/ci.common
+PACKAGE_VERSION=${1:-4.0.2}
+
+yum install git wget gcc gcc-c++ java-21-openjdk java-21-openjdk-devel java-21-openjdk-headless -y
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
+export PATH=$PATH:$JAVA_HOME
+
+wget https://archive.apache.org/dist/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.tar.gz
+tar -zxf apache-maven-3.8.7-bin.tar.gz
+cp -R apache-maven-3.8.7 /usr/local
+ln -s /usr/local/apache-maven-3.8.7/bin/mvn /usr/bin/mvn
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! ./mvnw -V clean install -ntp ; then
+    echo "------------------$PACKAGE_NAME:Install_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+    exit 1
+fi
+
+if ! mvn test ; then
+    echo "------------------$PACKAGE_NAME::Install_and_Test_fails-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail|  Install_and_Test_fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME::Install_and_Test_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
