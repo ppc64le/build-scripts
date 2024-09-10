108a61832bd818334be23a7cb3fb1e541e5b37c4
santhoshkotla2929 <KOTLA.SANTHOSH@IBM.com>
2024-09-10 12:15:27 +0530

    Update quartz_ubi_9.3.sh

--- q/quartz/quartz_ubi_9.3.sh
+++ q/quartz/quartz_ubi_9.3.sh
@@ -30,12 +30,6 @@ yum install -y git wget unzip java-11-openjdk-devel
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
-#GRADLE_VERSION=gradle-8.2-rc-1
-#wget https://services.gradle.org/distributions/${GRADLE_VERSION}-bin.zip  && unzip -d /gradle /${GRADLE_VERSION}-bin.zip
-#export GRADLE_HOME=/gradle/${GRADLE_VERSION}/ 
-# update the path env. variable
-#export PATH=${GRADLE_HOME}/bin:${PATH}
-
 # Cloning the repository
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
@@ -48,7 +42,6 @@ then
   echo "Build failed for $PACKAGE_NAME-$PACKAGE_VERSION"
   exit 1
 fi
-
    
 #Test
 ./gradlew test
