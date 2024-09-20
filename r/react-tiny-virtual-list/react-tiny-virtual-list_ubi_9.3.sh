6b1f566dcfe94f53b24c80881a2bae0da3daed77
Prachi Kurade <prachi.kurade1@ibm.com>
2024-09-20 10:34:23 +0530

    Add support for react-tiny-virtual-list

---
+++ r/react-tiny-virtual-list/react-tiny-virtual-list_ubi_9.3.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+#
+# Package	    : react-tiny-virtual-list
+# Version	    : bf90799
+# Source repo	    : https://github.com/clauderic/react-tiny-virtual-list
+# Tested on	    : UBI 9.3
+# Language          : Node
+# Travis-Check      : True
+# Script License    : MIT License
+# Maintainer	    : Prachi Kurade <prachi.kurade1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=react-tiny-virtual-list
+PACKAGE_VERSION=${1:-bf90799}
+PACKAGE_URL=https://github.com/clauderic/react-tiny-virtual-list
+
+export NODE_VERSION=${NODE_VERSION:-8}
+
+yum install -y python3 python3-devel.ppc64le git gcc gcc-c++ libffi make wget fontconfig-devel bzip2
+
+#Installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source "$HOME"/.bashrc
+echo "installing nodejs $NODE_VERSION"
+nvm install "$NODE_VERSION" >/dev/null
+nvm use $NODE_VERSION
+
+#Installing PhantomJS
+wget https://github.com/ibmsoe/phantomjs/releases/download/2.1.1/phantomjs-2.1.1-linux-ppc64.tar.bz2
+tar -xvf phantomjs-2.1.1-linux-ppc64.tar.bz2
+ln -s /phantomjs-2.1.1-linux-ppc64/bin/phantomjs /usr/local/bin/phantomjs
+export PATH=$PATH:/phantomjs-2.1.1-linux-ppc64/bin
+export OPENSSL_CONF=/etc/ssl
+
+
+git clone $PACKAGE_URL $PACKAGE_NAME
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! npm install -g yarn; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! yarn install; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! yarn test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
+
