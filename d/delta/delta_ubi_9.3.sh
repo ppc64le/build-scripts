67502fd3800e7b82e40b90a5929f9d3db5bdc510
Siddesh Sangodkar <siddesh226@gmail.com>
2024-09-26 12:09:12 +0530

    Update delta_ubi_9.3.sh

--- d/delta/delta_ubi_9.3.sh
+++ d/delta/delta_ubi_9.3.sh
@@ -57,7 +57,7 @@ if ! build/sbt compile ; then
     exit 1
 fi 
 
-if ! build/sbt test ; then
+if ! build/sbt "testOnly * -- -l org.apache.spark.sql.delta.DeltaGenerateSymlinkManifestSuite" test ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
