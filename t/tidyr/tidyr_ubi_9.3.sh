8eb88e67df8843233292e9733dbc7b6a3e9a1c90
Vipul-Ajmera <Vipul.Ajmera@ibm.com>
2024-09-27 12:39:03 +0530

    Update tidyr_ubi_9.3.sh

--- t/tidyr/tidyr_ubi_9.3.sh
+++ t/tidyr/tidyr_ubi_9.3.sh
@@ -24,7 +24,7 @@ PACKAGE_VERSION=${1:-1.3.1}
 PACKAGE_URL=https://github.com/cran/tidyr.git
 
 #install dependencies
-dnf install -y gcc gcc-c++ gcc-gfortran git wget xz cmake make openssl-devel yum-utils wget sudo llvm -y
+dnf install -y gcc gcc-c++ gcc-gfortran git wget xz cmake make yum-utils sudo llvm -y
 dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/ppc64le/os/
 dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/BaseOS/ppc64le/os/
 dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/CRB/ppc64le/os/
@@ -40,7 +40,7 @@ dnf install -y R-core R-core-devel
 dnf builddep R -y
 R --version
 
-dnf install -y geos-devel gdal-devel udunits2-devel unixODBC-devel libpq-devel proj-devel sqlite-devel gsl-devel libgit2-devel
+dnf install -y geos-devel gdal-devel udunits2-devel unixODBC-devel libpq-devel proj-devel sqlite-devel gsl-devel libgit2-devel openssl-devel
 
 #clone repository 
 git clone $PACKAGE_URL
