8ea3d5bdcf035cb1731abd73ac9fec854209194c
kiranNukal <sai.kiran.nukala@ibm.com>
2024-09-27 14:33:51 +0530

    Update mpld3_9.3.sh

--- m/mpld3/mpld3_9.3.sh
+++ m/mpld3/mpld3_9.3.sh
@@ -25,18 +25,15 @@ PACKAGE_VERSION=${1:-0.5.10}
 PACKAGE_URL=https://github.com/mpld3/mpld3
  
 # Step 1: Download and extract the mpld3 source code
+yum install -y python3-devel wget curl python3-setuptools python3-pip \
+    libjpeg-devel zlib-devel freetype-devel lcms2-devel libwebp-devel \
+    tcl-devel tk-devel gcc gcc-c++ make
 echo "Downloading $PACKAGE_NAME-$PACKAGE_VERSION..."
 wget https://files.pythonhosted.org/packages/90/58/19378f4189a034eb3efc17b133426b8551af1d3b2c70d641a63124579629/$PACKAGE_NAME-$PACKAGE_VERSION.tar.gz
  
 echo "Extracting $PACKAGE_NAME-$PACKAGE_VERSION..."
 tar -xvf $PACKAGE_NAME-$PACKAGE_VERSION.tar.gz
 cd $PACKAGE_NAME-$PACKAGE_VERSION
- 
-# Step 2: Install necessary development tools and libraries
-echo "Installing required dependencies..."
-yum install -y python3-devel wget curl python3-setuptools python3-pip \
-    libjpeg-devel zlib-devel freetype-devel lcms2-devel libwebp-devel \
-    tcl-devel tk-devel gcc gcc-c++ make
  
 # Step 3: Install dependencies using pip
 echo "Installing Python dependencies..."
