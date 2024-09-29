325c8f10b010e93c315a50e10a9cb0aed2c71eb2
Harithanagothu2 <haritha.nagothu2@ibm.com>
2024-09-29 16:02:18 +0530

    Create secretstorage_ubi_9.3.sh

---
+++ s/secretstorage/secretstorage_ubi_9.3.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+# 
+# Package       : secretstorage
+# Version       : 3.3.3
+# Source repo   : git clone https://github.com/mitya57/secretstorage.git
+# Tested on     : UBI:9.3
+# Language      : Python
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Haritha Nagothu <haritha.nagothu2@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+#variables
+PACKAGE_NAME=secretstorage
+PACKAGE_VERSION=${1:- 3.3.3}
+PACKAGE_URL=https://github.com/mitya57/secretstorage.git
+
+# Install dependencies and tools.
+yum install -y gcc gcc-c++ gcc-gfortran git make python-devel zlib-devel openssl-devel libjpeg-devel xz-devel bzip2-devel wget libX11-devel
+
+#clone repository 
+git clone $PACKAGE_URL
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+#installing rust
+if ! command -v rustc &> /dev/null; then
+    echo "Rust is not installed. Installing Rust..."
+    wget https://static.rust-lang.org/dist/rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
+	tar -xzf rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
+	cd rust-1.75.0-powerpc64le-unknown-linux-gnu
+	./install.sh
+	export PATH=$HOME/.cargo/bin:$PATH
+	rustc -V
+	cargo  -V
+	cd ..
+else
+    echo "Rust is already installed."
+fi
+
+#Install all dependencies
+pip install .
+
+#install
+if ! (python3 setup.py install) ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
