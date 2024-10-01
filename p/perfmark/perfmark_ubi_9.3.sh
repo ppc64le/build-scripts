9d07419456091c2103271758edcd97f6f255f70f
Mayur Bhosure <45959986+mayurbhosure@users.noreply.github.com>
2024-10-01 11:54:45 +0530

    Update perfmark_ubi_9.3.sh

--- p/perfmark/perfmark_ubi_9.3.sh
+++ p/perfmark/perfmark_ubi_9.3.sh
@@ -26,23 +26,29 @@ yum install git wget gcc gcc-c++ java-21-openjdk java-21-openjdk-devel java-21-o
 export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
 export PATH=$PATH:$JAVA_HOME
 
-wget https://archive.apache.org/dist/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.tar.gz
-tar -zxf apache-maven-3.8.7-bin.tar.gz
-cp -R apache-maven-3.8.7 /usr/local
-ln -s /usr/local/apache-maven-3.8.7/bin/mvn /usr/bin/mvn
+
+export GRADLE_VERSION="8.10.1"
+# download gradle distribution
+wget https://services.gradle.org/distributions/gradle-$GRADLE_VERSION-bin.zip -q -O /tmp/gradle-$GRADLE_VERSION-bin.zip
+
+# unzip and install
+unzip -d /tmp /tmp/gradle-$GRADLE_VERSION-bin.zip
+mv /tmp/gradle-$GRADLE_VERSION /usr/local/gradle
+export PATH=/usr/local/gradle/bin/:$PATH
+
 
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-if ! ./gradlew build ; then
+if ! gradle build -x javadoc ; then
      echo "------------------$PACKAGE_NAME:Build_fails---------------------"
      echo "$PACKAGE_VERSION $PACKAGE_NAME"
      echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails_"
      exit 2
 fi
 
-if ! ./gradlew check ; then
+if !  gradle check ; then
       echo "------------------$PACKAGE_NAME::Build_and_Test_fails-------------------------"
       echo "$PACKAGE_URL $PACKAGE_NAME"
       echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail|  Build_and_Test_fails"
