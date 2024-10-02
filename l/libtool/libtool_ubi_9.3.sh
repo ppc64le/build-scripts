e3b1a4bfd6dff7d285b753d05af396782eacbdb6
aastha-sharma2 <aastha.sharma4@ibm.com>
2024-10-03 03:17:01 +0530

    Create libtool_ubi_9.3.sh

---
+++ l/libtool/libtool_ubi_9.3.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : libtool
+# Version          : master
+# Source repo      : https://github.com/matan-h/libtool.git
+# Tested on        : UBI:9.3
+# Language         : Python
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Aastha Sharma <aastha.sharma4@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ---------------------------------------------------------------------------
+
+# Variables
+PACKAGE_NAME=certipy
+PACKAGE_VERSION=${1:-master}
+PACKAGE_URL=https://github.com/matan-h/libtool.git
+
+# Install dependencies
+yum install -y git gcc gcc-c++ make wget openssl-devel bzip2-devel libffi-devel zlib-devel
+
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME  # Change directory to the cloned repository
+git checkout $PACKAGE_VERSION  # Checkout the specified version
+
+# Check if Rust is installed
+if ! command -v rustc &> /dev/null; then
+    # If Rust is not found, install Rust
+    echo "Rust not found. Installing Rust..."
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    source "$HOME/.cargo/env"  # Update environment variables to use Rust
+else
+    echo "Rust is already installed."
+fi
+
+# Upgrade pip and install necessary Python packages
+python3 -m pip install --upgrade pip wheel setuptools pytest
+pip install .
+
+#install
+if ! (python3 setup.py install) ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
