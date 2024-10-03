6eaf96d5639e8c74c2b60aaa2847a9c99296c6b6
Shubham Garud <Shubham.Garud@ibm.com>
2024-10-03 03:18:07 -0400

    Updated maintainer details

--- p/purrr/purrr_ubi_9.3.sh
+++ p/purrr/purrr_ubi_9.3.sh
@@ -8,7 +8,7 @@
 # Language      : R
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: ICH <ich@us.ibm.com>
+# Maintainer	: Shubham Garud <Shubham.Garud@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
