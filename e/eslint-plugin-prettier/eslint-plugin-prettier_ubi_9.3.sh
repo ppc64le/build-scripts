ed0d5343dcf5a47396c5b2ab73e84aee93f1b481
Vipul-Ajmera <Vipul.Ajmera@ibm.com>
2024-10-04 13:58:38 +0530

    Currency: Created build_script and build_info.json for Eslint plugin prettier (#4572)
    * Create eslint-plugin-prettier_ubi_9.3.sh
    
    * Create LICENSE
    
    * Create build_info.json

---
+++ e/eslint-plugin-prettier/eslint-plugin-prettier_ubi_9.3.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : eslint-plugin-prettier
+# Version          : v5.2.1
+# Source repo      : https://github.com/prettier/eslint-plugin-prettier.git
+# Tested on        : UBI:9.3
+# Language         : JavaScript
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vipul Ajmera <Vipul.Ajmera@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ---------------------------------------------------------------------------
+
+#install variable
+PACKAGE_NAME=eslint-plugin-prettier
+PACKAGE_VERSION=${1:-v5.2.1}
+PACKAGE_URL=https://github.com/prettier/eslint-plugin-prettier.git
+
+export NODE_VERSION=${NODE_VERSION:-20}
+
+#install dependencies
+yum install -y python3 python3-devel.ppc64le git gcc gcc-c++ libffi make
+
+#Installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source "$HOME"/.bashrc
+echo "installing nodejs $NODE_VERSION"
+nvm install "$NODE_VERSION" >/dev/null
+nvm use $NODE_VERSION
+
+npm install -g pnpm
+
+#clone repository
+git clone $PACKAGE_URL $PACKAGE_NAME
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+#install
+if ! pnpm install ; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+#test
+if ! pnpm mocha ; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
