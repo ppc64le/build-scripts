d65e8835e9dca894b667aafcb7cee0d521f4a669
Prachi Kurade <prachi.kurade1@ibm.com>
2024-10-14 10:26:30 +0530

    Support for engine.io

---
+++ e/engine.io/engine.io_ubi_9.3.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+#
+# Package	    : engine.io
+# Version	    : 6.4.2
+# Source repo	    : https://github.com/socketio/engine.io
+# Tested on	    : UBI 9.3
+# Language          : Node
+# Travis-Check      : True
+# Script License    : Apache License, Version 2 or later
+# Maintainer	    : Prachi Kurade <prachi.kurade1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=engine.io
+PACKAGE_VERSION=${1:-6.4.2}
+PACKAGE_URL=https://github.com/socketio/engine.io
+
+export NODE_VERSION=${NODE_VERSION:-18}
+export NODE_OPTIONS="--dns-result-order=ipv4first"
+
+yum install -y python3 python3-devel.ppc64le git gcc gcc-c++ libffi make cairo
+
+#Installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source "$HOME"/.bashrc
+echo "installing nodejs $NODE_VERSION"
+nvm install "$NODE_VERSION" >/dev/null
+nvm use $NODE_VERSION
+
+
+git clone $PACKAGE_URL $PACKAGE_NAME
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! npm install && npm audit fix --force; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+
+if ! npm test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
+
+
