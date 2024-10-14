520cd4dbe04fcf344ea48ac996dd00604e7c8900
Harithanagothu2 <haritha.nagothu2@ibm.com>
2024-10-14 13:13:56 +0530

    Create seaborn_ubi_9.3.sh

---
+++ s/seaborn/seaborn_ubi_9.3.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+# 
+# Package       : seaborn
+# Version       : v0.13.2
+# Source repo   : https://github.com/mwaskom/seaborn.git
+# Tested on     : UBI:9.3
+# Language      : Python
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Haritha Nagothu <haritha.nagothu2@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+#variables
+PACKAGE_NAME=seaborn
+PACKAGE_VERSION=${1:- v0.13.2}
+PACKAGE_URL=https://github.com/mwaskom/seaborn.git
+
+# Install dependencies and tools.
+yum install -y gcc gcc-c++ gcc-gfortran git make python-devel zlib-devel libjpeg-devel openblas atlas wget
+
+#clone repository 
+git clone $PACKAGE_URL
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+#install
+if ! (pip install .) ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
