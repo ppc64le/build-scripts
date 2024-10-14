8e09b863fbdb21bab749dccaacceabd5980b62fd
aastha-sharma2 <aastha.sharma4@ibm.com>
2024-10-14 21:47:09 +0530

    Create cryptography_ubi_9.3.sh

---
+++ c/cryptography/cryptography_ubi_9.3.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : cryptography
+# Version          : 41.0.4
+# Source repo      : https://github.com/pyca/cryptography.git
+# Tested on        : UBI:9.3
+# Language         : Python
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Aastha Sharma <aastha.sharma4@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ---------------------------------------------------------------------------
+
+# Variables
+PACKAGE_NAME=cryptography
+PACKAGE_VERSION=${1:-41.0.4}
+PACKAGE_URL=https://github.com/pyca/cryptography.git
+
+# Install necessary system dependencies
+yum install -y git gcc gcc-c++ make wget openssl-devel bzip2-devel libffi-devel zlib-devel python-devel python-pip openssl openssl-devel 
+
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME  
+git checkout $PACKAGE_VERSION  
+
+# Check if Rust is installed
+if ! command -v rustc &> /dev/null; then
+    # If Rust is not found, install Rust
+    echo "Rust not found. Installing Rust..."
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    source "$HOME/.cargo/env"  
+else
+    echo "Rust is already installed."
+fi
+
+# Install additional dependencies
+pip install -r ci-constraints-requirements.txt
+pip install . 
+pip install build wheel cython nox
+
+#install
+if ! pyproject-build ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
