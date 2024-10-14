88e2dabfb00b63abfe8e20b91dc53c7662492103
aastha-sharma2 <aastha.sharma4@ibm.com>
2024-10-14 23:34:27 +0530

    Update cytoolz_ubi_9.3.sh

--- c/cytoolz/cytoolz_ubi_9.3.sh
+++ c/cytoolz/cytoolz_ubi_9.3.sh
@@ -42,16 +42,3 @@ if ! python3 -m setup build ; then
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
-
-if ! pytest; then
-    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-    exit 2
-else
-    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-    exit 0
-fi
-   
