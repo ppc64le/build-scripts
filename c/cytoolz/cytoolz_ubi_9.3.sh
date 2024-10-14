c940adfe45b4b324330d09c050bc840165d35a65
aastha-sharma2 <aastha.sharma4@ibm.com>
2024-10-14 22:57:29 +0530

    Create cytoolz_ubi_9.3.sh

---
+++ c/cytoolz/cytoolz_ubi_9.3.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : cytoolz
+# Version          : 0.10.1
+# Source repo      : https://github.com/pytoolz/cytoolz.git
+# Tested on        : UBI:9.3
+# Language         : Python
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Aastha Sharma <aastha.sharma4@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ---------------------------------------------------------------------------
+
+# Variables
+PACKAGE_NAME=cytoolz
+PACKAGE_VERSION=${1:-0.10.1}
+PACKAGE_URL=https://github.com/pytoolz/cytoolz.git
+
+# Install necessary system dependencies
+yum install -y git gcc gcc-c++ make wget openssl-devel bzip2-devel libffi-devel zlib-devel python-devel python-pip
+
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME  
+git checkout $PACKAGE_VERSION  
+
+# Install additional dependencies
+pip install .
+pip install pytest wheel build Cython==0.29.21
+
+#install
+if ! python3 -m setup build ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! pytest; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
+   
