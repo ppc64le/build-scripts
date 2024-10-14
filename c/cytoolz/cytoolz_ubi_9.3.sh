d5a5093828c7be038028eba91a595bedf46adef2
aastha-sharma2 <aastha.sharma4@ibm.com>
2024-10-14 23:48:54 +0530

    Update cytoolz_ubi_9.3.sh

--- c/cytoolz/cytoolz_ubi_9.3.sh
+++ c/cytoolz/cytoolz_ubi_9.3.sh
@@ -34,6 +34,7 @@ cd $PACKAGE_NAME-$PACKAGE_VERSION
 # Install additional dependencies
 pip install .
 pip install pytest wheel build Cython==0.29.21
+python3 -m pip install git+https://github.com/pytoolz/toolz.git
 
 #install
 if ! python3 -m setup build ; then
@@ -41,4 +42,16 @@ if ! python3 -m setup build ; then
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
+fi
+
+if ! pytest ; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
 fi
