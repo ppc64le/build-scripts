179b683d2b3695cb924014d84295434974daaa44
Prachi Kurade <prachi.kurade1@ibm.com>
2024-10-15 14:37:35 +0530

    Support for browserify-lib

---
+++ b/browserify-zlib/browserify-zlib_ubi_9.3.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+#
+# Package	    : browserify-zlib
+# Version	    : v0.2.0
+# Source repo	    : https://github.com/browserify/browserify-zlib
+# Tested on	    : UBI 9.3
+# Language          : Node
+# Travis-Check      : True
+# Script License    : Apache License, Version 2 or later
+# Maintainer	    : Prachi Kurade <prachi.kurade1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=browserify-zlib
+PACKAGE_VERSION=${1:-v0.2.0}
+PACKAGE_URL=https://github.com/browserify/browserify-zlib
+
+export NODE_VERSION=${NODE_VERSION:-8}
+
+yum install -y python3 python3-devel.ppc64le git gcc gcc-c++ libffi make wget fontconfig-devel bzip2
+
+#Installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source "$HOME"/.bashrc
+echo "installing nodejs $NODE_VERSION"
+nvm install "$NODE_VERSION" >/dev/null
+nvm use $NODE_VERSION
+
+#Installing PhantomJS
+wget https://github.com/ibmsoe/phantomjs/releases/download/2.1.1/phantomjs-2.1.1-linux-ppc64.tar.bz2
+tar -xvf phantomjs-2.1.1-linux-ppc64.tar.bz2
+ln -s /phantomjs-2.1.1-linux-ppc64/bin/phantomjs /usr/local/bin/phantomjs
+export PATH=$PATH:/phantomjs-2.1.1-linux-ppc64/bin
+export OPENSSL_CONF=/etc/ssl
+
+git clone $PACKAGE_URL $PACKAGE_NAME
+cd  $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+git submodule update --init
+
+if ! npm install && npm audit fix --force; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! npm run build; then
+    echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_Fails"
+    exit 1
+fi
+
+if ! npm run test:node; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
+
+
