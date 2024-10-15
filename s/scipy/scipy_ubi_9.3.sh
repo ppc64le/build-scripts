59b73b765a1b56eb21d6705087d1414988972db4
Salil Verlekar <Salil.Verlekar2@ibm.com>
2024-10-15 02:50:53 -0400

    build scipy==1.14.1 for Currency, using latest numpy, Cython and pybind11

--- s/scipy/scipy_ubi_9.3.sh
+++ s/scipy/scipy_ubi_9.3.sh
@@ -2,10 +2,10 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : scipy
-# Version       : v1.11.4
+# Version       : v1.14.1
 # Source repo   : https://github.com/scipy/scipy
 # Tested on     : UBI 9.3
-# Language      : Python, C, Fortran, C++, Cython, Meson 
+# Language      : Python, C, Fortran, C++, Cython, Meson
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Salil Verlekar <Salil.Verlekar2@ibm.com>
@@ -19,21 +19,21 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=scipy
-PACKAGE_VERSION=${1:-v1.11.4}
+PACKAGE_VERSION=${1:-v1.14.1}
 PACKAGE_URL=https://github.com/scipy/scipy
 
 OS_NAME=`cat /etc/os-release | grep "PRETTY" | awk -F '=' '{print $2}'`
 
 # install core dependencies
-yum install -y gcc gcc-c++ gcc-gfortran pkg-config openblas-devel python3.11 python3.11-pip python3.11-devel git atlas
+yum install -y gcc gcc-c++ gcc-fortran pkg-config openblas-devel python3.11 python3.11-pip python3.11-devel git atlas
 
 # change symbolic links so that python can find them
-ln -s /usr/lib64/atlas/libtatlas.so.3 /usr/lib64/atlas/libtatlas.so
+#ln -s /usr/lib64/atlas/libtatlas.so.3 /usr/lib64/atlas/libtatlas.so
 
 # install scipy dependency(numpy wheel gets built and installed) and build-setup dependencies
-python3.11 -m pip install meson ninja 'numpy==1.23.2' 'setuptools<60.0' Cython==0.29.37
+python3.11 -m pip install meson ninja numpy 'setuptools<60.0' Cython
 python3.11 -m pip install 'meson-python<0.15.0,>=0.12.1'
-python3.11 -m pip install 'pybind11<2.11.1,>=2.10.4'
+python3.11 -m pip install pybind11
 python3.11 -m pip install 'patchelf>=0.11.0'
 python3.11 -m pip install 'pythran<0.15.0,>=0.12.0'
 python3.11 -m pip install build
@@ -58,7 +58,7 @@ fi
 
 # test that installation happened successfully
 python3.11 -m pip show scipy
- 
+
 if [ $? == 0 ]; then
      echo "------------------$PACKAGE_NAME::Test_Pass---------------------"
      echo "$PACKAGE_VERSION $PACKAGE_NAME"
@@ -70,4 +70,3 @@ else
      echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Fail |  Test_Fail"
      exit 2
 fi
-
