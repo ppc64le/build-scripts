fe023175721b4d9a56a14230e732f21db9ec5e04
Siddesh Sangodkar <siddesh226@gmail.com>
2024-10-24 12:17:34 +0530

    Fix xz-java (#4670)
    * Update xz-java_ubi_9.3.sh
    
    * Update xz-java_v1_10_ubi_9.3.sh

--- x/xz-java/xz-java_ubi_9.3.sh
+++ x/xz-java/xz-java_ubi_9.3.sh
@@ -38,7 +38,7 @@ PACKAGE_URL=https://github.com/tukaani-project/xz-java
 #Install ant
 wget https://dlcdn.apache.org//ant/binaries/apache-ant-1.10.15-bin.tar.gz
 tar -xzf apache-ant-1.10.15-bin.tar.gz
-export PATH=$WORKDIR/apache-ant-1.10.15/bin/:$PATH
+export PATH=$(pwd)/apache-ant-1.10.15/bin/:$PATH
 
 # Clone, and build specified version
 git clone $PACKAGE_URL
