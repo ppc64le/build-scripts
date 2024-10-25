d444cae3c06063198151ee845f295f4896390753
kiranNukal <sai.kiran.nukala@ibm.com>
2024-10-25 17:44:09 +0530

    Create pyro_ubi_9.3.sh

---
+++ p/pyro-ppl/pyro_ubi_9.3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : pyro
+# Version       : 1.9.0
+# Source repo : https://github.com/pyro-ppl/pyro.git
+# Tested on     : UBI:9.3
+# Language      : Python
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Sai Kiran Nukala <sai.kiran.nukala@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on the given
+# platform using the mentioned version of the package.
+# It may not work as expected with newer versions of the
+# package and/or distribution. In such a case, please
+# contact the "Maintainer" of this script.
+#
+# -----------------------------------------------------------------------------
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+# Variables
+PACKAGE_NAME=pyro
+PACKAGE_VERSION=${1:-1.9.0}
+PACKAGE_URL=https://github.com/pyro-ppl/pyro.git
+
+
+# Install dependencies and tools
+yum install -y git wget gcc gcc-c++ python python3-devel python3 python3-pip openblas-devel
+pip install numpy wheel
+
+#install
+if ! (python3 setup.py install) ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+python3 setup.py bdist_wheel
