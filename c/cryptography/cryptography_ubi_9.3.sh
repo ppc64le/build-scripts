731ae1936c11a18a3436bf4a0ad627f61b88e05c
Md. Shafi Hussain <Md.Shafi.Hussain@ibm.com>
2024-10-25 15:07:54 +0530

    Update cryptography_ubi_9.3.sh
    Using pip/python instead of pip3/python3

--- c/cryptography/cryptography_ubi_9.3.sh
+++ c/cryptography/cryptography_ubi_9.3.sh
@@ -26,20 +26,21 @@ OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm \
     git redhat-rpm-config gcc libffi-devel python3-devel openssl-devel cargo pkg-config
+ln -s $(command -v python3) /usr/bin/python
 
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-pip3 install wheel setuptools_rust
-if ! python3 setup.py bdist_wheel && pip3 install dist/*.whl; then
+pip install wheel setuptools_rust
+if ! python setup.py bdist_wheel && pip install dist/*.whl; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
-pip3 install -r dev-requirements.txt
+pip install -r dev-requirements.txt
 # basic sanity test (subset)
 if ! pytest -n auto tests/conftest.py tests/test_cryptography_utils.py tests/test_rust_utils.py tests/test_fernet.py tests/test_warnings.py tests/hypothesis/ tests/bench/ tests/doubles.py; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
