0353d8dd270d09f164a886eb752ebb483eacb9c6
Bhagyashri Gaikwad <bhagyashri.gaikwad2@ibm.com>
2024-10-25 18:05:11 +0530

    updated code for create wheel

--- n/numpy/numpy_ubi_9.3.sh
+++ n/numpy/numpy_ubi_9.3.sh
@@ -28,7 +28,7 @@ yum install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-devel python${PYT
 
 # Install Python build dependencies
 python${PYTHON_VERSION} -m pip install --upgrade pip  # Ensure pip is up to date
-python${PYTHON_VERSION} -m pip install tox Cython pytest hypothesis
+python${PYTHON_VERSION} -m pip install tox Cython pytest hypothesis wheel
 
 # Clone the NumPy repository
 git clone $PACKAGE_URL
@@ -43,20 +43,35 @@ git submodule update --init
 # Check if the version is 1.26.0
 if [[ $PACKAGE_VERSION == "v1.26.0" ]]; then
     echo "Building NumPy using setup.py for version $PACKAGE_VERSION"
-
-    # Build using setup.py
-    if ! python${PYTHON_VERSION} setup.py build; then
+    # Build using setup.py and create a wheel
+    if ! python${PYTHON_VERSION} setup.py bdist_wheel; then
         echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_Fails"
         exit 1
     fi
-else
-    # Build for other versions
-    if ! python${PYTHON_VERSION} -m pip install .; then
+    # Install the created wheel
+    if ! python${PYTHON_VERSION} -m pip install dist/*.whl; then
         echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Install_Fails"
+        exit 1
+    fi
+
+else
+    # Build and create a wheel for other versions
+    if ! python${PYTHON_VERSION} -m pip wheel . -w dist; then
+        echo "------------------$PACKAGE_NAME:wheel_build_fails--------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Wheel_Build_Fails"
+        exit 1
+    fi
+
+    # Install the created wheel
+    if ! python${PYTHON_VERSION} -m pip install dist/*.whl; then
+        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Install_Fails"
         exit 1
     fi
 fi
