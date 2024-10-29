d3830539dc2c26b28f202057099581d66065af00
puneetsharma21 <puneet.sharma21@ibm.com>
2024-10-29 07:22:03 -0400

    adding PYTHON_VER env

--- s/scipy/scipy_v1.10.1_ubi_9.3.sh
+++ s/scipy/scipy_v1.10.1_ubi_9.3.sh
@@ -21,19 +21,20 @@
 PACKAGE_NAME=scipy
 PACKAGE_VERSION=${1:-v1.10.1}
 PACKAGE_URL=https://github.com/scipy/scipy
+PYTHON_VER=${2:-"3.11"}
 
 OS_NAME=`cat /etc/os-release | grep "PRETTY" | awk -F '=' '{print $2}'`
 
 # install core dependencies
-yum install -y gcc gcc-c++ gcc-fortran pkg-config openblas-devel python3.11 python3.11-pip python3.11-devel git atlas
+yum install -y gcc gcc-c++ gcc-fortran pkg-config openblas-devel python${PYTHON_VER} python${PYTHON_VER}-pip python${PYTHON_VER}-devel git atlas
 
 # install scipy dependency(numpy wheel gets built and installed) and build-setup dependencies
-python3.11 -m pip install meson ninja 'numpy<1.23' 'setuptools<60.0' Cython==0.29.37
-python3.11 -m pip install 'meson-python<0.15.0,>=0.12.1'
-python3.11 -m pip install pybind11
-python3.11 -m pip install 'patchelf>=0.11.0'
-python3.11 -m pip install 'pythran<0.15.0,>=0.12.0'
-python3.11 -m pip install build
+python${PYTHON_VER} -m pip install meson ninja 'numpy<1.23' 'setuptools<60.0' Cython==0.29.37
+python${PYTHON_VER} -m pip install 'meson-python<0.15.0,>=0.12.1'
+python${PYTHON_VER} -m pip install pybind11
+python${PYTHON_VER} -m pip install 'patchelf>=0.11.0'
+python${PYTHON_VER} -m pip install 'pythran<0.15.0,>=0.12.0'
+python${PYTHON_VER} -m pip install build
 
 # clone source repository
 git clone $PACKAGE_URL
@@ -42,7 +43,7 @@ git checkout $PACKAGE_VERSION
 git submodule update --init
 
 # build and install
-if ! python3.11 -m pip install -e . --no-build-isolation; then
+if ! python${PYTHON_VER} -m pip install -e . --no-build-isolation; then
         echo "------------------$PACKAGE_NAME:build_fails---------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
@@ -54,7 +55,7 @@ else
 fi
 
 # test that installation happened successfully
-python3.11 -m pip show scipy
+python${PYTHON_VER} -m pip show scipy
 
 if [ $? == 0 ]; then
      echo "------------------$PACKAGE_NAME::Test_Pass---------------------"
