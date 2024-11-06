b254789f93741a0630ababe9ad0af5875355d793
Md. Shafi Hussain <Md.Shafi.Hussain@ibm.com>
2024-11-06 17:55:23 +0530

    Update torchvision_ubi_9.3.sh

--- t/torchvision/torchvision_ubi_9.3.sh
+++ t/torchvision/torchvision_ubi_9.3.sh
@@ -24,53 +24,68 @@ PACKAGE_VERSION=${1:-v0.16.1}
 PACKAGE_URL=https://github.com/pytorch/vision.git
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 PYTHON_VER=${2:-3.9}
+PARALLEL=${PARALLEL:-$(nproc)}
 
 WORKDIR=$(pwd)
 
-dnf install -y git jq
-
-# install dependency - pytorch (skip tests)
-PYTORCH_VERSION=${3:-$(curl -sSL https://api.github.com/repos/pytorch/pytorch/releases/latest | jq -r .tag_name)}
-curl -sL https://raw.githubusercontent.com/ppc64le/build-scripts/master/p/pytorch/pytorch_ubi_9.3.sh | sed '/pip install pytest/q' | bash -s $PYTORCH_VERSION $PYTHON_VER
-
-cd $WORKDIR
-
-# install additional dependencies for pillow
 dnf install -y https://mirror.stream.centos.org/9-stream/BaseOS/ppc64le/os/Packages/centos-gpg-keys-9.0-24.el9.noarch.rpm \
             https://mirror.stream.centos.org/9-stream/BaseOS/`arch`/os/Packages/centos-stream-repos-9.0-24.el9.noarch.rpm \
-			https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+            https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/`arch`/os
 dnf config-manager --set-enabled crb
-dnf install -y libtiff-devel libjpeg-devel openjpeg2-devel zlib-devel \
-    libpng-devel freetype-devel lcms2-devel libwebp-devel tcl-devel tk-devel \
-    harfbuzz-devel fribidi-devel libraqm-devel libimagequant-devel libxcb-devel
-# install dependency - pillow (skip tests)
-PILLOW_VERSION=${4:-$(curl -sSL https://api.github.com/repos/python-pillow/Pillow/releases/latest | jq -r .tag_name)}
-curl -sL https://raw.githubusercontent.com/ppc64le/build-scripts/master/p/pillow/pillow_v11.0.0_ubi_9.3.sh | sed '/# Run tests to verify installation/q' | bash -s $PILLOW_VERSION $PYTHON_VER
+dnf install -y git cmake ninja-build g++ rust cargo jq \
+            python${PYTHON_VER}-devel python${PYTHON_VER}-wheel python${PYTHON_VER}-pip python${PYTHON_VER}-setuptools \
+            libtiff-devel libjpeg-devel openjpeg2-devel zlib-devel \
+            libpng-devel freetype-devel lcms2-devel libwebp-devel tcl-devel tk-devel \
+            harfbuzz-devel fribidi-devel libraqm-devel libimagequant-devel libxcb-devel
+
+if ! command -v python; then
+    ln -s $(command -v python${PYTHON_VER}) /usr/bin/python
+fi
+if ! command -v pip; then
+    ln -s $(command -v pip${PYTHON_VER}) /usr/bin/pip
+fi
+
+# install dependency - pytorch
+PYTORCH_VERSION=${PYTORCH_VERSION:-$(curl -sSL https://api.github.com/repos/pytorch/pytorch/releases/latest | jq -r .tag_name)}
+
+git clone https://github.com/pytorch/pytorch.git
+cd pytorch
+git checkout tags/$PYTORCH_VERSION
+
+PPC64LE_PATCH="69cbf05"
+if ! git log --pretty=format:"%H" | grep -q "$PPC64LE_PATCH"; then
+    echo "Applying POWER patch."
+    git config user.email "Md.Shafi.Hussain@ibm.com"
+    git config user.name "Md. Shafi Hussain"
+    git cherry-pick "$PPC64LE_PATCH"
+else
+    echo "POWER patch not needed."
+fi
+
+git submodule sync
+git submodule update --init --recursive
+pip install -r requirements.txt
+MAX_JOBS=$PARALLEL python setup.py install
 
 cd $WORKDIR
 
+# build torchvision
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-if ! (MAX_JOBS=$(nproc) python setup.py bdist_wheel && pip install dist/*.whl); then
+if ! (MAX_JOBS=$PARALLEL python setup.py bdist_wheel && pip install dist/*.whl); then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
-# register PrivateUse1HooksInterface
-python test/test_utils.py TestDeviceUtilsCPU.test_device_mode_ops_sparse_mm_reduce_cpu_bfloat16
-python test/test_utils.py TestDeviceUtilsCPU.test_device_mode_ops_sparse_mm_reduce_cpu_float16
-python test/test_utils.py TestDeviceUtilsCPU.test_device_mode_ops_sparse_mm_reduce_cpu_float32
-python test/test_utils.py TestDeviceUtilsCPU.test_device_mode_ops_sparse_mm_reduce_cpu_float64
-
 cd $WORKDIR
 pip install pytest pytest-xdist
 
-if ! pytest -n $(nproc) $PACKAGE_NAME/test/common_extended_utils.py $PACKAGE_NAME/test/common_utils.py $PACKAGE_NAME/test/smoke_test.py $PACKAGE_NAME/test/test_architecture_ops.py $PACKAGE_NAME/test/test_datasets_video_utils_opt.py $PACKAGE_NAME/test/test_tv_tensors.py; then
+if ! pytest -n $PARALLEL $PACKAGE_NAME/test/common_extended_utils.py $PACKAGE_NAME/test/common_utils.py $PACKAGE_NAME/test/smoke_test.py $PACKAGE_NAME/test/test_architecture_ops.py $PACKAGE_NAME/test/test_datasets_video_utils_opt.py $PACKAGE_NAME/test/test_tv_tensors.py; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
