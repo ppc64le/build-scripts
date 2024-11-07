52b172716a5096a85f50224fa2bed19d56d6b7e7
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-11-07 12:17:15 +0530

    Update ci.common_ubi_8.3.sh

--- c/ci.common/ci.common_ubi_8.3.sh
+++ c/ci.common/ci.common_ubi_8.3.sh
@@ -4,6 +4,7 @@
 # Version       : 1.5
 # Source repo   : https://github.com/OpenLiberty/ci.common/tree/ci.common-1.5
 # Tested on     : UBI 8.3
+# Travis-Check  : True
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Vaibhav Nazare <Vaibhav.Nazare@ibm.com>
 #
