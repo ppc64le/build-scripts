320669e115b10e528a0277287dd1501c62b54457
pratibh-ibm <pratibh.goshi@ibm.com>
2024-11-07 15:49:02 +0530

    Update and rename JCTools_ubi_9.3.sh to jctools_ubi_9.3.sh

--- j/JCTools/jctools_ubi_9.3.sh
+++ j/JCTools/jctools_ubi_9.3.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : JCTools
-# Version       : 4.0.5
+# Version       : v4.0.5
 # Source repo   : https://github.com/JCTools/JCTools
 # Tested on     : UBI 9.3
 # Language      : Java, Others
