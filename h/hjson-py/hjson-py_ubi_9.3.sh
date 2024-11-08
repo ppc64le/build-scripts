146490fbefbbf42d2fb8d0c836cac9490b3d3245
aastha-sharma2 <aastha.sharma4@ibm.com>
2024-11-08 18:04:23 +0530

    Update hjson-py_ubi_9.3.sh

--- h/hjson-py/hjson-py_ubi_9.3.sh
+++ h/hjson-py/hjson-py_ubi_9.3.sh
@@ -41,3 +41,14 @@ if ! (python3 setup.py install) ; then
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
+if ! pytest; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
