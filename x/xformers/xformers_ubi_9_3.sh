ecb0ac5a3e6dcdf862e56cc5ecbf3e364eb3ab8b
puneetsharma21 <puneet.sharma21@ibm.com>
2024-11-12 00:36:27 -0500

    build script for xformers

--- x/xformers/xformers_ubi_9_3.sh
+++ x/xformers/xformers_ubi_9_3.sh
@@ -21,9 +21,16 @@
 PACKAGE_NAME=xformers
 PACKAGE_VERSION=${1:-v0.0.28}
 PACKAGE_URL=https://github.com/facebookresearch/xformers.git
+PYTHON_VER=${2:-3.11}
+PARALLEL=${PARALLEL:-$(nproc)}
+export _GLIBCXX_USE_CXX11_ABI=1
 
 # Install dependencies
-yum install -y python3.11 python3.11-pip python3.11-devel gcc gcc-c++ git jq
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm \
+    git cmake ninja-build gcc gcc-c++ g++ rust cargo \
+    python${PYTHON_VER}-devel python${PYTHON_VER}-pip jq
+
+dnf install -y gcc-fortran pkg-config openblas-devel atlas
 
 # Clone repository
 git clone $PACKAGE_URL
@@ -32,48 +39,57 @@ git checkout $PACKAGE_VERSION
 git submodule update --init
 
 # Install Python dependencies
-python3.11 -m pip install ninja cmake 'pytest==8.2.2'
+python${PYTHON_VER} -m pip install ninja cmake 'pytest==8.2.2' hydra-core
 
-# install dependency - pytorch
-
+# Install dependency - pytorch
 PYTORCH_VERSION=${PYTORCH_VERSION:-$(curl -sSL https://api.github.com/repos/pytorch/pytorch/releases/latest | jq -r .tag_name)}
- 
+
 git clone https://github.com/pytorch/pytorch.git
 
 cd pytorch
 
 git checkout tags/$PYTORCH_VERSION
- 
+
 PPC64LE_PATCH="69cbf05"
 
 if ! git log --pretty=format:"%H" | grep -q "$PPC64LE_PATCH"; then
-
     echo "Applying POWER patch."
-
     git config user.email "Puneet.Sharma21@ibm.com"
-
     git config user.name "puneetsharma21"
-
     git cherry-pick "$PPC64LE_PATCH"
-
 else
-
     echo "POWER patch not needed."
-
 fi
- 
-git submodule sync
 
+git submodule sync
 git submodule update --init --recursive
 
-pip3.11 install -r requirements.txt
+# Set flags to suppress warnings
+export CXXFLAGS="-Wno-unused-variable -Wno-unused-parameter"
 
-MAX_JOBS=$PARALLEL python3.11 setup.py install
- 
+pip${PYTHON_VER} install -r requirements.txt
+MAX_JOBS=$PARALLEL python${PYTHON_VER} setup.py install
 
+# Install dependency - Scipy
+# install scipy dependency(numpy wheel gets built and installed) and build-setup dependencies
+cd ..
+git clone https://github.com/scipy/scipy
+cd scipy
+git checkout v1.14.1
+git submodule update --init
+
+python${PYTHON_VER}-m pip install meson ninja numpy 'setuptools<60.0' Cython
+python${PYTHON_VER}-m pip install 'meson-python<0.15.0,>=0.12.1'
+python${PYTHON_VER}-m pip install pybind11
+python${PYTHON_VER}-m pip install 'patchelf>=0.11.0'
+python${PYTHON_VER}-m pip install 'pythran<0.15.0,>=0.12.0'
+python${PYTHON_VER}-m pip install build
+
+python${PYTHON_VER}-m pip install --no-build-isolation .
 
 # Build and install xformers
-if ! python3.11 -m pip install -e .; then
+cd ..
+if ! python${PYTHON_VER} -m pip install -e .; then
     echo "------------------$PACKAGE_NAME:build_fails---------------------"
     exit 1
 else
@@ -81,16 +97,16 @@ else
 fi
 
 # Test installation
-if python3.11 -c "import xformers"; then
+if python${PYTHON_VER} -c "import xformers"; then
     echo "------------------$PACKAGE_NAME::Install_Success---------------------"
 else
     echo "------------------$PACKAGE_NAME::Install_Fail-------------------------"
     exit 2
 fi
 
-# Run tests
+# Run Specific tests
 export PY_IGNORE_IMPORTMISMATCH=1
-if ! pytest tests; then
+if ! python${PYTHON_VER} -m pytest tests/test_unbind.py tests/test_rotary_embeddings.py tests/test_hydra_helper.py tests/test_compositional_attention.py tests/test_global_attention.py; then
     echo "------------------$PACKAGE_NAME:test_fails---------------------"
     exit 2
 else
