b910af3dcc8bf8ce7ee8be49f2312f5b7ab54abe
puneetsharma21 <puneet.sharma21@ibm.com>
2024-11-13 17:52:57 -0500

    adding build script of scipy and pillow

--- s/scipy/scipy_v1.10.1_ubi_9.3.sh
+++ s/scipy/scipy_v1.10.1_ubi_9.3.sh
@@ -21,21 +21,20 @@
 PACKAGE_NAME=scipy
 PACKAGE_VERSION=${1:-v1.10.1}
 PACKAGE_URL=https://github.com/scipy/scipy
-PYTHON_VER=${2:-"3.11"}
 
 OS_NAME=$(cat /etc/os-release | grep "PRETTY" | awk -F '=' '{print $2}')
 
 # install core dependencies
-yum install -y gcc gcc-c++ gcc-fortran pkg-config openblas-devel python${PYTHON_VER} python${PYTHON_VER}-pip python${PYTHON_VER}-devel git atlas
+yum install -y gcc gcc-c++ gcc-fortran pkg-config openblas-devel git atlas
 
 # install scipy dependency (numpy wheel gets built and installed) and build-setup dependencies
-python${PYTHON_VER} -m pip install meson ninja 'numpy<1.23' 'setuptools<60.0' Cython==0.29.37
-python${PYTHON_VER} -m pip install 'meson-python<0.15.0,>=0.12.1'
-python${PYTHON_VER} -m pip install pybind11
-python${PYTHON_VER} -m pip install 'patchelf>=0.11.0'
-python${PYTHON_VER} -m pip install 'pythran<0.15.0,>=0.12.0'
-python${PYTHON_VER} -m pip install pooch pytest
-python${PYTHON_VER} -m pip install build
+pip install meson ninja 'numpy<1.23' 'setuptools<60.0' Cython==0.29.37
+pip install 'meson-python<0.15.0,>=0.12.1'
+pip install pybind11
+pip install 'patchelf>=0.11.0'
+pip install 'pythran<0.15.0,>=0.12.0'
+pip install pooch pytest
+pip install build
 
 # clone source repository
 git clone $PACKAGE_URL
@@ -44,7 +43,7 @@ git checkout $PACKAGE_VERSION
 git submodule update --init
 
 # build and install
-if ! python${PYTHON_VER} -m pip install -e . --no-build-isolation; then
+if ! pip install -e . --no-build-isolation; then
     echo "------------------$PACKAGE_NAME:build_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
@@ -56,7 +55,7 @@ else
 fi
 
 # run specific tests using pytest
-if ! python${PYTHON_VER} -m pytest scipy/interpolate/tests/test_polyint.py scipy/linalg/tests/test_basic.py; then
+if ! pytest scipy/interpolate/tests/test_polyint.py scipy/linalg/tests/test_basic.py; then
     echo "------------------$PACKAGE_NAME::Test_Fail-------------------------"
     echo "$PACKAGE_VERSION $PACKAGE_NAME"
     echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Fail |  Test_Fail"
