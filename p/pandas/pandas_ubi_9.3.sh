5c5a9acff7a6818318a471876b5732d2b0f78ba7
Bhagyashri Gaikwad <bhagyashri.gaikwad2@ibm.com>
2024-11-15 19:21:55 +0530

    Updated script changes for pandas

--- p/pandas/pandas_ubi_9.3.sh
+++ p/pandas/pandas_ubi_9.3.sh
@@ -20,50 +20,29 @@
 
 PACKAGE_NAME=pandas
 PACKAGE_VERSION=${1:-v2.2.0}
-PYTHON_VERSION=${2:-3.11}
 PACKAGE_URL=https://github.com/pandas-dev/pandas.git
 
-yum install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-devel python${PYTHON_VERSION}-pip git gcc gcc-c++ cmake ninja-build
+yum install -y python-devel python-pip git gcc gcc-c++ cmake ninja-build
 
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME/
 git checkout $PACKAGE_VERSION
 git submodule update --init --recursive
 
-# Setup virtual environment for python
-python${PYTHON_VERSION} -m venv pandas-env
-source pandas-env/bin/activate
 pip install Cython pytest hypothesis build meson meson-python
 
 # Build the package and create whl file (This is dependent on cython)
-python${PYTHON_VERSION} -m build --wheel
-
-# Install wheel
-python${PYTHON_VERSION} -m pip install dist/pandas-2.2.0-cp311-cp311-linux_ppc64le.whl
-if [ $? == 0 ]; then
-     echo "------------------$PACKAGE_NAME::Build_Pass---------------------"
-     echo "$PACKAGE_VERSION $PACKAGE_NAME"
-     echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Pass |  Build_Success"
-else
-     echo "------------------$PACKAGE_NAME::Build_Fail-------------------------"
-     echo "$PACKAGE_VERSION $PACKAGE_NAME"
-     echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Fail |  Build_Fail"
-     exit 1
-fi
+python3 -m build
 
 # Test the package
 cd ..
-python${PYTHON_VERSION} -m pip show pandas
-python${PYTHON_VERSION} -c "import pandas; print(pandas.__file__)"
+python3 -m pip show pandas
+python3 -c "import pandas; print(pandas.__file__)"
 
 if [ $? == 0 ]; then
      echo "------------------$PACKAGE_NAME::Test_Pass---------------------"
      echo "$PACKAGE_VERSION $PACKAGE_NAME"
      echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Pass |  Test_Success"
-
-     # Deactivate python environment (pandas-env)
-         deactivate
-
      exit 0
 else
      echo "------------------$PACKAGE_NAME::Test_Fail-------------------------"
