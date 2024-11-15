351f017996fd8191f415f15825c800031b621a8d
puneetsharma21 <puneet.sharma21@ibm.com>
2024-11-15 06:09:26 -0500

    Revert "removing python installation"
    This reverts commit a14d9add59e270f4076ac8534a52a5db5769f167.

--- x/xformers/xformers_ubi_9_3.sh
+++ x/xformers/xformers_ubi_9_3.sh
@@ -21,12 +21,14 @@
 PACKAGE_NAME=xformers
 PACKAGE_VERSION=${1:-v0.0.28}
 PACKAGE_URL=https://github.com/facebookresearch/xformers.git
+PYTHON_VER=${2:-3.11}
 PARALLEL=${PARALLEL:-$(nproc)}
 export _GLIBCXX_USE_CXX11_ABI=1
 
 # Install dependencies
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm \
-    git cmake ninja-build gcc gcc-c++ g++ rust cargo jq
+    git cmake ninja-build gcc gcc-c++ g++ rust cargo \
+    python${PYTHON_VER}-devel python${PYTHON_VER}-pip jq
 
 dnf install -y gcc-fortran pkg-config openblas-devel atlas
 
@@ -37,7 +39,7 @@ git checkout $PACKAGE_VERSION
 git submodule update --init
 
 # Install Python dependencies
-pip install ninja cmake 'pytest==8.2.2' hydra-core
+python${PYTHON_VER} -m pip install ninja cmake 'pytest==8.2.2' hydra-core
 
 # Install dependency - pytorch
 PYTORCH_VERSION=${PYTORCH_VERSION:-$(curl -sSL https://api.github.com/repos/pytorch/pytorch/releases/latest | jq -r .tag_name)}
@@ -65,8 +67,8 @@ git submodule update --init --recursive
 # Set flags to suppress warnings
 export CXXFLAGS="-Wno-unused-variable -Wno-unused-parameter"
 
-pip install -r requirements.txt
-MAX_JOBS=$PARALLEL python setup.py install
+pip${PYTHON_VER} install -r requirements.txt
+MAX_JOBS=$PARALLEL python${PYTHON_VER} setup.py install
 
 # Install dependency - Scipy
 # install scipy dependency(numpy wheel gets built and installed) and build-setup dependencies
@@ -76,18 +78,18 @@ cd scipy
 git checkout v1.14.1
 git submodule update --init
 
-pip install meson ninja numpy 'setuptools<60.0' Cython
-pip install 'meson-python<0.15.0,>=0.12.1'
-pip install pybind11
-pip install 'patchelf>=0.11.0'
-pip install 'pythran<0.15.0,>=0.12.0'
-pip install build
+python${PYTHON_VER} -m pip install meson ninja numpy 'setuptools<60.0' Cython
+python${PYTHON_VER} -m pip install 'meson-python<0.15.0,>=0.12.1'
+python${PYTHON_VER} -m pip install pybind11
+python${PYTHON_VER} -m pip install 'patchelf>=0.11.0'
+python${PYTHON_VER} -m pip install 'pythran<0.15.0,>=0.12.0'
+python${PYTHON_VER} -m pip install build
 
-pip install --no-build-isolation .
+python${PYTHON_VER} -m pip install --no-build-isolation .
 
 # Build and install xformers
 cd ..
-if ! pip install -e .; then
+if ! python${PYTHON_VER} -m pip install -e .; then
     echo "------------------$PACKAGE_NAME:build_fails---------------------"
     exit 1
 else
@@ -95,7 +97,7 @@ else
 fi
 
 # Test installation
-if python -c "import xformers"; then
+if python${PYTHON_VER} -c "import xformers"; then
     echo "------------------$PACKAGE_NAME::Install_Success---------------------"
 else
     echo "------------------$PACKAGE_NAME::Install_Fail-------------------------"
@@ -104,7 +106,7 @@ fi
 
 # Run Specific tests
 export PY_IGNORE_IMPORTMISMATCH=1
-if ! pytest tests/test_unbind.py tests/test_rotary_embeddings.py tests/test_hydra_helper.py tests/test_compositional_attention.py tests/test_global_attention.py; then
+if ! python${PYTHON_VER} -m pytest tests/test_unbind.py tests/test_rotary_embeddings.py tests/test_hydra_helper.py tests/test_compositional_attention.py tests/test_global_attention.py; then
     echo "------------------$PACKAGE_NAME:test_fails---------------------"
     exit 2
 else
