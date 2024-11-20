cd21d5d6fca60aa68f55dc282528b8c0b0a55edf
pratibh-ibm <pratibh.goshi@ibm.com>
2024-11-20 15:13:09 +0530

    Update pgjdbc_ubi_9.3.sh

--- p/pgjdbc/pgjdbc_ubi_9.3.sh
+++ p/pgjdbc/pgjdbc_ubi_9.3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # -----------------------------------------------------------------------------
 #
-# Package : pgjdbc
-# Version : REL42.7.4
-# Source repo : https://github.com/pgjdbc/pgjdbc.git
-# Tested on : UBI 9.3
-# Language : Java
-# Travis-Check : True
+# Package       : pgjdbc
+# Version       : REL42.7.4
+# Source repo   : https://github.com/pgjdbc/pgjdbc.git
+# Tested on     : UBI 9.3
+# Language      : Java
+# Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer : Pratibh Goshi<pratibh.goshi@ibm.com>
+# Maintainer    : Pratibh Goshi<pratibh.goshi@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
