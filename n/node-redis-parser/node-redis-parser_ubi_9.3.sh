02330ab4e08b9f23939b792f457f124df20c354e
Prachi Kurade <prachi.kurade1@ibm.com>
2024-11-21 17:53:20 +0530

    Support for node-redis-parser

--- n/node-redis-parser/node-redis-parser_ubi_9.3.sh
+++ n/node-redis-parser/node-redis-parser_ubi_9.3.sh
@@ -55,7 +55,7 @@ if ! npm install && npm audit fix --force; then
     exit 1
 fi
 
-if ! npm test; then
+if ! node ./node_modules/istanbul/lib/cli.js cover --preserve-comments ./node_modules/mocha/bin/_mocha -- -R spec --timeout 10000; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
