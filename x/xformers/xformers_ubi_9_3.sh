4434ceee23933a4ea2b9f238d98f6c455f996164
puneetsharma21 <puneet.sharma21@ibm.com>
2024-11-22 03:35:09 -0500

    updating torch version 2.4.1

--- x/xformers/xformers_ubi_9_3.sh
+++ x/xformers/xformers_ubi_9_3.sh
@@ -42,7 +42,7 @@ git submodule update --init
 python${PYTHON_VER} -m pip install ninja cmake 'pytest==8.2.2' hydra-core
 
 # Install dependency - pytorch
-PYTORCH_VERSION=v2.5.1
+PYTORCH_VERSION=v2.4.1
 
 git clone https://github.com/pytorch/pytorch.git
 
