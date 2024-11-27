0507a91c2f89be9b2b3c8c64f66f0a286dfe0993
stutiibm <Stuti.Wali@ibm.com>
2024-11-27 10:11:35 +0000

    removed 3 commands

--- b/biginteger.js/biginteger.js_ubi_9.3.sh
+++ b/biginteger.js/biginteger.js_ubi_9.3.sh
@@ -69,7 +69,7 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME/
 git checkout $PACKAGE_VERSION
 
-if !(npm install; npm audit fix --force; npm audit fix); then
+if ! npm install ; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
