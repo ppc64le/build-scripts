1170c8136d95837a0f6e229c0872d13adfac3b4b
aastha-sharma2 <aastha.sharma4@ibm.com>
2024-11-27 14:59:10 +0530

    Update cryptography_ubi_9.3.sh

--- c/cryptography/cryptography_ubi_9.3.sh
+++ c/cryptography/cryptography_ubi_9.3.sh
@@ -24,32 +24,45 @@ PACKAGE_VERSION=${1:-42.0.7}
 PACKAGE_URL=https://github.com/pyca/cryptography.git
 
 # Install necessary system dependencies
-yum install -y git gcc gcc-c++ make wget openssl-devel bzip2-devel libffi-devel zlib-devel python-devel python-pip openssl openssl-devel 
+yum install -y git gcc gcc-c++ make wget openssl-devel bzip2-devel libffi-devel zlib-devel python-devel python-pip openssl openssl-devel
 
 # Clone the repository
 git clone $PACKAGE_URL
-cd $PACKAGE_NAME  
-git checkout $PACKAGE_VERSION  
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
 
 # Check if Rust is installed
 if ! command -v rustc &> /dev/null; then
     # If Rust is not found, install Rust
     echo "Rust not found. Installing Rust..."
     curl https://sh.rustup.rs -sSf | sh -s -- -y
-    source "$HOME/.cargo/env"  
+    source "$HOME/.cargo/env"
 else
     echo "Rust is already installed."
 fi
 
 # Install additional dependencies
 pip install -r ci-constraints-requirements.txt
-pip install . 
 pip install build wheel cython nox
 
 #install
-if ! pyproject-build ; then
+if ! python3 -m pip install . ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
+fi
+
+#run tests
+if !(pytest -m "not supported or test_verify_cert_signature or verify_cert_signature" --ignore=tests/hazmat/primitives/test_ssh.py --disable-warnings); then
+    echo "------------------$PACKAGE_NAME:build_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:build_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Build_and_Test_Success"
+    deactivate
+    exit 0
 fi
