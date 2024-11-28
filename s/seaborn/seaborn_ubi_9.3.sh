4912d2a22398749a75f8270fc62485114bbc077c
Harithanagothu2 <haritha.nagothu2@ibm.com>
2024-11-28 16:08:55 +0530

    Update seaborn_ubi_9.3.sh

--- s/seaborn/seaborn_ubi_9.3.sh
+++ s/seaborn/seaborn_ubi_9.3.sh
@@ -38,3 +38,19 @@ if ! (pip install .) ; then
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
+
+pip install pytest
+
+#test
+#Skipping some testcase as it is failing on x_86 also.
+if ! (pytest -k "not TestKDEPlotBivariate"); then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
