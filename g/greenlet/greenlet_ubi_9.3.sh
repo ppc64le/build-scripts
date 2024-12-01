d13939b900920942d2ab2b7b8487b308cbc7d767
Harithanagothu2 <haritha.nagothu2@ibm.com>
2024-12-01 19:55:29 +0530

    Update greenlet_ubi_9.3.sh

--- g/greenlet/greenlet_ubi_9.3.sh
+++ g/greenlet/greenlet_ubi_9.3.sh
@@ -31,10 +31,27 @@ git clone $PACKAGE_URL
 cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
+pip install -r dev-requirements.txt
+
 #install
-if ! (python3 setup.py install) ; then
+if ! (pip install .) ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
+fi
+
+python3 -m pip install -U -e ".[test]"
+
+#test
+if ! python3 -m unittest discover -v greenlet.tests ; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
 fi
