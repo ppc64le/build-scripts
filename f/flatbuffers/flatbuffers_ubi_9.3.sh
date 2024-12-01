5c2b2caf22d519dbcd4da2688c898ac7bfe13216
Harithanagothu2 <haritha.nagothu2@ibm.com>
2024-12-01 19:31:38 +0530

    Update flatbuffers_ubi_9.3.sh

--- f/flatbuffers/flatbuffers_ubi_9.3.sh
+++ f/flatbuffers/flatbuffers_ubi_9.3.sh
@@ -24,20 +24,40 @@ PACKAGE_VERSION=${1:-v2.0.0}
 PACKAGE_URL=https://github.com/google/flatbuffers.git
 
 # Install dependencies and tools.
-yum install -y wget gcc gcc-c++ gcc-gfortran git make  python-devel  openssl-devel 
+yum install -y wget gcc gcc-c++ gcc-gfortran git make  python-devel  openssl-devel  cmake
 
 #clone repository 
 git clone $PACKAGE_URL
 cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
+cmake ./
+make 
+
 #checkout to Python folder
 cd python
 
 #install
-if ! (python3 setup.py install) ; then
+if ! (pip install .) ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
+fi
+
+#Install pytest
+pip install pytest
+
+#test
+cd ../tests
+if ! pytest; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
 fi
