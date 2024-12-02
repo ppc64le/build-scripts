b5f2ad3cb135b3edca106e5ebb943c6272fef182
Harithanagothu2 <haritha.nagothu2@ibm.com>
2024-12-02 10:10:43 +0530

    Update bsddb3_ubi_9.3.sh

--- b/bsddb3/bsddb3_ubi_9.3.sh
+++ b/bsddb3/bsddb3_ubi_9.3.sh
@@ -32,9 +32,22 @@ cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 #install
-if ! (python3 setup.py install) ; then
+if ! (pip install .) ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
+fi
+
+#test
+if !(python3 -m unittest discover  -p "test.py") ; then
+    echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
 fi
