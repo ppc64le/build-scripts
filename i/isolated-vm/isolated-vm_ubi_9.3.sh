a82f5c00d1c212d37c17526b80dac9f8d4d4c407
Pooja Shah <Pooja.Shah4@ibm.com>
2024-12-02 12:56:16 +0530

    Minox fix

--- i/isolated-vm/isolated-vm_ubi_9.3.sh
+++ i/isolated-vm/isolated-vm_ubi_9.3.sh
@@ -39,15 +39,13 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME/
 git checkout $PACKAGE_VERSION
 
-# export NODE_DISABLE_CACHE=1
-
-
 if ! npm install --build-from-source ; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
+
 npm cache clean --force
 if ! npm --max-old-space-size=4096 test; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
