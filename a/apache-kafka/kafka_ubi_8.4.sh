db2f75a81347f978fc3546b86336ad020eb3f7e2
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-12-04 16:35:14 +0530

    Update kafka_ubi_8.4.sh

--- a/apache-kafka/kafka_ubi_8.4.sh
+++ a/apache-kafka/kafka_ubi_8.4.sh
@@ -5,6 +5,8 @@
 # Source repo   : https://github.com/apache/kafka
 # Tested on     : UBI 8.2. UBI 8.4
 # Script License: Apache License, Version 2 or later
+# Language      : Java
+# Travis-Check  : False
 # Maintainer    : Amol Patil <amol.patil2@ibm.com>, Priya Seth <sethp@us.ibm.com>
 #
 # Disclaimer: This script has been tested in non-root mode on given
