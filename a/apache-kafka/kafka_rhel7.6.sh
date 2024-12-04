815322c699cd39037f58910d0f75291612eb2c25
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-12-04 11:08:03 +0000

    Update the build-scripts for kafka

--- a/apache-kafka/kafka_rhel7.6.sh
+++ a/apache-kafka/kafka_rhel7.6.sh
@@ -4,6 +4,8 @@
 # Version       : 1.1.1, 2.2.0, 2.2.1-rc1
 # Source repo   : https://github.com/apache/kafka
 # Tested on     : rhel_7.4
+# Language      : Java
+# Travis-Check  : False
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Priya Seth <sethp@us.ibm.com>
 #
--- a/apache-kafka/kafka_rhel_7.4.sh
+++ a/apache-kafka/kafka_rhel_7.4.sh
@@ -4,6 +4,8 @@
 # Version       : 1.1.0
 # Source repo   : https://github.com/apache/kafka
 # Tested on     : rhel_7.4
+# Language      : Java
+# Travis-Check  : False
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Priya Seth <sethp@us.ibm.com>
 #
