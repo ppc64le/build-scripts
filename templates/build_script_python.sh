e6938779c8e11d06f16c2996069ddde5500c11b9
atongse <aditya.tongse@ibm.com>
2024-12-04 11:30:33 +0530

    Update build_script_python.sh for auto PR

--- templates/build_script_python.sh
+++ templates/build_script_python.sh
@@ -23,75 +23,137 @@ PACKAGE_VERSION=
 PACKAGE_URL=
 
 yum install -y git python3 python3-devel.ppc64le gcc gcc-c++ make wget sudo
-pip3 install pytest tox
+pip3 install pytest tox nox
 PATH=$PATH:/usr/local/bin/
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2 | tr -d '"')
+SOURCE=Github
 
-#install rust
-wget https://static.rust-lang.org/dist/rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
-tar -xzf rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
-cd rust-1.75.0-powerpc64le-unknown-linux-gnu
-sudo ./install.sh
-export PATH=$HOME/.cargo/bin:$PATH
-rustc -V
-cargo  -V
-cd ../
-
-# Clone the repository
-git clone $PACKAGE_URL $PACKAGE_NAME
-
-cd $PACKAGE_NAME
-git checkout $PACKAGE_VERSION
-
-#install dependencies from requirements.txt
-
-REQUIREMENTS_PRESENT=(`find . -print | grep -i requirements.txt`)
-
-for i in "${REQUIREMENTS_PRESENT[@]}"; do
-        echo "Installing using pip from file:"
-        echo $i
-        pip3  install -r $i
-done
-
-#check if setup.py file is present
-if [ -f "setup.py" ];then
-        if ! python3 setup.py install ; then
-        echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
-        exit 1
-        fi
-        echo "setup.py file exists"
+# Check if Node.js is already installed
+if ! command -v node &>/dev/null; then
+    # Set the NVM directory
+    export NVM_DIR="$HOME/.nvm"
+    echo "Node.js not found, installing NVM and Node.js..."
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    source "$NVM_DIR/nvm.sh"
+    nvm install 18
+    nvm use 18
+    nvm alias default 18
+    # Add Node.js binary to PATH
+    export PATH="$NVM_DIR/versions/node/v18.0.0/bin:$PATH"
+    echo "Node.js version 18 has been installed and set as default."
 else
-        echo "setup.py not present"
+    echo "Node.js is already installed, skipping installation."
 fi
 
 
-#check if tox file is present
-if [ -f "tox.ini" ];then
-        echo "tox.ini file exists"
-        if ! (tox -e py3) ; then
-        echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-        exit 2
-        else
-        echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-        exit 0
-        fi
+# Installing rust
+if ! command -v rustc &>/dev/null; then
+    echo "Rust not found, installing Rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
+    echo "Rust has been installed."
 else
-        echo "tox.ini not present"
-        if ! pytest ; then
-        echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-        exit 2
-        else
-        echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-        exit 0
+    echo "Rust is already installed, skipping installation."
+fi
+
+# Installing Go
+if ! command -v go &>/dev/null; then
+    echo "Go not found, installing Go..."
+    wget https://golang.org/dl/go1.21.0.linux-ppc64le.tar.gz
+    tar -C /usr/local -xzf go1.21.0.linux-ppc64le.tar.gz
+    rm go1.21.0.linux-ppc64le.tar.gz
+    export PATH="/usr/local/go/bin:${PATH}"
+    echo "Go version 1.21.0 has been installed."
+else
+    echo "Go is already installed, skipping installation."
+fi
+
+
+if [[ "$PACKAGE_URL" == *github.com* ]]; then
+    # Use git clone if it's a Git repository
+    if [ -d "$PACKAGE_NAME" ]; then
+        cd "$PACKAGE_NAME" || exit
+    else
+        if ! git clone "$PACKAGE_URL" "$PACKAGE_NAME"; then
+            echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+            echo "$PACKAGE_URL $PACKAGE_NAME"
+            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Clone_Fails"   
+            exit 1
+        fi
+        cd "$PACKAGE_NAME" || exit
+        git checkout "$PACKAGE_VERSION" || exit
+    fi
+else
+        # If it's not a Git repository, download and untar
+    if [ -d "$PACKAGE_NAME" ]; then
+        cd "$PACKAGE_NAME" || exit
+    else
+        # Use download and untar if it's not a Git repository
+        if ! curl -L "$PACKAGE_URL" -o "$PACKAGE_NAME.tar.gz"; then
+            echo "------------------$PACKAGE_NAME:download_fails---------------------------------------"
+            echo "$PACKAGE_URL $PACKAGE_NAME"
+            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Download_Fails"   
+            exit 1
+        fi
+        mkdir "$PACKAGE_NAME"
+        # Extract the downloaded tarball
+        if ! tar -xzf "$PACKAGE_NAME.tar.gz" -C "$PACKAGE_NAME" --strip-components=1; then
+            echo "------------------$PACKAGE_NAME:untar_fails---------------------------------------"
+            echo "$PACKAGE_URL $PACKAGE_NAME"
+            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Untar_Fails"   
+            exit 1
         fi
 
+        cd "$PACKAGE_NAME" || exit
+    fi
+fi
+
+# Install via pip
+if ! python3 -m pip install ./; then
+    echo "------------------$PACKAGE_NAME:install_fails------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Install_Failed"   
+    exit 1
+fi
+
+if [ -f "pytest.ini" ] || [ -d "tests" ] || [ -d "test" ] || [ -d "$PACKAGE_NAME/tests" ] || [ -d "$PACKAGE_NAME/test" ]; then
+    if ! python3 -m pytest -v; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Install_success_but_test_Fails"   
+        exit 2
+    else
+        echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME" 
+        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success"
+        exit 0
+    fi
+
+elif [ -f noxfile.py ]; then
+    if ! python3 -m nox; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME" 
+        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Install_success_but_test_Fails"   
+        exit 2
+    else
+        echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success" 
+        exit 0
+    fi
+elif [ -f tox.ini ]; then
+    if ! python3 -m tox; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME" 
+        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Install_success_but_test_Fails"   
+        exit 2
+    else
+        echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success" 
+        exit 0
+    fi
+else 
+    echo "No tests were found"
+    exit 0
 fi
