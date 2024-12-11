f7f34055da974e731ba62842d355078d10616957
Bhagyashri Gaikwad <bhagyashri.gaikwad2@ibm.com>
2024-12-11 22:12:24 +0530

    remove wheel creation step

--- n/numpy/numpy_ubi_9.3.sh
+++ n/numpy/numpy_ubi_9.3.sh
@@ -46,35 +46,19 @@ git submodule update --init --recursive
 # Check if the version is 1.26.0
 if [[ $PACKAGE_VERSION == "v1.26.0" ]]; then
     echo "Building NumPy using setup.py for version $PACKAGE_VERSION"
-    # Build using setup.py and create a wheel
-    if ! python${PYTHON_VERSION} setup.py bdist_wheel; then
+    # Build using setup.py
+    if ! python${PYTHON_VERSION} setup.py install; then
         echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
         echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_Fails"
         exit 1
     fi
-    # Install the created wheel
-    if ! python${PYTHON_VERSION} -m pip install dist/*.whl; then
-        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Install_Fails"
-        exit 1
-    fi
-
 else
-    # Build and create a wheel for other versions
-    if ! python${PYTHON_VERSION} -m pip wheel . -w dist; then
-        echo "------------------$PACKAGE_NAME:wheel_build_fails--------------------------------"
+    # Build for other versions
+    if ! python${PYTHON_VERSION} -m pip install . ; then
+        echo "------------------$PACKAGE_NAME:build_fails--------------------------------"
         echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Wheel_Build_Fails"
-        exit 1
-    fi
-
-    # Install the created wheel
-    if ! python${PYTHON_VERSION} -m pip install dist/*.whl; then
-        echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Install_Fails"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Build_Fails"
         exit 1
     fi
 fi
