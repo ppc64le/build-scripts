fd0486191e58407e2445ae4a0fa9a39ec1cf61fd
Md. Shafi Hussain <Md.Shafi.Hussain@ibm.com>
2024-12-15 09:55:32 -0500

    Added build_script, License and build_info for psutil

---
+++ p/psutil/psutil_ubi_9.3.sh
@@ -0,0 +1,83 @@
+#!/bin/bash -e
+
+# -----------------------------------------------------------------------------
+#
+# Package           : psutil
+# Version           : release-6.1.0
+# Source repo       : https://github.com/giampaolo/psutil.git
+# Tested on         : UBI:9.3
+# Language          : Python
+# Travis-Check      : True
+# Script License    : Apache License, Version 2.0
+# Maintainer        : Md. Shafi Hussain <Md.Shafi.Hussain@ibm.com>
+#
+# Disclaimer        : This script has been tested in root mode on given
+# ==========          platform using the mentioned version of the package.
+#                     It may not work as expected with newer versions of the
+#                     package and/or distribution. In such case, please
+#                     contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=psutil
+PACKAGE_URL=https://github.com/giampaolo/psutil.git
+
+PACKAGE_VERSION=${1:-release-6.1.0}
+PYTHON_VERSION=${PYTHON_VERSION:-3.11}
+
+WORKDIR=$(pwd)
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+
+dnf install -yq git gcc-toolset-14 procps net-tools util-linux \
+    python$PYTHON_VERSION-devel \
+    python$PYTHON_VERSION-pip \
+    python$PYTHON_VERSION-wheel \
+    python$PYTHON_VERSION-setuptools
+
+export PATH=/opt/rh/gcc-toolset-14/root/bin:$PATH
+export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-14/root/lib:/opt/rh/gcc-toolset-14/root/lib64:$LD_LIBRARY_PATH
+
+if [ -z $PACKAGE_SOURCE_DIR ]; then
+    git clone $PACKAGE_URL -b $PACKAGE_VERSION
+    cd $PACKAGE_NAME
+    WORKDIR=$(pwd)
+else
+    WORKDIR=$PACKAGE_SOURCE_DIR
+    cd $WORKDIR
+    git checkout $PACKAGE_VERSION
+fi
+git submodule update --init --recursive
+
+# no venv - helps with meson build conflicts #
+rm -rf $WORKDIR/PY_PRIORITY
+mkdir $WORKDIR/PY_PRIORITY
+PATH=$WORKDIR/PY_PRIORITY:$PATH
+ln -sf $(command -v python$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/python
+ln -sf $(command -v python$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/python3
+ln -sf $(command -v python$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/python$PYTHON_VERSION
+ln -sf $(command -v pip$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/pip
+ln -sf $(command -v pip$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/pip3
+ln -sf $(command -v pip$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/pip$PYTHON_VERSION
+##############################################
+
+if ! python -m pip install -v -e .; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+python -m pip install pytest
+if ! make test-posix test-testutils test-unicode test-contracts test-connections; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
