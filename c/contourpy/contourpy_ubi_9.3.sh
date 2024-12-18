d13ee1252cbe778453c333e105d05379f58f11ce
rakshithr5 <rakshith.r5@ibm.com>
2024-12-18 13:58:18 +0530

    Create contourpy_ubi_9.3.sh

---
+++ c/contourpy/contourpy_ubi_9.3.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : contourpy
+# Version          : v1.0.5
+# Source repo      : https://github.com/contourpy/contourpy.git
+# Tested on        : UBI:9.3
+# Language         : Python
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Rakshith R <rakshith.r5@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# -----------------------------------------------------------------------------
+
+PACKAGE_NAME=contourpy
+PACKAGE_VERSION=${1:-v1.0.5}
+PACKAGE_URL=https://github.com/contourpy/contourpy.git
+
+# Update system and install dependencies
+dnf update -y
+#dnf groupinstall -y "Development Tools"
+dnf install -y python3-pip python3-devel libjpeg-devel gcc git
+
+# Install pytest for testing
+pip install matplotlib
+pip install pytest
+
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+# Install the package
+if ! pip install .; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+# Run tests
+if ! pytest; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
