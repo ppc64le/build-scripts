6a940991cc8ce22048a6b710f50c7e66d1497688
aastha-sharma2 <aastha.sharma4@ibm.com>
2024-12-18 21:18:37 +0530

    Update aiohttp_ubi_9.3.sh

--- a/aiohttp/aiohttp_ubi_9.3.sh
+++ a/aiohttp/aiohttp_ubi_9.3.sh
@@ -69,7 +69,7 @@ if ! (pip install .) ; then
 fi
 
 #run tests skipping few tests failing on both ppc64le and x86
-if ! pytest -k "not test_no_warnings and not test_expires and not test_max_age and not test_cookie_jar_clear_expired and not test_c_parser_loaded and not test_invalid_character and not test_invalid_linebreak and not test_subapp and not test_middleware_subapp and not test_unsupported_upgrade and not test_get_extra_info and not test_aiohttp_plugin and not test_imports and not test_simple_subapp and not test_request_tracing_url_params" --disable-warnings; then
+if ! pytest -k "not test_no_warnings and not test_expires and not test_max_age and not test_cookie_jar_clear_expired and not test_c_parser_loaded and not test_invalid_character and not test_invalid_linebreak and not test_subapp and not test_middleware_subapp and not test_unsupported_upgrade and not test_get_extra_info and not test_aiohttp_plugin and not test_import_time and not test_imports and not test_simple_subapp and not test_request_tracing_url_params" --disable-warnings; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
