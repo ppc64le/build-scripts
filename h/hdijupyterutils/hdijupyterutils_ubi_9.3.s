f2489faecc8b5f2766ecf0073322959db5329c4a
aastha-sharma2 <aastha.sharma4@ibm.com>
2024-12-18 14:23:16 +0530

    Revert "Currency: Created build script and build_info.json for Hdijupyterutils" (#4954)
    * Revert "Currency: Created build script and build_info.json for Hdijupyterutil…"
    
    This reverts commit f2c604122c2e2b3de493425b695388aeab0e2858.
    
    * Create LICENSE
    
    * Create build_info.json
    
    * Create hdijupyterutils_ubi_9.3.sh
    
    * Delete h/hdijupyterutils/h/hdijupyterutils/hdijupyterutils_ubi_9.3.sh
    
    * Create hdijupyterutils_ubi_9.3.s

--- h/hdijupyterutils/hdijupyterutils_ubi_9.3.s
+++ h/hdijupyterutils/hdijupyterutils_ubi_9.3.s
@@ -22,31 +22,29 @@
 PACKAGE_NAME=hdijupyterutils
 PACKAGE_VERSION=${1:-0.20.0}
 PACKAGE_URL=https://github.com/jupyter-incubator/sparkmagic.git
-INSTALL_DIR="/sparkmagic"
-PACKAGE_PATH="$INSTALL_DIR/$PACKAGE_NAME"
-
+PACKAGE_DIR=sparkmagic/hdijupyterutils
 
 # Install dependencies
-yum install -y git gcc gcc-c++ make wget openssl-devel bzip2-devel libffi-devel zlib-devel python-devel python-pip krb5-devel
+yum install -y git gcc gcc-c++ make wget openssl-devel bzip2-devel libffi-devel zlib-devel python3-devel python3-pip krb5-devel
 
 # Clone the repository
 git clone $PACKAGE_URL
-cd $PACKAGE_PATH
+cd $PACKAGE_DIR
 git checkout $PACKAGE_VERSION
 
 # install necessay dependencies
-pip install pytest mock build
-pip install -r requirements.txt
+pip install pytest mock
+pip install -r requirements.txt 
 
 #install
-if ! (python3 setup.py install) ; then
+if ! (pip install .) ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
-#run tests  
+#run tests skipping the test failing on both x86 and ppc64le 
 if !(pytest -k "not test_send_to_handler"); then
     echo "------------------$PACKAGE_NAME:build_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
