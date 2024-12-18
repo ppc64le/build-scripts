3447cb559d3da70f8a16e2bd639bef0711f2c0c1
Simran-Sirsat <simran.sirsat@ibm.com>
2024-12-18 11:44:07 +0530

    Update openbao_v2.1.0_ubi_9.3.sh
    Disabled the Travis-Check due to docker in docker issue on Travis.

--- o/openbao/openbao_v2.1.0_ubi_9.3.sh
+++ o/openbao/openbao_v2.1.0_ubi_9.3.sh
@@ -6,7 +6,7 @@
 # Source repo   : https://github.com/openbao/openbao
 # Tested on     : UBI:9.3
 # Language      : GO
-# Travis-Check  : True
+# Travis-Check  : False
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Simran Sirsat <Simran.Sirsat@ibm.com>
 #
