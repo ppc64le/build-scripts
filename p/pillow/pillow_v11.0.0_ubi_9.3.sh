1e2883138b509e8065ab75e094f32f4cb9f81ab6
puneetsharma21 <puneet.sharma21@ibm.com>
2024-12-19 17:06:50 +0530

    ODH: Updated Pillow build script for jenkins integration (#4896)
    * pillow build script for jenkins integration
    
    * changes
    
    * Trigger Travis CI build
    
    * installing git
    
    * changes

--- p/pillow/pillow_v11.0.0_ubi_9.3.sh
+++ p/pillow/pillow_v11.0.0_ubi_9.3.sh
@@ -25,49 +25,61 @@ PYTHON_VER=${2:-"3.11"}
 
 OS_NAME=$(grep '^PRETTY' /etc/os-release | awk -F '=' '{print $2}')
 
-# install core dependencies
-yum install -y python${PYTHON_VER} python${PYTHON_VER}-pip python${PYTHON_VER}-devel gcc git
+echo "Installing dependencies for Python ${PYTHON_VER}"
+yum install -y python${PYTHON_VER} python${PYTHON_VER}-pip python${PYTHON_VER}-devel python${PYTHON_VER}-wheel python${PYTHON_VER}-setuptools git
 
-# install pillow's minimum dependencies
-yum install -y zlib zlib-devel libjpeg-turbo libjpeg-turbo-devel
+dnf install -y https://mirror.stream.centos.org/9-stream/BaseOS/ppc64le/os/Packages/centos-gpg-keys-9.0-24.el9.noarch.rpm \
+	https://mirror.stream.centos.org/9-stream/BaseOS/`arch`/os/Packages/centos-stream-repos-9.0-24.el9.noarch.rpm \
+	https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/BaseOS/`arch`/os
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/`arch`/os
+dnf config-manager --set-enabled crb
+dnf install -y g++ cmake libtiff-devel libjpeg-devel openjpeg2-devel zlib-devel \
+    freetype-devel lcms2-devel libwebp-devel tcl-devel tk-devel \
+    harfbuzz-devel fribidi-devel libraqm-devel libimagequant-devel libxcb-devel
 
 # install build tools for wheel generation
-python${PYTHON_VER} -m pip install --upgrade pip setuptools wheel pytest
+python${PYTHON_VER} -m pip install --upgrade pip setuptools wheel pytest build
 
-# clone source repository
-git clone $PACKAGE_URL $PACKAGE_NAME
-cd $PACKAGE_NAME
+if [ -z $PACKAGE_SOURCE_DIR ]; then
+  git clone $PACKAGE_URL $PACKAGE_NAME
+  cd $PACKAGE_NAME  
+else  
+  cd $PACKAGE_SOURCE_DIR
+fi
+
 git checkout $PACKAGE_VERSION
-git submodule update --init
+git submodule update --init --recursive
 
-# check if setup.py file is present
-if [ -f "setup.py" ]; then
-    echo "setup.py file exists"
 
-    # Build the wheel file
-    if ! python${PYTHON_VER} setup.py bdist_wheel ; then
-        echo "------------------$PACKAGE_NAME:Build_wheel_fails-------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_wheel_Fails"
-        exit 1
-    fi
-
-    # Install the package from the wheel
-    WHEEL_FILE=$(ls dist/*.whl)
-    if ! python${PYTHON_VER} -m pip install $WHEEL_FILE ; then
-        echo "------------------$PACKAGE_NAME:Install_wheel_fails-------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_wheel_Fails"
-        exit 1
-    fi
-
-else
-    echo "setup.py not present"
+# Build the wheel file
+if ! python${PYTHON_VER} -m build --wheel; then
+    echo "------------------$PACKAGE_NAME:Build_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_Fails"
     exit 1
 fi
 
+# Identify the wheel file specific to this Python version
+WHEEL_FILE=$(ls dist/pillow-*-cp${PYTHON_VER//./}*.whl 2>/dev/null)
+if [ -z "$WHEEL_FILE" ]; then
+    echo "No wheel file found for Python ${PYTHON_VER}"
+    ls dist/  # This will list the contents of dist/ to check the wheel files
+    exit 1
+fi
+
+# Install the package from the wheel
+echo "Installing wheel: $WHEEL_FILE for Python ${PYTHON_VER}"
+if ! python${PYTHON_VER} -m pip install "$WHEEL_FILE"; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+
 # Run tests to verify installation
-if ! pytest Tests/test_lib_image.py Tests/test_core_resources.py Tests/test_file_jpeg.py Tests/check_png_dos.py Tests/test_file_apng.py Tests/test_file_png.py ; then
+if ! python${PYTHON_VER} -m pytest Tests/test_lib_image.py Tests/test_core_resources.py Tests/test_file_jpeg.py Tests/check_png_dos.py Tests/test_file_apng.py Tests/test_file_png.py ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
