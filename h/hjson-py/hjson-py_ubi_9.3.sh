b931dd02ba68c2f29b6bdc0de2c141b04efde2f5
aastha-sharma2 <aastha.sharma4@ibm.com>
2024-12-19 16:55:56 +0530

    Currency: Updated build script and build_info.json for hjson-py (#4983)
    * Create LICENSE
    
    * Create build_info.json
    
    * Create hjson-py_ubi_9.3.sh
    
    * Update hjson-py_ubi_9.3.sh

--- h/hjson-py/hjson-py_ubi_9.3.sh
+++ h/hjson-py/hjson-py_ubi_9.3.sh
@@ -24,15 +24,15 @@ PACKAGE_VERSION=${1:-v3.0.2}
 PACKAGE_URL=https://github.com/hjson/hjson-py.git
 
 # Install dependencies
-yum install -y git gcc gcc-c++ make wget openssl-devel bzip2-devel libffi-devel zlib-devel python-devel python-pip
+yum install -y git gcc gcc-c++ make wget openssl-devel bzip2-devel libffi-devel zlib-devel python3-devel python3-pip
 
 # Clone the repository
 git clone $PACKAGE_URL
-cd $PACKAGE_NAME  
-git checkout $PACKAGE_VERSION  
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
 
 # install necessary dependencies
-pip install pytest
+pip install pytest setuptools
 
 #install
 if ! (python3 setup.py install) ; then
@@ -41,7 +41,10 @@ if ! (python3 setup.py install) ; then
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
-if ! pytest; then
+
+#set testpath and run tests
+export PYTHONWARNINGS="ignore::DeprecationWarning"
+if ! pytest ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
