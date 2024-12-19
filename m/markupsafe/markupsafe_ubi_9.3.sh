e97adada85c065c4a1c0d341e922dac6952649f3
ICH <ich@us.ibm.com>
2024-12-19 07:27:32 +0000

    Added build_script and Build_info.json using automation for markupsafe

--- m/markupsafe/markupsafe_ubi_9.3.sh
+++ m/markupsafe/markupsafe_ubi_9.3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 # -----------------------------------------------------------------------------
 #
-# Package       : markupsafe
-# Version       : 2.1.5
-# Source repo   : https://github.com/pallets/markupsafe
-# Tested on     : UBI:9.3
+# Package          : markupsafe
+# Version          : 2.1.1
+# Source repo      : https://github.com/pallets/markupsafe
+# Tested on	: UBI:9.3
 # Language      : Python
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Pranith Rao <Pranith.Rao@ibm.com>
+# Maintainer    : ICH <ich@us.ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -19,33 +19,79 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=markupsafe
-PACKAGE_VERSION=${1:-2.1.5}
+PACKAGE_VERSION=${1:-2.1.1}
 PACKAGE_URL=https://github.com/pallets/markupsafe
 
-yum install -y git python3 python3-devel.ppc64le gcc gcc-c++
-yum remove -y python-chardet
-pip3 install tox
+yum install -y git python3 python3-devel.ppc64le gcc gcc-c++ make wget sudo
+pip3 install pytest tox
 PATH=$PATH:/usr/local/bin/
 
+#install rust
+wget https://static.rust-lang.org/dist/rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
+tar -xzf rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
+cd rust-1.75.0-powerpc64le-unknown-linux-gnu
+sudo ./install.sh
+export PATH=$HOME/.cargo/bin:$PATH
+rustc -V
+cargo  -V
+cd ../
+
+# Clone the repository
 git clone $PACKAGE_URL $PACKAGE_NAME
+
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-if ! python3 setup.py install ; then
-    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
-    exit 1
+#install dependencies from requirements.txt
+
+REQUIREMENTS_PRESENT=(`find . -print | grep -i requirements.txt`)
+
+for i in "${REQUIREMENTS_PRESENT[@]}"; do
+        echo "Installing using pip from file:"
+        echo $i
+        pip3  install -r $i
+done
+
+#check if setup.py file is present
+if [ -f "setup.py" ];then
+        if ! python3 setup.py install ; then
+        echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+        exit 1
+        fi
+        echo "setup.py file exists"
+else
+        echo "setup.py not present"
 fi
 
-if ! (tox -e py3) ; then
-    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-    exit 2
+
+#check if tox file is present
+if [ -f "tox.ini" ];then
+        echo "tox.ini file exists"
+        if ! (tox -e py3) ; then
+        echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+        exit 2
+        else
+        echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+        exit 0
+        fi
 else
-    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-    exit 0
+        echo "tox.ini not present"
+        if ! pytest ; then
+        echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+        exit 2
+        else
+        echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+        exit 0
+        fi
+
 fi
