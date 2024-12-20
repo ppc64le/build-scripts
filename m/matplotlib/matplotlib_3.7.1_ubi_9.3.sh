7a1aa20ffe75be571ee704f0c2f8dffcfe718608
kiranNukal <sai.kiran.nukala@ibm.com>
2024-12-20 16:47:43 +0530

    Create matplotlib_3.7.1_ubi_9.3.sh

---
+++ m/matplotlib/matplotlib_3.7.1_ubi_9.3.sh
@@ -0,0 +1,71 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : matplotlib
+# Version       : v3.7.1
+# Source repo :  https://github.com/matplotlib/matplotlib.git
+# Tested on     : UBI:9.3
+# Language      : Python
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer : Sai Kiran Nukala <sai.kiran.nukala@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on the given
+# platform using the mentioned version of the package.
+# It may not work as expected with newer versions of the
+# package and/or distribution. In such a case, please
+# contact the "Maintainer" of this script.
+#
+# -----------------------------------------------------------------------------
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+yum install -y python311 python3.11-devel python3.11-pip git gcc-c++ cmake wget
+yum install -y openblas-devel ninja-build
+yum install -y zlib zlib-devel libjpeg-turbo libjpeg-turbo-devel
+
+# Clone the matplotlib package.
+PACKAGE_NAME=matplotlib
+PACKAGE_VERSION=${1:-v3.7.1}
+PACKAGE_URL=https://github.com/matplotlib/matplotlib.git
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME/
+git checkout $PACKAGE_VERSION
+git submodule update --init
+
+# Download qhull
+mkdir -p build
+wget 'http://www.qhull.org/download/qhull-2020-src-8.0.2.tgz'
+gunzip qhull-2020-src-8.0.2.tgz
+tar -xvf qhull-2020-src-8.0.2.tar --no-same-owner
+mv qhull-2020.2 build/
+rm -f qhull-2020-src-8.0.2.tar
+
+# Setup virtual environment for python
+pip3.11 install pytest hypothesis build meson pybind11 meson-python
+
+# Build and Install the package (This is dependent on numpy,pillow)
+pip3.11 install  'numpy<2' fontTools setuptools-scm contourpy kiwisolver python-dateutil cycler pyparsing pillow certifi
+
+#install
+if ! (python3.11 -m pip install -e .) ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+# Test the package
+if ! (pytest ./lib/matplotlib/tests/test_units.py); then
+     echo "------------------$PACKAGE_NAME::Test_Pass---------------------"
+     echo "$PACKAGE_VERSION $PACKAGE_NAME"
+     echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Pass |  Test_Success"
+     exit 0
+else
+     echo "------------------$PACKAGE_NAME::Test_Fail-------------------------"
+     echo "$PACKAGE_VERSION $PACKAGE_NAME"
+     echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Fail |  Test_Fail"
+     exit 2
+fi
