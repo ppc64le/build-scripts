60a6393f0ad5634796c7617f5079407bbef7f829
stutiibm <Stuti.Wali@ibm.com>
2024-12-23 09:58:49 +0000

    Currency: renamed  t/text to k/kr-text

--- k/kr-text/text_ubi_8.5.sh
+++ k/kr-text/text_ubi_8.5.sh
@@ -6,7 +6,7 @@
 # Source repo   : https://github.com/kr/text
 # Tested on     : UBI: 8.5
 # Language      : Go
-# Travis-Check  : True
+# Travis-Check  : False
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Shreya Kajbaje <shreya.kajbaje@ibm.com>
 #
