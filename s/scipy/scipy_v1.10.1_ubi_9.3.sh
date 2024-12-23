da2927670e004473b931265e52fe8128c0b29e96
puneetsharma21 <puneet.sharma21@ibm.com>
2024-12-23 09:16:21 -0500

    changes numpy v

--- s/scipy/scipy_v1.10.1_ubi_9.3.sh
+++ s/scipy/scipy_v1.10.1_ubi_9.3.sh
@@ -25,12 +25,22 @@ PYTHON_VER=${2:-"3.11"}
 
 OS_NAME=$(cat /etc/os-release | grep "PRETTY" | awk -F '=' '{print $2}')
 
-# install core dependencies
+# Install core dependencies
 yum install -y gcc gcc-c++ gcc-fortran pkg-config openblas-devel \
     python${PYTHON_VER} python${PYTHON_VER}-pip python${PYTHON_VER}-devel git atlas
 
+# Determine numpy version based on Python version
+if [[ $PYTHON_VER == "3.11" ]]; then
+    NUMPY_VER="1.23.2"
+elif [[ $PYTHON_VER == "3.10" ]]; then
+    NUMPY_VER="1.21.6"
+elif [[ $PYTHON_VER == "3.9" || $PYTHON_VER == "3.8" ]]; then
+    NUMPY_VER="1.19.5"
+else
+    NUMPY_VER="numpy" # Default unpinned version for Python >= 3.12
+fi
 
-# Cloning the repository from remote to local
+# Cloning the repository
 if [ -z $PACKAGE_SOURCE_DIR ]; then
   git clone $PACKAGE_URL
   cd $PACKAGE_NAME
@@ -40,11 +50,10 @@ else
   WORKDIR=$(pwd)
 fi
 
-
 git checkout $PACKAGE_VERSION
 git submodule update --init --recursive
 
-# no venv - helps with meson build conflicts #
+# No venv - helps with meson build conflicts
 rm -rf $WORKDIR/PY_PRIORITY
 mkdir $WORKDIR/PY_PRIORITY
 PATH=$WORKDIR/PY_PRIORITY:$PATH
@@ -55,13 +64,19 @@ ln -sf $(command -v pip$PYTHON_VER) $WORKDIR/PY_PRIORITY/pip
 ln -sf $(command -v pip$PYTHON_VER) $WORKDIR/PY_PRIORITY/pip3
 ln -sf $(command -v pip$PYTHON_VER) $WORKDIR/PY_PRIORITY/pip$PYTHON_VER
 
-# install scipy dependency (numpy wheel gets built and installed) and build-setup dependencies
+# Install build dependencies
 python -m pip install --upgrade pip
-python -m pip install meson ninja 'numpy<1.23' 'setuptools<60.0' Cython==0.29.37
-python -m pip install 'meson-python<0.15.0,>=0.12.1' pybind11 'patchelf>=0.11.0' \
-    'pythran<0.15.0,>=0.12.0' pooch pytest build
+python -m pip install meson ninja \
+    "numpy==$NUMPY_VER" \
+    "pybind11==2.10.1" \
+    'meson-python>=0.11.0,<0.13.0' \
+    'pythran>=0.12.0,<0.13.0' \
+    'Cython>=0.29.32,<3.0' \
+    'wheel<0.39.0' \
+    patchelf>=0.11.0 \
+    pooch pytest build
 
-# build and install
+# Build and install
 if ! python -m pip install -e . -vvv; then
     echo "------------------$PACKAGE_NAME:build_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
@@ -73,7 +88,7 @@ else
     echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Build_Success"
 fi
 
-# run specific tests using pytest
+# Run specific tests using pytest
 if ! python -m pytest scipy/interpolate/tests/test_polyint.py scipy/linalg/tests/test_basic.py; then
     echo "------------------$PACKAGE_NAME::Test_Fail-------------------------"
     echo "$PACKAGE_VERSION $PACKAGE_NAME"
