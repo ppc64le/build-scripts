bdd28aaee33682e9b8cfd9b184f721b40bb67074
kiranNukal <sai.kiran.nukala@ibm.com>
2024-12-24 19:03:12 +0530

    Update numpy_1.23.5_ubi_9.3.sh

--- n/numpy/numpy_1.23.5_ubi_9.3.sh
+++ n/numpy/numpy_1.23.5_ubi_9.3.sh
@@ -20,6 +20,7 @@
 
 # Exit immediately if a command exits with a non-zero status
 set -e
+
 # Variables
 PACKAGE_NAME=numpy
 PACKAGE_VERSION=${1:-v1.23.5}
@@ -43,10 +44,11 @@ if ! pip install -e . ; then
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
-#Ensure the script changes to the working directory before running tests
+cd ../..
+#test
 export PYTEST_ADDOPTS="-k 'not test_cython and not test_extension_type' --deselect=typing/tests/test_generic_alias.py --deselect=random/tests/test_extending.py --deselect=core/tests/test_mem_policy.py --deselect=core/tests/test_numeric.py"
 
-if ! (pytest --pyargs numpy --rootdir=$wdir); then
+if ! (pytest --pyargs numpy); then
     echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
@@ -57,4 +59,3 @@ else
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
     exit 0
 fi
-
