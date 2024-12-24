716b4a49c681588bda7b4e0c7e85fbbb3fb0c540
kiranNukal <sai.kiran.nukala@ibm.com>
2024-12-24 18:20:39 +0530

    Update numpy_1.23.5_ubi_9.3.sh

--- n/numpy/numpy_1.23.5_ubi_9.3.sh
+++ n/numpy/numpy_1.23.5_ubi_9.3.sh
@@ -20,7 +20,6 @@
 
 # Exit immediately if a command exits with a non-zero status
 set -e
-# export wdir=`pwd`
 # Variables
 PACKAGE_NAME=numpy
 PACKAGE_VERSION=${1:-v1.23.5}
@@ -45,12 +44,9 @@ if ! pip install -e . ; then
     exit 1
 fi
 #Ensure the script changes to the working directory before running tests
-#To resolve the error "test cases cannot be run from root directory" 
-cd $wdir
-#test
 export PYTEST_ADDOPTS="-k 'not test_cython and not test_extension_type' --deselect=typing/tests/test_generic_alias.py --deselect=random/tests/test_extending.py --deselect=core/tests/test_mem_policy.py --deselect=core/tests/test_numeric.py"
 
-if ! (pytest --pyargs numpy); then
+if ! (pytest --pyargs numpy --rootdir=$wdir); then
     echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
