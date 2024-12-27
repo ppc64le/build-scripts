08e35ed0ec1d7619ff804705c73c6af73314f132
aastha-sharma2 <aastha.sharma4@ibm.com>
2024-12-27 13:28:47 +0530

    Update cffi_ubi_9_3.sh

--- c/cffi/cffi_ubi_9_3.sh
+++ c/cffi/cffi_ubi_9_3.sh
@@ -35,7 +35,7 @@ git checkout $PACKAGE_VERSION
 pip install pytest py
 
 # Install
-if ! pip install ; then
+if ! pip install .; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
