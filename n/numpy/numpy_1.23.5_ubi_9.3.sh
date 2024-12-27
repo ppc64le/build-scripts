e1eef35421aa3ac0a551fcc78407b844b47c68c2
kiranNukal <sai.kiran.nukala@ibm.com>
2024-12-27 10:06:11 +0530

    Update numpy_1.23.5_ubi_9.3.sh

--- n/numpy/numpy_1.23.5_ubi_9.3.sh
+++ n/numpy/numpy_1.23.5_ubi_9.3.sh
@@ -20,6 +20,7 @@
 
 # Exit immediately if a command exits with a non-zero status
 set -e
+# wdir=""
 # Variables
 PACKAGE_NAME=numpy
 PACKAGE_VERSION=${1:-v1.23.5}
@@ -33,7 +34,9 @@ pip3 install pytest==8.3.4 hypothesis==6.115.5 cython typing_extensions meson==1
 git clone $PACKAGE_URL
 cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
+
 git submodule update --init
+
 #install
 if ! pip install -e . ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
@@ -43,9 +46,8 @@ if ! pip install -e . ; then
 fi
 #test
 sed -i 's/suppress_health_check=hypothesis\.HealthCheck\.all()/suppress_health_check=list(hypothesis.HealthCheck)/' numpy/conftest.py
-export PYTEST_ADDOPTS="-k 'not test_cython and not test_extension_type' --deselect=typing/tests/test_generic_alias.py --deselect=random/tests/test_extending.py --deselect=core/tests/test_mem_policy.py --deselect=core/tests/test_numeric.py --deselect=array_api/tests/test_set_functions.py --deselect=typing/tests/test_typing.py"
+export PYTEST_ADDOPTS="-k 'not test_cython and not test_extension_type' --deselect=typing/tests/test_generic_alias.py --deselect=random/tests/test_extending.py --deselect=core/tests/test_mem_policy.py --deselect=core/tests/test_numeric.py --deselect=array_api/tests/test_set_functions.py --deselect=typing/tests/test_typing.py "
 cd ~
-
 if ! (pytest --pyargs numpy); then
     echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
