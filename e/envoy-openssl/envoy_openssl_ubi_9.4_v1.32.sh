1fa75626185c798b8d9ccbc765298efc125cb66c
anurag-chitrakar <118811657+anurag-chitrakar@users.noreply.github.com>
2025-01-02 15:24:25 +0530

    done some changes for checking correct path

--- e/envoy-openssl/envoy_openssl_ubi_9.4_v1.32.sh
+++ e/envoy-openssl/envoy_openssl_ubi_9.4_v1.32.sh
@@ -106,9 +106,12 @@ bazel/setup_clang.sh $wdir/clang+llvm-14.0.6-powerpc64le-linux-rhel-8.4/
 bazel build -c opt envoy --config=ppc --config=clang --cxxopt=-fpermissive > /dev/null 2>&1 || true
 
 #Generating the CARGO_BAZEL_GENERATOR_URL
-#scriptdir=$(dirname $(realpath $0))
-#echo $scriptdir
-#p=$(find . -name crate_universe)
+scriptdir=$(dirname $(realpath $0))
+echo $scriptdir
+pwd
+cd $wdir
+ls /home/envoy/.cache
+echo $(find /home/envoy/.cache/bazel -name rules_rust)
 echo $HOME
 pushd $(find /home/envoy/.cache/bazel -name rules_rust)
 cd crate_universe
