55df009b6f90a4efbe0d2775b9f6e013bce6ac36
puneetsharma21 <puneet.sharma21@ibm.com>
2025-01-06 15:27:49 -0500

    improvisation

--- a/apache-arrow/arrow_ubi_9.3.sh
+++ a/apache-arrow/arrow_ubi_9.3.sh
@@ -2,11 +2,11 @@
 # -----------------------------------------------------------------------------
 #
 # Package          : arrow
-# Version          : go/v16.1.0
+# Version          : apache-arrow-16.1.0
 
 # Source repo      : https://github.com/apache/arrow
 # Tested on        : UBI 9.3
-# Language         : C++,Go
+# Language         : C++,Python
 # Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
 # Maintainer       : Puneet Sharma <Puneet.Sharma21@ibm.com>
@@ -21,11 +21,10 @@
 
 PACKAGE_NAME=arrow
 PACKAGE_URL=https://github.com/apache/arrow.git
-PACKAGE_VERSION=${1:-go/v16.1.0}
+PACKAGE_VERSION=${1:-apache-arrow-16.1.0}
 PYTHON_VER=${PYTHON_VERSION:-3.11}
 
 OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
-GO_VERSION=`curl -s 'https://go.dev/VERSION?m=text' | grep ^go`
 
 #Dependencies
 yum install -y git sudo wget make gcc gcc-c++ cmake python${PYTHON_VER} python${PYTHON_VER}-pip python${PYTHON_VER}-devel
@@ -36,18 +35,9 @@ yum config-manager --add-repo https://mirror.stream.centos.org/9-stream/BaseOS/p
 yum config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/ppc64le/os
 yum config-manager --set-enabled crb
 
-yum install -y boost-devel.ppc64le gflags-devel rapidjson-devel.ppc64le re2-devel.ppc64le utf8proc-devel.ppc64le gtest-devel gmock-devel
+yum install -y boost-devel.ppc64le gflags-devel rapidjson-devel.ppc64le re2-devel.ppc64le \
+               utf8proc-devel.ppc64le gtest-devel gmock-devel snappy snappy-devel
 
-wget "https://go.dev/dl/$GO_VERSION.linux-ppc64le.tar.gz"
-rm -rf /usr/local/go
-tar -C /usr/local -xf $GO_VERSION.linux-ppc64le.tar.gz
-export GOROOT=/usr/local/go
-export GOPATH=$HOME
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-
-
-go install gotest.tools/gotestsum@latest
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.50.1
 
 if [ -d "$PACKAGE_NAME" ] ; then
 rm -rf $PACKAGE_NAME
@@ -73,7 +63,6 @@ git submodule update --init --recursive
 export PARQUET_TEST_DATA="${PWD}/cpp/submodules/parquet-testing/data"
 export ARROW_TEST_DATA="${PWD}/testing/data"
 
-go install honnef.co/go/tools/cmd/staticcheck@latest
 
 # Build Arrow C++ library
 mkdir cpp/build
@@ -83,85 +72,52 @@ cmake -DCMAKE_BUILD_TYPE=release \
       -Dutf8proc_LIB=/usr/lib64/libutf8proc.so \
       -Dutf8proc_INCLUDE_DIR=/usr/include \
       -DARROW_PYTHON=ON \
+      -DARROW_PARQUET=ON \
       -DARROW_BUILD_TESTS=ON \
       -DARROW_JEMALLOC=ON \
       ..
 make -j$(nproc)
 sudo make install
-cd ../..  # Return to the project root directory
 
-# Check if version requires Go-related steps to be skipped
-# Build Arrow Go bindings
-SKIP_GO=false
-if [[ $PACKAGE_VERSION == apache-arrow-* ]]; then
-    SKIP_GO=true
-    echo "Skipping Go build and tests for version: $PACKAGE_VERSION"
-fi
-
-# Build Arrow Go bindings if not skipped
-if [ "$SKIP_GO" = false ]; then
-    cd go
-    go mod tidy
-
-    if ! go build -tags arrow -v ./...; then
-        echo "------------------$PACKAGE_NAME: Build_fails ---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Build_Fails"
-        exit 1
-    fi
-
-    if ! go test -tags arrow -v ./...; then
-        echo "------------------$PACKAGE_NAME: Build_and_Test_fails -------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Build_and_Test_fails"
-        exit 2
-    else
-        echo "------------------$PACKAGE_NAME: Build_and_Test_success -------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass | Both_Build_and_Test_Success"
-    fi
-
-    cd ..  # Return to the project root directory
-fi
-
-cd python
+cd ../../python
 # Install necessary Python build tools
-pip${PYTHON_VER} install --upgrade setuptools wheel numpy
+pip${PYTHON_VER} install --upgrade pip setuptools wheel numpy setuptools_scm
 pip${PYTHON_VER} install Cython==3.0.8
 
 # Set the environment variable if needed
 export BUILD_TYPE=release 
 export BUNDLE_ARROW_CPP=1
 
-CMAKE_PREFIX_PATH=$ARROW_HOME python${PYTHON_VER} setup.py build_ext --inplace
+export CMAKE_PREFIX_PATH=$ARROW_HOME
 
 # Install the generated Python package
-if ! CMAKE_PREFIX_PATH=$ARROW_HOME python${PYTHON_VER} setup.py install; then
-    echo "------------------$PACKAGE_NAME:build_fails---------------------"
+if ! python${PYTHON_VER} -m pip install -e .; then
+    echo "------------------$PACKAGE_NAME:Build_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail |  Build_fails"
     exit 1
 else
-    echo "------------------$PACKAGE_NAME:build_success-------------------------"
-    echo "$PACKAGE_VERSION $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Build_Success"
+    echo "------------------$PACKAGE_NAME:Build_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Build_Success"
 fi
+
 
 pip${PYTHON_VER} install pytest==6.2.5
 pip${PYTHON_VER} install pytest-lazy-fixture hypothesis
 export PYTEST_PATH=$(pwd)/pyarrow
 
 # Skipped specific tests
-export PYTEST_ADDOPTS="-k 'not test_cython and not test_extension_type' --deselect=pyarrow/tests/test_extension_type.py"
+export PYTEST_ADDOPTS="-k 'not test_cython and not test_extension_type' --deselect=pyarrow/tests/test_extension_type.py --deselect=pyarrow/tests/parquet --deselect=pyarrow/tests/test_dataset.py --deselect=pyarrow/tests/test_pandas.py --deselect=pyarrow/tests/test_acero.py"
 
 # Run Python tests
 if ! python${PYTHON_VER} -m pytest $PYTEST_PATH ; then
-    echo "------------------$PACKAGE_NAME::Python_Test_fails-------------------------"
+    echo "------------------$PACKAGE_NAME::install_success_but_test_fails-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Python_Test_Fails"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Install_success_but_test_Fails"
     exit 3
 else
-    echo "------------------$PACKAGE_NAME::Build_and_All_Tests_success-------------------------"
+    echo "------------------$PACKAGE_NAME::both_install_and_test_success-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass | Build_and_All_Tests_Success"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass | Both_Install_and_Test_Success"
 fi
