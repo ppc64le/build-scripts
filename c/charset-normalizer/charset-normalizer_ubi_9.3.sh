60e54de538f1b992ff4b01d016960fcb1c5942e9
robin-ibm <robin.jain1@ibm.com>
2025-01-06 17:30:48 +0530

    Create charset-normalizer_ubi_9.3.sh

---
+++ c/charset-normalizer/charset-normalizer_ubi_9.3.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+#
+# Package       : charset-normalizer
+# Version       : 2.0.4
+# Source repo   : https://github.com/jawah/charset_normalizer.git
+# Tested on     : UBI:9.3
+# Language      : Python
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Robin Jain <robin.jain1@ibm.com>
+#
+# Disclaimer    : This script has been tested in root mode on given
+# ==========      platform using the mentioned version of the package.
+#                 It may not work as expected with newer versions of the
+#                 package and/or distribution. In such case, please
+#                 contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+# Variables
+PACKAGE_VERSION=${1:-2.0.4}
+PACKAGE_DIR=charset_normalizer
+PACKAGE_URL=https://github.com/jawah/charset_normalizer
+
+# Install dependencies and tools
+yum install -y git gcc gcc-c++ gzip tar make wget xz cmake yum-utils openssl-devel openblas-devel bzip2-devel bzip2 zip unzip libffi-devel zlib-devel autoconf automake libtool cargo pkgconf-pkg-config.ppc64le info.ppc64le fontconfig.ppc64le fontconfig-devel.ppc64le sqlite-devel python-devel
+
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_DIR
+git checkout $PACKAGE_VERSION
+
+# Install requirements
+pip install -r requirements.txt
+
+# Install the package
+if ! python3 setup.py install ; then
+    echo "------------------$PACKAGE_NAME: Install fails -------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Install_Fails"
+    exit 1
+fi
+
+# Run tests
+if ! pytest ; then
+    echo "------------------$PACKAGE_NAME: Install success but test fails -----------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME: Install & test both success ---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
