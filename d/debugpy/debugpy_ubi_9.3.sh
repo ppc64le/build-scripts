5e999c3e70a41a1744f18a06fd63ddd49ae18b82
vivek8123 <Vivek.Sharma20@ibm.com>
2025-01-06 19:58:40 +0530

    Update debugpy_ubi_9.3.sh
    Signed-off-by: vivek8123 <Vivek.Sharma20@ibm.com>

--- d/debugpy/debugpy_ubi_9.3.sh
+++ d/debugpy/debugpy_ubi_9.3.sh
@@ -43,7 +43,7 @@ if ! python3 setup.py build; then
 fi
 
 # Run tests
-if ! pytest --ignore=tests/debugpy/test_run.py --ignore=tests/debugpy/test_exception.py --ignore=tests/debugpy/test_input.py --ignore=tests/debugpy/test_log.py --ignore=tests/debugpy/test_flask.py --ignore=tests/debugpy/test_output.py --ignore=tests/debugpy/test_args.py --ignore=tests/debugpy/test_attach.py; then  
+if ! pytest --ignore=tests/debugpy/test_run.py --ignore=test/debugpy/test_gevent.py --ignore=test/debugpy/test_django.py --ignore=tests/debugpy/test_exception.py --ignore=tests/debugpy/test_input.py --ignore=tests/debugpy/test_log.py --ignore=tests/debugpy/test_flask.py --ignore=tests/debugpy/test_output.py --ignore=tests/debugpy/test_args.py --ignore=tests/debugpy/test_attach.py; then  
     echo "------------------$PACKAGE_NAME: Tests_Fail------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Tests_Fail"
