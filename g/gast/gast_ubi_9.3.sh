1a8b7fdf8fb1c1ddfa33d7aa3d79d2ab279096cc
robin-ibm <robin.jain1@ibm.com>
2025-01-06 16:06:09 +0530

    Create gast_ubi_9.3.sh

---
+++ g/gast/gast_ubi_9.3.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+#
+# Package       : gast
+# Version       : 0.4.0
+# Source repo   : https://github.com/serge-sans-paille/gast.git
+# Tested on     : UBI:9.3
+# Language      : Python
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Robin Jain <robin.jain1@ibm.com>
+#
+# Disclaimer    : This script has been tested in root mode on given
+# ==========      platform using the mentioned version of the package.
+#                 It may not work as expected with newer versions of the
+#                 package and/or distribution. In such case, please
+#                 contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+# Variables
+PACKAGE_VERSION=${1:-0.4.0}
+PACKAGE_NAME=gast
+PACKAGE_URL=https://github.com/serge-sans-paille/gast
+
+# Install dependencies and tools
+yum install -y git gcc gcc-c++ gzip tar make wget xz cmake yum-utils openssl-devel openblas-devel bzip2-devel bzip2 zip unzip libffi-devel zlib-devel cargo python-devel
+
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+# Install requirements
+pip install pytest astunparse
+
+# Install the package
+if ! python3 setup.py install ; then
+    echo "------------------$PACKAGE_NAME: Install fails -------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Install_Fails"
+    exit 1
+fi
+
+# Run tests
+if ! pytest -k "not testCompile" ; then
+    echo "------------------$PACKAGE_NAME: Install success but test fails -----------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME: Install & test both success ---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
