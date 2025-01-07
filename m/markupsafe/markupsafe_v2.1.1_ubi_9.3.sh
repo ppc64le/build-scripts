9e1061aaefe9d1417c9317ca7bed6b8f68336e74
Simran-Sirsat <simran.sirsat@ibm.com>
2025-01-07 15:55:39 +0530

    Currency : Added buildscript for markupsafe to resolve currency dashboard errors. (#5197)

--- m/markupsafe/markupsafe_v2.1.1_ubi_9.3.sh
+++ m/markupsafe/markupsafe_v2.1.1_ubi_9.3.sh
@@ -24,19 +24,12 @@ PACKAGE_URL=https://github.com/pallets/markupsafe
 
 yum install -y git python3 python3-pip python3-devel.ppc64le gcc gcc-c++ make wget openssl-devel bzip2-devel libffi-devel xz cmake zlib-devel openblas-devel
 
-PATH=$PATH:/usr/local/bin/
-
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 #Installing all the requirements
-pip install -r requirements/tests.txt
-pip install -r requirements/typing.txt
-pip install -r requirements/docs.txt
-pip install pytest tox==3.24.5
-pip install wheel==0.37.1
-pip install virtualenv==20.13.1
+pip install pytest setuptools
 
 if ! python3 setup.py install ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
@@ -45,7 +38,7 @@ if ! python3 setup.py install ; then
     exit 1
 fi
 
-if ! (tox -e py3) ; then
+if ! pytest ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
