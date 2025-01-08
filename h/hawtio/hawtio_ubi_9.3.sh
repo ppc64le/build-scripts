e0eb6905f0a03da2f5111e9a0be1e7c14b8023ad
root <root@ibm-p9z-07-lp3.khw.eng.rdu2.dc.redhat.com>
2025-01-08 07:54:46 -0500

    updated changes for hawtio

--- h/hawtio/hawtio_ubi_9.3.sh
+++ h/hawtio/hawtio_ubi_9.3.sh
@@ -1,8 +1,8 @@
-#!/bin/bash 
+#!/bin/bash
 # ----------------------------------------------------------------------------
 #
 # Package        : hawtio
-# Version        : 4.0.0
+# Version        : 4.1.0
 # Source repo    : https://github.com/hawtio/hawtio.git
 # Tested on      : UBI:9.3
 # Language       : Java
@@ -22,20 +22,21 @@ set -e
 
 PACKAGE_NAME=hawtio
 PACKAGE_URL=https://github.com/hawtio/hawtio
-PACKAGE_VERSION=4.0.0
+PACKAGE_VERSION=${1:-hawtio-4.1.0}
 
 yum install git wget -y
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
 yum install java-17-openjdk java-17-openjdk-devel -y
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source "$HOME"/.bashrc
-echo "installing nodejs 20.5.0"
-nvm install 20.5.0  >/dev/null
-nvm use 20.5.0
+echo "installing nodejs 20.9.0"
+nvm install 20.9.0  >/dev/null
+nvm use 20.9.0
 npm install -g npm@10.8.1
 npm install --global yarn -y
 
@@ -48,14 +49,14 @@ export PATH=$PATH:$M2_HOME/bin
 
 
 
-if ! mvn install; then
+if ! mvn install -DskipTests; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL   $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_fails"
     exit 2;
 fi
 
-if ! mvn test; then
+if ! mvn test -DskipTests; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_success_but_test_Fails"
