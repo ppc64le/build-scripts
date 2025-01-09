937e997a67ef359fa579c4e366af555ef0ef9f14
vivek8123 <Vivek.Sharma20@ibm.com>
2025-01-09 15:44:43 +0530

    Create jsonschema_ubi_9.3.sh
    Signed-off-by: vivek8123 <Vivek.Sharma20@ibm.com>

---
+++ j/jsonschema/jsonschema_ubi_9.3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package        : jsonschema
+# Version        : v4.17.3
+# Source repo    : https://github.com/python-jsonschema/jsonschema.git
+# Tested on      : UBI 9.3
+# Language       : Python
+# Travis-Check   : True
+# Script License : Apache License, Version 2 or later
+# Maintainer     : Vivek Sharma <vivek.sharma20@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on the given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such cases, please
+#             contact the "Maintainer" of this script.
+#
+# -----------------------------------------------------------------------------
+
+PACKAGE_NAME=jsonschema
+PACKAGE_VERSION=${1:-v4.17.3}
+PACKAGE_URL=https://github.com/python-jsonschema/jsonschema.git
+
+# Install necessary system packages
+yum install -y git gcc gcc-c++ gzip tar make wget xz cmake yum-utils openssl-devel openblas-devel bzip2-devel bzip2 zip unzip libffi-devel zlib-devel autoconf automake libtool cargo pkgconf-pkg-config.ppc64le info.ppc64le fontconfig.ppc64le fontconfig-devel.ppc64le sqlite-devel python-devel
+
+# Clone the repository
+git clone ${PACKAGE_URL} ${PACKAGE_NAME}
+cd ${PACKAGE_NAME}
+git checkout ${PACKAGE_VERSION}
+
+# Install pytest
+pip3 install pytest
+
+# Install the package
+pip3 install .
+
+# Run tests using pytest
+if ! pytest; then
+    echo "------------------$PACKAGE_NAME: Tests_Fail------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Tests_Fail"
+    exit 1
+else
+    echo "------------------$PACKAGE_NAME: Install & test both success ---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
