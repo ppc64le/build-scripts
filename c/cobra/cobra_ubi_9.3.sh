f31b9efe445ad2ab2c82ac43add5809ae5682a5f
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2025-01-15 05:58:26 +0000

    Currency:Added build-script,build_info.json for cobra

---
+++ c/cobra/cobra_ubi_9.3.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : cobra
+# Version          : v1.8.1
+# Source repo      : https://github.com/spf13/cobra
+# Tested on	   : UBI:9.3 
+# Language         : GO
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : vinodk99 <Vinod.K1@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+PACKAGE_NAME=cobra
+PACKAGE_VERSION=${1:-v1.8.1}
+PACKAGE_URL=https://github.com/spf13/cobra
+
+
+OS_NAME=$(grep ^PRETTY_NAME /etc/os-release | cut -d= -f2)
+
+yum install git wget gcc gcc-c++ make -y
+
+wget https://go.dev/dl/go1.23.1.linux-ppc64le.tar.gz
+tar -C  /usr/local -xf go1.23.1.linux-ppc64le.tar.gz
+export GOROOT=/usr/local/go
+export GOPATH=$HOME
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+git clone $PACKAGE_URL 
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+if ! go build ./...; then
+        echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+	exit 1
+fi
+
+if ! go test ./... -race ; then
+        echo "------------------$PACKAGE_NAME:test_fails---------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Test_Fails"
+	exit 2
+else
+        echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
+        echo "$PACKAGE_VERSION $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Install_and_Test_Success"
+	exit 0
+fi
