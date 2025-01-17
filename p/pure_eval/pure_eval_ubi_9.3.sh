8489c477b72d33a28b305a73a48aeb230947e47c
root <root@devfirst-vivek-sharma.pokprv.stglabs.ibm.com>
2025-01-17 04:01:01 -0500

    pure_eval

---
+++ p/pure_eval/pure_eval_ubi_9.3.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package        : pure_eval
+# Version        : 0.2.2
+# Source repo    : https://github.com/alexmojaki/pure_eval.git
+# Tested on      : UBI 9.3
+# Language       : Python
+# Travis-Check   : True
+# Script License : Apache License, Version 2 or later
+# Maintainer     : Vivek Sharma <vivek.sharma20@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on the given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such cases, please
+#             contact the "Maintainer" of this script.
+#
+# -----------------------------------------------------------------------------
+
+PACKAGE_NAME=pure_eval
+PACKAGE_VERSION=${1:-0.2.2}
+PACKAGE_URL=https://github.com/alexmojaki/pure_eval.git
+
+# Install necessary system packages
+yum install -y git gcc gcc-c++ make wget python3-devel
+
+# Clone the repository
+git clone ${PACKAGE_URL} ${PACKAGE_NAME}
+cd ${PACKAGE_NAME}
+git checkout v${PACKAGE_VERSION}
+
+# Install the package
+pip3 install .
+
+# Install test dependencies
+pip3 install pytest
+
+# Run tests
+if ! pytest; then
+    echo "------------------$PACKAGE_NAME: Tests_Fail------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Tests_Fail"
+    exit 1
+else
+    echo "------------------$PACKAGE_NAME: Install & test both success ---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
+
