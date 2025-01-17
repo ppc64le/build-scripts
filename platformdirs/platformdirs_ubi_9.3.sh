f304baefd36a05d8e92b45b244d42a0709cec2ba
root <root@devfirst-vivek-sharma.pokprv.stglabs.ibm.com>
2025-01-17 01:59:33 -0500

    platformdirs

---
+++ platformdirs/platformdirs_ubi_9.3.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package        : platformdirs
+# Version        : 2.5.2
+# Source repo    : https://github.com/tox-dev/platformdirs.git
+# Tested on      : UBI 9.3
+# Language       : Python
+# Travis-Check   : True
+# Script License : Apache License, Version 2 or later
+# Maintainer     : Vivek Sharma <vivek.sharma20@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on the given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such cases, please
+#             contact the "Maintainer" of this script.
+#
+# -----------------------------------------------------------------------------
+
+PACKAGE_NAME=platformdirs
+PACKAGE_VERSION=${1:-2.5.2}
+PACKAGE_URL=https://github.com/tox-dev/platformdirs.git
+
+# Install necessary system packages
+yum install -y git gcc gcc-c++ gzip tar make wget xz cmake yum-utils openssl-devel openblas-devel bzip2-devel bzip2 zip unzip libffi-devel zlib-devel autoconf automake libtool cargo sqlite-devel python-devel
+
+# Clone the repository
+git clone ${PACKAGE_URL} ${PACKAGE_NAME}
+cd ${PACKAGE_NAME}
+git checkout ${PACKAGE_VERSION}
+
+# Install the package
+pip3 install .
+
+# Install test dependencies
+pip3 install pytest pytest_mock appdirs
+
+
+# Run tests
+if ! pytest; then
+    echo "------------------$PACKAGE_NAME: Tests_Fail------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Tests_Fail"
+    exit 1
+else
+    echo "------------------$PACKAGE_NAME: Install & test both success ---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
+
