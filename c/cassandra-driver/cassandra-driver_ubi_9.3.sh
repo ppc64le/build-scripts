53a25167e9129cda97a5ac4aa69c0ceda7345768
Ramnath Nayak <ramnath.nayak@ibm.com>
2025-01-21 13:38:32 +0530

    Create cassandra-driver_ubi_9.3.sh

---
+++ c/cassandra-driver/cassandra-driver_ubi_9.3.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : cassandra-driver
+# Version          : 3.29.0
+# Source repo      : http://github.com/datastax/python-driver
+# Tested on	   : UBI:9.3
+# Language         : Python
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Ramnath Nayak <Ramnath.Nayak@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=cassandra-driver
+PACKAGE_VERSION=${1:-3.29.0}
+PACKAGE_URL=http://github.com/datastax/python-driver
+PACKAGE_DIR=python-driver
+
+# Install dependencies
+yum install -y git gcc gcc-c++ make wget openssl-devel bzip2-devel krb5-devel libffi-devel zlib-devel python-devel python-pip
+
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_DIR
+git checkout $PACKAGE_VERSION
+
+#install necessary Python packages
+pip install wheel pytest tox nox
+
+
+#Install
+if ! (python3 setup.py install) ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+# Run test cases
+if !(python3 -m tox -e py39); then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
