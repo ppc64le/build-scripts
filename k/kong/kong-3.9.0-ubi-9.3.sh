5ddf71d23cb8f955bd3cb92b8dd567dd0b6ebe25
kavita-rane2 <kavita.rane2@ibm.com>
2025-01-24 17:54:04 +0530

    Update kong-3.9.0-ubi-9.3.sh

--- k/kong/kong-3.9.0-ubi-9.3.sh
+++ k/kong/kong-3.9.0-ubi-9.3.sh
@@ -19,7 +19,7 @@
 # --------------------------------------------------------------------------------
 
 echo "GITHUB_TOKN"
-echo $GH_TOKEN
+echo ${{ secrets.MY_GITHUB_TOKEN }}
 if [ -z "${{ secrets.MY_GITHUB_TOKEN }}" ]; then
   echo "missing github_token. Please set Environment Variable <GITHUB_TOKEN>"
   exit 1
