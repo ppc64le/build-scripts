78010645f4b4ff9ac9a14761c46a03afb3dbe30a
Md. Shafi Hussain <Md.Shafi.Hussain@ibm.com>
2025-01-25 02:27:55 -0500

    ODH: Added build script, build_info and License for httptools

---
+++ h/httptools/httptools_ubi_9.3.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -e
+
+# -----------------------------------------------------------------------------
+#
+# Package           : httptools
+# Version           : v0.6.4
+# Source repo       : https://github.com/MagicStack/httptools.git
+# Tested on         : UBI:9.3
+# Language          : Python
+# Travis-Check      : True
+# Script License    : Apache License, Version 2.0
+# Maintainer        : Md. Shafi Hussain <Md.Shafi.Hussain@ibm.com>
+#
+# Disclaimer        : This script has been tested in root mode on given
+# ==========          platform using the mentioned version of the package.
+#                     It may not work as expected with newer versions of the
+#                     package and/or distribution. In such case, please
+#                     contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=httptools
+PACKAGE_URL=https://github.com/MagicStack/httptools.git
+
+PACKAGE_VERSION=${1:-v0.6.4}
+PYTHON_VERSION=${PYTHON_VERSION:-3.11}
+
+WORKDIR=$(pwd)
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+dnf install -y git g++ \
+    python${PYTHON_VERSION}-devel \
+    python${PYTHON_VERSION}-pip \
+    python${PYTHON_VERSION}-wheel \
+    python${PYTHON_VERSION}-setuptools 
+
+if [ -z $PACKAGE_SOURCE_DIR ]; then
+    git clone $PACKAGE_URL -b $PACKAGE_VERSION
+    cd $PACKAGE_NAME
+    WORKDIR=$(pwd)
+else
+    WORKDIR=$PACKAGE_SOURCE_DIR
+    cd $WORKDIR
+    git checkout $PACKAGE_VERSION
+fi
+
+git submodule sync
+git submodule update --init --recursive
+
+# no venv - helps with meson build conflicts #
+rm -rf $WORKDIR/PY_PRIORITY
+mkdir $WORKDIR/PY_PRIORITY
+PATH=$WORKDIR/PY_PRIORITY:$PATH
+ln -sf $(command -v python$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/python
+ln -sf $(command -v python$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/python3
+ln -sf $(command -v python$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/python$PYTHON_VERSION
+ln -sf $(command -v pip$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/pip
+ln -sf $(command -v pip$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/pip3
+ln -sf $(command -v pip$PYTHON_VERSION) $WORKDIR/PY_PRIORITY/pip$PYTHON_VERSION
+##############################################
+
+if ! python -m pip install -v .; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! python -m unittest -v tests.suite; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
