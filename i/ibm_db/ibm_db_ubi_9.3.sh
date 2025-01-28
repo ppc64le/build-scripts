69d937cffddbfc4a6806d2f7ec14fe9ae717a274
root <root@devfirst-vivek-sharma.pokprv.stglabs.ibm.com>
2025-01-28 01:26:56 -0500

    ibm_db

---
+++ i/ibm_db/ibm_db_ubi_9.3.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package        : ibm_db
+# Version        : v3.2.3
+# Source repo    : https://github.com/ibmdb/python-ibmdb.git
+# Tested on      : UBI 9.3
+# Language       : Python
+# Travis-Check   : True
+# Script License : Apache License, Version 2 or later
+# Maintainer     : Vivek Sharma <vivek.sharma20@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on the given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such cases, please
+#             contact the "Maintainer" of this script.
+#
+# -----------------------------------------------------------------------------
+
+PACKAGE_NAME=ibm_db
+PACKAGE_VERSION=${1:-v3.2.3}
+PACKAGE_DIR=python-ibmdb
+PACKAGE_URL=https://github.com/ibmdb/python-ibmdb.git
+
+# Install necessary system packages
+yum install -y git python-devel gcc gcc-c++ libxcrypt numactrl gzip tar make wget xz cmake yum-utils openssl-devel openblas-devel bzip2-devel bzip2 zip unzip libffi-devel zlib-devel autoconf automake libtool cargo pkgconf-pkg-config.ppc64le info.ppc64le fontconfig.ppc64le fontconfig-devel.ppc64le sqlite-devel
+
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_DIR
+git checkout $PACKAGE_VERSION
+
+# Install test dependencies
+pip3 install pytest config tox
+
+# Install the package
+if ! python3 setup.py install; then
+    echo "------------------$PACKAGE_NAME: Installation successful ---------------------"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | Installation_Success"
+else
+    echo "------------------$PACKAGE_NAME: Installation failed ---------------------"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Installation_Failure"
+    exit 1
+fi
+
+# Run tests
+if ! tox -e py3; then
+    echo "------------------$PACKAGE_NAME: Tests failed ------------------"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Tests_Failure"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME: Install & test both successful ---------------------"
+    echo "$PACKAGE_NAME | $PACKAGE_URL"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
+
