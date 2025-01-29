d2cf52a14c9268a5c3b0971fef8257fb57eec9b3
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2025-01-29 20:46:51 +0530

    Update pyarrow_ubi_9.3.sh

--- p/pyarrow/pyarrow_ubi_9.3.sh
+++ p/pyarrow/pyarrow_ubi_9.3.sh
@@ -17,36 +17,36 @@
 # contact the "Maintainer" of this script.
 #
 # -----------------------------------------------------------------------------
- 
+
 # Variables
 PACKAGE_NAME=pyarrow
 PACKAGE_VERSION=${1:-apache-arrow-11.0.0}
 PACKAGE_URL=https://github.com/apache/arrow.git
-PACKAGE_DIR=/arrow/python
-CURRENT_DIR=$pwd
- 
+PACKAGE_DIR=./arrow/python
+CURRENT_DIR="${PWD}"
+
 yum install -y git wget gcc gcc-c++ python python3-devel python3 python3-pip openssl-devel cmake
- 
+
 echo "Dependencies installed."
- 
+
 mkdir dist
 export CXX=g++
 export CC=gcc
 export ARROW_HOME=$(pwd)/dist
 export LD_LIBRARY_PATH=$ARROW_HOME/lib:$LD_LIBRARY_PATH
 export CMAKE_PREFIX_PATH=$ARROW_HOME:$CMAKE_PREFIX_PATH
- 
+
 echo "Cloning the repository..."
 git clone $PACKAGE_URL
 cd arrow
 git checkout $PACKAGE_VERSION
 git submodule update --init
 echo "Repository cloned and checked out to version $PACKAGE_VERSION."
- 
+
 echo "Setting test data paths..."
 export PARQUET_TEST_DATA="${PWD}/cpp/submodules/parquet-testing/data"
 export ARROW_TEST_DATA="${PWD}/testing/data"
- 
+
 echo "Applying fixes for nogil placement and rvalue issues..."
 sed -i -E 's/(nogil)(.*)(except[^:]*)/\2\3 \1/' python/pyarrow/error.pxi
 sed -i -E 's/(nogil)(.*)(except[^:]*)/\2\3 \1/' python/pyarrow/includes/libarrow.pxd
@@ -57,15 +57,15 @@ sed -i -E 's/\&\&/\&/g' python/pyarrow/includes/libarrow.pxd
 sed -i -E 's/\&\&/\&/g' python/pyarrow/lib.pxd
 sed -i -E 's/\&\&/\&/g' python/pyarrow/includes/libarrow_fs.pxd
 echo "Fixes applied."
- 
+
 pip install -r python/requirements-build.txt
 pip install cython  wheel numpy==1.21.2
- 
+
 echo "Preparing for build..."
- 
+
 mkdir cpp/build
 cd cpp/build
- 
+
 cmake -DCMAKE_INSTALL_PREFIX=$ARROW_HOME \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release \
@@ -87,14 +87,14 @@ cmake -DCMAKE_INSTALL_PREFIX=$ARROW_HOME \
       ..
 make -j$(nproc)
 make install
-cd /
- 
+cd ../../..
+
 cd $PACKAGE_DIR
 export PYARROW_WITH_PARQUET=1
 export PYARROW_WITH_DATASET=1
 export PYARROW_PARALLEL=4
 export PYARROW_BUILD_TYPE="release"
- 
+
 if ! python3 setup.py bdist_wheel ; then
         echo "------------------$PACKAGE_NAME:wheel_built_fails---------------------"
         echo "$PACKAGE_VERSION $PACKAGE_NAME"
@@ -106,8 +106,6 @@ else
         echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Wheel_built_success"
         exit 0
 fi
- 
-#skipping the tests as most of the tests are parity with x86 also
-cp -r $PACKAGE_DIR/dist/*.whl /$CURRENT_DIR
 
- 
+#skipping the tests as most of the tests are parity with x86 also
+cp -r $PACKAGE_DIR/dist/*.whl $CURRENT_DIR
