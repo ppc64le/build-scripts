7e387f0c342d3d3d17ca4f7d89f6904d42fc4238
Harithanagothu2 <haritha.nagothu2@ibm.com>
2025-02-03 15:07:56 +0530

    Update dask_ubi_9.3.sh

--- d/dask/dask_ubi_9.3.sh
+++ d/dask/dask_ubi_9.3.sh
@@ -58,9 +58,9 @@ fi
 pip install pytest
 pip install --upgrade pip setuptools wheel
 pip install packaging --no-binary :all:
-pip install pandas==1.5.3
 pip install numpy==1.26.3
 pip install ninja
+pip install pandas==1.5.3
 pip install cachey --no-binary :all:
 pip install distributed --no-binary :all:
 pip install graphviz --no-binary :all:
