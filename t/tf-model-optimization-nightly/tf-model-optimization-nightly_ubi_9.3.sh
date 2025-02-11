82e13acf94240bef054a5b7d137fb47ec11d2a11
vivek8123 <Vivek.Sharma20@ibm.com>
2025-02-11 13:57:09 +0530

    Create tf-model-optimization-nightly_ubi_9.3.sh
    Signed-off-by: vivek8123 <Vivek.Sharma20@ibm.com>

---
+++ t/tf-model-optimization-nightly/tf-model-optimization-nightly_ubi_9.3.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package        : tf-model-optimization-nightly
+# Version        : v0.7.4
+# Source repo    : https://github.com/tensorflow/model-optimization.git
+# Tested on      : UBI 9.3
+# Language       : Python
+# Script License : Apache License, Version 2 or later
+# Maintainer     : Vivek Sharma <vivek.sharma20@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on the given
+#             platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such cases, please
+#             contact the "Maintainer" of this script.
+#
+# -----------------------------------------------------------------------------
+
+PACKAGE_NAME=tf-model-optimization-nightly
+PACKAGE_VERSION=${1:-v0.7.4}
+PACKAGE_DIR=model-optimization
+PACKAGE_URL=https://github.com/tensorflow/model-optimization.git
+
+# Install necessary system packages
+yum install -y git gcc gcc-c++ gzip tar make wget xz cmake yum-utils openssl-devel openblas-devel bzip2-devel bzip2 zip unzip libffi-devel zlib-devel autoconf automake libtool cargo pkgconf-pkg-config.ppc64le info.ppc64le fontconfig.ppc64le fontconfig-devel.ppc64le sqlite-devel python3 python3-devel
+
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_DIR
+git checkout $PACKAGE_VERSION
+
+# Install test dependencies
+pip3 install pytest tox
+
+# Removing chardet becuase it is giving issue while build the Package.
+yum remove -y python-chardet
+
+# Install the package
+if ! (pip3 install .); then
+    echo "------------------$PACKAGE_NAME: Installation failed ---------------------"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Installation_Failure"
+    exit 1
+fi
+
+# Run tests
+if ! tox -e py3; then
+    echo "------------------$PACKAGE_NAME: Tests_Fail------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Tests_Fail"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME: Install & test both success ---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
