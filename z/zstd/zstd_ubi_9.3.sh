1102b55ac0b64f0c53b128258673b3aa5d5ff3cb
Harithanagothu2 <haritha.nagothu2@ibm.com>
2025-02-18 13:44:30 +0530

    Create zstd_ubi_9.3.sh

---
+++ z/zstd/zstd_ubi_9.3.sh
@@ -0,0 +1,56 @@
+!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : zstd
+# Version       : v1.5.6.1
+# Source repo   : https://github.com/sergey-dryabzhinsky/python-zstd.git
+# Tested on     : UBI:9.3
+# Language      : Python
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Haritha Nagothu <haritha.nagothu2@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on the given
+# platform using the mentioned version of the package.
+# It may not work as expected with newer versions of the
+# package and/or distribution. In such a case, please
+# contact the "Maintainer" of this script.
+#
+# -----------------------------------------------------------------------------
+
+# Variables
+PACKAGE_NAME=zstd
+PACKAGE_VERSION=${1:-v1.5.6.1}
+PACKAGE_URL=https://github.com/sergey-dryabzhinsky/python-zstd.git
+PACKAGE_DIR=python-zstd
+
+# Install dependencies and tools.
+yum install -y git wget gcc gcc-c++  python-devel python-pip openssl-devel cmake openblas-devel libzstd.ppc64le
+pip install pytest
+
+#clone repository
+git clone $PACKAGE_URL
+cd  $PACKAGE_DIR
+git checkout $PACKAGE_VERSION
+git submodule update --init
+
+#install
+if ! python3 setup.py install ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+#test
+if ! pytest; then
+    echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
