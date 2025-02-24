851173693be42f329603c06bbe9454c84560b001
Shivansh S <shivansh.s1@ibm.com>
2025-02-25 00:29:24 +0530

    updated pandas script

--- p/pandas/pandas_2.2.3_ubi_9.3.sh
+++ p/pandas/pandas_2.2.3_ubi_9.3.sh
@@ -2,13 +2,13 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : pandas
-# Version       : v2.2.3
+# Version       : v2.1.1
 # Source repo   : https://github.com/pandas-dev/pandas.git
 # Tested on     : UBI:9.3
-# Language      : Python, C, Cython, Html
+# Language      : Python, C, Cython, HTML
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Shivansh Sharma <Shivansh.S1@ibm.com>
+# Maintainer    : Vivek Sharma <vivek.sharma20@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -16,58 +16,61 @@
 #             package and/or distribution. In such case, please
 #             contact "Maintainer" of this script.
 #
-# ----------------------------------------------------------------------------
-
+# -----------------------------------------------------------------------------
 PACKAGE_NAME=pandas
 PACKAGE_VERSION=${1:-v2.2.3}
 PYTHON_VERSION=${2:-3.12}
 PACKAGE_URL=https://github.com/pandas-dev/pandas.git
+PACKAGE_DIR=pandas
 
-yum install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-devel python${PYTHON_VERSION}-pip git gcc gcc-c++ cmake ninja-build
+# Install system dependencies including SQLite and LZMA libraries
+yum install -y git gcc gcc-c++ python${PYTHON_VERSION} python${PYTHON_VERSION}-pip python${PYTHON_VERSION}-devel \
+    gzip tar make wget xz cmake yum-utils openssl-devel \
+    openblas-devel bzip2-devel bzip2 zip unzip libffi-devel zlib-devel autoconf \
+    automake libtool cargo pkgconf-pkg-config.ppc64le info.ppc64le fontconfig.ppc64le \
+    fontconfig-devel.ppc64le sqlite-devel
 
+# Clone the pandas repository and checkout the required version
 git clone $PACKAGE_URL
-cd $PACKAGE_NAME/
+cd $PACKAGE_DIR/
 git checkout $PACKAGE_VERSION
+
+# Initialize and update submodules
 git submodule update --init --recursive
 
-# Setup virtual environment for python
-python${PYTHON_VERSION} -m venv pandas-env
-source pandas-env/bin/activate
-pip install Cython pytest hypothesis build meson meson-python
+# Install dependencies
+python${PYTHON_VERSION} -m pip install --upgrade pip
+python${PYTHON_VERSION} -m pip install pytest hypothesis build meson meson-python
+python${PYTHON_VERSION} -m pip install cython
+python${PYTHON_VERSION} -m pip install --upgrade --force-reinstall setuptools
+python${PYTHON_VERSION} -m pip install --upgrade six
+python${PYTHON_VERSION} -m pip install meson-python==0.13.1
+python${PYTHON_VERSION} -m pip install patchelf==0.11.0
+python${PYTHON_VERSION} -m pip install meson==1.2.1
+python${PYTHON_VERSION} -m pip install oldest-supported-numpy==2022.8.16
+python${PYTHON_VERSION} -m pip install ninja
+python${PYTHON_VERSION} -m pip install versioneer[toml]
+python${PYTHON_VERSION} -m pip install numpy
 
-# Build the package and create whl file (This is dependent on cython)
-python${PYTHON_VERSION} -m build --wheel
-
-# Install wheel
-python${PYTHON_VERSION} -m pip install /$PACKAGE_NAME/dist/pandas*.whl
-if [ $? == 0 ]; then
-     echo "------------------$PACKAGE_NAME::Build_Pass---------------------"
-     echo "$PACKAGE_VERSION $PACKAGE_NAME"
-     echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Pass |  Build_Success"
-else
-     echo "------------------$PACKAGE_NAME::Build_Fail-------------------------"
-     echo "$PACKAGE_VERSION $PACKAGE_NAME"
-     echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Fail |  Build_Fail"
+# Install pandas package
+if ! (python${PYTHON_VERSION} -m pip install .); then
+     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+     echo "$PACKAGE_URL $PACKAGE_NAME"
+     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | Python $PYTHON_VERSION | GitHub | Fail | Install_Fails"
      exit 1
 fi
-
-# Test the package
+    
 cd ..
-python${PYTHON_VERSION} -m pip show pandas
-python${PYTHON_VERSION} -c "import pandas; print(pandas.__file__)"
 
-if [ $? == 0 ]; then
-     echo "------------------$PACKAGE_NAME::Test_Pass---------------------"
-     echo "$PACKAGE_VERSION $PACKAGE_NAME"
-     echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Pass |  Test_Success"
-
-     # Deactivate python environment (pandas-env)
-         deactivate
-
-     exit 0
+# Test pandas package
+if ! (python${PYTHON_VERSION} -c "import pandas; print(pandas.__version__)"); then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | Python $PYTHON_VERSION | GitHub | Fail | Install_Success_But_Test_Fails"
+    exit 2
 else
-     echo "------------------$PACKAGE_NAME::Test_Fail-------------------------"
-     echo "$PACKAGE_VERSION $PACKAGE_NAME"
-     echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Fail |  Test_Fail"
-     exit 2
+   echo "------------------$PACKAGE_NAME:Install_and_test_success---------------------------"
+   echo "$PACKAGE_URL $PACKAGE_NAME"
+   echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | Python $PYTHON_VERSION | GitHub | Pass | Install_and_Test_Success"
+   exit 0
 fi
