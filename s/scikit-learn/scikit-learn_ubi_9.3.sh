01840c893eded32b368aa1102eee96a1d404cc1f
Harithanagothu2 <haritha.nagothu2@ibm.com>
2025-02-26 13:20:07 +0530

    Update scikit-learn_ubi_9.3.sh

--- s/scikit-learn/scikit-learn_ubi_9.3.sh
+++ s/scikit-learn/scikit-learn_ubi_9.3.sh
@@ -22,11 +22,10 @@ PACKAGE_NAME=scikit-learn
 PACKAGE_VERSION=${1:-1.5.2}
 PACKAGE_URL=https://github.com/scikit-learn/scikit-learn.git
 
-yum install -y python python-pip python-devel openblas-devel gcc-toolset-13 git
+yum install -y python python-pip python-devel openblas-devel gcc gcc-c++ gcc-toolset-13 git
 
 export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
 
-OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
 # clone source repository
 git clone $PACKAGE_URL
@@ -36,31 +35,28 @@ git submodule update --init
 
 # install scikit-learn dependencies and build dependencies
 pip install wheel numpy scipy cython meson-python
-pip install ninja pytest>=7.1.2  joblib threadpoolctl
+pip install ninja pytest>=7.1.2  joblib threadpoolctl patchelf>=0.11.0 setuptools
 
 
-# build the project with pip and install
-if ! pip install . --no-build-isolation; then
-        echo "------------------$PACKAGE_NAME:build_fails---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
-        exit 1
-else
-        echo "------------------$PACKAGE_NAME:build_success-------------------------"
-        echo "$PACKAGE_VERSION $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Build_Success"
+#install
+if ! pip install . --no-build-isolation  ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
 fi
 
 # test using pytest - set below flag as suggested in GitHub forums to resolve ImportPathMismatchError
 export PY_IGNORE_IMPORTMISMATCH=1
-if ! pytest sklearn/tests/test_random_projection.py ; then
-        echo "------------------$PACKAGE_NAME:test_fails---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME "
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | Github | Fail |  Test_Fails"
-        exit 2
+
+if ! pytest sklearn/tests/test_random_projection.py; then
+    echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
 else
-        echo "------------------$PACKAGE_NAME:test_success-------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME "
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | Github | Pass |  Test_Success"
-	exit 0
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
 fi
