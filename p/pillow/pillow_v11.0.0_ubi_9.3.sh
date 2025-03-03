b0d1c2b192f335e519cf73f5730dafc945a26df9
Harithanagothu2 <haritha.nagothu2@ibm.com>
2025-03-03 13:15:53 +0530

    Update pillow_v11.0.0_ubi_9.3.sh

--- p/pillow/pillow_v11.0.0_ubi_9.3.sh
+++ p/pillow/pillow_v11.0.0_ubi_9.3.sh
@@ -8,8 +8,7 @@
 # Language      : Python, C
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Puneet Sharma <Puneet.Sharma21@ibm.com>
-#
+# Maintainer    : Haritha Nagothu <haritha.nagothu2@ibm.com>
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
 #             It may not work as expected with newer versions of the
@@ -19,62 +18,44 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=pillow
+PACKAGE_DIR=Pillow
 PACKAGE_VERSION=${1:-11.0.0}
 PACKAGE_URL=https://github.com/python-pillow/Pillow/
-PYTHON_VER=${2:-"3.11"}
-
-OS_NAME=$(grep '^PRETTY' /etc/os-release | awk -F '=' '{print $2}')
 
 # install core dependencies
-yum install -y python${PYTHON_VER} python${PYTHON_VER}-pip python${PYTHON_VER}-devel gcc git
+yum install -y python3 python3-pip python3-devel gcc git gcc gcc-c++ gcc-toolset-13 
+export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
+
 
 # install pillow's minimum dependencies
-yum install -y zlib zlib-devel libjpeg-turbo libjpeg-turbo-devel
+yum install -y zlib zlib-devel libjpeg-turbo libjpeg-turbo-devel openblas
 
 # install build tools for wheel generation
-python${PYTHON_VER} -m pip install --upgrade pip setuptools wheel pytest
+pip install --upgrade pip setuptools wheel pytest numpy
 
 # clone source repository
-git clone $PACKAGE_URL $PACKAGE_NAME
-cd $PACKAGE_NAME
+git clone $PACKAGE_URL
+cd $PACKAGE_DIR
 git checkout $PACKAGE_VERSION
 git submodule update --init
 
-# check if setup.py file is present
-if [ -f "setup.py" ]; then
-    echo "setup.py file exists"
-
-    # Build the wheel file
-    if ! python${PYTHON_VER} setup.py bdist_wheel ; then
-        echo "------------------$PACKAGE_NAME:Build_wheel_fails-------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_wheel_Fails"
-        exit 1
-    fi
-
-    # Install the package from the wheel
-    WHEEL_FILE=$(ls dist/*.whl)
-    if ! python${PYTHON_VER} -m pip install $WHEEL_FILE ; then
-        echo "------------------$PACKAGE_NAME:Install_wheel_fails-------------------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_wheel_Fails"
-        exit 1
-    fi
-
-else
-    echo "setup.py not present"
+if ! pip install . ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
-# Run tests to verify installation
-if ! pytest Tests/test_lib_image.py Tests/test_core_resources.py Tests/test_file_jpeg.py Tests/check_png_dos.py Tests/test_file_apng.py Tests/test_file_png.py ; then
-    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-    exit 2
+#test
+if ! pytest; then
+        echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+        exit 2
 else
-    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-    exit 0
+        echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+        exit 0
+
 fi
