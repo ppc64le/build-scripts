e926112d74b848f18539b49d0400812431d2721e
kiranNukal <sai.kiran.nukala@ibm.com>
2025-03-05 11:21:52 +0530

    Update onnxconvertercommon_ubi__9.3.sh

--- o/onnxconverter-common/onnxconvertercommon_ubi__9.3.sh
+++ o/onnxconverter-common/onnxconvertercommon_ubi__9.3.sh
@@ -25,29 +25,60 @@ PACKAGE_URL=https://github.com/microsoft/onnxconverter-common
 PACKAGE_DIR=onnxconverter-common
 
 echo "Installing dependencies..."
-yum install -y git make libtool gcc-toolset-13  gcc-c++ libevent-devel zlib-devel openssl-devel python python-devel cmake gcc-gfortran openblas openblas-devel
+yum install -y git make libtool gcc-toolset-13 gcc-c++ libevent-devel zlib-devel openssl-devel python python-devel cmake gcc-gfortran openblas openblas-devel patch
 
-echo "Downloading and installing protobuf-c"
+# Download and install protobuf-c
+echo "Downloading and installing protobuf-c..."
 git clone https://github.com/protocolbuffers/protobuf.git
 cd protobuf
 git checkout v3.20.2
 git submodule update --init --recursive
 mkdir build_source && cd build_source
 cmake ../cmake -Dprotobuf_BUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_SYSCONFDIR=/etc -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
-echo "Building..."
+echo "Building protobuf-c..."
 make -j$(nproc)
-echo "Installing..."
+echo "Installing protobuf-c..."
 make install
 cd ../..
 
-echo "Cloning and installing..."
+# Clone and install onnxruntime
+echo "Cloning and installing onnxruntime..."
+git clone https://github.com/microsoft/onnxruntime
+cd onnxruntime
+git checkout d1fb58b0f2be7a8541bfa73f8cbb6b9eba05fb6b
+
+# Build the onnxruntime package and create the wheel
+echo "Building onnxruntime..."
+./build.sh \
+  --cmake_extra_defines "onnxruntime_PREFER_SYSTEM_LIB=ON" \
+  --cmake_generator Ninja \
+  --build_shared_lib \
+  --config Release \
+  --update \
+  --build \
+  --skip_submodule_sync \
+  --allow_running_as_root \
+  --compile_no_warning_as_error \
+  --build_wheel
+
+# Install the built onnxruntime wheel
+echo "Installing onnxruntime wheel..."
+cp ./build/Linux/Release/dist/* ./
+pip3 install ./*.whl
+
+# Clean up the onnxruntime repository
+cd ..
+rm -rf onnxruntime
+
+# Clone and install onnxconverter-common
+echo "Cloning and installing onnxconverter-common..."
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 git submodule update --init --recursive
 
 pip install cmake setuptools ninja wheel pytest
-pip install numpy==1.24.3 onnx==1.17.0 nbval pythran scipy cython
+pip install numpy==1.24.3 onnx==1.17.0 nbval pythran scipy cython onnxmltools
 
 sed -i 's/\bprotobuf==[^ ]*\b/protobuf==4.25.3/g' pyproject.toml
 sed -i 's/\"onnx\"/\"onnx==1.17.0\"/' pyproject.toml
@@ -60,16 +91,11 @@ if ! python3 setup.py install; then
         echo "$PACKAGE_VERSION $PACKAGE_NAME"
         echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  wheel_built_fails"
         exit 1
-else
-        echo "------------------$PACKAGE_NAME:wheel_built_success-------------------------"
-        echo "$PACKAGE_VERSION $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Wheel_built_success"
-        exit 0
 fi
 
 echo "Running tests for $PACKAGE_NAME..."
-# Test the package
-if ! python3 -c "import onnxconverter_common"; then
+# Test the onnxconverter-common package
+if ! pytest --ignore=tests/test_auto_mixed_precision.py; then
     echo "------------------$PACKAGE_NAME:build_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Build_success_but_test_Fails"
