98703658abb79e60d0cb467ceeba424e771b035b
aastha-sharma2 <aastha.sharma4@ibm.com>
2025-03-12 13:41:10 +0530

    Update llvmlite_v0.44.0rc1_ubi_9_3.sh

--- l/llvmlite/llvmlite_v0.44.0rc1_ubi_9_3.sh
+++ l/llvmlite/llvmlite_v0.44.0rc1_ubi_9_3.sh
@@ -70,8 +70,9 @@ if ! (pip install .) ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
-    exit 1
     deactivate
+    exit 1
+    
 fi
 
 # Run tests
@@ -79,12 +80,13 @@ if ! python -c "import llvmlite; import llvmlite.binding; import llvmlite.ir; im
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-    exit 2
     deactivate
+    exit 2
+    
 else
     echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-    exit 0
     deactivate
+    exit 0    
 fi
