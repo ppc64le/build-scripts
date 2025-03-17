b4a41f008a4d36738a3f8804b4578fe746ba5b91
aastha-sharma2 <aastha.sharma4@ibm.com>
2025-03-17 18:39:38 +0530

    Update openmpi_ubi_9.3.sh

--- o/openmpi/openmpi_ubi_9.3.sh
+++ o/openmpi/openmpi_ubi_9.3.sh
@@ -66,7 +66,7 @@ make install
 
 # Create the necessary directory structure and copy OpenMPI files
 mkdir -p local/openmpi
-cp -r $PACKAGE_DIR/prefix/* local/openmpi/
+cp -r prefix/* local/openmpi/
 
 # Set path for mpi/ompi
 export PATH=$PWD/prefix/bin:$PATH
