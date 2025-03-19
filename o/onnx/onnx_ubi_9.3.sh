2204c67cdb845f7d90f45406977f19d6fdeae930
kiranNukal <sai.kiran.nukala@ibm.com>
2025-03-19 11:49:29 +0530

    Update onnx_ubi_9.3.sh

--- o/onnx/onnx_ubi_9.3.sh
+++ o/onnx/onnx_ubi_9.3.sh
@@ -28,7 +28,6 @@ echo "Installing dependencies..."
 yum install -y git make libtool  gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc-gfortran libevent-devel zlib-devel openssl-devel clang python3-devel python3.12 python3.12-devel python3.12-pip cmake openblas openblas-devel
 export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
 export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
-source /opt/rh/gcc-toolset-13/enable
 
 # Set library paths and package configuration paths
 export LD_LIBRARY_PATH="/usr/lib64:/usr/local/lib:$LD_LIBRARY_PATH"
@@ -39,17 +38,41 @@ echo "Cloning and installing..."
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
+
+export ONNX_ML=1
+export ONNX_PREFIX=$(pwd)/../onnx-prefix
+export CONDA_PREFIX="$PREFIX"
+AR=$gcc_home/bin/ar
+LD=$gcc_home/bin/ld
+NM=$gcc_home/bin/nm
+OBJCOPY=$gcc_home/bin/objcopy
+OBJDUMP=$gcc_home/bin/objdump
+RANLIB=$gcc_home/bin/ranlib
+STRIP=$gcc_home/bin/strip
+export CMAKE_ARGS=""
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$ONNX_PREFIX"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_AR=${AR}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_LINKER=${LD}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_NM=${NM}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_OBJCOPY=${OBJCOPY}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_OBJDUMP=${OBJDUMP}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_RANLIB=${RANLIB}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_STRIP=${STRIP}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CXX_STANDARD=17"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH"
+
 git submodule update --init --recursive
+#Adding this source due to - (Unable to detect linker for compiler `cc -Wl,--version`)
+source /opt/rh/gcc-toolset-13/enable
 echo "installing cython.."
 pip3.12 install cython
-echo "installing scipy.."
-pip3.12 install scipy==1.15.2
+pip3.12 install numpy==2.0.2
 echo "installing parameterized.."
 pip3.12 install parameterized
 echo "installing python dependencies...."
 pip3.12 install pytest nbval pythran
 pip3.12 install protobuf==4.25.3
-pip3.12 install numpy==2.0.2
+pip3.12 install scipy==1.15.2
 
 echo "installing..."
 if ! pip3.12 install -e . ; then
