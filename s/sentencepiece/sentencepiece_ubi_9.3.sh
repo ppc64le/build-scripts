42cce61543d1e6a2a23032cb094265291a2fd94d
puneetsharma21 <puneet.sharma21@ibm.com>
2025-03-19 16:30:30 -0400

    updated with gcc13

--- s/sentencepiece/sentencepiece_ubi_9.3.sh
+++ s/sentencepiece/sentencepiece_ubi_9.3.sh
@@ -30,11 +30,13 @@ WORKDIR=$(pwd)
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
 
-dnf install -yq git cmake g++ libatomic \
+dnf install -yq git cmake gcc-toolset-13 libatomic \
     python$PYTHON_VERSION-devel \
     python$PYTHON_VERSION-pip \
     python$PYTHON_VERSION-wheel \
     python$PYTHON_VERSION-setuptools
+
+source /opt/rh/gcc-toolset-13/enable
 
 if [ -z $PACKAGE_SOURCE_DIR ]; then
     git clone $PACKAGE_URL -b $PACKAGE_VERSION
