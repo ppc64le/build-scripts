9a0b97a93303839a612851eedc6805a8852083c8
Amit Kumar <amit.kumar282@ibm.com>
2025-03-19 14:44:10 +0530

    Updated xerces-c_ubi_9.3.sh

--- x/xerces/xerces-c_ubi_9.3.sh
+++ x/xerces/xerces-c_ubi_9.3.sh
@@ -1,35 +1,38 @@
+#!/bin/bash -e
 # ----------------------------------------------------------------------------
+#
 # Package  : xerces-c
-# Version  : 3.2.5
+# Version  : v3.2.5
 # Source repo  : https://github.com/apache/xerces-c
-# Tested on  : ubi_9.3
+# Tested on  : UBI 9.3
+# Language      : C++
+# Travis-Check  : true
 # Script License  : Apache License, Version 2 or later
 # Maintainer  : Amit Kumar <amit.kumar282@ibm.com>
 #
-# Disclaimer: This script has been tested in non-root mode on given
+# Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
 #             It may not work as expected with newer versions of the
 #             package and/or distribution. In such case, please
 #             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-#!/bin/bash
-
-# Install dependencies.
-yum update -y
-yum install  git gcc-c++ gcc glibc-devel libtool* autoconf automake make valgrind-devel -y
 
 SCRIPT_PACKAGE_VERSION=v3.2.5
 PACKAGE_NAME=xerces-c
 PACKAGE_VERSION=${1:-${SCRIPT_PACKAGE_VERSION}}
 PACKAGE_URL=https://github.com/apache/xerces-c.git
 
-# Clone and build source.
+#Install dependencies.
+yum update -y
+yum install  git gcc-c++ gcc glibc-devel libtool* autoconf automake make valgrind-devel -y
+
+#Clone and build source.
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-#configure and build
+#Configure and build
 export XERCESCROOT=`pwd`
 ./reconf
 ./configure
@@ -38,3 +41,4 @@ make install
 
 #run the test suite
 make check
+echo "SUCCESS: Build and test success!"
