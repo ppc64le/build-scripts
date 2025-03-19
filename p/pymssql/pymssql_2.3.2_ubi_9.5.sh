436f3fcfbb1dd4599b844889bb27b5f2d04ad936
Shivansh S <shivansh.s1@ibm.com>
2025-03-19 14:08:49 +0530

    updated build script for pymssql

--- p/pymssql/pymssql_2.3.2_ubi_9.5.sh
+++ p/pymssql/pymssql_2.3.2_ubi_9.5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # ----------------------------------------------------------------------------
-# 
+#
 # Package       : pymssql
 # Version       : v2.3.2
 # Source repo   : https://github.com/pymssql/pymssql.git
@@ -8,7 +8,7 @@
 # Language      : Python
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Haritha Nagothu <haritha.nagothu2@ibm.com>
+# Maintainer    : Shivansh Sharma <shivansh.s1@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -27,25 +27,36 @@ PACKAGE_URL=https://github.com/pymssql/pymssql.git
 yum install -y wget
 dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/ppc64le/os/
 dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/BaseOS/ppc64le/os/
-dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/CRB/ppc64le/os/   
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/CRB/ppc64le/os/
 wget http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-Official
 mv RPM-GPG-KEY-CentOS-Official /etc/pki/rpm-gpg/.
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-Official
 dnf install --nodocs -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-yum install -y git gcc gcc-c++ python3-devel freetds-devel openssl-devel krb5-devel
+yum install -y git gcc gcc-c++ python3.12 python3.12-pip python3.12-devel openssl-devel krb5-devel
 
-#clone repository 
+# Clone repository
 git clone $PACKAGE_URL
-cd  $PACKAGE_NAME
+cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-pip install -r dev/requirements-dev.txt
-pip install cython
-pip install setuptools_scm>=5.0
-pip install wheel>=0.36.2
+# Install Python dependencies
+python3.12 -m pip install -r dev/requirements-dev.txt
+python3.12 -m pip install cython
+python3.12 -m pip install setuptools_scm>=5.0
+python3.12 -m pip install wheel>=0.36.2
+
+python3.12 dev/build.py \
+            --ws-dir=./freetds \
+            --dist-dir=./dist \
+            --with-openssl=yes \
+            --enable-krb5 \
+            --sdist \
+            --static-freetds
+python3.12 -m pip install pymssql --no-index -f dist
+python3.12 setup.py bdist_wheel
 
 #install
-if ! ( python3 setup.py install) ; then
+if ! ( python3.12 -c "import pymssql; print(pymssql.version_info())" ) ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
@@ -53,7 +64,7 @@ if ! ( python3 setup.py install) ; then
 fi
 
 #test
-if ! pytest; then
+if ! pytest -sv --durations=0; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
@@ -62,5 +73,7 @@ else
     echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    # copying wheel to the root location 
+    cp ./dist/*.whl /
     exit 0
 fi
