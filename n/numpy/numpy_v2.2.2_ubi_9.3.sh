f51b269591f40f6455ffc378907f11077a278cbb
kiranNukal <sai.kiran.nukala@ibm.com>
2025-03-20 18:57:09 +0530

    Update numpy_v2.2.2_ubi_9.3.sh

--- n/numpy/numpy_v2.2.2_ubi_9.3.sh
+++ n/numpy/numpy_v2.2.2_ubi_9.3.sh
@@ -2,13 +2,13 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : numpy
-# Version       : v2.2.2
+# Version       : v2.0.2
 # Source repo   : https://github.com/numpy/numpy
 # Tested on     : UBI:9.3
 # Language      : Python
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Shubham Garud <Shubham.Garud@ibm.com>
+# Maintainer    : Sai Kiran Nukala <sai.kiran.nukala@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -18,28 +18,84 @@
 #
 # ----------------------------------------------------------------------------
 PACKAGE_NAME=numpy
-PACKAGE_VERSION=${1:-v2.2.2}
+PACKAGE_VERSION=${1:-v2.0.2}
 PACKAGE_URL=https://github.com/numpy/numpy.git
 PACKAGE_DIR=$PACKAGE_NAME
-yum install -y python3.12  python3.12-devel python3.12-pip git gcc-toolset-13 gcc-gfortran make g++ openblas
+yum install -y python3.12  python3.12-devel python3.12-pip git gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc-gfortran make
 export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
-
 ln -sf /usr/bin/python3.12 /usr/bin/python3
-
 python3 -m pip install --upgrade pip
 python3 -m pip install tox Cython pytest hypothesis wheel meson ninja
-
 export SITE_PACKAGE_PATH=/usr/local/lib/python3.12/site-packages
-export OpenBLAS_HOME="/usr/include/openblas"
 
+#clone and install openblas from source
+git clone https://github.com/OpenMathLib/OpenBLAS
+cd OpenBLAS
+git checkout v0.3.29
+git submodule update --init
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/python-ecosystem/o/openblas/pyproject.toml
+sed -i "s/{PACKAGE_VERSION}/v0.3.29/g" pyproject.toml
+PREFIX=local/openblas
+# Set build options
+declare -a build_opts
+# Fix ctest not automatically discovering tests
+LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,--gc-sections//g")
+export CF="${CFLAGS} -Wno-unused-parameter -Wno-old-style-declaration"
+unset CFLAGS
+export USE_OPENMP=1
+build_opts+=(USE_OPENMP=${USE_OPENMP})
+export PREFIX=${PREFIX}
+# Handle Fortran flags
+if [ ! -z "$FFLAGS" ]; then
+    export FFLAGS="${FFLAGS/-fopenmp/ }"
+    export FFLAGS="${FFLAGS} -frecursive"
+    export LAPACK_FFLAGS="${FFLAGS}"
+fi
+export PLATFORM=$(uname -m)
+build_opts+=(BINARY="64")
+build_opts+=(DYNAMIC_ARCH=1)
+build_opts+=(TARGET="POWER9")
+BUILD_BFLOAT16=1
+# Placeholder for future builds that may include ILP64 variants.
+build_opts+=(INTERFACE64=0)
+build_opts+=(SYMBOLSUFFIX="")
+# Build LAPACK
+build_opts+=(NO_LAPACK=0)
+# Enable threading and set the number of threads
+build_opts+=(USE_THREAD=1)
+build_opts+=(NUM_THREADS=8)
+# Disable CPU/memory affinity handling to avoid problems with NumPy and R
+build_opts+=(NO_AFFINITY=1)
+# Build OpenBLAS
+make -j8 ${build_opts[@]} CFLAGS="${CF}" FFLAGS="${FFLAGS}" prefix=${PREFIX}
+# Install OpenBLAS
+CFLAGS="${CF}" FFLAGS="${FFLAGS}" make install PREFIX="${PREFIX}" ${build_opts[@]}
+export OpenBLAS_HOME="$SITE_PACKAGE_PATH/openblas/lib"
 UNAME_M=$(uname -m)
-
 
 #clone package
 git clone $PACKAGE_URL
 cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 git submodule update --init
+
+EXTRA_OPTS=""
+export GCC_HOME=/opt/rh/gcc-toolset-13/root/usr
+echo $GCC_HOME
+
+export PATH=$GCC_HOME/bin:$PATH
+export CC=$GCC_HOME/bin/gcc
+export CXX=$GCC_HOME/bin/g++
+export GCC=$CC
+export GXX=$CXX
+export AR=${GCC_HOME}/bin/ar
+export LD=${GCC_HOME}/bin/ld
+export NM=${GCC_HOME}/bin/nm
+export OBJCOPY=${GCC_HOME}/bin/objcopy
+export OBJDUMP=${GCC_HOME}/bin/objdump
+export RANLIB=${GCC_HOME}/bin/ranlib
+export STRIP=${GCC_HOME}/bin/strip
+export READELF=${GCC_HOME}/bin/readelf
 
 case "$UNAME_M" in
     ppc64*)
