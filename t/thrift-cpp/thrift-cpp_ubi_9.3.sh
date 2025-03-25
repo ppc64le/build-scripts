3bc0a4668757819ac57c570c423c81ecd8d119b6
Harithanagothu2 <haritha.nagothu2@ibm.com>
2025-03-25 17:40:35 +0530

    Update thrift-cpp_ubi_9.3.sh

--- t/thrift-cpp/thrift-cpp_ubi_9.3.sh
+++ t/thrift-cpp/thrift-cpp_ubi_9.3.sh
@@ -151,17 +151,23 @@ wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/python-e
 sed -i s/{PACKAGE_VERSION}/$PACKAGE_VERSION/g pyproject.toml
 
 
-#test
-make -k check
-
-if ! pip install . ; then
+#install
+if ! (pip install .) ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
-else
-    echo "------------------$PACKAGE_NAME:Install_success-------------------------"
+fi
+
+#test
+if ! make -k check; then
+    echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_Success"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
     exit 0
 fi
