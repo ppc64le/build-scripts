1de4b0dc6aa6f890c0817e5547999b04795554ab
kiranNukal <sai.kiran.nukala@ibm.com>
2025-03-26 17:17:18 +0530

    Update utf8proc_ubi_9.3.sh

--- u/utf8proc/utf8proc_ubi_9.3.sh
+++ u/utf8proc/utf8proc_ubi_9.3.sh
@@ -28,38 +28,53 @@ PACKAGE_URL=https://github.com/JuliaStrings/utf8proc.git
 PACKAGE_DIR=utf8proc
 WORK_DIR=$(pwd)
 
-# Install dependencies and tools.
+# Install dependencies and tools
 yum install -y wget git gcc-toolset-13-gcc cmake python3-devel python3-pip
 export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
 export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
+
 # Download the source package
 echo "Cloning and installing..."
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
+
+# Initialize git submodules
+git submodule update --init
+
+# Checkout the specified version
 git checkout $PACKAGE_VERSION
-wget https://raw.githubusercontent.com/ppc64le/build-scripts/d958955bd1c9fa9633a2a2b194d4b2fd0ecfe5a6/u/utf8proc/pyproject.toml
-sed -i s/{PACKAGE_VERSION}/$PACKAGE_VERSION/g pyproject.toml
+
+# Setup prefix for installation
 mkdir prefix
 export PREFIX=$(pwd)/prefix
 
+# Create build directory
 mkdir build
 cd build
+
+# Run cmake to configure the build
 cmake -G "Unix Makefiles" \
   -DCMAKE_BUILD_TYPE="Release" \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_POSITION_INDEPENDENT_CODE=1 \
   -DBUILD_SHARED_LIBS=1 \
   ..
+# Build and install
+cmake --build .
 cmake --build . --target install
-cd ..
-mkdir -p local/$PACKAGE
-cp -r prefix/* local/$PACKAGE/
-echo "installing..."
+cd $WORK_DIR
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/python-ecosystem/u/utf8proc/pyproject.toml
+sed -i s/{PACKAGE_VERSION}/$PACKAGE_VERSION/g pyproject.toml
+mkdir -p local/$PACKAGE_NAME
+# Copy the installation files to the local directory
+cp -r $PACKAGE_NAME/prefix/* local/$PACKAGE_NAME/
+echo "Installing with pip..."
 if ! pip install . ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
+
 echo "Build and installation completed successfully."
-echo "There are no test cases available. skipping the test cases"
+echo "There are no test cases available. Skipping the test cases."
