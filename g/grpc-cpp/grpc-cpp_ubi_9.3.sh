83fb6accec7f9f5dd1d228a0eb6ebb4c4a55b055
Harithanagothu2 <haritha.nagothu2@ibm.com>
2025-03-26 16:57:10 +0530

    Create grpc-cpp_ubi_9.3.sh

---
+++ g/grpc-cpp/grpc-cpp_ubi_9.3.sh
@@ -0,0 +1,225 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : grpc-cpp 
+# Version       : v1.68.0
+# Source repo   : https://github.com/grpc/grpc
+# Tested on     : UBI:9.3
+# Language      : Python, C++
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Haritha Nagothu <haritha.nagothu2@ibm.com>
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+yum install -y wget
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/ppc64le/os/
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/BaseOS/ppc64le/os/
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/CRB/ppc64le/os/
+dnf install --nodocs -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+wget http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-Official
+mv RPM-GPG-KEY-CentOS-Official /etc/pki/rpm-gpg/.
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-Official
+
+yum install -y make libtool cmake git wget xz zlib-devel openssl-devel bzip2-devel libffi-devel libevent-devel patch python python-devel ninja-build gcc-toolset-13  pkg-config c-ares-devel
+
+export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
+
+SCRIPT_DIR=$(pwd)
+pip install --upgrade cmake pip setuptools wheel ninja packaging pytest
+
+#Building abseil-cpp
+ABSEIL_VERSION=20240116.2
+ABSEIL_URL="https://github.com/abseil/abseil-cpp"
+mkdir $SCRIPT_DIR/abseil-prefix
+PREFIX=$SCRIPT_DIR/abseil-prefix
+
+git clone $ABSEIL_URL -b $ABSEIL_VERSION
+cd abseil-cpp
+
+SOURCE_DIR=$(pwd)
+
+mkdir -p $SOURCE_DIR/local/abseilcpp
+ABSEIL_CPP=$SOURCE_DIR/local/abseilcpp
+
+echo "abseil-cpp build starts"
+mkdir build
+cd build
+
+cmake -G Ninja \
+    ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DBUILD_SHARED_LIBS=ON \
+    -DABSL_PROPAGATE_CXX_STD=ON \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+   ..
+cmake --build .
+cmake --install .
+
+cd $SCRIPT_DIR
+cp -r  $PREFIX/* $ABSEIL_CPP/
+
+echo "------------abseil-cpp installed--------------"
+
+#building libprotobuf
+export C_COMPILER=$(which gcc)
+export CXX_COMPILER=$(which g++)
+
+git clone https://github.com/protocolbuffers/protobuf
+cd protobuf
+git checkout v4.25.3
+
+LIBPROTO_DIR=$(pwd)
+mkdir -p $LIBPROTO_DIR/local/libprotobuf
+LIBPROTO_INSTALL=$LIBPROTO_DIR/local/libprotobuf
+
+git submodule update --init --recursive
+rm -rf ./third_party/googletest | true
+rm -rf ./third_party/abseil-cpp | true
+
+cp -r $SCRIPT_DIR/abseil-cpp ./third_party/
+
+mkdir build
+cd build
+
+cmake -G "Ninja" \
+   ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_C_COMPILER=$C_COMPILER \
+    -DCMAKE_CXX_COMPILER=$CXX_COMPILER \
+    -DCMAKE_INSTALL_PREFIX=$LIBPROTO_INSTALL \
+    -Dprotobuf_BUILD_TESTS=OFF \
+    -Dprotobuf_BUILD_LIBUPB=OFF \
+    -Dprotobuf_BUILD_SHARED_LIBS=ON \
+    -Dprotobuf_ABSL_PROVIDER="module" \
+    -DCMAKE_PREFIX_PATH=$ABSEIL_CPP \
+    -Dprotobuf_JSONCPP_PROVIDER="package" \
+    -Dprotobuf_USE_EXTERNAL_GTEST=OFF \
+    ..
+
+cmake --build . --verbose
+cmake --install .
+cd $SCRIPT_DIR 
+
+echo "------------ libprotobuf installed--------------"
+
+#re2 install from sosurce
+git clone http://github.com/google/re2
+cd re2
+git checkout 2022-04-01
+git submodule update --init
+
+mkdir re2-prefix
+export RE2_PREFIX=$(pwd)/re2-prefix
+
+export CPU_COUNT=`nproc`
+
+mkdir build-cmake
+pushd build-cmake
+
+cmake ${CMAKE_ARGS} -GNinja \
+  -DCMAKE_PREFIX_PATH=$RE2_PREFIX \
+  -DCMAKE_INSTALL_PREFIX="${RE2_PREFIX}" \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DENABLE_TESTING=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=ON \
+  ..
+  
+  ninja -v install
+  
+  popd
+
+make -j "${CPU_COUNT}" prefix=${RE2_PREFIX} shared-install
+cd $SCRIPT_DIR 
+
+echo "------------ re2 installed--------------"
+
+PACKAGE_NAME=grpc-cpp
+PACKAGE_DIR=grpc
+PACKAGE_VERSION=${1:-v1.68.0}
+
+PACKAGE_URL=https://github.com/grpc/grpc
+
+git clone $PACKAGE_URL
+cd $PACKAGE_DIR
+git checkout $PACKAGE_VERSION
+
+git submodule update --init
+
+mkdir grpc-prefix
+export GRPC_PREFIX=$(pwd)/grpc-prefix
+
+AR=`which ar`
+RANLIB=`which ranlib`
+
+PROTOC_BIN=$LIBPROTO_INSTALL/bin/protoc
+PROTOBUF_SRC=$LIBPROTO_INSTALL
+
+export CMAKE_PREFIX_PATH="$PREFIX;$RE2_PREFIX;$LIBPROTO_INSTALL"
+
+export LD_LIBRARY_PATH=$PREFIX/lib:${LD_LIBRARY_PATH}
+
+target_platform=$(uname)-$(uname -m)
+
+if [[ "${target_platform}" == osx* ]]; then
+  export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CXX_STANDARD=14"
+else
+  export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CXX_STANDARD=17"
+fi
+
+
+mkdir -p build-cpp
+pushd build-cpp
+cmake ${CMAKE_ARGS} ..  \
+      -GNinja \
+      -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=$GRPC_PREFIX \
+      -DgRPC_CARES_PROVIDER="package" \
+      -DgRPC_GFLAGS_PROVIDER="package" \
+      -DgRPC_PROTOBUF_PROVIDER="package" \
+      -DProtobuf_ROOT=$PROTOBUF_SRC \
+      -DgRPC_SSL_PROVIDER="package" \
+      -DgRPC_ZLIB_PROVIDER="package" \
+      -DgRPC_ABSL_PROVIDER="package" \
+      -DgRPC_RE2_PROVIDER="package" \
+      -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH \
+      -DCMAKE_AR=${AR} \
+      -DCMAKE_RANLIB=${RANLIB} \
+      -DCMAKE_VERBOSE_MAKEFILE=ON \
+      -DProtobuf_PROTOC_EXECUTABLE=$PROTOC_BIN
+
+ninja install -v
+popd
+
+cd $SCRIPT_DIR
+mkdir -p local/grpccpp
+
+cp -r grpc/grpc-prefix/* local/grpccpp/
+
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/python-ecosystem/g/grpc-cpp/pyproject.toml
+sed -i s/{PACKAGE_VERSION}/$PACKAGE_VERSION/g pyproject.toml
+
+if ! pip install . ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+else
+    echo "------------------$PACKAGE_NAME:Install_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_Success"
+    exit 0
+fi
+
+#There are no test cases found, so skiping testing part.
