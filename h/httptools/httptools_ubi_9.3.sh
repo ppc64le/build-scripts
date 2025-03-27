bae2dd0bf30302d682c817ae8aa27c36ca0da0a8
Bhagyashri <Bhagyashri.Gaikwad2@ibm.com>
2025-03-27 10:48:14 +0530

    ODH:- Modified httptool v0.41.1 build script to use gcc13 (#5640)

--- h/httptools/httptools_ubi_9.3.sh
+++ h/httptools/httptools_ubi_9.3.sh
@@ -29,11 +29,13 @@ WORKDIR=$(pwd)
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
-dnf install -y git g++ \
+dnf install -y git gcc-toolset-13 \
     python${PYTHON_VERSION}-devel \
     python${PYTHON_VERSION}-pip \
     python${PYTHON_VERSION}-wheel \
     python${PYTHON_VERSION}-setuptools 
+
+source /opt/rh/gcc-toolset-13/enable
 
 if [ -z $PACKAGE_SOURCE_DIR ]; then
     git clone $PACKAGE_URL -b $PACKAGE_VERSION
