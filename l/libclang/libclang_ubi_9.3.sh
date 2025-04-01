c2e171edc8fca27f826b357658dccdcd13261183
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2025-04-01 06:31:18 +0000

    Currency:Updated build-script,build_info.json for libclang

--- l/libclang/libclang_ubi_9.3.sh
+++ l/libclang/libclang_ubi_9.3.sh
@@ -2,13 +2,13 @@
 # -----------------------------------------------------------------------------
 #
 # Package          : libclang
-# Version          : 14.0.6
+# Version          : llvm-18.1.1
 # Source repo      : https://github.com/sighingnow/libclang.git
 # Tested on        : UBI:9.3
 # Language         : Python
 # Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
-# Maintainer       : Aastha Sharma <aastha.sharma4@ibm.com>
+# Maintainer       : Vinod.K1 <Vinod.K1@ibm.com>
 #
 # Disclaimer       : This script has been tested in root mode on given
 # ==========         platform using the mentioned version of the package.
@@ -20,11 +20,17 @@
 
 # Variables
 PACKAGE_NAME=libclang
-PACKAGE_VERSION=${1:-llvm-14.0.6}
+PACKAGE_VERSION=${1:-llvm-18.1.1}
 PACKAGE_URL=https://github.com/sighingnow/libclang.git
+PACKAGE_DIR=libclang
+CURRENT_DIR="${PWD}"
 
 # Install necessary system dependencies
-yum install -y git gcc gcc-c++ make wget openssl-devel bzip2-devel libffi-devel zlib-devel python3-devel python3-pip cmake clang
+yum install -y git gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc-gfortran make wget llvm-devel clang-devel openssl-devel bzip2-devel libffi-devel zlib-devel python-devel python-pip cmake clang
+
+source /opt/rh/gcc-toolset-13/enable
+export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
+export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
 
 # Clone the repository
 git clone $PACKAGE_URL
@@ -35,7 +41,7 @@ git checkout $PACKAGE_VERSION
 if ! command -v rustc &> /dev/null; then
     echo "Rust not found. Installing Rust..."
     curl https://sh.rustup.rs -sSf | sh -s -- -y
-    source "$HOME/.cargo/env"  
+    source "$HOME/.cargo/env"
 else
     echo "Rust is already installed."
 fi
@@ -51,4 +57,13 @@ if ! pip install . ; then
     exit 1
 fi
 
-# No tests to run for this package 
+# Building wheel with script itself as it needs to locate the libzmq target file
+if ! python3  setup.py bdist_wheel --plat-name manylinux2014_ppc64le --dist-dir="$CURRENT_DIR"; then
+    echo "------------------$PACKAGE_NAME: Wheel Build Failed ---------------------"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME: Wheel Build Success -------------------------"
+    exit 0
+fi
+
+# No tests to run for this package
