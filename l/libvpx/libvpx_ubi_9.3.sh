927ec97ed559cfdf116fc238c0e12dd7f5b63cb9
JeSh30 <131438492+JeSh30@users.noreply.github.com>
2025-04-02 15:00:05 +0530

    Updated libvpx_ubi_9.3.sh

--- l/libvpx/libvpx_ubi_9.3.sh
+++ l/libvpx/libvpx_ubi_9.3.sh
@@ -23,7 +23,7 @@ PACKAGE_VERSION=${1:-v1.13.1}
 PACKAGE_URL=https://github.com/webmproject/libvpx
 
 # install core dependencies
-yum install -y python python-pip python-devel git cmake gcc-toolset-13 wget
+yum install -y git cmake gcc-toolset-13 wget
 export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
 
 # clone source repository
@@ -64,22 +64,7 @@ ${CPU_DETECT}                \
 make -j${CPU_COUNT}
 make install
 
-
-mkdir -p local/libvpx
-cp -r prefix/* local/libvpx/
-
-pip install setuptools
-
-#dDownloading pyproject.toml file
-wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/python-ecosystem/l/libvpx/pyproject.toml
-sed -i s/{PACKAGE_VERSION}/$PACKAGE_VERSION/g pyproject.toml
-
-if ! pip install . ; then
-    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
-    exit 1
-else
+if [ $? == 0 ]; then
     echo "------------------$PACKAGE_NAME:Install_success-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_Success"
