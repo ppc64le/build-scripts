1657a3d528a62326fefd523943ce75d47f285362
Harithanagothu2 <haritha.nagothu2@ibm.com>
2025-04-04 13:51:05 +0530

    Update pyarrow_19.0.0_ubi_9.3.sh

--- p/pyarrow/pyarrow_19.0.0_ubi_9.3.sh
+++ p/pyarrow/pyarrow_19.0.0_ubi_9.3.sh
@@ -17,16 +17,17 @@
 #
 # ----------------------------------------------------------------------------
 
-
-yum install -y python python-pip python-devel wget git make  python-devel  openssl-devel cmake zlib-devel libjpeg-devel gcc-toolset-13 cmake libevent libtool pkg-config  brotli-devel.ppc64le bzip2-devel lz4-devel 
-
-export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
+set -e
 
 PACKAGE_NAME=pyarrow
 PACKAGE_DIR=arrow
 PACKAGE_VERSION=${1:-apache-arrow-19.0.0}
 PACKAGE_URL=https://github.com/apache/arrow
 version=19.0.0
+
+yum install -y python python-pip python-devel wget git make  python-devel xz-devel openssl-devel cmake zlib-devel libjpeg-devel gcc-toolset-13 cmake libevent libtool pkg-config  brotli-devel.ppc64le bzip2-devel lz4-devel 
+
+export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
 
 SCRIPT_DIR=$(pwd)
 pip install ninja setuptools
@@ -50,7 +51,7 @@ cd bison-3.8.2
 make -j$(nproc)
 make install
 cd $SCRIPT_DIR
- 
+
 echo "------------ gflags installing-------------------"
 git clone https://github.com/gflags/gflags.git
 cd gflags
@@ -92,14 +93,22 @@ echo "-----------------snappy installing----------------"
 git clone --recurse-submodules https://github.com/google/snappy.git
 cd snappy
 git submodule update --init --recursive
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
+git clone 1.2.2
+
+mkdir -p local/snappy
+export SNAPPY_PREFIX=$(pwd)/local/snappy
+mkdir build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=$SNAPPY_PREFIX \
+      -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      ..
 make -j$(nproc)
 make install
-cd $SCRIPT_DIR
-export SNAPPY_HOME=/usr/local
-export CMAKE_PREFIX_PATH=$SNAPPY_HOME
-export LD_LIBRARY_PATH=$SNAPPY_HOME/lib64:$LD_LIBRARY_PATH
+cd ..
+cd $SCRIPT_DIR 
+
 
 echo "------------libzstd installing-------------------------"
 git clone https://github.com/facebook/zstd.git
@@ -140,7 +149,7 @@ cmake ${CMAKE_ARGS} -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=ON \
   ..
-  
+
   ninja -v install
   popd
 make -j "${CPU_COUNT}" prefix=${RE2_PREFIX} shared-install
@@ -174,6 +183,139 @@ cmake --build .
 cmake --build . --target install
 cd $SCRIPT_DIR 
 
+echo "------------ abseil_cpp cloning-------------------"
+
+ABSEIL_VERSION=20240116.2
+ABSEIL_URL="https://github.com/abseil/abseil-cpp"
+
+
+git clone $ABSEIL_URL -b $ABSEIL_VERSION
+
+
+echo "------------ libprotobuf installing-------------------"
+
+export C_COMPILER=$(which gcc)
+export CXX_COMPILER=$(which g++)
+
+#Build libprotobuf
+git clone https://github.com/protocolbuffers/protobuf
+cd protobuf
+git checkout v4.25.3
+
+LIBPROTO_DIR=$(pwd)
+mkdir -p $LIBPROTO_DIR/local/libprotobuf
+LIBPROTO_INSTALL=$LIBPROTO_DIR/local/libprotobuf
+
+git submodule update --init --recursive
+rm -rf ./third_party/googletest | true
+rm -rf ./third_party/abseil-cpp | true
+
+cp -r $SCRIPT_DIR/abseil-cpp ./third_party/
+
+mkdir build
+cd build
+
+cmake -G "Ninja" \
+   ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_C_COMPILER=$C_COMPILER \
+    -DCMAKE_CXX_COMPILER=$CXX_COMPILER \
+    -DCMAKE_INSTALL_PREFIX=$LIBPROTO_INSTALL \
+    -Dprotobuf_BUILD_TESTS=OFF \
+    -Dprotobuf_BUILD_LIBUPB=OFF \
+    -Dprotobuf_BUILD_SHARED_LIBS=ON \
+    -Dprotobuf_ABSL_PROVIDER="module" \
+    -Dprotobuf_JSONCPP_PROVIDER="package" \
+    -Dprotobuf_USE_EXTERNAL_GTEST=OFF \
+    ..
+
+cmake --build . --verbose
+cmake --install .
+cd $SCRIPT_DIR 
+
+echo "------------ orc installing-------------------"
+
+git clone https://github.com/apache/orc
+cd orc
+git checkout v2.0.3
+
+
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/python-ecosystem/o/orc/orc.patch
+git apply orc.patch
+
+mkdir orc_prefix
+export ORC_PREFIX=$(pwd)/orc_prefix
+
+mkdir -p build
+cd build
+
+export PROTOBUF_PREFIX=$LIBPROTO_INSTALL
+export CMAKE_PREFIX_PATH=$LIBPROTO_INSTALL
+export LD_LIBRARY_PATH=$LIBPROTO_INSTALL/lib64
+
+export CC=$(which gcc)
+export CXX=$(which g++)
+export GCC=$CC
+export GXX=$CXX
+
+export HOST=$(uname)-$(uname -m)
+
+
+
+export HOST=$(uname)-$(uname -m)
+
+CPPFLAGS="${CPPFLAGS} -Wl,-rpath,$VIRTUAL_ENV_PATH/**/lib"
+
+
+declare -a _CMAKE_EXTRA_CONFIG
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+    _CMAKE_EXTRA_CONFIG+=(-DHAS_PRE_1970_EXITCODE=0)
+    _CMAKE_EXTRA_CONFIG+=(-DHAS_PRE_1970_EXITCODE__TRYRUN_OUTPUT=)
+    _CMAKE_EXTRA_CONFIG+=(-DHAS_POST_2038_EXITCODE=0)
+    _CMAKE_EXTRA_CONFIG+=(-DHAS_POST_2038_EXITCODE__TRYRUN_OUTPUT=)
+fi
+if [[ ${HOST} =~ .*darwin.* ]]; then
+    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_AR=${AR})
+    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_RANLIB=${RANLIB})
+    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_LINKER=${LD})
+fi
+if [[ ${HOST} =~ .*Linux.* ]]; then
+    CXXFLAGS="${CXXFLAGS//-std=c++17/-std=c++11}"
+    # I hate you so much CMake.
+    LIBPTHREAD=$(find ${PREFIX} -name "libpthread.so")
+    _CMAKE_EXTRA_CONFIG+=(-DPTHREAD_LIBRARY=${LIBPTHREAD})
+fi
+
+CPPFLAGS="${CPPFLAGS} -Wl,-rpath,$VIRTUAL_ENV_PATH/**/lib"
+
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_PREFIX_PATH=$ORC_PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_JAVA=False \
+    -DLZ4_HOME=/usr \
+    -DZLIB_HOME=/usr \
+    -DZSTD_HOME=/usr/local \
+    -DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
+    -DProtobuf_ROOT=$PROTOBUF_PREFIX \
+    -DPROTOBUF_HOME=$PROTOBUF_PREFIX \
+    -DPROTOBUF_EXECUTABLE=$PROTOBUF_PREFIX/bin/protoc \
+    -DSNAPPY_HOME=$SNAPPY_PREFIX \
+    -DBUILD_LIBHDFSPP=NO \
+    -DBUILD_CPP_TESTS=OFF \
+    -DCMAKE_INSTALL_PREFIX=$ORC_PREFIX \
+    -DCMAKE_C_COMPILER=$(type -p ${CC})     \
+    -DCMAKE_CXX_COMPILER=$(type -p ${CXX})  \
+    -DCMAKE_C_FLAGS="$CFLAGS"  \
+    -DCMAKE_CXX_FLAGS="$CXXFLAGS -Wno-unused-parameter" \
+    "${_CMAKE_EXTRA_CONFIG[@]}" \
+    -GNinja ..
+
+ninja
+ninja install
+
+cd $SCRIPT_DIR
 
 
 echo "-----------------boost_cpp installing-----------------------"
@@ -194,7 +336,7 @@ export CXX=$(which g++)
 export target_platform=$(uname)-$(uname -m)
 CXXFLAGS="${CXXFLAGS} -fPIC"
 TOOLSET=gcc
- 
+
  # http://www.boost.org/build/doc/html/bbv2/tasks/crosscompile.html
 cat <<EOF > tools/build/example/site-config.jam
 using ${TOOLSET} : : ${CXX} ;
@@ -213,7 +355,7 @@ CFLAGS="$(echo ${CFLAGS} | sed 's/ -march=[^ ]*//g' | sed 's/ -mcpu=[^ ]*//g' |s
     ARCHITECTURE=power
 	ABI="sysv"
 	 BINARY_FORMAT="elf"
-	 
+
 	 export CPU_COUNT=$(nproc)
 
 ./b2 -q \
@@ -284,84 +426,6 @@ make install
 cd $SCRIPT_DIR 
 
 
-echo "------------ abseil_cpp installing-------------------"
-
-ABSEIL_VERSION=20240116.2
-ABSEIL_URL="https://github.com/abseil/abseil-cpp"
-mkdir $SCRIPT_DIR/abseil-prefix
-
-ABSEIL_PREFIX=$SCRIPT_DIR/abseil-prefix
-
-git clone $ABSEIL_URL -b $ABSEIL_VERSION
-cd abseil-cpp
-
-SOURCE_DIR=$(pwd)
-
-mkdir -p $SOURCE_DIR/local/abseilcpp
-ABSEIL_CPP=$SOURCE_DIR/local/abseilcpp
-
-echo "abseil-cpp build starts"
-mkdir build
-cd build
-
-cmake -G Ninja \
-    ${CMAKE_ARGS} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_STANDARD=17 \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_INSTALL_PREFIX=${ABSEIL_PREFIX} \
-    -DBUILD_SHARED_LIBS=ON \
-    -DABSL_PROPAGATE_CXX_STD=ON \
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-   ..
-cmake --build .
-cmake --install .
-
-cd $SCRIPT_DIR
-cp -r  $ABSEIL_PREFIX/* $ABSEIL_CPP/
-
-echo "------------ libprotobuf installing-------------------"
-
-export C_COMPILER=$(which gcc)
-export CXX_COMPILER=$(which g++)
-
-#Build libprotobuf
-git clone https://github.com/protocolbuffers/protobuf
-cd protobuf
-git checkout v4.25.3
-
-LIBPROTO_DIR=$(pwd)
-mkdir -p $LIBPROTO_DIR/local/libprotobuf
-LIBPROTO_INSTALL=$LIBPROTO_DIR/local/libprotobuf
-
-git submodule update --init --recursive
-rm -rf ./third_party/googletest | true
-rm -rf ./third_party/abseil-cpp | true
-
-cp -r $SCRIPT_DIR/abseil-cpp ./third_party/
-
-mkdir build
-cd build
-
-cmake -G "Ninja" \
-   ${CMAKE_ARGS} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_STANDARD=17 \
-    -DCMAKE_C_COMPILER=$C_COMPILER \
-    -DCMAKE_CXX_COMPILER=$CXX_COMPILER \
-    -DCMAKE_INSTALL_PREFIX=$LIBPROTO_INSTALL \
-    -Dprotobuf_BUILD_TESTS=OFF \
-    -Dprotobuf_BUILD_LIBUPB=OFF \
-    -Dprotobuf_BUILD_SHARED_LIBS=ON \
-    -Dprotobuf_ABSL_PROVIDER="module" \
-    -DCMAKE_PREFIX_PATH=$ABSEIL_CPP \
-    -Dprotobuf_JSONCPP_PROVIDER="package" \
-    -Dprotobuf_USE_EXTERNAL_GTEST=OFF \
-    ..
-
-cmake --build . --verbose
-cmake --install .
-cd $SCRIPT_DIR 
 
 
 echo "------------ grpc_cpp installing-------------------"
@@ -372,22 +436,18 @@ git checkout v1.68.0
 
 git submodule update --init
 
-
 mkdir grpc-prefix
 export GRPC_PREFIX=$(pwd)/grpc-prefix
 
 AR=`which ar`
 RANLIB=`which ranlib`
 
-
 PROTOC_BIN=$LIBPROTO_INSTALL/bin/protoc
-
 PROTOBUF_SRC=$LIBPROTO_INSTALL
 
-export CMAKE_PREFIX_PATH="$PREFIX;$RE2_PREFIX;$LIBPROTO_INSTALL"
+export CMAKE_PREFIX_PATH="$C_ARES_PREFIX;$RE2_PREFIX;$LIBPROTO_INSTALL"
 
-
-export LD_LIBRARY_PATH=$PREFIX/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=$LIBPROTO_INSTALL/lib64:${LD_LIBRARY_PATH}
 
 target_platform=$(uname)-$(uname -m)
 
@@ -400,8 +460,6 @@ fi
 
 mkdir -p build-cpp
 pushd build-cpp
-
-
 cmake ${CMAKE_ARGS} ..  \
       -GNinja \
       -DBUILD_SHARED_LIBS=ON \
@@ -421,95 +479,11 @@ cmake ${CMAKE_ARGS} ..  \
       -DCMAKE_VERBOSE_MAKEFILE=ON \
       -DProtobuf_PROTOC_EXECUTABLE=$PROTOC_BIN
 
-
 ninja install -v
 popd
 
 cd $SCRIPT_DIR
 
-
-echo "------------ orc installing-------------------"
-
-git clone https://github.com/apache/orc
-cd orc
-git checkout v2.0.3
-
-
-wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/python-ecosystem/o/orc/orc.patch
-git apply orc.patch
-
-mkdir orc_prefix
-export ORC_PREFIX=$(pwd)/orc_prefix
-
-mkdir -p build
-cd build
-
-export PROTOBUF_PREFIX=$LIBPROTO_INSTALL
-export CMAKE_PREFIX_PATH=$ABSEIL_PREFIX
-export LD_LIBRARY_PATH=$ABSEIL_PREFIX/lib
-
-export CC=$(which gcc)
-export CXX=$(which g++)
-export GCC=$CC
-export GXX=$CXX
-
-export HOST=$(uname)-$(uname -m)
-
-
-
-export HOST=$(uname)-$(uname -m)
-
-CPPFLAGS="${CPPFLAGS} -Wl,-rpath,$VIRTUAL_ENV_PATH/**/lib"
-
-
-declare -a _CMAKE_EXTRA_CONFIG
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
-    _CMAKE_EXTRA_CONFIG+=(-DHAS_PRE_1970_EXITCODE=0)
-    _CMAKE_EXTRA_CONFIG+=(-DHAS_PRE_1970_EXITCODE__TRYRUN_OUTPUT=)
-    _CMAKE_EXTRA_CONFIG+=(-DHAS_POST_2038_EXITCODE=0)
-    _CMAKE_EXTRA_CONFIG+=(-DHAS_POST_2038_EXITCODE__TRYRUN_OUTPUT=)
-fi
-if [[ ${HOST} =~ .*darwin.* ]]; then
-    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_AR=${AR})
-    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_RANLIB=${RANLIB})
-    _CMAKE_EXTRA_CONFIG+=(-DCMAKE_LINKER=${LD})
-fi
-if [[ ${HOST} =~ .*Linux.* ]]; then
-    CXXFLAGS="${CXXFLAGS//-std=c++17/-std=c++11}"
-    # I hate you so much CMake.
-    LIBPTHREAD=$(find ${PREFIX} -name "libpthread.so")
-    _CMAKE_EXTRA_CONFIG+=(-DPTHREAD_LIBRARY=${LIBPTHREAD})
-fi
-
-CPPFLAGS="${CPPFLAGS} -Wl,-rpath,$VIRTUAL_ENV_PATH/**/lib"
-
-cmake ${CMAKE_ARGS} \
-    -DCMAKE_PREFIX_PATH=$ORC_PREFIX \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_SHARED_LIBS=ON \
-    -DBUILD_JAVA=False \
-    -DLZ4_HOME=/usr \
-    -DZLIB_HOME=/usr \
-    -DZSTD_HOME=/usr \
-    -DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
-    -DProtobuf_ROOT=$PROTOBUF_PREFIX \
-    -DPROTOBUF_HOME=$PROTOBUF_PREFIX \
-    -DPROTOBUF_EXECUTABLE=$PROTOBUF_PREFIX/bin/protoc \
-    -DSNAPPY_HOME=/usr \
-    -DBUILD_LIBHDFSPP=NO \
-    -DBUILD_CPP_TESTS=OFF \
-    -DCMAKE_INSTALL_PREFIX=$ORC_PREFIX \
-    -DCMAKE_C_COMPILER=$(type -p ${CC})     \
-    -DCMAKE_CXX_COMPILER=$(type -p ${CXX})  \
-    -DCMAKE_C_FLAGS="$CFLAGS"  \
-    -DCMAKE_CXX_FLAGS="$CXXFLAGS -Wno-unused-parameter" \
-    "${_CMAKE_EXTRA_CONFIG[@]}" \
-    -GNinja ..
-
-ninja
-ninja install
-
-cd $SCRIPT_DIR
 
 
 
@@ -598,8 +572,8 @@ export PYARROW_PREFIX=$(pwd)/pyarrow_prefix
 export ARROW_HOME=$PYARROW_PREFIX
 export target_platform=$(uname)-$(uname -m)
 export CXX=$(which g++)
-export CMAKE_PREFIX_PATH=$ABSEIL_PREFIX:$LIBPROTO_INSTALL:$RE2_PREFIX:$GRPC_PREFIX:$ORC_PREFIX:$BOOST_PREFIX:${UTF8PROC_PREFIX}:$THRIFT_PREFIX:/usr
-export LD_LIBRARY_PATH=$ABSEIL_PREFIX/lib:$GRPC_PREFIX/lib:$LIBPROTO_INSTALL/lib64
+export CMAKE_PREFIX_PATH=$C_ARES_PREFIX:$LIBPROTO_INSTALL:$RE2_PREFIX:$GRPC_PREFIX:$ORC_PREFIX:$BOOST_PREFIX:${UTF8PROC_PREFIX}:$THRIFT_PREFIX:/usr
+export LD_LIBRARY_PATH=$GRPC_PREFIX/lib:$LIBPROTO_INSTALL/lib64
 
 mkdir cpp/build
 pushd cpp/build
@@ -664,14 +638,12 @@ cmake \
     -DARROW_DATASET=ON \
     -DARROW_DEPENDENCY_SOURCE=SYSTEM \
     -DARROW_FLIGHT=ON \
-    -DARROW_FLIGHT_REQUIRE_TLSCREDENTIALSOPTIONS=ON \
     -DARROW_HDFS=ON \
     -DARROW_JEMALLOC=ON \
     -DARROW_MIMALLOC=ON \
     -DARROW_ORC=ON \
     -DARROW_PACKAGE_PREFIX=$PYARROW_PREFIX \
     -DARROW_PARQUET=ON \
-    -DARROW_PLASMA=ON \
     -DARROW_PYTHON=ON \
     -DARROW_S3=OFF \
     -DARROW_WITH_BROTLI=ON \
@@ -684,25 +656,13 @@ cmake \
     -DCMAKE_BUILD_TYPE=release \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_INSTALL_PREFIX=$ARROW_HOME \
-    -DLLVM_TOOLS_BINARY_DIR=/usr/bin \
     -DPYTHON_EXECUTABLE=python \
     -DPython3_EXECUTABLE=python \
-    -DProtobuf_DIR=${LIBPROTO_INSTALL} \
-    -DProtobuf_LIBRARIES=${LIBPROTO_INSTALL}/lib64 \
-    -DProtobuf_INCLUDE_DIR=${LIBPROTO_INSTALL}/include \
     -DProtobuf_PROTOC_EXECUTABLE=${LIBPROTO_INSTALL}/bin/protoc \
-    -DORC_LIBRARIES=${ORC_PREFIX}/lib \
     -DORC_INCLUDE_DIR=${ORC_PREFIX}/include \
     -DgRPC_DIR=${GRPC_PREFIX} \
-    -DGRPCPP_IMPORTED_LOCATION=${GRPC_PREFIX}/lib/ \
-    -DGRPC_CPP_PLUGIN=${GRPC_PREFIX}/bin/ \
     -DBoost_DIR=${BOOST_PREFIX} \
-    -DBoost_LIB=${BOOST_PREFIX}/lib/ \
     -DBoost_INCLUDE_DIR=${BOOST_PREFIX}/include/ \
-    -DTHRIFT_DIR=${THRIFT_PREFIX} \
-    -DTHRIFT_LIB=${THRIFT_PREFIX}/lib \
-    -DTHRIFT_INCLUDE_DIR=${THRIFT_PREFIX}/include \
-    -Dutf8proc_DIR=${UTF8PROC_PREFIX} \
     -Dutf8proc_LIB=${UTF8PROC_PREFIX}/lib/libutf8proc.so ${UTF8PROC_PREFIX}/lib/libutf8proc.so.2 ${UTF8PROC_PREFIX}/lib/libutf8proc.so.2.4.1 \
     -Dutf8proc_INCLUDE_DIR=${UTF8PROC_PREFIX}/include \
     -DCMAKE_AR=${AR} \
