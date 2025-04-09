7dbb2b7d33bdd7545ba5133c70e39b1340a6dbcd
aastha-sharma2 <aastha.sharma4@ibm.com>
2025-04-09 15:44:59 +0530

    Update onnxmltools_ubi_9.3.sh

--- o/onnxmltools/onnxmltools_ubi_9.3.sh
+++ o/onnxmltools/onnxmltools_ubi_9.3.sh
@@ -50,9 +50,6 @@ git clone https://github.com/OpenMathLib/OpenBLAS
 cd OpenBLAS
 git checkout v0.3.29
 git submodule update --init
-wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/python-ecosystem/o/openblas/pyproject.toml
-
-sed -i "s/{PACKAGE_VERSION}/v0.3.29/g" pyproject.toml
 
 PREFIX=local/openblas
 OPENBLAS_SOURCE=$(pwd)
