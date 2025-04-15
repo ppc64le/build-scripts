e2709d55fd004e0a455abeb5d8e4813c7a9b5b8a
kiranNukal <sai.kiran.nukala@ibm.com>
2025-04-15 11:59:13 +0530

    Update statsmodels_0.14.4_ubi_9.3.sh

--- s/statsmodels/statsmodels_0.14.4_ubi_9.3.sh
+++ s/statsmodels/statsmodels_0.14.4_ubi_9.3.sh
@@ -16,7 +16,7 @@
 #             package and/or distribution. In such case, please
 #             contact "Maintainer" of this script.
 #
-# ----------------------------------------------------------------------------- 
+# -----------------------------------------------------------------------------
 echo "------------------------------------------------------------Cloning statsmodels github repo--------------------------------------------------------------"
 PACKAGE_NAME=statsmodels
 PACKAGE_VERSION=${1:-v0.14.4}
@@ -24,21 +24,11 @@ PACKAGE_URL=https://github.com/statsmodels/statsmodels.git
 PACKAGE_DIR=statsmodels
 
 echo "------------------------------------------------------------Installing requirements for statsmodels------------------------------------------------------"
-#dnf groupinstall -y "Development Tools" 
-dnf install -y wget
-dnf install -y openssl-devel
-dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/ppc64le/os/
-dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/BaseOS/ppc64le/os/
-dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/CRB/ppc64le/os/
-wget http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-Official
-mv RPM-GPG-KEY-CentOS-Official /etc/pki/rpm-gpg/.
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-Official
-dnf install --nodocs -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-
-dnf install -y git g++ gcc gcc-c++ gcc-gfortran \
+dnf install -y wget git g++ gcc gcc-c++ gcc-gfortran \
     meson ninja-build openblas-devel libjpeg-devel bzip2-devel libffi-devel zlib-devel \
-    libtiff-devel freetype-devel 
-dnf install -y make cmake automake autoconf procps-ng python3.12 python3.12-devel python3.12-pip
+    libtiff-devel freetype-devel make cmake automake autoconf procps-ng \
+    python3.12 python3.12-devel python3.12-pip
+
 git clone $PACKAGE_URL
 cd $PACKAGE_DIR
 git checkout $PACKAGE_VERSION
@@ -52,8 +42,7 @@ if ! python3.12 -m pip install .; then
 fi
 
 python3.12 -m pip install pytest
-if  [ "$(python3.12 --version  | grep "3.12")" ]
-then
+if [ "$(python3.12 --version | grep "3.12")" ]; then
   python3.12 -m pip install numpy==2.2.2
   python3.12 -m pip install pandas==2.2.3
   python3.12 -m pip install scipy==1.15.2 --prefer-binary
@@ -61,12 +50,11 @@ fi
 
 echo "------------------------------------------------------------Run tests for statsmodels------------------------------------------------------"
 cd $PACKAGE_DIR
-#skipping the collections errors to avoid modifying multiple test functions manually
 export PYTEST_ADDOPTS="--continue-on-collection-errors --ignore=tsa/tests/test_stattools.py"
 if ! pytest --import-mode=importlib; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_Test_Fails"
     exit 2
 else
     echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
