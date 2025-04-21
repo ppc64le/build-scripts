2e0bbed0396e64b88615d5582b5b38c36453c145
aastha-sharma2 <aastha.sharma4@ibm.com>
2025-04-21 15:52:22 +0530

    Update pymssql_ubi_9.3.sh (#5905)

--- p/pymssql/pymssql_ubi_9.3.sh
+++ p/pymssql/pymssql_ubi_9.3.sh
@@ -8,7 +8,7 @@
 # Language      : Python
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Haritha Nagothu <haritha.nagothu2@ibm.com>
+# Maintainer    : Aastha Sharma <aastha.sharma4@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -18,41 +18,48 @@
 #
 # ----------------------------------------------------------------------------
 
-#variables
+# variables
 PACKAGE_NAME=pymssql
 PACKAGE_VERSION=${1:-v2.2.5}
 PACKAGE_URL=https://github.com/pymssql/pymssql.git
+CURRENT_DIR=$(pwd)
 
 # Install dependencies and tools.
-yum install -y wget
-dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/ppc64le/os/
-dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/BaseOS/ppc64le/os/
-dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/CRB/ppc64le/os/   
-wget http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-Official
-mv RPM-GPG-KEY-CentOS-Official /etc/pki/rpm-gpg/.
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-Official
-dnf install --nodocs -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-yum install -y git gcc gcc-c++ python3-devel freetds-devel openssl-devel
+yum install -y git gcc gcc-c++ python3-devel openssl-devel
 
-#clone repository 
+#build freetds from source
+curl -LO ftp://ftp.freetds.org/pub/freetds/stable/freetds-1.3.17.tar.gz
+tar -xzf freetds-1.3.17.tar.gz
+cd freetds-1.3.17
+./configure --prefix=/usr/local --with-tdsver=7.3
+make -j$(nproc)
+make install
+cd $CURRENT_DIR
+
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+# Clone repository
 git clone $PACKAGE_URL
-cd  $PACKAGE_NAME
+cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-pip install -r dev/requirements-dev.txt
-pip install cython==0.29.33
-pip install setuptools_scm>=5.0
-pip install wheel>=0.36.2
+# Ensure compatible Cython version BEFORE installing requirements
+pip install cython==0.29.33 typing-extensions>=4.6.0
+#  Now install remaining build tools
+pip install setuptools_scm>=5.0 wheel>=0.36.2 pluggy exceptiongroup iniconfig
 
-#install
-if ! ( python3 setup.py install) ; then
+#  Install requirements WITHOUT upgrading Cython
+pip install --no-deps -r dev/requirements-dev.txt
+
+# Install package
+if ! ( pip install --no-build-isolation --no-use-pep517 . ); then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
-#test
+# Run tests
 if ! pytest; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
