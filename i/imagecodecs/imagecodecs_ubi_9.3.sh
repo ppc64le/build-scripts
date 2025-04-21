51b72c6b9f602f78a3f7d2c9725f3be3ec4d2088
Ramnath Nayak <ramnath.nayak@ibm.com>
2025-04-21 16:31:00 +0530

    Currency: Removed Centos/EPEL/RHEL/Fedora repos from imagecodecs (#5906)
    * Update imagecodecs_ubi_9.3.sh
    
    * Update build_info.json
    
    * Update imagecodecs_ubi_9.3.sh
    
    * Update imagecodecs_ubi_9.3.sh

--- i/imagecodecs/imagecodecs_ubi_9.3.sh
+++ i/imagecodecs/imagecodecs_ubi_9.3.sh
@@ -9,7 +9,7 @@
 # Language          : C,Python
 # Travis-Check      : True
 # Script License    : Apache License, Version 2.0
-# Maintainer        : Vinod K<Vinod.K1@ibm.com>
+# Maintainer        : Ramnath Nayak <Ramnath.Nayak@ibm.com>
 #
 # Disclaimer        : This script has been tested in root mode on given
 # ==========          platform using the mentioned version of the package.
@@ -25,29 +25,12 @@ PACKAGE_DIR=./imagecodecs
 CURRENT_DIR="${PWD}"
 
 
-yum install -y wget
-
-dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/ppc64le/os/
-dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/BaseOS/ppc64le/os/
-dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/CRB/ppc64le/os/
-
-# Import CentOS GPG key
-wget http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-Official
-mv RPM-GPG-KEY-CentOS-Official /etc/pki/rpm-gpg/.
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-Official
-
-# Enable EPEL
-dnf install --nodocs -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-
-yum install -y gcc gcc-c++ gcc-gfortran git make cmake autoconf automake \
-    python python-devel openssl-devel perl nasm yasm \
-    brotli brotli-devel bzip2 bzip2-devel blosc blosc-devel cfitsio cfitsio-devel \
-    CharLS giflib giflib-devel jxrlib jxrlib-devel liblerc lcms2 lcms2-devel \
-    libaec libaec-devel libavif libdeflate libdeflate-devel libheif libheif-devel \
-    libheif-tools libjpeg-turbo libjpeg-turbo-devel libjxl liblzf libpng libpng-devel \
-    libtiff libtiff-devel libtiff-tools libwebp libwebp-devel lz4 lz4-devel \
-    openjpeg2 openjpeg2-devel snappy snappy-devel xz xz-devel zlib zlib-devel \
-    zlib-ng zopfli zopfli-devel zstd libzstd-devel pkgconfig libtool hdf5 hdf5-devel
+yum install -y wget gcc gcc-c++ gcc-gfortran git make cmake autoconf automake \
+    python python-devel openssl-devel perl \
+    brotli brotli-devel bzip2 bzip2-devel giflib \
+    libjpeg-turbo libjpeg-turbo-devel libpng libpng-devel \
+    libtiff libtiff-devel libwebp libwebp-devel lz4 lz4-devel \
+    xz xz-devel zlib zlib-devel pkgconfig libtool openjpeg2 lcms2
 
 
 echo "Checking Python version..."
@@ -60,6 +43,7 @@ else
     echo "Python version is < 3.11, installing numpy 1.23.5..."
     pip install cython==0.29.36 numpy==1.26.4 wheel pylzma pytest
 fi
+
 # Installing below dependencies from source as those are not able to install from yum
 # Install libtiff-4.5.1 from source
 wget http://download.osgeo.org/libtiff/tiff-4.5.1.tar.gz
@@ -95,6 +79,119 @@ ldconfig
 x265 --version
 cd ../../
 
+#install libaec from source
+git clone https://gitlab.dkrz.de/k202009/libaec.git
+cd libaec
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local
+make -j$(nproc)
+make install
+cd ../..
+
+#install blosc from source
+git clone https://github.com/Blosc/c-blosc.git
+cd c-blosc
+mkdir build && cd build
+cmake ..
+make -j$(nproc)
+make install
+ldconfig
+cd ../..
+
+#install cfitsio from source
+wget https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-4.2.0.tar.gz
+tar -xf cfitsio-4.2.0.tar.gz && cd cfitsio-4.2.0
+./configure --prefix=/usr/local
+make
+make install
+cd ..
+
+#install charls from source
+git clone https://github.com/team-charls/charls.git
+cd charls && mkdir build && cd build
+cmake .. && make -j$(nproc)
+make install
+cd ../..
+
+#install giflib from source
+#installing from source because yum version lacks required headers for build
+wget https://downloads.sourceforge.net/project/giflib/giflib-5.2.1.tar.gz
+tar -xf giflib-5.2.1.tar.gz && cd giflib-5.2.1
+make -j$(nproc)
+make install
+cd ..
+
+#install jxrlib from source
+git clone https://github.com/MoonchildProductions/jxrlib.git
+cd jxrlib
+make 
+make install
+cd ..
+
+#install liblerc from source
+git clone https://github.com/Esri/lerc.git
+cd lerc
+mkdir cmake_build && cd cmake_build
+cmake .. && make -j$(nproc)
+make install
+cd ../..
+
+#install libdeflate from source
+git clone https://github.com/ebiggers/libdeflate.git
+cd libdeflate && mkdir build && cd build
+cmake .. && make -j$(nproc)
+make install
+cd ../..
+
+#install libheif from source
+git clone https://github.com/strukturag/libheif.git
+cd libheif
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc) && make install
+cd ../..
+
+#install liblzf from source
+wget http://dist.schmorp.de/liblzf/liblzf-3.6.tar.gz
+tar -xf liblzf-3.6.tar.gz && cd liblzf-3.6
+./configure && make -j$(nproc)
+make install
+cd ..
+
+#install openjpeg from source
+#installing from source because yum version lacks required headers for build
+git clone https://github.com/uclouvain/openjpeg.git
+cd openjpeg && mkdir build && cd build
+cmake .. && make -j$(nproc)
+make install
+cd ../..
+
+#install snappy from source
+git clone https://github.com/google/snappy.git
+cd snappy
+mkdir build && cd build
+cmake .. -DBUILD_SHARED_LIBS=ON -DSNAPPY_BUILD_TESTS=OFF -DSNAPPY_BUILD_BENCHMARKS=OFF
+make -j$(nproc)
+make install
+ldconfig
+cd ../..
+
+#install zopfli from source
+git clone https://github.com/google/zopfli.git
+cd zopfli
+make 
+cd ..
+cp $CURRENT_DIR/zopfli/src/zopfli/zopfli.h $CURRENT_DIR/usr/local/include/zopfli.h
+cp $CURRENT_DIR/zopfli/libzopfli.a $CURRENT_DIR/usr/local/lib/libzopfli.a
+
+#install lcms2 from source
+#installing from source because yum version lacks required headers for build
+wget https://downloads.sourceforge.net/project/lcms/lcms/2.14/lcms2-2.14.tar.gz
+tar -xf lcms2-2.14.tar.gz && cd lcms2-2.14
+./configure && make -j$(nproc)
+make install
+cd ..
+
 #install zfp
 git clone https://github.com/LLNL/zfp
 cd zfp
@@ -113,8 +210,20 @@ make -j
 make -j install
 cd ..
 
-#install bitshuffle
-yum install hdf5 hdf5-devel
+#install zstd from source
+git clone https://github.com/facebook/zstd.git
+cd zstd && make -j$(nproc)
+make install
+cd ..
+
+#install hdf5 from source
+wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.14/hdf5-1.14.3/src/hdf5-1.14.3.tar.gz
+tar -xf hdf5-1.14.3.tar.gz && cd hdf5-1.14.3
+./configure && make -j$(nproc)
+make install
+cd ..
+
+#install bitshuffle from source
 git clone https://github.com/kiyo-masui/bitshuffle
 cd bitshuffle
 git submodule update --init
@@ -125,8 +234,8 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-export CFLAGS="-I/usr/include/cfitsio"
-export LDFLAGS="-L/usr/lib64"
+export CFLAGS="-I/usr/local/include/openjpeg-2.5 -I/usr/lib/jxrlib-1.1/include/libjxr/image/ -I/usr/lib/jxrlib-1.1/include/libjxr/common/ -I/usr/lib/jxrlib-1.1/include/libjxr/glue/"
+export LDFLAGS="-L/usr/lib64 -L/usr/lib/jxrlib-1.1/lib"
 python3 setup.py build_ext --inplace
 
 if ! pip install . ; then
