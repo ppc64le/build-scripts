2a20e6a0b69985e63238e36545967eb6d81ee8d2
ICH <shubham-dayma-ibm>
2025-04-21 11:35:13 +0000

    Added build_script and Build_info.json using automation for tf-model-optimization-nightly

--- t/tf-model-optimization-nightly/tf-model-optimization-nightly_ubi_9.3.sh
+++ t/tf-model-optimization-nightly/tf-model-optimization-nightly_ubi_9.3.sh
@@ -1,58 +1,123 @@
 #!/bin/bash -e
 # -----------------------------------------------------------------------------
 #
-# Package        : tf-model-optimization-nightly
-# Version        : v0.7.4
-# Source repo    : https://github.com/tensorflow/model-optimization.git
-# Tested on      : UBI 9.3
-# Language       : Python
-# Travis-Check   : True
-# Script License : Apache License, Version 2 or later
-# Maintainer     : Vivek Sharma <vivek.sharma20@ibm.com>
+# Package          : tf-model-optimization-nightly
+# Version          : v0.8.0
+# Source repo      : https://github.com/tensorflow/model-optimization.git
+# Tested on	: UBI:9.3
+# Language      : Python
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : ICH <shubham-dayma-ibm>
 #
-# Disclaimer: This script has been tested in root mode on the given
-#             platform using the mentioned version of the package.
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
 #             It may not work as expected with newer versions of the
-#             package and/or distribution. In such cases, please
-#             contact the "Maintainer" of this script.
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
 #
-# -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 PACKAGE_NAME=tf-model-optimization-nightly
-PACKAGE_VERSION=${1:-v0.7.4}
-PACKAGE_DIR=model-optimization
+PACKAGE_VERSION=${1:-v0.8.0}
 PACKAGE_URL=https://github.com/tensorflow/model-optimization.git
+PACKAGE_DIR=tf-model-optimization-nightly
 
-# Install necessary system packages
-yum install -y git gcc gcc-c++ gzip tar make wget xz cmake yum-utils openssl-devel openblas-devel bzip2-devel bzip2 zip unzip libffi-devel zlib-devel autoconf automake libtool cargo pkgconf-pkg-config.ppc64le info.ppc64le fontconfig.ppc64le fontconfig-devel.ppc64le sqlite-devel python3 python3-devel
+yum install -y git  python3 python3-devel.ppc64le gcc gcc-c++ make wget sudo cmake
+pip3 install pytest tox nox
+PATH=$PATH:/usr/local/bin/
 
-# Clone the repository
-git clone $PACKAGE_URL
-cd $PACKAGE_DIR
-git checkout $PACKAGE_VERSION
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+SOURCE=Github
 
-# Install test dependencies
-pip3 install pytest tox
-
-# Removing chardet becuase it is giving issue while build the Package.
-yum remove -y python-chardet
-
-# Install the package
-if ! (pip3 install .); then
-    echo "------------------$PACKAGE_NAME: Installation failed ---------------------"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Installation_Failure"
-    exit 1
+# Install rust
+if ! command -v rustc &> /dev/null
+then
+    wget https://static.rust-lang.org/dist/rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
+    tar -xzf rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
+    cd rust-1.75.0-powerpc64le-unknown-linux-gnu
+    sudo ./install.sh
+    export PATH=$HOME/.cargo/bin:$PATH
+    rustc -V
+    cargo -V
+    cd ../
 fi
 
-# Run tests
-if ! tox -e py3; then
-    echo "------------------$PACKAGE_NAME: Tests_Fail------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Tests_Fail"
-    exit 2
+if [[ "$PACKAGE_URL" == *github.com* ]]; then
+    # Use git clone if it's a Git repository
+    if [ -d "$PACKAGE_DIR" ]; then
+        cd "$PACKAGE_DIR" || exit
+    else
+        if ! git clone "$PACKAGE_URL" "$PACKAGE_DIR"; then
+            echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+            echo "$PACKAGE_URL $PACKAGE_NAME"
+            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Clone_Fails"  
+            exit 1
+        fi
+        cd "$PACKAGE_DIR" || exit
+        git checkout "$PACKAGE_VERSION" || exit
+    fi
 else
-    echo "------------------$PACKAGE_NAME: Install & test both success ---------------------"
+    # If it's not a Git repository, download and untar
+    if [ -d "$PACKAGE_DIR" ]; then
+        cd "$PACKAGE_DIR" || exit
+    else
+        # Use download and untar if it's not a Git repository
+        if ! curl -L "$PACKAGE_URL" -o "$PACKAGE_DIR.tar.gz"; then
+            echo "------------------$PACKAGE_URL:download_fails---------------------------------------"
+            echo "$PACKAGE_URL $PACKAGE_NAME"
+            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Download_Fails"  
+            exit 1
+        fi
+        mkdir "$PACKAGE_DIR"
+
+        if ! tar -xzf "$PACKAGE_DIR.tar.gz" -C "$PACKAGE_DIR" --strip-components=1; then
+            echo "------------------$PACKAGE_NAME:untar_fails---------------------------------------"
+            echo "$PACKAGE_URL $PACKAGE_NAME"
+            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Untar_Fails"  
+            exit 1
+        fi
+
+        cd "$PACKAGE_DIR" || exit
+    fi
+fi
+
+# Install via pip3
+if !  python3 -m pip install ./; then
+        echo "------------------$PACKAGE_NAME:install_fails------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Install_Failed"  
+        exit 1
+fi
+
+# Run Tox
+python3 -m tox -e py39
+if [ $? -eq 0 ]; then
+    echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | Both_Install_and_Test_Success"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success"
     exit 0
+fi
+
+# Run Pytest
+python3 -m pytest
+if [ $? -eq 0 ]; then
+    echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
+
+# Run Nox
+python3 -m nox
+if [ $? -eq 0 ]; then
+    echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success"
+    exit 0
+else
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Install_success_but_test_Fails"
+    exit 2
 fi
