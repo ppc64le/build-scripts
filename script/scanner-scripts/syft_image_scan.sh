2512380b11ef405fecdc4bbe033e66dde4f4db1f
saiNammi <118737990+saiNammi@users.noreply.github.com>
2025-04-22 15:16:18 +0530

    Update syft_image_scan.sh

--- script/scanner-scripts/syft_image_scan.sh
+++ script/scanner-scripts/syft_image_scan.sh
@@ -5,7 +5,7 @@ build_docker=$BUILD_DOCKER
 
 if [ $build_docker == true ];then
         SYFT_VERSION=$(curl -s https://api.github.com/repos/anchore/syft/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
-        wget https://github.com/anchore/syft/releases/download/$SYFT_VERSION/grype_${SYFT_VERSION#v}_linux_ppc64le.tar.gz
+        wget https://github.com/anchore/syft/releases/download/$SYFT_VERSION/syft_${SYFT_VERSION#v}_linux_ppc64le.tar.gz
         tar -xzf grype_${SYFT_VERSION#v}_linux_ppc64le.tar.gz
         chmod +x syft
         sudo mv syft /usr/bin  
