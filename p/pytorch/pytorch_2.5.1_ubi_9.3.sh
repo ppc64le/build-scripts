cc61822a0c3461d3fceb37e01204dab4a763e77d
Harithanagothu2 <haritha.nagothu2@ibm.com>
2025-04-23 15:28:50 +0530

    Update pytorch_2.5.1_ubi_9.3.sh

--- p/pytorch/pytorch_2.5.1_ubi_9.3.sh
+++ p/pytorch/pytorch_2.5.1_ubi_9.3.sh
@@ -207,7 +207,7 @@ export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
 
 #Apply patch
-wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/p/protobuf/set_cpp_to_17_v4.25.3.patch
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/p/protobuf/set_cpp_to_17_v4.25.3.patch 
 git apply set_cpp_to_17_v4.25.3.patch
 
 cd python
