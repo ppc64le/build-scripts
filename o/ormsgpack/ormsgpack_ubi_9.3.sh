251c688c49f2578396ec9cba1a9b3274ae5ac6f0
vivek8123 <Vivek.Sharma20@ibm.com>
2025-04-28 10:55:27 +0530

    Create ormsgpack_ubi_9.3.sh
    Signed-off-by: vivek8123 <Vivek.Sharma20@ibm.com>

---
+++ o/ormsgpack/ormsgpack_ubi_9.3.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package        : ormsgpack
+# Version        : 1.9.1
+# Source repo    : https://github.com/aviramha/ormsgpack.git
+# Tested on      : UBI 9.3
+# Language       : Python/Rust
+# Travis-Check   : True
+# Script License : Apache License, Version 2 or later
+# Maintainer     : Vivek Sharma <vivek.sharma20@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on the given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such cases, please
+#             contact the "Maintainer" of this script.
+#
+# -----------------------------------------------------------------------------
+set -x
+PACKAGE_NAME=ormsgpack
+PACKAGE_VERSION=${1:-1.9.1}
+PACKAGE_URL=https://github.com/aviramha/ormsgpack.git
+RUST_VERSION=1.81.0
+source $HOME/.cargo/env
+
+yum install -y git gcc gcc-c++ python-devel gzip tar make wget xz cmake yum-utils openssl-devel     openblas-devel bzip2-devel bzip2 zip unzip libffi-devel zlib-devel autoconf     automake libtool cargo pkgconf-pkg-config.ppc64le info.ppc64le fontconfig.ppc64le     fontconfig-devel.ppc64le sqlite-devel
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain $RUST_VERSION
+rustup update stable
+rustup default $RUST_VERSION
+rustup install nightly
+rustup default nightly
+rustc --version
+
+# Set up Python environment
+pip3 install --upgrade pip
+pip3 install tox pytz maturin pytest msgpack pydantic numpy
+
+# Clone the repository
+git clone ${PACKAGE_URL}
+cd ${PACKAGE_NAME}
+git checkout ${PACKAGE_VERSION}
+
+# Build and install the package
+if ! maturin build --release .; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+# Run tests using tox
+if ! tox; then
+    echo "------------------$PACKAGE_NAME: Tests_Fail------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Tests_Fail"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME: Install & test both success ---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
+
