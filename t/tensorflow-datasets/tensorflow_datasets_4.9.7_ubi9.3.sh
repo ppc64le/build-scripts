f453e1a32ed22e91b9b323b998c19d69da41735d
stutiibm <Stuti.Wali@ibm.com>
2025-04-29 10:40:05 +0000

    Currency opence conversion: updated patch lkink

--- t/tensorflow-datasets/tensorflow_datasets_4.9.7_ubi9.3.sh
+++ t/tensorflow-datasets/tensorflow_datasets_4.9.7_ubi9.3.sh
@@ -1210,7 +1210,7 @@ git clone $PACKAGE_URL
 cd $PACKAGE_DIR
 git checkout $PACKAGE_VERSION
 SRC_DIR=$(pwd)
-wget https://raw.githubusercontent.com/stutiibm/build-scripts/refs/heads/master/t/tensorflow-datasets/TFDS-fix.patch
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/t/tensorflow-datasets/TFDS-fix.patch
 git apply TFDS-fix.patch
 
 #Install
