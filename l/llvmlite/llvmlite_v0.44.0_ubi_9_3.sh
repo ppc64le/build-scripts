3436cc6cf40bfc16d758c0fd0d257d57f364e22e
Shivansh S <shivansh.s1@ibm.com>
2025-05-09 12:47:00 +0530

    removed 0.44.04rc1 version from build_info

--- l/llvmlite/llvmlite_v0.44.0_ubi_9_3.sh
+++ l/llvmlite/llvmlite_v0.44.0_ubi_9_3.sh
@@ -88,7 +88,7 @@ if ! (python3.11 -m pip install .) ; then
 
 fi
 # Run tests
-if ! python -c "import llvmlite; import llvmlite.binding; import llvmlite.ir; import llvmlite.tests;"; then
+if ! python3.11 -c "import llvmlite; import llvmlite.binding; import llvmlite.ir; import llvmlite.tests;"; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
