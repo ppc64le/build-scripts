7985ab272a6534ae98175f3e7df5c22d9fbfa336
bharti-0211 <Bharti.Somra@ibm.com>
2025-05-12 05:21:32 -0400

    Updated script according to new package url

--- a/activemq-artemis-operator/activemq-artemis-operator_ubi_9_3.sh
+++ a/activemq-artemis-operator/activemq-artemis-operator_ubi_9_3.sh
@@ -2,31 +2,30 @@
 # -----------------------------------------------------------------------------
 #
 # Package           : activemq-artemis-operator
-# Version           : 1.2.4
-# Source repo       : https://github.com/artemiscloud/activemq-artemis-operator
+# Version           : master
+# Source repo       : https://github.com/rh-messaging/activemq-artemis-operator
 # Tested on         : UBI:9.3
 # Language          : Go
 # Travis-Check      : True
 # Script License    : Apache License, Version 2 or later
-# Maintainer        : Shubham Gupta(Shubham.Gupta43@ibm.com)
+# Maintainer        : Bharti Somra(Bharti.Somra@ibm.com)
 #
-# Disclaimer: This script has been tested in **root/non-root** mode on given
+# Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
 #             It may not work as expected with newer versions of the
 #             package and/or distribution. In such case, please
 #             contact "Maintainer" of this script.
-# run as root user
 # ----------------------------------------------------------------------------
 #
 
 PACKAGE_NAME=activemq-artemis-operator
-PACKAGE_URL=https://github.com/artemiscloud/activemq-artemis-operator
+PACKAGE_URL=https://github.com/rh-messaging/activemq-artemis-operator
 SDK_PACKAGE_NAME=operator-sdk
 SDK_PACKAGE_URL=https://github.com/operator-framework/operator-sdk
-PACKAGE_VERSION=${1:-1.2.4}
-GO_VERSION=1.20
+SDK_PACKAGE_VERSION=${1:-v1.28.0}
+GO_VERSION=1.22.7
 
-dnf install -y git wget gcc-c++ gcc make
+dnf update -y  && dnf install -y git wget gcc-c++ gcc make
 
 #Go installation
 wget https://go.dev/dl/go${GO_VERSION}.linux-ppc64le.tar.gz
@@ -46,29 +45,25 @@ cd ../
 #Check if package exists
 git clone $PACKAGE_URL $PACKAGE_NAME
 cd $PACKAGE_NAME/
-git checkout $PACKAGE_VERSION
 #ppc64 supported version of kustomize
 sed -i "s/v3.8.7/v5.4.1/g" Makefile
 
 #building and testing
 if ! make build ; then
     echo "------------------$PACKAGE_NAME:Install_Failure---------------------"
-        echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Failure"
+        echo "$PACKAGE_NAME  |  $PACKAGE_URL | GitHub | Fail |  Install_Failure"
         exit 1
 fi
 
 if ! make test; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | GitHub | Fail |  Install_success_but_test_Fails"
     exit 2
 else
     echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | GitHub  | Pass |  Both_Install_and_Test_Success"
     exit 0
 fi
 
-# As artemiscloud/activemq-artemis-operator testing requires clusters. Currently not supporting it.
-# We need to work on cluster testing (probably using minikube or something) and enable tests.
-# make test
