51657b9cdb33f93e05324cac7a681a42cff8a7e1
Bhagyashri <Bhagyashri.Gaikwad2@ibm.com>
2025-05-12 10:50:01 +0530

    Fixed issues with script

--- x/xformers/xformers_ubi_9_3.sh
+++ x/xformers/xformers_ubi_9_3.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : xformers
-# Version       : v0.0.28
+# Version       : v0.0.29
 # Source repo   : https://github.com/facebookresearch/xformers.git
 # Tested on     : UBI 9.3
 # Language      : Python, C++
@@ -19,7 +19,7 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=xformers
-PACKAGE_VERSION=${1:-v0.0.28}
+PACKAGE_VERSION=${1:-v0.0.29}
 PACKAGE_URL=https://github.com/facebookresearch/xformers.git
 PYTHON_VER=${PYTHON_VERSION:-3.11}
 BUILD_DEPS=${BUILD_DEPS:-true}
@@ -98,7 +98,7 @@ else
 fi
 
 # Build and install xformers
-if ! python${PYTHON_VER} -m pip install --use-pep517 -e . --config-settings editable_mode=compat -vvv; then
+if ! python${PYTHON_VER} -m pip install . -vvv; then
     echo "------------------$PACKAGE_NAME:Build_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail |  Build_fails"
