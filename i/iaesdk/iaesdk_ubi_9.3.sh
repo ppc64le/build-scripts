94e559a0f54e6c80ff89765d1ad6444237cba851
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2025-05-14 19:34:33 +0530

    Currency:Updated build-script,build_info.json for iaesdk:v3.3.0 (#6198)

--- i/iaesdk/iaesdk_ubi_9.3.sh
+++ i/iaesdk/iaesdk_ubi_9.3.sh
@@ -4,120 +4,58 @@
 # Package          : iaesdk
 # Version          : v3.3.0
 # Source repo      : https://github.com/IBM/ibm-iae-python-sdk
-# Tested on	: UBI:9.3
-# Language      : Python
-# Travis-Check  : True
-# Script License: Apache License, Version 2 or later
-# Maintainer    : ICH <shubham-dayma-ibm>
+# Tested on        : UBI:9.3
+# Language         : Python
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vinod.K1 <Vinod.K1@ibm.com>
 #
-# Disclaimer: This script has been tested in root mode on given
-# ==========  platform using the mentioned version of the package.
-#             It may not work as expected with newer versions of the
-#             package and/or distribution. In such case, please
-#             contact "Maintainer" of this script.
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
 #
-# ----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 
+# Variables
 PACKAGE_NAME=iaesdk
 PACKAGE_VERSION=${1:-v3.3.0}
 PACKAGE_URL=https://github.com/IBM/ibm-iae-python-sdk
-PACKAGE_DIR=iaesdk
+PACKAGE_DIR=ibm-iae-python-sdk
+CURRENT_DIR=$(pwd)
 
-yum install -y git  python3 python3-devel.ppc64le gcc gcc-c++ make wget sudo cmake
-pip3 install pytest tox nox
-PATH=$PATH:/usr/local/bin/
+# Install dependencies
+yum install -y git make wget gcc-toolset-13 openssl-devel python3 python3-pip python3-devel
 
-OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
-SOURCE=Github
+export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
+export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
 
-# Install rust
-if ! command -v rustc &> /dev/null
-then
-    wget https://static.rust-lang.org/dist/rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
-    tar -xzf rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
-    cd rust-1.75.0-powerpc64le-unknown-linux-gnu
-    sudo ./install.sh
-    export PATH=$HOME/.cargo/bin:$PATH
-    rustc -V
-    cargo -V
-    cd ../
-fi
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_DIR
+git checkout $PACKAGE_VERSION
 
-if [[ "$PACKAGE_URL" == *github.com* ]]; then
-    # Use git clone if it's a Git repository
-    if [ -d "$PACKAGE_DIR" ]; then
-        cd "$PACKAGE_DIR" || exit
-    else
-        if ! git clone "$PACKAGE_URL" "$PACKAGE_DIR"; then
-            echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
-            echo "$PACKAGE_URL $PACKAGE_NAME"
-            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Clone_Fails"  
-            exit 1
-        fi
-        cd "$PACKAGE_DIR" || exit
-        git checkout "$PACKAGE_VERSION" || exit
-    fi
-else
-    # If it's not a Git repository, download and untar
-    if [ -d "$PACKAGE_DIR" ]; then
-        cd "$PACKAGE_DIR" || exit
-    else
-        # Use download and untar if it's not a Git repository
-        if ! curl -L "$PACKAGE_URL" -o "$PACKAGE_DIR.tar.gz"; then
-            echo "------------------$PACKAGE_URL:download_fails---------------------------------------"
-            echo "$PACKAGE_URL $PACKAGE_NAME"
-            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Download_Fails"  
-            exit 1
-        fi
-        mkdir "$PACKAGE_DIR"
+#install python dependencies
+pip install pytest setuptools responses
 
-        if ! tar -xzf "$PACKAGE_DIR.tar.gz" -C "$PACKAGE_DIR" --strip-components=1; then
-            echo "------------------$PACKAGE_NAME:untar_fails---------------------------------------"
-            echo "$PACKAGE_URL $PACKAGE_NAME"
-            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Untar_Fails"  
-            exit 1
-        fi
-
-        cd "$PACKAGE_DIR" || exit
-    fi
-fi
-
-# Install via pip3
-if !  python3 -m pip install ./; then
-        echo "------------------$PACKAGE_NAME:install_fails------------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Install_Failed"  
-        exit 1
-fi
-
-# Run Tox
-python3 -m tox -e py39
-if [ $? -eq 0 ]; then
-    echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
+#install
+if ! pip3 install -e . ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success"
-    exit 0
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
 fi
 
-# Run Pytest
-python3 -m pytest
-if [ $? -eq 0 ]; then
-    echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
+# Run tests
+if ! pytest ; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success"
-    exit 0
-fi
-
-# Run Nox
-python3 -m nox
-if [ $? -eq 0 ]; then
-    echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success"
-    exit 0
-else
-    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Install_success_but_test_Fails"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
     exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
 fi
