8f35d3b200c1ab05429d98c38557ad9ad2b26db3
Md. Shafi Hussain <Md.Shafi.Hussain@ibm.com>
2025-05-16 11:14:20 +0530

    Add conditional checks to test different versions
    Signed-off-by: Md. Shafi Hussain <Md.Shafi.Hussain@ibm.com>

--- p/pyyaml/pyyaml_6.0_ubi_9.3.sh
+++ p/pyyaml/pyyaml_6.0_ubi_9.3.sh
@@ -39,7 +39,16 @@ if ! python3 setup.py install ; then
     exit 1
 fi
 
-if ! python3 setup.py test ; then
+python3 -m pip install pytest-xdist
+
+# Note: 6.0 & 6.0.1 use setup.py, 6.0.2 onwards use pytest
+if [[ "$PACKAGE_VERSION" == "6.0" ]] || [[ "$PACKAGE_VERSION" == "6.0.1" ]]; then
+	TEST_CMD="setup.py test"
+else
+    TEST_CMD="-m pytest -n auto"
+fi
+
+if ! python3 $TEST_CMD ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
