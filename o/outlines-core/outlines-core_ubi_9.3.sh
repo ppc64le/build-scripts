4cbbb55dd32f21902536cc6abe6abc81c7dca28a
Ramnath Nayak <ramnath.nayak@ibm.com>
2025-05-23 18:50:46 +0530

    Update outlines-core_ubi_9.3.sh

--- o/outlines-core/outlines-core_ubi_9.3.sh
+++ o/outlines-core/outlines-core_ubi_9.3.sh
@@ -748,8 +748,6 @@ if ! (pip3.12 install -e .) ; then
     exit 1
 fi
 export LD_LIBRARY_PATH=$OPENBLAS_PREFIX/lib:$LD_LIBRARY_PATH
-echo "ld path---------------------------"
-echo $LD_LIBRARY_PATH
 
 #run tests
 if !(pytest --cov=outlines_core -vv); then
