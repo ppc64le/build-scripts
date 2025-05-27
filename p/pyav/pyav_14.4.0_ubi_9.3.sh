8eaf517a1529111b8db07db3c01fad1b2b5bffdf
Shivansh S <shivansh.s1@ibm.com>
2025-05-27 12:38:44 +0530

    added build script for pyav==14.4.0

---
+++ p/pyav/pyav_14.4.0_ubi_9.3.sh
@@ -0,0 +1,278 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : PyAV
+# Version       : v14.4.0
+# Source repo   : https://github.com/PyAV-Org/PyAV
+# Tested on     : UBI 9.3
+# Language      : c
+# Travis-Check  : True
+# Script License: Apache License 2.0
+# Maintainer    : Shivansh Shamra <Shivansh.s1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+set -e 
+
+PACKAGE_NAME=PyAV
+PACKAGE_VERSION=${1:-v14.4.0}
+PACKAGE_URL=https://github.com/PyAV-Org/PyAV
+CURRENT_DIR=$(pwd)
+PACKAGE_DIR=PyAV
+
+# install core dependencies
+yum install -y wget python python-pip python-devel  gcc-toolset-13 gcc-toolset-13-binutils gcc-toolset-13-binutils-devel gcc-toolset-13-gcc-c++ git make cmake binutils 
+
+export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
+export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
+gcc --version
+
+export GCC_HOME=/opt/rh/gcc-toolset-13/root/usr
+export CC=$GCC_HOME/bin/gcc
+export CXX=$GCC_HOME/bin/g++
+
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+python -m pip install --upgrade pip
+
+INSTALL_ROOT="/install-deps"
+mkdir -p $INSTALL_ROOT
+
+
+for package in openblas lame opus libvpx ffmpeg pillow numpy; do
+    mkdir -p ${INSTALL_ROOT}/${package}
+    export "${package^^}_PREFIX=${INSTALL_ROOT}/${package}"
+    echo "Exported ${package^^}_PREFIX=${INSTALL_ROOT}/${package}"
+done
+
+python -m pip install numpy==2.0.2 cython pytest
+
+#installing openblas
+cd $CURRENT_DIR
+git clone https://github.com/OpenMathLib/OpenBLAS
+cd OpenBLAS
+git checkout v0.3.29
+git submodule update --init
+# Set build options
+declare -a build_opts
+# Fix ctest not automatically discovering tests
+LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,--gc-sections//g")
+export CF="${CFLAGS} -Wno-unused-parameter -Wno-old-style-declaration"
+unset CFLAGS
+export USE_OPENMP=1
+build_opts+=(USE_OPENMP=${USE_OPENMP})
+# Handle Fortran flags
+if [ ! -z "$FFLAGS" ]; then
+    export FFLAGS="${FFLAGS/-fopenmp/ }"
+    export FFLAGS="${FFLAGS} -frecursive"
+    export LAPACK_FFLAGS="${FFLAGS}"
+fi
+export PLATFORM=$(uname -m)
+build_opts+=(BINARY="64")
+build_opts+=(DYNAMIC_ARCH=1)
+build_opts+=(TARGET="POWER9")
+BUILD_BFLOAT16=1
+# Placeholder for future builds that may include ILP64 variants.
+build_opts+=(INTERFACE64=0)
+build_opts+=(SYMBOLSUFFIX="")
+# Build LAPACK
+build_opts+=(NO_LAPACK=0)
+# Enable threading and set the number of threads
+build_opts+=(USE_THREAD=1)
+build_opts+=(NUM_THREADS=8)
+# Disable CPU/memory affinity handling to avoid problems with NumPy and R
+build_opts+=(NO_AFFINITY=1)
+# Build OpenBLAS
+make ${build_opts[@]} CFLAGS="${CF}" FFLAGS="${FFLAGS}" prefix=${OPENBLAS_PREFIX}
+# Install OpenBLAS
+CFLAGS="${CF}" FFLAGS="${FFLAGS}" make install PREFIX="${OPENBLAS_PREFIX}" ${build_opts[@]}
+export LD_LIBRARY_PATH=${OPENBLAS_PREFIX}/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${OPENBLAS_PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH
+pkg-config --modversion openblas
+echo "-----------------------------------------------------Installed openblas-----------------------------------------------------"
+
+#installing libvpx
+cd $CURRENT_DIR
+git clone https://github.com/webmproject/libvpx.git
+cd libvpx
+git checkout v1.13.1
+export target_platform=$(uname)-$(uname -m)
+if [[ ${target_platform} == Linux-* ]]; then
+    LDFLAGS="$LDFLAGS -pthread"
+fi
+CPU_DETECT="${CPU_DETECT} --enable-runtime-cpu-detect"
+
+./configure --prefix=$LIBVPX_PREFIX \
+--as=yasm                    \
+--enable-shared              \
+--disable-static             \
+--disable-install-docs       \
+--disable-install-srcs       \
+--enable-vp8                 \
+--enable-postproc            \
+--enable-vp9                 \
+--enable-vp9-highbitdepth    \
+--enable-pic                 \
+${CPU_DETECT}                \
+--enable-experimental || { cat config.log; exit 1; }
+
+make 
+make install PREFIX="${LIBVPX_PREFIX}"
+export LD_LIBRARY_PATH=${LIBVPX_PREFIX}/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${LIBVPX_PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH
+pkg-config --modversion vpx
+echo "-----------------------------------------------------Installed libvpx------------------------------------------------"
+
+
+#installing lame
+cd $CURRENT_DIR
+wget https://downloads.sourceforge.net/sourceforge/lame/lame-3.100.tar.gz
+tar -xvf lame-3.100.tar.gz
+cd lame-3.100
+# remove libtool files
+find $LAME_PREFIX -name '*.la' -delete
+
+./configure --prefix=$LAME_PREFIX \
+            --disable-dependency-tracking \
+            --disable-debug \
+            --enable-shared \
+            --enable-static \
+            --enable-nasm
+
+make 
+make install PREFIX="${LAME_PREFIX}"
+export LD_LIBRARY_PATH=/install-deps/lame/lib:$LD_LIBRARY_PATH
+export PATH="/install-deps/lame/bin:$PATH"
+lame --version 
+echo "-----------------------------------------------------Installed lame------------------------------------------------"
+
+
+#installing opus
+cd $CURRENT_DIR
+git clone https://github.com/xiph/opus
+cd opus
+git checkout v1.3.1
+yum install -y autoconf automake libtool
+./autogen.sh
+./configure --prefix=$OPUS_PREFIX
+make 
+make install PREFIX="${OPUS_PREFIX}"
+export LD_LIBRARY_PATH=${OPUS_PREFIX}/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${OPUS_PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH
+pkg-config --modversion opus
+echo "-----------------------------------------------------Installed opus------------------------------------------------"
+
+
+#installing ffmpeg
+cd $CURRENT_DIR
+git clone https://github.com/FFmpeg/FFmpeg
+cd FFmpeg
+git checkout n7.1
+git submodule update --init
+
+yum install -y gmp-devel freetype-devel openssl-devel
+
+
+USE_NONFREE=no   #the options below are set for NO
+./configure \
+        --prefix="$FFMPEG_PREFIX" \
+        --cc=${CC} \
+        --disable-doc \
+        --enable-gmp \
+        --enable-hardcoded-tables \
+        --enable-libfreetype \
+        --enable-pthreads \
+        --enable-postproc \
+        --enable-pic \
+        --enable-pthreads \
+        --enable-shared \
+        --enable-static \
+        --enable-version3 \
+        --enable-zlib \
+        --enable-libopus \
+        --enable-libmp3lame \
+        --enable-libvpx \
+        --extra-cflags="-I${LAME_PREFIX}/include -I${OPUS_PREFIX}/include -I${libvpx_PREFIX}/include" \
+        --extra-ldflags="-L${LAME_PREFIX}/lib -L${OPUS_PREFIX}/lib -L${libvpx_PREFIX}/lib" \
+        --disable-decoder=libh264 \
+        --disable-decoder=libx264 \
+        --disable-decoder=libopenh264 \
+        --disable-encoder=libopenh264 \
+        --disable-encoder=libx264 \
+        --disable-decoder=libx264rgb \
+        --disable-encoder=libx264rgb \
+        --disable-encoder=hevc \
+        --disable-decoder=hevc \
+        --disable-decoder=aac_fixed \
+        --disable-encoder=aac_latm \
+        --disable-decoder=aac_latm \
+        --disable-nonfree --disable-gpl --disable-gnutls --enable-openssl --disable-libopenh264 --disable-libx264
+
+make 
+make install PREFIX="${FFMPEG_PREFIX}"
+export PKG_CONFIG_PATH=${FFMPEG_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
+export LD_LIBRARY_PATH=${FFMPEG_PREFIX}/lib:${LD_LIBRARY_PATH}
+export PATH="/install-deps/ffmpeg/bin:$PATH"
+ffmpeg -version
+echo "-----------------------------------------------------Installed ffmpeg------------------------------------------------"
+
+
+#installing pillow
+cd $CURRENT_DIR
+git clone https://github.com/python-pillow/Pillow
+cd Pillow
+git checkout 11.1.0
+
+yum install -y libjpeg-turbo-devel
+git submodule update --init
+
+python -m pip install . 
+
+echo "-----------------------------------------------------Installed pillow------------------------------------------------"
+
+
+# clone source repository
+cd $CURRENT_DIR
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+git submodule update --init
+
+export CFLAGS="${CFLAGS} -I/install-deps/ffmpeg/include"
+export LDFLAGS="${LDFLAGS} -L/install-deps/ffmpeg/lib"
+
+#Build package
+if ! (python setup.py build_ext --inplace) ; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+#During wheel creation for this package we need installed dependencies from source. Once script get exit, and if we build wheel through wrapper script, then those are not applicable during wheel creation. So we are generating wheel for this package in script itself.
+echo "---------------------------------------------------Building the wheel--------------------------------------------------"
+pip install --upgrade pip build setuptools wheel
+python -m build --wheel --no-isolation --outdir="$CURRENT_DIR/"
+
+echo "----------------------------------------------Testing pkg-------------------------------------------------------"
+#Test package
+#Skipping few tests as they are failing because of disabling some codecs related to audio and video in ffmpeg. We disabled those codecs because of license associated with them.
+
+if ! (pytest -k "not test_encoding_dnxhd and not test_encoding_dvvideo and not test_encoding_h264 and not test_encoding_mjpeg and not test_encoding_mpeg1video and not test_encoding_mpeg4 and not test_encoding_png and not test_encoding_tiff and not test_encoding_xvid and not test_mov and not test_decode_video_corrupt and not test_decoded_motion_vectors and not test_decoded_motion_vectors_no_flag and not test_decoded_time_base and not test_decoded_video_frame_count and not test_flush_decoded_video_frame_count and not test_av_stream_Stream and not test_encoding_with_pts and not test_stream_audio_resample and not test_max_b_frames and not test_container_probing and not test_stream_probing and not test_stream_probing and not test_writing_to_custom_io_dash and not test_decode_half and not test_stream_seek and not test_side_data and not test_opaque and not test_reformat_pixel_format_align") ; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
