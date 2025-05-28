46efc696f29a1a2b051a25117d1acba7746c9c3e
Ramnath Nayak <ramnath.nayak@ibm.com>
2025-05-28 14:49:12 +0530

    Update and rename sentry-python_ubi_9_3.sh to sentry-python_ubi_9.3.sh

--- s/sentry-python/sentry-python_ubi_9.3.sh
+++ s/sentry-python/sentry-python_ubi_9.3.sh
@@ -2,13 +2,13 @@
 # ----------------------------------------------------------------------------
 #
 # Package       : sentry-python
-# Version       : 2.7.1
+# Version       : 2.29.1
 # Source repo   : https://github.com/getsentry/sentry-python
-# Tested on     : UBI: 9.3
+# Tested on     : UBI:9.3
 # Language      : Python
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Stuti Wali <Stuti.Wali@ibm.com>
+# Maintainer    : Ramnath Nayak <Ramnath.Nayak@ibm.com>
 #
 #
 # Disclaimer: This script has been tested in root mode on given
@@ -23,41 +23,37 @@ set -e
 
 #variables
 PACKAGE_NAME=sentry-python
-PACKAGE_VERSION=${1:-2.7.1}
+PACKAGE_VERSION=${1:-2.29.1}
 PACKAGE_URL=https://github.com/getsentry/sentry-python
-HOME_DIR=${PWD}
+PACKAGE_DIR=sentry-python
+CURRENT_DIR=$(pwd)
 
-sudo dnf install -y ncurses wget git python3 python3-devel make gcc gcc-c++ 
+yum install -y ncurses wget git python3.12 python3.12-devel python3.12-pip make gcc-toolset-13 gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc
 
-#creating virtual environment
-pip3 install virtualenv
-export PATH=$HOME_DIR/.local/bin:$PATH
-virtualenv --version
-virtualenv myenv
-virtualenv -p /usr/bin/python3 myenv
-source myenv/bin/activate
-
-
-sudo ln -s /usr/bin/python3 /usr/bin/python
+export GCC_TOOLSET_PATH=/opt/rh/gcc-toolset-13/root/usr
+export PATH=$GCC_TOOLSET_PATH/bin:$PATH
 
 #cloning the repo
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
-pip install --upgrade requests tox
-pip install pytest==6.2.5
-pip install -r requirements-testing.txt
+
+pip3.12 install --upgrade requests tox
+pip3.12 install -r requirements-testing.txt
 
 #Build package
-if ! pip3 install -e . ; then
+if ! pip3.12 install -e . ; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
+pip3.12 install -r scripts/populate_tox/requirements.txt
+pip3.12 install -r scripts/split_tox_gh_actions/requirements.txt
+
 #Test package
-if ! pytest ; then
+if ! tox -e py3.12 ; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
