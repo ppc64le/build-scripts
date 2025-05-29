6b24c7f497d21d17149240f109209a2f8cd287e8
kiranNukal <sai.kiran.nukala@ibm.com>
2025-05-29 10:28:35 +0530

    Update and rename bsdiff4_ubi_8.7.sh to bsdiff4_ubi_9.3.sh

--- b/bsdiff4/bsdiff4_ubi_9.3.sh
+++ b/bsdiff4/bsdiff4_ubi_9.3.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package          : bsdiff4
-# Version          : 1.2.4
+# Version          : 1.2.6
 # Source repo      : https://github.com/ilanschnell/bsdiff4
 # Tested on        : UBI 9.3
 # Language         : C, Python
@@ -17,11 +17,9 @@
 # -----------------------------------------------------------------------------
 
 PACKAGE_NAME=bsdiff4
-PACKAGE_VERSION=${1:-1.2.4}
+PACKAGE_VERSION=${1:-1.2.6}
 PACKAGE_URL=https://github.com/ilanschnell/bsdiff4
 PACKAGE_DIR=bsdiff4
-HOME_DIR=${PWD}
-OS_NAME="UBI 9.3"
 
 # Install dependencies
 yum install -y git wget gcc-toolset-13 gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc-gfortran gcc-toolset-13-binutils gcc-toolset-13-binutils-devel python3.12 python3.12-pip python3.12-devel make automake autoconf libtool gdb rpm-build gettext
@@ -38,7 +36,6 @@ export PATH=$PATH:/usr/local/bin
 python3.12 -m pip install pytest
 
 # Clone the package
-cd $HOME_DIR
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
@@ -47,7 +44,7 @@ git checkout $PACKAGE_VERSION
 if ! python3.12 -m pip install -e .; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_VERSION $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | GitHub | Fail |  Build_Fails"
     exit 1
 fi
 
@@ -56,11 +53,11 @@ python3.12 -m pip install ".[test]"
 if ! pytest; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
     exit 2
 else
     echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
     echo "$PACKAGE_VERSION $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Install_and_Test_Success"
+    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | GitHub  | Pass |  Install_and_Test_Success"
     exit 0
 fi
