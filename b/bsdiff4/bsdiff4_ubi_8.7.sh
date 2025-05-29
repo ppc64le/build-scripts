4e5cb48c7b590213451c59cdb09533d27f49024c
kiranNukal <sai.kiran.nukala@ibm.com>
2025-05-29 08:59:26 +0530

    Update bsdiff4_ubi_8.7.sh

--- b/bsdiff4/bsdiff4_ubi_8.7.sh
+++ b/bsdiff4/bsdiff4_ubi_8.7.sh
@@ -4,57 +4,63 @@
 # Package          : bsdiff4
 # Version          : 1.2.4
 # Source repo      : https://github.com/ilanschnell/bsdiff4
-# Tested on        : UBI 8.7
-# Language         : C,Python
-# Travis-Check     : True
+# Tested on        : UBI 9.3
+# Language         : C, Python
 # Script License   : Apache License, Version 2 or later
-# Maintainer       : Vinod K <Vinod.K1@ibm.com>
+# Maintainer       : Sai Kiran Nukala <sai.kiran.nukala@ibm.com>
 #
-# Disclaimer       : This script has been tested in root mode on given
-# ==========         platform using the mentioned version of the package.
+# Disclaimer       : This script has been tested in root mode on the given
+#                    platform using the mentioned version of the package.
 #                    It may not work as expected with newer versions of the
-#                    package and/or distribution. In such case, please
-#                    contact "Maintainer" of this script.
+#                    package and/or distribution.
 #
-# ----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 PACKAGE_NAME=bsdiff4
 PACKAGE_VERSION=${1:-1.2.4}
 PACKAGE_URL=https://github.com/ilanschnell/bsdiff4
+PACKAGE_DIR=bsdiff4
 HOME_DIR=${PWD}
+OS_NAME="UBI 9.3"
 
-yum install git wget gcc gcc-c++ python39 python39-pip python39-devel yum-utils make automake autoconf libtool gdb* binutils rpm-build gettext  -y
+# Install dependencies
+yum install -y git wget gcc-toolset-13 gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc-gfortran gcc-toolset-13-binutils gcc-toolset-13-binutils-devel python3.12 python3.12-pip python3.12-devel make automake autoconf libtool gdb rpm-build gettext
 
-# Install pip and activate venv
-python3 -m ensurepip --upgrade
+export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
+export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
+
+# Ensure pip is working for Python 3.12
+python3.12 -m ensurepip --upgrade
+python3.12 -m pip install --upgrade pip setuptools wheel
 export PATH=$PATH:/usr/local/bin
 
-pip3 install pytest
+# Install test dependencies
+python3.12 -m pip install pytest
 
-# Clone package repository
+# Clone the package
 cd $HOME_DIR
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-# Install
-if !  python3 -m pip install -e .; then
-        echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
-        echo "$PACKAGE_VERSION $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
-        exit 1
+# Install the package
+if ! python3.12 -m pip install -e .; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_VERSION $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+    exit 1
 fi
 
-# Test
-python3 -m pip install bsdiff4[test]
-if ! pytest ; then
-        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
-        exit 2
+# Run tests
+python3.12 -m pip install ".[test]"
+if ! pytest; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
 else
-        echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
-        echo "$PACKAGE_VERSION $PACKAGE_NAME"
-        echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Install_and_Test_Success"
-        exit 0
+    echo "------------------$PACKAGE_NAME:install_and_test_success-------------------------"
+    echo "$PACKAGE_VERSION $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Install_and_Test_Success"
+    exit 0
 fi
