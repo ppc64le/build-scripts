d9b4fc08462a46c2186eaf8a8d3f0b42ecf33d64
kiranNukal <sai.kiran.nukala@ibm.com>
2025-05-29 12:29:45 +0530

    Create itsdangerous_ubi_9_3.sh

---
+++ i/itsdangerous/itsdangerous_ubi_9_3.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+#
+# -----------------------------------------------------------------------------
+#
+# Package       : itsdangerous
+# Version       : 2.2.0
+# Source repo   : https://github.com/pallets/itsdangerous
+# Tested on     : UBI 9.3
+# Language      : c
+# Travis-Check  : True
+# Script License: Apache License 2.0
+# Maintainer    : Sai Kiran Nukala <sai.kiran.nukala@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+set -e
+
+PACKAGE_NAME=itsdangerous
+PACKAGE_VERSION=${1:-2.2.0}
+PACKAGE_URL=https://github.com/pallets/itsdangerous
+PACKAGE_DIR=itsdangerous
+
+# Install dependencies
+yum install -y git gcc-toolset-13 gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make python3.12 python3.12-devel python3.12-pip openssl-devel 
+pip3.12 install setuptools mock ipython_genutils traitlets pytest freezegun
+
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+# Build package
+if ! python3.12 -m pip install -e . ; then
+    echo "------------------$PACKAGE_NAME:build_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_Fails"
+    exit 1
+fi
+
+# Run test cases
+if !(pytest); then
+    echo "------------------$PACKAGE_NAME:build_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:build_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Build_and_Test_Success"
+    exit 0
+fi
