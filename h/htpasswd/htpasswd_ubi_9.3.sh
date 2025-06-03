a9eaff37d4f1f1a06bc68faa514c203601f86add
James Jenkins <James.Jenkins@ibm.com>
2025-06-03 13:05:20 -0400

    Build and test htpasswd source

--- h/htpasswd/htpasswd_ubi_9.3.sh
+++ h/htpasswd/htpasswd_ubi_9.3.sh
@@ -3,7 +3,7 @@
 #
 # Package	: htpasswd
 # Version	: 2.3
-# Source repo	: 
+# Source repo	: https://github.com/thesharp/htpasswd
 # Tested on	: UBI:9.3
 # Language      : Python
 # Travis-Check  : True
@@ -19,53 +19,105 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME=htpasswd
-PACKAGE_VERSION=${1:-2.3}
+PACKAGE_VERSION=2.3
+PACKAGE_URL=https://files.pythonhosted.org/packages/b9/2f/8b76f8b77125b75c3532966f3291f9e8787268be65fc4c9694887cba9375/htpasswd-2.3.tar.gz
 PACKAGE_DIR=htpasswd
 
-yum install -y sudo zlib-devel pip wget python3 python3-devel.ppc64le ncurses git gcc gcc-c++ \
-libffi libffi-devel sqlite sqlite-devel sqlite-libs make cmake cargo openssl-devel
-pip3 install build
+yum install -y git  python3 python3-devel.ppc64le gcc gcc-c++ make wget sudo cmake
+pip3 install pytest tox nox
 PATH=$PATH:/usr/local/bin/
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
-mkdir $PACKAGE_DIR
-cd $PACKAGE_DIR
-HTPASSWD_DIR=$(pwd)
+SOURCE=Github
 
-# Install Python 3.11.9
-if ! python3.11 --version; then
-    cd /usr/src && \
-wget https://www.python.org/ftp/python/3.11.9/Python-3.11.9.tgz && \
-    tar xzf Python-3.11.9.tgz && \
-    cd Python-3.11.9 && \
-    ./configure --enable-optimizations && \
-    make altinstall && \
-    ln -s /usr/local/bin/python3.11 /usr/bin/python3.11 && \
-    cd /usr/src && \
-    rm -rf Python-3.11.9.tgz Python-3.11.9
-    # Manually install pip if it's not installed
-    if ! python3.11 -m pip --version; then
-wget https://bootstrap.pypa.io/get-pip.py && \
-python3.11 get-pip.py && \
-rm get-pip.py
+# Install rust
+if ! command -v rustc &> /dev/null
+then
+    wget https://static.rust-lang.org/dist/rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
+    tar -xzf rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
+    cd rust-1.75.0-powerpc64le-unknown-linux-gnu
+    sudo ./install.sh
+    export PATH=$HOME/.cargo/bin:$PATH
+    rustc -V
+    cargo -V
+    cd ../
+fi
+
+if [[ "$PACKAGE_URL" == *github.com* ]]; then
+    # Use git clone if it's a Git repository
+    if [ -d "$PACKAGE_DIR" ]; then
+        cd "$PACKAGE_DIR" || exit
+    else
+        if ! git clone "$PACKAGE_URL" "$PACKAGE_DIR"; then
+            echo "------------------$PACKAGE_NAME:clone_fails---------------------------------------"
+            echo "$PACKAGE_URL $PACKAGE_NAME"
+            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Clone_Fails"  
+            exit 1
+        fi
+        cd "$PACKAGE_DIR" || exit
+        git checkout "$PACKAGE_VERSION" || exit
+    fi
+else
+    # If it's not a Git repository, download and untar
+    if [ -d "$PACKAGE_DIR" ]; then
+        cd "$PACKAGE_DIR" || exit
+    else
+        # Use download and untar if it's not a Git repository
+        if ! curl -L "$PACKAGE_URL" -o "$PACKAGE_DIR.tar.gz"; then
+            echo "------------------$PACKAGE_URL:download_fails---------------------------------------"
+            echo "$PACKAGE_URL $PACKAGE_NAME"
+            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Download_Fails"  
+            exit 1
+        fi
+        mkdir "$PACKAGE_DIR"
+
+        if ! tar -xzf "$PACKAGE_DIR.tar.gz" -C "$PACKAGE_DIR" --strip-components=1; then
+            echo "------------------$PACKAGE_NAME:untar_fails---------------------------------------"
+            echo "$PACKAGE_URL $PACKAGE_NAME"
+            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Untar_Fails"  
+            exit 1
+        fi
+
+        cd "$PACKAGE_DIR" || exit
     fi
 fi
 
-cd $HTPASSWD_DIR
-
-# create venv
-python3.11 -m venv venv
-source venv/bin/activate
-
-# build wheel
-if ! python3.11 -m pip wheel --no-deps htpasswd==${PACKAGE_VERSION}; then
-    echo "--------------------$PACKAGE_NAME:wheel_build_fails----------------------------------------"
-else
-    echo "--------------------$PACKAGE_NAME:wheel_build_success----------------------------------------"
+# Install via pip3
+if !  python3 -m pip install ./; then
+        echo "------------------$PACKAGE_NAME:install_fails------------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Install_Failed"  
+        exit 1
 fi
 
-# cleanup
-deactivate
-rm -rf venv
+# Run Tox
+python3 -m tox -e py39
+if [ $? -eq 0 ]; then
+    echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
 
-exit 0
+# Run Pytest
+python3 -m pytest
+if [ $? -eq 0 ]; then
+    echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success"
+    exit 0
+fi
+
+# Run Nox
+python3 -m nox
+if [ $? -eq 0 ]; then
+    echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Pass | Both_Install_and_Test_Success"
+    exit 0
+else
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Install_success_but_test_Fails"
+    exit 2
+fi
