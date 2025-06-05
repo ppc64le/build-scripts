6fd2b78a4444ba3a1acb3d2e94a1d2efc56c4104
Ramnath Nayak <ramnath.nayak@ibm.com>
2025-06-05 09:48:18 +0530

    Update btrees_ubi_9.3.sh

--- b/btrees/btrees_ubi_9.3.sh
+++ b/btrees/btrees_ubi_9.3.sh
@@ -25,7 +25,8 @@ PACKAGE_DIR=Btrees
 
 CURRENT_DIR=${PWD}
 
-yum install -y git make cmake zip tar wget python3 python3-devel python3-pip gcc-toolset-13 gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc zlib-devel libjpeg-devel 
+yum install -y git make cmake zip tar wget python3 python3-devel python3-pip gcc-toolset-13 gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc zlib-devel libjpeg-devel
+yum remove -y python3-chardet
 
 export GCC_TOOLSET_PATH=/opt/rh/gcc-toolset-13/root/usr
 export PATH=$GCC_TOOLSET_PATH/bin:$PATH
@@ -33,7 +34,6 @@ export PATH=$GCC_TOOLSET_PATH/bin:$PATH
 pip install -U pip
 pip install -U "setuptools <74" wheel twine
 pip install cffi
-pip uninstall chardet
 pip install -U persistent tox
 
 git clone $PACKAGE_URL
