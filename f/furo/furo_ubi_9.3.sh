64b6c229f595daa4b5183f19f9677dbf576d7e55
Harithanagothu2 <haritha.nagothu2@ibm.com>
2025-06-10 14:11:39 +0530

    Create furo_ubi_9.3.sh

---
+++ f/furo/furo_ubi_9.3.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : furo
+# Version          : 2024.08.06
+# Source repo      : https://github.com/pradyunsg/furo
+# Tested on        : UBI 9.5
+# Language         : Python
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Haritha Nagothu <haritha.nagothu2@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+set -e
+
+PACKAGE_NAME=furo
+PACKAGE_VERSION=${1:-"2024.08.06"}
+PACKAGE_URL=https://github.com/pradyunsg/furo
+
+# Install dependencies
+yum install -y python3.12-devel git python3.12-pip gcc-toolset-13 make
+source /opt/rh/gcc-toolset-13/enable
+export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
+
+git clone https://github.com/nodakai/tree-command.git
+cd tree-command
+make
+cp tree /usr/local/bin/
+cd ..
+
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION 
+
+python3.12 -m pip install tomli pytest httpx
+
+if ! python3.12 -m pip install . ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! python3.12 -m pytest ; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
