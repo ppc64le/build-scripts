33d74d4c40eb7ebc09b4a694df6986da0fd5d53e
Ramnath Nayak <ramnath.nayak@ibm.com>
2025-06-11 16:44:16 +0530

    Update pallets-sphinx-themes_ubi_9.3.sh

--- p/pallets-sphinx-themes/pallets-sphinx-themes_ubi_9.3.sh
+++ p/pallets-sphinx-themes/pallets-sphinx-themes_ubi_9.3.sh
@@ -34,10 +34,10 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-pip3.12 install --upgrade pip tox
+python3.12 -m pip install --upgrade pip tox
 
 #Build package
-if ! pip3.12 install . ; then
+if ! python3.12 -m pip install . ; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
