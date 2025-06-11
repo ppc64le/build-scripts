983475804fd3e87a7680969af38b1cd365fabc10
JeSh30 <Anumala.Rajesh@ibm.com>
2025-06-11 16:09:19 +0530

    minor changes are made in sphinx

--- s/sphinx/sphinx_ubi_9.3.sh
+++ s/sphinx/sphinx_ubi_9.3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# 
+#
 # ----------------------------------------------------------------------------
 # Package          : sphinx
 # Version          : v8.2.3
@@ -15,51 +15,50 @@
 #                    It may not work as expected with newer versions of the
 #                    package and/or distribution. In such case, please
 #                    contact "Maintainer" of this script.
-#   
+#
 # ----------------------------------------------------------------------------
 
-set -ex 
+set -ex
 
 # Variables
 PACKAGE_NAME=sphinx
 PACKAGE_URL=https://github.com/sphinx-doc/sphinx
-PACKAGE_VERSION=${1:-v8.2.3}
+PACKAGE_VERSION=${1:-v8.2.3} 
+
+PYTHON_VERSION=3.12
 
 #Dependencies
-yum install -y python3.12 python3.12-devel python3.12-pip ncurses git gcc-toolset-13 libffi libffi-devel sqlite 
-yum install -y sqlite-devel sqlite-libs python3.12-pytest make cmake wget cargo rust openssl-devel graphviz
+yum install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-devel python${PYTHON_VERSION}-pip ncurses make cmake
+yum install -y git gcc-toolset-13 libffi libffi-devel sqlite openssl-devel xz-devel bzip2-devel ncurses-devel wget
+yum install -y sqlite-devel sqlite-libs python${PYTHON_VERSION}-pytest cargo rust graphviz zlib-devel findutils
 
 export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
 
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 
- 
 # Cloning the repository from remote to local
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 # Upgrade pip and essential build tools
-python3.12 -m pip install --upgrade pip setuptools wheel
+"python${PYTHON_VERSION}" -m pip install --upgrade pip setuptools wheel
 
 
-echo " --------------------------- Sphinx Installing --------------------------- " 
+echo " --------------------------- Sphinx Installing --------------------------- "
 
-# Install the package  
+# Install the package
 if ! python3.12 -m pip install .; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_Fails"
     exit 1
-fi 
+fi
 
-# Install required testing dependencies
-python3.12 -m pip install .[test] pytest pytest-xdist
+"python${PYTHON_VERSION}" -m pip install .[test] pytest pytest-xdist
 
 # Skipped due to AssertionError from Unicode character mismatch (e.g., 'Х' vs 'РҐ');
 # likely environment- or encoding-dependent. Prevents false failures in CI. 
-echo " --------------------------- Sphinx Testing --------------------------- " 
-
-if ! make test PYTHON=$(which python3.12) TEST="--junitxml=test-reports/pytest/results.xml -vv -k 'not test_ModuleAnalyzer_for_module'"; then
+if ! make test PYTHON=$(which python${PYTHON_VERSION}) TEST="--junitxml=test-reports/pytest/results.xml -vv -k 'not test_ModuleAnalyzer_for_module'"; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Install_success_but_test_Fails"
@@ -69,4 +68,4 @@ else
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
     exit 0
-fi 
+fi
