598f84c32a18fa5351abfd070dffeb0017235321
James Jenkins <James.Jenkins@ibm.com>
2025-06-12 11:12:25 -0400

    Remove unnecessary lines from htpasswd build script

--- h/htpasswd/htpasswd_ubi_9.3.sh
+++ h/htpasswd/htpasswd_ubi_9.3.sh
@@ -29,19 +29,6 @@ PATH=$PATH:/usr/local/bin/
 OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
 SOURCE=Github
 
-# Install rust
-if ! command -v rustc &> /dev/null
-then
-    wget https://static.rust-lang.org/dist/rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
-    tar -xzf rust-1.75.0-powerpc64le-unknown-linux-gnu.tar.gz
-    cd rust-1.75.0-powerpc64le-unknown-linux-gnu
-    sudo ./install.sh
-    export PATH=$HOME/.cargo/bin:$PATH
-    rustc -V
-    cargo -V
-    cd ../
-fi
-
 if [[ "$PACKAGE_URL" == *github.com* ]]; then
     # Use git clone if it's a Git repository
     if [ -d "$PACKAGE_DIR" ]; then
@@ -56,29 +43,6 @@ if [[ "$PACKAGE_URL" == *github.com* ]]; then
         cd "$PACKAGE_DIR" || exit
         git checkout master || exit
     fi
-else
-    # If it's not a Git repository, download and untar
-    if [ -d "$PACKAGE_DIR" ]; then
-        cd "$PACKAGE_DIR" || exit
-    else
-        # Use download and untar if it's not a Git repository
-        if ! curl -L "$PACKAGE_URL" -o "$PACKAGE_DIR.tar.gz"; then
-            echo "------------------$PACKAGE_URL:download_fails---------------------------------------"
-            echo "$PACKAGE_URL $PACKAGE_NAME"
-            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Download_Fails"  
-            exit 1
-        fi
-        mkdir "$PACKAGE_DIR"
-
-        if ! tar -xzf "$PACKAGE_DIR.tar.gz" -C "$PACKAGE_DIR" --strip-components=1; then
-            echo "------------------$PACKAGE_NAME:untar_fails---------------------------------------"
-            echo "$PACKAGE_URL $PACKAGE_NAME"
-            echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | $SOURCE | Fail | Untar_Fails"  
-            exit 1
-        fi
-
-        cd "$PACKAGE_DIR" || exit
-    fi
 fi
 
 # Install via pip3
@@ -90,19 +54,13 @@ if !  python3.11 -m pip install ./; then
 fi
 
 # build wheel
-python3.11 -m venv venv
-source venv/bin/activate
 python3.11 -m pip install wheel
 
 if ! python3.11 -m pip wheel --no-deps htpasswd==${PACKAGE_VERSION}; then
     echo "--------------------$PACKAGE_NAME:wheel_build_fails----------------------------------------"
-    exit 3
+    exit 2
 else
     echo "--------------------$PACKAGE_NAME:wheel_build_success----------------------------------------"
 fi
-
-# cleanup
-deactivate
-rm -rf venv
 
 exit 0
