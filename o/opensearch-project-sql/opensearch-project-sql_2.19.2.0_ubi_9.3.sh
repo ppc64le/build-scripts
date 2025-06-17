df10b7d13be65235a4e6b216cccf5b3d7729aa24
prachi-gaonkar <prachi.gaonkar@ibm.com>
2025-06-17 16:09:27 +0530

    Updated build script and patch for opensearch-project-sql 2.19.2.0

--- o/opensearch-project-sql/opensearch-project-sql_2.19.2.0_ubi_9.3.sh
+++ o/opensearch-project-sql/opensearch-project-sql_2.19.2.0_ubi_9.3.sh
@@ -18,101 +18,43 @@
 #
 # ---------------------------------------------------------------------------
 
+PACKAGE_NAME=sql
+PACKAGE_URL=https://github.com/opensearch-project/sql
+PACKAGE_VERSION=${1:-2.19.2.0}
+OPENSEARCH_URL=https://github.com/opensearch-project/OpenSearch.git
+OPENSEARCH_VERSION=2.19.2
+OPENSEARCH_PACKAGE=OpenSearch
+wdir=`pwd`
+SCRIPT=$(readlink -f $0)
+SCRIPT_DIR=$(dirname $SCRIPT)
 
-# ---------------------------
-# Check for root user
-# ---------------------------
-if ! ((${EUID:-0} || "$(id -u)")); then
-	set +ex
-        echo "FAIL: This script must be run as a non-root user with sudo permissions"
-        exit 3
-fi
+sudo yum install -y  git java-21-openjdk-devel
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
+export PATH=$PATH:$JAVA_HOME/bin
 
-# ---------------------------
-# Configuration
-# ---------------------------
-PACKAGE_NAME="sql"
-PACKAGE_ORG="opensearch-project"
-PACKAGE_VERSION="2.19.2.0"
-PACKAGE_URL="https://github.com/${PACKAGE_ORG}/${PACKAGE_NAME}.git"
-OPENSEARCH_BUILD_VERSION=2.19.2
-SCRIPT_PATH=$(dirname $(realpath $0))
-RUNTESTS=1
-BUILD_HOME="$(pwd)"
+cd $wdir
+git clone ${OPENSEARCH_URL}
+cd ${OPENSEARCH_PACKAGE} && git checkout ${OPENSEARCH_VERSION}
+git apply $SCRIPT_DIR/${OPENSEARCH_PACKAGE}_${OPENSEARCH_VERSION}.patch
+./gradlew -p distribution/archives/linux-ppc64le-tar assemble
 
+# Export Custom Distribution path
+export OPENSEARCH_DIST_PATH=file://${wdir}/OpenSearch/distribution/archives/linux-ppc64le-tar/build/distributions/opensearch-min-${OPENSEARCH_VERSION}-SNAPSHOT-linux-ppc64le.tar.gz
 
-# -------------------
-# Parse CLI Arguments
-# -------------------
-for i in "$@"; do
-  case $i in
-    --skip-tests)
-      RUNTESTS=0
-      echo "Skipping tests"
-      shift
-      ;;
-    -*|--*)
-      echo "Unknown option $i"
-      exit 3
-      ;;
-    *)
-      PACKAGE_VERSION=$i
-      echo "Building ${PACKAGE_NAME} ${PACKAGE_VERSION}"
-      ;;
-  esac
-done
-
-# ---------------------------
-# Dependency Installation
-# ---------------------------
-sudo yum install -y git wget sudo unzip make cmake gcc gcc-c++ perl python3-devel python3-pip java-17-openjdk-devel
-export JAVA_HOME=$(compgen -G '/usr/lib/jvm/java-17-openjdk-*')
-export JRE_HOME=${JAVA_HOME}/jre
-export PATH=${JAVA_HOME}/bin:$PATH
-
-# ----------------------------------------------
-# Build opensearch tarball for integation tests
-# ----------------------------------------------
-cd $BUILD_HOME
-if [ -z "$(ls -A $BUILD_HOME/opensearch-build)" ]; then
-	git clone https://github.com/opensearch-project/opensearch-build
-	cd opensearch-build
-	git checkout $OPENSEARCH_BUILD_VERSION
-	rm -rf $HOME/.pyenv
-	curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-	export PYENV_ROOT="$HOME/.pyenv"
-	python3 -m pip install pipenv
-	./build.sh manifests/$OPENSEARCH_BUILD_VERSION/opensearch-$OPENSEARCH_BUILD_VERSION.yml -s -c OpenSearch
-fi
-
-CUSTOM_OS_DIST=file://${BUILD_HOME}/opensearch-build/tar/builds/opensearch/dist/opensearch-min-${OPENSEARCH_BUILD_VERSION}-SNAPSHOT-linux-ppc64le.tar.gz
-# ---------------------------
-# Clone and Prepare Repository
-# ---------------------------
-cd "${BUILD_HOME}"
-git clone "${PACKAGE_URL}"
-cd "${PACKAGE_NAME}"
-git checkout "${PACKAGE_VERSION}"
+cd $wdir
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME && git checkout $PACKAGE_VERSION
 git apply  $SCRIPT_DIR/${PACKAGE_NAME}_${PACKAGE_VERSION}.patch
 
 # --------
 # Build
 # --------
 ret=0
-./gradlew build -x test -x integTest -x async-query:jacocoTestCoverageVerification  -PcustomDistributionUrl=${BUILD_HOME}/opensearch-build/tar/builds/opensearch/dist/opensearch-min-${OPENSEARCH_BUILD_VERSION}-SNAPSHOT-linux-ppc64le.tar.gz || ret=$?
+./gradlew build -x test -x integTest -x async-query:jacocoTestCoverageVerification || ret=$?
 if [ $ret -ne 0 ]; then
         set +ex
 	echo "------------------ ${PACKAGE_NAME}: Build Failed ------------------"
 	exit 1
-fi
-
-# ---------------------------
-# Skip Tests?
-# ---------------------------
-if [ "$RUNTESTS" -eq 0 ]; then
-        set +ex
-        echo "------------------ Complete: Build and install successful! Tests skipped. ------------------"
-        exit 0
 fi
 
 # ----------
@@ -131,12 +73,12 @@ fi
 # Integration Test
 # -----------------
 ret=0
-./gradlew integTest  -PcustomDistributionUrl=${BUILD_HOME}/opensearch-build/tar/builds/opensearch/dist/opensearch-min-${OPENSEARCH_BUILD_VERSION}-SNAPSHOT-linux-ppc64le.tar.gz -Dtests.heap.size=4096m || ret=$?
+./gradlew integTest -PcustomDistributionUrl=file://${wdir}/OpenSearch/distribution/archives/linux-ppc64le-tar/build/distributions/opensearch-min-${OPENSEARCH_VERSION}-SNAPSHOT-linux-ppc64le.tar.gz || ret=$?
 if [ $ret -ne 0 ]; then
 	set +ex
 	echo "------------------ ${PACKAGE_NAME}: Integration Test Failed ------------------"
 	exit 2
 fi
 
-echo "Skipping async-query:jacocoTestCoverageVerification in build as it is flaky and in parity with intel"
+echo "Skipping async-query:jacocoTestCoverageVerification in build as it is in parity with intel and is flaky"
 echo "Disabling testExtractDatePartWithTimeType in unit test as it is in parity with intel and is flaky"
