1bfc40e5eab55dbf940e4a716144c0cfb631df22
Harithanagothu2 <haritha.nagothu2@ibm.com>
2025-06-20 11:44:58 +0530

    Update python-memcached_ubi_9.3.sh

--- p/python_memcached/python-memcached_ubi_9.3.sh
+++ p/python_memcached/python-memcached_ubi_9.3.sh
@@ -47,7 +47,7 @@ pip install -r test-requirements.txt
 pip install pytest setuptools
 
 
-if ! python setup.py install; then
+if ! pip install .; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
