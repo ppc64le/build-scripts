afb2cb429c216a139ff88915bcf1909b72ac46fe
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2025-06-20 11:04:12 +0530

    Update pystan_ubi_9.3.sh

--- p/pystan/pystan_ubi_9.3.sh
+++ p/pystan/pystan_ubi_9.3.sh
@@ -24,72 +24,66 @@ PACKAGE_VERSION=${1:-3.10.0}
 PACKAGE_URL=https://github.com/stan-dev/pystan
 CURRENT_DIR=$(pwd)
 
-yum install -y git make wget gcc-toolset-13 openssl-devel python3 python3-devel python3-pip
+yum install -y git make wget gcc-toolset-13 openssl-devel python3.12 python3.12-pip python3.12-devel
 
 source /opt/rh/gcc-toolset-13/enable
 export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
 export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
-PYTHON_BIN=$(which python3)
-PIP_BIN=$(which pip3)
 
-# Clone and build CmdStan
-cd "$CURRENT_DIR"
+# Clone and build CmdStan (for stanc)
+cd $CURRENT_DIR
 git clone https://github.com/stan-dev/cmdstan
 cd cmdstan
 git checkout v2.34.1
 git submodule update --init --recursive
-make build -j"$(nproc)"
+make build -j$(nproc)
 export PATH=$CURRENT_DIR/cmdstan/bin:$PATH
 which stanc
 stanc --version
 
 # Clone and build httpstan
-cd "$CURRENT_DIR"
+cd $CURRENT_DIR
 git clone https://github.com/stan-dev/httpstan
 cd httpstan
-cp "$CURRENT_DIR/cmdstan/bin/stanc" "$CURRENT_DIR/httpstan/httpstan/stanc"
-chmod +x "$CURRENT_DIR/httpstan/httpstan/stanc"
+cp $CURRENT_DIR/cmdstan/bin/stanc $CURRENT_DIR/httpstan/httpstan/stanc
+chmod +x $CURRENT_DIR/httpstan/httpstan/stanc
+ln -sf $(ls -1 /usr/bin/python3.[0-9]* | grep -E '^/usr/bin/python3\.[0-9]+$' | sort -V | tail -n 1) /usr/bin/python3
 
-$PYTHON_BIN -m pip install --upgrade pip setuptools wheel pandas
-$PYTHON_BIN -m pip install poetry==1.7.1
+# Get Python include path
+PYTHON_INCLUDE=$(python3.12 -c "from sysconfig import get_paths; print(get_paths()['include'])")
+export CPLUS_INCLUDE_PATH=$PYTHON_INCLUDE:$CPLUS_INCLUDE_PATH
+export C_INCLUDE_PATH=$PYTHON_INCLUDE:$C_INCLUDE_PATH
 
+make 
+
+python3.12 -m pip install --upgrade pip setuptools wheel pandas
+python3.12 -m pip install poetry==1.7.1
 poetry export -f requirements.txt --without-hashes --dev -o requirements.txt
 
-PYTHON_INCLUDE="$($PYTHON_BIN -c 'import sysconfig; print(sysconfig.get_path("include"))')"
-PYTHON_PLATINCLUDE="$($PYTHON_BIN -c 'import sysconfig; print(sysconfig.get_path("platinclude"))')"
-PYTHON_CFLAGS="$($PYTHON_BIN -c 'import sysconfig; print(" ".join(sysconfig.get_config_vars("CFLAGS")))')"
-PYTHON_CCSHARED="$($PYTHON_BIN -c 'import sysconfig; print(" ".join(sysconfig.get_config_vars("CCSHARED")))')"
+python3.12 -m pip install -e .
 
-make \
-  PYTHON_INCLUDE="-I$PYTHON_INCLUDE" \
-  PYTHON_PLATINCLUDE="-I$PYTHON_PLATINCLUDE" \
-  PYTHON_CFLAGS="$PYTHON_CFLAGS" \
-  PYTHON_CCSHARED="$PYTHON_CCSHARED"
+cd $CURRENT_DIR
 
-$PYTHON_BIN -m pip install -e .
+# Clone the repository
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
 
-cd "$CURRENT_DIR"
-
-# Clone and install pystan
-git clone "$PACKAGE_URL"
-cd "$PACKAGE_NAME"
-git checkout "$PACKAGE_VERSION"
-
-$PYTHON_BIN -m pip install -r "$CURRENT_DIR/httpstan/requirements.txt"
+python3.12 -m pip install -r $CURRENT_DIR/httpstan/requirements.txt
 poetry build -v
 
-if ! $PIP_BIN install -e .; then
+if ! pip3.12 install -e . ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
-rm -f "$CURRENT_DIR/httpstan/httpstan/stanc"
-cp "$CURRENT_DIR/cmdstan/bin/stanc" "$CURRENT_DIR/httpstan/httpstan/stanc"
-chmod +x "$CURRENT_DIR/httpstan/httpstan/stanc"
-
-if ! pytest; then
+rm -f $CURRENT_DIR/httpstan/httpstan/stanc
+cp $CURRENT_DIR/cmdstan/bin/stanc $CURRENT_DIR/httpstan/httpstan/stanc
+chmod +x $CURRENT_DIR/httpstan/httpstan/stanc
+ 
+if ! pytest ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
