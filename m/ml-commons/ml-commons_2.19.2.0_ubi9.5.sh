25a95f1cd3e67b5d1572a790d280ba0ba2dc31a3
sumitd2 <91451282+sumitd2@users.noreply.github.com>
2025-06-24 18:52:08 +0530

    Update ml-commons_2.19.2.0_ubi9.5.sh

--- m/ml-commons/ml-commons_2.19.2.0_ubi9.5.sh
+++ m/ml-commons/ml-commons_2.19.2.0_ubi9.5.sh
@@ -5,7 +5,7 @@
 # Source repo    : https://github.com/opensearch-project/ml-commons
 # Tested on      : UBI 9.5
 # Language       : Java
-# Travis-check   : false
+# Travis-Check   : false
 # Maintainer     : Sumit Dubey <sumit.dubey2@ibm.com>
 # Script License : Apache License, Version 2.0 or later
 #
