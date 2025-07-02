b764640b6e8843ca1a2a4af40b8ffcfb8eaafd70
meetj15 <meet.jani@ibm.com>
2025-07-02 14:23:28 +0530

    Updated pyav_ubi_9.3.sh

--- p/pyav/pyav_ubi_9.3.sh
+++ p/pyav/pyav_ubi_9.3.sh
@@ -2,13 +2,13 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : PyAV
-# Version       : v13.1.0
+# Version       : v14.4.0
 # Source repo   : https://github.com/PyAV-Org/PyAV
 # Tested on     : UBI 9.3
 # Language      : c
 # Travis-Check  : True
 # Script License: Apache License 2.0
-# Maintainer    : Stuti Wali <Stuti.Wali@ibm.com>
+# Maintainer    : Shivansh Sharma <Shivansh.s1@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -18,10 +18,10 @@
 #
 # ----------------------------------------------------------------------------
 
-set -e 
+set -e
 
 PACKAGE_NAME=PyAV
-PACKAGE_VERSION=${1:-v13.1.0}
+PACKAGE_VERSION=${1:-v14.4.0}
 PACKAGE_URL=https://github.com/PyAV-Org/PyAV
 CURRENT_DIR=$(pwd)
 PACKAGE_DIR=PyAV
@@ -48,7 +48,7 @@ mkdir -p $INSTALL_ROOT
 
 for package in openblas lame opus libvpx ffmpeg pillow numpy; do
     mkdir -p ${INSTALL_ROOT}/${package}
-    export "${package^^}_PREFIX=${INSTALL_ROOT}/${package}"
+    export "${package^^}_PREFIX=${INSTALL_ROOT}/${package}" # convert package name to upper code ${package^^}
     echo "Exported ${package^^}_PREFIX=${INSTALL_ROOT}/${package}"
 done
 
@@ -200,8 +200,8 @@ USE_NONFREE=no   #the options below are set for NO
         --enable-libopus \
         --enable-libmp3lame \
         --enable-libvpx \
-        --extra-cflags="-I${LAME_PREFIX}/include -I${OPUS_PREFIX}/include -I${libvpx_PREFIX}/include" \
-        --extra-ldflags="-L${LAME_PREFIX}/lib -L${OPUS_PREFIX}/lib -L${libvpx_PREFIX}/lib" \
+        --extra-cflags="-I$LAME_PREFIX/include -I$OPUS_PREFIX/include -I$LIBVPX_PREFIX/include" \
+        --extra-ldflags="-L$LAME_PREFIX/lib -L$OPUS_PREFIX/lib -L$LIBVPX_PREFIX/lib" \
         --disable-encoder=h264 \
         --disable-decoder=h264 \
         --disable-decoder=libh264 \
@@ -242,7 +242,7 @@ USE_NONFREE=no   #the options below are set for NO
         --disable-decoder=h264_v4l2m2m \
         --disable-encoder=hevc_v4l2m2m \
         --disable-decoder=hevc_v4l2m2m \
-        --disable-nonfree --disable-gpl --disable-gnutls --enable-openssl --disable-libopenh264 --disable-libx264
+        --disable-nonfree --disable-gpl --disable-gnutls --enable-openssl --disable-libopenh264 --disable-libx264    #"${_CONFIG_OPTS[@]}"
 
 make -j$CPU_COUNT
 make install PREFIX="${FFMPEG_PREFIX}"
@@ -277,6 +277,9 @@ git submodule update --init
 export CFLAGS="${CFLAGS} -I/install-deps/ffmpeg/include"
 export LDFLAGS="${LDFLAGS} -L/install-deps/ffmpeg/lib"
 
+# Fix license field in pyproject.toml to comply with PEP 621
+sed -i 's/^license = "BSD-3-Clause"/license = { text = "BSD-3-Clause" }/' pyproject.toml
+
 #Build package
 if ! (python setup.py build_ext --inplace) ; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
@@ -293,7 +296,7 @@ python -m build --wheel --no-isolation --outdir="$CURRENT_DIR/"
 echo "----------------------------------------------Testing pkg-------------------------------------------------------"
 #Test package
 #Skipping few tests as they are failing because of disabling some codecs related to audio and video in ffmpeg. We disabled those codecs because of license associated with them.
-if ! (pytest -k "not test_encoding_dnxhd and not test_encoding_dvvideo and not test_encoding_h264 and not test_encoding_mjpeg and not test_encoding_mpeg1video and not test_encoding_mpeg4 and not test_encoding_png and not test_encoding_tiff and not test_encoding_xvid and not test_mov and not test_decode_video_corrupt and not test_decoded_motion_vectors and not test_decoded_motion_vectors_no_flag and not test_decoded_time_base and not test_decoded_video_frame_count and not test_flush_decoded_video_frame_count and not test_av_stream_Stream and not test_encoding_with_pts and not test_stream_audio_resample and not test_max_b_frames and not test_container_probing and not test_stream_probing and not test_writing_to_custom_io_dash and not test_decode_half and not test_stream_seek and not test_side_data and not test_opaque and not test_reformat_pixel_format_align and not test_filter_output_parameters and not test_codec_mpeg4_decoder and not test_codec_mpeg4_encoder and not test_codec_delay and not test_codec_tag and not test_decoder_extradata and not test_decoder_gop_size and not test_decoder_timebase and not test_encoder_extradata and not test_encoder_pix_fmt and not test_parse and not test_sky_timelapse and not test_av_codec_codec_Codec and not test_av_enum_EnumFlag and not test_av_enum_EnumItem and not test_default_options and not test_encoding and not test_encoding_with_unicode_filename and not test_stream_index and not test_writing_to_buffer and not test_writing_to_buffer_broken and not test_writing_to_buffer_broken_with_close and not test_writing_to_file and not test_writing_to_pipe_writeonly") ; then
+if ! (pytest -k "not test_encoding_dnxhd and not test_encoding_dvvideo and not test_encoding_h264 and not test_encoding_mjpeg and not test_encoding_mpeg1video and not test_encoding_mpeg4 and not test_encoding_png and not test_encoding_tiff and not test_encoding_xvid and not test_mov and not test_decode_video_corrupt and not test_decoded_motion_vectors and not test_decoded_motion_vectors_no_flag and not test_decoded_time_base and not test_decoded_video_frame_count and not test_flush_decoded_video_frame_count and not test_av_stream_Stream and not test_encoding_with_pts and not test_stream_audio_resample and not test_max_b_frames and not test_container_probing and not test_stream_probing and not test_writing_to_custom_io_dash and not test_decode_half and not test_stream_seek and not test_side_data and not test_opaque and not test_reformat_pixel_format_align and not test_filter_output_parameters and not test_codec_mpeg4_decoder and not test_codec_mpeg4_encoder and not test_codec_delay and not test_codec_tag and not test_decoder_extradata and not test_decoder_gop_size and not test_decoder_timebase and not test_encoder_extradata and not test_encoder_pix_fmt and not test_parse and not test_sky_timelapse and not test_av_codec_codec_Codec and not test_av_enum_EnumFlag and not test_av_enum_EnumItem and not test_default_options and not test_encoding and not test_encoding_with_unicode_filename and not test_stream_index and not test_writing_to_buffer and not test_writing_to_buffer_broken and not test_writing_to_buffer_broken_with_close and not test_writing_to_file and not test_writing_to_pipe_writeonly and not test_filter_output_parameters and not test_codec_mpeg4_decoder and not test_codec_mpeg4_encoder and not test_codec_delay and test_codec_tag and not test_sky_timelapse and not test_av_codec_codec_Codec and not test_writing_to_buffer and not test_writing_to_file and not test_writing_to_pipe_writeonly") ; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
