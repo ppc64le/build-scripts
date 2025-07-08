85269b6f0c54bad98213e983202aca02027c7a3d
Sumit Dubey <sumit.dubey2@ibm.com>
2025-07-08 07:17:45 -0400

    Travis check on

--- o/opensearch-project-opensearch-remote-metadata-sdk/opensearch-remote-metadata-sdk_2.19.2.0_ubi9.5.sh
+++ o/opensearch-project-opensearch-remote-metadata-sdk/opensearch-remote-metadata-sdk_2.19.2.0_ubi9.5.sh
@@ -5,7 +5,7 @@
 # Source repo    : https://github.com/opensearch-project/opensearch-remote-metadata-sdk
 # Tested on      : UBI 9.5
 # Language       : Java
-# Travis-Check   : false
+# Travis-Check   : true
 # Maintainer     : Sumit Dubey <sumit.dubey2@ibm.com>
 # Script License : Apache License, Version 2.0 or later
 #
