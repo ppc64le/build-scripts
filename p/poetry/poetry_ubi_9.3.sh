358f00ccbcb66ebf32a59800c2f8b83d10d5e0d7
rusiyamanya <Manya.Rusiya@ibm.com>
2025-07-08 17:51:42 +0530

    Update poetry_ubi_9.3.sh

--- p/poetry/poetry_ubi_9.3.sh
+++ p/poetry/poetry_ubi_9.3.sh
@@ -65,7 +65,7 @@ if ! python3 -m pip install .; then
     exit 1
 fi
 
-if ! (tox -e py3); then
+if ! (tox -e py39); then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
