bbd09437c1e0921789536309529150f017e26d55
rusiyamanya <Manya.Rusiya@ibm.com>
2025-07-09 10:40:16 +0530

    Currency: Modified build-script for Poetry version 1.8.5 (#7140)
    * Modified build script for poetry 1.8.5
    
    * Update poetry_ubi_9.3.sh

--- p/poetry/poetry_ubi_9.3.sh
+++ p/poetry/poetry_ubi_9.3.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package          : poetry
-# Version          : 1.8.3
+# Version          : 1.8.5
 # Source repo      : https://github.com/python-poetry/poetry.git
 # Tested on        : UBI:9.3
 # Language         : Python
@@ -20,7 +20,7 @@
 
 #variables
 PACKAGE_NAME=poetry
-PACKAGE_VERSION=${1:-1.8.3}
+PACKAGE_VERSION=${1:-1.8.5}
 PACKAGE_URL=https://github.com/python-poetry/poetry.git
 
 #install dependencies
@@ -55,6 +55,7 @@ fi
 # Install required Python packages
 pip install pytest==7.1.2 httpretty keyring
 pip install pytest-xdist mocker pytest-mock deepdiff
+pip install tox
 
 #install
 if ! python3 -m pip install .; then
@@ -64,7 +65,7 @@ if ! python3 -m pip install .; then
     exit 1
 fi
 
-if ! pytest; then
+if ! (tox -e py39); then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
