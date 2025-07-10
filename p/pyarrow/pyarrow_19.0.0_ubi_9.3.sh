9786d819346a6e11b79f84696894aa65892bbe86
Harithanagothu2 <haritha.nagothu2@ibm.com>
2025-07-10 11:10:58 +0530

    Update pyarrow_19.0.0_ubi_9.3.sh (#7153)

--- p/pyarrow/pyarrow_19.0.0_ubi_9.3.sh
+++ p/pyarrow/pyarrow_19.0.0_ubi_9.3.sh
@@ -59,7 +59,7 @@ make install
 cd $SCRIPT_DIR 
 
 echo "-------bison installing----------------------"
-wget https://ftp.gnu.org/gnu/bison/bison-3.8.2.tar.gz
+wget https://mirrors.cloud.tencent.com/gnu/bison/bison-3.8.2.tar.gz
 tar -xvf bison-3.8.2.tar.gz
 cd bison-3.8.2
 echo "Configuring bison installation..."
