098ccb531f444375dbd1c8ac6e0a11a45bbbe181
rusiyamanya <Manya.Rusiya@ibm.com>
2025-07-11 11:00:14 +0530

    Modified pandas_ubi_9.3.sh

--- p/pandas/pandas_ubi_9.3.sh
+++ p/pandas/pandas_ubi_9.3.sh
@@ -16,40 +16,36 @@
 #             package and/or distribution. In such case, please
 #             contact "Maintainer" of this script.
 #
-# ----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 PACKAGE_NAME=pandas
-PACKAGE_VERSION=${1:-v2.2.0}
-PYTHON_VERSION=${2:-3.11}
+PACKAGE_VERSION=${1:-v2.3.0}
 PACKAGE_URL=https://github.com/pandas-dev/pandas.git
 
-yum install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-devel python${PYTHON_VERSION}-pip git gcc gcc-c++ cmake ninja-build openblas-devel  gcc-gfortran
+yum install -y python3.12 python3.12-devel python3.12-pip git gcc gcc-c++ cmake ninja-build openblas-devel gcc-gfortran
 
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME/
 git checkout $PACKAGE_VERSION
 git submodule update --init --recursive
 
-# Setup virtual environment for python
-python${PYTHON_VERSION} -m venv pandas-env
-source pandas-env/bin/activate
+# Setup virtual environment for python3.12
+#python3.12 -m venv pandas-env
+#source pandas-env/bin/activate
 
-pip install --upgrade pip setuptools wheel
-pip install "numpy==2.0.2" "scipy>=1.8.0,<1.16.0"
-pip install cython meson-python ninja joblib threadpoolctl patchelf pytest build
-
+python3.12 -m pip install --upgrade pip setuptools wheel
+python3.12 -m pip install "numpy==2.0.2" "scipy>=1.8.0,<1.16.0"
+python3.12 -m pip install cython meson-python ninja joblib threadpoolctl patchelf pytest build
 
 # Optional install via setup (dev install)
-python${PYTHON_VERSION} -m pip install .
+python3.12 -m pip install .
 
 # Build the package and create whl file (This is dependent on cython)
-python${PYTHON_VERSION} -m build --wheel
-
+#python3.12 -m build --wheel
 
 # Test the package
 cd ..
-python${PYTHON_VERSION} -m pip show pandas
-python${PYTHON_VERSION} -c "import pandas; print(pandas.__file__)"
+python3.12 -c "import pandas; print(pandas.__file__)"
 
 if [ $? == 0 ]; then
      echo "------------------$PACKAGE_NAME::Test_Pass---------------------"
@@ -57,8 +53,7 @@ if [ $? == 0 ]; then
      echo "$PACKAGE_NAME  | $PACKAGE_URL | $PACKAGE_VERSION  | Pass |  Test_Success"
 
      # Deactivate python environment (pandas-env)
-         deactivate
-
+     deactivate
      exit 0
 else
      echo "------------------$PACKAGE_NAME::Test_Fail-------------------------"
