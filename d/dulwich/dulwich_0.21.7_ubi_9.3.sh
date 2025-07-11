dd533eb03900f46298183e95d4b42aebf44d0e76
sohambadjate-IBM <soham.badjate@ibm.com>
2025-07-11 19:04:38 +0530

    Update dulwich_0.21.7_ubi_9.3.sh

--- d/dulwich/dulwich_0.21.7_ubi_9.3.sh
+++ d/dulwich/dulwich_0.21.7_ubi_9.3.sh
@@ -24,21 +24,25 @@ PACKAGE_VERSION=${1:-0.21.7}
 PACKAGE_URL=https://github.com/jelmer/dulwich.git
 
 # Install necessary system packages
-yum install git python3 python3-devel python3-pip make cmake gcc -y
+yum install git python3.12 python3-devel python3.12-pip make cmake gcc -y
 
 # Upgrade pip and install build dependencies
-pip3 install --upgrade pip setuptools wheel setuptools_rust
 
 # cd to installation directory
 cd /root
 
+#creating symbolic link for python and pip
+# ln -sf /usr/bin/pip3.12 /usr/bin/pip3
+# ln -sf /usr/bin/python3.12 /usr/bin/python
+
+python3.12 -m pip install --upgrade pip setuptools wheel setuptools_rust
 # Clone the repository
 git clone ${PACKAGE_URL}
 cd ${PACKAGE_DIR}
 git checkout dulwich-${PACKAGE_VERSION}
 
 # Install build requirements
-pip3 install geventhttpclient==2.2.0 merge3
+pip install geventhttpclient==2.2.0 merge3 pytest
 
 #Build and install the package
 if ! pip3 install . ; then
