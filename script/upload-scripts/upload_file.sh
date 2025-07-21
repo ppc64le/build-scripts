b15b5a48ff3aa7dab657cf02468d26dbaf13da49
saiNammi <118737990+saiNammi@users.noreply.github.com>
2025-07-21 14:33:26 +0530

    Update upload_file.sh

--- script/upload-scripts/upload_file.sh
+++ script/upload-scripts/upload_file.sh
@@ -34,7 +34,7 @@ elif [ "$IS_SETUP" = "pen" ]; then
     token_request=$(curl -X POST https://iam.cloud.ibm.com/identity/token \
     -H "content-type: application/x-www-form-urlencoded" \
     -H "accept: application/json" \
-    -d "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey=$travis_currency_service_id_api_key_dev")
+    -d "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey=$travis_currency_service_id_api_key_pen")
 
     if [[ $(echo "$token_request" | jq -r '.errorCode') == "null" ]]; then
         token=$(echo "$token_request" | jq -r '.access_token')
