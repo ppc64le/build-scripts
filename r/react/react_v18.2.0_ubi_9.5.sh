34aa606bd39b56576b31570a8840aae167ee0de6
sanket-patil-11 <Sanket.Patil11@ibm.com>
2025-07-29 11:08:28 +0530

    Updated react v18.2.0 build script

--- r/react/react_v18.2.0_ubi_9.5.sh
+++ r/react/react_v18.2.0_ubi_9.5.sh
@@ -18,22 +18,21 @@
 #
 # ----------------------------------------------------------------------------
 
-
 WORK_DIR=$(pwd)
-REPO_URL=https://github.com/facebook/react
-PACKAGE_NAME=react
-MODULE_NAME=react
-MODULE_NAME2=react-test-renderer
-VERSION=${1:-v18.2.0}
-NODE_VERSION=v14.17.6
-  
+REPO_URL="https://github.com/facebook/react"
+PACKAGE_NAME="react"
+MODULE_NAME="react"
+MODULE_NAME2="react-test-renderer"
+VERSION="${1:-v18.2.0}"
+NODE_VERSION="v14.17.6"
+
+# Install Dependencies
 dnf install -y git curl zlib-devel autoconf automake libtool gcc gcc-c++ make python3 python3-devel glibc-devel libpng-devel --allowerasing
 
 export NVM_DIR="$HOME/.nvm"
 if [ ! -d "$NVM_DIR" ]; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 fi
-
 source "$NVM_DIR/nvm.sh"
 
 nvm install "$NODE_VERSION"
@@ -42,41 +41,46 @@ nvm alias default "$NODE_VERSION"
 
 export PATH="$NVM_DIR/versions/node/v$NODE_VERSION/bin:$PATH"
 
+# Clone Repository
 cd "$WORK_DIR"
 git clone "$REPO_URL"
 cd "$PACKAGE_NAME"
 git checkout "$VERSION"
 
+#Skipping 'electron' to build on ppc64le
 sed -i '/"electron":/d' packages/react-devtools/package.json
 
 npm install yarn -g
 
-# Build
-if ! CPPFLAGS='-DPNG_POWERPC_VSX_OPT=0' yarn install; then
+# Build the Package
+ret=0
+CPPFLAGS='-DPNG_POWERPC_VSX_OPT=0' yarn install || ret=$?
+if [ "$ret" -ne 0 ]; then
   echo "----${PACKAGE_NAME}: Build Fail----"
+  exit 1
 else
   echo "----${PACKAGE_NAME}: Build Success----"
 fi
 
-
-
-# Run tests
-# Run tests for react module
-echo "----Running tests for react----"
-if ! yarn test; then
+# Run tests for react
+echo "----Running tests for ${MODULE_NAME}----"
+yarn test || ret=$?
+if [ "$ret" -ne 0 ]; then
   echo "----${MODULE_NAME}: Test Fail----"
   exit 2
 else
   echo "----${MODULE_NAME}: Test Success----"
 fi
 
-# Run tests for react-test-renderer module
-echo "----Running tests for react-test-renderer----"
-if ! yarn test packages/react-test-renderer; then
+# Run tests for react-test-renderer
+echo "----Running tests for ${MODULE_NAME2}----"
+yarn test packages/${MODULE_NAME2} || ret=$?
+if [ "$ret" -ne 0 ]; then
   echo "----${MODULE_NAME2}: Test Fail----"
-  exit 3
+  exit 2
 else
   echo "----${MODULE_NAME2}: Test Success----"
 fi
 
-echo "PASS: react and react-test-renderer version $VERSION built and tested successfully."
+echo "PASS: ${MODULE_NAME} and ${MODULE_NAME2} version ${VERSION} built and tested successfully."
+exit 0
