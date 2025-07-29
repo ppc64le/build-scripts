34405442bb9985d8d62d4fd745e1705565e8f894
sanket-patil-11 <Sanket.Patil11@ibm.com>
2025-07-29 12:00:19 +0530

    Updated swagger-codegen v3.0.64 build script

--- s/swagger-codegen/swagger-codegen_v3.0.64_ubi_9.5.sh
+++ s/swagger-codegen/swagger-codegen_v3.0.64_ubi_9.5.sh
@@ -19,27 +19,39 @@
 # ----------------------------------------------------------------------------
 
 # Variables
-PACKAGE_NAME=swagger-codegen
-PACKAGE_VERSION=${1:-v3.0.64}
-PACKAGE_URL=https://github.com/swagger-api/swagger-codegen
+PACKAGE_NAME="swagger-codegen"
+PACKAGE_VERSION="${1:-v3.0.64}"
+PACKAGE_URL="https://github.com/swagger-api/swagger-codegen"
 
+# Install Dependencies
 yum install -y git wget gcc gcc-c++ java-11-openjdk java-11-openjdk-devel java-11-openjdk-headless maven
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
-git clone $PACKAGE_URL
-cd $PACKAGE_NAME/
-git checkout $PACKAGE_VERSION
+# Clone Repository
+git clone "$PACKAGE_URL"
+cd "$PACKAGE_NAME"
+git checkout "$PACKAGE_VERSION"
 
-if ! mvn clean install -DskipTests; then
-    echo "------------------$PACKAGE_NAME:Build_fails---------------------"
+# Build Package
+ret=0
+mvn clean install -DskipTests || ret=$?
+if [ "$ret" -ne 0 ]; then
+    echo "---- ${PACKAGE_NAME}: Build Failed ----"
+    exit 1
 else
-    echo "------------------$PACKAGE_NAME:Build_Success---------------------"
+    echo "---- ${PACKAGE_NAME}: Build Success ----"
 fi
 
-if ! mvn test; then
-    echo "------------------$PACKAGE_NAME:Test_fail---------------------"
+# Test
+mvn test || ret=$?
+if [ "$ret" -ne 0 ]; then
+    echo "---- ${PACKAGE_NAME}: Tests Failed ----"
+    exit 2
 else
-    echo "------------------$PACKAGE_NAME:Test_Success---------------------"
+    echo "---- ${PACKAGE_NAME}: Tests Passed ----"
 fi
+
+echo "PASS: ${PACKAGE_NAME} version ${PACKAGE_VERSION} built and tested successfully."
+exit 0
