ef960a3d66a2cc4dd5f62803c284f6151347bfcc
Sai Vikram Kuppala <sai.vikram.kuppala@ibm.com>
2025-08-02 00:20:56 +0530

    opentelemetry-cpp source commit

--- m/milvus-lite/milvus-lite_2.5.0_ubi_9.3.sh
+++ m/milvus-lite/milvus-lite_2.5.0_ubi_9.3.sh
@@ -1,158 +1,145 @@
 #!/bin/bash -e
 # -----------------------------------------------------------------------------
+# Combined build script for:
+#   1) opentelemetry-cpp v1.15.0
+#   2) milvus-lite v2.5.0
 #
-# Package       : milvus-lite
-# Version       : 2.5.0
-# Source repo   : https://github.com/milvus-io/milvus-lite
-# Tested on     : UBI:9.3
-# Language      : Python
-# Travis-Check  : True
-# Script License: Apache License, Version 2 or later
-# Maintainer    : Sai Vikram Kuppala <sai.vikram.kuppala@ibm.com>
-#
-# Disclaimer: This script has been tested in root mode on given
-# ========== platform using the mentioned version of the package.
-# It may not work as expected with newer versions of the
-# package and/or distribution. In such case, please
-# contact "Maintainer" of this script.
-#
-# ----------------------------------------------------------------------------
-SCRIPT_PATH=$(dirname $(realpath $0))
-SCRIPT_PACKAGE_VERSION=v2.5.0
-PACKAGE_NAME=milvus-lite
-PACKAGE_VERSION=${1:-${SCRIPT_PACKAGE_VERSION}}
-PACKAGE_URL="https://github.com/milvus-io/milvus-lite"
-PWDIR=$(pwd)
+# Maintainer : Sai Vikram Kuppala <sai.vikram.kuppala@ibm.com>
+# -----------------------------------------------------------------------------
 
-###############################################################################
-# 0. System prerequisites
-###############################################################################
-yum remove -y gcc-toolset-13 || true                # avoid profile conflicts
+PACKAGE_URL_OTEL=https://github.com/open-telemetry/opentelemetry-cpp
+OTEL_VERSION=${1:-v1.15.0}
 
-yum install -y \
-        wget perl git cargo gcc gcc-c++ libstdc++-static \
-        libaio libuuid-devel ncurses-devel libtool m4 autoconf automake \
-        ninja-build zlib-devel libffi-devel scl-utils \
-        openblas-devel ncurses-devel xz openssl-devel patch \
-        python3.12 python3.12-devel python3.12-pip opentelemetry-cpp-devel
+SCRIPT_PATH=$(dirname "$(realpath "$0")")
+MILVUS_VERSION=v2.5.0
+MILVUS_URL="https://github.com/milvus-io/milvus-lite"
 
-# Ensure python3 and pip3 refer to python3.12 explicitly
-ln -sf /usr/bin/python3.12 /usr/bin/python3
-ln -sf /usr/bin/pip3.12    /usr/bin/pip3
+# 0. Install system tools
+yum install -y git gcc-c++ gcc wget make python3 yum-utils \
+               apr-devel perl openssl-devel automake autoconf libtool cmake \
+               cargo libstdc++-static libaio libuuid-devel ncurses-devel \
+               libtool m4 autoconf automake ninja-build zlib-devel \
+               libffi-devel scl-utils openblas-devel xz patch \
+               python3-devel python3-pip
 
-###############################################################################
-# 1. Python build tools & Conan
-###############################################################################
-python3 -m pip install  --upgrade \
-        pip \
-        wheel \
-        "setuptools>=70.0.0" \
-        "conan==1.64.1"
+# Ensure python3 refers to the distro’s default (if needed)
+ln -sf /usr/bin/python3.12 /usr/bin/python3 || true
+ln -sf /usr/bin/pip3.12    /usr/bin/pip3   || true
 
-###############################################################################
-# 2. Build & install Texinfo
-###############################################################################
-    TEX_VER=7.1
-    wget "https://ftp.gnu.org/gnu/texinfo/texinfo-${TEX_VER}.tar.xz"
-    tar -xf "texinfo-${TEX_VER}.tar.xz"
-    pushd "texinfo-${TEX_VER}"
-        ./configure
-        make -j2
-        make install
-    popd
-    rm -rf "texinfo-${TEX_VER}"*
-
-###############################################################################
-# 3. Install Rust (for Arrow / Parquet deps used in Milvus-Lite)
-###############################################################################
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs \
-   | sh -s -- --default-toolchain stable -y
-source "$HOME/.cargo/env"
-rustc --version
-
-###############################################################################
-# 4. Build & register CMake 3.30.5 as a Conan application package
-###############################################################################
-CMAKE_VERSION=3.30.5
-mkdir -p "${PWDIR}/workspace"
-pushd "${PWDIR}/workspace"
-    if [[ ! -d "cmake-${CMAKE_VERSION}" ]]; then
-        wget -c "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz"
-        tar -xzf "cmake-${CMAKE_VERSION}.tar.gz"
-    fi
-    rm -rf cmake-${CMAKE_VERSION}.tar.gz
-    cd "cmake-${CMAKE_VERSION}"
-    ./bootstrap --prefix=/usr/local/cmake --parallel=2 \
-                -- -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_USE_OPENSSL=ON
-    make -j2
-    make install
+# -----------------------------------------------------------------------------
+# 1. Build & install googletest (gtest)
+# -----------------------------------------------------------------------------
+git clone https://github.com/google/googletest.git
+pushd googletest
+  git checkout v1.14.0
+  mkdir build && cd build
+  cmake .. && make -j$(nproc) && make install
 popd
-export PATH=/usr/local/cmake/bin:$PATH
-cmake --version
 
-# Package CMake into Conan so downstream C++ components can depend on a fixed version
-CMAKE_REQUIRED_VERSION=3.30.5
-create_cmake_conanfile()
-{
-    touch /usr/local/cmake/conanfile.py
-    cat <<EOT >> /usr/local/cmake/conanfile.py
-from conans import ConanFile, tools
-class CmakeConan(ConanFile):
-  name = "cmake"
-  package_type = "application"
-  version = "${CMAKE_REQUIRED_VERSION}"
-  description = "CMake, the cross-platform, open-source build system."
-  homepage = "https://github.com/Kitware/CMake"
-  license = "BSD-3-Clause"
-  topics = ("build", "installer")
-  settings = "os", "arch"
-  def package(self):
-    self.copy("*")
-  def package_info(self):
-    self.cpp_info.libs = tools.collect_libs(self)
-EOT
-}
-git clone "${PACKAGE_URL}"
-echo "<<<<<<<<<<<<<<<< clonning package directory >>>>>>>"
-pushd ${PACKAGE_NAME}
-    git checkout "${PACKAGE_VERSION}"
-    git submodule update --init --recursive
-   PATCH_FILE=${SCRIPT_PATH}/${PACKAGE_NAME}-${SCRIPT_PACKAGE_VERSION}.patch
-   #mv "$PATCH_FILE" /milvus-lite/thirdparty/
-   #cd thirdparty
-   #git apply -p1 $PATCH_FILE
-    #PATCH_FILE="/milvus-lite-v2.5.0.patch"
-    if [[ -f "${PATCH_FILE}" ]]; then
-       echo "patch file path is $PATCH_FILE"
-       #patch -p1 --forward < "${PATCH_FILE}"
-       patch -d thirdparty/milvus -p1 --forward < "$PATCH_FILE"
-       echo "<<<<<<<<<<<<<<<< patch applied properly >>>>>>>"
-    else
-       echo "Error: Patch file '${PATCH_FILE}' not found!"
-    fi
-    #git clone https://github.com/conan-io/conan-center-index.git
-    #cd conan-center-index/recipes/opentelemetry-proto/all
-    #conan export . opentelemetry-proto/1.3.2@
-    #cd ../../opentelemetry-cpp/all
-    #conan create . opentelemetry-cpp/1.14.2@ --build=missing
+# -----------------------------------------------------------------------------
+# 2. Build & install Google Benchmark
+# -----------------------------------------------------------------------------
+git clone https://github.com/google/benchmark.git
+pushd benchmark
+  cmake -E make_directory build
+  cmake -E chdir build \
+        cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on \
+              -DCMAKE_BUILD_TYPE=Release \
+              ..
+  cmake --build build --config Release -- -j$(nproc)
+  cmake -E chdir build ctest --build-config Release
+  cmake --build build --config Release --target install
 popd
-export VCPKG_FORCE_SYSTEM_BINARIES=1
-pushd /usr/local/cmake
-    create_cmake_conanfile
-    conan export-pkg . cmake/3.30.5@ -s os=Linux -s arch=$(uname -m) -f
+
+# -----------------------------------------------------------------------------
+# 3. Clone, build & install OpenTelemetry-CPP
+# -----------------------------------------------------------------------------
+git clone --recursive "$PACKAGE_URL_OTEL"
+pushd opentelemetry-cpp
+  git checkout "$OTEL_VERSION"
+  mkdir build && cd build
+  cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_TESTS=OFF
+  cmake --build . -- -j$(nproc)
+  cmake --install .    # installs into /usr/local by default
 popd
+
+# -----------------------------------------------------------------------------
+# 4. Set up Conan (for Milvus-Lite’s C++ deps)
+# -----------------------------------------------------------------------------
+python3 -m pip install --upgrade pip wheel setuptools conan==1.64.1
 conan profile update settings.compiler.libcxx=libstdc++11 default
 
-###############################################################################
-# 5. Clone & build Milvus-Lite Python package
-###############################################################################
-cd /milvus-lite
-# Build Python wheels for py 3.12
-pushd python
-   echo "<<<<<<<<<<<<<<<< Entering python directory >>>>>>>"
-    python3 -m pip install  -r requirements.txt
-    python3 -m pip install  build
-    python3 setup.py install
-    python3 -m build --wheel --no-isolation --outdir "${PWDIR}/"
+# -----------------------------------------------------------------------------
+# 5. Build & install Texinfo (required by milvus-lite)
+# -----------------------------------------------------------------------------
+TEX_VER=7.1
+wget "https://ftp.gnu.org/gnu/texinfo/texinfo-${TEX_VER}.tar.xz"
+tar -xf "texinfo-${TEX_VER}.tar.xz"
+pushd "texinfo-${TEX_VER}"
+  ./configure && make -j2 && make install
 popd
-echo "==========  Milvus-Lite v${PACKAGE_VERSION} built successfully for Python 3.12  =========="
+rm -rf "texinfo-${TEX_VER}"*
+
+# -----------------------------------------------------------------------------
+# 6. Build CMake 3.30.5 and register as a Conan package
+# -----------------------------------------------------------------------------
+CMAKE_VERSION=3.30.5
+mkdir -p workspace && pushd workspace
+  wget -c "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz"
+  tar -xzf "cmake-${CMAKE_VERSION}.tar.gz"
+  pushd "cmake-${CMAKE_VERSION}"
+    ./bootstrap --prefix=/usr/local/cmake \
+                -- -DBUILD_TESTING=OFF -DCMAKE_USE_OPENSSL=ON
+    make -j2 && make install
+  popd
+popd
+export PATH=/usr/local/cmake/bin:$PATH
+
+cat <<EOF > /usr/local/cmake/conanfile.py
+from conans import ConanFile, tools
+class CmakeConan(ConanFile):
+    name = "cmake"
+    version = "${CMAKE_VERSION}"
+    package_type = "application"
+    settings = "os", "arch"
+    def package(self):
+        self.copy("*")
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+EOF
+
+conan export-pkg /usr/local/cmake \
+                 cmake/${CMAKE_VERSION}@ \
+                 -s os=Linux \
+                 -s arch=$(uname -m) \
+                 -f
+
+# -----------------------------------------------------------------------------
+# 7. Clone, patch & build Milvus-Lite
+# -----------------------------------------------------------------------------
+git clone "$MILVUS_URL"
+pushd milvus-lite
+  git checkout "${MILVUS_VERSION}"
+  git submodule update --init --recursive
+
+  PATCH_FILE="${SCRIPT_PATH}/milvus-lite-${MILVUS_VERSION}.patch"
+  if [[ -f "$PATCH_FILE" ]]; then
+    echo "Applying patch: $PATCH_FILE"
+    patch -d thirdparty/milvus -p1 --forward < "$PATCH_FILE"
+  else
+    echo "Warning: patch not found at $PATCH_FILE"
+  fi
+
+  # Build Python wheel and install
+  pushd python
+    python3 -m pip install -r requirements.txt build
+    python3 setup.py install
+    python3 -m build --wheel --no-isolation --outdir "${PWD}/dist"
+  popd
+popd
+
+echo "==========  All builds & installs completed successfully!  =========="
+exit 0
