b004a6f85b1c50bfe6ac53f2b3a456f13e4af32d
Amit Kumar <amit.kumar282@ibm.com>
2025-08-11 22:02:47 +0530

    Added script

---
+++ j/jest/jest_v29.6.3_ubi_9.3.sh
@@ -0,0 +1,99 @@
+#!/bin/bash -e
+###############################################################################
+#
+# Package         : jest
+# Version         : v29.6.3
+# Source repo     : https://github.com/jestjs/jest
+# Language        : JavaScript
+# Tested on       : UBI 9.3 (ppc64le)
+# Travis-Check    : True
+# Maintainer      : Amit Kumar <amit.kumar282@ibm.com>
+# License         : Apache License, Version 2.0 or later
+#
+# Disclaimer: This script has been tested in root mode on the given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+###############################################################################
+
+# ------------------------------------------------------------------
+# Configuration & Initialization
+# ------------------------------------------------------------------
+PACKAGE_NAME="jest"
+PACKAGE_VERSION="${1:-v29.6.3}"
+PACKAGE_URL="https://github.com/jestjs/jest"
+
+BUILD_HOME="$(pwd)"
+NODE_VERSION="${NODE_VERSION:-20.14.0}"
+
+# ------------------------------------------------------------------
+# Install Core Dependencies
+# ------------------------------------------------------------------
+yum install -y python3 python3-devel.ppc64le git gcc gcc-c++ libffi make wget tar gzip
+
+# ------------------------------------------------------------------
+# Setup Node.js via NVM
+# ------------------------------------------------------------------
+export NVM_DIR="$HOME/.nvm"
+if [ ! -d "$NVM_DIR" ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+fi
+source "$NVM_DIR/nvm.sh"
+
+nvm install "$NODE_VERSION"
+nvm alias default "$NODE_VERSION"
+nvm use "$NODE_VERSION"
+
+export PATH="$NVM_DIR/versions/node/v$NODE_VERSION/bin:$PATH"
+export NODE_OPTIONS="--dns-result-order=ipv4first --tls-min-v1.2"
+
+# ------------------------------------------------------------------
+# Clone and Checkout Package
+# ------------------------------------------------------------------
+cd "$BUILD_HOME"
+git clone "$PACKAGE_URL"
+cd "$PACKAGE_NAME"
+git checkout "$PACKAGE_VERSION"
+
+# ------------------------------------------------------------------
+# Enable Corepack with Yarn 3.6.1 and add all required dependencies
+# ------------------------------------------------------------------
+corepack enable
+corepack prepare yarn@3.6.1 --activate
+yarn --version  # 3.6.1
+
+yarn add --dev @babel/preset-flow @babel/preset-typescript @babel/preset-env @babel/core babel-jest metro-react-native-babel-preset
+
+# ------------------------------------------------------------------
+# Install and Build
+# ------------------------------------------------------------------
+ret=0
+yarn install || ret=$?
+if [ "$ret" -ne 0 ]; then
+  echo "----${PACKAGE_NAME}: Install Failed----"
+  exit 1
+fi
+
+yarn build || ret=$?
+if [ "$ret" -ne 0 ]; then
+  echo "----${PACKAGE_NAME}: Build Failed----"
+  exit 1
+fi
+
+# ------------------------------------------------------------------
+# Run full test suite including babel-jest
+# ------------------------------------------------------------------
+npx jest --runInBand -u || ret=$?
+if [ "$ret" -ne 0 ]; then
+  echo "----${PACKAGE_NAME}: tests Failed----"
+  exit 2
+fi
+
+# ------------------------------------------------------------------
+# Smoke Test
+# ------------------------------------------------------------------
+JEST_VERSION=$(node packages/jest-cli/bin/jest.js --version | sed 's/-dev//')
+echo "PASS: ${PACKAGE_NAME} v${JEST_VERSION} built and tested successfully."
+exit 0
