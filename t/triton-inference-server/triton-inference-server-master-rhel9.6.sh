3ba0ab55723787c3839d1283193a40dc8a27c06e
Sunidhi-Gaonkar1 <sunidhi.gaonkar@ibm.com>
2025-08-11 13:40:18 +0530

    Updated triton script to add support to python onnx and pytorch backend.

--- t/triton-inference-server/triton-inference-server-master-rhel9.6.sh
+++ t/triton-inference-server/triton-inference-server-master-rhel9.6.sh
@@ -22,26 +22,24 @@
 
 wdir=`pwd`
 PACKAGE_NAME=server
-PACKAGE_VERSION=${1:-v2.59.0}
 PACKAGE_URL=https://github.com/triton-inference-server/server
 
 yum install git python3.12-devel python3.12-pip cmake -y
 
 ln /usr/bin/pip3.12 /usr/bin/pip3 -f && ln /usr/bin/python3.12 /usr/bin/python3 -f &&  ln /usr/bin/pip3.12 /usr/bin/pip -f
 
-python3.12 -m pip install distro requests
+pip install distro requests
 
-git clone $PACKAGE_URL -b $PACKAGE_VERSION
+git clone $PACKAGE_URL
 cd $PACKAGE_NAME
-git submodule update --init --recursive
-cp $wdir/onnxruntime_backend.patch .       
-git apply $wdir/triton_disable_test.patch  
+
+cp $wdir/onnxruntime_backend.patch .
+cp $wdir/pytorch_backend.patch .
 git apply $wdir/rhelppc.patch
 
-if ! ./build.py --enable-logging --endpoint http --backend onnxruntime --backend python --image base,registry.access.redhat.com/ubi9/ubi:9.6 ; then
+if ! ./build.py --enable-logging --endpoint http --backend onnxruntime --backend python --backend pytorch --image base,registry.access.redhat.com/ubi9/ubi:9.6 ; then
     echo "------------------$PACKAGE_NAME:Build_fails---------------------"
     exit 2
-    cd .. && rm -rf $PACKAGE_NAME
 else
     echo "------------------$PACKAGE_NAME:Build_success-------------------"
     exit 0
@@ -49,5 +47,4 @@ fi
 
 # Notes about patches:
 # onnxruntime_backend.patch: We are patching https://github.com/triton-inference-server/onnxruntime_backend/blob/main/tools/gen_ort_dockerfile.py, these changes are required to build onnxruntime_backend on RHEL.To remove this patch we will have to upstream changes.
-# triton_disable_test.patch: Patch to disable 1 test, issue is also observed on x86 as mentioned by third party user in https://github.com/triton-inference-server/server/issues/8202, can be removed once this issue is fixed.
 # rhelppc.patch: This patch is required to build triton server image on RHEL.We will have to upstream changes to remove this patch.
