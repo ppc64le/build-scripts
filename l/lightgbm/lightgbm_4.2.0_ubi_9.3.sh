d4f4c708de0b4923e7eb1136fedf28a608054eac
Vaibhav bhadade <vaibhav.bhadade@ibm.com>
2025-08-25 02:08:46 -0700

    Updated numpy to 1.23.5

--- l/lightgbm/lightgbm_4.2.0_ubi_9.3.sh
+++ l/lightgbm/lightgbm_4.2.0_ubi_9.3.sh
@@ -89,7 +89,7 @@ pip install pytest
 echo "installing cython.."
 pip install cython==0.29.36
 echo "installing numpy.."
-pip install numpy==1.23.2
+pip install numpy==1.23.5
 echo "installing scipy.."
 pip install scipy==1.13.1
 echo "installing joblib.."
@@ -135,7 +135,7 @@ echo "Installing Python dependencies..."
 cd ..
 pip install -r arrow/python/requirements-build.txt
 pip install -r arrow/python/requirements-test.txt
-pip install pytest==6.2.5 numpy==1.23.2
+pip install pytest==6.2.5 numpy==1.23.5
 pip install --upgrade setuptools wheel
 pip install hypothesis pytest-lazy-fixture pytz
 
