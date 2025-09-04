390811d1475139b788f466fad5e2b68ca5ee51a9
Pankhudi Jain <Pankhudi.Jain@ibm.com>
2025-09-04 13:55:48 +0530

    Updated build script for ml-metadata

--- m/ml-metadata/ml-metadata_ubi_9.6.sh
+++ m/ml-metadata/ml-metadata_ubi_9.6.sh
@@ -50,7 +50,7 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME/
 git apply $SCRIPT_DIR/ml_metadata_ubi9.6.patch
 
-export BAZEL_ARGS="--jobs=8"
+export BAZEL_ARGS="--jobs=6"
 export PYTHON_BIN_PATH=$(which python3)
 
 if ! (python3.9 -m pip install .); then 
