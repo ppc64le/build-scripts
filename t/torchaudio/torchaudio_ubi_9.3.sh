8faa1e95f2862ecb8847aa53a8157183d5b45e70
Nishidha Panpaliya <nishidha.panpaliya@partner.ibm.com>
2025-09-05 08:17:58 -0400

    Apply patch
    Signed-off-by: Nishidha Panpaliya <nishidha.panpaliya@partner.ibm.com>

--- t/torchaudio/torchaudio_ubi_9.3.sh
+++ t/torchaudio/torchaudio_ubi_9.3.sh
@@ -263,6 +263,10 @@ git checkout $PACKAGE_VERSION
 echo "------------------------Applying patch-------------------"
 wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/t/torchaudio/torchaudio_v2.6.0.patch
 git apply torchaudio_v2.6.0.patch
+
+# Below patch excludes the source files that has commercial license
+git apply 0001-Excluded-source-that-has-commercial-license.patch
+
 echo "-----------------------Applied patch successfully---------------------------------------"
 
 SRC_DIR=$(pwd)
