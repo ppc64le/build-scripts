3e39e3ce9be90f9ac0b136f1dc1d0527a568bd7b
Pankhudi Jain <Pankhudi.Jain@ibm.com>
2025-09-05 15:03:53 +0530

    Updated build script for ml-metadata

--- m/ml-metadata/ml-metadata_ubi_9.6.sh
+++ m/ml-metadata/ml-metadata_ubi_9.6.sh
@@ -49,6 +49,8 @@ python3.9 -m pip install numpy pytest
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME/
 git apply $SCRIPT_DIR/ml_metadata_ubi9.6.patch
+
+export BAZEL_ARGS="--jobs=2 --local_cpu_resources=2 --local_ram_resources=2048 --host_jvm_args=-Xms512m"
 export PYTHON_BIN_PATH=$(which python3)
 
 if ! (python3.9 -m pip install .); then 
