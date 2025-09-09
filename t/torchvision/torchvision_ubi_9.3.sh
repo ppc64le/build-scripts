74606093bd963357b0b5e4723bc0f977bb884fc4
Nishidha Panpaliya <nishidha.panpaliya@partner.ibm.com>
2025-09-09 11:43:11 +0530

    Apply patch (#7371)
    * Apply patch
    
    Signed-off-by: Nishidha Panpaliya <nishidha.panpaliya@partner.ibm.com>
    
    * Update torchvision_ubi_9.3.sh
    
    * Update torchvision_ubi_9.3.sh
    
    ---------
    
    Signed-off-by: Nishidha Panpaliya <nishidha.panpaliya@partner.ibm.com>

--- t/torchvision/torchvision_ubi_9.3.sh
+++ t/torchvision/torchvision_ubi_9.3.sh
@@ -562,6 +562,11 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/t/torchvision/0001-Exclude-source-that-has-commercial-license.patch
+
+# Below patch is needed to exclude the models that come under SWAG license (CC-BY-NC-4.0)
+git apply ./0001-Exclude-source-that-has-commercial-license.patch
+
 sed -i '/elif sha != "Unknown":/,+1d' setup.py
 
 if ! python3 setup.py bdist_wheel --dist-dir $CURRENT_DIR; then
