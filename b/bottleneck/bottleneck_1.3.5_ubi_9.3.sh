46dc8fde0117c8b05d1957edf90c753fa4633edb
Shivansh S <shivansh.s1@ibm.com>
2025-09-10 10:51:46 +0530

    updated build script to build bottlencek==1.6.0 latest version

--- b/bottleneck/bottleneck_1.3.5_ubi_9.3.sh
+++ b/bottleneck/bottleneck_1.3.5_ubi_9.3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 # ----------------------------------------------------------------------------
-# 
+#
 # Package       : bottleneck
-# Version       : v1.3.5
+# Version       : v1.6.0
 # Source repo   : https://github.com/pydata/bottleneck.git
 # Tested on     : UBI:9.3
 # Language      : Python
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Haritha Nagothu <haritha.nagothu2@ibm.com>
+# Maintainer    : Shivansh Sharma <shivansh.s1@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -20,31 +20,33 @@
 
 #variables
 PACKAGE_NAME=bottleneck
-PACKAGE_VERSION=${1:-v1.3.5}
+PACKAGE_VERSION=${1:-v1.6.0}
 PACKAGE_URL=https://github.com/pydata/bottleneck.git
-    
+
 # Install dependencies and tools.
-yum install -y wget gcc gcc-c++ gcc-gfortran git make  python-devel  openssl-devel 
-    
-#clone repository 
+yum install -y wget gcc gcc-c++ gcc-gfortran git make  python3.12 python3.12-pip python3.12-devel  openssl-devel
+
+#clone repository
 git clone $PACKAGE_URL
 cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
-    
+
 #install
-if ! (pip install .) ; then
+if ! (python3.12 -m pip install .) ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
-    
-pip install chardet --upgrade
-pip install requests --upgrade
-pip install tox
-    
-#test
-if ! tox -e py39; then
+
+python3.12 -m pip install chardet --upgrade
+python3.12 -m pip install requests --upgrade
+python3.12 -m pip install tox
+python3.12 -m pip install pytest hypothesis
+
+# test
+cd ..
+if ! python3.12 -c "import bottleneck; bottleneck.test()"; then
     echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
