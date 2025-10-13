f6b4c320afc54234745d18dba84232caecde0193
Stuti Ravikiran Wali <Stuti.Wali@ibm.com>
2025-10-13 14:01:07 +0530

    Workflow updation (#7453)
    * Update currency-build.yaml related upload to COS
    
    * Update currency-build.yaml
    
    * Update currency-build.yaml
    
    * Update upload_file.sh
    
    * Update upload_wheel.sh
    
    * Update upload_docker_image.sh
    
    * Update upload_file.sh

--- gha-script/upload-scripts/upload_docker_image.sh
+++ gha-script/upload-scripts/upload_docker_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-echo "$travis_currency_service_id_api_key" | docker login -u iamapikey --password-stdin icr.io
+echo "$gha_currency_service_id_api_key" | docker login -u iamapikey --password-stdin icr.io
 if [ $? -ne 0 ]; then
     echo "Docker login failed. Exiting script."
     exit 1
--- gha-script/upload-scripts/upload_wheel.sh
+++ gha-script/upload-scripts/upload_wheel.sh
@@ -3,7 +3,7 @@
 token_request=$(curl -X POST https://iam.cloud.ibm.com/identity/token \
   -H "content-type: application/x-www-form-urlencoded" \
   -H "accept: application/json" \
-  -d "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey=$currency_ecosystem_dev_service_api_key")
+  -d "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey=$gha_currency_service_id_api_key")
 
 #token=$(echo "$token_request" | jq -r '.access_token')
 #curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: application/gzip" -T $1 "https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket/$PACKAGE_NAME/$VERSION/$1"
