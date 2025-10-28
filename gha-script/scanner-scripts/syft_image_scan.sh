9a81741b988f805d4def83b6828af9141a440f21
Bharat-Tatti <bharat.tatti@ibm.com>
2025-10-28 17:04:20 +0530

    Changed script

--- gha-script/scanner-scripts/syft_image_scan.sh
+++ gha-script/scanner-scripts/syft_image_scan.sh
@@ -11,13 +11,14 @@ echo "DEBUG: VERSION=$VERSION"
 echo "DEBUG: image_name=$image_name"
 
 echo "$GHA_CURRENCY_SERVICE_ID_API_KEY" | docker login -u iamapikey --password-stdin icr.io
+cat ~/.docker/config.json
 docker pull "$image_name"
-if [ $build_docker == true ];then
-        SYFT_VERSION=$(curl -s https://api.github.com/repos/anchore/syft/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
-        wget https://github.com/anchore/syft/releases/download/$SYFT_VERSION/syft_${SYFT_VERSION#v}_linux_ppc64le.tar.gz
-        tar -xzf syft_${SYFT_VERSION#v}_linux_ppc64le.tar.gz
-        chmod +x syft
-        sudo mv syft /usr/bin  
-        echo "Executing syft scanner"
-        sudo syft -q -s AllLayers -o cyclonedx-json ${image_name} > syft_image_sbom_results.json
-fi
+# if [ $build_docker == true ];then
+#         SYFT_VERSION=$(curl -s https://api.github.com/repos/anchore/syft/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+#         wget https://github.com/anchore/syft/releases/download/$SYFT_VERSION/syft_${SYFT_VERSION#v}_linux_ppc64le.tar.gz
+#         tar -xzf syft_${SYFT_VERSION#v}_linux_ppc64le.tar.gz
+#         chmod +x syft
+#         sudo mv syft /usr/bin  
+#         echo "Executing syft scanner"
+#         sudo syft -q -s AllLayers -o cyclonedx-json ${image_name} > syft_image_sbom_results.json
+# fi
