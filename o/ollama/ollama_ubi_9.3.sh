35de53c491452105af75fd6a125c1cc88673de66
shalinib-ibm <Shalini.Salomi.Bodapati@ibm.com>
2025-11-06 12:55:08 +0530

    Update ollama_ubi_9.3.sh

--- o/ollama/ollama_ubi_9.3.sh
+++ o/ollama/ollama_ubi_9.3.sh
@@ -87,6 +87,8 @@ wget -q -O ../build_power.patch https://github.com/ollama/ollama/pull/12070/comm
 wget -q -O ../set_threads_env.patch https://github.com/ollama/ollama/pull/11413/commits/1364a887a1d7c25522e9c921d55e50a6aea44964.patch
 wget -q -O ../enable_mma.patch https://github.com/ollama/ollama/pull/12085/commits/fe924304809ae8e6bd6957b0ce5759eb2a796d42.patch
 
+echo "** Downloaded Patches..."
+
 patch -p1 < ../build_power.patch
 patch -p1 < ../set_threads_env.patch
 patch -p1 < ../enable_mma.patch
