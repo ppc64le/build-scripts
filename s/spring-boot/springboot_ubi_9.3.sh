9858978b3e012297d8259b6b836dfa71a874e966
karanam-santhosh <karanam.santhosh@ibm.com>
2025-12-22 12:33:04 +0530

    Update springboot_ubi_9.3.sh

--- s/spring-boot/springboot_ubi_9.3.sh
+++ s/spring-boot/springboot_ubi_9.3.sh
@@ -2,13 +2,13 @@
 # ----------------------------------------------------------------------------
 #
 # Package       : spring-boot
-# Version       : v3.3.3 
+# Version       : v4.0.0 
 # Source repo   : https://github.com/spring-projects/spring-boot
 # Tested on	: UBI 9.3
 # Language      : Java
 # Ci-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer	: Siddesh Sangodkar <siddesh.sangodkar1@ibm.com>
+# Maintainer	: Karanam Santhosh <Karanam.Santhosh@ibm.com>
 
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
@@ -22,27 +22,27 @@ set -e
 
 PACKAGE_NAME=spring-boot
 PACKAGE_URL=https://github.com/spring-projects/spring-boot
-PACKAGE_VERSION=${1:-v3.3.3}
+PACKAGE_VERSION=${1:-v4.0.0}
 
 yum install git wget -y
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-yum install java-17-openjdk java-17-openjdk-devel -y
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+yum install java-25-openjdk java-25-openjdk-devel -y
+export JAVA_HOME=/usr/lib/jvm/java-25-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
 
 
-if ! ./gradlew build ; then
+if ! ./gradlew build -x :documentation:spring-boot-docs:checkArchitectureMain; then
     echo "------------------$PACKAGE_NAME:Build_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
     exit 1
 fi
 
-if ! ./gradlew test ; then
+if ! ./gradlew test -x :documentation:spring-boot-docs:checkArchitectureMain  ; then
     echo "------------------$PACKAGE_NAME::Build_and_Test_fails-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail|  Build_and_Test_fails"
