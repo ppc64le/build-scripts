3d2d263ed6f91b09ca9b93a8a7eacddb2854846a
Aastha Sharma <aastha.sharma4@ibm.com>
2025-12-22 17:18:40 +0530

    Update pymssql_2.3.8_ubi_9.6.sh

--- p/pymssql/pymssql_2.3.8_ubi_9.6.sh
+++ p/pymssql/pymssql_2.3.8_ubi_9.6.sh
@@ -8,7 +8,7 @@
 # Language      : Python
 # Ci-Check      : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Sakshi Jain <sakshi.jain16@ibm.com>
+# Maintainer    : Aastha Sharma <Aastha.Sharma4@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
