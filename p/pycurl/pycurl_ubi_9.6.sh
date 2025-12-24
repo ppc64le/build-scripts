31c50eae3d3112d9fcb73c73417eb2af43bd06ff
Aastha Sharma <aastha.sharma4@ibm.com>
2025-12-24 16:42:04 +0530

    Update pycurl_ubi_9.6.sh

--- p/pycurl/pycurl_ubi_9.6.sh
+++ p/pycurl/pycurl_ubi_9.6.sh
@@ -6,7 +6,7 @@
 # Source repo      : https://github.com/pycurl/pycurl
 # Tested on        : UBI:9.6
 # Language         : Python
-# Travis-Check     : True
+# Ci-Check         : True
 # Script License   : Apache License, Version 2 or later
 # Maintainer    : ICH <OpenSource-Edge-for-IBM-Tool-1>
 #
