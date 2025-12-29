a6b2565b91dca40f8d9d594763bdcc38d1cf0db1
shreya kajbaje <51292172+shreyakajbaje@users.noreply.github.com>
2025-12-29 11:53:45 +0530

    Update quarkus_ubi_9.3.sh

--- q/quarkus/quarkus_ubi_9.3.sh
+++ q/quarkus/quarkus_ubi_9.3.sh
@@ -6,7 +6,7 @@
 # Source repo	: https://github.com/quarkusio/quarkus
 # Tested on	: UBI 9
 # Language      : java
-# Travis-Check  : True
+# Ci-Check     : True
 # Script License: Apache License, Version 2 or later
 # Maintainer	: Shreya Kajbaje <Shreya.Kajbaje@ibm.com>
 #
