9c29808dfef45cc1a1a842fcceef864da861e48b
Pratik Tonage <Pratik.Tonage@ibm.com>
2026-01-01 15:26:47 +0530

    Update prettier_2.8.8_ubi_9.5.sh

--- p/prettier/prettier_2.8.8_ubi_9.5.sh
+++ p/prettier/prettier_2.8.8_ubi_9.5.sh
@@ -6,7 +6,7 @@
 # Source repo   : https://github.com/prettier/prettier
 # Tested on     : UBI:9.5
 # Language      : JavaScript
-# Travis-Check  : True
+# Ci-Check      : True
 # Script License: Apache License Version 2.0
 # Maintainer    : Pratik Tonage <Pratik.Tonage@ibm.com>
 #
