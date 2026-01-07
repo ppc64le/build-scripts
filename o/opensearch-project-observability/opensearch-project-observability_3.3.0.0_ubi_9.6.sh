4df650b2987b7ebf28f314b8c76096a6e40ac81a
rusiyamanya <Manya.Rusiya@ibm.com>
2026-01-07 14:35:23 +0530

    observability_3.3.0.0 file add

---
+++ o/opensearch-project-observability/opensearch-project-observability_3.3.0.0_ubi_9.6.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : observability
+# Version          : 3.3.0.0
+# Source repo      : https://github.com/opensearch-project/observability
+# Tested on        : UBI:9.6
+# Language         : Java
+# Ci-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Manya Rusiya <Manya.Rusiya@ibm.com>
+#
+# Disclaimer       : This script has been tested in non-root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ---------------------------------------------------------------------------
+
+PACKAGE_NAME=observability
+PACKAGE_URL=https://github.com/opensearch-project/observability
+PACKAGE_VERSION=${1:-3.3.0.0}
+BUILD_HOME="$(pwd)"
+SCRIPT=$(readlink -f $0)
+SCRIPT_DIR=$(dirname $SCRIPT)
+
+sudo yum install -y git java-21-openjdk-devel
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
+export PATH=$PATH:$JAVA_HOME/bin
+
+# ------------------------------
+# Build Opensearch common-utils
+# ------------------------------
+cd $BUILD_HOME
+git clone https://github.com/opensearch-project/common-utils.git
+cd common-utils
+git checkout $PACKAGE_VERSION
+./gradlew assemble
+./gradlew -Prelease=true publishToMavenLocal
+
+cd $BUILD_HOME
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+git apply $SCRIPT_DIR/observability_3.3.0.0.patch
+
+if ! ./gradlew assemble ; then
+    echo "------------------$PACKAGE_NAME:Build_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail |  Build_Fails"
+    exit 1
+elif ! ./gradlew test; then
+    echo "------------------$PACKAGE_NAME::Test_fails-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail|  Test_fails"
+    exit 2
+else
+    # If both the build and test are successful, print the success message
+    echo "------------------$PACKAGE_NAME:: Build_and_Test_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Build_and_Test_Success"
+    exit 0
+fi
