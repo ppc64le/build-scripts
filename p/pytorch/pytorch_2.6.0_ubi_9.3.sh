14a33d814e9218caa5b64cb05e05ccb6b7a9b3bf
Vipul Ajmera <Vipul.Ajmera@ibm.com>
2026-01-19 18:39:14 +0530

    Update pytorch_2.6.0_ubi_9.3.sh

--- p/pytorch/pytorch_2.6.0_ubi_9.3.sh
+++ p/pytorch/pytorch_2.6.0_ubi_9.3.sh
@@ -254,8 +254,8 @@ export LD_LIBRARY_PATH="/protobuf/local/libprotobuf/lib64:${LD_LIBRARY_PATH}"
 export LD_LIBRARY_PATH="/protobuf/third_party/abseil-cpp/local/abseilcpp/lib:${LD_LIBRARY_PATH}"
 export CXXFLAGS="${CXXFLAGS} -mcpu=${cpu_opt_arch} -mtune=${cpu_opt_tune}"
 export CFLAGS="${CFLAGS} -mcpu=${cpu_opt_arch} -mtune=${cpu_opt_tune}"
-export LD_LIBRARY_PATH=":${SCRIPT_DIR}/pytorch/torch/lib/:${LD_LIBRARY_PATH}"
-export LD_LIBRARY_PATH=":${SCRIPT_DIR}/protobuf/local/libprotobuf/lib64/:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH="${SCRIPT_DIR}/pytorch/torch/lib/:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${SCRIPT_DIR}/protobuf/local/libprotobuf/lib64/:${LD_LIBRARY_PATH}"
 
 echo "required env variables got set"
 
