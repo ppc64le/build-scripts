115e53402606eeb97d7cac2194598d47d9a4091f
Aastha Sharma <aastha.sharma4@ibm.com>
2026-01-20 17:03:59 +0530

    OSE: updated build_script for Frozenlist (#7736)
    * Update and rename frozenlist_ubi_9.3.sh to frozenlist_ubi_9.6.sh
    
    * Update build_info.json
    
    * Update frozenlist_ubi_9.6.sh

--- f/frozenlist/frozenlist_ubi_9.6.sh
+++ f/frozenlist/frozenlist_ubi_9.6.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 # ----------------------------------------------------------------------------
-# 
+#
 # Package       : frozenlist
 # Version       : v1.3.3
 # Source repo   : https://github.com/aio-libs/frozenlist.git
-# Tested on     : UBI:9.3
+# Tested on     : UBI:9.6
 # Language      : Python
-# Ci-Check  : True
+# Ci-Check      : True
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Haritha Nagothu <haritha.nagothu2@ibm.com>
 #
@@ -18,34 +18,40 @@
 #
 # ----------------------------------------------------------------------------
 
+set -ex
 #variables
 PACKAGE_NAME=frozenlist
 PACKAGE_VERSION=${1:-v1.3.3}
 PACKAGE_URL=https://github.com/aio-libs/frozenlist.git
+PACKAGE_DIR=frozenlist
 
 # Install dependencies and tools.
-yum install -y wget gcc gcc-c++ gcc-gfortran git make  python-devel  openssl-devel
+yum install -y wget gcc-toolset-13 gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc-gfortran git make python3-devel python3-pip openssl-devel
 
-#clone repository 
+export PATH=$PATH:/usr/local/bin/
+export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
+export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
+
+#clone repository
 git clone $PACKAGE_URL
 cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 #install cython,pytest-cov
-pip install cython pytest-cov
+pip3 install cython pytest-cov
 
 cython frozenlist/_frozenlist.pyx
 
 #install
-if ! (python3 setup.py install) ; then
+if ! (pip3 install .) ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
- 
+
 #test
-if ! pytest --cov; then
+if ! pytest -p no:pytest_cov -o addopts=""; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
