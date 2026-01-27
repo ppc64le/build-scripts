6b79d6f4230197edbbdf4a604692f192feb0fa2a
Rushikesh Sathe <Rushikesh.Sathe1@ibm.com>
2026-01-27 20:36:39 +0530

    Updated xformers script (#7789)

--- x/xformers/xformers_ubi_9_3.sh
+++ x/xformers/xformers_ubi_9_3.sh
@@ -55,7 +55,7 @@ pip3 install --upgrade pip setuptools wheel
 pip3 install ninja 'cmake<4' 'pytest==8.2.2' hydra-core
 
 # Install dependency - pytorch
-PYTORCH_VERSION=v2.5.1
+PYTORCH_VERSION=v2.7.1
 cd $CURRENT_DIR
 
 git clone https://github.com/pytorch/pytorch.git
@@ -85,7 +85,7 @@ MAX_JOBS=$PARALLEL python3 setup.py install
 cd $CURRENT_DIR/$PACKAGE_NAME
 
 # Build and install xformers
-if ! pip3 install . -vvv; then
+if ! pip3 install . --no-build-isolation -vvv; then
     echo "------------------$PACKAGE_NAME:Build_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail |  Build_fails"
