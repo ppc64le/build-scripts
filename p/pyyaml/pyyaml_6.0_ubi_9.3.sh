fbf2067dd34f7d3f01b314eddbc2ec9e0a58cdc5
Stuti Ravikiran Wali <Stuti.Wali@ibm.com>
2026-01-28 17:22:33 +0530

    Add pytest to installation and update test command

--- p/pyyaml/pyyaml_6.0_ubi_9.3.sh
+++ p/pyyaml/pyyaml_6.0_ubi_9.3.sh
@@ -25,7 +25,7 @@ PACKAGE_URL=https://github.com/yaml/pyyaml.git
 yum install -y git python3 python3-devel.ppc64le libyaml-devel gcc gcc-c++
 
 pip3 install --upgrade pip
-pip3 install "cython<3.0.0" wheel
+pip3 install "cython<3.0.0" wheel pytest
 PATH=$PATH:/usr/local/bin/
 
 git clone $PACKAGE_URL $PACKAGE_NAME
@@ -39,7 +39,7 @@ if ! python3 setup.py install ; then
     exit 1
 fi
 
-if ! python3 setup.py test ; then
+if ! pytest ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
