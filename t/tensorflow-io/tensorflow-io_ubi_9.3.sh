1693621ee1d2398342627cf9f019da4ab97c5968
Pooja Shah <Pooja.Shah4@ibm.com>
2026-02-03 00:42:43 +0530

    Updated installation command in tensorflow-io_ubi_9.3.sh

--- t/tensorflow-io/tensorflow-io_ubi_9.3.sh
+++ t/tensorflow-io/tensorflow-io_ubi_9.3.sh
@@ -80,7 +80,6 @@ git clone https://github.com/HDFGroup/hdf5
 cd hdf5/
 git checkout hdf5-1_12_1
 git submodule update --init
-yum install -y zlib zlib-devel
 ./configure --prefix=/usr/local/hdf5 --enable-cxx --enable-fortran  --with-pthread=yes --enable-threadsafe  --enable-build-mode=production --enable-unsupported  --enable-using-memchecker  --enable-clear-file-buffers --with-ssl
 make 
 make install
@@ -150,7 +149,6 @@ echo "-----------------------------------------------------Installed openblas---
 
 #installing patchelf from source
 cd $CURRENT_DIR
-yum install -y git autoconf automake libtool make
 git clone https://github.com/NixOS/patchelf.git
 cd patchelf
 ./bootstrap.sh
@@ -163,9 +161,9 @@ echo "-----------------------------------------------------Installed patchelf---
 
 #installing patchelf from source
 cd $CURRENT_DIR
+yum install -y krb5-devel
 git clone https://github.com/alisw/libtirpc
 cd libtirpc
-yum install -y krb5-devel
 ./bootstrap
 ./configure --prefix=/usr/local
 make -j$(nproc)
@@ -198,7 +196,28 @@ env EXTRA_BAZEL_ARGS="--tool_java_runtime_version=local_jdk" bash ./compile.sh
 cp output/bazel /usr/local/bin
 export PATH=/usr/local/bin:$PATH
 bazel --version
+
+#installing dm-tree
 cd $CURRENT_DIR
+yum install -y make libtool cmake git wget xz zlib-devel openssl-devel bzip2-devel libffi-devel libevent-devel libjpeg-turbo-devel
+
+export PATH=/opt/rh/gcc-toolset-12/root/usr/bin:$PATH
+
+git clone https://github.com/deepmind/tree
+cd tree
+git checkout 0.1.8
+
+python3.11 -m pip install --upgrade pip setuptools wheel
+
+# install scikit-learn dependencies and build dependencies
+python3.11 -m pip install pytest absl-py attr numpy wrapt attrs
+
+#Download and apply the patch file
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/d/dm-tree/update_abseil_version_and_linking_fix.patch
+git apply update_abseil_version_and_linking_fix.patch
+#Build
+python3.11 -m pip install .
+echo "-----------------------------------------------------Installed dm-tree-----------------------------------------------------"
 
 # Install six.
 echo "------------------------Installing dependencies-------------------"
@@ -208,7 +227,6 @@ python3.11 -m pip install "numpy<2" wheel==0.38.4 werkzeug
 python3.11 -m pip install "urllib3<1.27,>=1.21.1" requests
 python3.11 -m pip install "protobuf<=4.25.2"
 python3.11 -m pip install tensorflow-datasets
-
 
 # Install numpy, scipy and scikit-learn required by the builds
 ln -s /usr/include/locale.h /usr/include/xlocale.h
@@ -272,7 +290,7 @@ wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/t
 git apply tf_2.14.1_fix.patch
 echo "-----------------------Applied patch successfully---------------------------------------"
 
-export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-12/root/usr/lib64:$LD_LIBRARY_PATH
 yes n | ./configure
 
 echo "------------------------Bazel query-------------------"
@@ -417,4 +435,3 @@ else
     echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass | Both_Install_and_Test_Success"
     exit 0
 fi
-
