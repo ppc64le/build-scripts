4435f16994fe90ba322e33e95a513cf05e6a3f79
Vibhav Dhaimode <Vibhav.Dhaimode3@ibm.com>
2026-02-08 06:55:35 +0000

    Corrected numpy pin

--- p/pytorch/pytorch_2.0.1_ubi_9.3.sh
+++ p/pytorch/pytorch_2.0.1_ubi_9.3.sh
@@ -47,7 +47,7 @@ export NO_WERROR=1
 export BUILD_TEST=0
 
 echo "Installing required Python packages..."
-pip install wheel scipy==1.13.1 ninja build pytest
+pip install wheel "numpy<2.0" scipy==1.13.1 ninja build pytest
 echo "Installing required numpy"
 pip install "numpy<2.0"
  
