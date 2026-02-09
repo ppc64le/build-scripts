37ae28cf3b481d975f36515cfd2c98c5d3d5eb23
Veenious Geevarghese <Veenious.Geevarghese@ibm.com>
2026-02-09 15:38:31 +0530

    Typo error in  "Ci-Check      : True"

--- c/catboost/catboost-1.2.7_ubi_9.6.sh
+++ c/catboost/catboost-1.2.7_ubi_9.6.sh
@@ -6,7 +6,7 @@
 # Source repo   : https://github.com/catboost/catboost.git
 # Tested on     : UBI:9.6 (ppc64le)
 # Language      : Python,c,c++
-# CI-Check      : True
+# Ci-Check      : True
 # Script License: Apache License Version 2.0
 # Maintainer    : Veenious D Geevarghese <Veenious.Geevarghese@ibm.com>
 #
