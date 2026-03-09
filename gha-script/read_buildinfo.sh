33070088977106d50cd151cff7d6ecbe732b3075
Vipul Ajmera <Vipul.Ajmera@ibm.com>
2026-03-09 18:21:29 +0530

    Update read_buildinfo.sh

--- gha-script/read_buildinfo.sh
+++ gha-script/read_buildinfo.sh
@@ -159,9 +159,6 @@ echo "export BASENAME=$basename" >> $CUR_DIR/variable.sh
 echo "export NON_ROOT_BUILD=$nonRootBuild" >> $CUR_DIR/variable.sh
 echo "export TESTED_ON=$tested_on" >> $CUR_DIR/variable.sh
 
-BUILD_SCRIPT_DATE=$(git log -1 --format=%ci -- "$package_dirpath$build_script")
-echo "export BUILD_SCRIPT_DATE=$BUILD_SCRIPT_DATE" >> $CUR_DIR/variable.sh
-
 chmod +x $CUR_DIR/variable.sh
 cat $CUR_DIR/variable.sh
 cd $CUR_DIR
