0f534856d60ef6f4c3c398ddf3b4251d2ab347ea
Stuti Ravikiran Wali <Stuti.Wali@ibm.com>
2026-03-26 14:20:41 +0530

    Update validate_builds.py

--- gha-script/validate_builds.py
+++ gha-script/validate_builds.py
@@ -12,7 +12,8 @@ import re
 
 
 GITHUB_BUILD_SCRIPT_BASE_REPO = "build-scripts"
-GITHUB_BUILD_SCRIPT_BASE_OWNER = "ppc64le"
+#GITHUB_BUILD_SCRIPT_BASE_OWNER = "ppc64le"
+GITHUB_BUILD_SCRIPT_BASE_OWNER = "stutiibm"
 HOME = os.getcwd()
 
 package_data = {}
