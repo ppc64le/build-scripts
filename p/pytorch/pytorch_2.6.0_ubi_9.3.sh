df6ee98dd8a83ab2b56519e17fa0a48d59e466dc
Bhagyashri <Bhagyashri.Gaikwad2@ibm.com>
2026-04-12 21:22:09 +0530

    Update pytorch_2.6.0_ubi_9.3.sh
    Fixed issues

--- p/pytorch/pytorch_2.6.0_ubi_9.3.sh
+++ p/pytorch/pytorch_2.6.0_ubi_9.3.sh
@@ -23,8 +23,6 @@ set -e
 PACKAGE_NAME=pytorch
 PACKAGE_URL=https://github.com/pytorch/pytorch.git
 PACKAGE_VERSION=${1:-v2.10.0}
-export TORCH_VERSION=2.10.0
-PACKAGE_DIR=pytorch
 SCRIPT_DIR=$(pwd)
 
 yum install -y git make wget python3.12 python3.12-devel python3.12-pip pkgconfig atlas
@@ -37,9 +35,9 @@ echo "Installed required deps from RH"
 export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
 export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
 
+# Ensure pkg-config can find Python (required for NumPy build with Meson)
 export PKG_CONFIG_PATH="/usr/lib64/pkgconfig:${PKG_CONFIG_PATH:-}"
 
-# -------------------- CMake --------------------
 echo "Installing cmake..."
 wget https://cmake.org/files/v3.31/cmake-3.31.6.tar.gz
 tar -zxvf cmake-3.31.6.tar.gz
@@ -109,26 +107,39 @@ rm -rf ./third_party/abseil-cpp || true
 cp -r $SCRIPT_DIR/abseil-cpp ./third_party/
 
 mkdir build && cd build
-
+echo "Building libprotobuf"
 cmake -G "Ninja" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_C_COMPILER=$C_COMPILER \
+    -DCMAKE_CXX_COMPILER=$CXX_COMPILER \
     -DCMAKE_INSTALL_PREFIX=$LIBPROTO_INSTALL \
     -Dprotobuf_BUILD_TESTS=OFF \
+    -Dprotobuf_BUILD_LIBUPB=OFF \
     -Dprotobuf_BUILD_SHARED_LIBS=ON \
+    -Dprotobuf_ABSL_PROVIDER="module" \
+    -Dprotobuf_JSONCPP_PROVIDER="package" \
+    -Dprotobuf_USE_EXTERNAL_GTEST=OFF \
     ..
 
+echo "building libprotobuf...."
 cmake --build . --verbose
+echo "Installing libprotobuf...."
 cmake --install .
 
 cd ..
-
+echo "Building protobuf"
 export PROTOC=$LIBPROTO_DIR/build/protoc
-export LD_LIBRARY_PATH=$LIBPROTO_INSTALL/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=$SCRIPT_DIR/abseil-cpp/abseilcpp/lib:$(pwd)/build/libprotobuf.so:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$(pwd)/build/libprotobuf.so:$LD_LIBRARY_PATH
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
 
+#Apply patch
 wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/p/protobuf/set_cpp_to_17_v4.25.3.patch
 git apply set_cpp_to_17_v4.25.3.patch
 
+echo "Installing protobuf...."
 cd python
 python3.12 -m pip install --no-build-isolation .
 cd $SCRIPT_DIR
@@ -148,9 +159,14 @@ git submodule update --init --recursive
 sed -i '/lintrunner ;/s/$/ and platform_machine != "ppc64le"/' requirements.txt
 
 # -------------------- ENV --------------------
+ARCH=`uname -p`
+BUILD_NUM="1"
 export OPENBLAS_INCLUDE=${OpenBLASInstallPATH}/include
 export OpenBLAS_HOME=${OpenBLASInstallPATH}
-
+export build_type="cpu"
+export cpu_opt_arch="power9"
+export cpu_opt_tune="power10"
+export CPU_COUNT=$(nproc --all)
 export _GLIBCXX_USE_CXX11_ABI=1
 export C_INCLUDE_DIR="${OpenBLASInstallPATH}/include"
 export CPLUS_INCLUDE_DIR="${OpenBLASInstallPATH}/include"
@@ -160,20 +176,54 @@ export CPU_COUNT=$(nproc)
 export CXXFLAGS="${CXXFLAGS} -mcpu=power9 -mtune=power10 -fplt"
 export CFLAGS="${CFLAGS} -mcpu=power9 -mtune=power10 -fplt"
 
+export LDFLAGS="$(echo ${LDFLAGS} | sed -e 's/-Wl\,--as-needed//')"
+export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${LIBPROTO_INSTALL}/lib64 -Wl,-rpath-link,${OpenBLASInstallPATH}/lib"
+export CXXFLAGS="${CXXFLAGS} -fplt"
+export CFLAGS="${CFLAGS} -fplt"
 export BLAS=OpenBLAS
 export USE_FBGEMM=0
+export USE_SYSTEM_NCCL=1
 export USE_MKLDNN=0
 export USE_NNPACK=0
 export USE_QNNPACK=0
 export USE_XNNPACK=0
+export USE_PYTORCH_QNNPACK=0
+export TH_BINARY_BUILD=1
+export USE_LMDB=1
+export USE_LEVELDB=1
+export USE_NINJA=0
+export USE_MPI=0
+export USE_OPENMP=1
+export USE_TBB=0
+export BUILD_CUSTOM_PROTOBUF=OFF
+export BUILD_CAFFE2=1
+export PYTORCH_BUILD_VERSION=${PACKAGE_VERSION#v}
+export PYTORCH_BUILD_NUMBER=${BUILD_NUM}
 export USE_CUDA=0
+export USE_CUDNN=0
+export USE_TENSORRT=0
+export Protobuf_INCLUDE_DIR=${LIBPROTO_INSTALL}/include
+export Protobuf_LIBRARIES=${LIBPROTO_INSTALL}/lib64
+export Protobuf_LIBRARY=${LIBPROTO_INSTALL}/lib64/libprotobuf.so
+export Protobuf_LITE_LIBRARY=${LIBPROTO_INSTALL}/lib64/libprotobuf-lite.so
+export Protobuf_PROTOC_EXECUTABLE=${LIBPROTO_INSTALL}/bin/protoc
+export PATH="/protobuf/local/libprotobuf/bin/protoc:${PATH}"
+export LD_LIBRARY_PATH="/protobuf/local/libprotobuf/lib64:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="/protobuf/third_party/abseil-cpp/local/abseilcpp/lib:${LD_LIBRARY_PATH}"
+export CXXFLAGS="${CXXFLAGS} -mcpu=${cpu_opt_arch} -mtune=${cpu_opt_tune}"
+export CFLAGS="${CFLAGS} -mcpu=${cpu_opt_arch} -mtune=${cpu_opt_tune}"
+export LD_LIBRARY_PATH="${SCRIPT_DIR}/pytorch/torch/lib/:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${SCRIPT_DIR}/pytorch/torch/lib64/:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${SCRIPT_DIR}/protobuf/local/libprotobuf/lib64/:${LD_LIBRARY_PATH}"
+echo "required env variables got set"
 
 sed -i "s/cmake/cmake==3.*/g" requirements.txt
 python3.12 -m pip install -r requirements.txt
 
 # -------------------- Build --------------------
 if ! (MAX_JOBS=$(nproc) python3.12 setup.py install); then
-    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
