5e96fe7eefc2b6ed674dd237760d442c02d4c6ad
puneetsharma21 <puneet.sharma21@ibm.com>
2026-04-16 03:01:52 -0400

    fix python

--- n/numba/numba_0.61.2_ubi_9.6.sh
+++ n/numba/numba_0.61.2_ubi_9.6.sh
@@ -28,7 +28,7 @@ echo "------------------------------------------------------------------"
 echo "$WORKING_DIR"
 # Install necessary  dependencies
 
-yum install -y git make wget python3.12 python3.12-devel python3.12-pip gcc-toolset-13
+yum install -y git make wget python python-devel python-pip gcc-toolset-13
 export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
 export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
 export LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib/gcc/ppc64le-redhat-linux/13:$LIBRARY_PATH
@@ -53,7 +53,7 @@ LLVM_INSTALL_DIR=$WORKING_DIR/llvm-install
 git clone -b ${LLVM_PROJECT_GIT_TAG} ${LLVM_PROJECT_GIT_URL}
 git clone -b ${LLVMLITE_VERSION} ${LLVMLITE_PACKAGE_URL}
 
-python3.12 -m pip install ninja "setuptools<70" wheel
+python -m pip install ninja "setuptools<70" wheel
 
 cd $LLVM_SRC_DIR
 git fetch --all --tags
@@ -72,7 +72,7 @@ cd $WORKING_DIR/llvmlite
 # Build & install llvmlite
 export CMAKE_PREFIX_PATH=$LLVM_INSTALL_DIR/lib/cmake/llvm
 export CXXFLAGS="-fPIC"
-python3.12 -m pip install . --no-build-isolation
+python -m pip install . --no-build-isolation
 
 echo "-------------------successfully Installed llvmlite----------------------"
 
@@ -89,7 +89,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64:/usr/local/lib
 cd ..
 echo "------------openblas installed--------------------"
 
-python3.12 -m pip install numpy==2.0.2
+python -m pip install numpy==2.0.2
 
 cd $WORKING_DIR
 
@@ -104,10 +104,10 @@ git checkout $PACKAGE_VERSION
 export CXXFLAGS=-I/usr/include
 # echo "after CXXFLAGS............$CXXFLAGS........."
 
-PYTHON_VERSION=$(python3.12 -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
+PYTHON_VERSION=$(python -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
 
 #install
-if ! python3.12 -m pip install . ; then
+if ! python -m pip install . ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
@@ -116,7 +116,7 @@ fi
 
 #test
 cd $WORKING_DIR
-if ! python3.12 -c "import numba; import numba.core.annotations; import numba.core.datamodel; import numba.core.rewrites; import numba.core.runtime; import numba.core.typing; import numba.core.unsafe; import numba.experimental.jitclass; import numba.np.ufunc; import numba.pycc; import numba.scripts; import numba.testing; import numba.tests; import numba.tests.npyufunc;"; then
+if ! python -c "import numba; import numba.core.annotations; import numba.core.datamodel; import numba.core.rewrites; import numba.core.runtime; import numba.core.typing; import numba.core.unsafe; import numba.experimental.jitclass; import numba.np.ufunc; import numba.pycc; import numba.scripts; import numba.testing; import numba.tests; import numba.tests.npyufunc;"; then
     echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
