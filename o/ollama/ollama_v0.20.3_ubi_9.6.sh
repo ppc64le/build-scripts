d0cbf2734d2069f554b2e25c69aa698376b873c4
shalinib-ibm <Shalini.Salomi.Bodapati@ibm.com>
2026-05-12 09:37:18 +0530

    Read version from env var while generating setup.py

--- o/ollama/ollama_v0.20.3_ubi_9.6.sh
+++ o/ollama/ollama_v0.20.3_ubi_9.6.sh
@@ -118,7 +118,7 @@ from setuptools.command.build_py import build_py
 import os, shutil, stat
 
 PYTHON_PACKAGE_NAME = "ollama_python_package"
-VERSION = "0.20.3"
+VERSION = "${PACKAGE_VERSION#v}"
 
 BIN_SRC = os.path.join(os.getcwd(), "ollama")
 LIB_SRC = os.path.join(os.getcwd(), "build", "lib", "ollama")
