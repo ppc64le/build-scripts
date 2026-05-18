2cd5502dfa3b95e9d789df894887fbf2e8a867a4
Prabhu K <Prabhu.K@ibm.com>
2026-05-18 18:32:12 +0530

    Refactor build script to use Docker commands
    Updated the script to use Docker instead of Buildah for building images, modified commands for better error handling, and adjusted Dockerfile references.

--- e/eclipse-che/che-incubator/che-code/che_code_7.117.0_rhel_9.7.sh
+++ e/eclipse-che/che-incubator/che-code/che_code_7.117.0_rhel_9.7.sh
@@ -22,89 +22,66 @@
 # docker must be installed and running.
 #
 # ----------------------------------------------------------------------------
-set -e
+
+set -euxo pipefail
 
 PACKAGE_URL=https://github.com/che-incubator/che-code
 PACKAGE_NAME=che-code
 VERSION=7.117.0
 
-export CWD=`pwd`
+export CWD=$(pwd)
 
-yum update -y
-yum install git -y
-yum install wget -y
-
-########## Container-in-Container Compatibility Patch (Only required for CI/containerized environments) #########
-
-## Install container tools required for container-in-container CI builds
-#yum install -y buildah podman fuse-overlayfs
-
-## Configure containers storage explicitly for CI/container environments
-#mkdir -p /etc/containers
-#mkdir -p /var/lib/containers/storage
-#mkdir -p /var/run/containers/storage
-
-#cat > /etc/containers/storage.conf <<'EOF'
-#[storage]
-#driver = "vfs"
-#runroot = "/var/run/containers/storage"
-#graphroot = "/var/lib/containers/storage"
-#EOF
-
-## Required for restricted/containerized CI environments
-#export BUILDAH_ISOLATION=chroot
-#export STORAGE_DRIVER=vfs
-#export CONTAINERS_STORAGE_CONF=/etc/containers/storage.conf
-
-######################
-
+yum install -y git wget docker
 
 #Clone repo
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
-git checkout $VERSION
+git fetch --tags
+git checkout tags/$VERSION
 
 #Move dockerfiles to main folder
-#cp $CWD/Dockerfiles/linux-musl.Dockerfile .
-wget https://raw.githubusercontent.com/prabhuk25/build-scripts/refs/heads/che-code/e/eclipse-che/che-incubator/che-code/Dockerfiles/linux-musl.Dockerfile
+wget --tries=5 --retry-connrefused https://raw.githubusercontent.com/prabhuk25/build-scripts/che-code/e/eclipse-che/che-incubator/che-code/Dockerfiles/linux-musl.Dockerfile
 
 cp build/dockerfiles/linux-libc-ubi8.Dockerfile .
 cp build/dockerfiles/linux-libc-ubi9.Dockerfile .
 cp build/dockerfiles/assembly.Dockerfile .
 
 #Patch package-lock.json to skip @vscode/vsce-sign, since it has no binary for ppc64le and its exit with code 1 on postinstall.
+
+grep -q "@vscode/vsce-sign" code/build/package-lock.json
+
 sed -i '/@vscode\/vsce-sign/,/\}/s/"hasInstallScript": true/"hasInstallScript": false/' \
        code/build/package-lock.json
 
 #Build linux-musl image
 echo "linux-musl build started"
-buildah bud --isolation=chroot --storage-driver=vfs --format=docker -t linux-musl -f linux-musl.Dockerfile . 
+docker build -t linux-musl -f linux-musl.Dockerfile .
 echo "linux-musl build completed"
 
 #Build linux-libc-ubi8
 echo "linux-libc-ubi8 build started"
-buildah bud --isolation=chroot --storage-driver=vfs --format=docker -t linux-libc-ubi8 -f linux-libc-ubi8.Dockerfile .
+docker build -t linux-libc-ubi8 -f linux-libc-ubi8.Dockerfile .
 echo "linux-libc-ubi8 build completed"
 
 #Build linux-libc-ubi9
 echo "linux-libc-ubi9 build started"
-buildah bud --isolation=chroot --storage-driver=vfs --format=docker -t linux-libc-ubi9 -f linux-libc-ubi9.Dockerfile .
+docker build -t linux-libc-ubi9 -f linux-libc-ubi9.Dockerfile .
 echo "linux-libc-ubi9 build completed"
 
 echo "Available local images:"
-buildah images
+docker images
 
 #Patch the images names to use locally build images in assesbly.Dockerfile
 sed -i \
--e 's|FROM linux-libc-ubi8|FROM localhost/linux-libc-ubi8|' \
--e 's|FROM linux-libc-ubi9|FROM localhost/linux-libc-ubi9|' \
--e 's|FROM linux-musl|FROM localhost/linux-musl|' \
+-e 's|FROM linux-libc-ubi8|FROM linux-libc-ubi8:latest|' \
+-e 's|FROM linux-libc-ubi9|FROM linux-libc-ubi9:latest|' \
+-e 's|FROM linux-musl|FROM linux-musl:latest|' \
 assembly.Dockerfile
 
 #Build che-code
 echo "che-code build started"
-buildah bud --isolation=chroot --storage-driver=vfs --format=docker -t che-code -f assembly.Dockerfile .
+docker build -t che-code -f assembly.Dockerfile .
 echo "che-code build completed"
 
 #If you want to test image, use below command
-#podman run --rm -it -p 3100:3100 -e CODE_HOST=0.0.0.0 che-code:latest
+#docker run --rm -it -p 3100:3100 -e CODE_HOST=0.0.0.0 che-code:latest
