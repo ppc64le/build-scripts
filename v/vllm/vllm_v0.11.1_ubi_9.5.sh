6e50a944546d7abfd26decc6714eae7af6044459
Vipul Ajmera <Vipul.Ajmera@ibm.com>
2026-06-03 14:54:41 +0530

    Update vllm_v0.11.1_ubi_9.5.sh

--- v/vllm/vllm_v0.11.1_ubi_9.5.sh
+++ v/vllm/vllm_v0.11.1_ubi_9.5.sh
@@ -318,6 +318,7 @@ python3.12 -m pip install xgrammar
 export VLLM_TARGET_DEVICE=cpu
 export MAX_JOBS=$(nproc)
 
+export SETUPTOOLS_SCM_PRETEND_VERSION=${PACKAGE_VERSION#v}
 python3.12 setup.py install
 
 python3.12 setup.py bdist_wheel --dist-dir="${CURRENT_DIR}"
