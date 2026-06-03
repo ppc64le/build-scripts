896d6ad086869dfca2511f62d4a6c00b7178cf1e
Bhagyashri <Bhagyashri.Gaikwad2@ibm.com>
2026-06-03 19:47:06 +0530

    Update xgrammar_0.2.1_ubi_9.6.sh

--- x/xgrammar/xgrammar_0.2.1_ubi_9.6.sh
+++ x/xgrammar/xgrammar_0.2.1_ubi_9.6.sh
@@ -7,7 +7,7 @@
 # Language      : Python
 # Ci-Check      : True
 # Script License: Apache License, Version 2 or later
-# Maintainer : Sai Kiran Nukala <sai.kiran.nukala@ibm.com>
+# Maintainer : Bhagyashri Gaikwad <Bhagyashri.Gaikwad2@ibm.com> 
 #
 # Disclaimer    : This script has been tested in root mode on given
 # ==========      platform using the mentioned version of the package.
