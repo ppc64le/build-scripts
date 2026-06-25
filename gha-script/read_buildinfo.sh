09b2f8945ed9db5d0a71780ac956d951b3f707aa
Vipul Ajmera <Vipul.Ajmera@ibm.com>
2026-06-25 11:17:24 +0530

    Exclude updates (#8311)
    * Auditwheel wheel exclude updates
    
    * Update create_wheel_wrapper.sh

--- gha-script/read_buildinfo.sh
+++ gha-script/read_buildinfo.sh
@@ -146,6 +146,12 @@ if [ -f "$stripped_build_script" ]; then
   echo "Tested on value: $tested_on"
 fi
 
+# Extract auditwheel exclusions
+AUDITWHEEL_EXCLUDE=""
+if $(jq 'has("auditwheel_exclude")' $config_file); then
+  AUDITWHEEL_EXCLUDE=$(jq -r '.auditwheel_exclude[]' $config_file | tr '\n' ' ')
+fi
+
 # Export variables
 
 echo "export VERSION=$VERSION" > $CUR_DIR/variable.sh
@@ -158,6 +164,7 @@ echo "export VARIANT=$variant" >> $CUR_DIR/variable.sh
 echo "export BASENAME=$basename" >> $CUR_DIR/variable.sh
 echo "export NON_ROOT_BUILD=$nonRootBuild" >> $CUR_DIR/variable.sh
 echo "export TESTED_ON=$tested_on" >> $CUR_DIR/variable.sh
+echo "export AUDITWHEEL_EXCLUDE=\"$AUDITWHEEL_EXCLUDE\"" >> $CUR_DIR/variable.sh
 
 chmod +x $CUR_DIR/variable.sh
 cat $CUR_DIR/variable.sh
