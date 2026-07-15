005667f79ae2a5de39e32bf5e487cb85a86dc945
Varsha Kumar <varsha.kumar@ibm.com>
2026-07-15 16:25:42 +0000

    Add buildscript for numexpr 2.14.1
    Signed-off-by: Varsha Kumar <varsha.kumar@ibm.com>

---
+++ n/numexpr/numexpr_2.14.1_ubi_9.6.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+#
+# Package       : numexpr
+# Version       : v2.14.1
+# Source repo   : https://github.com/pydata/numexpr.git
+# Tested on     : UBI:9.6
+# Language      : Python
+# Ci-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Varsha Kumar <varsha.kumar@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+#variables
+PACKAGE_NAME=numexpr
+PACKAGE_VERSION=${1:-v2.14.1}
+PACKAGE_URL=https://github.com/pydata/numexpr.git
+
+# Install dependencies
+dnf install -y git gcc gcc-c++ make wget openssl-devel \
+    python3.12-devel python3.12-pip bzip2-devel libffi-devel \
+    wget xz zlib-devel cmake openblas-devel
+
+# Clone and checkout
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+# Remove the conflicting license field from pyproject.toml.
+sed -i '/^license = /d' pyproject.toml
+
+# Install dependencies
+python3.12 -m pip install pytest numpy
+
+# Install package
+if ! python3.12 -m pip install -e . ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+# Run tests
+if ! python3.12 -m pytest numexpr/tests/ -v ; then
+    echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
