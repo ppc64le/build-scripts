abe5ebca6643d2cfa83920f9df0c749188f4638b
Varsha Kumar <varsha.kumar@ibm.com>
2026-07-27 15:53:48 +0000

    Added update line
    Signed-off-by: Varsha Kumar <varsha.kumar@ibm.com>

--- l/lz4/lz4_4.4.5_ubi_9.6.sh
+++ l/lz4/lz4_4.4.5_ubi_9.6.sh
@@ -28,6 +28,7 @@ SOURCE_ROOT="$(pwd)"
 echo "Building ${PACKAGE_NAME} ${PACKAGE_VERSION}"
 
 # Install system dependencies
+dnf update -y openssl openssl-libs
 dnf install -y gcc-toolset-13 git python3.12 python3.12-devel python3.12-pip
 
 export PATH="/opt/rh/gcc-toolset-13/root/usr/bin:$PATH"
