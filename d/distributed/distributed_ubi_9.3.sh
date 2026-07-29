bd2525d66a602905898e62f68745bcc72289f35c
Vipul Ajmera <Vipul.Ajmera@ibm.com>
2026-07-29 14:25:11 +0530

    Update distributed_ubi_9.3.sh

--- d/distributed/distributed_ubi_9.3.sh
+++ d/distributed/distributed_ubi_9.3.sh
@@ -27,30 +27,31 @@ PACKAGE_DIR=distributed
 yum install -y python3.12 python3.12-pip python3.12-devel git gcc-toolset-13 gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ openssl-devel xz-devel xz.ppc64le
 export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
 export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
-pip3.12 install  versioneer pytest-cov  pytest-timeout pytest pytest-rerunfailures
+python3.12 -m pip install versioneer pytest-cov  pytest-timeout pytest pytest-rerunfailures
 
 #Install dask from source  repository
 git clone https://github.com/dask/dask.git
 cd  dask
 git checkout 2025.5.1
-pip3.12 install .
+python3.12 -m pip install .
 cd ..
 
 git clone $PACKAGE_URL
 cd  $PACKAGE_NAME
-git checkout $PACKAGE_VERSION 
+git checkout $PACKAGE_VERSION
+
+# Replaced yaml.CSafeDumper with yaml.SafeDumper for compatibility with PyYAML >= 6.0
+sed -i 's/yaml\.CSafeDumper/yaml.SafeDumper/g' distributed/cluster_dump.py
 
 export SETUPTOOLS_SCM_PRETEND_VERSION=${PACKAGE_VERSION}
 
-if ! pip3.12 install -e . ;  then  
+if ! python3.12 -m pip install -e . ;  then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 export DISABLE_IPV6=1
-# Replaced yaml.CSafeDumper with yaml.SafeDumper for compatibility with PyYAML >= 6.0
-sed -i 's/yaml\.CSafeDumper/yaml.SafeDumper/g' distributed/cluster_dump.py
 #skipping unstable assertions errors and permission errors
 if ! pytest -k "not test_unwritable_base_dir and not test_bad_local_directory and not test_spillbuffer_oserror and not test_resubmit_nondeterministic_task_different_deps and not test_ws and not test_local and not test_logging_default"; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
