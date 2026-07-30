c012950562eb32ab99e17894c3444562ec00c4f2
tejasBadjateIBM <161455399+tejasBadjateIBM@users.noreply.github.com>
2026-07-30 15:23:58 +0530

    Update argon2-cffi-bindings_ubi_10.1.sh

--- a/argon2-cffi-bindings/argon2-cffi-bindings_ubi_10.1.sh
+++ a/argon2-cffi-bindings/argon2-cffi-bindings_ubi_10.1.sh
@@ -5,7 +5,7 @@
 # Package           : argon2-cffi-bindings
 # Version           : 21.2.0
 # Source repo       : https://github.com/hynek/argon2-cffi-bindings
-# Tested on         : UBI:10
+# Tested on         : UBI:10.1
 # Language          : Python
 # Ci-Check      : True
 # Script License    : Apache License, Version 2.0
