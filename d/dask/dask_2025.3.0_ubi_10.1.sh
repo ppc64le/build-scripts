a011d214593156a19f4231f628b05209aa4f99e2
shivansh-ibm <shivansh.s1@ibm.com>
2026-07-29 21:13:17 -0700

    Added build script for dask==2025.3.0 for UBI:10.1

---
+++ d/dask/dask_2025.3.0_ubi_10.1.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+#
+# Package       : dask
+# Version       : 2025.3.0
+# Source repo   : https://github.com/dask/dask.git
+# Tested on     : UBI:10.1
+# Language      : Python
+# Ci-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Shivansh Sharma <shivansh.s1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+#variables
+PACKAGE_NAME=dask
+PACKAGE_VERSION=${1:-2025.3.0}
+PACKAGE_URL=https://github.com/dask/dask.git
+PACKAGE_DIR=dask
+
+# Install dependencies and tools.
+yum install -y wget gcc gcc-c++ gcc-gfortran git make  python3.12-devel openssl-devel xz-devel xz.ppc64le
+
+#clone repository
+git clone $PACKAGE_URL
+cd  $PACKAGE_DIR
+git checkout $PACKAGE_VERSION
+
+python3.12 -m pip install pytest pytest-cov
+
+#install
+if ! (python3.12 -m pip install .) ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+#test
+if ! pytest -p no:warnings --ignore=dask/tests/test_config.py; then
+    echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
+
