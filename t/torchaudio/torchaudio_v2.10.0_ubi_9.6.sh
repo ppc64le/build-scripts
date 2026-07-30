e7de6d977e953b59733b345c56e673a998b9d92d
Amit Kumar <amit.kumar282@ibm.com>
2026-07-30 12:28:53 +0530

    Added files

---
+++ t/torchaudio/torchaudio_v2.10.0_ubi_9.6.sh
@@ -0,0 +1,285 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : torchaudio
+# Version          : v2.10.0
+# Source repo      : https://github.com/pytorch/audio.git
+# Tested on        : UBI:9.6
+# Language         : Python
+# Ci-Check         : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Amit Kumar <amit.kumar282@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ---------------------------------------------------------------------------
+
+set -ex
+
+# Variables
+PACKAGE_NAME=audio
+PACKAGE_URL=https://github.com/pytorch/audio.git
+PACKAGE_VERSION=${1:-v2.10.0}
+PACKAGE_DIR=./audio
+SCRIPT_DIR=$(pwd)
+IBM_WHEEL_INDEX=https://wheels.developerfirst.ibm.com/ppc64le/linux
+
+yum install -y git make wget python3.12 python3.12-devel python3.12-pip pkgconfig atlas
+yum install gcc-toolset-13 -y
+echo "Installed gcc-toolset"
+yum install -y make libtool xz zlib-devel openssl-devel bzip2-devel libffi-devel libevent-devel patch ninja-build pkg-config
+dnf install -y gcc-toolset-13-libatomic-devel
+echo "Installed required deps from RH"
+
+export BUILD_VERSION=${PACKAGE_VERSION#v}
+export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
+export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
+
+echo "Installing cmake..."
+wget https://cmake.org/files/v3.31/cmake-3.31.6.tar.gz
+tar -zxvf cmake-3.31.6.tar.gz
+cd cmake-3.31.6
+./bootstrap
+echo "Building Cmake"
+make
+echo "Installing Cmake"
+make install
+cd $SCRIPT_DIR
+
+echo "---------------------openblas installing---------------------"
+
+# Build OpenBLAS from source (CMake FindOpenBLAS requires system headers/libs)
+[ -d "OpenBLAS" ] && rm -rf OpenBLAS
+git clone https://github.com/OpenMathLib/OpenBLAS
+cd OpenBLAS
+git checkout v0.3.33
+make -j$(nproc) TARGET=POWER9 BUILD_BFLOAT16=1 BINARY=64 USE_OPENMP=1 USE_THREAD=1 NUM_THREADS=120 DYNAMIC_ARCH=1 INTERFACE64=0
+make install PREFIX=/usr/local
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64:/usr/local/lib
+cd $SCRIPT_DIR
+echo "--------------------openblas installed-------------------------------"
+
+# Install scipy from pre-built wheel available on DevPI
+python3.12 -m pip install "scipy==1.18.0+ppc64le1" --extra-index-url $IBM_WHEEL_INDEX
+echo "--------------------scipy installed-------------------------------"
+
+# Clone abseil-cpp (required as a submodule for protobuf source build)
+ABSEIL_VERSION=20240116.2
+[ -d "abseil-cpp" ] && rm -rf abseil-cpp
+git clone https://github.com/abseil/abseil-cpp -b $ABSEIL_VERSION
+echo "------------abseil-cpp cloned--------------"
+
+# Build protobuf from source (CMake requires system-level headers, libs and protoc)
+export C_COMPILER=$(which gcc)
+export CXX_COMPILER=$(which g++)
+
+[ -d "protobuf" ] && rm -rf protobuf
+git clone https://github.com/protocolbuffers/protobuf
+cd protobuf
+git checkout v4.25.8
+
+LIBPROTO_DIR=$(pwd)
+mkdir -p $LIBPROTO_DIR/local/libprotobuf
+LIBPROTO_INSTALL=$LIBPROTO_DIR/local/libprotobuf
+
+git submodule update --init --recursive
+rm -rf ./third_party/googletest | true
+rm -rf ./third_party/abseil-cpp | true
+
+cp -r $SCRIPT_DIR/abseil-cpp ./third_party/
+
+mkdir build
+cd build
+
+echo "Building libprotobuf"
+cmake -G "Ninja" \
+  ${CMAKE_ARGS} \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DCMAKE_CXX_STANDARD=17 \
+   -DCMAKE_C_COMPILER=$C_COMPILER \
+   -DCMAKE_CXX_COMPILER=$CXX_COMPILER \
+   -DCMAKE_INSTALL_PREFIX=$LIBPROTO_INSTALL \
+   -Dprotobuf_BUILD_TESTS=OFF \
+   -Dprotobuf_BUILD_LIBUPB=OFF \
+   -Dprotobuf_BUILD_SHARED_LIBS=ON \
+   -Dprotobuf_ABSL_PROVIDER="module" \
+   -Dprotobuf_JSONCPP_PROVIDER="package" \
+   -Dprotobuf_USE_EXTERNAL_GTEST=OFF \
+   ..
+echo "building libprotobuf...."
+cmake --build . --verbose
+echo "Installing libprotobuf...."
+cmake --install .
+
+cd ..
+
+echo "Building protobuf python bindings"
+export PROTOC=$LIBPROTO_DIR/build/protoc
+export LD_LIBRARY_PATH=$SCRIPT_DIR/abseil-cpp/abseilcpp/lib:$(pwd)/build/libprotobuf.so:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$(pwd)/build/libprotobuf.so:$LD_LIBRARY_PATH
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
+
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/p/protobuf/set_cpp_to_17_v4.25.3.patch
+git apply set_cpp_to_17_v4.25.3.patch
+
+echo "Installing protobuf...."
+cd python
+python3.12 -m pip install . --no-build-isolation
+cd $SCRIPT_DIR
+
+echo "------------ libprotobuf,protobuf installed--------------"
+
+echo "----Installing rust------"
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source "$HOME/.cargo/env"
+
+echo "------------cloning pytorch----------------"
+[ -d "pytorch" ] && rm -rf pytorch
+git clone https://github.com/pytorch/pytorch.git
+cd pytorch
+git checkout $PACKAGE_VERSION
+git submodule sync
+git submodule update --init --recursive
+
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/p/pytorch/pytorch_v2.9.0.patch
+git apply pytorch_v2.9.0.patch
+
+ARCH=`uname -p`
+BUILD_NUM="1"
+export OPENBLAS_INCLUDE=/usr/local/include
+export OpenBLAS_HOME=/usr/local
+export build_type="cpu"
+export cpu_opt_arch="power9"
+export cpu_opt_tune="power10"
+export CPU_COUNT=$(nproc --all)
+export CXXFLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS"
+export LDFLAGS="$(echo ${LDFLAGS} | sed -e 's/-Wl\,--as-needed//')"
+export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${LIBPROTO_INSTALL}/lib64 -Wl,-rpath-link,/usr/local/lib"
+export CXXFLAGS="${CXXFLAGS} -fplt"
+export CFLAGS="${CFLAGS} -fplt"
+export BLAS=OpenBLAS
+export USE_FBGEMM=0
+export USE_SYSTEM_NCCL=1
+export USE_MKLDNN=0
+export USE_NNPACK=0
+export USE_QNNPACK=0
+export USE_XNNPACK=0
+export USE_PYTORCH_QNNPACK=0
+export TH_BINARY_BUILD=1
+export USE_LMDB=1
+export USE_LEVELDB=1
+export USE_NINJA=0
+export USE_MPI=0
+export USE_OPENMP=1
+export USE_TBB=0
+export BUILD_CUSTOM_PROTOBUF=OFF
+export BUILD_CAFFE2=1
+export PYTORCH_BUILD_VERSION=${PACKAGE_VERSION#v}
+export PYTORCH_BUILD_NUMBER=${BUILD_NUM}
+export USE_CUDA=0
+export USE_CUDNN=0
+export USE_TENSORRT=0
+export Protobuf_INCLUDE_DIR=${LIBPROTO_INSTALL}/include
+export Protobuf_LIBRARIES=${LIBPROTO_INSTALL}/lib64
+export Protobuf_LIBRARY=${LIBPROTO_INSTALL}/lib64/libprotobuf.so
+export Protobuf_LITE_LIBRARY=${LIBPROTO_INSTALL}/lib64/libprotobuf-lite.so
+export Protobuf_PROTOC_EXECUTABLE=${LIBPROTO_INSTALL}/bin/protoc
+export PATH="${LIBPROTO_INSTALL}/bin:${PATH}"
+export LD_LIBRARY_PATH="${LIBPROTO_INSTALL}/lib64:${LD_LIBRARY_PATH}"
+export CXXFLAGS="${CXXFLAGS} -mcpu=${cpu_opt_arch} -mtune=${cpu_opt_tune}"
+export CFLAGS="${CFLAGS} -mcpu=${cpu_opt_arch} -mtune=${cpu_opt_tune}"
+
+echo "required env variables got set"
+sed -i "s/cmake/cmake==3.*/g" requirements.txt
+python3.12 -m pip install -r requirements.txt
+echo "Installed requirement files from source"
+
+echo "Installing pytorch...."
+# Limit parallel jobs to avoid OOM 
+if ! (MAX_JOBS=12 python3.12 setup.py install);then
+   echo "------------------pytorch:Install_fails-------------------------------------"
+   echo "https://github.com/pytorch/pytorch.git pytorch"
+   echo "pytorch  |  https://github.com/pytorch/pytorch.git  | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+   exit 1
+fi
+
+#basic import test
+echo " Basic Import test for torch"
+cd ..
+export LD_LIBRARY_PATH=/usr/local/lib64:/usr/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/lib:$LD_LIBRARY_PATH
+
+if ! (python3.12 -c "import torch;"); then
+     echo "--------------------pytorch:Install_success_but_test_fails---------------------"
+     echo "https://github.com/pytorch/pytorch.git pytorch"
+     echo "pytorch  |  https://github.com/pytorch/pytorch.git  | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but__Import_Fails"
+     exit 2
+fi
+cd $SCRIPT_DIR
+
+echo "------------------------clone and build torchaudio-------------------"
+[ -d "$PACKAGE_NAME" ] && rm -rf $PACKAGE_NAME
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+# Apply the patch
+echo "------------------------Applying patch-------------------"
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/t/torchaudio/torchaudio_v2.9.1.patch
+git apply torchaudio_v2.9.1.patch
+
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/t/torchaudio/0001-Excluded-source-that-has-commercial-license-new.patch
+# Below patch excludes the source files that has commercial license
+git apply 0001-Excluded-source-that-has-commercial-license-new.patch
+echo "-----------------------Applied patch successfully---------------------------------------"
+
+SRC_DIR=$(pwd)
+
+export USE_FFMPEG=OFF
+export BUILD_SOX=OFF
+export USE_OPENMP=OFF
+export BUILD_TORCHAUDIO_PYTHON_EXTENSION=ON
+export LIBPROTO_INSTALL=${SCRIPT_DIR}/protobuf/local/libprotobuf
+export Protobuf_INCLUDE_DIR=${LIBPROTO_INSTALL}/include
+export Protobuf_LIBRARIES=${LIBPROTO_INSTALL}/lib64
+export Protobuf_LIBRARY=${LIBPROTO_INSTALL}/lib64/libprotobuf.so
+export Protobuf_LITE_LIBRARY=${LIBPROTO_INSTALL}/lib64/libprotobuf-lite.so
+export Protobuf_PROTOC_EXECUTABLE=${LIBPROTO_INSTALL}/bin/protoc
+export PATH="${LIBPROTO_INSTALL}/bin:${PATH}"
+export LD_LIBRARY_PATH="${LIBPROTO_INSTALL}/lib64:/usr/local/lib64:/usr/local/lib:${LD_LIBRARY_PATH}"
+
+PY_VER=$(python3.12 -c 'import sys; print(f"{sys.version_info.major}{sys.version_info.minor}")')
+export LD_LIBRARY_PATH="${SCRIPT_DIR}/audio/build/lib.linux-ppc64le-cpython-${PY_VER}/torchaudio/lib:$LD_LIBRARY_PATH"
+
+echo "LD_LIBRARY_PATH= $LD_LIBRARY_PATH"
+echo "Installing torchaudio..."
+if ! (python3.12 -m pip install -v . --no-build-isolation --no-deps);then
+   echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+   echo "$PACKAGE_URL $PACKAGE_NAME"
+   echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+   exit 1
+fi
+
+if ! (python3.12 -m build --wheel --no-isolation --outdir="$SCRIPT_DIR/"); then
+   echo "------------------$PACKAGE_NAME:Failed to build wheel-------------------------------------"
+   echo "$PACKAGE_URL $PACKAGE_NAME"
+   echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+   exit 1
+fi
+
+if ! (python3.12 -c "import torch; import torch._C; import torchaudio"); then
+     echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails--------------------"
+     echo "$PACKAGE_URL $PACKAGE_NAME"
+     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but__Import_Fails"
+     exit 2
+else
+     echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+     echo "$PACKAGE_URL $PACKAGE_NAME"
+     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Import_Success"
+     exit 0
+fi
