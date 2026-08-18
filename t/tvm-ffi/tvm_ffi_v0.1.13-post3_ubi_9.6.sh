322d69ece776b86f02ad6a38841769b9fe7fae5a
PrernaKumbhar20 <Prerna.Kumbhar@ibm.com>
2026-08-18 05:53:13 -0400

    Added build script for apache-tvm-ffi==0.1.13.post3

---
+++ t/tvm-ffi/tvm_ffi_v0.1.13-post3_ubi_9.6.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : tvm-ffi
+# Version          : v0.1.13-post3
+# Source repo      : https://github.com/apache/tvm-ffi.git
+# Tested on        : UBI:9.6
+# Language         : Python
+# Ci-Check         : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Prerna Kumbhar <Prerna.Kumbhar@ibm.com>
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+set -ex
+
+# Variables
+PACKAGE_NAME=tvm-ffi
+PACKAGE_VERSION=${1:-"v0.1.13-post3"}
+PACKAGE_URL=https://github.com/apache/tvm-ffi.git
+PACKAGE_DIR=tvm-ffi
+
+# Install dependencies
+yum install -y git gcc gcc-c++ make cmake ninja-build python3.11 python3.11-devel python3.11-pip python3.11-setuptools
+
+ln -sf /usr/bin/python3.11 /usr/bin/python3
+ln -sf /usr/bin/pip3.11 /usr/bin/pip
+ln -sf /usr/bin/pip3.11 /usr/bin/pip3
+
+# Upgrade pip and install build/test tools
+python3 -m pip install --upgrade pip setuptools wheel pytest numpy pytest-xdist
+
+# Clone repository
+git clone $PACKAGE_URL
+cd $PACKAGE_DIR
+
+git checkout $PACKAGE_VERSION
+
+git submodule update --init --recursive
+
+# Install package
+if ! python3 -m pip install -v .; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Install_Fails"
+    exit 1
+fi
+
+
+# Run tests if available
+if [ -d "tests" ]; then
+    if ! python3 -m pytest -v; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Install_success_but_test_Fails"
+        exit 2
+    fi
+fi
+
+echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+echo "$PACKAGE_URL $PACKAGE_NAME"
+echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass | Both_Install_and_Test_Success"
+exit 0
