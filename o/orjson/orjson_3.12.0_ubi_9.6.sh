7148b3f5eb1eea621429ac35eb142694f8a5381f
Prerna Kumbhar <Prerna.Kumbhar@ibm.com>
2026-08-24 11:09:33 +0530

    Added build script for orjson==3.12.0 (#8644)

---
+++ o/orjson/orjson_3.12.0_ubi_9.6.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : orjson
+# Version          : 3.12.0
+# Source repo      : https://github.com/ijl/orjson.git
+# Tested on        : UBI:9.6
+# Language         : Python
+# Ci-Check         : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Prerna Kumbhar <Prerna.Kumbhar@ibm.com>
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+set -ex
+
+# Variables
+PACKAGE_NAME=orjson
+PACKAGE_VERSION=${1:-"3.12.0"}
+PACKAGE_URL=https://github.com/ijl/orjson.git
+
+# Install dependencies
+yum install -y git gcc gcc-c++ make cmake ninja-build python3.11 python3.11-devel python3.11-pip python3.11-setuptools
+
+ln -sf /usr/bin/python3.11 /usr/bin/python3
+ln -sf /usr/bin/pip3.11 /usr/bin/pip
+ln -sf /usr/bin/pip3.11 /usr/bin/pip3
+
+# Upgrade pip and install build/test tools
+python3 -m pip install --upgrade pip setuptools wheel pytest  maturin puccinialin
+
+# Clone repository
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+
+git checkout $PACKAGE_VERSION
+
+# Install package
+if ! python3 -m pip install -v .; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Install_Fails"
+    exit 1
+fi
+
+
+# Run tests if available
+if [ -d "tests" ]; then
+    if ! python3 -m pytest -v; then
+        echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+        echo "$PACKAGE_URL $PACKAGE_NAME"
+        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Install_success_but_test_Fails"
+        exit 2
+    fi
+fi
+
+echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+echo "$PACKAGE_URL $PACKAGE_NAME"
+echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass | Both_Install_and_Test_Success"
+exit 0
