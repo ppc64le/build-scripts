644acaa421de50281e661e6556c549d6462b876a
Tejas Badjate <tejas.badjate@ibm.com>
2026-09-02 16:25:34 +0530

    updated the script

--- c/c-ares/cares_ubi_10.2.sh
+++ c/c-ares/cares_ubi_10.2.sh
@@ -140,23 +140,3 @@ else
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
     exit 0
 fi
-
-
-if ! ninja test; then
-    if [ "$(cat /proc/sys/net/ipv6/conf/all/disable_ipv6 2>/dev/null)" = "1" ]; then
-        echo "------------------$PACKAGE_NAME:test_skipped_ipv6_disabled---------------------"
-        echo "$PACKAGE_URL $PACKAGE_NAME"
-        echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | IPv6_Disabled_Test_Environment"
-        exit 0
-    fi
-
-    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Install_success_but_test_Fails"
-    exit 2
-else
-    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Pass | Both_Install_and_Test_Success"
-    exit 0
-fi
