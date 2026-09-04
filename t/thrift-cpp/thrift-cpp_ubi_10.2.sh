7c2c0ddc1ef4677a07e3d20fdbacac98dd70f045
Sakshi Jain <sakshi.jain16@Sakshis-MacBook-Air.local>
2026-09-04 16:27:05 +0530

    updated script for v0.24.0

--- t/thrift-cpp/thrift-cpp_ubi_10.2.sh
+++ t/thrift-cpp/thrift-cpp_ubi_10.2.sh
@@ -2,87 +2,67 @@
 # -----------------------------------------------------------------------------
 #
 # Package       : thrift-cpp
-# Version       : 0.21.0
+# Version       : 0.24.0
 # Source repo   : https://github.com/apache/thrift
 # Tested on     : UBI:10.2
 # Language      : Python, C++
-# Ci-Check      : True
+# Ci-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Sakshi Jain <sakshi.jain16@ibm.com>
+# Maintainer    : Nayana Thorat <Nayana.Thorat1@ibm.com>
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
 #
 # ----------------------------------------------------------------------------
-
 PACKAGE_NAME=thrift-cpp
 PACKAGE_DIR=thrift
-PACKAGE_VERSION=${1:-0.21.0}
+PACKAGE_VERSION=${1:-0.24.0}
 PACKAGE_URL=https://github.com/apache/thrift
 
-FLEX_VERSION=2.6.4
-BISON_VERSION=3.8.2
-BOOST_VERSION=boost-1.81.0
+yum install -y python3.14 python3.14-pip git make cmake zlib-devel libjpeg-devel gcc-toolset-15 libevent libtool wget perl-Unicode-Normalize openssl-devel
+python3.14 -m pip install ninja setuptools
 
-CURRENT_DIR="$(pwd)"
-MAX_JOBS=${MAX_JOBS:-$(nproc)}
-
-yum install -y python3.14 python3.14-pip python3.14-devel git make openssl-devel cmake zlib-devel libjpeg-devel gcc gcc-c++ libevent libevent-devel libtool wget autoconf automake pkgconfig diffutils file
-
-# Installing flex bison
+export PATH=/opt/rh/gcc-toolset-15/root/usr/bin:$PATH
+SCRIPT_DIR=$(pwd)
+echo $SCRIPT_DIR
 
 echo "-----------flex installing------------------"
-
-cd $CURRENT_DIR
-
-wget https://github.com/westes/flex/releases/download/v${FLEX_VERSION}/flex-${FLEX_VERSION}.tar.gz
-tar -xvf flex-${FLEX_VERSION}.tar.gz
-cd flex-${FLEX_VERSION}
-
-echo "Configuring flex installation..."
-./configure --prefix=/usr/local
-
-echo "Compiling the source code for flex..."
-make -j${MAX_JOBS}
-
-echo "Installing flex..."
+wget https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz
+tar -xvf flex-2.6.4.tar.gz
+cd flex-2.6.4
+./configure --prefix=/usr
+make -j$(nproc)
 make install
+cd $SCRIPT_DIR
 
-export PATH="/usr/local/bin:${PATH}"
-
-cd $CURRENT_DIR
+echo "-----------texinfo installing------------------"
+wget https://ftp.gnu.org/gnu/texinfo/texinfo-7.2.tar.gz
+tar -xzf texinfo-7.2.tar.gz
+cd texinfo-7.2
+./configure --prefix=/usr/local
+make -j"$(nproc)"
+make install
+cd $SCRIPT_DIR
 
 echo "-------bison installing----------------------"
-
-wget https://ftp.gnu.org/gnu/bison/bison-${BISON_VERSION}.tar.gz
-tar -xvf bison-${BISON_VERSION}.tar.gz
-cd bison-${BISON_VERSION}
-
+wget https://ftp.gnu.org/gnu/bison/bison-3.8.2.tar.gz
+tar -xvf bison-3.8.2.tar.gz
+cd bison-3.8.2
 echo "Configuring bison installation..."
 ./configure --prefix=/usr/local
-
 echo "Compiling the source code bison..."
-make -j${MAX_JOBS}
-
+make -j$(nproc)
 echo "Installing bison..."
 make install
+cd $SCRIPT_DIR
 
-export PATH="/usr/local/bin:${PATH}"
+echo "-------boost installing----------------------"
 
-cd $CURRENT_DIR
-
-if ! command -v yacc >/dev/null 2>&1; then
-    ln -sf "$(command -v bison)" /usr/local/bin/yacc
-fi
-
-echo "Checking build tools:"
-command -v flex
-command -v bison
-command -v yacc
-
-# installing boost
-
-git clone https://github.com/boostorg/boost
+git clone -b boost-1.81.0 https://github.com/boostorg/boost
 cd boost
-git checkout ${BOOST_VERSION}
-git submodule update --init --recursive
+git submodule update --init
 
 mkdir Boost_prefix
 export BOOST_PREFIX=$(pwd)/Boost_prefix
@@ -93,32 +73,26 @@ LIBRARY_PATH="${BOOST_PREFIX}/lib"
 export CC=$(which gcc)
 export CXX=$(which g++)
 export target_platform=$(uname)-$(uname -m)
-CXXFLAGS="${CXXFLAGS:-} -fPIC"
+CXXFLAGS="${CXXFLAGS} -fPIC"
 TOOLSET=gcc
 
+ # http://www.boost.org/build/doc/html/bbv2/tasks/crosscompile.html
 cat <<EOF > tools/build/example/site-config.jam
 using ${TOOLSET} : : ${CXX} ;
 EOF
 
-LINKFLAGS="${LINKFLAGS:-} -L${LIBRARY_PATH}"
+LINKFLAGS="${LINKFLAGS} -L${LIBRARY_PATH}"
 
-export CXXFLAGS="$(echo "${CXXFLAGS:-}" \
-    | sed 's/ -march=[^ ]*//g' \
-    | sed 's/ -mcpu=[^ ]*//g' \
-    | sed 's/ -mtune=[^ ]*//g') -fPIC"
-
-export CFLAGS="$(echo "${CFLAGS:-}" \
-    | sed 's/ -march=[^ ]*//g' \
-    | sed 's/ -mcpu=[^ ]*//g' \
-    | sed 's/ -mtune=[^ ]*//g')"
-
-./bootstrap.sh \
+CXXFLAGS="$(echo ${CXXFLAGS} | sed 's/ -march=[^ ]*//g' | sed 's/ -mcpu=[^ ]*//g' |sed 's/ -mtune=[^ ]*//g')" \
+CFLAGS="$(echo ${CFLAGS} | sed 's/ -march=[^ ]*//g' | sed 's/ -mcpu=[^ ]*//g' |sed 's/ -mtune=[^ ]*//g')" \
+    CXX=${CXX_FOR_BUILD:-${CXX}} CC=${CC_FOR_BUILD:-${CC}} ./bootstrap.sh \
     --prefix="${BOOST_PREFIX}" \
     --without-libraries=python \
-    --with-toolset="${TOOLSET}" || (cat bootstrap.log; exit 1)
+    --with-toolset=${TOOLSET} \
+    --with-icu="${BOOST_PREFIX}" || (cat bootstrap.log; exit 1)
 
 ADDRESS_MODEL=64
-ARCHITECTURE=power
+ARCHITECTURE="power"
 ABI="sysv"
 BINARY_FORMAT="elf"
 
@@ -142,23 +116,21 @@ export CPU_COUNT=$(nproc)
     -j"${CPU_COUNT}" \
     install
 
-rm -f "${BOOST_PREFIX}/include/boost/python.hpp"
-rm -rf "${BOOST_PREFIX}/include/boost/python"
+rm "${BOOST_PREFIX}/include/boost/python.hpp"
+rm -r "${BOOST_PREFIX}/include/boost/python"
 
-cd $CURRENT_DIR
 
+cd $SCRIPT_DIR
 echo "------------------- boost installed-------------------"
 
+echo "------------------- thrift installing-------------------"
 # clone source repository
-
 git clone $PACKAGE_URL
-cd $PACKAGE_DIR
-git checkout $PACKAGE_VERSION
-
-Source_DIR=$(pwd)
+cd thrift
+git checkout v$PACKAGE_VERSION
 
 mkdir prefix
-export PREFIX=$Source_DIR/prefix
+export PREFIX=$(pwd)/prefix
 
 export BOOST_ROOT=${BOOST_PREFIX}
 export ZLIB_ROOT=/usr
@@ -167,21 +139,7 @@ export LIBEVENT_ROOT=/usr
 export OPENSSL_ROOT=/usr
 export OPENSSL_ROOT_DIR=/usr
 
-# Thrift 0.21.0 Mutex fix
-
-MUTEX_HEADER="lib/cpp/src/thrift/concurrency/Mutex.h"
-
-if [ ! -f "${MUTEX_HEADER}" ]; then
-    echo "ERROR: ${MUTEX_HEADER} not found"
-    exit 1
-fi
-
-if ! grep -q '#include <cstdint>' "${MUTEX_HEADER}"; then
-    sed -i '/#include <thrift\/TNonCopyable.h>/a #include <cstdint>' "${MUTEX_HEADER}"
-fi
-
 ./bootstrap.sh
-
 ./configure --prefix=$PREFIX \
     --with-python=no \
     --with-py3=no \
@@ -194,6 +152,7 @@ fi
     --with-haxe=no \
     --with-rs=no \
     --with-cpp=yes \
+    --with-PACKAGE=yes \
     --with-zlib=$ZLIB_ROOT \
     --with-libevent=$LIBEVENT_ROOT \
     --with-boost=$BOOST_ROOT \
@@ -201,41 +160,24 @@ fi
     --enable-tests=no \
     --enable-tutorial=no
 
-if ! make -j${MAX_JOBS}; then
-    echo "------------------$PACKAGE_NAME:Build_fails-------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Build_Fails"
-    exit 1
-fi
+make -j$(nproc)
+make install
 
-if ! make install; then
-    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Install_Fails"
-    exit 1
-fi
-
-cd $Source_DIR
-mkdir -p local/thriftcpp
-
-cp -r $PREFIX/* local/thriftcpp/
 
 #pyproject.toml
-
 wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/t/thrift-cpp/pyproject.toml
 sed -i s/{PACKAGE_VERSION}/$PACKAGE_VERSION/g pyproject.toml
 
-#test
+mkdir -p local/thriftcpp
+cp -r prefix/* local/thriftcpp/
 
-if ! make -k check; then
-    echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+python3.14 -m pip wheel -w $SCRIPT_DIR -vv --no-build-isolation --no-deps .
+
+#install
+if ! (python3.14 -m pip install .) ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
-    exit 2
-else
-    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-    exit 0
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
 fi
 
