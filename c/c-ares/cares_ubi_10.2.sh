c0136b6e80ce6d6483e07633af6a1d63d1eb810e
Tejas Badjate <tejas.badjate@ibm.com>
2026-09-04 13:36:50 +0530

    updated script

--- c/c-ares/cares_ubi_10.2.sh
+++ c/c-ares/cares_ubi_10.2.sh
@@ -131,9 +131,8 @@ export AR="${BINUTILS_INSTALL_DIR}/bin/ar"
 export RANLIB="${BINUTILS_INSTALL_DIR}/bin/ranlib"
 
 # install dependency
-python3.14 -m pip install --upgrade pip
+# python3.14 -m pip install --upgrade pip
 python3.14 -m pip install setuptools ninja build wheel auditwheel
-
 # clone source repository
 cd $CURRENT_DIR
 git clone $PACKAGE_URL
