32d113c412ea91386455f10acc828fadda011de2
shivansh-ibm <shivansh.s1@ibm.com>
2026-09-05 23:54:32 +0530

    Update and rename cytoolz_1.0.1_ubi_10.1.sh to cytoolz_1.1.0_ubi_10.2.sh

--- c/cytoolz/cytoolz_1.1.0_ubi_10.2.sh
+++ c/cytoolz/cytoolz_1.1.0_ubi_10.2.sh
@@ -2,9 +2,9 @@
 # -----------------------------------------------------------------------------
 #
 # Package          : cytoolz
-# Version          : 1.0.1
+# Version          : 1.1.0
 # Source repo      : https://github.com/pytoolz/cytoolz.git
-# Tested on        : UBI:10.1
+# Tested on        : UBI:10.2
 # Language         : Python
 # Ci-Check     : True
 # Script License   : Apache License, Version 2 or later
@@ -20,12 +20,13 @@
 
 # Variables
 PACKAGE_NAME=cytoolz
-PACKAGE_VERSION=${1:-1.0.1}
+PACKAGE_VERSION=${1:-1.1.0}
 PACKAGE_URL=https://github.com/pytoolz/cytoolz.git
 PACKAGE_DIR=./cytoolz
+CURRENT_DIR=$(pwd)
 
 # Install necessary system dependencies
-yum install -y git gcc gcc-c++ make cmake wget openssl-devel bzip2-devel libffi-devel zlib-devel python3.12-devel python3.12-pip
+yum install -y git gcc gcc-c++ make cmake wget openssl-devel bzip2-devel libffi-devel zlib-devel python3.14-devel python3.14-pip
 
 
 # Clone the repository
@@ -34,11 +35,11 @@ cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 # Install additional dependencies
-python3.12 -m pip install Cython pytest setuptools
-python3.12 -m pip install -e git+https://github.com/pytoolz/toolz.git#egg=toolz
+python3.14 -m pip install Cython pytest setuptools
+python3.14 -m pip install toolz==1.1.0
 
 # Install
-if ! python3.12 setup.py install ; then
+if ! python3.14 setup.py install ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
@@ -46,7 +47,10 @@ if ! python3.12 setup.py install ; then
 fi
 
 # Run tests
-if ! pytest cytoolz/; then
+cd $CURRENT_DIR
+mkdir $CURRENT_DIR/test_dir
+cp -r $CURRENT_DIR/$PACKAGE_NAME/cytoolz/tests $CURRENT_DIR/test_dir
+if ! pytest $CURRENT_DIR/test_dir; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
