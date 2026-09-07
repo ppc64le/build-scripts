38b6dc848d9943b561eb9c03412ca388e5e62d0e
Sakshi Jain <sakshi.jain16@Sakshis-MacBook-Air.local>
2026-09-07 13:33:37 +0530

    updated to gcc toolset 15

--- n/numpy/numpy_2.5.0_ubi_10.2.sh
+++ n/numpy/numpy_2.5.0_ubi_10.2.sh
@@ -6,34 +6,51 @@
 # Source repo   : https://github.com/numpy/numpy
 # Tested on     : UBI:10.2
 # Language      : Python
-# Ci-Check  : True
+# Ci-Check      : True
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Sakshi Jain <sakshi.jain16@ibm.com>
 #
-# Disclaimer: This script has been tested in root mode on given
-# ==========  platform using the mentioned version of the package.
-#             It may not work as expected with newer versions of the
-#             package and/or distribution. In such case, please
-#             contact "Maintainer" of this script.
-#
-# ----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 PACKAGE_NAME=numpy
 PACKAGE_VERSION=${1:-v2.5.0}
 PACKAGE_URL=https://github.com/numpy/numpy.git
 PACKAGE_DIR=numpy
 CURRENT_DIR="${PWD}"
 
-yum install -y wget python3.14 python3.14-devel python3.14-pip git gcc gcc-c++ gcc-gfortran make pkgconf-pkg-config 
+yum install -y wget python3.14 python3.14-devel python3.14-pip git gcc-toolset-15-gcc gcc-toolset-15-gcc-c++ gcc-toolset-15-gcc-gfortran make pkgconf-pkg-config
+
+# Activate GCC Toolset 15
+if [[ -f /opt/rh/gcc-toolset-15/enable ]]; then
+    source /opt/rh/gcc-toolset-15/enable
+elif [[ -d /opt/rh/gcc-toolset-15/root/usr/bin ]]; then
+    export PATH="/opt/rh/gcc-toolset-15/root/usr/bin:$PATH"
+    export LD_LIBRARY_PATH="/opt/rh/gcc-toolset-15/root/usr/lib64:${LD_LIBRARY_PATH:-}"
+else
+    echo "ERROR: gcc-toolset-15 not found"
+    exit 1
+fi
+
+# GCC Toolset 15 compilers
+export CC="$(command -v gcc)"
+export CXX="$(command -v g++)"
+export FC="$(command -v gfortran)"
+export AR="$(command -v ar)"
+export LD="$(command -v ld)"
+export NM="$(command -v nm)"
+export OBJCOPY="$(command -v objcopy)"
+export OBJDUMP="$(command -v objdump)"
+export RANLIB="$(command -v ranlib)"
+export STRIP="$(command -v strip)"
+export READELF="$(command -v readelf)"
+
 python3.14 -m pip install --upgrade pip
 python3.14 -m pip install tox Cython pytest hypothesis wheel meson ninja build meson-python patchelf
 
-echo " --------------------------------------------------- OpenBlas Installing --------------------------------------------------- "
-
-# OpenBLAS version and source
+# OpenBLAS
 OPENBLAS_VERSION=v0.3.33
 OPENBLAS_URL=https://github.com/OpenMathLib/OpenBLAS
-
-# Clone OpenBLAS
+OPENBLAS_PREFIX=/opt/openblas
 
 git clone "${OPENBLAS_URL}"
 cd OpenBLAS
@@ -60,75 +77,70 @@ if [ -n "${FFLAGS:-}" ]; then
     export LAPACK_FFLAGS="${FFLAGS}"
 fi
 
-make -j"${MAX_JOBS}" TARGET="${TARGET}" BUILD_BFLOAT16="${BUILD_BFLOAT16}" BINARY=64 USE_OPENMP="${USE_OPENMP}" USE_THREAD="${USE_THREAD}" NUM_THREADS="${NUM_THREADS}" DYNAMIC_ARCH="${DYNAMIC_ARCH}" INTERFACE64="${INTERFACE64}" NO_AFFINITY="${NO_AFFINITY}" CFLAGS="${CF}" FFLAGS="${FFLAGS:-}"
+make -j"${MAX_JOBS:-$(nproc)}" \
+    TARGET="${TARGET}" \
+    BUILD_BFLOAT16="${BUILD_BFLOAT16}" \
+    BINARY=64 \
+    USE_OPENMP="${USE_OPENMP}" \
+    USE_THREAD="${USE_THREAD}" \
+    NUM_THREADS="${NUM_THREADS}" \
+    DYNAMIC_ARCH="${DYNAMIC_ARCH}" \
+    INTERFACE64="${INTERFACE64}" \
+    NO_AFFINITY="${NO_AFFINITY}" \
+    CFLAGS="${CF}" \
+    FFLAGS="${FFLAGS:-}"
 
 make install PREFIX="${OPENBLAS_PREFIX}"
 
 export LD_LIBRARY_PATH="${OPENBLAS_PREFIX}/lib:${OPENBLAS_PREFIX}/lib64:${LD_LIBRARY_PATH:-}"
+export LIBRARY_PATH="${OPENBLAS_PREFIX}/lib:${OPENBLAS_PREFIX}/lib64:${LIBRARY_PATH:-}"
+export CPATH="${OPENBLAS_PREFIX}/include:${CPATH:-}"
 export PKG_CONFIG_PATH="${OPENBLAS_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
 
 pkg-config --modversion openblas
 
-echo "-----------------------------------------------------Installed OpenBLAS-----------------------------------------------------"
+cd "${CURRENT_DIR}"
 
-cd $CURRENT_DIR
-
-
-#clone package
-git clone $PACKAGE_URL
-cd  $PACKAGE_NAME
-git checkout $PACKAGE_VERSION
+# NumPy
+git clone "${PACKAGE_URL}"
+cd "${PACKAGE_DIR}"
+git checkout "${PACKAGE_VERSION}"
 git submodule update --init
-export CC=/usr/bin/gcc
-export CXX=/usr/bin/g++
-export FC=/usr/bin/gfortran
-export AR=/usr/bin/ar
-export LD=/usr/bin/ld
-export NM=/usr/bin/nm
-export OBJCOPY=/usr/bin/objcopy
-export OBJDUMP=/usr/bin/objdump
-export RANLIB=/usr/bin/ranlib
-export STRIP=/usr/bin/strip
-export READELF=/usr/bin/readelf
+
 UNAME_M=$(uname -m)
 case "$UNAME_M" in
     ppc64*)
-        # Optimizations trigger compiler bug.
-         export CXXFLAGS="$(echo ${CXXFLAGS} | sed -e 's/ -fno-plt//')"
-         export CFLAGS="$(echo ${CFLAGS} | sed -e 's/ -fno-plt//')"
-        ;;
-    *)
-        EXTRA_OPTS=""
+        export CXXFLAGS="$(echo "${CXXFLAGS:-}" | sed -e 's/ -fno-plt//')"
+        export CFLAGS="$(echo "${CFLAGS:-}" | sed -e 's/ -fno-plt//')"
         ;;
 esac
 
-if ! (python3.14 -m pip install . --no-build-isolation);then
-    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+if ! python3.14 -m pip install . --no-build-isolation; then
+    echo "------------------${PACKAGE_NAME}:Install_fails-------------------------------------"
+    echo "${PACKAGE_URL} ${PACKAGE_NAME}"
+    echo "${PACKAGE_NAME} | ${PACKAGE_URL} | ${PACKAGE_VERSION} | GitHub | Fail | Install_Fails"
     exit 1
 fi
 
-if ! python3.14 -m build --wheel --no-isolation --outdir="$CURRENT_DIR/"; then
-        echo "============ Wheel Creation Failed for Python $PYTHON_VERSION (without isolation) ================="
-        echo "Attempting to build with isolation..."
+if ! python3.14 -m build --wheel --no-isolation --outdir="${CURRENT_DIR}/"; then
+    echo "============ Wheel Creation Failed for Python 3.14 (without isolation) ============"
 
-        # Attempt to build the wheel without isolation
-        if ! python3.14 -m build --wheel --outdir="$CURRENT_DIR/"; then
-            echo "============ Wheel Creation Failed for Python $PYTHON_VERSION ================="
-        fi
+    if ! python3.14 -m build --wheel --outdir="${CURRENT_DIR}/"; then
+        echo "============ Wheel Creation Failed for Python 3.14 ==============================="
+        exit 1
+    fi
 fi
 
 export CFLAGS="-DCYTHON_PEP489_MULTI_PHASE_INIT=0"
 
-if ! (tox -e py3); then
-    echo "--------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+if ! tox -e py3; then
+    echo "--------------------${PACKAGE_NAME}:Install_success_but_test_fails---------------------"
+    echo "${PACKAGE_URL} ${PACKAGE_NAME}"
+    echo "${PACKAGE_NAME} | ${PACKAGE_URL} | ${PACKAGE_VERSION} | GitHub | Fail | Install_success_but_test_Fails"
     exit 2
 else
-    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    echo "------------------${PACKAGE_NAME}:Install_&_test_both_success-------------------------"
+    echo "${PACKAGE_URL} ${PACKAGE_NAME}"
+    echo "${PACKAGE_NAME} | ${PACKAGE_URL} | ${PACKAGE_VERSION} | GitHub | Pass | Both_Install_and_Test_Success"
     exit 0
 fi
