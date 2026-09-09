ada69d8f6ff2e97c3bfdf0ce217b4c61dd90545b
Veenious Geevarghese <Veenious.Geevarghese@ibm.com>
2026-09-09 15:49:04 +0530

    Fixed workflow issue

--- a/azure-core-cpp/azure-core-cpp_1.16.3_ubi_9.8.sh
+++ a/azure-core-cpp/azure-core-cpp_1.16.3_ubi_9.8.sh
@@ -22,7 +22,6 @@ PACKAGE_NAME=azure-core-cpp
 PACKAGE_VERSION=${1:-"1.16.3"}
 PACKAGE_URL=https://github.com/Azure/azure-sdk-for-cpp
 WORKING_DIR=$(pwd)
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Detect CPU generation and set optimization flags accordingly
 if grep -q "POWER10" /proc/cpuinfo 2>/dev/null; then
@@ -159,8 +158,7 @@ fi
 cd "$SOURCE_DIR"
 python3 -m pip install --upgrade pip setuptools wheel build
 
-cp "$SCRIPT_DIR/pyproject.toml" .
-
+wget https://raw.githubusercontent.com/ppc64le/build-scripts/refs/heads/master/a/azure-core-cpp/pyproject.toml
 sed -i "s/{PACKAGE_VERSION}/${PACKAGE_VERSION}/g" pyproject.toml
 
 if ! python3 -m pip install . --no-build-isolation; then
