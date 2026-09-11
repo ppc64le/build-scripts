8e76b4545a2adc042b72c170018a8605aec09e5d
shivansh-ibm <shivansh.s1@ibm.com>
2026-09-11 11:44:13 +0530

    Update cx-oracle_8.3.0_ubi_10.2.sh

--- c/cx-oracle/cx-oracle_8.3.0_ubi_10.2.sh
+++ c/cx-oracle/cx-oracle_8.3.0_ubi_10.2.sh
@@ -8,7 +8,7 @@
 # Language         : C
 # Ci-Check         : True
 # Script License   : Apache License, Version 2 or later
-# Maintainer       : 
+# Maintainer       : Shivansh.s1@ibm.com
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
