7980edd633078ca95b3c1211fa08052980e24711
Yogita-Kulkarni19 <yogita.kulkarni1@ibm.com>
2026-09-23 15:55:00 +0530

    Created ubi10.2 tag

--- o/openblas/openblas_ubi_10.2.sh
+++ o/openblas/openblas_ubi_10.2.sh
@@ -106,14 +106,14 @@ CFLAGS="${CF}" FFLAGS="${FFLAGS}" \
 #install pyproject.toml
 wget https://raw.githubusercontent.com/i-wheels-cpd/build-scripts/refs/heads/main/o/openblas/pyproject.toml
 sed -i "s/{PACKAGE_VERSION}/$PACKAGE_VERSION/g" pyproject.toml
-sed -i 's/plat-name = "linux_ppc64le"//g' pyproject.toml
+sed -i 's/plat-name = "linux_ppc64le"/plat-name = "manylinux_2_39_ppc64le"/g' pyproject.toml
 
 # Finalize OpenBLAS package layout
 rm -rf "${PREFIX}/bin"
 
 
 #building wheel
-python3.14 -m pip wheel -v . --no-build-isolation --no-deps
+python3.14 -m pip wheel -v . --no-build-isolation --no-deps -w "$CURRENT_DIR/"
 
 echo "------------------------Installing Python package-------------------"
 
